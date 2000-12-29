104c1889ed4028a8398db70504cb9ba8d81bf15b
Erik Walthinsen <omega@temple-baptist.org>
2000-12-29 05:38:06 +0000

    Updated copyright notices.
    Original commit message from CVS:
    Updated copyright notices.

--- plugins/elements/gstaudiosrc.c
+++ plugins/elements/gstaudiosrc.c
@@ -1,5 +1,8 @@
-/* Gnome-Streamer
- * Copyright (C) <1999> Erik Walthinsen <omega@cse.ogi.edu>
+/* GStreamer
+ * Copyright (C) 1999,2000 Erik Walthinsen <omega@cse.ogi.edu>
+ *                    2000 Wim Taymans <wtay@chello.be>
+ *
+ * gstaudiosrc.c: 
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
