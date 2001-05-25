75e66711f9454f0cfd64a16dc5074b587feae5a1
Wim Taymans <wim.taymans@gmail.com>
2001-05-25 20:41:07 +0000

    some minor fixes..
    Original commit message from CVS:
    some minor fixes..

--- test/dv2mp1.c
+++ test/dv2mp1.c
@@ -34,8 +34,8 @@ main (int argc,char *argv[])
 
   dvdec = gst_elementfactory_make ("dvdec", "decoder");
   cspace = gst_elementfactory_make ("colorspace", "cspace");
-  //videoscale = gst_elementfactory_make ("videoscale", "videoscale");
-  //gtk_object_set (GTK_OBJECT (videoscale), "width", 352, "height",288, NULL);
+  videoscale = gst_elementfactory_make ("videoscale", "videoscale");
+  gtk_object_set (GTK_OBJECT (videoscale), "width", 352, "height",288, NULL);
   encoder = gst_elementfactory_make ("mpeg2enc", "mpeg2enc");
   fdsink = gst_elementfactory_make ("fdsink", "fdsink");
 
@@ -45,13 +45,13 @@ main (int argc,char *argv[])
   gst_bin_add (GST_BIN (bin), GST_ELEMENT (src));
   gst_bin_add (GST_BIN (bin), GST_ELEMENT (dvdec));
   gst_bin_add (GST_BIN (bin), GST_ELEMENT (cspace));
-  //gst_bin_add (GST_BIN (bin), GST_ELEMENT (videoscale));
+  gst_bin_add (GST_BIN (bin), GST_ELEMENT (videoscale));
   gst_bin_add (GST_BIN (bin), GST_ELEMENT (encoder));
   gst_bin_add (GST_BIN (bin), GST_ELEMENT (fdsink));
 
   gst_element_connect (src, "src", dvdec, "sink");
-  gst_element_connect (cspace, "src", encoder, "sink");
-  //gst_element_connect (videoscale, "src", encoder, "sink");
+  gst_element_connect (cspace, "src", videoscale, "sink");
+  gst_element_connect (videoscale, "src", encoder, "sink");
   gst_element_connect (encoder, "src", fdsink, "sink");
   gst_element_connect (dvdec, "video", cspace, "sink");
 
