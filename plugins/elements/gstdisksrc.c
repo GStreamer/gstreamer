13e5c7ba91e903c63836034ef25cedc5436a8540
Wim Taymans <wim.taymans@gmail.com>
2001-06-30 00:35:55 +0000

    Fix an endless loop at EOS.. this is a hack...
    Original commit message from CVS:
    Fix an endless loop at EOS.. this is a hack...

--- plugins/elements/gstdisksrc.c
+++ plugins/elements/gstdisksrc.c
@@ -238,7 +238,9 @@ gst_disksrc_get (GstPad *pad)
   if (src->curoffset >= src->size) {
     GST_DEBUG (0,"map offset %ld >= size %ld --> eos\n", src->curoffset, src->size);
     gst_pad_set_eos (pad);
-    return NULL;
+    buf =  gst_buffer_new();
+    GST_BUFFER_FLAG_SET (buf, GST_BUFFER_EOS);
+    return buf;
   }
 
   /* create the buffer */
