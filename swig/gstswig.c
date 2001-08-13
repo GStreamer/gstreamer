2f1ce27730760cfa20d59427c089eb81a01b5a66
Thomas Vander Stichele <thomas@apestaart.org>
2001-08-13 14:45:01 +0000

    added info trace as well
    Original commit message from CVS:
    added info trace as well

--- swig/gstswig.c
+++ swig/gstswig.c
@@ -3,14 +3,16 @@
 
 void init ()
 {
-  int argc = 2;
+  int argc = 3;
   char **argv = NULL;
 
-  argv = (char **) malloc (sizeof (char *) * 2);
+  argv = (char **) malloc (sizeof (char *) * 3);
   argv[0] = (char *) malloc (80);
   argv[1] = (char *) malloc (80);
+  argv[2] = (char *) malloc (80);
   strcpy (argv[0], "swigged gst");
   strcpy (argv[1], "--gst-debug-mask=-1");
+  strcpy (argv[2], "--gst-info-mask=-1");
   
   gst_init (&argc, &argv);
 }
