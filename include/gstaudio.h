1d7203ab8335fa7c3bf5219e86bc96a2fe4afbb6
Andy Wingo <wingo@pobox.com>
2001-08-24 05:26:39 +0000

    defines needed for passthrough and its filterstamped descendants (just speed and volume for now)
    Original commit message from CVS:
    defines needed for passthrough and its filterstamped descendants (just speed and
    volume for now)

--- include/gstaudio.h
+++ include/gstaudio.h
@@ -2,7 +2,7 @@
  * Copyright (C) 1999,2000 Erik Walthinsen <omega@cse.ogi.edu>
  *                    2000 Wim Taymans <wtay@chello.be>
  *
- * gstaudio.h: 
+ * gstaudio.h: convenience macros for pad capabilities
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -20,7 +20,28 @@
  * Boston, MA 02111-1307, USA.
  */
 
+/* for people that are looking at this source: the purpose of these defines is
+ * to make GstCaps a bit easier, in that you don't have to know all of the
+ * properties that need to be defined. you can just use these macros. currently
+ * (8/01) the only plugins that use these are the passthrough, speed, volume,
+ * and [de]interleave plugins. so. these are for convenience only, and do not
+ * specify the 'limits' of gstreamer. you might also use these definitions as a
+ * base for making your own caps, if need be.
+ *
+ * for example, to make a source pad that can output mono streams of either
+ * float or int:
 
+    template = gst_padtemplate_new 
+      ("sink", GST_PAD_SINK, GST_PAD_ALWAYS,
+      gst_caps_append(gst_caps_new ("sink_int",  "audio/raw",
+                                    GST_AUDIO_INT_PAD_TEMPLATE_PROPS),
+                      gst_caps_new ("sink_float", "audio/raw",
+                                    GST_AUDIO_FLOAT_MONO_PAD_TEMPLATE_PROPS)),
+      NULL);
+
+    srcpad = gst_pad_new_from_template(template,"src");
+
+ * Andy Wingo, 18 August 2001 */
 
 #define GST_AUDIO_INT_PAD_TEMPLATE_PROPS \
         gst_props_new (\
@@ -33,6 +54,31 @@
             					),\
             "width",            GST_PROPS_INT_RANGE (8, 16),\
             "depth",            GST_PROPS_INT_RANGE (8, 16),\
-            "rate",             GST_PROPS_INT_RANGE (4000, 48000),\
-            "channels",         GST_PROPS_INT_RANGE (1, 2),\
-          NULL))
+            "rate",             GST_PROPS_INT_RANGE (4000, 96000),\
+            "channels",         GST_PROPS_INT_RANGE (1, G_MAXINT),\
+          NULL)
+
+#define GST_AUDIO_INT_MONO_PAD_TEMPLATE_PROPS \
+        gst_props_new (\
+          "format",             GST_PROPS_STRING ("int"),\
+            "law",              GST_PROPS_INT (0),\
+            "endianness",       GST_PROPS_INT (G_BYTE_ORDER),\
+            "signed",           GST_PROPS_LIST (\
+            					  GST_PROPS_BOOLEAN (TRUE),\
+            					  GST_PROPS_BOOLEAN(FALSE)\
+            					),\
+            "width",            GST_PROPS_INT_RANGE (8, 16),\
+            "depth",            GST_PROPS_INT_RANGE (8, 16),\
+            "rate",             GST_PROPS_INT_RANGE (4000, 96000),\
+            "channels",         GST_PROPS_INT (1),\
+          NULL)
+
+#define GST_AUDIO_FLOAT_MONO_PAD_TEMPLATE_PROPS \
+        gst_props_new (\
+          "format",             GST_PROPS_STRING ("float"),\
+            "layout",           GST_PROPS_STRING ("gfloat"),\
+            "intercept",        GST_PROPS_FLOAT (0.0),\
+            "slope",            GST_PROPS_FLOAT (1.0),\
+            "rate",             GST_PROPS_INT_RANGE (4000, 96000),\
+            "channels",         GST_PROPS_INT (1),\
+            NULL)
