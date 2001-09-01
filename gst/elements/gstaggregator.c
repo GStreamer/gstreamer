d374b67d1945160beda0f326af1a5a9178b007f6
David I. Lehn <dlehn@users.sourceforge.net>
2001-09-01 01:05:11 +0000

    typo fix
    Original commit message from CVS:
    typo fix

--- gst/elements/gstaggregator.c
+++ gst/elements/gstaggregator.c
@@ -2,7 +2,7 @@
  * Copyright (C) 1999,2000 Erik Walthinsen <omega@cse.ogi.edu>
  *                    2000 Wim Taymans <wim.taymans@chello.be>
  *
- * gstaggregator.c: Aggregator element, one in N out
+ * gstaggregator.c: Aggregator element, N in 1 out
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
