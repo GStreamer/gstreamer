1aff968e49e7c9774e0b9c38b34f4a0b15189071
Erik Walthinsen <omega@temple-baptist.org>
2001-09-05 21:36:24 +0000

    and moved all the architecture- specific code out of the many header files and put them all in gstarch.h.
    Original commit message from CVS:
    Added MIPS support to cothreads (untested), and moved all the architecture-
    specific code out of the many header files and put them all in gstarch.h.

--- gst/gstarch.h
+++ gst/gstarch.h
@@ -27,16 +27,123 @@
 #include "config.h"
 #endif
 
+
+
+/***** Intel x86 *****/
 #if defined(HAVE_CPU_I386)
-#include "gsti386.h"
+#define GST_ARCH_SET_SP(stackpointer) \
+  __asm__( "movl %0, %%esp\n" : : "r"(stackpointer) );
+
+#define GST_ARCH_CALL(target) \
+    __asm__("call *%0" : : "r"(target) );
+
+// assuming the stackframe is 16 bytes
+#define GST_ARCH_SETUP_STACK(sp) sp -= 4
+
+
+
+/***** PowerPC *****/
 #elif defined (HAVE_CPU_PPC)
-#include "gstppc.h"
+
+// should bring this in line with others and use an "r"
+#define GST_ARCH_SET_SP(stackpointer) \
+    __asm__("lwz 1,%0" : : "m"(stackpointer))
+  
+#define GST_ARCH_CALL(target) \
+    __asm__( "mr 0,%0\n\t" \
+             "mtlr 0\n\t" \
+             "blrl" : : "r"(target) );
+  
+struct minimal_ppc_stackframe {
+    unsigned long back_chain;
+    unsigned long LR_save;
+    unsigned long unused1;
+    unsigned long unused2;
+};
+
+#define GST_ARCH_SETUP_STACK(sp) \
+    sp = ((unsigned long *)(sp)) - 4; \
+    ((struct minimal_ppc_stackframe *)sp)->back_chain = 0;
+
+
+
+/***** DEC[/Compaq/HP?/Intel?] Alpha *****/
 #elif defined(HAVE_CPU_ALPHA)
-#include "gstalpha.h"
+
+#define GST_ARCH_SET_SP(stackpointer) \
+    __asm__("bis $31,%0,$30" : : "r"(stackpointer));
+
+#define GST_ARCH_CALL(target) \
+    __asm__( "bis $31,%0,$27\n\t" \
+             "jsr $26,($27),0" : : "r"(target) );
+
+// Need to get more information about the stackframe format
+// and get the fields more correct.  Check GDB sources maybe?
+struct minimal_stackframe {
+    unsigned long back_chain;
+    unsigned long LR_save;
+    unsigned long unused1;
+    unsigned long unused2;
+};
+
+#define GST_ARCH_SETUP_STACK(sp) \
+    sp = ((unsigned long *)(sp)) - 4; \
+    ((struct minimal_stackframe *)sp)->back_chain = 0;
+
+
+
+/***** ARM *****/
 #elif defined(HAVE_CPU_ARM)
-#include "gstarm.h"
+
+#define GST_ARCH_SET_SP(stackpointer) \
+    __asm__( "mov sp, %0" : : "r"(stackpointer));
+
+#define GST_ARCH_CALL(target) \
+    __asm__( "mov pc, %0" : : "r"(target) );
+
+// Need to get more information about the stackframe format
+// and get the fields more correct.  Check GDB sources maybe?
+
+#define GST_ARCH_SETUP_STACK(sp) sp -= 4
+
+
+
+/***** Sun SPARC *****/
 #elif defined(HAVE_CPU_SPARC)
-#include "gstsparc.h"
+
+#define GST_ARCH_SET_SP(stackpointer) \
+    __asm__( "ta 3\n\t" \
+             "mov %0, %%sp" : : "r"(stackpointer));
+
+#define GST_ARCH_CALL(target) \
+    __asm__( "call %0,0\n\t" \
+             "nop" : : "r"(target) );
+
+#define GST_ARCH_PRESETJMP() \
+    __asm__( "ta 3" );
+
+// Need to get more information about the stackframe format
+// and get the fields more correct.  Check GDB sources maybe?
+
+#define GST_ARCH_SETUP_STACK(sp) sp -= 4
+
+
+
+/***** MIPS *****/
+#elif defined(HAVE_CPU_MIPS)
+
+#define GST_ARCH_SET_SP(stackpointer) \
+    __asm__("move $sp,%0\n\t" : : "r"(stackpointer));
+
+#define GST_ARCH_CALL(target) \
+    __asm__("move $25,%1\n\t"	// call via $25 \
+            "jal  $25\n\t" : : "r"(target));
+
+// assuming the stackframe is 16 bytes
+#define GST_ARCH_SETUP_STACK(sp) sp -= 4
+    
+
+
 #else
 #error Need to know about this architecture, or have a generic implementation
 #endif
