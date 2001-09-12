7cfa1521d95d2d55c568fea5ccaf46c32e58703a
Steve Baker <steve@stevebaker.org>
2001-09-12 11:19:57 +0000

    correct usage of gtk_arg_copy
    Original commit message from CVS:
    correct usage of gtk_arg_copy

--- gst/gobject2gtk.h
+++ gst/gobject2gtk.h
@@ -230,7 +230,7 @@ struct _GParamSpec {
 };
 
 #define g_value_init(value,t)			((value)->type = (t))
-#define g_value_copy			gtk_arg_copy
+#define g_value_copy(src_val,dest_val)	(dest_val = gtk_arg_copy(src_val,dest_val))
 #define g_value_unset(val)
 
 #define g_object_class_install_property		g2g_object_class_install_property
