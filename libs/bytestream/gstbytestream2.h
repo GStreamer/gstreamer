8f3cfa70e6645d21c0c5be9922dba2dce87bf35d
Erik Walthinsen <omega@temple-baptist.org>
2001-09-21 06:13:42 +0000

    I'm too lazy to comment this
    Original commit message from CVS:
    *** empty log message ***

--- libs/bytestream/gstbytestream2.h
+++ libs/bytestream/gstbytestream2.h
@@ -12,15 +12,8 @@ typedef struct _GstByteStream2 GstByteStream2;
 struct _GstByteStream2 {
   GstPad *pad;
 
-  guint64 readptr;
-  guint64 flushptr;
-  guint64 size;
-
-  GstBuffer *curbuf;
-  guint32 curbufavail;
-
   GSList *buflist;
-  gint listcount;
+  guint32 headbufavail;
   guint32 listavail;
 };
 
@@ -30,5 +23,6 @@ GstBuffer *		gst_bytestream2_read		(GstByteStream2 *bs, guint32 len);
 GstBuffer *		gst_bytestream2_peek		(GstByteStream2 *bs, guint32 len);
 gboolean		gst_bytestream2_flush		(GstByteStream2 *bs, guint32 len);
 guint8 *		gst_bytestream2_peek_bytes	(GstByteStream2 *bs, guint32 len);
+void gst_bytestream2_print_status(GstByteStream2 *bs);
 
 #endif /* __GST_BYTESTREAM2_H__ */
