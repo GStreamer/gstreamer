de4882210441222d922cbc5acd34cb53f2bfe407
Erik Walthinsen <omega@temple-baptist.org>
2001-10-21 02:06:12 +0000

    added empty string for category 31, lest the coloration break
    Original commit message from CVS:
    added empty string for category 31, lest the coloration break

--- gst/gstinfo.c
+++ gst/gstinfo.c
@@ -116,6 +116,7 @@ const gchar *_gst_category_colors[32] = {
   [GST_CAT_PARAMS]		= "00;30;43",		// !!
 
   [GST_CAT_CALL_TRACE]		= "",
+  [31]				= "",
 };
 
 /* colorization hash - DEPRACATED in favor of above */
