90160b020289663b69e3d0506af1e9e2110c3cee
Thomas Vander Stichele <thomas@apestaart.org>
2001-12-06 21:40:23 +0000

    fixed FIXME's
    Original commit message from CVS:
    fixed FIXME's

--- tests/old/testsuite/capsnego/converter2.c
+++ tests/old/testsuite/capsnego/converter2.c
@@ -206,10 +206,10 @@ main (int argc, char *argv[])
   g_print ("result: %d, converter status: %d %d, target: %d\n\n", result, 
 		  converter_in, converter_out, target_rate);
 
-  /* result = FIXME */ gst_pad_connect (srcpad, sinkconvpad);
+  result = gst_pad_try_connect (srcpad, sinkconvpad);
   g_print ("pad connect 1: %d\n", result);
   overall &= (result == TRUE);
-  /* result = FIXME */ gst_pad_connect (srcconvpad, sinkpad);
+  result = gst_pad_try_connect (srcconvpad, sinkpad);
   g_print ("pad connect 2: %d\n", result);
   overall &= (result == TRUE);
 
