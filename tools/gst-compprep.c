c0110cf644fe9dc930099f3d9b0b795871c4feab
David I. Lehn <dlehn@users.sourceforge.net>
2002-01-12 01:25:14 +0000

    convert 'adding factory' message to a plugin loading debug message
    Original commit message from CVS:
    convert 'adding factory' message to a plugin loading debug message

--- tools/gst-compprep.c
+++ tools/gst-compprep.c
@@ -42,7 +42,7 @@ int main(int argc,char *argv[]) {
       xmlNewChild (factorynode, NULL, "name", gst_object_get_name (GST_OBJECT (factory)));
 
       element = gst_elementfactory_create(factory,"element");
-      fprintf(stdout,"adding factory %s\n", 
+      GST_DEBUG(GST_CAT_PLUGIN_LOADING, "adding factory %s\n", 
               gst_object_get_name (GST_OBJECT (factory)));
       if (element == NULL) {
         fprintf(stderr,"couldn't construct element from factory %s\n", 
