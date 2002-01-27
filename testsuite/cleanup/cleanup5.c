49ae4007afe1597894dd5a7c1e55ad0a164ff7dc
Benjamin Otte <otte@gnome.org>
2002-01-27 22:10:10 +0000

    make the test have output
    Original commit message from CVS:
    make the test have output

--- testsuite/cleanup/cleanup5.c
+++ testsuite/cleanup/cleanup5.c
@@ -5,12 +5,18 @@ int main(int argc,char *argv[])
   GstElement *bin, *element;
   gint i=10000;
 
+  free (malloc(8)); /* -lefence */
+
   gst_init (&argc, &argv);
+
+  g_mem_chunk_info ();
 
   bin = gst_pipeline_new ("pipeline");
 
   while (i--)
   {
+    fprintf (stderr, "+");
+
     element = gst_elementfactory_make ("tee", "tee");
     if (!element) 
       break;
@@ -19,5 +25,11 @@ int main(int argc,char *argv[])
 
     gst_bin_add (GST_BIN (bin), element);
     gst_bin_remove (GST_BIN (bin), element);
+
   }
+  fprintf (stderr, "+\n");
+
+  gst_object_unref (GST_OBJECT (bin));
+
+  g_mem_chunk_info ();
 }
