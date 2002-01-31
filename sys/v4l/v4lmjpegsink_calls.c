ab9253b9fe9b1af9428445d55188236134733c8b
Ronald S. Bultje <rbultje@ronald.bitfreak.net>
2002-01-31 22:37:15 +0000

    Oops - last minute fix
    Original commit message from CVS:
    Oops - last minute fix

--- sys/v4l/v4lmjpegsink_calls.c
+++ sys/v4l/v4lmjpegsink_calls.c
@@ -226,7 +226,7 @@ gst_v4lmjpegsink_set_playback (GstV4lMjpegSink *v4lmjpegsink,
 #endif
 
   GST_V4L_CHECK_OPEN(GST_V4LELEMENT(v4lmjpegsink));
-  GST_V4L_CHECK_NOT_ACTIVE(GST_V4LELEMENT(v4lmjpegsink));
+  //GST_V4L_CHECK_NOT_ACTIVE(GST_V4LELEMENT(v4lmjpegsink));
 
   if (ioctl(GST_V4LELEMENT(v4lmjpegsink)->video_fd, MJPIOC_G_PARAMS, &bparm) < 0)
   {
