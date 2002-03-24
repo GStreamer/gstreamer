be74250a11a8c8f6aabef00a3f1e53fcc507550c
David I. Lehn <dlehn@users.sourceforge.net>
2002-03-24 07:57:00 +0000

    add libtool uninstalled lib paths - yuck
    Original commit message from CVS:
    add libtool uninstalled lib paths - yuck

--- gst/__init__.py
+++ gst/__init__.py
@@ -1,1 +1,13 @@
+import sys
+import os
+
+"libtool lib location"
+devloc = os.path.join(__path__[0],'.libs')
+
+if os.path.exists(devloc):
+   sys.path.append(devloc)
+
+sys.setdlopenflags(1)
+del devloc, sys, os
+
 from _gstreamer import *
