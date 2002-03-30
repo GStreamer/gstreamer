f7cf0134f4213feac4a55a8bafe446d3c0b6ecda
Wim Taymans <wim.taymans@gmail.com>
2002-03-30 17:06:26 +0000

    Changed to the new props API
    Original commit message from CVS:
    Changed to the new props API
    Other small tuff.

--- ext/lame/test-lame.c
+++ ext/lame/test-lame.c
@@ -63,8 +63,11 @@ main (int argc, char *argv[])
       "signed", GST_PROPS_BOOLEAN (TRUE),
       "channels", GST_PROPS_INT (1)
       );
-  caps->fixed = TRUE;
-  g_assert (caps != NULL);
+
+  g_object_set (G_OBJECT (src), "sizetype", 3, 
+		  		"filltype", 3, NULL);
+
+  
   gst_element_set_state (pipeline, GST_STATE_READY);
   g_print ("Setting caps on fakesrc's src pad\n");
   if (! (gst_pad_try_set_caps (gst_element_get_pad (src, "src"), caps)))
