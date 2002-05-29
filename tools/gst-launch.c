d4e56cc9256da38d4059249a81a201394b951020
Steve Baker <steve@stevebaker.org>
2002-05-29 15:30:10 +0000

    this wasn't actually needed
    Original commit message from CVS:
    this wasn't actually needed

--- tools/gst-launch.c
+++ tools/gst-launch.c
@@ -205,7 +205,6 @@ main(int argc, char *argv[])
         gst_main ();
     } else {
         g_print ("waiting for the state change...\n");
-        gst_element_set_state(pipeline,GST_STATE_PLAYING);
         gst_element_wait_state_change (pipeline);
     }
 
