6004504f6ad96354096062faa7022da24efe605c
Steve Baker <steve@stevebaker.org>
2002-06-01 15:01:51 +0000

    added a test which shows a problem with state changes when the toplevel bin is a thread.
    Original commit message from CVS:
    added a test which shows a problem with state changes when the toplevel bin is a thread.
    there is some kind of deadlock. It would be good if wingo or wtay could have a look.

---
+++ tests/threadstate/threadstate1.c
@@ -0,0 +1,35 @@
+#include <gst/gst.h>
+
+int main(int argc,char *argv[]) 
+{
+  GstElement *fakesrc, *fakesink;
+  GstElement *thread;
+  gint x;
+
+  gst_init(&argc,&argv);
+
+  thread = gst_thread_new("thread");
+  g_assert(thread != NULL);
+
+  fakesrc = gst_element_factory_make("fakesrc", "fake_source");
+  g_assert(fakesrc != NULL);
+
+  fakesink = gst_element_factory_make("fakesink", "fake_sink");
+  g_assert(fakesink != NULL);
+
+  gst_bin_add_many (GST_BIN(thread), fakesrc, fakesink, NULL);
+  gst_element_connect (fakesrc, fakesink);
+
+  for (x = 0 ; x < 10 ; x++){
+    g_print("playing %d\n", x);
+    gst_element_set_state(GST_ELEMENT(thread), GST_STATE_PLAYING);
+    sleep(1);
+
+    g_print("pausing %d\n", x);
+    gst_element_set_state(GST_ELEMENT(thread), GST_STATE_PAUSED);
+    sleep(1);
+  }
+
+  exit(0);
+}
+
