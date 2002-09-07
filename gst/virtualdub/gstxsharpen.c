1a9b949a96ac6e619363d2fb9726184a9f2427ac
Christian Schaller <uraeus@gnome.org>
2002-09-07 18:49:41 +0000

    Fix license to LGPL and add a file explaining the LGPL license
    Original commit message from CVS:
    Fix license to LGPL and add a file explaining the LGPL license

--- gst/virtualdub/gstxsharpen.c
+++ gst/virtualdub/gstxsharpen.c
@@ -4,15 +4,16 @@
  * Filter:
  * Copyright (C) 2000 Donald A. Graft  
  *
- * Port done with help of transcode xsharpen filter by Tilmann Bitterberg
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
  *
- * EffecTV is free software. We release this product under the terms of the
- * GNU General Public License version 2. The license is included in the file
- * COPYING.
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
  */
 
 #include <string.h>
