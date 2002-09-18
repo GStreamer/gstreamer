4da8cc062b94ec199349afee217421436727e835
David Schleef <ds@schleef.org>
2002-09-18 06:21:58 +0000

    Fix for compiler warnings on PowerPC.  Compiler complains about longjmp() clobbering local variables, which appear to...
    Original commit message from CVS:
    Fix for compiler warnings on PowerPC.  Compiler complains about longjmp()
    clobbering local variables, which appear to be bogus.

--- gst/schedulers/cothreads_compat.h
+++ gst/schedulers/cothreads_compat.h
@@ -96,20 +96,21 @@ typedef cothread cothread_context;
 #define do_cothreads_stackquery(stack,size)     \
   cothreads_alloc_thread_stack (stack, size)
 
-#define do_cothread_switch(to)	G_STMT_START{   			  \
-  cothread *from = cothread_self ();            			  \
-  if (from == (to)) {                           			  \
-    GST_DEBUG (GST_CAT_COTHREAD_SWITCH,         			  \
-               "trying to switch to the same cothread (%p), not allowed", \
-               (to));                                                     \
-    g_warning ("trying to switch to the same cothread, not allowed");     \
-  } else {                                                                \
-    GST_INFO (GST_CAT_COTHREAD_SWITCH,                                    \
-              "switching from cothread %p to cothread %p", from, (to));   \
-    cothread_switch (from, (to));                                         \
-    GST_INFO (GST_CAT_COTHREAD_SWITCH, "we're in cothread %p now", from); \
-  }                                                                       \
-}G_STMT_END
+static void do_cothread_switch(cothread *to)
+{
+  cothread *from = cothread_self ();
+  if (from == (to)) {
+    GST_DEBUG (GST_CAT_COTHREAD_SWITCH,
+               "trying to switch to the same cothread (%p), not allowed",
+               (to));
+    g_warning ("trying to switch to the same cothread, not allowed");
+  } else {
+    GST_INFO (GST_CAT_COTHREAD_SWITCH,
+              "switching from cothread %p to cothread %p", from, (to));
+    cothread_switch (from, (to));
+    GST_INFO (GST_CAT_COTHREAD_SWITCH, "we're in cothread %p now", from);
+  }
+}
 
 #define do_cothread_create(new_cothread, context, func, argc, argv)         \
   G_STMT_START{                                                             \
