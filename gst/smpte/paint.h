bc93903d019bd83f382c2d4b93f1e08d761405a7
Wim Taymans <wim.taymans@gmail.com>
2002-10-10 21:19:12 +0000

    Added the base framework for the SMPTE transitions and wipes.
    Original commit message from CVS:
    Added the base framework for the SMPTE transitions and wipes.

---
+++ gst/smpte/paint.h
@@ -0,0 +1,35 @@
+/* GStreamer
+ * Copyright (C) <1999> Erik Walthinsen <omega@cse.ogi.edu>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#ifndef __GST_SMPTE_PAINT_H__
+#define __GST_SMPTE_PAINT_H__
+
+#include <glib.h>
+
+void 	gst_smpte_paint_rect 	(guint8 *dest, gint stride, 
+				 gint x, gint y, gint w, gint h, 
+				 guint8 color);
+
+void 	gst_smpte_paint_rect_s 	(guint8 *dest, gint stride, 
+				 gint x, gint y, gint w, gint h, 
+				 guint8 color);
+
+
+
+#endif /* __GST_SMPTE_PAINT_H__ */
