8bca9f16bb88f33064d077f7bb9aa70d32f8219c
Thomas Vander Stichele <thomas@apestaart.org>
2002-10-25 14:31:15 +0000

    a no-brainer addition
    Original commit message from CVS:
    a no-brainer addition

--- gst-libs/gst/gconf/test-gconf.c
+++ gst-libs/gst/gconf/test-gconf.c
@@ -7,5 +7,9 @@ main (int argc, char *argv[])
           gst_gconf_get_string ("default/videosink"));
   printf ("Default audio sink : %s\n", 
           gst_gconf_get_string ("default/audiosink"));
+  printf ("Default video src : %s\n", 
+          gst_gconf_get_string ("default/videosrc"));
+  printf ("Default audio src : %s\n", 
+          gst_gconf_get_string ("default/audiosrc"));
   return 0;
 }
