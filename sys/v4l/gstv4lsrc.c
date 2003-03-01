b1972de3936f9679087f19f86b0e71d454ac8717
Sjoerd Simons <sjoerd@luon.net>
2003-03-01 13:11:02 +0000

    Use GST_TIMEVAL_TO_TIME (patch from sjoerd@luon.net)
    Original commit message from CVS:
    Use GST_TIMEVAL_TO_TIME (patch from sjoerd@luon.net)

--- sys/v4l/gstv4lsrc.c
+++ sys/v4l/gstv4lsrc.c
@@ -464,10 +464,12 @@ gst_v4lsrc_get (GstPad *pad)
   GST_BUFFER_SIZE(buf) = v4lsrc->buffer_size;
 
   if (!v4lsrc->first_timestamp)
-    v4lsrc->first_timestamp = v4lsrc->timestamp_soft_sync[num].tv_sec * GST_SECOND +
-      v4lsrc->timestamp_soft_sync[num].tv_usec * GST_SECOND/1000000;
-  GST_BUFFER_TIMESTAMP(buf) = v4lsrc->timestamp_soft_sync[num].tv_sec * GST_SECOND +
-    v4lsrc->timestamp_soft_sync[num].tv_usec * GST_SECOND/1000000 - v4lsrc->first_timestamp;
+    v4lsrc->first_timestamp =
+      GST_TIMEVAL_TO_TIME(v4lsrc->timestamp_soft_sync[num]);
+
+  GST_BUFFER_TIMESTAMP(buf) =
+    GST_TIMEVAL_TO_TIME(v4lsrc->timestamp_soft_sync[num]) -
+      v4lsrc->first_timestamp;
 
   return buf;
 }
