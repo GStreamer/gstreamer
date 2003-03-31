4b04aa61d96578a1cb0a4133fb7aca6a64869d12
Wim Taymans <wim.taymans@gmail.com>
2003-03-31 19:48:42 +0000

    Small indentation fixes
    Original commit message from CVS:
    Small indentation fixes

--- gst/gstelement.h
+++ gst/gstelement.h
@@ -291,18 +291,18 @@ GList*			gst_element_get_pad_template_list	(GstElement *element);
 GstPadTemplate*		gst_element_get_compatible_pad_template (GstElement *element, GstPadTemplate *compattempl);
 
 gboolean		gst_element_link		(GstElement *src, GstElement *dest);
-gboolean		gst_element_link_many 	(GstElement *element_1, GstElement *element_2, ...);
+gboolean		gst_element_link_many 		(GstElement *element_1, GstElement *element_2, ...);
 gboolean		gst_element_link_filtered 	(GstElement *src, GstElement *dest,
 							 GstCaps *filtercaps);
 void			gst_element_unlink 		(GstElement *src, GstElement *dest);
 void			gst_element_unlink_many 	(GstElement *element_1, GstElement *element_2, ...);
 
-gboolean		gst_element_link_pads	(GstElement *src, const gchar *srcpadname,
+gboolean		gst_element_link_pads		(GstElement *src, const gchar *srcpadname,
 							 GstElement *dest, const gchar *destpadname);
-gboolean		gst_element_link_pads_filtered (GstElement *src, const gchar *srcpadname,
+gboolean		gst_element_link_pads_filtered 	(GstElement *src, const gchar *srcpadname,
 							 GstElement *dest, const gchar *destpadname,
 							 GstCaps *filtercaps);
-void			gst_element_unlink_pads	(GstElement *src, const gchar *srcpadname,
+void			gst_element_unlink_pads		(GstElement *src, const gchar *srcpadname,
 							 GstElement *dest, const gchar *destpadname);
 
 const GstEventMask*	gst_element_get_event_masks	(GstElement *element);
