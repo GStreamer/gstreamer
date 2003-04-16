1a2a989d151ad014e27e24272bd0480da6381233
Ronald S. Bultje <rbultje@ronald.bitfreak.net>
2003-04-16 22:08:47 +0000

    Backport fix from HEAD
    Original commit message from CVS:
    Backport fix from HEAD

--- gst/gstqueue.c
+++ gst/gstqueue.c
@@ -253,6 +253,8 @@ gst_queue_dispose (GObject *object)
 {
   GstQueue *queue = GST_QUEUE (object);
 
+  gst_element_set_state (GST_ELEMENT (queue), GST_STATE_NULL);
+
   g_mutex_free (queue->qlock);
   g_cond_free (queue->not_empty);
   g_cond_free (queue->not_full);
