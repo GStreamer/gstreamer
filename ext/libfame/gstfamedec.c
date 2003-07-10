8242fc6938184d462c9ba380721f7f53a49b063f
Christian Schaller <uraeus@gnome.org>
2003-07-10 15:48:09 +0000

    fix license of fame plugin to say LGPL not GPL as the libfame library is LGPL
    Original commit message from CVS:
    fix license of fame plugin to say LGPL not GPL as the libfame library is LGPL
    Add readme with some info

--- ext/libfame/gstfamedec.c
+++ ext/libfame/gstfamedec.c
@@ -32,7 +32,7 @@
 static GstElementDetails gst_famedec_details = {
   "MPEG1 and MPEG4 video decoder using the famedec library",
   "Codec/Video/Encoder",
-  "GPL",
+  "LGPL",
   "Uses famedec to decode MPEG video streams",
   VERSION,
   "famedec: (C) 2000-2001, Vivien Chappelier\n"
