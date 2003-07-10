9b48b95257259b5f08a628da88881e0f1bc7b39e
Benjamin Otte <otte@gnome.org>
2003-07-10 11:13:34 +0000

    fix issue with null pointer cast use brackets around macro arguments
    Original commit message from CVS:
    fix issue with null pointer cast
    use brackets around macro arguments

--- gst/gstinfo.h
+++ gst/gstinfo.h
@@ -279,13 +279,13 @@ extern GstDebugCategory *	GST_CAT_DEFAULT;
 #ifdef G_HAVE_ISO_VARARGS
 #define GST_CAT_LEVEL_LOG(cat,level,object,...) G_STMT_START{			\
   if (gst_debug_is_active ()) {				\
-    gst_debug_log (cat, level, __FILE__, GST_FUNCTION, __LINE__, G_OBJECT (object), __VA_ARGS__); \
+    gst_debug_log ((cat), (level), __FILE__, GST_FUNCTION, __LINE__, (GObject *) (object), __VA_ARGS__); \
   }										\
 }G_STMT_END
 #else /* G_HAVE_GNUC_VARARGS */
 #define GST_CAT_LEVEL_LOG(cat,level,object,args...) G_STMT_START{			\
   if (gst_debug_is_active ()) {				\
-    gst_debug_log (cat, level, __FILE__, GST_FUNCTION, __LINE__, G_OBJECT (object), ##args ); \
+    gst_debug_log ((cat), (level), __FILE__, GST_FUNCTION, __LINE__, (GObject *) (object), ##args ); \
   }										\
 }G_STMT_END
 #endif /* G_HAVE_ISO_VARARGS */
