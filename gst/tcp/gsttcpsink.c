bcdde63659339768b482164d37b3745bdbe3eee0
Angel Carpintero <ack@telefonica.net>
2003-07-18 15:59:03 +0000

    Update to new debugging system
    Original commit message from CVS:
    Update to new debugging system

--- gst/tcp/gsttcpsink.c
+++ gst/tcp/gsttcpsink.c
@@ -252,7 +252,7 @@ gst_tcpsink_chain (GstPad *pad, GstBuffer *buf)
   if (tcpsink->clock) {
     GstClockID id = gst_clock_new_single_shot_id (tcpsink->clock, GST_BUFFER_TIMESTAMP (buf));
 
-    GST_DEBUG (0, "tcpsink: clock wait: %" G_GUINT64_FORMAT "\n", GST_BUFFER_TIMESTAMP (buf));
+    GST_DEBUG ("tcpsink: clock wait: %" G_GUINT64_FORMAT "\n", GST_BUFFER_TIMESTAMP (buf));
     gst_element_clock_wait (GST_ELEMENT (tcpsink), id, NULL);
     gst_clock_id_free (id);
   }
