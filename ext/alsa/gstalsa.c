ccbd5cbade59ac7640f2e44a601e131f55f1b013
Benjamin Otte <otte@gnome.org>
2003-07-25 18:43:15 +0000

    copy license from .h to .c, so it's really the LGPL header now
    Original commit message from CVS:
    copy license from .h to .c, so it's really the LGPL header now

--- ext/alsa/gstalsa.c
+++ ext/alsa/gstalsa.c
@@ -11,14 +11,14 @@
  *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Library General Public License for more details.
  *
  * You should have received a copy of the GNU Library General Public
  * License along with this library; if not, write to the Free
  * Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
-
+                                                                                                                    
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
