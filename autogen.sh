869763cfdd837f0f36c3352b8a284b80c9876a35
David Schleef <ds@schleef.org>
2003-08-06 01:15:48 +0000

    Automake-1.5 chokes on gst/Makefile.am, so updating requirement to 1.6.
    Original commit message from CVS:
    Automake-1.5 chokes on gst/Makefile.am, so updating requirement to
    1.6.

--- autogen.sh
+++ autogen.sh
@@ -27,8 +27,8 @@ echo -n "+ check for build tools"
 if test ! -z "$NOCHECK"; then echo ": skipped version checks"; else  echo; fi
 version_check "autoconf" "$AUTOCONF autoconf autoconf-2.54 autoconf-2.53 autoconf-2.52" \
               "ftp://ftp.gnu.org/pub/gnu/autoconf/" 2 52 || DIE=1
-version_check "automake" "$AUTOMAKE automake automake-1.7 automake-1.6 automake-1.5" \
-              "ftp://ftp.gnu.org/pub/gnu/automake/" 1 5 || DIE=1
+version_check "automake" "$AUTOMAKE automake automake-1.7 automake-1.6" \
+              "ftp://ftp.gnu.org/pub/gnu/automake/" 1 6 || DIE=1
 version_check "libtool" "" \
               "ftp://ftp.gnu.org/pub/gnu/libtool/" 1 4 0 || DIE=1
 version_check "pkg-config" "" \
