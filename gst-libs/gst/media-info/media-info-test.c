f600f3e4e804a220720fe8134bbdaf258788385f
Iain Holmes <iain@prettypeople.org>
2003-09-23 22:49:35 +0000

    Sync'ed files up to nautilus-media/media-info versions, and fixed them for errors and deprecated functions
    Original commit message from CVS:
    Sync'ed files up to nautilus-media/media-info versions, and fixed them for errors and deprecated functions

--- gst-libs/gst/media-info/media-info-test.c
+++ gst-libs/gst/media-info/media-info-test.c
@@ -62,11 +62,16 @@ info_print (GstMediaInfoStream *stream)
   GstMediaInfoTrack *track;
 
   g_print ("- mime type: %s\n", stream->mime);
-  g_print ("- length: %.3f seconds\n", 
+  g_print ("- length: %.3f seconds\n",
 	   (gdouble) stream->length_time / GST_SECOND);
   g_print ("- bitrate: %.3f kbps\n", stream->bitrate / 1000.0);
   g_print ("- number of tracks: %ld\n", stream->length_tracks);
   p = stream->tracks;
+  if (p == NULL)
+  {
+    g_print ("- no track information, probably an error\n");
+    return;
+  }
   for (i = 0; i < stream->length_tracks; ++i)
   {
     g_print ("- track %d\n", i);
@@ -85,23 +90,32 @@ int
 main (int argc, char *argv[])
 {
   GstMediaInfo *info;
-  GstMediaInfoStream *stream;
+  GstMediaInfoStream *stream = NULL;
   gint i;
-	
+
   g_assert (argc > 1);
 
   gst_init (&argc, &argv);
 
   info = g_object_new (GST_MEDIA_INFO_TYPE, NULL);
+  g_print ("stream: %p, &stream: %p\n", stream, &stream);
   for (i = 1; i < argc; ++i)
   {
+
+    /*
     stream = gst_media_info_read (info, argv[i], GST_MEDIA_INFO_ALL);
+    */
+    gst_media_info_read_with_idler (info, argv[i], GST_MEDIA_INFO_ALL);
+    while (gst_media_info_read_idler (info, &stream) && stream == NULL)
+      /* keep idling */ g_print ("+");
     g_print ("\nFILE: %s\n", argv[i]);
+    g_print ("stream: %p, &stream: %p\n", stream, &stream);
     if (stream)
       info_print (stream);
     else
       g_print ("no media info found.\n");
+    stream = NULL;
   }
-  
-  return 0;	
+
+  return 0;
 }
