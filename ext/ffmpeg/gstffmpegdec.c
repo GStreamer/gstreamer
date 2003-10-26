b34d003bfeeb8204c3ae6980cc2f4533d618d279
Ronald S. Bultje <rbultje@ronald.bitfreak.net>
2003-10-26 12:00:25 +0000

    < <-> > confusion
    Original commit message from CVS:
    < <-> > confusion

--- ext/ffmpeg/gstffmpegdec.c
+++ ext/ffmpeg/gstffmpegdec.c
@@ -440,7 +440,7 @@ gst_ffmpegdec_register (GstPlugin *plugin)
     /* no quasi-codecs, please */
     if (in_plugin->id == CODEC_ID_RAWVIDEO ||
 	(in_plugin->id >= CODEC_ID_PCM_S16LE &&
-	 in_plugin->id >= CODEC_ID_PCM_ALAW)) {
+	 in_plugin->id <= CODEC_ID_PCM_ALAW)) {
       goto next;
     }
 
