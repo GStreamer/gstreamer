bbc133ec8f8829d8f66593264cf78c32ef5ff1e1
Benjamin Otte <otte@gnome.org>
2003-10-28 20:52:41 +0000

    merge TYPEFIND branch. Major changes:
    Original commit message from CVS:
    merge TYPEFIND branch. Major changes:
    - totally reworked type(find) system
    - all typefind functions are in gst/typefind now
    - more typefind functions then before
    - some plugins might fail to compile now because I don't have them installed and they
    a) require bytestream or
    b) haven't had their typefind fixed.
    Please fix those plugins and put the typefind functions into gst/typefind if they don't have dependencies

--- ext/jack/gstjack.h
+++ ext/jack/gstjack.h
@@ -22,7 +22,7 @@
 
 #include <jack/jack.h>
 #include <gst/gst.h>
-#include <gst/gstbytestream.h>
+#include <gst/bytestream.h>
 
 //#define JACK_DEBUG(str, a...) g_message (str, ##a)
 #define JACK_DEBUG(str, a...)
