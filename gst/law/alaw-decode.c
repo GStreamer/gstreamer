95f73bcf6dd90053087c3a62901db31240b38f09
David Schleef <ds@schleef.org>
2003-11-06 22:14:17 +0000

    make sure config.h is included first
    Original commit message from CVS:
    make sure config.h is included first

--- gst/law/alaw-decode.c
+++ gst/law/alaw-decode.c
@@ -19,6 +19,9 @@
  * Boston, MA 02111-1307, USA.
  */
 
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
 #include <gst/gst.h>
 #include "alaw-decode.h"
 
--- gst/level/gstlevel.c
+++ gst/level/gstlevel.c
@@ -21,6 +21,9 @@
  * Boston, MA 02111-1307, USA.
  */
 
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
 #include <gst/gst.h>
 #include "gstlevel.h"
 #include "math.h"
