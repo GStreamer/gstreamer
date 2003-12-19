5345dd61ec4f3b56c52c2f7455b33751434891d5
Thomas Vander Stichele <thomas@apestaart.org>
2003-12-19 14:39:03 +0000

    superfluous space segregation
    Original commit message from CVS:
    superfluous space segregation

--- examples/gstplay/player.c
+++ examples/gstplay/player.c
@@ -107,7 +107,7 @@ main (int argc, char *argv[])
     g_print ("usage: %s <video filename>\n", argv[0]);
     exit (-1);
   }
-  
+
   loop = g_main_loop_new (NULL, FALSE);
 
   /* Creating the GstPlay object */
@@ -118,18 +118,18 @@ main (int argc, char *argv[])
   video_sink = gst_element_factory_make ("xvimagesink", "video_sink");
   vis_element = gst_element_factory_make ("goom", "vis_element");
   data_src = gst_element_factory_make ("gnomevfssrc", "source");
-  
+
   /* Let's send them to GstPlay object */
   gst_play_set_audio_sink (play, audio_sink);
   gst_play_set_video_sink (play, video_sink);
   gst_play_set_data_src (play, data_src);
   gst_play_set_visualization (play, vis_element);
-  
+
   /* Setting location we want to play */
   gst_play_set_location (play, argv[1]);
 
   /* gst_xml_write_file (GST_ELEMENT (play), stdout); */
-  
+
   g_signal_connect (G_OBJECT (play), "time_tick",
                     G_CALLBACK (got_time_tick), NULL);
   g_signal_connect (G_OBJECT (play), "stream_length",
@@ -140,13 +140,13 @@ main (int argc, char *argv[])
                     G_CALLBACK (got_found_tag), NULL);
   g_signal_connect (G_OBJECT (play), "eos",
                     G_CALLBACK (got_eos), NULL);
-  
+
   /* Change state to PLAYING */
   gst_element_set_state (GST_ELEMENT (play), GST_STATE_PLAYING);
 
   g_idle_add ((GSourceFunc) idle_iterate, play);
   g_timeout_add (20000, (GSourceFunc) seek_timer, play);
-  
+
   g_main_loop_run (loop);
 
   /* unref */
