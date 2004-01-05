151cca6d7fc20e41b61da535ec010b5db9d35b13
David Schleef <ds@schleef.org>
2004-01-05 00:58:39 +0000

    remove c99-ism
    Original commit message from CVS:
    remove c99-ism

--- ext/mad/gstid3tag.c
+++ ext/mad/gstid3tag.c
@@ -478,8 +478,10 @@ gst_mad_id3_to_tag_list(const struct id3_tag *tag)
 	  tmp = strtoul (utf8, &check, 10);
 	  if (*check != '\0') break;
 	  if (strcmp (tag_name, GST_TAG_DATE) == 0) {
+	    GDate *d;
+
 	    if (tmp == 0) break;
-	    GDate *d = g_date_new_dmy (1, 1, tmp);
+	    d = g_date_new_dmy (1, 1, tmp);
 	    tmp = g_date_get_julian (d);
 	    g_date_free (d);
 	  }
