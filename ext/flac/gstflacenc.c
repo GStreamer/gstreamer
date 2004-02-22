c80c21d5d712bd80c50e35290f444e7316d097c5
Benjamin Otte <otte@gnome.org>
2004-02-22 15:31:30 +0000

    gst/audioconvert/gstaudioconvert.c: set rank to PRIMARY
    Original commit message from CVS:
    2004-02-22  Benjamin Otte  <otte@gnome.org>
    
    reported by: Stefan Kost <kost@imn.htwk-leipzig.de>
    
    * gst/audioconvert/gstaudioconvert.c: (plugin_init):
    set rank to PRIMARY
    * gst/volume/gstvolume.c: (plugin_init):
    set rank to NONE
    fixes #134960
    
    2004-02-22   Julio M. Merino Vidal <jmmv@menta.net>
    
    reviewed by Benjamin Otte  <otte@gnome.org>
    
    * ext/flac/gstflacenc.c: (gst_flacenc_chain):
    escape NULL strings in GST_ELEMENT_ERROR properly (fixes #135116)

--- ext/flac/gstflacenc.c
+++ ext/flac/gstflacenc.c
@@ -575,7 +575,7 @@ gst_flacenc_chain (GstPad *pad, GstData *_data)
     gst_flacenc_set_metadata (flacenc);
     state = FLAC__seekable_stream_encoder_init (flacenc->encoder);
     if (state != FLAC__STREAM_ENCODER_OK) {
-      GST_ELEMENT_ERROR (flacenc, LIBRARY, INIT, NULL,
+      GST_ELEMENT_ERROR (flacenc, LIBRARY, INIT, (NULL),
 		         ("could not initialize encoder (wrong parameters?)"));
       return;
     }
@@ -610,7 +610,7 @@ gst_flacenc_chain (GstPad *pad, GstData *_data)
   flacenc->data = NULL;
 
   if (!res) {
-    GST_ELEMENT_ERROR (flacenc, STREAM, ENCODE, NULL, NULL);
+    GST_ELEMENT_ERROR (flacenc, STREAM, ENCODE, (NULL), (NULL));
   }
 }
 
