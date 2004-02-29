b4450adfec6a987a63f3c8012afe6d45a1443f02
Thomas Vander Stichele <thomas@apestaart.org>
2004-02-29 08:34:03 +0000

    update libtool requirement for latest plugin symbol changes
    Original commit message from CVS:
    update libtool requirement for latest plugin symbol changes

--- autogen.sh
+++ autogen.sh
@@ -35,8 +35,8 @@ version_check "automake" "$AUTOMAKE automake automake-1.7 automake17 automake-1.
               "ftp://ftp.gnu.org/pub/gnu/automake/" 1 6 || DIE=1
 version_check "autopoint" "autopoint" \
               "ftp://ftp.gnu.org/pub/gnu/gettext/" 0 11 5 || DIE=1
-version_check "libtoolize" "libtoolize libtoolize14" \
-              "ftp://ftp.gnu.org/pub/gnu/libtool/" 1 4 0 || DIE=1
+version_check "libtoolize" "libtoolize" \
+              "ftp://ftp.gnu.org/pub/gnu/libtool/" 1 5 0 || DIE=1
 version_check "pkg-config" "" \
               "http://www.freedesktop.org/software/pkgconfig" 0 8 0 || DIE=1
 
