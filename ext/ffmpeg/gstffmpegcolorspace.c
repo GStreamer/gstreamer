0fc948904272bbefec153594f1d15ac09c28c822
Thomas Vander Stichele <thomas@apestaart.org>
2004-03-18 12:55:38 +0000

    don't critical (#137588)
    Original commit message from CVS:
    don't critical (#137588)

--- ext/ffmpeg/gstffmpegcolorspace.c
+++ ext/ffmpeg/gstffmpegcolorspace.c
@@ -341,7 +341,8 @@ gst_ffmpegcsp_chain (GstPad  *pad,
 
   if (space->from_pixfmt == PIX_FMT_NB ||
       space->to_pixfmt == PIX_FMT_NB) {
-    g_critical ("attempting to convert unknown formats");
+    GST_ELEMENT_ERROR (space, CORE, NOT_IMPLEMENTED, NULL,
+         ("attempting to convert colorspaces between unknown formats"));
     gst_buffer_unref (inbuf);
     return;
   }
