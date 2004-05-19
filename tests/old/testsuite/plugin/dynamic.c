d29bf4183dfebcb6bef939757b4c954107cfc048
Thomas Vander Stichele <thomas@apestaart.org>
2004-05-19 18:50:35 +0000

    tell us more about your failure
    Original commit message from CVS:
    tell us more about your failure

--- tests/old/testsuite/plugin/dynamic.c
+++ tests/old/testsuite/plugin/dynamic.c
@@ -10,12 +10,12 @@ main (int argc, char *argv[])
   gst_init (&argc, &argv);
 
   plugin = gst_plugin_load_file (".libs/libtestplugin.so", &error);
-  g_assert (plugin != NULL);
   if (error) {
     g_print ("ERROR loading plug-in: %s\n", error->message);
     g_free (error);
     return 1;
   }
+  g_assert (plugin != NULL);
 
   g_print ("testplugin: %s\n", gst_plugin_get_name (plugin));
 
