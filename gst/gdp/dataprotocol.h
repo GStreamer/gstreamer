2070420420e453809907a62748ef7088dfe4d51b
Thomas Vander Stichele <thomas@apestaart.org>
2004-05-24 16:38:15 +0000

    gdp: wrap header in _NEW
    Original commit message from CVS:
    wrap header in _NEW

--- gst/gdp/dataprotocol.h
+++ gst/gdp/dataprotocol.h
@@ -20,6 +20,7 @@
  * Boston, MA 02111-1307, USA.
  */
 
+#ifdef GST_ENABLE_NEW
 #ifndef __GST_DATA_PROTOCOL_H__
 #define __GST_DATA_PROTOCOL_H__
 
@@ -98,3 +99,4 @@ gboolean	gst_dp_validate_packet		(guint header_length,
 G_END_DECLS
 
 #endif /* __GST_DATA_PROTOCOL_H__ */
+#endif /* GST_ENABLE_NEW */
