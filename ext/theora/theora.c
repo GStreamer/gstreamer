ab7738c8d1bb19cf6dae35414120bfa4a808079b
Thomas Vander Stichele <thomas@apestaart.org>
2004-07-03 23:11:04 +0000

    fix name
    Original commit message from CVS:
    fix name

--- ext/theora/theora.c
+++ ext/theora/theora.c
@@ -45,6 +45,6 @@ plugin_init (GstPlugin * plugin)
 
 GST_PLUGIN_DEFINE (GST_VERSION_MAJOR,
     GST_VERSION_MINOR,
-    "gsttheora",
+    "theora",
     "Theora plugin library",
     plugin_init, VERSION, "LGPL", GST_PACKAGE, GST_ORIGIN)
