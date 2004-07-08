842f7aacbea883d93bd3e5b3843f53bbb20a0446
Thomas Vander Stichele <thomas@apestaart.org>
2004-07-08 14:01:28 +0000

    put them back
    Original commit message from CVS:
    put them back

--- ext/ffmpeg/gstffmpegdec.c
+++ ext/ffmpeg/gstffmpegdec.c
@@ -134,7 +134,7 @@ gst_ffmpegdec_base_init (GstFFMpegDecClass * klass)
   /* construct the element details struct */
   details.longname = g_strdup_printf ("FFMPEG %s decoder",
       params->in_plugin->name);
-  details.klass = g_strdup_printf ("Codec/%s/Decoder",
+  details.klass = g_strdup_printf ("Codec/Decoder/%s",
       (params->in_plugin->type == CODEC_TYPE_VIDEO) ? "Video" : "Audio");
   details.description = g_strdup_printf ("FFMPEG %s decoder",
       params->in_plugin->name);
--- ext/ffmpeg/gstffmpegenc.c
+++ ext/ffmpeg/gstffmpegenc.c
@@ -166,7 +166,7 @@ gst_ffmpegenc_base_init (GstFFMpegEncClass * klass)
   /* construct the element details struct */
   details.longname = g_strdup_printf ("FFMPEG %s encoder",
       params->in_plugin->name);
-  details.klass = g_strdup_printf ("Codec/%s/Encoder",
+  details.klass = g_strdup_printf ("Codec/Encoder/%s",
       (params->in_plugin->type == CODEC_TYPE_VIDEO) ? "Video" : "Audio");
   details.description = g_strdup_printf ("FFMPEG %s encoder",
       params->in_plugin->name);
