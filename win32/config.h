0c7ff9acaebbe97b1aeb54860d0e1b97fe2ff088
Steve Lhomme <steve.lhomme@free.fr>
2004-07-12 20:15:56 +0000

    Update to another gettext public build
    Original commit message from CVS:
    Update to another gettext public build

--- win32/config.h
+++ win32/config.h
@@ -1,7 +1,7 @@
 /* config.h.in.  Generated from configure.ac by autoheader.  */
 
 /* always defined to indicate that i18n is enabled */
-#define ENABLE_NLS 0
+#define ENABLE_NLS 1
 
 /* gettext package name */
 #define GETTEXT_PACKAGE "gstreamer-0.8"
