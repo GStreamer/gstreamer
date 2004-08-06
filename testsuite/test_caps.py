04d47f4be29514f94d217edabf1e16ccf882f683
Thomas Vander Stichele <thomas@apestaart.org>
2004-08-06 18:49:53 +0000

    add constructor for caps
    Original commit message from CVS:
    add constructor for caps

--- testsuite/test_caps.py
+++ testsuite/test_caps.py
@@ -19,6 +19,34 @@ class CapsTest(unittest.TestCase):
 	mime = structure.get_name()
 	assert mime == 'video/x-raw-rgb'
 
+    def testCapsConstructEmpty(self):
+        caps = gst.Caps()
+	assert isinstance(caps, gst.Caps)
+
+    def testCapsConstructFromString(self):
+        caps = gst.Caps('video/x-raw-yuv,width=10')
+	assert isinstance(caps, gst.Caps)
+        assert len(caps) == 1
+	assert isinstance(caps[0], gst.Structure)
+        assert caps[0].get_name() == 'video/x-raw-yuv'
+	assert isinstance(caps[0]['width'], int)
+        assert caps[0]['width'] == 10
+
+    def testCapsConstructFromStructures(self):
+        struct1 = gst.structure_from_string('video/x-raw-yuv,width=10')
+        struct2 = gst.structure_from_string('video/x-raw-rgb,width=20')
+        caps = gst.Caps(struct1, struct2)
+	assert isinstance(caps, gst.Caps)
+        assert len(caps) == 2
+	assert isinstance(caps[0], gst.Structure)
+	assert isinstance(caps[1], gst.Structure)
+        assert caps[0].get_name() == 'video/x-raw-yuv'
+        assert caps[1].get_name() == 'video/x-raw-rgb'
+	assert isinstance(caps[0]['width'], int)
+	assert isinstance(caps[1]['width'], int)
+        assert caps[0]['width'] == 10
+        assert caps[1]['width'] == 20
+
     def testCapsStructureChange(self):
 	'test if changing the structure of the caps works by reference'
 	assert self.structure['width'] == 10
