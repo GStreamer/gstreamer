607bb26a147f363c2753893a5804abefb28264e1
Johan Dahlin <johan@gnome.org>
2004-08-06 18:14:30 +0000

    gst/gst.override (_wrap_gst_structure_from_string): Impl.
    Original commit message from CVS:
    * gst/gst.override (_wrap_gst_structure_from_string): Impl.
    (_wrap_gst_tag_list_has_key): Impl.
    (_wrap_gst_caps_get_structure): Don't copy or free me
    
    * testsuite/struct.py (StructureTest.testStructureChange): Enable
    some tests.
    Improve tests
    
    * gst/gst.override (_wrap_gst_structure_ass_subscript): Impl

--- testsuite/test_struct.py
+++ testsuite/test_struct.py
@@ -2,13 +2,39 @@ import sys
 from common import gst, unittest
 
 class StructureTest(unittest.TestCase):
-    def testStructureChange(self):
-	caps = gst.caps_from_string('video/x-raw-yuv,width=10,pixel-aspect-ratio=1/2,framerate=5.0')
-	structure = caps.get_structure(0)
-	assert structure['width'] == 10
-	structure['width'] = 5
-	assert structure['width'] == 5, structure['width']
+    def setUp(self):
+        self.struct = gst.structure_from_string('video/x-raw-yuv,width=10,foo="bar",pixel-aspect-ratio=1/2,framerate=5.0')
 
+    def testName(self):
+        assert self.struct.get_name() == 'video/x-raw-yuv'
+        self.struct.set_name('foobar')
+        assert self.struct.get_name() == 'foobar'
+        
+    def testInt(self):
+        assert self.struct.has_key('width')
+        assert isinstance(self.struct['width'], int)
+	assert self.struct['width'] == 10, self.struct['width']
+	self.struct['width'] = 5
+        assert self.struct.has_key('width')
+        assert isinstance(self.struct['width'], int)
+	assert self.struct['width'] == 5, self.struct['width']
+
+    def testString(self):
+        assert self.struct.has_key('foo')
+        assert isinstance(self.struct['foo'], str)
+	assert self.struct['foo'] == 'bar', self.struct['foo']
+	self.struct['foo'] = 'baz'
+        assert self.struct.has_key('foo')
+        assert isinstance(self.struct['foo'], str)
+	assert self.struct['foo'] == 'baz', self.struct['foo']
+
+    def testCreateInt(self):
+	self.struct['integer'] = 5
+        assert self.struct.has_key('integer')
+        assert isinstance(self.struct['integer'], int)
+	assert self.struct['integer'] == 5, self.struct['integer']
+        
+    def testStructureChange(self):
         #assert structure['pixel-aspect-ratio'].numerator == 1
         #assert structure['pixel-aspect-ratio'].denominator == 2
         #assert float(structure['pixel-aspect-ratio']) == 0.5
@@ -17,9 +43,9 @@ class StructureTest(unittest.TestCase):
         #assert structure['pixel-aspect-ratio'].denominator == 4
         #assert float(structure['pixel-aspect-ratio']) == 0.75
         
-	assert structure['framerate'] == 5.0
-	structure['framerate'] = 10.0
-	assert structure['framerate'] == 10.0
+	assert self.struct['framerate'] == 5.0
+	self.struct['framerate'] = 10.0
+	assert self.struct['framerate'] == 10.0
 
 	# a list of heights
 	#structure['height'] = (20, 40, 60)
