4f1fcba44a781a61bd5ee254ec638116f5aca645
Zaheer Abbas Merali <zaheerabbas@merali.org>
2004-08-13 14:55:27 +0000

    ext/lame/gstlame.c: whoops forgot break, thanks teuf
    Original commit message from CVS:
    2004-08-13  Zaheer Abbas Merali  <zaheerabbas at merali dot org>
    
    * ext/lame/gstlame.c: (gst_lame_get_property):
    whoops forgot break, thanks teuf

--- ext/lame/gstlame.c
+++ ext/lame/gstlame.c
@@ -858,6 +858,7 @@ gst_lame_get_property (GObject * object, guint prop_id, GValue * value,
       break;
     case ARG_XINGHEADER:
       g_value_set_boolean (value, lame->xingheader);
+      break;
     default:
       G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
       break;
