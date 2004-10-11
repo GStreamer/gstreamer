b8446ab36dd6dcdb66a86791a6572eaa1a48abcf
Johan Dahlin <johan@gnome.org>
2004-10-11 15:19:24 +0000

    Pass distcheck please
    Original commit message from CVS:
    Pass distcheck please

--- testsuite/test_interface.py
+++ testsuite/test_interface.py
@@ -1,22 +1,21 @@
 from common import gst, unittest
 
 import gobject
-from gst import interfaces
 
 class Availability(unittest.TestCase):
     def testXOverlay(self):
-        assert hasattr(interfaces, 'XOverlay')
-        assert issubclass(interfaces.XOverlay, gobject.GInterface)
+        assert hasattr(gst.interfaces, 'XOverlay')
+        assert issubclass(gst.interfaces.XOverlay, gobject.GInterface)
 
     def testMixer(self):
-        assert hasattr(interfaces, 'Mixer')
-        assert issubclass(interfaces.Mixer, gobject.GInterface)
+        assert hasattr(gst.interfaces, 'Mixer')
+        assert issubclass(gst.interfaces.Mixer, gobject.GInterface)
 
 class FunctionCall(unittest.TestCase):
     def testXOverlay(self):
         element = gst.element_factory_make('xvimagesink')
         assert isinstance(element, gst.Element)
-        assert isinstance(element, interfaces.XOverlay)
+        assert isinstance(element, gst.interfaces.XOverlay)
         element.set_xwindow_id(0L)
         
 if __name__ == "__main__":
