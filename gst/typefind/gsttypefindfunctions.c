c362d7a14fd4de038c9750d5225991d081244cb6
Ronald S. Bultje <rbultje@ronald.bitfreak.net>
2004-10-25 09:01:43 +0000

    gst/asfdemux/gstasfdemux.c: Don't push incomplete packets.
    Original commit message from CVS:
    Reviewd by:  Ronald S. Bultje  <rbultje@ronald.bitfreak.net>
    * gst/asfdemux/gstasfdemux.c: (gst_asf_demux_process_chunk):
    Don't push incomplete packets.
    * gst/typefind/gsttypefindfunctions.c: (m4a_type_find):
    Fix MPEG-4 audio typefinding.

--- gst/typefind/gsttypefindfunctions.c
+++ gst/typefind/gsttypefindfunctions.c
@@ -878,7 +878,7 @@ m4a_type_find (GstTypeFind * tf, gpointer unused)
   guint8 *data = gst_type_find_peek (tf, 4, 8);
 
   if (data && memcmp (data, "ftypM4A ", 8) == 0) {
-    gst_type_find_suggest (tf, GST_TYPE_FIND_MAXIMUM, AAC_CAPS);
+    gst_type_find_suggest (tf, GST_TYPE_FIND_MAXIMUM, M4A_CAPS);
   }
 }
 
