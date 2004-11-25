051fe366bd8723dd8da602c3a16918a77eabb8b9
Ronald S. Bultje <rbultje@ronald.bitfreak.net>
2004-11-25 20:22:13 +0000

    gst/playback/gstdecodebin.c: We sometimes need parsers for playback, so add those too.
    Original commit message from CVS:
    * gst/playback/gstdecodebin.c: (gst_decode_bin_factory_filter):
    We sometimes need parsers for playback, so add those too.

--- gst/playback/gstdecodebin.c
+++ gst/playback/gstdecodebin.c
@@ -242,7 +242,8 @@ gst_decode_bin_factory_filter (GstPluginFeature * feature,
 
   klass = gst_element_factory_get_klass (GST_ELEMENT_FACTORY (feature));
   /* only demuxers and decoders can play */
-  if (strstr (klass, "Demux") == NULL && strstr (klass, "Decoder") == NULL) {
+  if (strstr (klass, "Demux") == NULL &&
+      strstr (klass, "Decoder") == NULL && strstr (klass, "Parse") == NULL) {
     return FALSE;
   }
 
