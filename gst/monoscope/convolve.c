e08a835a4a9f27f21360f52b47b0b4fb0f2d4d1b
Christian Schaller <uraeus@gnome.org>
2004-12-07 10:32:11 +0000

    fix licensing information for monoscope plugin
    Original commit message from CVS:
    fix licensing information for monoscope plugin

--- gst/monoscope/convolve.c
+++ gst/monoscope/convolve.c
@@ -2,21 +2,25 @@
  *
  *  Copyright (C) 1999 Ralph Loader <suckfish@ihug.co.nz>
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  
- *  
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
  *
+ * 
+ * Note: 7th December 2004: This file used to be licensed under the GPL,
+ * 	 but we got permission from Ralp Loader to relicense it to LGPL.
+ * 
  *  $Id$
  *
  */
