fd8b04a8e7a715fdc8c8b681f366dabbbcdbb15b
Ronald S. Bultje <rbultje@ronald.bitfreak.net>
2005-01-09 20:08:45 +0000

    gst/tta/gstttaparse.c: Fix gcc-2.95 compile (#163485).
    Original commit message from CVS:
    Reviewed by:  Ronald S. Bultje  <rbultje@ronald.bitfreak.net>
    * gst/tta/gstttaparse.c: (gst_tta_src_event):
    Fix gcc-2.95 compile (#163485).

--- gst/tta/gstttaparse.c
+++ gst/tta/gstttaparse.c
@@ -162,11 +162,14 @@ gst_tta_src_event (GstPad * pad, GstEvent * event)
     case GST_EVENT_SEEK:
     {
       if (GST_EVENT_SEEK_FORMAT (event) == GST_FORMAT_TIME) {
-        GST_DEBUG_OBJECT (ttaparse, "got seek event");
         GstEvent *seek_event;
-        guint64 time = GST_EVENT_SEEK_OFFSET (event);
-        guint64 seek_frame = time / (FRAME_TIME * 1000000000);
-        guint64 seekpos = ttaparse->index[seek_frame].pos;
+        guint64 time;
+        guint64 seek_frame;
+        guint64 seekpos;
+
+        time = GST_EVENT_SEEK_OFFSET (event);
+        seek_frame = time / (FRAME_TIME * 1000000000);
+        seekpos = ttaparse->index[seek_frame].pos;
 
         GST_DEBUG_OBJECT (ttaparse, "seeking to %u", (guint) seekpos);
         seek_event =
