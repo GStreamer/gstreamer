e7a01297745886464a227e4738f250e2a0861300
Ronald S. Bultje <rbultje@ronald.bitfreak.net>
2005-01-17 23:34:45 +0000

    ext/faad/gstfaad.c: Argh...
    Original commit message from CVS:
    * ext/faad/gstfaad.c: (gst_faad_srcgetcaps):
    Argh...

--- ext/faad/gstfaad.c
+++ ext/faad/gstfaad.c
@@ -323,7 +323,7 @@ gst_faad_srcgetcaps (GstPad * pad)
     guchar *supported_fpos = g_new0 (guchar, num_supported_positions);
     gint n;
 
-    for (n = 0; n <= num_supported_positions; n++) {
+    for (n = 0; n < num_supported_positions; n++) {
       supported_fpos[n] = n + FRONT_CHANNEL_CENTER;
     }
     supported_positions = gst_faad_chanpos_to_gst (supported_fpos,
