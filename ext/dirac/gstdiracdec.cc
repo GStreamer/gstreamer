3b9710a1da22b24e4dd5046fe9291beb60fd3cc5
Tim-Philipp Müller <tim@centricular.net>
2005-02-21 23:41:23 +0000

    Since dirac 0.5.0 the framerate in dirac is expressed as a rational number. Fix build and up requirement to 0.5.0, an...
    Original commit message from CVS:
    Since dirac 0.5.0 the framerate in dirac is expressed as a rational number. Fix build and up requirement to 0.5.0, and also pass parameters to gst_diracdec_link in the right order. (fixes #167959)

--- ext/dirac/gstdiracdec.cc
+++ ext/dirac/gstdiracdec.cc
@@ -253,13 +253,18 @@ gst_diracdec_chain (GstPad * pad, GstData * _data)
 
       case STATE_SEQUENCE:{
         guint8 *buf[3];
+        gint fps_num, fps_denom;
+
+        fps_num = diracdec->decoder->seq_params.frame_rate.numerator;
+        fps_denom = diracdec->decoder->seq_params.frame_rate.denominator;
 
         /* start-of-sequence - allocate buffer */
         if (!gst_diracdec_link (diracdec,
                 diracdec->decoder->seq_params.width,
                 diracdec->decoder->seq_params.height,
+                (gdouble) fps_num / (gdouble) fps_denom,
                 gst_diracdec_chroma_to_fourcc (diracdec->decoder->seq_params.
-                    chroma), diracdec->decoder->seq_params.frame_rate)) {
+                    chroma))) {
           GST_ELEMENT_ERROR (diracdec, CORE, NEGOTIATION, (NULL),
               ("Failed to set caps to %dx%d @ %d fps (format=" GST_FOURCC_FORMAT
                   "/%d)", diracdec->decoder->seq_params.width,
