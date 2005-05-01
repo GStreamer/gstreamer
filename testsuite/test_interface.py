8641156552621a8ff1c3c2134d80490ac1e520d9
Thomas Vander Stichele <thomas@apestaart.org>
2005-05-01 17:36:38 +0000

    do not do stuff in a testsuite that isn't guaranteed to be there.
    Original commit message from CVS:
    do not do stuff in a testsuite that isn't guaranteed to be there.
    could do with some way of detecting and skipping.

--- testsuite/test_interface.py
+++ testsuite/test_interface.py
@@ -12,7 +12,10 @@ class Availability(unittest.TestCase):
         assert issubclass(gst.interfaces.Mixer, gobject.GInterface)
 
 class FunctionCall(unittest.TestCase):
-    def testXOverlay(self):
+    def FIXME_testXOverlay(self):
+        # obviously a testsuite is not allowed to instantiate this
+        # since it needs a running X or will fail.  find some way to
+        # deal with that.
         element = gst.element_factory_make('xvimagesink')
         assert isinstance(element, gst.Element)
         assert isinstance(element, gst.interfaces.XOverlay)
