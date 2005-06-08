72ad1d89ff2fc13831b2bb10d47abb229b9f9974
Edward Hervey <bilboed@bilboed.com>
2005-06-08 17:16:09 +0000

    examples/gst/: renamed gst-file.py to gstfile.py and made the Discoverer class useable in other applications/scripts
    Original commit message from CVS:
    * examples/gst/Makefile.am:
    * examples/gst/gstfile.py:
    renamed gst-file.py to gstfile.py and made the Discoverer class useable
    in other applications/scripts
    * examples/gst/audioconcat.py:
    Example application that uses the Discoverer and the gnonlin elements to
    concatenate several audio files to one single one

---
+++ examples/gst/audioconcat.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+
+# audio concat tool
+# takes in one or more audio files and creates one audio file of the combination
+
+# Uses the gnonlin elements (http://gnonlin.sf.net/)
+
+import os
+import sys
+import gobject
+import gst
+
+from gstfile import Discoverer, time_to_string
+
+class AudioSource(gst.Bin):
+    """A bin for audio sources with proper audio converters"""
+
+    def __init__(self, filename, caps):
+        gst.Bin.__init__(self)
+        self.filename = filename
+        self.outcaps = caps
+
+        self.filesrc = gst.element_factory_make("filesrc")
+        self.filesrc.set_property("location", self.filename)
+        self.dbin = gst.element_factory_make("decodebin")
+        self.ident = gst.element_factory_make("identity")
+        self.audioconvert = gst.element_factory_make("audioconvert")
+        self.audioscale = gst.element_factory_make("audioscale")
+        
+        self.add_many(self.filesrc, self.dbin, self.ident,
+                      self.audioconvert, self.audioscale)
+        self.filesrc.link(self.dbin)
+        self.audioconvert.link(self.audioscale)
+        self.audioscale.link(self.ident, caps)
+        self.add_ghost_pad(self.ident.get_pad("src"), "src")
+        
+        self.dbin.connect("new-decoded-pad", self._new_decoded_pad_cb)
+
+    def _new_decoded_pad_cb(self, dbin, pad, is_last):
+        if not "audio" in pad.get_caps().to_string():
+            return
+        pad.link(self.audioconvert.get_pad("sink"))
+
+gobject.type_register(AudioSource)
+
+class AudioConcat(gst.Thread):
+    """A Gstreamer thread that concatenates a series of audio files to another audio file"""
+
+    def __init__(self, infiles, outfile, audioenc="rawvorbisenc", muxer="oggmux"):
+        gst.Thread.__init__(self)
+        self.infiles = infiles
+        self.outfile = outfile
+        self.audioenc = gst.element_factory_make(audioenc)
+        if not self.audioenc:
+            raise NameError, str(audioenc + " audio encoder is not available")
+        self.muxer = gst.element_factory_make(muxer)
+        if not self.muxer:
+            raise NameError, str(muxer + " muxer is not available")
+        self.filesink = gst.element_factory_make("filesink")
+        self.filesink.set_property("location", self.outfile)
+
+        self.timeline = gst.element_factory_make("gnltimeline")
+        self.audiocomp = gst.element_factory_make("gnlcomposition", "audiocomp")
+
+        self.audioconvert = gst.element_factory_make("audioconvert")
+        self.add_many(self.timeline, self.audioconvert,
+                      self.audioenc, self.muxer, self.filesink)
+        self.audioconvert.link(self.audioenc)
+        self.audioenc.link(self.muxer)
+        self.muxer.link(self.filesink)
+
+        self.timeline.add(self.audiocomp)
+
+        caps = gst.caps_from_string("audio/x-raw-int,channels=2,rate=44100,depth=16")
+        pos = 0L
+        for infile in self.infiles:
+            d = Discoverer(infile)
+            if not d.audiolength:
+                continue
+            print "file", infile, "has length", time_to_string(d.audiolength)
+            asource = AudioSource(infile, caps)
+            gnlsource = gst.element_factory_make("gnlsource")
+            gnlsource.set_property("element", asource)
+            gnlsource.set_property("media-start", 0L)
+            gnlsource.set_property("media-stop", d.audiolength)
+            gnlsource.set_property("start", pos)
+            gnlsource.set_property("stop", pos + d.audiolength)
+            self.audiocomp.add(gnlsource)
+            pos += d.audiolength
+
+        self.timeline.get_pad("src_audiocomp").link(self.audioconvert.get_pad("sink"))
+
+gobject.type_register(AudioConcat)
+
+def eos_cb(pipeline):
+    sys.exit()
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print "Usage : %s <input file(s)> <output file>" % sys.argv[0]
+        print "\tCreates an ogg file from all the audio input files"
+        sys.exit()
+    if not gst.element_factory_make("gnltimeline"):
+        print "You need the gnonlin elements installed (http://gnonlin.sf.net/)"
+        sys.exit()
+    concat = AudioConcat(sys.argv[1:-1], sys.argv[-1])
+    concat.connect("eos", eos_cb)
+    concat.set_state(gst.STATE_PLAYING)
+    gst.main()
---
+++ examples/gst/gstfile.py
@@ -0,0 +1,236 @@
+#!/usr/bin/env python
+
+# gstfile.py
+# (c) 2005 Edward Hervey <edward at fluendo dot com>
+# Discovers and prints out multimedia information of files
+
+# This example shows how to use gst-python:
+# _ in an object-oriented way (Discoverer class)
+# _ subclassing a gst.Pipeline
+# _ and overidding existing methods (do_iterate())
+
+import os
+import sys
+
+import gobject
+import gst
+
+def time_to_string(value):
+    """
+    transform a value in nanoseconds into a human-readable string
+    """
+    ms = value / gst.MSECOND
+    sec = ms / 1000
+    ms = ms % 1000
+    min = sec / 60
+    sec = sec % 60
+    return "%2dm %2ds %3d" % (min, sec, ms)
+
+
+class Discoverer(gst.Pipeline):
+    """
+    Discovers information about files
+    """
+    mimetype = None
+
+    audiocaps = {}
+    videocaps = {}
+
+    videowidth = 0
+    videoheight = 0
+    videorate = 0
+
+    audiofloat = False
+    audiorate = 0
+    audiodepth = 0
+    audiowidth = 0
+    audiochannels = 0
+
+    audiolength = 0L
+    videolength = 0L
+
+    is_video = False
+    is_audio = False
+
+    otherstreams = []
+
+    finished = False
+    tags = {}
+
+
+    def __init__(self, filename):
+        gobject.GObject.__init__(self)
+
+        self.mimetype = None
+
+        self.audiocaps = {}
+        self.videocaps = {}
+
+        self.videowidth = 0
+        self.videoheight = 0
+        self.videorate = 0
+
+        self.audiofloat = False
+        self.audiorate = 0
+        self.audiodepth = 0
+        self.audiowidth = 0
+        self.audiochannels = 0
+
+        self.audiolength = 0L
+        self.videolength = 0L
+
+        self.is_video = False
+        self.is_audio = False
+
+        self.otherstreams = []
+
+        self.finished = False
+        self.tags = {}
+        
+        if not os.path.isfile(filename):
+            self.finished = True
+            return
+        
+        # the initial elements of the pipeline
+        self.src = gst.element_factory_make("filesrc")
+        self.src.set_property("location", filename)
+        self.dbin = gst.element_factory_make("decodebin")
+        self.add_many(self.src, self.dbin)
+        self.src.link(self.dbin)
+        self.typefind = self.dbin.get_by_name("typefind")
+
+        # callbacks
+        self.typefind.connect("have-type", self._have_type_cb)
+        self.dbin.connect("new-decoded-pad", self._new_decoded_pad_cb)
+        self.dbin.connect("unknown-type", self._unknown_type_cb)
+        self.dbin.connect("found-tag", self._found_tag_cb)
+
+        self.discover()
+
+    def discover(self):
+        """iterate on ourself to find the information on the given file"""
+        if self.finished:
+            return
+        self.set_state(gst.STATE_PLAYING)
+        while 1:
+            if not self.iterate():
+                break
+        self.set_state(gst.STATE_NULL)
+
+    def print_info(self):
+        """prints out the information on the given file"""
+        if not self.finished:
+            self.discover()
+        if not self.mimetype:
+            print "Unknow media type"
+            return
+        print "Mime Type :\t", self.mimetype
+        if not self.is_video and not self.is_audio:
+            return
+        print "Length :\t", time_to_string(max(self.audiolength, self.videolength))
+        print "Audio:", time_to_string(self.audiolength), "\tVideo:", time_to_string(self.videolength)
+        if self.is_video:
+            print "Video :"
+            print "\t%d x %d @ %.2f fps" % (self.videowidth,
+                                            self.videoheight,
+                                            self.videorate)
+            if self.tags.has_key("video-codec"):
+                print "\tCodec :", self.tags.pop("video-codec")
+        if self.is_audio:
+            print "Audio :"
+            if self.audiofloat:
+                print "\t%d channels(s) : %dHz @ %dbits (float)" % (self.audiochannels,
+                                                                    self.audiorate,
+                                                                    self.audiowidth)
+            else:
+                print "\t%d channels(s) : %dHz @ %dbits (int)" % (self.audiochannels,
+                                                                  self.audiorate,
+                                                                  self.audiodepth)
+            if self.tags.has_key("audio-codec"):
+                print "\tCodec :", self.tags.pop("audio-codec")
+        for stream in self.otherstreams:
+            if not stream == self.mimetype:
+                print "Other unsuported Multimedia stream :", stream
+        if self.tags:
+            print "Additional information :"
+            for tag in self.tags.keys():
+                print "%20s :\t" % tag, self.tags[tag]
+
+    def _unknown_type_cb(self, dbin, pad, caps):
+        self.otherstreams.append(caps.to_string())
+
+    def _have_type_cb(self, typefind, prob, caps):
+        self.mimetype = caps.to_string()
+
+    def _notify_caps_cb(self, pad, args):
+        caps = pad.get_negotiated_caps()
+        if not caps:
+            return
+        # the caps are fixed
+        # We now get the total length of that stream
+        length = pad.get_peer().query(gst.QUERY_TOTAL, gst.FORMAT_TIME)
+        # We store the caps and length in the proper location
+        if "audio" in caps.to_string():
+            self.audiocaps = caps
+            self.audiolength = length
+            self.audiorate = caps[0]["rate"]
+            self.audiowidth = caps[0]["width"]
+            self.audiochannels = caps[0]["channels"]
+            if "x-raw-float" in caps.to_string():
+                self.audiofloat = True
+            else:
+                self.audiodepth = caps[0]["depth"]
+            if (not self.is_video) or self.videocaps:
+                self.finished = True
+        elif "video" in caps.to_string():
+            self.videocaps = caps
+            self.videolength = length
+            self.videowidth = caps[0]["width"]
+            self.videoheight = caps[0]["height"]
+            self.videorate = caps[0]["framerate"]
+            if (not self.is_audio) or self.audiocaps:
+                self.finished = True
+
+    def _new_decoded_pad_cb(self, dbin, pad, is_last):
+        # Does the file contain got audio or video ?
+        if "audio" in pad.get_caps().to_string():
+            self.is_audio = True
+        elif "video" in pad.get_caps().to_string():
+            self.is_video = True
+        # we connect a fakesink to the new pad...
+        fakesink = gst.element_factory_make("fakesink")
+        self.add(fakesink)
+        sinkpad = fakesink.get_pad("sink")
+        # ... and connect a callback for when the caps are fixed
+        sinkpad.connect("notify::caps", self._notify_caps_cb)
+        pad.link(sinkpad)
+        fakesink.set_state(gst.STATE_PLAYING)
+
+    def _found_tag_cb(self, dbin, source, tags):
+        self.tags.update(tags)
+
+    def do_iterate(self):
+        # this overrides the GstBin 'iterate' method
+        # if we have finished discovering we stop the iteration
+        if self.finished:
+            return False
+        # else we call the parent class method
+        return gst.Pipeline.do_iterate(self)
+
+gobject.type_register(Discoverer)
+
+def main(args):
+    if len(args) < 2:
+        print 'usage: %s files...' % args[0]
+        return 2
+
+    if len(args[1:]) > 1:
+        for filename in args[1:]:
+            print "File :", filename
+            Discoverer(filename).print_info()
+            print "\n"
+    else:
+        Discoverer(args[1]).print_info()
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
