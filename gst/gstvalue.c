f05cd57bf26cadf25f51d9a6a31fe40c8b3f5c28
Ronald S. Bultje <rbultje@ronald.bitfreak.net>
2005-06-30 10:10:00 +0000

    gst/gstvalue.c: Fix memleak (#309125).
    Original commit message from CVS:
    Reviewed by:  Ronald S. Bultje  <rbultje@ronald.bitfreak.net>
    * gst/gstvalue.c: (gst_value_intersect_list):
    Fix memleak (#309125).

--- gst/gstvalue.c
+++ gst/gstvalue.c
@@ -1713,6 +1713,7 @@ gst_value_intersect_list (GValue * dest, const GValue * value1,
         gst_value_init_and_copy (&temp, dest);
         g_value_unset (dest);
         gst_value_list_concat (dest, &temp, &intersection);
+        g_value_unset (&temp);
       }
       g_value_unset (&intersection);
     }
