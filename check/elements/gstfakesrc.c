e8007886c16f1cad8d6c60b84e96dec9eadbd2b0
Thomas Vander Stichele <thomas@apestaart.org>
2005-08-05 06:55:03 +0000

    add sizetype tests for fakesrc
    Original commit message from CVS:
    add sizetype tests for fakesrc

--- check/elements/gstfakesrc.c
+++ check/elements/gstfakesrc.c
@@ -57,7 +57,8 @@ event_func (GstPad * pad, GstEvent * event)
   return FALSE;
 }
 
-GST_START_TEST (test_num_buffers)
+GstElement *
+setup_fakesrc ()
 {
   GstElement *src;
   GstPad *srcpad, *sinkpad;
@@ -70,8 +71,6 @@ GST_START_TEST (test_num_buffers)
       "sink");
   fail_if (sinkpad == NULL, "Could not create a sinkpad");
 
-  g_object_set (G_OBJECT (src), "num-buffers", 3, NULL);
-
   srcpad = gst_element_get_pad (src, "src");
   fail_if (srcpad == NULL, "Could not get source pad from fakesrc");
   gst_pad_set_caps (sinkpad, NULL);
@@ -80,7 +79,41 @@ GST_START_TEST (test_num_buffers)
 
   fail_unless (gst_pad_link (srcpad, sinkpad) == GST_PAD_LINK_OK,
       "Could not link source and sink pads");
+  return src;
+}
 
+void
+cleanup_fakesrc (GstElement * src)
+{
+  GstPad *srcpad, *sinkpad;
+
+  fail_unless (gst_element_set_state (src, GST_STATE_NULL) == GST_STATE_SUCCESS,
+      "could not set to null");
+
+  srcpad = gst_element_get_pad (src, "src");
+  sinkpad = gst_pad_get_peer (srcpad);
+
+  ASSERT_OBJECT_REFCOUNT (src, "src", 1);
+  gst_object_unref (src);
+
+  gst_pad_unlink (srcpad, sinkpad);
+
+  /* pad refs held by both creator and this function (through _get) */
+  ASSERT_OBJECT_REFCOUNT (srcpad, "srcpad", 2);
+  gst_object_unref (srcpad);
+  gst_object_unref (srcpad);
+
+  ASSERT_OBJECT_REFCOUNT (sinkpad, "sinkpad", 2);
+  gst_object_unref (sinkpad);
+  gst_object_unref (sinkpad);
+}
+
+GST_START_TEST (test_num_buffers)
+{
+  GstElement *src;
+
+  src = setup_fakesrc ();
+  g_object_set (G_OBJECT (src), "num-buffers", 3, NULL);
   fail_unless (gst_element_set_state (src,
           GST_STATE_PLAYING) == GST_STATE_SUCCESS, "could not set to playing");
 
@@ -93,14 +126,117 @@ GST_START_TEST (test_num_buffers)
   g_list_free (buffers);
 
   /* cleanup */
-  fail_unless (gst_element_set_state (src, GST_STATE_NULL) == GST_STATE_SUCCESS,
-      "could not set to null");
-
-  gst_object_unref (src);
-  gst_object_unref (sinkpad);
+  cleanup_fakesrc (src);
 }
 
 GST_END_TEST;
+
+GST_START_TEST (test_sizetype_empty)
+{
+  GstElement *src;
+  GList *l;
+
+  src = setup_fakesrc ();
+
+  g_object_set (G_OBJECT (src), "sizetype", 1, NULL);
+  g_object_set (G_OBJECT (src), "num-buffers", 100, NULL);
+
+  fail_unless (gst_element_set_state (src,
+          GST_STATE_PLAYING) == GST_STATE_SUCCESS, "could not set to playing");
+
+  while (!have_eos) {
+    g_usleep (1000);
+  }
+
+  fail_unless (g_list_length (buffers) == 100);
+  l = buffers;
+  while (l) {
+    GstBuffer *buf = l->data;
+
+    fail_unless (GST_BUFFER_SIZE (buf) == 0);
+    l = l->next;
+  }
+  g_list_foreach (buffers, (GFunc) gst_mini_object_unref, NULL);
+  g_list_free (buffers);
+
+  /* cleanup */
+  cleanup_fakesrc (src);
+}
+
+GST_END_TEST;
+
+GST_START_TEST (test_sizetype_fixed)
+{
+  GstElement *src;
+  GList *l;
+
+  src = setup_fakesrc ();
+
+  g_object_set (G_OBJECT (src), "sizetype", 2, NULL);
+  g_object_set (G_OBJECT (src), "sizemax", 8192, NULL);
+  g_object_set (G_OBJECT (src), "num-buffers", 100, NULL);
+
+  fail_unless (gst_element_set_state (src,
+          GST_STATE_PLAYING) == GST_STATE_SUCCESS, "could not set to playing");
+
+  while (!have_eos) {
+    g_usleep (1000);
+  }
+
+  fail_unless (g_list_length (buffers) == 100);
+  l = buffers;
+  while (l) {
+    GstBuffer *buf = l->data;
+
+    fail_unless (GST_BUFFER_SIZE (buf) == 8192);
+    l = l->next;
+  }
+  g_list_foreach (buffers, (GFunc) gst_mini_object_unref, NULL);
+  g_list_free (buffers);
+
+  /* cleanup */
+  cleanup_fakesrc (src);
+}
+
+GST_END_TEST;
+
+GST_START_TEST (test_sizetype_random)
+{
+  GstElement *src;
+  GList *l;
+
+  src = setup_fakesrc ();
+
+  g_object_set (G_OBJECT (src), "sizetype", 3, NULL);
+  g_object_set (G_OBJECT (src), "sizemin", 4096, NULL);
+  g_object_set (G_OBJECT (src), "sizemax", 8192, NULL);
+  g_object_set (G_OBJECT (src), "num-buffers", 100, NULL);
+
+  fail_unless (gst_element_set_state (src,
+          GST_STATE_PLAYING) == GST_STATE_SUCCESS, "could not set to playing");
+
+  while (!have_eos) {
+    g_usleep (1000);
+  }
+
+  fail_unless (g_list_length (buffers) == 100);
+  l = buffers;
+  while (l) {
+    GstBuffer *buf = l->data;
+
+    fail_if (GST_BUFFER_SIZE (buf) > 8192);
+    fail_if (GST_BUFFER_SIZE (buf) < 4096);
+    l = l->next;
+  }
+  g_list_foreach (buffers, (GFunc) gst_mini_object_unref, NULL);
+  g_list_free (buffers);
+
+  /* cleanup */
+  cleanup_fakesrc (src);
+}
+
+GST_END_TEST;
+
 
 Suite *
 fakesrc_suite (void)
@@ -110,6 +246,9 @@ fakesrc_suite (void)
 
   suite_add_tcase (s, tc_chain);
   tcase_add_test (tc_chain, test_num_buffers);
+  tcase_add_test (tc_chain, test_sizetype_empty);
+  tcase_add_test (tc_chain, test_sizetype_fixed);
+  tcase_add_test (tc_chain, test_sizetype_random);
 
   return s;
 }
