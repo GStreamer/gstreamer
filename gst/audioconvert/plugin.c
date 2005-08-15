3e589b0977fbec68c596893b47278ea4fc60b34f
Thomas Vander Stichele <thomas@apestaart.org>
2005-08-15 14:15:40 +0000

    rename plugin
    Original commit message from CVS:
    rename plugin

--- gst/audioconvert/plugin.c
+++ gst/audioconvert/plugin.c
@@ -39,6 +39,6 @@ plugin_init (GstPlugin * plugin)
 
 GST_PLUGIN_DEFINE (GST_VERSION_MAJOR,
     GST_VERSION_MINOR,
-    "gstaudioconvert",
+    "audioconvert",
     "Convert audio to different formats",
     plugin_init, VERSION, "LGPL", GST_PACKAGE, GST_ORIGIN)
