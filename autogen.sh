d131d746f97b08693fa9e06985b3a7c73da80c5f
Thomas Vander Stichele <thomas@apestaart.org>
2005-09-05 17:55:10 +0000

    maintenance updates
    Original commit message from CVS:
    maintenance updates

--- autogen.sh
+++ autogen.sh
@@ -6,12 +6,14 @@ package=gstreamer
 srcfile=gst/gst.c
 
 # a quick cvs co to ease the transition
-if test ! -d common; then
-  if test -f CVS/Tag; then
-    # get everything from CVS/Tag from second character on
+if test ! -d common;
+then
+  echo "+ getting common/ from cvs"
+  if test -e CVS/Tag
+  then
     TAG="-r `tail -c +2 CVS/Tag`"
   fi
-  echo "+ getting common from cvs"; cvs co $TAG common
+  cvs co $TAG common
 fi
 
 # source helper functions
