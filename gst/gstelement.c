361d0f2922fa9d9ed857a1b4591dd1493beaed26
Thomas Vander Stichele <thomas@apestaart.org>
2005-09-12 16:10:18 +0000

    docs/: add a quote
    Original commit message from CVS:
    
    * docs/gst/tmpl/gstelement.sgml:
    * docs/manual/appendix-quotes.xml:
    add a quote
    * gst/gstelement.c: (gst_element_set_state):
    add some debug

--- gst/gstelement.c
+++ gst/gstelement.c
@@ -1772,6 +1772,8 @@ gst_element_set_state (GstElement * element, GstState state)
 
   g_return_val_if_fail (GST_IS_ELEMENT (element), GST_STATE_CHANGE_FAILURE);
 
+  GST_DEBUG_OBJECT (element, "set_state to %s",
+      gst_element_state_get_name (state));
   /* get current element state,  need to call the method so that
    * we call the virtual method and subclasses can implement their
    * own algorithms */
