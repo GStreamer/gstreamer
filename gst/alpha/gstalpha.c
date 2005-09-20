cdb18ecaa700952e2f42be49260afac3ee599047
Stefan Kost <ensonic@users.sourceforge.net>
2005-09-20 07:30:31 +0000

    gst/alpha/gstalpha.c: fix element description
    Original commit message from CVS:
    * gst/alpha/gstalpha.c:
    fix element description

--- gst/alpha/gstalpha.c
+++ gst/alpha/gstalpha.c
@@ -891,5 +891,5 @@ plugin_init (GstPlugin * plugin)
 GST_PLUGIN_DEFINE (GST_VERSION_MAJOR,
     GST_VERSION_MINOR,
     "alpha",
-    "resizes a video by adding borders or cropping",
+    "adds an alpha channel to video",
     plugin_init, VERSION, GST_LICENSE, GST_PACKAGE, GST_ORIGIN)
