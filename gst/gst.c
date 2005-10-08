e1d580ab0e135bc21bb347c80753042b65881ae0
Thomas Vander Stichele <thomas@apestaart.org>
2005-10-08 11:10:17 +0000

    Makefile.am: remove include dir
    Original commit message from CVS:
    
    * Makefile.am:
    remove include dir
    * configure.ac:
    remove PLUGINS_BUILDDIR stuff
    * gst/gst.c: (init_post):
    reorder parsing of GST_PLUGIN_PATH and GST_PLUGIN_SYSTEM_PATH
    * idiottest.mak:
    remove, it was condescending and not needed

--- gst/gst.c
+++ gst/gst.c
@@ -692,39 +692,8 @@ init_post (void)
     GST_DEBUG ("Reading registry cache");
     gst_registry_xml_read_cache (default_registry, registry_file);
 
-    plugin_path = g_getenv ("GST_PLUGIN_SYSTEM_PATH");
-    if (plugin_path == NULL) {
-      GST_DEBUG ("GST_PLUGIN_SYSTEM_PATH not set");
-#ifdef PLUGINS_USE_BUILDDIR
-      /* location libgstelements.so */
-      gst_registry_scan_path (default_registry,
-          PLUGINS_BUILDDIR "/gst/elements/.libs");
-      gst_registry_scan_path (default_registry,
-          PLUGINS_BUILDDIR "/gst/indexers/.libs");
-#else
-      char *home_plugins;
-
-      /* add the main (installed) library path */
-      gst_registry_scan_path (default_registry, PLUGINS_DIR);
-
-      home_plugins = g_build_filename (g_get_home_dir (),
-          ".gstreamer-0.9", "plugins", NULL);
-      gst_registry_scan_path (default_registry, home_plugins);
-      g_free (home_plugins);
-#endif /* PLUGINS_USE_BUILDDIR */
-    } else {
-      char **list;
-      int i;
-
-      GST_DEBUG ("GST_PLUGIN_SYSTEM_PATH set to %s", plugin_path);
-      /* FIXME this doesn't split paths correctly on windows */
-      list = g_strsplit (plugin_path, ":", 0);
-      for (i = 0; list[i]; i++) {
-        gst_registry_scan_path (default_registry, list[i]);
-      }
-      g_strfreev (list);
-    }
-
+    /* GST_PLUGIN_PATH specifies a list of directories to scan for
+     * additional plugins.  These take precedence over the system plugins */
     plugin_path = g_getenv ("GST_PLUGIN_PATH");
     if (plugin_path) {
       char **list;
@@ -741,12 +710,44 @@ init_post (void)
       GST_DEBUG ("GST_PLUGIN_PATH not set");
     }
 
+    /* GST_PLUGIN_SYSTEM_PATH specifies a list of plugins that are always
+     * loaded by default.  If not set, this defaults to the system-installed
+     * path, and the plugins installed in the user's home directory */
+    plugin_path = g_getenv ("GST_PLUGIN_SYSTEM_PATH");
+    if (plugin_path == NULL) {
+      char *home_plugins;
+
+      GST_DEBUG ("GST_PLUGIN_SYSTEM_PATH not set");
+
+      /* plugins in the user's home directory take precedence over
+       * system-installed ones */
+      home_plugins = g_build_filename (g_get_home_dir (),
+          ".gstreamer-0.9", "plugins", NULL);
+      gst_registry_scan_path (default_registry, home_plugins);
+      g_free (home_plugins);
+
+      /* add the main (installed) library path */
+      gst_registry_scan_path (default_registry, PLUGINS_DIR);
+    } else {
+      char **list;
+      int i;
+
+      GST_DEBUG ("GST_PLUGIN_SYSTEM_PATH set to %s", plugin_path);
+      /* FIXME this doesn't split paths correctly on windows */
+      list = g_strsplit (plugin_path, ":", 0);
+      for (i = 0; list[i]; i++) {
+        gst_registry_scan_path (default_registry, list[i]);
+      }
+      g_strfreev (list);
+    }
+
     gst_registry_xml_write_cache (default_registry, registry_file);
 
     _gst_registry_remove_cache_plugins (default_registry);
 
     g_free (registry_file);
   }
+
 #endif /* GST_DISABLE_REGISTRY */
 
   /* if we need to preload plugins */
