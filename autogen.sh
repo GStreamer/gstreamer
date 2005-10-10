0f03b8eef651fb7f471a9ec576a61eb8cbce21ab
Andy Wingo <wingo@pobox.com>
2005-10-10 08:51:59 +0000

    autogen.sh (CONFIGURE_DEF_OPT): No more --plugin-buiddir, yay
    Original commit message from CVS:
    2005-10-10  Andy Wingo  <wingo@pobox.com>
    
    * autogen.sh (CONFIGURE_DEF_OPT): No more --plugin-buiddir, yay

--- autogen.sh
+++ autogen.sh
@@ -25,7 +25,7 @@ then
 fi
 . common/gst-autogen.sh
 
-CONFIGURE_DEF_OPT='--enable-maintainer-mode --enable-plugin-builddir --enable-failing-tests --enable-poisoning --enable-gtk-doc --enable-docbook'
+CONFIGURE_DEF_OPT='--enable-maintainer-mode --enable-failing-tests --enable-poisoning --enable-gtk-doc --enable-docbook'
 
 autogen_options $@
 
