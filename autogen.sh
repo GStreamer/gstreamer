de1f0d89a4e61a68b00bbf5c2192b7c041639065
Thomas Vander Stichele <thomas@apestaart.org>
2005-10-12 02:16:26 +0000

    autoconf for freebsd
    Original commit message from CVS:
    autoconf for freebsd

--- autogen.sh
+++ autogen.sh
@@ -31,7 +31,7 @@ autogen_options $@
 
 echo -n "+ check for build tools"
 if test ! -z "$NOCHECK"; then echo ": skipped version checks"; else  echo; fi
-version_check "autoconf" "$AUTOCONF autoconf autoconf-2.54 autoconf-2.53 autoconf-2.52" \
+version_check "autoconf" "$AUTOCONF autoconf autoconf-2.54 autoconf-2.53 autoconf-2.52 autoconf252" \
               "ftp://ftp.gnu.org/pub/gnu/autoconf/" 2 52 || DIE=1
 version_check "automake" "$AUTOMAKE automake automake-1.7 automake17 automake-1.6" \
               "ftp://ftp.gnu.org/pub/gnu/automake/" 1 6 || DIE=1
