3e85dcc15ea80184b85e105563ffd7e638616806
Andy Wingo <wingo@pobox.com>
2005-10-12 17:20:17 +0000

    autogen.sh (CONFIGURE_DEF_OPT): Check for automake 1.9 and 1.8, but not for 1.5.
    Original commit message from CVS:
    2005-10-12  Andy Wingo  <wingo@pobox.com>
    
    * autogen.sh (CONFIGURE_DEF_OPT): Check for automake 1.9 and 1.8,
    but not for 1.5.

--- autogen.sh
+++ autogen.sh
@@ -33,7 +33,7 @@ echo -n "+ check for build tools"
 if test ! -z "$NOCHECK"; then echo " skipped"; else  echo; fi
 version_check "autoconf" "$AUTOCONF autoconf autoconf-2.54 autoconf-2.53 autoconf-2.52" \
               "ftp://ftp.gnu.org/pub/gnu/autoconf/" 2 52 || DIE=1
-version_check "automake" "$AUTOMAKE automake automake-1.7 automake-1.6 automake-1.5" \
+version_check "automake" "$AUTOMAKE automake automake-1.9 automake-1.8 automake-1.7 automake-1.6" \
               "ftp://ftp.gnu.org/pub/gnu/automake/" 1 6 || DIE=1
 version_check "libtoolize" "$LIBTOOLIZE libtoolize" \
               "ftp://ftp.gnu.org/pub/gnu/libtool/" 1 5 0 || DIE=1
