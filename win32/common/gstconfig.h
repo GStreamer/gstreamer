8817d3e16af0a80ff19bf54d2c6e3c746fd6983c
Thomas Vander Stichele <thomas@apestaart.org>
2005-10-12 20:23:42 +0000

    Makefile.am: add win32-update target
    Original commit message from CVS:
    
    * Makefile.am:
    add win32-update target
    * win32/common/gstconfig.h:
    * win32/common/gstenumtypes.c:
    * win32/common/gstenumtypes.h:
    * win32/common/gstversion.h:
    add files that visual studio can't generate

---
+++ win32/common/gstconfig.h
@@ -0,0 +1,156 @@
+/* GStreamer
+ * Copyright (C) 1999,2000 Erik Walthinsen <omega@cse.ogi.edu>
+ *               2004,2005 Wim Taymans <wim@fluendo.com>
+ *
+ * gstconfig.h: GST_DISABLE_* macros for build configuration
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+/**
+ * SECTION:gstconfig
+ * @short_description: Build configuration options
+ *
+ * This describes the configuration options for GStreamer. When building 
+ * GStreamer there are a lot of parts (known internally as "subsystems" ) that can 
+ * be disabled for various reasons. The most common reasons are speed and size, 
+ * which is important because GStreamer is designed to run on embedded systems.
+ * 
+ * If a subsystem is disabled, most of this changes are done in an API compatible
+ * way, so you don't need to adapt your code in most cases. It is never done in an
+ * ABI compatible way though. So if you want to disable a suybsystem, you have to
+ * rebuild all programs depending on GStreamer, too.
+ * 
+ * If a subsystem is disabled in GStreamer, a value is defined in 
+ * &lt;gst/gst.h&gt;. You can check this if you do subsystem-specific stuff.
+ * <example>
+ * <title>Doing subsystem specific things</title>
+ * <programlisting>
+ * &hash;ifndef GST_DISABLE_GST_DEBUG
+ * // do stuff specific to the debugging subsystem
+ * &hash;endif // GST_DISABLE_GST_DEBUG
+ * </programlisting>
+ * </example>
+ */
+
+#ifndef __GST_CONFIG_H__
+#define __GST_CONFIG_H__
+
+/***** trick gtk-doc into believing these symbols are defined (yes, it's ugly) */
+
+#if 0
+#define GST_DISABLE_LOADSAVE_REGISTRY 1
+#define GST_DISABLE_GST_DEBUG 1
+#define GST_DISABLE_LOADSAVE 1
+#define GST_DISABLE_PARSE 1
+#define GST_DISABLE_TRACE 1
+#define GST_DISABLE_ALLOC_TRACE 1
+#define GST_DISABLE_REGISTRY 1
+#define GST_DISABLE_ENUMTYPES 1
+#define GST_DISABLE_INDEX 1
+#define GST_DISABLE_PLUGIN 1
+#define GST_DISABLE_URI 1
+#define GST_HAVE_GLIB_2_8 1
+#endif
+
+/***** default padding of structures *****/
+#define GST_PADDING		4
+#define GST_PADDING_INIT	{0}
+
+/***** disabling of subsystems *****/
+
+/* wether or not the debugging subsystem is enabled */
+/* #undef GST_DISABLE_GST_DEBUG */
+
+/* DOES NOT WORK */
+/* #undef GST_DISABLE_LOADSAVE */
+
+/* DOES NOT WORK */
+/* #undef GST_DISABLE_PARSE */
+
+/* DOES NOT WORK */
+/* #undef GST_DISABLE_TRACE */
+
+/* DOES NOT WORK */
+/* #undef GST_DISABLE_ALLOC_TRACE */
+
+/* DOES NOT WORK */
+/* #undef GST_DISABLE_REGISTRY */
+
+/* DOES NOT WORK */
+/* #undef GST_DISABLE_ENUMTYPES */
+
+/* DOES NOT WORK */
+/* #undef GST_DISABLE_INDEX */
+
+/* DOES NOT WORK */
+/* #undef GST_DISABLE_PLUGIN */
+
+/* DOES NOT WORK */
+/* #undef GST_DISABLE_URI */
+
+/* printf extension format */
+/**
+ * GST_PTR_FORMAT:
+ *
+ * printf format type used to debug GStreamer types.
+ * This can only be used on types whose size is >= sizeof(gpointer).
+ */
+#define GST_PTR_FORMAT "p"
+
+/* whether or not the CPU supports unaligned access */
+#define GST_HAVE_UNALIGNED_ACCESS 1
+
+/* whether or not we are using glib 2.8 api, e.g. atomic gobject
+   refcounting */
+#define GST_HAVE_GLIB_2_8 1
+
+/***** Deal with XML stuff, we have to handle both loadsave and registry *****/
+
+#if (! (defined(GST_DISABLE_LOADSAVE) && defined(GST_DISABLE_REGISTRY)) )
+# include <libxml/parser.h>
+#else
+# define GST_DISABLE_LOADSAVE_REGISTRY
+#endif
+
+/**
+ * GST_EXPORT:
+ *
+ * Export the given variable from the built shared object.
+ *
+ * On Windows, this exports the variable from the DLL.
+ * On other platforms, this gets defined to "extern".
+ */
+/**
+ * GST_PLUGIN_EXPORT:
+ *
+ * Export the plugin's definition.
+ *
+ * On Windows, this exports the plugin definition from the DLL.
+ * On other platforms, this gets defined as a no-op.
+ */
+#if defined(WIN32) && (!defined(__MINGW32__))
+#define GST_PLUGIN_EXPORT __declspec(dllexport) extern
+#ifdef GST_EXPORTS
+#define GST_EXPORT __declspec(dllexport) extern
+#else
+#define GST_EXPORT __declspec(dllimport) extern
+#endif
+#else /* not WIN32 */
+#define GST_PLUGIN_EXPORT
+#define GST_EXPORT extern
+#endif
+
+#endif /* __GST_CONFIG_H__ */
---
+++ win32/common/gstversion.h
@@ -0,0 +1,66 @@
+/* GStreamer
+ * Copyright (C) 1999,2000 Erik Walthinsen <omega@cse.ogi.edu>
+ *                    2000 Wim Taymans <wtay@chello.be>
+ *
+ * gstversion.h: Version information for GStreamer
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+/**
+ * SECTION:gstversion
+ * @short_description: GStreamer version macros.
+ *
+ * Use the GST_VERSION_* macros e.g. when defining own plugins. The GStreamer
+ * runtime checks if these plugin and core version match and refuses to use a
+ * plugin compiled against a different version of GStreamer.
+ * You can also use the macros to keep the GStreamer version information in your
+ * application.
+ *
+ * Use the gst_version() function if you want to know which version of 
+ * GStreamer you are currently linked against.
+ *
+ * The version macros get defined by including "gst/gst.h".
+ */
+
+#ifndef __GST_VERSION_H__
+#define __GST_VERSION_H__
+
+G_BEGIN_DECLS
+
+/**
+ * GST_VERSION_MAJOR:
+ *
+ * The major version of GStreamer at compile time:
+ */
+#define GST_VERSION_MAJOR (0)
+/**
+ * GST_VERSION_MINOR:
+ *
+ * The minor version of GStreamer at compile time:
+ */
+#define GST_VERSION_MINOR (9)
+/**
+ * GST_VERSION_MICRO:
+ *
+ * The micro version of GStreamer at compile time:
+ */
+#define GST_VERSION_MICRO (3)
+
+void    gst_version     (guint *major, guint *minor, guint *micro);
+
+G_END_DECLS
+
+#endif /* __GST_VERSION_H__ */
