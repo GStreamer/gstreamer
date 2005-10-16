c0b08f1bfbf59f51c105df047a643e33e1515847
Thomas Vander Stichele <thomas@apestaart.org>
2005-10-16 12:37:14 +0000

    Dear Andy: here's a script to update stuff from CVS
    Original commit message from CVS:
    Dear Andy:
    here's a script to update stuff from CVS
    
    Love,
    Thomas

---
+++ scripts/cvs-update.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# update all known gstreamer modules
+# build them one by one
+# report failures at the end
+# run this from a directory that contains the checkouts for each of the
+# modules
+
+FAILURE=
+
+for m in \
+  gstreamer gst-plugins-base \
+  gst-plugins-good gst-plugins-ugly gst-plugins-bad \
+  gst-ffmpeg \
+  gst-python \
+  ; do
+  if test -d $m; then
+    cd $m
+    cvs update
+    if test $? -ne 0
+    then
+      FAILURE="$FAILURE$m: update\n"
+      cd ..
+      continue
+    fi
+    make
+    if test $? -ne 0
+    then
+      FAILURE="$FAILURE$m: make\n"
+      cd ..
+      continue
+    fi
+    make check
+    if test $? -ne 0
+    then
+      FAILURE="$FAILURE$m: check\n"
+      cd ..
+      continue
+    fi
+    cd ..
+  fi
+done
+
+if test "x$FAILURE" != "x";  then
+  echo "Failures:"
+  echo
+  echo -e $FAILURE
+fi
