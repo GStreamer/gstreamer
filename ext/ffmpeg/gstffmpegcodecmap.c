eb2e4a055a12ada228d155322a3c01147baf01ff
Julien Moutte <julien@moutte.net>
2005-10-23 16:07:46 +0000

    ext/ffmpeg/Makefile.am: Fix build.
    Original commit message from CVS:
    2005-10-23  Julien MOUTTE  <julien@moutte.net>
    
    * ext/ffmpeg/Makefile.am: Fix build.
    * ext/ffmpeg/gstffmpegcodecmap.c: (gst_ffmpeg_codecid_to_caps),
    (gst_ffmpeg_caps_to_codecid), (gst_ffmpeg_get_codecid_longname):
    * ext/ffmpeg/gstffmpegdec.c: (gst_ffmpegdec_register):
    * ext/ffmpeg/gstffmpegenc.c: (gst_ffmpegenc_register): Add QDM2
    and TrueMotion 2.

--- ext/ffmpeg/gstffmpegcodecmap.c
+++ ext/ffmpeg/gstffmpegcodecmap.c
@@ -483,6 +483,9 @@ gst_ffmpeg_codecid_to_caps (enum CodecID codec_id,
       caps = GST_FF_VID_CAPS_NEW ("video/x-wmv",
           "wmvversion", G_TYPE_INT, 3, NULL);
       break;
+    case CODEC_ID_QDM2:
+      caps = GST_FF_AUD_CAPS_NEW ("audio/x-qdm2", NULL);
+      break;  
 
     case CODEC_ID_WS_VQA:
     case CODEC_ID_IDCIN:
@@ -490,6 +493,7 @@ gst_ffmpeg_codecid_to_caps (enum CodecID codec_id,
     case CODEC_ID_SMC:
     case CODEC_ID_FLIC:
     case CODEC_ID_TRUEMOTION1:
+    case CODEC_ID_TRUEMOTION2:
     case CODEC_ID_VMDVIDEO:
     case CODEC_ID_VMDAUDIO:
     case CODEC_ID_MSZH:
@@ -1582,6 +1586,9 @@ gst_ffmpeg_caps_to_codecid (const GstCaps * caps, AVCodecContext * context)
   } else if (!strcmp (mimetype, "audio/x-vorbis")) {
     id = CODEC_ID_VORBIS;
     audio = TRUE;
+  } else if (!strcmp (mimetype, "audio/x-qdm2")) {
+    id = CODEC_ID_QDM2;
+    audio = TRUE;
   } else if (!strcmp (mimetype, "audio/mpeg")) {
     gint layer = 0;
     gint mpegversion = 0;
@@ -1943,6 +1950,9 @@ gst_ffmpeg_get_codecid_longname (enum CodecID codec_id)
     case CODEC_ID_VORBIS:
       name = "Vorbis audio";
       break;
+    case CODEC_ID_QDM2:
+      name = "QDesign Music 2";
+      break;
     case CODEC_ID_AC3:
       name = "AC-3 audio";
       break;
@@ -2102,6 +2112,9 @@ gst_ffmpeg_get_codecid_longname (enum CodecID codec_id)
       break;
     case CODEC_ID_TRUEMOTION1:
       name = "Duck Truemotion video";
+      break;
+    case CODEC_ID_TRUEMOTION2:
+      name = "Duck Truemotion 2 video";
       break;
     case CODEC_ID_VMDVIDEO:
       name = "Sierra VMD video";
