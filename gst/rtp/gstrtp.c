4f40e2e01e913e7005d8f85d3c4694e8366c59d6
Christian Schaller <uraeus@gnome.org>
2005-10-25 13:03:04 +0000

    fix mistaken claim on GPL, its LGPL
    Original commit message from CVS:
    fix mistaken claim on GPL, its LGPL

--- gst/rtp/gstrtp.c
+++ gst/rtp/gstrtp.c
@@ -84,4 +84,4 @@ GST_PLUGIN_DEFINE (GST_VERSION_MAJOR,
     GST_VERSION_MINOR,
     "rtp",
     "Real-time protocol plugins",
-    plugin_init, VERSION, "GPL", GST_PACKAGE, GST_ORIGIN)
+    plugin_init, VERSION, "LGPL", GST_PACKAGE, GST_ORIGIN)
