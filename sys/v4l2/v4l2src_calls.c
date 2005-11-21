09eabf5cf92c5a8e5d52e90c710c6b5fabbbff40
Tim-Philipp Müller <tim@centricular.net>
2005-11-21 14:39:04 +0000

    Update for GST_FOURCC_FORMAT API change.
    Original commit message from CVS:
    * ext/directfb/dfbvideosink.c:
    (gst_dfbvideosink_get_format_from_caps):
    * ext/sdl/sdlvideosink.c: (gst_sdlvideosink_create):
    * gst/qtdemux/qtdemux.c: (gst_qtdemux_loop_header),
    (qtdemux_parse), (qtdemux_type_get), (qtdemux_node_dump_foreach),
    (qtdemux_dump_hdlr), (qtdemux_dump_dref), (qtdemux_dump_stsd),
    (qtdemux_dump_dcom), (qtdemux_parse_trak), (qtdemux_video_caps),
    (qtdemux_audio_caps):
    * sys/v4l2/gstv4l2src.c: (gst_v4l2src_v4l2fourcc_to_caps):
    * sys/v4l2/v4l2src_calls.c: (gst_v4l2src_fill_format_list),
    (gst_v4l2src_capture_init), (gst_v4l2src_get_size_limits):
    Update for GST_FOURCC_FORMAT API change.

--- sys/v4l2/v4l2src_calls.c
+++ sys/v4l2/v4l2src_calls.c
@@ -81,7 +81,7 @@ gst_v4l2src_fill_format_list (GstV4l2Src * v4l2src)
         return FALSE;
       }
     }
-    GST_LOG_OBJECT (v4l2src, "got format" GST_FOURCC_FORMAT,
+    GST_LOG_OBJECT (v4l2src, "got format %" GST_FOURCC_FORMAT,
         GST_FOURCC_ARGS (format->pixelformat));
     v4l2src->formats = g_slist_prepend (v4l2src->formats, format);
   }
@@ -273,7 +273,7 @@ gst_v4l2src_capture_init (GstV4l2Src * v4l2src)
     g_object_notify (G_OBJECT (v4l2src), "num_buffers");
 
   GST_INFO_OBJECT (v4l2src,
-      "Got %d buffers (" GST_FOURCC_FORMAT ") of size %d KB\n",
+      "Got %d buffers (%" GST_FOURCC_FORMAT ") of size %d KB\n",
       v4l2src->breq.count,
       GST_FOURCC_ARGS (v4l2src->format.fmt.pix.pixelformat),
       v4l2src->format.fmt.pix.sizeimage / 1024);
@@ -474,7 +474,8 @@ gst_v4l2src_get_size_limits (GstV4l2Src * v4l2src,
 {
   struct v4l2_format fmt;
 
-  GST_LOG_OBJECT (v4l2src, "getting size limits with format " GST_FOURCC_FORMAT,
+  GST_LOG_OBJECT (v4l2src,
+      "getting size limits with format %" GST_FOURCC_FORMAT,
       GST_FOURCC_ARGS (format->pixelformat));
 
   /* get size delimiters */
