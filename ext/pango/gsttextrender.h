e54c5a2adbaa4cd0a1f5534607f1e5163591355c
Tim-Philipp Müller <tim@centricular.net>
2005-11-30 14:53:29 +0000

    ext/pango/gsttextrender.*: Add missing files.
    Original commit message from CVS:
    * ext/pango/gsttextrender.c: (gst_text_render_base_init),
    (gst_text_render_class_init), (resize_bitmap),
    (gst_text_render_render_text), (gst_text_render_setcaps),
    (gst_text_render_fixate_caps), (gst_text_renderer_bitmap_to_ayuv),
    (gst_text_render_chain), (gst_text_render_finalize),
    (gst_text_render_init), (gst_text_render_set_property):
    * ext/pango/gsttextrender.h:
    Add missing files.

---
+++ ext/pango/gsttextrender.h
@@ -0,0 +1,46 @@
+#ifndef __GST_TEXT_RENDER_H__
+#define __GST_TEXT_RENDER_H__
+
+#include <gst/gst.h>
+#include <pango/pangoft2.h>
+
+G_BEGIN_DECLS
+
+#define GST_TYPE_TEXT_RENDER           (gst_text_render_get_type())
+#define GST_TEXT_RENDER(obj)           (G_TYPE_CHECK_INSTANCE_CAST((obj),\
+                                        GST_TYPE_TEXT_RENDER, GstTextRender))
+#define GST_TEXT_RENDER_CLASS(klass)   (G_TYPE_CHECK_CLASS_CAST((klass),\
+                                        GST_TYPE_ULAW, GstTextRender))
+#define GST_TEXT_RENDER_GET_CLASS(obj) (G_TYPE_INSTANCE_GET_CLASS ((obj),\
+                                        GST_TYPE_TEXT_RENDER, GstTextRenderClass))
+#define GST_IS_TEXT_RENDER(obj)        (G_TYPE_CHECK_INSTANCE_TYPE((obj),\
+                                        GST_TYPE_TEXT_RENDER))
+#define GST_IS_TEXT_RENDER_CLASS(obj)  (G_TYPE_CHECK_CLASS_TYPE((klass),\
+                                        GST_TYPE_TEXT_RENDER))
+
+typedef struct _GstTextRender      GstTextRender;
+typedef struct _GstTextRenderClass GstTextRenderClass;
+
+struct _GstTextRender {
+    GstElement            element;
+
+    GstPad               *sinkpad, *srcpad;
+    gint                  width;
+    gint                  height;
+    PangoLayout          *layout;
+    FT_Bitmap             bitmap;
+    gint                  bitmap_buffer_size;
+    gint                  baseline_y;
+};
+
+struct _GstTextRenderClass {
+    GstElementClass parent_class;
+
+    PangoContext *pango_context;
+};
+
+GType gst_text_render_get_type(void) G_GNUC_CONST;
+
+G_END_DECLS
+
+#endif /* __GST_TEXT_RENDER_H */
