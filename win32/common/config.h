715efa9247f7acf8ad368ce0320fba3fef4e0a64
Thomas Vander Stichele <thomas@apestaart.org>
2005-12-05 18:05:01 +0000

    releasing 0.10.0
    Original commit message from CVS:
    releasing 0.10.0

--- win32/common/config.h
+++ win32/common/config.h
@@ -21,7 +21,7 @@
 #undef GST_GCOV_ENABLED
 
 /* Default errorlevel to use */
-#define GST_LEVEL_DEFAULT GST_LEVEL_ERROR
+#define GST_LEVEL_DEFAULT GST_LEVEL_NONE
 
 /* GStreamer license */
 #define GST_LICENSE "LGPL"
@@ -30,10 +30,10 @@
 #define GST_PACKAGE_ORIGIN "Unknown package origin"
 
 /* package name in plugins */
-#define GST_PACKAGE_NAME "GStreamer CVS/prerelease"
+#define GST_PACKAGE_NAME "GStreamer source release"
 
 /* Define the version */
-#define GST_VERSION "0.9.7.1"
+#define GST_VERSION "0.10.0"
 
 /* Define if the host CPU is an Alpha */
 #undef HAVE_CPU_ALPHA
@@ -213,7 +213,7 @@
 #undef USE_POISONING
 
 /* Version number of package */
-#define VERSION "0.9.7.1"
+#define VERSION "0.10.0"
 
 /* Define to 1 if your processor stores words with the most significant byte
    first (like Motorola and SPARC, unlike Intel and VAX). */
