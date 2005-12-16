84f4cacb6f9cef32811f764cbc5b804271afe3c0
Benjamin Pineau <ben.pineau@gmail.com>
2005-12-16 10:12:49 +0000

    gst/rtsp/rtspconnection.c: Add <netinet/in.h> include and move <arpa/inet.h> include to make things work on OpenBSD a...
    Original commit message from CVS:
    * gst/rtsp/rtspconnection.c:
    Add <netinet/in.h> include and move <arpa/inet.h> include
    to make things work on OpenBSD as well (fixes #323717;
    patch by: Benjamin Pineau)

--- gst/rtsp/rtspconnection.c
+++ gst/rtsp/rtspconnection.c
@@ -23,8 +23,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <netdb.h>
-#include <arpa/inet.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 #include "rtspconnection.h"
 
