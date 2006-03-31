f84de0efb7b6073c96d38109e9936a0978a93bba
Zaheer Abbas Merali <zaheerabbas@merali.org>
2006-03-31 17:52:36 +0000

    Add tests and fix PAR caps issue to ximagesrc
    Original commit message from CVS:
    2006-03-31  Zaheer Abbas Merali  <zaheerabbas at merali dot org>
    
    * Makefile.am:
    * configure.ac:
    * sys/ximagesrc/ximagesrc.c:
    (gst_ximagesrc_ximage_get),
    (gst_ximagesrc_get_caps), (gst_ximagesrc_class_init):
    * sys/ximagesrc/ximageutil.c:
    * tests/Makefile.am:
    * tests/icles/Makefile.am:
    * tests/icles/ximagesrc-test.c: (terminate_playback), (main):
    Add tests and fix PAR caps issue to ximagesrc

---
+++ tests/icles/ximagesrc-test.c
@@ -0,0 +1,65 @@
+/* GStreamer
+ * Copyright (C) <2006> Zaheer Abbas Merali <zaheerabbas at merali dot org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <gst/gst.h>
+
+static GMainLoop *loop;
+
+static gboolean
+terminate_playback (GstElement * pipeline)
+{
+  g_print ("Terminating playback\n");
+  g_main_loop_quit (loop);
+  return FALSE;
+}
+
+int
+main (int argc, char **argv)
+{
+  GstElement *pipeline;
+  GstBus *bus;
+  GError *error = NULL;
+
+  gst_init (&argc, &argv);
+
+  pipeline = gst_parse_launch ("ximagesrc ! fakesink", &error);
+  if (error) {
+    g_print ("Error while parsing pipeline description: %s\n", error->message);
+    return -1;
+  }
+
+  loop = g_main_loop_new (NULL, FALSE);
+
+  bus = gst_pipeline_get_bus (GST_PIPELINE (pipeline));
+
+  gst_element_set_state (pipeline, GST_STATE_PLAYING);
+
+  /* We want to get out after */
+  g_timeout_add (500000, (GSourceFunc) terminate_playback, pipeline);
+
+  g_main_loop_run (loop);
+
+  g_main_loop_unref (loop);
+
+  return 0;
+}
