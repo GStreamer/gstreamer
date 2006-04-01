e291df2299862f81495def5c187ba8885396b0ff
Thomas Vander Stichele <thomas@apestaart.org>
2006-04-01 10:09:11 +0000

    [MOVED FROM GOOD] rework build; add translations for v4l2
    Original commit message from CVS:
    rework build; add translations for v4l2

--- sys/directdraw/gstdirectdrawplugin.c
+++ sys/directdraw/gstdirectdrawplugin.c
@@ -39,4 +39,4 @@ GST_PLUGIN_DEFINE (GST_VERSION_MAJOR,
     GST_VERSION_MINOR,
     "directdraw",
     "DIRECTDRAW plugin library",
-    plugin_init, VERSION, "LGPL", GST_PACKAGE, GST_ORIGIN)
+    plugin_init, VERSION, "LGPL", GST_PACKAGE_NAME, GST_PACKAGE_ORIGIN)
