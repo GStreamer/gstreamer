2149d90020eb0d5ce97650c1e27fe8cbb88eab9c
Thomas Vander Stichele <thomas@apestaart.org>
2006-04-01 10:09:10 +0000

    rework build; add translations for v4l2
    Original commit message from CVS:
    rework build; add translations for v4l2

--- gst/mpegaudioparse/gstxingmux.c
+++ gst/mpegaudioparse/gstxingmux.c
@@ -377,4 +377,4 @@ GST_PLUGIN_DEFINE (GST_VERSION_MAJOR,
     GST_VERSION_MINOR,
     "xingheader",
     "Add a xing header to mp3 encoded data",
-    plugin_init, VERSION, "LGPL", GST_PACKAGE, GST_ORIGIN)
+    plugin_init, VERSION, "LGPL", GST_PACKAGE_NAME, GST_PACKAGE_ORIGIN)
