c3141ee26c975e3303f43252a4b6e5f36281a230
Thomas Vander Stichele <thomas@apestaart.org>
2006-04-01 09:56:45 +0000

    configure.ac: clean up, use AS_VERSION and AS_NANO
    Original commit message from CVS:
    
    * configure.ac:
    clean up, use AS_VERSION and AS_NANO
    * gst/matroska/matroska-mux.c: (gst_matroska_mux_start):
    use PACKAGE_VERSION define
    * po/af.po:
    * po/az.po:
    * po/cs.po:
    * po/en_GB.po:
    * po/hu.po:
    * po/it.po:
    * po/nb.po:
    * po/nl.po:
    * po/or.po:
    * po/sq.po:
    * po/sr.po:
    * po/sv.po:
    * po/uk.po:
    * po/vi.po:
    updated

--- gst/matroska/matroska-mux.c
+++ gst/matroska/matroska-mux.c
@@ -1068,7 +1068,7 @@ gst_matroska_mux_start (GstMatroskaMux * mux)
   gst_ebml_write_float (ebml, GST_MATROSKA_ID_DURATION,
       duration / gst_guint64_to_gdouble (mux->time_scale));
   gst_ebml_write_utf8 (ebml, GST_MATROSKA_ID_MUXINGAPP,
-      "GStreamer plugin version " GST_PLUGINS_GOOD_VERSION);
+      "GStreamer plugin version " PACKAGE_VERSION);
   if (mux->writing_app && mux->writing_app[0]) {
     gst_ebml_write_utf8 (ebml, GST_MATROSKA_ID_WRITINGAPP, mux->writing_app);
   }
