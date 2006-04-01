1844742e145e12aaf0854ae003b8394dea0b09ef
Thomas Vander Stichele <thomas@apestaart.org>
2006-04-01 10:09:11 +0000

    [MOVED FROM BAD] rework build; add translations for v4l2
    Original commit message from CVS:
    rework build; add translations for v4l2

--- gst/y4m/gsty4mencode.c
+++ gst/y4m/gsty4mencode.c
@@ -288,4 +288,4 @@ GST_PLUGIN_DEFINE (GST_VERSION_MAJOR,
     GST_VERSION_MINOR,
     "y4menc",
     "Encodes a YUV frame into the yuv4mpeg format (mjpegtools)",
-    plugin_init, VERSION, GST_LICENSE, GST_PACKAGE, GST_ORIGIN)
+    plugin_init, VERSION, GST_LICENSE, GST_PACKAGE_NAME, GST_PACKAGE_ORIGIN)
