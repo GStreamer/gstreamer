f902029f7e772ce86851ad87737903e2d73e73e0
Tim-Philipp Müller <tim@centricular.net>
2006-04-03 10:34:54 +0000

    GST_ORIGIN => GST_PACKAGE_ORIGIN
    Original commit message from CVS:
    * ext/soundtouch/gstpitch.cc:
    * ext/theora/theoradec.c:
    * gst/modplug/gstmodplug.cc:
    GST_ORIGIN => GST_PACKAGE_ORIGIN
    GST_PACKAGE => GST_PACKAGE_NAME

--- ext/soundtouch/gstpitch.cc
+++ ext/soundtouch/gstpitch.cc
@@ -706,4 +706,4 @@ GST_PLUGIN_DEFINE (GST_VERSION_MAJOR,
     GST_VERSION_MINOR,
     "soundtouch",
     "Audio Pitch Controller",
-    plugin_init, VERSION, "LGPL", GST_PACKAGE, GST_ORIGIN)
+    plugin_init, VERSION, "LGPL", GST_PACKAGE_NAME, GST_PACKAGE_ORIGIN)
--- gst/modplug/gstmodplug.cc
+++ gst/modplug/gstmodplug.cc
@@ -839,4 +839,4 @@ GST_PLUGIN_DEFINE (GST_VERSION_MAJOR,
     GST_VERSION_MINOR,
     "modplug",
     ".MOD audio decoding",
-    plugin_init, VERSION, "LGPL", GST_PACKAGE, GST_ORIGIN)
+    plugin_init, VERSION, "LGPL", GST_PACKAGE_NAME, GST_PACKAGE_ORIGIN)
