3d27036e7affbca3d690f772cf1943b3ab3cea21
Stefan Kost <ensonic@users.sourceforge.net>
2006-04-08 18:46:44 +0000

    testsuite/test-object.h: Fix broken GObject macros
    Original commit message from CVS:
    * testsuite/test-object.h:
    Fix broken GObject macros

--- testsuite/test-object.h
+++ testsuite/test-object.h
@@ -17,7 +17,7 @@ typedef struct {
 #define TEST_OBJECT(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), TEST_TYPE_OBJECT, TestObject))
 #define TEST_OBJECT_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass), TEST_TYPE_OBJECT, TestObjectClass))
 #define TEST_IS_OBJECT(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), TEST_TYPE_OBJECT))
-#define TEST_IS_OBJECT_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((obj), TEST_TYPE_OBJECT))
+#define TEST_IS_OBJECT_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), TEST_TYPE_OBJECT))
 #define TEST_OBJECT_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS((obj), TEST_TYPE_OBJECT, TestObjectClass))
 
 GType test_object_get_type (void);
