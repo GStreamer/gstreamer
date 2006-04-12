0a75b8a4150d69c2cb1adb977f5174ca06401f30
Thomas Vander Stichele <thomas@apestaart.org>
2006-04-12 10:57:26 +0000

    prerelease
    Original commit message from CVS:
    prerelease

--- win32/common/config.h
+++ win32/common/config.h
@@ -169,7 +169,7 @@
 #define HAVE_WIN32 1
 
 /* Define host CPU */
-#define HOST_CPU "x86_64"
+#define HOST_CPU "i686"
 
 /* library dir */
 #ifdef _DEBUG
@@ -191,13 +191,13 @@
 #define PACKAGE_NAME "GStreamer"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "GStreamer 0.10.4.1"
+#define PACKAGE_STRING "GStreamer 0.10.4.2"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "gstreamer"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.10.4.1"
+#define PACKAGE_VERSION "0.10.4.2"
 
 /* Define the plugin directory */
 #ifdef _DEBUG
@@ -213,7 +213,7 @@
 #undef USE_POISONING
 
 /* Version number of package */
-#define VERSION "0.10.4.1"
+#define VERSION "0.10.4.2"
 
 /* Define to 1 if your processor stores words with the most significant byte
    first (like Motorola and SPARC, unlike Intel and VAX). */
