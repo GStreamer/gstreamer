7f9bc84725c56fc9783073f630a13c3d75ed08c6
Stefan Kost <ensonic@users.sourceforge.net>
2006-06-16 09:49:07 +0000

    gst/spectrum/: port to use message to get results, cleanly exit when closing the window
    Original commit message from CVS:
    * gst/spectrum/demo-audiotest.c: (on_window_destroy),
    (draw_spectrum), (message_handler), (main):
    * gst/spectrum/demo-osssrc.c: (on_window_destroy), (draw_spectrum),
    (message_handler), (main):
    port to use message to get results, cleanly exit when closing the window
    * gst/spectrum/gstspectrum.c: (gst_spectrum_class_init),
    (gst_spectrum_init), (gst_spectrum_dispose),
    (gst_spectrum_set_property), (gst_spectrum_get_property),
    (gst_spectrum_set_caps), (gst_spectrum_start),
    (gst_spectrum_message_new), (gst_spectrum_transform_ip):
    * gst/spectrum/gstspectrum.h:
    port to derive from basetransform and send results via messages
    (like level element)

--- gst/spectrum/demo-osssrc.c
+++ gst/spectrum/demo-osssrc.c
@@ -1,6 +1,27 @@
+/* GStreamer
+ * Copyright (C) 2006 Stefan Kost <ensonic@users.sf.net>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
+#include <string.h>
+#include <math.h>
 #include <gst/gst.h>
 #include <gtk/gtk.h>
 
@@ -10,22 +31,57 @@
 static GtkWidget *drawingarea = NULL;
 
 static void
-spectrum_chain (GstElement * sink, GstBuffer * buf, GstPad * pad,
-    gpointer unused)
+on_window_destroy (GtkObject * object, gpointer user_data)
+{
+  drawingarea = NULL;
+  gtk_main_quit ();
+}
+
+/* draw frequency spectrum as a bunch of bars */
+static void
+draw_spectrum (guchar * data)
 {
   gint i;
-  guchar *data = buf->data;
-  gint width = GST_BUFFER_SIZE (buf);
-  GdkRectangle rect = { 0, 0, width, 50 };
+  GdkRectangle rect = { 0, 0, SPECT_BANDS, 50 };
+
+  if (!drawingarea)
+    return;
 
   gdk_window_begin_paint_rect (drawingarea->window, &rect);
   gdk_draw_rectangle (drawingarea->window, drawingarea->style->black_gc,
-      TRUE, 0, 0, width, 50);
-  for (i = 0; i < width; i++) {
+      TRUE, 0, 0, SPECT_BANDS, 50);
+  for (i = 0; i < SPECT_BANDS; i++) {
     gdk_draw_rectangle (drawingarea->window, drawingarea->style->white_gc,
         TRUE, i, 64 - data[i], 1, data[i]);
   }
   gdk_window_end_paint (drawingarea->window);
+}
+
+/* receive spectral data from element message */
+gboolean
+message_handler (GstBus * bus, GstMessage * message, gpointer data)
+{
+  if (message->type == GST_MESSAGE_ELEMENT) {
+    const GstStructure *s = gst_message_get_structure (message);
+    const gchar *name = gst_structure_get_name (s);
+
+    if (strcmp (name, "spectrum") == 0) {
+      guchar spect[SPECT_BANDS];
+      const GValue *list;
+      const GValue *value;
+      guint i;
+
+      list = gst_structure_get_value (s, "spectrum");
+      for (i = 0; i < SPECT_BANDS; ++i) {
+        value = gst_value_list_get_value (list, i);
+        spect[i] = g_value_get_uchar (value);
+      }
+      draw_spectrum (spect);
+    }
+  }
+  /* we handled the message we want, and ignored the ones we didn't want.
+   * so the core can unref the message for us */
+  return TRUE;
 }
 
 int
@@ -33,7 +89,7 @@ main (int argc, char *argv[])
 {
   GstElement *bin;
   GstElement *src, *spectrum, *sink;
-
+  GstBus *bus;
   GtkWidget *appwindow;
 
   gst_init (&argc, &argv);
@@ -42,16 +98,12 @@ main (int argc, char *argv[])
   bin = gst_pipeline_new ("bin");
 
   src = gst_element_factory_make (DEFAULT_AUDIOSRC, "src");
-  g_object_set (G_OBJECT (src), "blocksize", (gulong) 1024 * 2, NULL);
 
   spectrum = gst_element_factory_make ("spectrum", "spectrum");
-  g_object_set (G_OBJECT (spectrum), "width", SPECT_BANDS, "threshold", -80,
-      NULL);
+  g_object_set (G_OBJECT (spectrum), "bands", SPECT_BANDS, "threshold", -80,
+      "message", TRUE, NULL);
 
   sink = gst_element_factory_make ("fakesink", "sink");
-  g_object_set (G_OBJECT (sink), "signal-handoffs", TRUE, NULL);
-
-  g_signal_connect (sink, "handoff", G_CALLBACK (spectrum_chain), NULL);
 
   gst_bin_add_many (GST_BIN (bin), src, spectrum, sink, NULL);
   if (!gst_element_link_many (src, spectrum, sink, NULL)) {
@@ -59,7 +111,14 @@ main (int argc, char *argv[])
     exit (1);
   }
 
+  bus = gst_element_get_bus (bin);
+  gst_bus_add_watch (bus, message_handler, NULL);
+  gst_object_unref (bus);
+
   appwindow = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+  g_signal_connect (G_OBJECT (appwindow), "destroy",
+      G_CALLBACK (on_window_destroy), NULL);
+
   drawingarea = gtk_drawing_area_new ();
   gtk_drawing_area_size (GTK_DRAWING_AREA (drawingarea), SPECT_BANDS, 64);
   gtk_container_add (GTK_CONTAINER (appwindow), drawingarea);
