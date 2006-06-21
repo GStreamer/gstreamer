d09ded2065fb210ca4df11fb70cd63540bf4f7de
Khaled Mohammed <khaled.mohammed@gmail.com>
2006-06-21 07:25:57 +0000

    Custom Signal Handler for Element.cs
    git-svn-id: svn://anonsvn.mono-project.com/source/branches/abock/gstreamer-sharp@61891 e3ebcda4-bce8-0310-ba0a-eca2169e7518

---
+++ gstreamer-sharp/DynamicSignalMarshalHandler.cs
@@ -0,0 +1,14 @@
+//
+// Authors
+//   Khaled Mohammed < Khaled.Mohammed@gmail.com >
+//
+// (C) 2006 Novell
+//
+
+using System;
+
+namespace Gst {
+	public abstract class DynamicSignalMarshalHandler {
+		public abstract Delegate Func();
+	}
+}
