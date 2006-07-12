448405d86af65ecf1b3c988f1b0e73ea13ee8e28
Aaron Bockover <aaron@abock.org>
2006-07-12 21:49:32 +0000

    2006-07-12  Aaron Bockover  <aaron@abock.org>
        * gstreamer-sharp.mds:
        * gstreamer-sharp.mdp: Updated MonoDevelo project/solution
    
        * source/gstreamer-sharp-source.xml: Cleaned up a bit
    
        * sample/DecodeBinTranscoder.cs: Updated to reflect renaming of
        Clock.GstSecond to Clock.Second, etc.
    
        * gstreamer-sharp/glue/clock.c: Added gstsharp_gst_clock_get_time_none
        and gstsharp_gst_clock_time_is_valid
    
        * gstreamer-sharp/Clock.custom: Added bindings for new clock glue
        and renamed GstNSecond to NSecond (and friends)
    
    
    
    git-svn-id: svn://anonsvn.mono-project.com/source/branches/abock/gstreamer-sharp@62535 e3ebcda4-bce8-0310-ba0a-eca2169e7518

--- gstreamer-sharp/glue/clock.c
+++ gstreamer-sharp/glue/clock.c
@@ -20,3 +20,14 @@ guint64 gstsharp_gst_clock_get_gst_nsecond()
 {
     return GST_NSECOND;
 }
+
+guint64 gstsharp_gst_clock_get_time_none()
+{
+	return GST_CLOCK_TIME_NONE;
+}
+
+gboolean gstsharp_gst_clock_time_is_valid(GstClockTime time)
+{
+	return GST_CLOCK_TIME_IS_VALID(time);
+}
+
