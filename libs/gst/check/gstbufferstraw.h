1cd7bc447b209464b4b97ffff94a21ffa6d69ea3
Andy Wingo <wingo@pobox.com>
2006-07-21 15:48:04 +0000

    libs/gst/check/Makefile.am (libgstcheck_@GST_MAJORMINOR@include_HEADERS)
    Original commit message from CVS:
    2006-07-21  Andy Wingo  <wingo@pobox.com>
    
    * libs/gst/check/Makefile.am
    (libgstcheck_@GST_MAJORMINOR@include_HEADERS)
    (libgstcheck_@GST_MAJORMINOR@_la_SOURCES):
    * libs/gst/check/gstbufferstraw.h:
    * libs/gst/check/gstbufferstraw.c: Add some new hype testing
    functions, thus proving I am still a GStreamer haxor. OK I wrote
    them a long time ago, but anyways.

---
+++ libs/gst/check/gstbufferstraw.h
@@ -0,0 +1,33 @@
+/* GStreamer
+ *
+ * Copyright (C) 2006 Andy Wingo <wingo at pobox.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#ifndef __GST_BUFFER_STRAW_H__
+#define __GST_BUFFER_STRAW_H__
+
+
+#include <gst/check/gstcheck.h>
+
+
+void gst_buffer_straw_start_pipeline (GstElement * bin, GstPad * pad);
+GstBuffer *gst_buffer_straw_get_buffer (GstElement * bin, GstPad * pad);
+void gst_buffer_straw_stop_pipeline (GstElement * bin, GstPad * pad);
+
+
+#endif /* __GST_BUFFER_STRAW_H__ */
