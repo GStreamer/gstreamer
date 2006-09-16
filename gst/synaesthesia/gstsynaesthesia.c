f890fc26beb8a0f0104da4ff2871736d72b5d971
Stefan Kost <ensonic@users.sourceforge.net>
2006-09-16 21:58:48 +0000

    More G_OBJECT macro fixing.
    Original commit message from CVS:
    * ext/mad/gstid3tag.c:
    * gst/mpegstream/gstrfc2250enc.h:
    * gst/synaesthesia/gstsynaesthesia.c:
    More G_OBJECT macro fixing.

--- gst/synaesthesia/gstsynaesthesia.c
+++ gst/synaesthesia/gstsynaesthesia.c
@@ -36,7 +36,7 @@
 #define GST_SYNAESTHESIA(obj) (G_TYPE_CHECK_INSTANCE_CAST((obj),GST_TYPE_SYNAESTHESIA,GstSynaesthesia))
 #define GST_SYNAESTHESIA_CLASS(klass) (G_TYPE_CHECK_CLASS_CAST((klass),GST_TYPE_SYNAESTHESIA,GstSynaesthesia))
 #define GST_IS_SYNAESTHESIA(obj) (G_TYPE_CHECK_INSTANCE_TYPE((obj),GST_TYPE_SYNAESTHESIA))
-#define GST_IS_SYNAESTHESIA_CLASS(obj) (G_TYPE_CHECK_CLASS_TYPE((klass),GST_TYPE_SYNAESTHESIA))
+#define GST_IS_SYNAESTHESIA_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE((klass),GST_TYPE_SYNAESTHESIA))
 
 typedef struct _GstSynaesthesia GstSynaesthesia;
 typedef struct _GstSynaesthesiaClass GstSynaesthesiaClass;
