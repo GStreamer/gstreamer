1720784dc1fedb10aabfb74ecd03818fe3d10cf9
Michael Smith <msmith@xiph.org>
2006-09-17 21:58:06 +0000

    gst/typefind/gsttypefindfunctions.c: Don't crash on truncated files - check that we got an 8 byte buffer before tryin...
    Original commit message from CVS:
    * gst/typefind/gsttypefindfunctions.c: (ogganx_type_find):
    Don't crash on truncated files - check that we got an 8 byte buffer
    before trying to memcmp it.

--- gst/typefind/gsttypefindfunctions.c
+++ gst/typefind/gsttypefindfunctions.c
@@ -1953,7 +1953,7 @@ ogganx_type_find (GstTypeFind * tf, gpointer private)
 
     /* Check for an annodex fishbone header */
     data = gst_type_find_peek (tf, 28, 8);
-    if (memcmp (data, "fishead\0", 8) == 0)
+    if (data && memcmp (data, "fishead\0", 8) == 0)
       gst_type_find_suggest (tf, GST_TYPE_FIND_MAXIMUM, OGGANX_CAPS);
 
     gst_type_find_suggest (tf, GST_TYPE_FIND_MAXIMUM,
