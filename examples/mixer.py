7a27ebbd7927004c5413b1dc80eff40e5e933203
Thomas Vander Stichele <thomas@apestaart.org>
2007-01-17 11:22:04 +0000

    wrap mixer set_volume, use tuple to match get_volume
    Original commit message from CVS:
    * examples/mixer.py:
    * gst/interfaces.override:
    wrap mixer set_volume, use tuple to match get_volume

--- examples/mixer.py
+++ examples/mixer.py
@@ -6,15 +6,26 @@ import sys
 import gst
 import gst.interfaces
 
-a = gst.element_factory_make('alsasrc')
-print a.set_state(gst.STATE_PLAYING)
+pipeline = "alsasrc"
+if sys.argv[1:]:
+    pipeline = " ".join(sys.argv[1:])
+a = gst.element_factory_make(pipeline)
+print dir(a)
+
+res = a.set_state(gst.STATE_PAUSED)
+if res != gst.STATE_CHANGE_SUCCESS:
+    print "Could not set pipeline %s to PAUSED" % pipeline
 
 print "Inputs:"
 for t in a.list_tracks():
     if t.flags & gst.interfaces.MIXER_TRACK_INPUT:
         sys.stdout.write(t.label)
     sys.stdout.write(': %d - %d' % (t.min_volume, t.max_volume))
-    sys.stdout.write(': %r' % (a.get_volume(t), ))
+    volumes = a.get_volume(t)
+    sys.stdout.write(': %r' % (volumes, ))
+    if t.props.num_channels > 0:
+        a.set_volume(t, volumes=volumes)
     if t.flags & gst.interfaces.MIXER_TRACK_RECORD:
         sys.stdout.write(' (selected)')
     sys.stdout.write('\n')
+
