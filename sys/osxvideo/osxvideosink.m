96b4ce16929b6e937cc353930a221c3459c99464
Thomas Vander Stichele <thomas@apestaart.org>
2007-04-29 13:56:18 +0000

    80 char police
    Original commit message from CVS:
    80 char police

--- sys/osxvideo/osxvideosink.m
+++ sys/osxvideo/osxvideosink.m
@@ -18,8 +18,8 @@
  * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
  * Boston, MA 02111-1307, USA.
  *
- * The development of this code was made possible due to the involvement of Pioneers 
- * of the Inevitable, the creators of the Songbird Music player
+ * The development of this code was made possible due to the involvement of
+ * Pioneers of the Inevitable, the creators of the Songbird Music player.
  * 
  */
 
