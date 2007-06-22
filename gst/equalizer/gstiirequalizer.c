c37fc2d3c23528c4ebf91298855c9156a1c61803
Jens Granseuer <jensgr@gmx.net>
2007-06-22 20:23:18 +0000

    gst/: Build fixes for gcc-2.9x (no mid-block variable declarations etc.).
    Original commit message from CVS:
    Patch by: Jens Granseuer  <jensgr at gmx net>
    * gst/equalizer/gstiirequalizer.c:
    * gst/equalizer/gstiirequalizer10bands.c:
    * gst/equalizer/gstiirequalizer3bands.c:
    * gst/equalizer/gstiirequalizernbands.c:
    * gst/rtpmanager/async_jitter_queue.c:
    (async_jitter_queue_push_sorted):
    * gst/rtpmanager/gstrtpjitterbuffer.c:
    (gst_rtp_jitter_buffer_chain):
    * gst/switch/gstswitch.c: (gst_switch_chain):
    Build fixes for gcc-2.9x (no mid-block variable declarations etc.).
    Fixes #450185.

--- gst/equalizer/gstiirequalizer.c
+++ gst/equalizer/gstiirequalizer.c
@@ -53,7 +53,7 @@ static gboolean gst_iir_equalizer_setup (GstAudioFilter * filter,
 static GstFlowReturn gst_iir_equalizer_transform_ip (GstBaseTransform * btrans,
     GstBuffer * buf);
 
-GST_DEBUG_CATEGORY_EXTERN ()(equalizer_debug);
+GST_DEBUG_CATEGORY_EXTERN (equalizer_debug);
 #define GST_CAT_DEFAULT equalizer_debug
 
 #define ALLOWED_CAPS \
