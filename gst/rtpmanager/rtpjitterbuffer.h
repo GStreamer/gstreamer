9e50d836d4173f127adc56446fa74f1e103fa673
Wim Taymans <wim.taymans@gmail.com>
2007-08-10 17:16:53 +0000

    gst/rtpmanager/: Remove complicated async queue and replace with more simple jitterbuffer code while also fixing some...
    Original commit message from CVS:
    * gst/rtpmanager/Makefile.am:
    * gst/rtpmanager/async_jitter_queue.c:
    * gst/rtpmanager/async_jitter_queue.h:
    * gst/rtpmanager/rtpjitterbuffer.c: (rtp_jitter_buffer_class_init),
    (rtp_jitter_buffer_init), (rtp_jitter_buffer_finalize),
    (rtp_jitter_buffer_new), (compare_seqnum),
    (rtp_jitter_buffer_insert), (rtp_jitter_buffer_pop),
    (rtp_jitter_buffer_flush), (rtp_jitter_buffer_num_packets),
    (rtp_jitter_buffer_get_ts_diff):
    * gst/rtpmanager/rtpjitterbuffer.h:
    Remove complicated async queue and replace with more simple jitterbuffer
    code while also fixing some bugs.
    * gst/rtpmanager/gstrtpbin-marshal.list:
    * gst/rtpmanager/gstrtpbin.c: (on_new_ssrc), (on_ssrc_collision),
    (on_ssrc_validated), (on_bye_ssrc), (on_bye_timeout), (on_timeout),
    (create_session), (gst_rtp_bin_class_init), (create_recv_rtp),
    (create_send_rtp):
    * gst/rtpmanager/gstrtpbin.h:
    * gst/rtpmanager/gstrtpjitterbuffer.c:
    (gst_rtp_jitter_buffer_init), (gst_rtp_jitter_buffer_dispose),
    (gst_jitter_buffer_sink_parse_caps),
    (gst_rtp_jitter_buffer_flush_start),
    (gst_rtp_jitter_buffer_flush_stop),
    (gst_rtp_jitter_buffer_change_state),
    (gst_rtp_jitter_buffer_sink_event), (gst_rtp_jitter_buffer_chain),
    (gst_rtp_jitter_buffer_loop), (gst_rtp_jitter_buffer_set_property):
    * gst/rtpmanager/gstrtpsession.c: (on_new_ssrc),
    (on_ssrc_collision), (on_ssrc_validated), (on_bye_ssrc),
    (on_bye_timeout), (on_timeout), (gst_rtp_session_class_init),
    (gst_rtp_session_init):
    * gst/rtpmanager/gstrtpsession.h:
    * gst/rtpmanager/rtpsession.c: (on_bye_ssrc), (session_cleanup):
    Use new jitterbuffer code.
    Expose some new signals in preparation for handling EOS.

---
+++ gst/rtpmanager/rtpjitterbuffer.h
@@ -0,0 +1,67 @@
+/* GStreamer
+ * Copyright (C) <2007> Wim Taymans <wim@fluendo.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#ifndef __RTP_JITTER_BUFFER_H__
+#define __RTP_JITTER_BUFFER_H__
+
+#include <gst/gst.h>
+#include <gst/rtp/gstrtcpbuffer.h>
+#include <gst/netbuffer/gstnetbuffer.h>
+
+typedef struct _RTPJitterBuffer RTPJitterBuffer;
+typedef struct _RTPJitterBufferClass RTPJitterBufferClass;
+
+#define RTP_TYPE_JITTER_BUFFER             (rtp_jitter_buffer_get_type())
+#define RTP_JITTER_BUFFER(src)             (G_TYPE_CHECK_INSTANCE_CAST((src),RTP_TYPE_JITTER_BUFFER,RTPJitterBuffer))
+#define RTP_JITTER_BUFFER_CLASS(klass)     (G_TYPE_CHECK_CLASS_CAST((klass),RTP_TYPE_JITTER_BUFFER,RTPJitterBufferClass))
+#define RTP_IS_JITTER_BUFFER(src)          (G_TYPE_CHECK_INSTANCE_TYPE((src),RTP_TYPE_JITTER_BUFFER))
+#define RTP_IS_JITTER_BUFFER_CLASS(klass)  (G_TYPE_CHECK_CLASS_TYPE((klass),RTP_TYPE_JITTER_BUFFER))
+#define RTP_JITTER_BUFFER_CAST(src)        ((RTPJitterBuffer *)(src))
+
+/**
+ * RTPJitterBuffer:
+ *
+ * A JitterBuffer in the #RTPSession
+ */
+struct _RTPJitterBuffer {
+  GObject       object;
+
+  GQueue       *packets;
+};
+
+struct _RTPJitterBufferClass {
+  GObjectClass   parent_class;
+};
+
+GType rtp_jitter_buffer_get_type (void);
+
+/* managing lifetime */
+RTPJitterBuffer*      rtp_jitter_buffer_new            (void);
+
+gboolean              rtp_jitter_buffer_insert         (RTPJitterBuffer *jbuf, GstBuffer *buf);
+GstBuffer *           rtp_jitter_buffer_pop            (RTPJitterBuffer *jbuf);
+
+void                  rtp_jitter_buffer_flush          (RTPJitterBuffer *jbuf);
+
+guint                 rtp_jitter_buffer_num_packets    (RTPJitterBuffer *jbuf);
+guint32               rtp_jitter_buffer_get_ts_diff    (RTPJitterBuffer *jbuf);
+
+
+
+#endif /* __RTP_JITTER_BUFFER_H__ */
