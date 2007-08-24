b4c31f289b9efe97613f1b83bc2cef8e829849cc
Zaheer Abbas Merali <zaheerabbas@merali.org>
2007-08-24 11:23:28 +0000

    gst/mpegtsparse/: Update licences to reflect LGPL-ness of these files also.
    Original commit message from CVS:
    * gst/mpegtsparse/flutspatinfo.c:
    * gst/mpegtsparse/flutspatinfo.h:
    * gst/mpegtsparse/flutspmtinfo.c:
    * gst/mpegtsparse/flutspmtinfo.h:
    Update licences to reflect LGPL-ness of these files also.

--- gst/mpegtsparse/flutspatinfo.h
+++ gst/mpegtsparse/flutspatinfo.h
@@ -16,6 +16,17 @@
  * Fluendo, S.L. All Rights Reserved.
  *
  * Contributor(s): Jan Schmidt <jan@fluendo.com>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * the GNU Lesser General Public License Version 2 or later (the "LGPL"),
+ * in which case the provisions of the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of the MPL or the LGPL.
  */
 
 #ifndef __MPEGTS_PAT_INFO_H__
