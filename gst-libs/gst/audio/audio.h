55a3eaafea583ef65ab30b379c20e1cf8853f7b1
Tim-Philipp Müller <tim@centricular.net>
2007-10-31 15:30:15 +0000

    gst-libs/gst/audio/audio.h: Don't guard deprecated enum with #ifndef DISABLE_DEPRECATED, or compilation with DISABLE_...
    Original commit message from CVS:
    * gst-libs/gst/audio/audio.h:
    Don't guard deprecated enum with #ifndef DISABLE_DEPRECATED, or
    compilation with DISABLE_DEPRECATED and without REMOVE_DEPRECATED
    (ie. normal cvs builds) will fail.

--- gst-libs/gst/audio/audio.h
+++ gst-libs/gst/audio/audio.h
@@ -135,7 +135,6 @@ gboolean gst_audio_is_buffer_framed     (GstPad* pad, GstBuffer* buf);
  *
  * Deprecated: use gst_structure_set() directly
  */
-#ifndef GST_DISABLE_DEPRECATED
 typedef enum {
   GST_AUDIO_FIELD_RATE          = (1 << 0),
   GST_AUDIO_FIELD_CHANNELS      = (1 << 1),
@@ -144,7 +143,6 @@ typedef enum {
   GST_AUDIO_FIELD_DEPTH         = (1 << 4),
   GST_AUDIO_FIELD_SIGNED        = (1 << 5),
 } GstAudioFieldFlag;
-#endif /* GST_DISABLE_DEPRECATED */
 
 #ifndef GST_DISABLE_DEPRECATED
 void gst_audio_structure_set_int (GstStructure *structure, GstAudioFieldFlag flag);
