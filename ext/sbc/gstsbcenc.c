2a21d83ce023f53bdef54b1ae1ec73e286a1a75f
Luiz Augusto von Dentz <luiz.dentz@openbossa.org>
2007-11-13 20:04:12 +0000

    sbc: Add sbc_reinit.

--- ext/sbc/gstsbcenc.c
+++ ext/sbc/gstsbcenc.c
@@ -218,8 +218,7 @@ gst_sbc_enc_fill_sbc_params (GstSbcEnc * enc, GstCaps * caps)
   if (!(allocation = gst_structure_get_string (structure, "allocation")))
     return FALSE;
 
-  sbc_finish (&enc->sbc);
-  sbc_init (&enc->sbc, 0);
+  sbc_reinit (&enc->sbc, 0);
   enc->sbc.rate = rate;
   enc->sbc.channels = channels;
   enc->blocks = blocks;
