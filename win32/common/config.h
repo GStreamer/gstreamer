f68d08a092e8ab4eb58fbc2b2b3d99d62814a6e9
Jan Schmidt <thaytan@mad.scientist.com>
2007-11-16 00:07:54 +0000

    configure.ac: releasing 0.10.15, "October"
    Original commit message from CVS:
    === release 0.10.15 ===
    
    2007-11-15  Jan Schmidt <jan.schmidt@sun.com>
    
    * configure.ac:
    releasing 0.10.15, "October"

--- win32/common/config.h
+++ win32/common/config.h
@@ -175,7 +175,7 @@
 #define HAVE_WIN32 1
 
 /* Define host CPU */
-#define HOST_CPU "x86_64"
+#define HOST_CPU "i686"
 
 /* library dir */
 #ifdef _DEBUG
@@ -197,13 +197,13 @@
 #define PACKAGE_NAME "GStreamer"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "GStreamer 0.10.14"
+#define PACKAGE_STRING "GStreamer 0.10.15"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "gstreamer"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.10.14"
+#define PACKAGE_VERSION "0.10.15"
 
 /* Define the plugin directory */
 #ifdef _DEBUG
@@ -219,7 +219,7 @@
 #undef USE_POISONING
 
 /* Version number of package */
-#define VERSION "0.10.14"
+#define VERSION "0.10.15"
 
 /* Define to 1 if your processor stores words with the most significant byte
    first (like Motorola and SPARC, unlike Intel and VAX). */
