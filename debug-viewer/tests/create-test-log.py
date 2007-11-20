9de84e42e75bde0945b116534f0bf26c658ec227
René Stadler <mail@renestadler.de>
2007-11-20 15:58:52 +0200

    Add test script to generate a simple test log. Fix level name space adjustment

---
+++ debug-viewer/tests/create-test-log.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+def main ():
+
+    import sys
+    import os.path
+    sys.path.append (os.path.dirname (os.path.dirname (sys.argv[0])))
+
+    from GstDebugViewer import Data
+
+    count = 100000
+
+    ts = 0
+    pid = 12345
+    thread = int ("89abcdef", 16)
+    level = Data.debug_level_log
+    category = "GST_DUMMY"
+    filename = "gstdummyfilename.c"
+    file_line = 1
+    function = "gst_dummy_function"
+    object_ = "dummyobj0"
+    message = "dummy message with no content"
+
+    levels = (Data.debug_level_log,
+              Data.debug_level_debug,
+              Data.debug_level_info,)
+
+    for i in xrange (count):
+
+        ts = i * 10000
+        level = levels[i % 3]
+        line = Data.LogLine ([ts, pid, thread, level, category, filename, file_line, function, object_, message])
+        print line.line_string ()
+
+if __name__ == "__main__":
+    main ()
