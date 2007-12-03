ab433ee7c99cd3fd1f9f0c39aec0e06d2c75cb1d
Edward Hervey <bilboed@bilboed.com>
2007-12-03 11:32:30 +0000

    gst-libs/gst/riff/riff-media.c: Add 'WVC1' codec mapping for Windows Media VC-1 video codec.
    Original commit message from CVS:
    * gst-libs/gst/riff/riff-media.c: (gst_riff_create_video_caps):
    Add 'WVC1' codec mapping for Windows Media VC-1 video codec.

--- gst-libs/gst/riff/riff-media.c
+++ gst-libs/gst/riff/riff-media.c
@@ -390,6 +390,14 @@ gst_riff_create_video_caps (guint32 codec_fcc,
         *codec_name = g_strdup ("Microsoft Windows Media Advanced Profile");
       break;
 
+    case GST_MAKE_FOURCC ('W', 'V', 'C', '1'):
+      caps = gst_caps_new_simple ("video/x-wmv",
+          "wmvversion", G_TYPE_INT, 3, "fourcc", GST_TYPE_FOURCC,
+          codec_fcc, NULL);
+      if (codec_name)
+        *codec_name = g_strdup ("Microsoft Windows Media VC-1");
+      break;
+
     case GST_MAKE_FOURCC ('c', 'v', 'i', 'd'):
       caps = gst_caps_new_simple ("video/x-cinepak", NULL);
       if (codec_name)
