6c413217419556f27b349b10e26a09e04035c144
Tim-Philipp Müller <tim@centricular.net>
2007-12-06 10:48:19 +0000

    gst/mpegtsparse/mpegtsparse.c: Mark crc values table as constant.
    Original commit message from CVS:
    * gst/mpegtsparse/mpegtsparse.c: (crc_tab):
    Mark crc values table as constant.

--- gst/mpegtsparse/mpegtsparse.c
+++ gst/mpegtsparse/mpegtsparse.c
@@ -128,7 +128,7 @@ static GstStateChangeReturn mpegts_parse_change_state (GstElement * element,
 
 GST_BOILERPLATE (MpegTSParse, mpegts_parse, GstElement, GST_TYPE_ELEMENT);
 
-static guint32 crc_tab[256] = {
+static const guint32 crc_tab[256] = {
   0x00000000, 0x04c11db7, 0x09823b6e, 0x0d4326d9, 0x130476dc, 0x17c56b6b,
   0x1a864db2, 0x1e475005, 0x2608edb8, 0x22c9f00f, 0x2f8ad6d6, 0x2b4bcb61,
   0x350c9b64, 0x31cd86d3, 0x3c8ea00a, 0x384fbdbd, 0x4c11db70, 0x48d0c6c7,
