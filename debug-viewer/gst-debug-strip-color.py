7b6c1274695f5f71c84824e37f40dc810fd5e8e7
René Stadler <mail@renestadler.de>
2007-12-13 13:36:45 +0200

    Add gst-debug-strip-color.py, a script to strip color codes

---
+++ debug-viewer/gst-debug-strip-color.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import sys
+import re
+
+def strip_color (input, output):
+
+    _escape = re.compile ("\x1b\\[[0-9;]*m")
+    # TODO: This can be optimized further!
+
+    for line in input:
+        while "\x1b" in line:
+            line = _escape.sub ("", line)
+        print output.write (line)
+
+def main ():
+
+    if len (sys.argv) == 1 or sys.argv[1] == "-":
+        strip_color (sys.stdin, sys.stdout)
+    else:
+        strip_color (file (sys.argv[1], "rb"), sys.stdout)
+
+if __name__ == "__main__":
+    main ()
