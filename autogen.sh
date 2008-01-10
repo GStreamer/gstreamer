25a75c7cc8e2f117aa4e0c7a3def53c52e37996f
Sebastian Dröge <slomo@circular-chaos.org>
2008-01-10 12:14:04 +0000

    autogen.sh: Add -Wno-portability to the automake parameters to stop warnings about GNU make extensions being used. We...
    Original commit message from CVS:
    * autogen.sh:
    Add -Wno-portability to the automake parameters to stop warnings
    about GNU make extensions being used. We require GNU make in almost
    every Makefile anyway.
    * configure.ac:
    Use AM_PROG_CC_C_O as a compiler that accepts both -c and -o
    at the same time is required for per target flags.

--- autogen.sh
+++ autogen.sh
@@ -84,7 +84,7 @@ echo timestamp > stamp-h.in 2> /dev/null
 
 tool_run "$autoconf"
 debug "automake: $automake"
-tool_run "$automake" "--add-missing --copy"
+tool_run "$automake" "--add-missing --copy -Wno-portability"
 
 test -n "$NOCONFIGURE" && {
   echo "skipping configure stage for package $package, as requested."
