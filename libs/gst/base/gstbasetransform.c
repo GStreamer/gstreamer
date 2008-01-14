e8d69fc5519f0b23580bd196da7b02e1b47b0f78
Stefan Kost <ensonic@users.sourceforge.net>
2008-01-14 11:40:55 +0000

    libs/gst/base/gstbasetransform.c: Fix long property description for QoS.
    Original commit message from CVS:
    * libs/gst/base/gstbasetransform.c:
    Fix long property description for QoS.

--- libs/gst/base/gstbasetransform.c
+++ libs/gst/base/gstbasetransform.c
@@ -339,7 +339,7 @@ gst_base_transform_class_init (GstBaseTransformClass * klass)
       GST_DEBUG_FUNCPTR (gst_base_transform_get_property);
 
   g_object_class_install_property (gobject_class, PROP_QOS,
-      g_param_spec_boolean ("qos", "QoS", "handle QoS messages",
+      g_param_spec_boolean ("qos", "QoS", "Handle Quality-of-Service events",
           DEFAULT_PROP_QOS, G_PARAM_READWRITE));
 
   gobject_class->finalize = GST_DEBUG_FUNCPTR (gst_base_transform_finalize);
