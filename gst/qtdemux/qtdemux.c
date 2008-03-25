7f18fb8c35153a5dc4c561e87dc3263718c69d50
Wim Taymans <wim.taymans@gmail.com>
2008-03-25 16:44:20 +0000

    gst/qtdemux/qtdemux.c: Unbreak streaming mode again.
    Original commit message from CVS:
    * gst/qtdemux/qtdemux.c: (next_entry_size):
    Unbreak streaming mode again.

--- gst/qtdemux/qtdemux.c
+++ gst/qtdemux/qtdemux.c
@@ -1866,6 +1866,9 @@ next_entry_size (GstQTDemux * demux)
   for (i = 0; i < demux->n_streams; i++) {
     stream = demux->streams[i];
 
+    if (stream->sample_index == -1)
+      stream->sample_index = 0;
+
     GST_LOG_OBJECT (demux,
         "Checking Stream %d (sample_index:%d / offset:%lld / size:%d / chunk:%d)",
         i, stream->sample_index, stream->samples[stream->sample_index].offset,
