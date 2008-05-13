21556043d21ceaa19911a0bd35e13d47f800577d
Wim Taymans <wim.taymans@gmail.com>
2008-05-13 08:59:41 +0000

    gst/rtp/gstrtpg729depay.c: Fix wrong caps string.
    Original commit message from CVS:
    * gst/rtp/gstrtpg729depay.c: (gst_rtp_g729_depay_setcaps):
    Fix wrong caps string.

--- gst/rtp/gstrtpg729depay.c
+++ gst/rtp/gstrtpg729depay.c
@@ -53,14 +53,14 @@ enum
  *
  */
 static GstStaticPadTemplate gst_rtp_g729_depay_sink_template =
-GST_STATIC_PAD_TEMPLATE ("sink",
+    GST_STATIC_PAD_TEMPLATE ("sink",
     GST_PAD_SINK,
     GST_PAD_ALWAYS,
     GST_STATIC_CAPS ("application/x-rtp, "
         "media = (string) \"audio\", "
         "payload = (int) " GST_RTP_PAYLOAD_DYNAMIC_STRING ", "
         "clock-rate = (int) 8000, "
-        "encoding-name = (string) \"G729\", "
+        "encoding-name = (string) \"G729\"; "
         "application/x-rtp, "
         "media = (string) \"audio\", "
         "payload = (int) " GST_RTP_PAYLOAD_G729_STRING ", "
