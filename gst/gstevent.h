2a1af2c947934f53e768aeba9ee84e255c78131f
Tim-Philipp Müller <tim@centricular.net>
2008-05-25 14:44:44 +0000

    gst/gstevent.h: Clarify docs for GST_SEEK_TYPE_CUR (#534505).
    Original commit message from CVS:
    * gst/gstevent.h:
    Clarify docs for GST_SEEK_TYPE_CUR (#534505).

--- gst/gstevent.h
+++ gst/gstevent.h
@@ -228,7 +228,10 @@ typedef struct _GstEventClass GstEventClass;
 /**
  * GstSeekType:
  * @GST_SEEK_TYPE_NONE: no change in position is required
- * @GST_SEEK_TYPE_CUR: change relative to current position
+ * @GST_SEEK_TYPE_CUR: change relative to currently configured segment. This
+ *    can't be used to seek relative to the current playback position - do a
+ *    position query, calculate the desired position and then do an absolute
+ *    position seek instead if that's what you want to do.
  * @GST_SEEK_TYPE_SET: absolute position is requested
  * @GST_SEEK_TYPE_END: relative position to duration is requested
  *
