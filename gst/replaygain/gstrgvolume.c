52bc66555f6542d93480e7564a985037b30e75e2
Wim Taymans <wim.taymans@gmail.com>
2008-05-26 09:57:40 +0000

    Don't use _gst_pad().
    Original commit message from CVS:
    * examples/switch/switcher.c: (switch_timer):
    * gst/replaygain/gstrgvolume.c: (gst_rg_volume_init):
    * gst/rtpmanager/gstrtpclient.c: (create_stream):
    * gst/sdp/gstsdpdemux.c: (gst_sdp_demux_stream_configure_udp),
    (gst_sdp_demux_stream_configure_udp_sink):
    * tests/check/elements/deinterleave.c: (GST_START_TEST),
    (pad_added_setup_data_check_float32_8ch_cb):
    * tests/check/elements/rganalysis.c: (send_eos_event),
    (send_tag_event):
    Don't use _gst_pad().

--- gst/replaygain/gstrgvolume.c
+++ gst/replaygain/gstrgvolume.c
@@ -341,14 +341,14 @@ gst_rg_volume_init (GstRgVolume * self, GstRgVolumeClass * gclass)
   GST_BIN_CLASS (parent_class)->add_element (GST_BIN_CAST (self),
       self->volume_element);
 
-  volume_pad = gst_element_get_pad (self->volume_element, "sink");
+  volume_pad = gst_element_get_static_pad (self->volume_element, "sink");
   ghost_pad = gst_ghost_pad_new_from_template ("sink", volume_pad,
       gst_pad_get_pad_template (volume_pad));
   gst_object_unref (volume_pad);
   gst_pad_set_event_function (ghost_pad, gst_rg_volume_sink_event);
   gst_element_add_pad (GST_ELEMENT_CAST (self), ghost_pad);
 
-  volume_pad = gst_element_get_pad (self->volume_element, "src");
+  volume_pad = gst_element_get_static_pad (self->volume_element, "src");
   ghost_pad = gst_ghost_pad_new_from_template ("src", volume_pad,
       gst_pad_get_pad_template (volume_pad));
   gst_object_unref (volume_pad);
