71185cf5c33b031e23084a0343ddaa7a287e18d0
Sebastian Dröge <slomo@circular-chaos.org>
2008-08-02 18:36:11 +0000

    [MOVED FROM BAD 38/56] gst/deinterlace2/: Add the remaining tvtime deinterlacing methods and fix the deinterlace_frame() implementation of G...
    Original commit message from CVS:
    * gst/deinterlace2/Makefile.am:
    * gst/deinterlace2/gstdeinterlace2.c:
    (gst_deinterlace_simple_method_deinterlace_frame),
    (gst_deinterlace2_methods_get_type), (gst_deinterlace2_set_method):
    * gst/deinterlace2/gstdeinterlace2.h:
    * gst/deinterlace2/tvtime/linear.c:
    (deinterlace_scanline_linear_c), (deinterlace_scanline_linear_mmx),
    (deinterlace_scanline_linear_mmxext),
    (gst_deinterlace_method_linear_class_init),
    (gst_deinterlace_method_linear_init):
    * gst/deinterlace2/tvtime/linearblend.c:
    (deinterlace_scanline_linear_blend_c),
    (deinterlace_scanline_linear_blend2_c),
    (deinterlace_scanline_linear_blend_mmx),
    (deinterlace_scanline_linear_blend2_mmx),
    (gst_deinterlace_method_linear_blend_class_init),
    (gst_deinterlace_method_linear_blend_init):
    * gst/deinterlace2/tvtime/plugins.h:
    * gst/deinterlace2/tvtime/scalerbob.c:
    (deinterlace_scanline_scaler_bob),
    (gst_deinterlace_method_scaler_bob_class_init),
    (gst_deinterlace_method_scaler_bob_init):
    * gst/deinterlace2/tvtime/weave.c: (deinterlace_scanline_weave),
    (copy_scanline), (gst_deinterlace_method_weave_class_init),
    (gst_deinterlace_method_weave_init):
    * gst/deinterlace2/tvtime/weavebff.c: (deinterlace_scanline_weave),
    (copy_scanline), (gst_deinterlace_method_weave_bff_class_init),
    (gst_deinterlace_method_weave_bff_init):
    * gst/deinterlace2/tvtime/weavetff.c: (deinterlace_scanline_weave),
    (copy_scanline), (gst_deinterlace_method_weave_tff_class_init),
    (gst_deinterlace_method_weave_tff_init):
    Add the remaining tvtime deinterlacing methods and fix the
    deinterlace_frame() implementation of GstDeinterlaceSimpleMethod.

---
+++ gst/deinterlace2/tvtime/scalerbob.c
@@ -0,0 +1,74 @@
+/**
+ * Double lines
+ * Copyright (C) 2008 Sebastian Dröge <sebastian.droege@collabora.co.uk>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include "_stdint.h"
+#include "gstdeinterlace2.h"
+#include <string.h>
+
+#define GST_TYPE_DEINTERLACE_METHOD_SCALER_BOB	(gst_deinterlace_method_scaler_bob_get_type ())
+#define GST_IS_DEINTERLACE_METHOD_SCALER_BOB(obj)		(G_TYPE_CHECK_INSTANCE_TYPE ((obj), GST_TYPE_DEINTERLACE_METHOD_SCALER_BOB))
+#define GST_IS_DEINTERLACE_METHOD_SCALER_BOB_CLASS(klass)	(G_TYPE_CHECK_CLASS_TYPE ((klass), GST_TYPE_DEINTERLACE_METHOD_SCALER_BOB))
+#define GST_DEINTERLACE_METHOD_SCALER_BOB_GET_CLASS(obj)	(G_TYPE_INSTANCE_GET_CLASS ((obj), GST_TYPE_DEINTERLACE_METHOD_SCALER_BOB, GstDeinterlaceMethodScalerBobClass))
+#define GST_DEINTERLACE_METHOD_SCALER_BOB(obj)		(G_TYPE_CHECK_INSTANCE_CAST ((obj), GST_TYPE_DEINTERLACE_METHOD_SCALER_BOB, GstDeinterlaceMethodScalerBob))
+#define GST_DEINTERLACE_METHOD_SCALER_BOB_CLASS(klass)	(G_TYPE_CHECK_CLASS_CAST ((klass), GST_TYPE_DEINTERLACE_METHOD_SCALER_BOB, GstDeinterlaceMethodScalerBobClass))
+#define GST_DEINTERLACE_METHOD_SCALER_BOB_CAST(obj)	((GstDeinterlaceMethodScalerBob*)(obj))
+
+GType gst_deinterlace_method_scaler_bob_get_type (void);
+
+typedef GstDeinterlaceSimpleMethod GstDeinterlaceMethodScalerBob;
+
+typedef GstDeinterlaceSimpleMethodClass GstDeinterlaceMethodScalerBobClass;
+
+
+static void
+deinterlace_scanline_scaler_bob (GstDeinterlaceMethod * self,
+    GstDeinterlace2 * parent, guint8 * out,
+    GstDeinterlaceScanlineData * scanlines, gint width)
+{
+  memcpy (out, scanlines->t0, parent->line_length);
+}
+
+G_DEFINE_TYPE (GstDeinterlaceMethodScalerBob, gst_deinterlace_method_scaler_bob,
+    GST_TYPE_DEINTERLACE_SIMPLE_METHOD);
+
+static void
+gst_deinterlace_method_scaler_bob_class_init (GstDeinterlaceMethodScalerBobClass
+    * klass)
+{
+  GstDeinterlaceMethodClass *dim_class = (GstDeinterlaceMethodClass *) klass;
+  GstDeinterlaceSimpleMethodClass *dism_class =
+      (GstDeinterlaceSimpleMethodClass *) klass;
+
+  dim_class->fields_required = 1;
+  dim_class->name = "Double lines";
+  dim_class->nick = "scalerbob";
+  dim_class->latency = 0;
+
+  dism_class->interpolate_scanline = deinterlace_scanline_scaler_bob;
+}
+
+static void
+gst_deinterlace_method_scaler_bob_init (GstDeinterlaceMethodScalerBob * self)
+{
+}
---
+++ gst/deinterlace2/tvtime/weave.c
@@ -0,0 +1,82 @@
+/**
+ * Weave frames
+ * Copyright (C) 2002 Billy Biggs <vektor@dumbterm.net>.
+ * Copyright (C) 2008 Sebastian Dröge <sebastian.droege@collabora.co.uk>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include "_stdint.h"
+#include "gstdeinterlace2.h"
+#include <string.h>
+
+#define GST_TYPE_DEINTERLACE_METHOD_WEAVE	(gst_deinterlace_method_weave_get_type ())
+#define GST_IS_DEINTERLACE_METHOD_WEAVE(obj)		(G_TYPE_CHECK_INSTANCE_TYPE ((obj), GST_TYPE_DEINTERLACE_METHOD_WEAVE))
+#define GST_IS_DEINTERLACE_METHOD_WEAVE_CLASS(klass)	(G_TYPE_CHECK_CLASS_TYPE ((klass), GST_TYPE_DEINTERLACE_METHOD_WEAVE))
+#define GST_DEINTERLACE_METHOD_WEAVE_GET_CLASS(obj)	(G_TYPE_INSTANCE_GET_CLASS ((obj), GST_TYPE_DEINTERLACE_METHOD_WEAVE, GstDeinterlaceMethodWeaveClass))
+#define GST_DEINTERLACE_METHOD_WEAVE(obj)		(G_TYPE_CHECK_INSTANCE_CAST ((obj), GST_TYPE_DEINTERLACE_METHOD_WEAVE, GstDeinterlaceMethodWeave))
+#define GST_DEINTERLACE_METHOD_WEAVE_CLASS(klass)	(G_TYPE_CHECK_CLASS_CAST ((klass), GST_TYPE_DEINTERLACE_METHOD_WEAVE, GstDeinterlaceMethodWeaveClass))
+#define GST_DEINTERLACE_METHOD_WEAVE_CAST(obj)	((GstDeinterlaceMethodWeave*)(obj))
+
+GType gst_deinterlace_method_weave_get_type (void);
+
+typedef GstDeinterlaceSimpleMethod GstDeinterlaceMethodWeave;
+
+typedef GstDeinterlaceSimpleMethodClass GstDeinterlaceMethodWeaveClass;
+
+
+static void
+deinterlace_scanline_weave (GstDeinterlaceMethod * self,
+    GstDeinterlace2 * parent, guint8 * out,
+    GstDeinterlaceScanlineData * scanlines, gint width)
+{
+  memcpy (out, scanlines->m1, parent->line_length);
+}
+
+static void
+copy_scanline (GstDeinterlaceMethod * self, GstDeinterlace2 * parent,
+    guint8 * out, GstDeinterlaceScanlineData * scanlines, gint width)
+{
+  memcpy (out, scanlines->m0, parent->line_length);
+}
+
+G_DEFINE_TYPE (GstDeinterlaceMethodWeave, gst_deinterlace_method_weave,
+    GST_TYPE_DEINTERLACE_SIMPLE_METHOD);
+
+static void
+gst_deinterlace_method_weave_class_init (GstDeinterlaceMethodWeaveClass * klass)
+{
+  GstDeinterlaceMethodClass *dim_class = (GstDeinterlaceMethodClass *) klass;
+  GstDeinterlaceSimpleMethodClass *dism_class =
+      (GstDeinterlaceSimpleMethodClass *) klass;
+
+  dim_class->fields_required = 2;
+  dim_class->name = "Weave";
+  dim_class->nick = "weave";
+  dim_class->latency = 0;
+
+  dism_class->interpolate_scanline = deinterlace_scanline_weave;
+  dism_class->copy_scanline = copy_scanline;
+}
+
+static void
+gst_deinterlace_method_weave_init (GstDeinterlaceMethodWeave * self)
+{
+}
