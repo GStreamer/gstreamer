dddfa0d890fd83847d1026264d54edc5578ea07a
Frederic Crozat <fcrozat@mandriva.org>
2008-08-07 16:14:42 +0000

    Make sure gettext returns translations in UTF-8 encoding rather than in the current locale encoding (#546822).
    Original commit message from CVS:
    Patch by: Frederic Crozat <fcrozat@mandriva.org>
    * ext/dvdread/dvdreadsrc.c: (plugin_init):
    * ext/lame/gstlame.c: (plugin_init):
    * gst/asfdemux/gstasf.c: (plugin_init):
    Make sure gettext returns translations in UTF-8 encoding rather
    than in the current locale encoding (#546822).

--- ext/dvdread/dvdreadsrc.c
+++ ext/dvdread/dvdreadsrc.c
@@ -1722,6 +1722,7 @@ plugin_init (GstPlugin * plugin)
   GST_DEBUG ("binding text domain %s to locale dir %s", GETTEXT_PACKAGE,
       LOCALEDIR);
   bindtextdomain (GETTEXT_PACKAGE, LOCALEDIR);
+  bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
 #endif /* ENABLE_NLS */
 
   if (!gst_element_register (plugin, "dvdreadsrc", GST_RANK_SECONDARY,
