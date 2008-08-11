ce17f1a2db12a4060bb4006aae10312fcf3575fd
Filippo Argiolas <filippo.argiolas@gmail.com>
2008-08-11 09:52:16 +0200

    [143/906] Add init and reset callbacks in GstGLFilter to run arbitrary gl code at start and stop. Useful to init and cleanup custom gl resources.

--- gst-libs/gst/gl/gstgldisplay.h
+++ gst-libs/gst/gl/gstgldisplay.h
@@ -1,6 +1,7 @@
 /* 
  * GStreamer
  * Copyright (C) 2008 Julien Isorce <julien.isorce@gmail.com>
+ * Copyright (C) 2008 Filippo Argiolas <filippo.argiolas@gmail.com>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
