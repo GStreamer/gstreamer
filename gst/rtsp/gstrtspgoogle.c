7f880435535413895430106425963cebc6bc32df
Wim Taymans <wim.taymans@gmail.com>
2008-08-20 17:42:21 +0000

    gst/rtsp/gstrtspgoogle.c: Things that can happen when your brain is in google mode trying to deal with their google r...
    Original commit message from CVS:
    * gst/rtsp/gstrtspgoogle.c:
    Things that can happen when your brain is in google mode trying to
    deal with their google rtsp server extensions and trying to type your
    google mail account.

--- gst/rtsp/gstrtspgoogle.c
+++ gst/rtsp/gstrtspgoogle.c
@@ -1,5 +1,5 @@
 /* GStreamer
- * Copyright (C) <2008> Wim Taymans <wim.taymans@google.com>
+ * Copyright (C) <2008> Wim Taymans <wim.taymans@gmail.com>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
