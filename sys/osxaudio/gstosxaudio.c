2e7ea63d43e173b4a7f66ffb6d92813b64469910
Michael Smith <msmith@xiph.org>
2008-08-26 23:57:05 +0000

    sys/osxaudio/gstosxaudio.c: Revert accidental element rename from testing.
    Original commit message from CVS:
    * sys/osxaudio/gstosxaudio.c:
    Revert accidental element rename from testing.

--- sys/osxaudio/gstosxaudio.c
+++ sys/osxaudio/gstosxaudio.c
@@ -56,11 +56,11 @@ static gboolean
 plugin_init (GstPlugin * plugin)
 {
 
-  if (!gst_element_register (plugin, "fixedosxaudiosink", GST_RANK_PRIMARY + 1,
+  if (!gst_element_register (plugin, "osxaudiosink", GST_RANK_PRIMARY + 1,
           GST_TYPE_OSX_AUDIO_SINK)) {
     return FALSE;
   }
-  if (!gst_element_register (plugin, "fixedosxaudiosrc", GST_RANK_PRIMARY + 1,
+  if (!gst_element_register (plugin, "osxaudiosrc", GST_RANK_PRIMARY + 1,
           GST_TYPE_OSX_AUDIO_SRC)) {
     return FALSE;
   }
