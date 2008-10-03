bf1d2cf12a16faae474dee9eb710603e6f83dde1
Jan Schmidt <thaytan@mad.scientist.com>
2008-10-03 00:03:05 +0000

    Release 0.10.21
    Original commit message from CVS:
    Release 0.10.21

--- win32/common/config.h
+++ win32/common/config.h
@@ -208,13 +208,13 @@
 #define PACKAGE_NAME "GStreamer Base Plug-ins"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "GStreamer Base Plug-ins 0.10.20"
+#define PACKAGE_STRING "GStreamer Base Plug-ins 0.10.21"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "gst-plugins-base"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.10.20"
+#define PACKAGE_VERSION "0.10.21"
 
 /* directory where plugins are located */
 #undef PLUGINDIR
@@ -238,7 +238,7 @@
 #undef STDC_HEADERS
 
 /* Version number of package */
-#define VERSION "0.10.20"
+#define VERSION "0.10.21"
 
 /* Define to 1 if your processor stores words with the most significant byte
    first (like Motorola and SPARC, unlike Intel and VAX). */
