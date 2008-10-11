d84cc788195f1196327f7d2cd34618eaf75b8e75
Jan Schmidt <thaytan@mad.scientist.com>
2008-10-11 11:02:14 +0000

    tests/check/pipelines/metadata.c: Make the metadata test not fail when jpegenc isn't available.... as it isn't here, ...
    Original commit message from CVS:
    * tests/check/pipelines/metadata.c:
    Make the metadata test not fail when jpegenc isn't available....
    as it isn't here, because it's not in this module, and
    therefore not in the plugin path when the check runs.

--- tests/check/pipelines/metadata.c
+++ tests/check/pipelines/metadata.c
@@ -95,7 +95,10 @@ test_tags (const gchar * tag_str)
   g_object_set (G_OBJECT (videotestsrc), "num-buffers", 1, NULL);
 
   jpegenc = gst_element_factory_make ("jpegenc", "enc");
-  fail_unless (jpegenc != NULL);
+  if (jpegenc == NULL) {
+    g_print ("Cannot test - jpegenc not available\n");
+    return;
+  }
 
   metadatamux = gst_element_factory_make ("metadatamux", "mux");
   g_object_set (G_OBJECT (metadatamux), "exif", TRUE, NULL);
