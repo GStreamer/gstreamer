d956310612fc61878fd0f4e905d37ba3a3ac075c
Sebastian Dröge <slomo@circular-chaos.org>
2008-10-27 08:40:02 +0000

    gst/udp/gstudpnetutils.h: Define the correct WINVER so getaddinfo() can be used when using mingw32. Fixes bug #557294.
    Original commit message from CVS:
    Patch by: <lrn1986 at gmail dot com>
    * gst/udp/gstudpnetutils.h:
    Define the correct WINVER so getaddinfo() can be used when using
    mingw32. Fixes bug #557294.

--- gst/udp/gstudpnetutils.h
+++ gst/udp/gstudpnetutils.h
@@ -27,6 +27,9 @@
 #include <glib.h>
 
 #ifdef G_OS_WIN32
+/* ws2_32.dll has getaddrinfo and freeaddrinfo on Windows XP and later.
+ * minwg32 headers check WINVER before allowing the use of these */
+#define WINVER 0x0501
 #include <winsock2.h>
 #include <ws2tcpip.h>
 
