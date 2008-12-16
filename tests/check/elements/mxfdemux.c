da7ea42f920df07059e5eab0208fcaec535d187a
Sebastian Dröge <slomo@circular-chaos.org>
2008-12-16 12:46:37 +0000

    tests/check/elements/mxfdemux.c: Increase the timeout to 3 minutes to prevent timeouts.
    Original commit message from CVS:
    * tests/check/elements/mxfdemux.c: (mxfdemux_suite):
    Increase the timeout to 3 minutes to prevent timeouts.

--- tests/check/elements/mxfdemux.c
+++ tests/check/elements/mxfdemux.c
@@ -264,6 +264,7 @@ mxfdemux_suite (void)
   TCase *tc_chain = tcase_create ("general");
 
   suite_add_tcase (s, tc_chain);
+  tcase_set_timeout (tc_chain, 180);
   tcase_add_test (tc_chain, test_pull);
   tcase_add_test (tc_chain, test_push);
 
