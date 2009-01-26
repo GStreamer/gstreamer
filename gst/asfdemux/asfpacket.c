3bcd050fab33a4b9390eab5d5fdd1996e8f6f7bf
Hans de Goede <jwrdegoede@fedoraproject.org>
2009-01-26 10:00:57 +0100

    Add seeking support to asfdemux in push mode
    Fixes bug #568836.

--- gst/asfdemux/asfpacket.c
+++ gst/asfdemux/asfpacket.c
@@ -127,6 +127,11 @@ gst_asf_payload_queue_for_stream (GstASFDemux * demux, AsfPayload * payload,
     GST_DEBUG_OBJECT (demux, "first ts: %" GST_TIME_FORMAT,
         GST_TIME_ARGS (payload->ts));
     demux->first_ts = payload->ts;
+    if (demux->push_mode) {
+      gst_segment_set_seek (&demux->segment, demux->segment.rate,
+          GST_FORMAT_TIME, demux->segment.flags, GST_SEEK_TYPE_SET,
+          demux->first_ts, GST_SEEK_TYPE_NONE, 0, NULL);
+    }
   }
 
   /* better drop a few frames at the beginning than send bogus timestamps */
@@ -140,7 +145,8 @@ gst_asf_payload_queue_for_stream (GstASFDemux * demux, AsfPayload * payload,
   }
 
   /* make timestamps start from 0 */
-  payload->ts -= demux->first_ts;
+  if (!demux->push_mode)
+    payload->ts -= demux->first_ts;
 
   /* remove any incomplete payloads that will never be completed */
   while (stream->payloads->len > 0) {
