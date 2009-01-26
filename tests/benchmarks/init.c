dab725ee6951962b514d340fd0fb07f381f4849e
Stefan Kost <ensonic@users.sf.net>
2009-01-26 17:14:07 +0200

    Add a trivial source for tracking gst_init time accross versions.

---
+++ tests/benchmarks/init.c
@@ -0,0 +1,31 @@
+/* GStreamer
+ * Copyright (C) 2006 Stefan Kost <stefan.kost@nokia.com>
+ *
+ * init.c: benchmark gst initialisation
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+
+#include <gst/gst.h>
+
+gint
+main (gint argc, gchar * argv[])
+{
+  gst_init (&argc, &argv);
+
+  return 0;
+}
