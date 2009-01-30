e1d532d7bd89b4de6e5182ec9d2e301bd7c9a718
Edward Hervey <bilboed@bilboed.com>
2009-01-30 09:03:42 +0100

    autogen.sh: actually setting up a symbolic link for the pre-commit hook

--- autogen.sh
+++ autogen.sh
@@ -25,8 +25,8 @@ fi
 # install pre-commit hook for doing clean commits
 if test ! \( -x .git/hooks/pre-commit -a -L .git/hooks/pre-commit \);
 then
-    cp common/hooks/pre-commit.hook .git/hooks/pre-commit
-    chmod +x .git/hooks/pre-commit
+    rm -f .git/hooks/pre-commit
+    ln -s ../../common/hooks/pre-commit.hook .git/hooks/pre-commit
 fi
 
 CONFIGURE_DEF_OPT='--enable-maintainer-mode --enable-gtk-doc'
