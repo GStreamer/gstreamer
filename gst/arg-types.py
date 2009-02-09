9291aa202fe21ea51232d60d93c20436a05db7f4
Edward Hervey <bilboed@bilboed.com>
2009-02-09 13:25:11 +0100

    GstMiniObject: Re-increment the C refcount after using a miniobject.
    This behaviour is symmetrical to what we do at the very beginning (incrementing
    the Python refcount of the wrapper object and decrementing the C refcount of the
    actual object).

--- gst/arg-types.py
+++ gst/arg-types.py
@@ -160,7 +160,7 @@ class GstMiniObjectParam(Parameter):
 					      "    py_%s = Py_None;\n"
 					      "}"
 					      % (self.name, self.name, self.name, self.name, self.name)),
-					cleanup=("Py_DECREF(py_%s);" % self.name))
+					cleanup=("gst_mini_object_ref ((GstMiniObject *) %s); Py_DECREF(py_%s);" % (self.name, self.name)))
 		self.wrapper.add_pyargv_item("py_%s" % self.name)
 
 matcher.register_reverse('GstMiniObject*', GstMiniObjectParam)
