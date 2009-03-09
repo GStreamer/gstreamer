b870b61c0096e001396b49f5ccbb5a0aad11d555
Edward Hervey <bilboed@bilboed.com>
2009-03-09 09:07:13 +0100

    riff: Add the 'DVR ' mapping for mpeg2video.
    Found this in 3 files from the insanity suite and mapping is also present
    in libavformat.

--- gst-libs/gst/riff/riff-media.c
+++ gst-libs/gst/riff/riff-media.c
@@ -195,6 +195,7 @@ gst_riff_create_video_caps (guint32 codec_fcc,
     case GST_MAKE_FOURCC ('M', 'P', 'G', '2'):
     case GST_MAKE_FOURCC ('m', 'p', 'g', '2'):
     case GST_MAKE_FOURCC ('P', 'I', 'M', '2'):
+    case GST_MAKE_FOURCC ('D', 'V', 'R', ' '):
     case GST_MAKE_FOURCC (0x02, 0x00, 0x00, 0x10):
       caps = gst_caps_new_simple ("video/mpeg",
           "systemstream", G_TYPE_BOOLEAN, FALSE,
