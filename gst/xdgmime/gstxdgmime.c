eaea85ed4a2f8e442b713b50d89d5df35322c746
Sebastian Dröge <sebastian.droege@collabora.co.uk>
2009-03-26 12:10:05 +0100

    xdgmime: Use GIOs g_content_type_guess() if possible

--- gst/xdgmime/gstxdgmime.c
+++ gst/xdgmime/gstxdgmime.c
@@ -26,15 +26,18 @@
 GST_DEBUG_CATEGORY (xdgmime_debug);
 #define GST_CAT_DEFAULT xdgmime_debug
 
+#if GLIB_CHECK_VERSION (2, 16, 0)
+#include <gio/gio.h>
+#else
 G_LOCK_DEFINE_STATIC (xdg_lock);
+#endif
 
 static void
 xdgmime_typefind (GstTypeFind * find, gpointer user_data)
 {
-  const gchar *mimetype;
+  gchar *mimetype;
   gsize length = 16384;
   guint64 tf_length;
-  gint prio;
   guint8 *data;
 
   if ((tf_length = gst_type_find_get_length (find)) > 0)
@@ -43,18 +46,45 @@ xdgmime_typefind (GstTypeFind * find, gpointer user_data)
   if ((data = gst_type_find_peek (find, 0, length)) == NULL)
     return;
 
-  /* FIXME: xdg-mime is not thread-safe as it stores the cache globally
-   * and updates it from every call if changes were done without
-   * any locking
-   */
-  G_LOCK (xdg_lock);
-  mimetype = xdg_mime_get_mime_type_for_data (data, length, &prio);
-  G_UNLOCK (xdg_lock);
+#if GLIB_CHECK_VERSION (2, 16, 0)
+  {
+    gchar *tmp;
 
-  if (mimetype == NULL || g_str_equal (mimetype, XDG_MIME_TYPE_UNKNOWN))
-    return;
+    tmp = g_content_type_guess (NULL, data, length, NULL);
+    if (tmp == NULL || g_content_type_is_unknown (tmp)) {
+      g_free (tmp);
+      return;
+    }
 
-  GST_DEBUG ("Got mimetype '%s' with prio %d", mimetype, prio);
+    mimetype = g_content_type_get_mime_type (tmp);
+    g_free (tmp);
+
+    if (mimetype == NULL)
+      return;
+
+    GST_DEBUG ("Got mimetype '%s'", mimetype);
+  }
+#else
+  {
+    const gchar *tmp;
+    gint prio;
+
+    /* FIXME: xdg-mime is not thread-safe as it stores the cache globally
+     * and updates it from every call if changes were done without
+     * any locking
+     */
+    G_LOCK (xdg_lock);
+    tmp = xdg_mime_get_mime_type_for_data (data, length, &prio);
+    G_UNLOCK (xdg_lock);
+
+    if (tmp == NULL || g_str_equal (tmp, XDG_MIME_TYPE_UNKNOWN))
+      return;
+
+    GST_DEBUG ("Got mimetype '%s' with prio %d", tmp, prio);
+
+    mimetype = g_strdup (tmp);
+  }
+#endif
 
   /* Ignore audio/video types:
    *  - our own typefinders in -base are likely to be better at this
@@ -67,6 +97,7 @@ xdgmime_typefind (GstTypeFind * find, gpointer user_data)
   if (g_str_has_prefix (mimetype, "audio/") ||
       g_str_has_prefix (mimetype, "video/")) {
     GST_LOG ("Ignoring audio/video mime type");
+    g_free (mimetype);
     return;
   }
 
@@ -75,6 +106,7 @@ xdgmime_typefind (GstTypeFind * find, gpointer user_data)
    * uncertain results of our typefinders, but not more than that. */
   GST_LOG ("Suggesting '%s' with probability POSSIBLE", mimetype);
   gst_type_find_suggest_simple (find, GST_TYPE_FIND_POSSIBLE, mimetype, NULL);
+  g_free (mimetype);
 }
 
 static gboolean
