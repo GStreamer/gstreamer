ffc752c45855fcfc999ba30497ce1777adc2d4f0
Wim Taymans <wim.taymans@collabora.co.uk>
2009-03-27 16:15:10 +0100

    element: Fix a little debug message

--- gst/gstelement.c
+++ gst/gstelement.c
@@ -1772,7 +1772,7 @@ gst_element_set_locked_state (GstElement * element, gboolean locked_state)
 
 was_ok:
   {
-    GST_CAT_DEBUG (GST_CAT_STATES, "elements %s was in locked state %d",
+    GST_CAT_DEBUG (GST_CAT_STATES, "elements %s was already in locked state %d",
         GST_ELEMENT_NAME (element), old);
     GST_OBJECT_UNLOCK (element);
 
