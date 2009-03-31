dfe96ce6188693fe3bef16407bb0b87b85afbbe9
Tim-Philipp Müller <tim.muller@collabora.co.uk>
2009-03-31 18:13:19 +0100

    rtsp: clear the entire builder structure
    And use structure instead of variable with sizeof when
    clearing the rtsp message structure, for clarity.

--- gst-libs/gst/rtsp/gstrtspconnection.c
+++ gst-libs/gst/rtsp/gstrtspconnection.c
@@ -239,7 +239,7 @@ static void
 build_reset (GstRTSPBuilder * builder)
 {
   g_free (builder->body_data);
-  memset (builder, 0, sizeof (builder));
+  memset (builder, 0, sizeof (GstRTSPBuilder));
 }
 
 /**
