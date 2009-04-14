3743c83acea97f65a219c07d3b4b4a7fd1c6089e
David Hoyt <dhoyt at llnl.gov>
2009-04-14 19:16:46 +0200

    synaesthesia: fix compilation on windows
    Fix compilation under MSVC due to references to headers
    that are not available with the MS SDKs.
    Fixes #578524

--- gst/synaesthesia/synaescope.c
+++ gst/synaesthesia/synaescope.c
@@ -28,12 +28,16 @@
 #include <pthread.h>
 #include <dirent.h>
 #include <sys/stat.h>
+#ifndef _MSC_VER
 #include <sys/time.h>
 #include <time.h>
+#endif
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#ifndef _MSC_VER
 #include <unistd.h>
+#endif
 #include <string.h>
 #include <assert.h>
 
