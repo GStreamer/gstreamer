65b728234f1a74f6f1e00b6287db775e521c5e8e
Sebastian Dröge <sebastian.droege@collabora.co.uk>
2009-04-14 17:58:28 +0200

    Add Gst.StaticPadTemplate.Free() and fix indention

--- gstreamer-sharp/glue/staticcaps.c
+++ gstreamer-sharp/glue/staticcaps.c
@@ -1,12 +1,14 @@
 #include <gst/gst.h>
 
 const gchar *
-gstsharp_gst_static_caps_get_string (const GstStaticCaps *caps) {
+gstsharp_gst_static_caps_get_string (const GstStaticCaps * caps)
+{
   return caps->string;
 }
 
 GstStaticCaps *
-gstsharp_gst_static_caps_new (const gchar *string) {
+gstsharp_gst_static_caps_new (const gchar * string)
+{
   GstStaticCaps *caps = g_new0 (GstStaticCaps, 1);
 
   caps->string = g_strdup (string);
