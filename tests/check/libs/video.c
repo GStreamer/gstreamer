a8e3b4cacbcc73f2b153c1199dc0c73520b4571f
Jan Schmidt <thaytan@noraisin.net>
2009-04-16 00:31:55 +0100

    check: Add GST_VIDEO_FORMAT_YVYU to the test so it passes.

--- tests/check/libs/video.c
+++ tests/check/libs/video.c
@@ -338,6 +338,7 @@ video_format_is_packed (GstVideoFormat fmt)
     case GST_VIDEO_FORMAT_Y42B:
       return FALSE;
     case GST_VIDEO_FORMAT_YUY2:
+    case GST_VIDEO_FORMAT_YVYU:
     case GST_VIDEO_FORMAT_UYVY:
     case GST_VIDEO_FORMAT_AYUV:
     case GST_VIDEO_FORMAT_RGBx:
