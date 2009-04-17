a3d1cfe2474a49ae5cb1cc7d3273528e6aa58a3c
Jan Schmidt <thaytan@noraisin.net>
2009-04-17 13:46:39 +0100

    check: Add a simple test that the FFmpeg plugin loads
    Check that the ffmpeg plugin actually exists after building.

---
+++ tests/check/generic/plugin-test.c
@@ -0,0 +1,71 @@
+/* GStreamer
+ * Copyright (C) 2009 Jan Schmidt <thaytan@noraisin.net>
+ *
+ * Test that the FFmpeg plugin is loadable, and not broken in some stupid
+ * way.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+
+#include <gst/check/gstcheck.h>
+#include <stdlib.h>
+
+GST_START_TEST (test_ffmpeg_plugin)
+{
+  GstPlugin *plugin = gst_plugin_load_by_name ("ffmpeg");
+
+  fail_if (plugin == NULL, "Could not load FFmpeg plugin");
+
+  gst_object_unref (plugin);
+
+}
+
+GST_END_TEST;
+
+Suite *
+plugin_test_suite (void)
+{
+  gint timeout = 0;
+
+  Suite *s = suite_create ("Plugin");
+  TCase *tc_chain = tcase_create ("existence");
+
+  suite_add_tcase (s, tc_chain);
+
+  tcase_add_test (tc_chain, test_ffmpeg_plugin);
+
+  return s;
+}
+
+int
+main (int argc, char **argv)
+{
+  SRunner *sr;
+  Suite *s;
+  int nf;
+
+  gst_check_init (&argc, &argv);
+
+  s = plugin_test_suite ();
+  sr = srunner_create (s);
+
+  srunner_run_all (sr, CK_NORMAL);
+  nf = srunner_ntests_failed (sr);
+  srunner_free (sr);
+
+  return nf;
+}
