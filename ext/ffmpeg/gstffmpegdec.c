199d95c390d0a975e7747f7e4ead060d5adf08a4
Edward Hervey <bilboed@bilboed.com>
2009-04-18 08:50:12 +0200

    gstffmpegdec: Fix build on macosx.
    The arguments were in the wrong order (and therefore the types were wrong).

--- ext/ffmpeg/gstffmpegdec.c
+++ ext/ffmpeg/gstffmpegdec.c
@@ -2252,8 +2252,9 @@ gst_ffmpegdec_chain (GstPad * pad, GstBuffer * inbuf)
   in_offset = GST_BUFFER_OFFSET (inbuf);
 
   GST_LOG_OBJECT (ffmpegdec,
-      "Received new data of size %d, offset:%" G_GINT64_FORMAT ", ts:%"
-      GST_TIME_FORMAT ", dur:%" GST_TIME_FORMAT, GST_BUFFER_OFFSET (inbuf),
+      "Received new data of size %u, offset:%" G_GUINT64_FORMAT ", ts:%"
+      GST_TIME_FORMAT ", dur:%" GST_TIME_FORMAT,
+      GST_BUFFER_SIZE (inbuf), GST_BUFFER_OFFSET (inbuf),
       GST_BUFFER_SIZE (inbuf), GST_TIME_ARGS (in_timestamp),
       GST_TIME_ARGS (in_duration));
 
