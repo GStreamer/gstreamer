a23154fc2fc47c6664b0981780ac4324098fb4f5
Sebastian Dröge <sebastian.droege@collabora.co.uk>
2009-04-21 14:31:29 +0200

    Complete/fix bindings for Gst.Tag and Gst.TagList

---
+++ gstreamer-sharp/glue/taglist.c
@@ -0,0 +1,6 @@
+#include <gst/gst.h>
+
+void
+gstsharp_gst_tag_list_add_value (GstTagList *list, GstTagMergeMode mode, const gchar * tag, const GValue *v) {
+  gst_tag_list_add_values (list, mode, tag, v, NULL);
+}
