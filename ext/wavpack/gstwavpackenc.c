9bb965430f5100dc598fcc2e954f62da89b9a702
Sebastian Dröge <sebastian.droege@collabora.co.uk>
2009-05-04 12:40:12 +0200

    wavpack: Implement preset interface

--- ext/wavpack/gstwavpackenc.c
+++ ext/wavpack/gstwavpackenc.c
@@ -197,7 +197,21 @@ gst_wavpack_enc_joint_stereo_mode_get_type (void)
   return qtype;
 }
 
-GST_BOILERPLATE (GstWavpackEnc, gst_wavpack_enc, GstElement, GST_TYPE_ELEMENT);
+static void
+_do_init (GType object_type)
+{
+  const GInterfaceInfo preset_interface_info = {
+    NULL,                       /* interface_init */
+    NULL,                       /* interface_finalize */
+    NULL                        /* interface_data */
+  };
+
+  g_type_add_interface_static (object_type, GST_TYPE_PRESET,
+      &preset_interface_info);
+}
+
+GST_BOILERPLATE_FULL (GstWavpackEnc, gst_wavpack_enc, GstElement,
+    GST_TYPE_ELEMENT, _do_init);
 
 static void
 gst_wavpack_enc_base_init (gpointer klass)
