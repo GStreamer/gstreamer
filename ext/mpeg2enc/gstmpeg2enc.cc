e9e52d0a9d9fdafc3ee499a9151f8ffbc22752af
Sebastian Dröge <sebastian.droege@collabora.co.uk>
2009-05-04 12:33:41 +0200

    mpeg2enc: Implement Preset interface

--- ext/mpeg2enc/gstmpeg2enc.cc
+++ ext/mpeg2enc/gstmpeg2enc.cc
@@ -99,7 +99,21 @@ static void gst_mpeg2enc_get_property (GObject * object,
 static void gst_mpeg2enc_set_property (GObject * object,
     guint prop_id, const GValue * value, GParamSpec * pspec);
 
-GST_BOILERPLATE (GstMpeg2enc, gst_mpeg2enc, GstElement, GST_TYPE_ELEMENT);
+static void
+_do_init (GType object_type)
+{
+  const GInterfaceInfo preset_interface_info = {
+    NULL,                       /* interface_init */
+    NULL,                       /* interface_finalize */
+    NULL                        /* interface_data */
+  };
+
+  g_type_add_interface_static (object_type, GST_TYPE_PRESET,
+      &preset_interface_info);
+}
+
+GST_BOILERPLATE_FULL (GstMpeg2enc, gst_mpeg2enc, GstElement, GST_TYPE_ELEMENT,
+    _do_init);
 
 static void
 gst_mpeg2enc_base_init (gpointer klass)
