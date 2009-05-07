5988e1ad3955b77cc4f15fb0222bf138c28d2349
Sebastian Dröge <sebastian.droege@collabora.co.uk>
2009-05-07 10:39:38 +0200

    Update API fixups for the latest protected-fields patch

--- gstreamer-sharp/glue/task.c
+++ gstreamer-sharp/glue/task.c
@@ -6,8 +6,3 @@ gstsharp_gst_task_get_cond_offset (void)
   return (uint) G_STRUCT_OFFSET (GstTask, cond);
 }
 
-uint
-gstsharp_gst_task_get_running_offset (void)
-{
-  return (uint) G_STRUCT_OFFSET (GstTask, running);
-}
