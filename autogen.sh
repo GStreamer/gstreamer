ce34fdfc309617595c53e7b4d5b083171205f9f6
Marc-Andre Lureau <marcandre.lureau@gmail.com>
2009-05-10 11:17:19 +0200

    Run libtoolize before aclocal
    This unbreaks the build in some cases. Fixes bug #582021

--- autogen.sh
+++ autogen.sh
@@ -77,8 +77,8 @@ fi
 tool_run "$autopoint --force"
 patch -p0 < common/gettext.patch
 
-tool_run "$aclocal" "-I m4 -I common/m4 $ACLOCAL_FLAGS"
 tool_run "$libtoolize" "--copy --force"
+tool_run "$aclocal" "-I m4 -I common/m4 $ACLOCAL_FLAGS"
 tool_run "$autoheader"
 
 # touch the stamp-h.in build stamp so we don't re-run autoheader in maintainer mode -- wingo
