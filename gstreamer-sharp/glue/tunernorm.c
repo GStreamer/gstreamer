75c1fe23d19b5e5cbd9f6e06497946e4c706a0d0
Sebastian Dröge <sebastian.droege@collabora.co.uk>
2009-05-12 12:04:53 +0200

    Add Gst.Interfaces.Tuner* bindings

---
+++ gstreamer-sharp/glue/tunernorm.c
@@ -0,0 +1,8 @@
+#include <gst/interfaces/tunernorm.h>
+
+guint
+gst__interfacessharp_gst__interfaces_tunernorm_get_framerate_offset (void)
+{
+        return (guint)G_STRUCT_OFFSET (GstTunerNorm, framerate);
+}
+
