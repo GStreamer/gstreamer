243d366b34b80b9b64fdfea4a2d45bef77ddd560
Wim Taymans <wim.taymans@collabora.co.uk>
2009-05-20 00:48:40 +0200

    videoutils: remove adapter methods
    Remove adapter methods now that they are in core.

--- gst-libs/gst/video/gstbasevideoutils.h
+++ gst-libs/gst/video/gstbasevideoutils.h
@@ -84,8 +84,6 @@ gboolean gst_base_video_state_from_caps (GstVideoState *state,
 GstClockTime gst_video_state_get_timestamp (const GstVideoState *state,
     int frame_number);
 
-int gst_adapter_masked_scan_uint32 (GstAdapter *adapter,
-    guint32 pattern, guint32 mask, int offset, int n);
 GstBuffer *gst_adapter_get_buffer (GstAdapter *adapter);
 
 G_END_DECLS
