8eac0482fdebf7bd8874daa18d9ca0bd4e6a6901
Jan Schmidt <thaytan@noraisin.net>
2009-06-04 16:29:31 +0100

    mpegdemux: Add a GST_MEMDUMP line in the descriptor parsing
    Make it possible to see descriptor contents in the debug output
    (GST_DEBUG=mpegtsdesc:9), and remove a stray semi-colon.

--- gst/mpegdemux/gstmpegdesc.c
+++ gst/mpegdemux/gstmpegdesc.c
@@ -65,7 +65,9 @@ gst_mpeg_descriptor_parse_1 (guint8 * data, guint size)
   if (length > size)
     return 0;
 
-  return length + 2;;
+  GST_MEMDUMP ("tag contents:", data, length);
+
+  return length + 2;
 }
 
 GstMPEGDescriptor *
