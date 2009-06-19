c5793a6a45cb05980a1a41301d51830bbf4ec4e0
Tim-Philipp Müller <tim.muller@collabora.co.uk>
2009-06-19 16:21:28 +0100

    Make build without warnings with debugging disabled

--- gst/rtpmanager/gstrtpjitterbuffer.c
+++ gst/rtpmanager/gstrtpjitterbuffer.c
@@ -1688,9 +1688,8 @@ flushing:
   }
 pause:
   {
-    const gchar *reason = gst_flow_get_name (result);
-
-    GST_DEBUG_OBJECT (jitterbuffer, "pausing task, reason %s", reason);
+    GST_DEBUG_OBJECT (jitterbuffer, "pausing task, reason %s",
+        gst_flow_get_name (result));
 
     JBUF_LOCK (priv);
     /* store result */
