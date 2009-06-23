26d91daacb73bbdf97a0803c2cc61cafb2e42dea
Chad Hanna <channa@ligo.caltech.edu>
2009-06-23 12:56:59 +0200

    capsfilter: Add GAP flag support
    capsfilter doesn't actually touch the data so we don't want the GAP flag to
    be unset by basetransform.
    
    Fixes bug #586566.

--- plugins/elements/gstcapsfilter.c
+++ plugins/elements/gstcapsfilter.c
@@ -122,6 +122,8 @@ gst_capsfilter_class_init (GstCapsFilterClass * klass)
 static void
 gst_capsfilter_init (GstCapsFilter * filter, GstCapsFilterClass * g_class)
 {
+  GstBaseTransform *trans = GST_BASE_TRANSFORM (filter);
+  gst_base_transform_set_gap_aware (trans, TRUE);
   filter->filter_caps = gst_caps_new_any ();
 }
 
