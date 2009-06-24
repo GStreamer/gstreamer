adff66fc83c659ca8332fe6b39cd896f9ad95dd0
Tim-Philipp Müller <tim.muller@collabora.co.uk>
2009-06-24 09:48:41 +0100

    pbutils: add description for multipart
    So we get slightly nicer error messages when multipartdemux is missing.

--- gst-libs/gst/pbutils/descriptions.c
+++ gst-libs/gst/pbutils/descriptions.c
@@ -68,6 +68,7 @@ static const FormatInfo formats[] = {
   {"application/x-apetag", N_("APE tag"), FLAG_CONTAINER},
   {"application/x-icy", N_("ICY internet radio"), FLAG_CONTAINER},
   {"application/x-3gp", "3GP", FLAG_CONTAINER},
+  {"multipart/x-mixed-replace", "Multipart", FLAG_CONTAINER},
   {"video/x-fli", "FLI/FLC/FLX Animation", FLAG_CONTAINER},
   {"video/x-flv", "Flash", FLAG_CONTAINER},
   {"video/x-matroska", "Matroska", FLAG_CONTAINER},
