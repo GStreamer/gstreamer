234f34c14cc396b9b58828d7f45f86d8586c84ed
Jan Schmidt <thaytan@noraisin.net>
2009-06-26 14:40:14 +0100

    autogen.sh: Use printf instead of 'echo -n'. Check for automake-1.1[01]
    Check for more automake command variants. Use printf instead of 'echo -n'
    for portability

--- autogen.sh
+++ autogen.sh
@@ -38,7 +38,7 @@ echo -n "+ check for build tools"
 if test ! -z "$NOCHECK"; then echo " skipped"; else  echo; fi
 version_check "autoconf" "$AUTOCONF autoconf autoconf-2.54 autoconf-2.53 autoconf-2.52" \
               "ftp://ftp.gnu.org/pub/gnu/autoconf/" 2 52 || DIE=1
-version_check "automake" "$AUTOMAKE automake automake-1.9 automake-1.7 automake-1.6 automake-1.5" \
+version_check "automake" "$AUTOMAKE automake automake-1.11 automake-1.10 automake-1.9 automake-1.7 automake-1.6 automake-1.5" \
               "ftp://ftp.gnu.org/pub/gnu/automake/" 1 7 || DIE=1
 version_check "autopoint" "autopoint" \
               "ftp://ftp.gnu.org/pub/gnu/gettext/" 0 17 || DIE=1
