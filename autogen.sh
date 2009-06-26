9ab7cfd976af5f0a74c9cf5d70914314ea60975f
Jan Schmidt <thaytan@noraisin.net>
2009-06-26 15:00:14 +0100

    autogen.sh: Actually do the 'echo -n' -> printf change.

--- autogen.sh
+++ autogen.sh
@@ -34,7 +34,7 @@ CONFIGURE_DEF_OPT='--enable-maintainer-mode --enable-gtk-doc --enable-plugin-doc
 
 autogen_options $@
 
-echo -n "+ check for build tools"
+printf "+ check for build tools"
 if test ! -z "$NOCHECK"; then echo " skipped"; else  echo; fi
 version_check "autoconf" "$AUTOCONF autoconf autoconf-2.54 autoconf-2.53 autoconf-2.52" \
               "ftp://ftp.gnu.org/pub/gnu/autoconf/" 2 52 || DIE=1
