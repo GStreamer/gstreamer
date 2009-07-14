ad5edd7e32ad1e803d77965ac9b6338647733b83
Filippo Argiolas <filippo.argiolas@gmail.com>
2009-07-14 20:25:28 +0200

    [360/906] examples: add missing copyright/license to my examples

--- tests/examples/gtk/fxtest/pixbufdrop.c
+++ tests/examples/gtk/fxtest/pixbufdrop.c
@@ -1,3 +1,23 @@
+/*
+ * GStreamer
+ * Copyright (C) 2008-2009 Filippo Argiolas <filippo.argiolas@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
 #include <gst/gst.h>
 #include <gtk/gtk.h>
 #include <gdk/gdk.h>
@@ -29,7 +49,8 @@ create_window (GstBus * bus, GstMessage * message, GtkWidget * widget)
   if (!gst_structure_has_name (message->structure, "prepare-xwindow-id"))
     return GST_BUS_PASS;
 
-  gst_x_overlay_set_gtk_window (GST_X_OVERLAY (GST_MESSAGE_SRC (message)), widget);
+  gst_x_overlay_set_gtk_window (GST_X_OVERLAY (GST_MESSAGE_SRC (message)),
+      widget);
 
   gst_message_unref (message);
 
