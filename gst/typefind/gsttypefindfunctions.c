3c8ff21ba29d79621409ef2a5356209cceaf7fd9
Sebastian Dröge <sebastian.droege@collabora.co.uk>
2009-08-26 09:08:12 +0200

    typefindfunctions: Detect AVF files as RIFF files too
    AVF is valid RIFF but has AVF0 as first fourcc instead of RIFF.
    
    Partially fixes bug #593117.

--- gst/typefind/gsttypefindfunctions.c
+++ gst/typefind/gsttypefindfunctions.c
@@ -3298,7 +3298,7 @@ riff_type_find (GstTypeFind * tf, gpointer private)
   GstTypeFindData *riff_data = (GstTypeFindData *) private;
   guint8 *data = gst_type_find_peek (tf, 0, 12);
 
-  if (data && memcmp (data, "RIFF", 4) == 0) {
+  if (data && (memcmp (data, "RIFF", 4) == 0 || memcmp (data, "AVF0", 4) == 0)) {
     data += 8;
     if (memcmp (data, riff_data->data, 4) == 0)
       gst_type_find_suggest (tf, riff_data->probability, riff_data->caps);
