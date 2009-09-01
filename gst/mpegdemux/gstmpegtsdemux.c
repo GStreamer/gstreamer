3f35ef45846cb1ba657c5740693225a59acc563e
Sebastian Dröge <sebastian.droege@collabora.co.uk>
2009-09-01 12:41:19 +0200

    mpegtsdemux: Stream type 0x81 is normal AC3, not EAC3
    Not sure what the stream type for EAC3 is though.
    
    Fixes bug #593059.

--- gst/mpegdemux/gstmpegtsdemux.c
+++ gst/mpegdemux/gstmpegtsdemux.c
@@ -671,7 +671,7 @@ gst_mpegts_demux_fill_stream (GstMpegTSStream * stream, guint8 id,
     case ST_BD_AUDIO_AC3:
       template = klass->audio_template;
       name = g_strdup_printf ("audio_%04x", stream->PID);
-      caps = gst_caps_new_simple ("audio/x-eac3", NULL);
+      caps = gst_caps_new_simple ("audio/x-ac3", NULL);
       break;
     case ST_PS_AUDIO_DTS:
       template = klass->audio_template;
