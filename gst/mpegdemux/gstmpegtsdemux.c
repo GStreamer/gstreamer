1e865242a7a30ca398af3dcb89908785b46b7862
Josep Torra <josep@fluendo.com>
2009-09-03 15:40:14 +0200

    mpegtsdemux: Implement EAC3 handling according some ATSC specs.
    Fixes bug #594030.

--- gst/mpegdemux/gstmpegtsdemux.c
+++ gst/mpegdemux/gstmpegtsdemux.c
@@ -669,9 +669,21 @@ gst_mpegts_demux_fill_stream (GstMpegTSStream * stream, guint8 id,
           NULL);
       break;
     case ST_BD_AUDIO_AC3:
+      if (gst_mpeg_descriptor_find (stream->ES_info, DESC_DVB_AC3)) {
+        template = klass->audio_template;
+        name = g_strdup_printf ("audio_%04x", stream->PID);
+        caps = gst_caps_new_simple ("audio/x-ac3", NULL);
+      } else if (gst_mpeg_descriptor_find (stream->ES_info,
+              DESC_DVB_ENHANCED_AC3)) {
+        template = klass->audio_template;
+        name = g_strdup_printf ("audio_%04x", stream->PID);
+        caps = gst_caps_new_simple ("audio/x-eac3", NULL);
+      }
+      break;
+    case ST_BD_AUDIO_EAC3:
       template = klass->audio_template;
       name = g_strdup_printf ("audio_%04x", stream->PID);
-      caps = gst_caps_new_simple ("audio/x-ac3", NULL);
+      caps = gst_caps_new_simple ("audio/x-eac3", NULL);
       break;
     case ST_PS_AUDIO_DTS:
       template = klass->audio_template;
