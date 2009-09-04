b35b752c41c6b207db862309d6fae4e0977635f2
Sebastian Dröge <sebastian.droege@collabora.co.uk>
2009-09-04 07:10:03 +0200

    matroskademux: Correctly handle NULL GstIndex

--- gst/matroska/matroska-demux.c
+++ gst/matroska/matroska-demux.c
@@ -5803,7 +5803,7 @@ gst_matroska_demux_set_index (GstElement * element, GstIndex * index)
   GST_OBJECT_LOCK (demux);
   if (demux->element_index)
     gst_object_unref (demux->element_index);
-  demux->element_index = gst_object_ref (index);
+  demux->element_index = index ? gst_object_ref (index) : NULL;
   GST_OBJECT_UNLOCK (demux);
   GST_DEBUG_OBJECT (demux, "Set index %" GST_PTR_FORMAT, demux->element_index);
 }
