872b6c8702337e4b13fe85c74fb60e5735ebdbaf
Jan Schmidt <thaytan@noraisin.net>
2009-10-21 17:14:34 +0100

    release 0.10.15

--- win32/common/config.h
+++ win32/common/config.h
@@ -199,7 +199,7 @@
 #undef USE_POISONING
 
 /* Version number of package */
-#define VERSION "0.10.14.4"
+#define VERSION "0.10.15"
 
 /* Define to 1 if your processor stores words with the most significant byte
    first (like Motorola and SPARC, unlike Intel and VAX). */
