8a1b22836f3dd8d38ebd369b39d2f202f8d90fe4
Tim-Philipp Müller <tim.muller@collabora.co.uk>
2009-10-31 17:20:53 +0000

    aiff: rename 'aiffparse' plugin to 'aiff'

--- gst/aiff/aiffparse.c
+++ gst/aiff/aiffparse.c
@@ -1605,6 +1605,6 @@ plugin_init (GstPlugin * plugin)
 
 GST_PLUGIN_DEFINE (GST_VERSION_MAJOR,
     GST_VERSION_MINOR,
-    "aiffparse",
+    "aiff",
     "Parse an .aiff file into raw audio",
     plugin_init, VERSION, GST_LICENSE, GST_PACKAGE_NAME, GST_PACKAGE_ORIGIN)
