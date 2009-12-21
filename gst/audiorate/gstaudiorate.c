93f82f16cdf0ee9fe817cba96b36c4eb6c5cc270
Mark Nauwelaerts <mark.nauwelaerts@collabora.co.uk>
2009-12-21 18:45:58 +0100

    audiorate: add Since marker for the new tolerance property

--- gst/audiorate/gstaudiorate.c
+++ gst/audiorate/gstaudiorate.c
@@ -199,6 +199,14 @@ gst_audio_rate_class_init (GstAudioRateClass * klass)
       g_param_spec_boolean ("silent", "silent",
           "Don't emit notify for dropped and duplicated frames", DEFAULT_SILENT,
           G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS));
+  /**
+   * GstAudioRate:tolerance
+   *
+   * The difference between incoming timestamp and next timestamp must exceed
+   * the given value for audiorate to add or drop samples.
+   *
+   * Since: 0.10.26
+   **/
   g_object_class_install_property (object_class, ARG_TOLERANCE,
       g_param_spec_uint64 ("tolerance", "tolerance",
           "Only act if timestamp jitter/imperfection exceeds indicated tolerance (ns)",
