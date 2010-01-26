5c7a160de52ab64426182a0024753b6c899f01b7
Olivier Crête <olivier.crete@collabora.co.uk>
2010-01-26 12:34:34 +0200

    shmsink: Get potentially modified after opening shmpipe

--- gst/shm/gstshmsink.c
+++ gst/shm/gstshmsink.c
@@ -288,6 +288,9 @@ gst_shm_sink_start (GstBaseSink * bsink)
     return FALSE;
   }
 
+  g_free (self->socket_path);
+  self->socket_path = g_strdup (sp_writer_get_path (self->pipe));
+
   self->poll = gst_poll_new (TRUE);
   gst_poll_fd_init (&self->serverpollfd);
   self->serverpollfd.fd = sp_get_fd (self->pipe);
