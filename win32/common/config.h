edfabdfca997f29479c7995182aacea37ccc24e0
Tim-Philipp Müller <tim.muller@collabora.co.uk>
2010-02-04 18:30:56 +0000

    0.10.25.3 pre-release

--- win32/common/config.h
+++ win32/common/config.h
@@ -316,7 +316,7 @@
 #define PACKAGE_NAME "GStreamer"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "GStreamer 0.10.25.2"
+#define PACKAGE_STRING "GStreamer 0.10.25.3"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "gstreamer"
@@ -325,7 +325,7 @@
 #undef PACKAGE_URL
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.10.25.2"
+#define PACKAGE_VERSION "0.10.25.3"
 
 /* directory where plugins are located */
 #ifdef _DEBUG
@@ -356,7 +356,7 @@
 #undef USE_POISONING
 
 /* Version number of package */
-#define VERSION "0.10.25.2"
+#define VERSION "0.10.25.3"
 
 /* Define WORDS_BIGENDIAN to 1 if your processor stores words with the most
    significant byte first (like Motorola and SPARC, unlike Intel). */
