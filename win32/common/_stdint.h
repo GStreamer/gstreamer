047059e50477ef87ac9c306e10574c70f3d64dca
Tim-Philipp Müller <tim.muller@collabora.co.uk>
2010-02-05 01:18:43 +0000

    0.10.25.3 pre-release

--- win32/common/_stdint.h
+++ win32/common/_stdint.h
@@ -1,8 +1,8 @@
 #ifndef _GST_PLUGINS_BASE__STDINT_H
 #define _GST_PLUGINS_BASE__STDINT_H 1
 #ifndef _GENERATED_STDINT_H
-#define _GENERATED_STDINT_H "gst-plugins-base 0.10.25.2"
-/* generated using gnu compiler gcc (Debian 4.4.2-9) 4.4.3 20100108 (prerelease) */
+#define _GENERATED_STDINT_H "gst-plugins-base 0.10.25.3"
+/* generated using gnu compiler gcc (Debian 4.4.3-1) 4.4.3 */
 #define _STDINT_HAVE_STDINT_H 1
 #include <stdint.h>
 #endif
--- win32/common/config.h
+++ win32/common/config.h
@@ -319,7 +319,7 @@
 #define PACKAGE_NAME "GStreamer Base Plug-ins"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "GStreamer Base Plug-ins 0.10.25.2"
+#define PACKAGE_STRING "GStreamer Base Plug-ins 0.10.25.3"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "gst-plugins-base"
@@ -328,7 +328,7 @@
 #undef PACKAGE_URL
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.10.25.2"
+#define PACKAGE_VERSION "0.10.25.3"
 
 /* directory where plugins are located */
 #ifdef _DEBUG
@@ -356,7 +356,7 @@
 #undef STDC_HEADERS
 
 /* Version number of package */
-#define VERSION "0.10.25.2"
+#define VERSION "0.10.25.3"
 
 /* Define WORDS_BIGENDIAN to 1 if your processor stores words with the most
    significant byte first (like Motorola and SPARC, unlike Intel). */
