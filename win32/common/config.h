b1f1e6717b4ea4a99696e0394da7d2c6531d3468
Tim-Philipp Müller <tim.muller@collabora.co.uk>
2010-02-10 20:17:36 +0000

    Release 0.10.26

--- win32/common/config.h
+++ win32/common/config.h
@@ -66,7 +66,7 @@
 #define GST_LICENSE "LGPL"
 
 /* package name in plugins */
-#define GST_PACKAGE_NAME "GStreamer Base Plug-ins prerelease"
+#define GST_PACKAGE_NAME "GStreamer Base Plug-ins source release"
 
 /* package origin */
 #define GST_PACKAGE_ORIGIN "Unknown package origin"
@@ -319,7 +319,7 @@
 #define PACKAGE_NAME "GStreamer Base Plug-ins"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "GStreamer Base Plug-ins 0.10.25.3"
+#define PACKAGE_STRING "GStreamer Base Plug-ins 0.10.26"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "gst-plugins-base"
@@ -328,7 +328,7 @@
 #undef PACKAGE_URL
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.10.25.3"
+#define PACKAGE_VERSION "0.10.26"
 
 /* directory where plugins are located */
 #ifdef _DEBUG
@@ -356,7 +356,7 @@
 #undef STDC_HEADERS
 
 /* Version number of package */
-#define VERSION "0.10.25.3"
+#define VERSION "0.10.26"
 
 /* Define WORDS_BIGENDIAN to 1 if your processor stores words with the most
    significant byte first (like Motorola and SPARC, unlike Intel). */
