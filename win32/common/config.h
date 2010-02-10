d0ed2b0fd66d38e56c3e3b7313ebcb0d1c6e975d
Tim-Philipp Müller <tim.muller@collabora.co.uk>
2010-02-10 19:17:28 +0000

    Release 0.10.26

--- win32/common/config.h
+++ win32/common/config.h
@@ -50,13 +50,13 @@
 #define GST_MAJORMINOR "0.10"
 
 /* package name in plugins */
-#define GST_PACKAGE_NAME "GStreamer prerelease"
+#define GST_PACKAGE_NAME "GStreamer source release"
 
 /* package origin */
 #define GST_PACKAGE_ORIGIN "Unknown package origin"
 
 /* location of the installed gst-plugin-scanner */
-#undef GST_PLUGIN_SCANNER_INSTALLED
+#define GST_PLUGIN_SCANNER_INSTALLED LIBDIR "\\gst-plugin-scanner"
 
 /* Define to 1 if you have the MacOS X function CFLocaleCopyCurrent in the
    CoreFoundation framework. */
@@ -316,7 +316,7 @@
 #define PACKAGE_NAME "GStreamer"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "GStreamer 0.10.25.3"
+#define PACKAGE_STRING "GStreamer 0.10.26"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "gstreamer"
@@ -325,7 +325,7 @@
 #undef PACKAGE_URL
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.10.25.3"
+#define PACKAGE_VERSION "0.10.26"
 
 /* directory where plugins are located */
 #ifdef _DEBUG
@@ -356,7 +356,7 @@
 #undef USE_POISONING
 
 /* Version number of package */
-#define VERSION "0.10.25.3"
+#define VERSION "0.10.26"
 
 /* Define WORDS_BIGENDIAN to 1 if your processor stores words with the most
    significant byte first (like Motorola and SPARC, unlike Intel). */
