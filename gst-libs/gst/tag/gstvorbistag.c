b330e9aedc8328796f415fcceedbe06664e15a59
Stefan Kost <ensonic@users.sf.net>
2010-02-11 14:09:05 +0200

    comment: fix wrong header comment

--- gst-libs/gst/tag/gstvorbistag.c
+++ gst-libs/gst/tag/gstvorbistag.c
@@ -1,7 +1,7 @@
 /* GStreamer
  * Copyright (C) 2003 Benjamin Otte <in7y118@public.uni-hamburg.de>
  *
- * gstvorbistagsetter.c: plugin for reading / modifying vorbis tags
+ * gstvorbistag.c: library for reading / modifying vorbis tags
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
