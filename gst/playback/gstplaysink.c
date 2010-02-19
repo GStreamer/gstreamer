9fa9834535c0e14e36af0acd0ad63ae3f469d8f4
Sebastian Dröge <sebastian.droege@collabora.co.uk>
2010-02-19 17:44:18 +0100

    playsink: Ghost the video sinkpad if a text sinkpad is available
    Only don't ghost it if no visualizations are need and if
    no text is needed and no textchain was created yet.
    
    Fixes bug #610379.

--- gst/playback/gstplaysink.c
+++ gst/playback/gstplaysink.c
@@ -1876,8 +1876,7 @@ gst_play_sink_reconfigure (GstPlaySink * playsink)
       add_chain (GST_PLAY_CHAIN (playsink->videochain), TRUE);
       activate_chain (GST_PLAY_CHAIN (playsink->videochain), TRUE);
       /* if we are not part of vis or subtitles, set the ghostpad target */
-      if (!need_vis && !need_text && !playsink->text_pad
-          && !playsink->textchain) {
+      if (!need_vis && !need_text && !playsink->textchain) {
         GST_DEBUG_OBJECT (playsink, "ghosting video sinkpad");
         gst_ghost_pad_set_target (GST_GHOST_PAD_CAST (playsink->video_pad),
             playsink->videochain->sinkpad);
