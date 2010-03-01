6da643e07290ca82e27e461aff03e47009994c5c
Benjamin Otte <otte@redhat.com>
2010-03-01 13:49:39 +0100

    apexsink: Use "apexsink" for plugin name

--- ext/apexsink/gstapexplugin.c
+++ ext/apexsink/gstapexplugin.c
@@ -39,6 +39,6 @@ plugin_init (GstPlugin * plugin)
 GST_PLUGIN_DEFINE
     (GST_VERSION_MAJOR,
     GST_VERSION_MINOR,
-    "apex",
+    "apexsink",
     "Apple AirPort Express Plugin",
     plugin_init, VERSION, "LGPL", GST_PACKAGE_NAME, GST_PACKAGE_ORIGIN)
