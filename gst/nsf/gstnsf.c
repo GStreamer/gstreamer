1982ca6e516090ed1bc964402867d40cbcef8345
Benjamin Otte <otte@redhat.com>
2010-03-01 13:18:59 +0100

    nsf: Name the plugin nsf, not nsfdec
    The plugin in gstnsf.so should be named nsf, not nsfdec.

--- gst/nsf/gstnsf.c
+++ gst/nsf/gstnsf.c
@@ -645,6 +645,6 @@ plugin_init (GstPlugin * plugin)
 }
 
 GST_PLUGIN_DEFINE (GST_VERSION_MAJOR, GST_VERSION_MINOR,
-    "nsfdec",
+    "nsf",
     "Uses nosefart to decode .nsf files",
     plugin_init, VERSION, "GPL", GST_PACKAGE_NAME, GST_PACKAGE_ORIGIN);
