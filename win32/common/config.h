051cf9804002f06b50c00edb61242f14fa2f86a9
Tim-Philipp Müller <tim.muller@collabora.co.uk>
2010-03-03 22:52:12 +0000

    0.10.13.4 pre-release

--- win32/common/config.h
+++ win32/common/config.h
@@ -224,7 +224,7 @@
 #define PACKAGE_NAME "GStreamer Ugly Plug-ins"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "GStreamer Ugly Plug-ins 0.10.13.3"
+#define PACKAGE_STRING "GStreamer Ugly Plug-ins 0.10.13.4"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "gst-plugins-ugly"
@@ -233,7 +233,7 @@
 #undef PACKAGE_URL
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.10.13.3"
+#define PACKAGE_VERSION "0.10.13.4"
 
 /* directory where plugins are located */
 #ifdef _DEBUG
@@ -261,7 +261,7 @@
 #undef STDC_HEADERS
 
 /* Version number of package */
-#define VERSION "0.10.13.3"
+#define VERSION "0.10.13.4"
 
 /* Define WORDS_BIGENDIAN to 1 if your processor stores words with the most
    significant byte first (like Motorola and SPARC, unlike Intel). */
