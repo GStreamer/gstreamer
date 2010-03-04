14113e8661e30f865736b222c49497b6df633b66
Wim Taymans <wim.taymans@collabora.co.uk>
2010-03-04 15:21:37 +0100

    element: fix typo in comments

--- gst/gstelement.c
+++ gst/gstelement.c
@@ -1949,7 +1949,7 @@ gst_element_get_state_func (GstElement * element,
   GST_CAT_DEBUG_OBJECT (GST_CAT_STATES, element, "RETURN is %s",
       gst_element_state_change_return_get_name (ret));
 
-  /* we got an error, report immediatly */
+  /* we got an error, report immediately */
   if (ret == GST_STATE_CHANGE_FAILURE)
     goto done;
 
