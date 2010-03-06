43793782392fa32d6894878ac6b7da208f7a5cc0
Tim-Philipp Müller <tim.muller@collabora.co.uk>
2010-03-06 00:43:03 +0000

    Release 0.10.19

--- win32/common/config.h
+++ win32/common/config.h
@@ -40,7 +40,7 @@
 #define GST_LICENSE "LGPL"
 
 /* package name in plugins */
-#define GST_PACKAGE_NAME "GStreamer Good Plug-ins prerelease"
+#define GST_PACKAGE_NAME "GStreamer Good Plug-ins source release"
 
 /* package origin */
 #define GST_PACKAGE_ORIGIN "Unknown package origin"
@@ -359,7 +359,7 @@
 #define PACKAGE_NAME "GStreamer Good Plug-ins"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "GStreamer Good Plug-ins 0.10.18.4"
+#define PACKAGE_STRING "GStreamer Good Plug-ins 0.10.19"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "gst-plugins-good"
@@ -368,7 +368,7 @@
 #undef PACKAGE_URL
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.10.18.4"
+#define PACKAGE_VERSION "0.10.19"
 
 /* directory where plugins are located */
 #ifdef _DEBUG
@@ -399,7 +399,7 @@
 #undef STDC_HEADERS
 
 /* Version number of package */
-#define VERSION "0.10.18.4"
+#define VERSION "0.10.19"
 
 /* old wavpack API */
 #undef WAVPACK_OLD_API
