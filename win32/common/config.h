b74d73980b1bd99fd8ba29fa3789a5911058b598
Tim-Philipp Müller <tim.muller@collabora.co.uk>
2010-03-09 17:32:39 +0000

    Back to development

--- win32/common/config.h
+++ win32/common/config.h
@@ -50,7 +50,7 @@
 #define GST_MAJORMINOR "0.10"
 
 /* package name in plugins */
-#define GST_PACKAGE_NAME "GStreamer source release"
+#define GST_PACKAGE_NAME "GStreamer git"
 
 /* package origin */
 #define GST_PACKAGE_ORIGIN "Unknown package origin"
@@ -316,7 +316,7 @@
 #define PACKAGE_NAME "GStreamer"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "GStreamer 0.10.28"
+#define PACKAGE_STRING "GStreamer 0.10.28.1"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "gstreamer"
@@ -325,7 +325,7 @@
 #undef PACKAGE_URL
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.10.28"
+#define PACKAGE_VERSION "0.10.28.1"
 
 /* directory where plugins are located */
 #ifdef _DEBUG
@@ -356,7 +356,7 @@
 #undef USE_POISONING
 
 /* Version number of package */
-#define VERSION "0.10.28"
+#define VERSION "0.10.28.1"
 
 /* Define WORDS_BIGENDIAN to 1 if your processor stores words with the most
    significant byte first (like Motorola and SPARC, unlike Intel). */
