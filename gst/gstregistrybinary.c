0d4d5a57daaab131c78c1346cf4ce27d5423b46c
Benjamin Otte <otte@redhat.com>
2010-03-10 19:17:42 +0100

    Revert "registry: remove unused function"
    Turns out  the function is not unused, but was in an #ifdef WIN32
    section.
    Whoops.
    
    This reverts commit 57d5db424c68ab5a61f33ce36ce0179eb30251ac.

--- gst/gstregistrybinary.c
+++ gst/gstregistrybinary.c
@@ -106,6 +106,18 @@ gst_registry_binary_cache_init (GstRegistry * registry, const char *location)
   return cache;
 }
 
+static int
+gst_registry_binary_cache_write (BinaryRegistryCache * cache,
+    unsigned long offset, const void *data, int length)
+{
+  cache->len = MAX (offset + length, cache->len);
+  cache->mem = g_realloc (cache->mem, cache->len);
+
+  memcpy (cache->mem + offset, data, length);
+
+  return length;
+}
+
 static gboolean
 gst_registry_binary_cache_finish (BinaryRegistryCache * cache, gboolean success)
 {
