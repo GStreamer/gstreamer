352241b6b2fac2c9b7dc199165b081ef1b357ac0
Stefan Kost <ensonic@users.sf.net>
2010-03-22 17:25:09 +0200

    i18n: fix the build
    Don't inlcude locale.h which we include in gettext.h if needed. Guard the
    inlcude like we do in the simillar headers in core.

--- gst-libs/gst/gst-i18n-plugin.h
+++ gst-libs/gst/gst-i18n-plugin.h
@@ -22,16 +22,24 @@
 #ifndef __GST_I18N_PLUGIN_H__
 #define __GST_I18N_PLUGIN_H__
 
-#include <locale.h>  /* some people need it and some people don't */
-#include "gettext.h" /* included with gettext distribution and copied */
-
 #ifndef GETTEXT_PACKAGE
 #error You must define GETTEXT_PACKAGE before including this header.
 #endif
+
+#ifdef ENABLE_NLS
+
+#include "gettext.h" /* included with gettext distribution and copied */
 
 /* we want to use shorthand _() for translating and N_() for marking */
 #define _(String) dgettext (GETTEXT_PACKAGE, String)
 #define N_(String) gettext_noop (String)
 /* FIXME: if we need it, we can add Q_ as well, like in glib */
+
+#else
+#define _(String) String
+#define N_(String) String
+#define ngettext(Singular,Plural,Count) ((Count>1)?Plural:Singular)
+
+#endif
 
 #endif /* __GST_I18N_PLUGIN_H__ */
