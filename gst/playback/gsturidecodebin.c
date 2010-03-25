133f804d2d5ff1e9c6c58e41849507f39137e489
Wim Taymans <wim.taymans@collabora.co.uk>
2010-03-25 18:45:48 +0100

    uridecodebin: we can handle avi in download mode too
    Add avi to the whitelisted types that can be used for download buffering.

--- gst/playback/gsturidecodebin.c
+++ gst/playback/gsturidecodebin.c
@@ -888,7 +888,7 @@ static const gchar *no_media_mimes[] = {
 
 /* media types we can download */
 static const gchar *download_media[] = {
-  "video/quicktime", "video/x-flv", NULL
+  "video/quicktime", "video/x-flv", "video/x-msvideo", NULL
 };
 
 #define IS_STREAM_URI(uri)          (array_has_uri_value (stream_uris, uri))
