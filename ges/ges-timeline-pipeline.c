2d7962bf939fc5b99bc65bd4214f2ce6db334887
Edward Hervey <edward.hervey@collabora.co.uk>
2010-04-20 12:53:51 +0200

    GESTimelinePipeline: Remove unused code

--- ges/ges-timeline-pipeline.c
+++ ges/ges-timeline-pipeline.c
@@ -47,31 +47,7 @@ G_DEFINE_TYPE (GESTimelinePipeline, ges_timeline_pipeline, GST_TYPE_PIPELINE);
 
 static GstStateChangeReturn ges_timeline_pipeline_change_state (GstElement *
     element, GstStateChange transition);
-static void
-ges_timeline_pipeline_get_property (GObject * object,
-    guint property_id, GValue * value, GParamSpec * pspec)
-{
-  switch (property_id) {
-    default:
-      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
-  }
-}
 
-static void
-ges_timeline_pipeline_set_property (GObject * object, guint property_id,
-    const GValue * value, GParamSpec * pspec)
-{
-  switch (property_id) {
-    default:
-      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
-  }
-}
-
-static void
-ges_timeline_pipeline_dispose (GObject * object)
-{
-  G_OBJECT_CLASS (ges_timeline_pipeline_parent_class)->dispose (object);
-}
 
 static void
 ges_timeline_pipeline_finalize (GObject * object)
@@ -92,9 +68,6 @@ ges_timeline_pipeline_class_init (GESTimelinePipelineClass * klass)
   GObjectClass *object_class = G_OBJECT_CLASS (klass);
   GstElementClass *element_class = GST_ELEMENT_CLASS (klass);
 
-  object_class->get_property = ges_timeline_pipeline_get_property;
-  object_class->set_property = ges_timeline_pipeline_set_property;
-  object_class->dispose = ges_timeline_pipeline_dispose;
   object_class->finalize = ges_timeline_pipeline_finalize;
 
   element_class->change_state =
