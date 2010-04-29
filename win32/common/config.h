5de7036292c8bb2884bdb586529fba73545dcced
Tim-Philipp Müller <tim.muller@collabora.co.uk>
2010-04-29 15:18:07 +0100

    Back to development.

--- win32/common/config.h
+++ win32/common/config.h
@@ -40,7 +40,7 @@
 #define GST_LICENSE "LGPL"
 
 /* package name in plugins */
-#define GST_PACKAGE_NAME "GStreamer Good Plug-ins source release"
+#define GST_PACKAGE_NAME "GStreamer Good Plug-ins git"
 
 /* package origin */
 #define GST_PACKAGE_ORIGIN "Unknown package origin"
@@ -359,7 +359,7 @@
 #define PACKAGE_NAME "GStreamer Good Plug-ins"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "GStreamer Good Plug-ins 0.10.22"
+#define PACKAGE_STRING "GStreamer Good Plug-ins 0.10.22.1"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "gst-plugins-good"
@@ -368,7 +368,7 @@
 #undef PACKAGE_URL
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.10.22"
+#define PACKAGE_VERSION "0.10.22.1"
 
 /* directory where plugins are located */
 #ifdef _DEBUG
@@ -399,7 +399,7 @@
 #undef STDC_HEADERS
 
 /* Version number of package */
-#define VERSION "0.10.22"
+#define VERSION "0.10.22.1"
 
 /* old wavpack API */
 #undef WAVPACK_OLD_API
