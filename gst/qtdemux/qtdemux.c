e84795779078bcbfb6b8531548d85b5baf05cb94
David Schleef <ds@schleef.org>
2010-05-12 18:38:48 -0700

    qtdemux: Add VP8

--- gst/qtdemux/qtdemux.c
+++ gst/qtdemux/qtdemux.c
@@ -7767,6 +7767,10 @@ qtdemux_video_caps (GstQTDemux * qtdemux, QtDemuxStream * stream,
       _codec ("AVID DNxHD");
       caps = gst_caps_from_string ("video/x-dnxhd");
       break;
+    case GST_MAKE_FOURCC ('V', 'P', '8', '0'):
+      _codec ("On2 VP8");
+      caps = gst_caps_from_string ("video/x-vp8");
+      break;
     case GST_MAKE_FOURCC ('k', 'p', 'c', 'd'):
     default:
     {
