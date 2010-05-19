GStreamer/gstreamer

commit 9d7538247ff1bf9841b53eeb71ddc47f2c662415

playbin2: Don't put "raw" subtitle types in the raw caps for decodebin2
We handle them from the autoplug-continue signal, where the caps supported
by the subtitle sink or overlay are known already.

gst/playback/gstplaybin2.c
@@ -559,6 +559,10 @@ static GstElementClass *parent_class;
 
 static guint gst_play_bin_signals[LAST_SIGNAL] = { 0 };
 
+static GstStaticCaps av_raw_caps = GST_STATIC_CAPS ("audio/x-raw-int; "
+    "video/x-raw-float; "
+    "video/x-raw-yuv; " "video/x-raw-rgb; " "video/x-raw-gray;");
+
 #define REMOVE_SIGNAL(obj,id)            \
 if (id) {                                \
   g_signal_handler_disconnect (obj, id); \
@@ -3179,6 +3183,8 @@ activate_group (GstPlayBin * playbin, GstSourceGroup * group, GstState target)
     uridecodebin = gst_element_factory_make ("uridecodebin", NULL);
     if (!uridecodebin)
       goto no_decodebin;
+    g_object_set (uridecodebin, "caps", gst_static_caps_get (&av_raw_caps),
+        NULL);
     gst_bin_add (GST_BIN_CAST (playbin), uridecodebin);
     group->uridecodebin = gst_object_ref (uridecodebin);
   }
@@ -3250,7 +3256,8 @@ activate_group (GstPlayBin * playbin, GstSourceGroup * group, GstState target)
       suburidecodebin = gst_element_factory_make ("uridecodebin", NULL);
       if (!suburidecodebin)
         goto no_decodebin;
-
+      g_object_set (uridecodebin, "caps", gst_static_caps_get (&av_raw_caps),
+          NULL);
       gst_bin_add (GST_BIN_CAST (playbin), suburidecodebin);
       group->suburidecodebin = gst_object_ref (suburidecodebin);
     }

commit a9c6c978b61d61d502180a839cb27f11b1d765ea

playbin2: fix a typo introduced by 9d753824.
video/x-raw-float => audio/x-raw-float. Fixes #619090.

gst/playback/gstplaybin2.c
@@ -560,7 +560,7 @@ static GstElementClass *parent_class;
 static guint gst_play_bin_signals[LAST_SIGNAL] = { 0 };
 
 static GstStaticCaps av_raw_caps = GST_STATIC_CAPS ("audio/x-raw-int; "
-    "video/x-raw-float; "
+    "audio/x-raw-float; "
     "video/x-raw-yuv; " "video/x-raw-rgb; " "video/x-raw-gray;");
 
 #define REMOVE_SIGNAL(obj,id)            \