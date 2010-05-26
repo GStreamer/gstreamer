64a01683adbf86364feb8fe54c6138a330b197e4
Tim-Philipp Müller <tim.muller@collabora.co.uk>
2010-05-26 08:54:33 +0100

    0.10.22.3 pre-release

--- win32/common/config.h
+++ win32/common/config.h
@@ -368,7 +368,7 @@
 #define PACKAGE_NAME "GStreamer Good Plug-ins"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "GStreamer Good Plug-ins 0.10.22.2"
+#define PACKAGE_STRING "GStreamer Good Plug-ins 0.10.22.3"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "gst-plugins-good"
@@ -377,7 +377,7 @@
 #undef PACKAGE_URL
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.10.22.2"
+#define PACKAGE_VERSION "0.10.22.3"
 
 /* directory where plugins are located */
 #ifdef _DEBUG
@@ -408,7 +408,7 @@
 #undef STDC_HEADERS
 
 /* Version number of package */
-#define VERSION "0.10.22.2"
+#define VERSION "0.10.22.3"
 
 /* old wavpack API */
 #undef WAVPACK_OLD_API
