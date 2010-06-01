b6655a48b8aff80c9b8ba21491e9ca1572411f54
Tim-Philipp Müller <tim.muller@collabora.co.uk>
2010-06-01 10:06:10 +0100

    win32: add more generated marshal and enumtype files to win32-update

--- win32/common/gstudp-enumtypes.c
+++ win32/common/gstudp-enumtypes.c
@@ -1,15 +1,16 @@
 
 /* Generated data (by glib-mkenums) */
 
-#include <gstudp.h>
+#include "gstudp-enumtypes.h"
+
+#include "gstudp.h"
 
 /* enumerations from "gstudp.h" */
 GType
 gst_udp_control_get_type (void)
 {
-  static GType etype = 0;
-
-  if (etype == 0) {
+  static volatile gsize g_define_type_id__volatile = 0;
+  if (g_once_init_enter (&g_define_type_id__volatile)) {
     static const GEnumValue values[] = {
       {CONTROL_ZERO, "CONTROL_ZERO", "zero"},
       {CONTROL_NONE, "CONTROL_NONE", "none"},
@@ -17,9 +18,10 @@ gst_udp_control_get_type (void)
       {CONTROL_TCP, "CONTROL_TCP", "tcp"},
       {0, NULL, NULL}
     };
-    etype = g_enum_register_static ("GstUDPControl", values);
+    GType g_define_type_id = g_enum_register_static ("GstUDPControl", values);
+    g_once_init_leave (&g_define_type_id__volatile, g_define_type_id);
   }
-  return etype;
+  return g_define_type_id__volatile;
 }
 
 /* Generated data ends here */
