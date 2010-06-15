0dac3747859ca8dcd3190b25db515b2993065acf
Brandon Lewis <brandon@collabora.co.uk>
2010-06-15 17:09:31 +0200

    these should have been checked in before

---
+++ ges/ges-track-video-title-source.c
@@ -0,0 +1,175 @@
+/* GStreamer Editing Services
+ * Copyright (C) 2010 Brandon Lewis <brandon.lewis@collabora.co.uk>
+ *               2010 Nokia Corporation
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+/**
+ * SECTION:ges-track-source
+ * @short_description: Base Class for single-media sources
+ */
+
+#include "ges-internal.h"
+#include "ges-track-object.h"
+#include "ges-track-video-title-source.h"
+
+G_DEFINE_TYPE (GESTrackVideoTitleSource, ges_track_video_title_src,
+    GES_TYPE_TRACK_TITLE_SOURCE);
+
+enum
+{
+  PROP_0,
+};
+
+static void ges_track_video_title_src_dispose (GObject * object);
+
+static void ges_track_video_title_src_finalize (GObject * object);
+
+static void ges_track_video_title_src_get_property (GObject * object, guint
+    property_id, GValue * value, GParamSpec * pspec);
+
+static void ges_track_video_title_src_set_property (GObject * object, guint
+    property_id, const GValue * value, GParamSpec * pspec);
+
+static GstElement *ges_track_video_title_src_create_element (GESTrackTitleSource
+    * self);
+
+static void
+ges_track_video_title_src_class_init (GESTrackVideoTitleSourceClass * klass)
+{
+  GObjectClass *object_class;
+  GESTrackTitleSourceClass *bg_class;
+
+  object_class = G_OBJECT_CLASS (klass);
+  bg_class = GES_TRACK_TITLE_SOURCE_CLASS (klass);
+
+  object_class->get_property = ges_track_video_title_src_get_property;
+  object_class->set_property = ges_track_video_title_src_set_property;
+  object_class->dispose = ges_track_video_title_src_dispose;
+  object_class->finalize = ges_track_video_title_src_finalize;
+
+  bg_class->create_element = ges_track_video_title_src_create_element;
+}
+
+static void
+ges_track_video_title_src_init (GESTrackVideoTitleSource * self)
+{
+  self->text = NULL;
+  self->text_el = NULL;
+  self->background_el = NULL;
+}
+
+static void
+ges_track_video_title_src_dispose (GObject * object)
+{
+  GESTrackVideoTitleSource *self = GES_TRACK_VIDEO_TITLE_SOURCE (object);
+  if (self->text) {
+    g_free (self->text);
+  }
+
+  if (self->text_el) {
+    g_object_unref (self->text_el);
+    self->text_el = NULL;
+  }
+
+  if (self->text_el) {
+    g_object_unref (self->text_el);
+    self->background_el = NULL;
+  }
+
+  G_OBJECT_CLASS (ges_track_video_title_src_parent_class)->dispose (object);
+}
+
+static void
+ges_track_video_title_src_finalize (GObject * object)
+{
+  G_OBJECT_CLASS (ges_track_video_title_src_parent_class)->finalize (object);
+}
+
+static void
+ges_track_video_title_src_get_property (GObject * object,
+    guint property_id, GValue * value, GParamSpec * pspec)
+{
+  switch (property_id) {
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+  }
+}
+
+static void
+ges_track_video_title_src_set_property (GObject * object,
+    guint property_id, const GValue * value, GParamSpec * pspec)
+{
+  switch (property_id) {
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+  }
+}
+
+static GstElement *
+ges_track_video_title_src_create_element (GESTrackTitleSource * object)
+{
+  GESTrackVideoTitleSource *self = GES_TRACK_VIDEO_TITLE_SOURCE (object);
+  GstElement *topbin, *background, *text;
+  GstPad *src;
+
+  topbin = gst_bin_new ("titlesrc-bin");
+  background = gst_element_factory_make ("videotestsrc", "titlesrc-bg");
+  text = gst_element_factory_make ("textoverlay", "titlsrc-text");
+  if (self->text) {
+    g_object_set (text, "text", self->text, NULL);
+  }
+
+  gst_bin_add_many (GST_BIN (topbin), background, text, NULL);
+
+  gst_element_link (background, text);
+
+  src = gst_ghost_pad_new ("src", gst_element_get_static_pad (text, "src"));
+  gst_element_add_pad (topbin, src);
+
+  g_object_ref (text);
+  g_object_ref (background);
+
+  self->text_el = text;
+  self->background_el = background;
+
+  return topbin;
+}
+
+/**
+ * ges_track_video_title_source_set_text:
+ * @self: the #GESTrackVideoTitleSource* to set text on
+ * @text: the text to render. an internal copy of this text will be
+ * made.
+ * 
+ * Sets the text this track object will render.
+ *
+ */
+
+void
+ges_track_video_title_source_set_text (GESTrackVideoTitleSource * self,
+    const gchar * text)
+{
+  self->text = g_strdup (text);
+  g_object_set (self->text_el, "text", text, NULL);
+}
+
+GESTrackVideoTitleSource *
+ges_track_video_title_source_new (void)
+{
+  return g_object_new (GES_TYPE_TRACK_VIDEO_TITLE_SOURCE, NULL);
+}
---
+++ ges/ges-track-video-title-source.h
@@ -0,0 +1,80 @@
+/* GStreamer Editing Services
+ * Copyright (C) 2010 Brandon Lewis <brandon.lewis@collabora.co.uk>
+ *               2010 Nokia Corporation
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#ifndef _GES_TRACK_VIDEO_TITLE_SOURCE
+#define _GES_TRACK_VIDEO_TITLE_SOURCE
+
+#include <glib-object.h>
+#include <ges/ges-types.h>
+#include <ges/ges-track-title-source.h>
+
+G_BEGIN_DECLS
+
+#define GES_TYPE_TRACK_VIDEO_TITLE_SOURCE ges_track_video_title_src_get_type()
+
+#define GES_TRACK_VIDEO_TITLE_SOURCE(obj) \
+  (G_TYPE_CHECK_INSTANCE_CAST ((obj), GES_TYPE_TRACK_VIDEO_TITLE_SOURCE, GESTrackVideoTitleSource))
+
+#define GES_TRACK_VIDEO_TITLE_SOURCE_CLASS(klass) \
+  (G_TYPE_CHECK_CLASS_CAST ((klass), GES_TYPE_TRACK_VIDEO_TITLE_SOURCE, GESTrackVideoTitleSourceClass))
+
+#define GES_IS_TRACK_VIDEO_TITLE_SOURCE(obj) \
+  (G_TYPE_CHECK_INSTANCE_TYPE ((obj), GES_TYPE_TRACK_VIDEO_TITLE_SOURCE))
+
+#define GES_IS_TRACK_VIDEO_TITLE_SOURCE_CLASS(klass) \
+  (G_TYPE_CHECK_CLASS_TYPE ((klass), GES_TYPE_TRACK_VIDEO_TITLE_SOURCE))
+
+#define GES_TRACK_VIDEO_TITLE_SOURCE_GET_CLASS(obj) \
+  (G_TYPE_INSTANCE_GET_CLASS ((obj), GES_TYPE_TRACK_VIDEO_TITLE_SOURCE, GESTrackVideoTitleSourceClass))
+
+/** 
+ * GESTrackVideoTitleSource:
+ * @parent: parent
+ *
+ */
+struct _GESTrackVideoTitleSource {
+  GESTrackTitleSource parent;
+
+  /*< private >*/
+  gchar         *text;
+  GstElement    *text_el;
+  GstElement    *background_el;
+};
+
+/**
+ * GESTrackVideoTitleSourceClass:
+ * @parent_class: parent class
+ */
+
+struct _GESTrackVideoTitleSourceClass {
+  GESTrackTitleSourceClass parent_class;
+
+  /*< private >*/
+};
+
+GType ges_track_video_title_src_get_type (void);
+
+void ges_track_video_title_source_set_text(GESTrackVideoTitleSource *, const gchar *);
+GESTrackVideoTitleSource* ges_track_video_title_source_new (void);
+
+G_END_DECLS
+
+#endif /* _GES_TRACK_VIDEO_TITLE_SOURCE */
+
