aef6123172fe74d9cb44f0cd635eb47e28e93cc5
Stefan Kost <ensonic@users.sf.net>
2010-06-18 16:37:14 +0300

    tests: use our own macros for the tests main function

--- tests/check/elements/vorbisdec.c
+++ tests/check/elements/vorbisdec.c
@@ -336,19 +336,4 @@ vorbisdec_suite (void)
   return s;
 }
 
-int
-main (int argc, char **argv)
-{
-  int nf;
-
-  Suite *s = vorbisdec_suite ();
-  SRunner *sr = srunner_create (s);
-
-  gst_check_init (&argc, &argv);
-
-  srunner_run_all (sr, CK_NORMAL);
-  nf = srunner_ntests_failed (sr);
-  srunner_free (sr);
-
-  return nf;
-}
+GST_CHECK_MAIN (vorbisdec);
