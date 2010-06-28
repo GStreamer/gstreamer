9300c5b29b4a78cda99effc4b675b224994be4cb
Tim-Philipp Müller <tim.muller@collabora.co.uk>
2010-06-28 15:28:59 +0100

    Bump automake requirement to 1.10
    For maintainability reasons and $(builddir).
    
    Fixes #622944.

--- autogen.sh
+++ autogen.sh
@@ -39,8 +39,8 @@ printf "+ check for build tools"
 if test ! -z "$NOCHECK"; then echo ": skipped version checks"; else  echo; fi
 version_check "autoconf" "$AUTOCONF autoconf autoconf270 autoconf269 autoconf268 autoconf267 autoconf266 autoconf265 autoconf264 autoconf263 autoconf262 autoconf261 autoconf260" \
               "ftp://ftp.gnu.org/pub/gnu/autoconf/" 2 60 || DIE=1
-version_check "automake" "$AUTOMAKE automake automake-1.11 automake-1.10 automake-1.9 automake19 automake-1.8 automake18 automake-1.7 automake17 automake-1.6 automake16" \
-              "ftp://ftp.gnu.org/pub/gnu/automake/" 1 7 || DIE=1
+version_check "automake" "$AUTOMAKE automake automake-1.11 automake-1.10" \
+              "ftp://ftp.gnu.org/pub/gnu/automake/" 1 10 || DIE=1
 version_check "autopoint" "autopoint" \
               "ftp://ftp.gnu.org/pub/gnu/gettext/" 0 17 || DIE=1
 version_check "libtoolize" "libtoolize libtoolize15 glibtoolize" \
@@ -50,7 +50,6 @@ version_check "pkg-config" "" \
 
 die_check $DIE
 
-autoconf_2_52d_check || DIE=1
 aclocal_check || DIE=1
 autoheader_check || DIE=1
 
