d774b460770d9433a483367da59d9241e085991b
Sebastian Dröge <sebastian.droege@collabora.co.uk>
2010-07-04 17:26:03 +0200

    ffmpegcolorspace: Use correct Y offset for the YUY2 -> RGB conversions
    Fixes bug #623530.

--- gst/ffmpegcolorspace/imgconvert_template.h
+++ gst/ffmpegcolorspace/imgconvert_template.h
@@ -80,7 +80,7 @@ static void glue (yuv422_to_, RGB_NAME)(AVPicture *dst, const AVPicture *src,
             RGB_OUT(d1, r, g, b);
             d1 += BPP;
 
-            YUV_TO_RGB2_CCIR(r, g, b, s1[1]);
+            YUV_TO_RGB2_CCIR(r, g, b, s1[2]);
             RGB_OUT(d1, r, g, b);
             d1 += BPP;
 
