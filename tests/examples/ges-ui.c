6c3e0b3f342fff93781aca8d611aef3697fa747b
Brandon Lewis <brandon@collabora.co.uk>
2010-07-20 17:05:26 +0200

    add protos so it builds under c90 mode

--- tests/examples/ges-ui.c
+++ tests/examples/ges-ui.c
@@ -1,4 +1,16 @@
 #include <gtk/gtk.h>
+#include <glib.h>
+#include <ges/ges.h>
+
+void window_destroy_cb (GtkObject * window, gpointer user);
+
+void quit_item_activate_cb (GtkMenuItem * item, gpointer user);
+
+void delete_item_activate_cb (GtkMenuItem * item, gpointer user);
+
+void add_file_item_activate_cb (GtkMenuItem * item, gpointer user);
+
+GtkWidget *create_ui (void);
 
 void
 window_destroy_cb (GtkObject * window, gpointer user)
@@ -24,12 +36,11 @@ add_file_item_activate_cb (GtkMenuItem * item, gpointer user)
   g_print ("add file");
 }
 
-int
-main (int argc, char *argv[])
+GtkWidget *
+create_ui (void)
 {
   GtkBuilder *builder;
   GtkWidget *window;
-  gtk_init (&argc, &argv);
 
   builder = gtk_builder_new ();
   gtk_builder_add_from_file (builder, "ges-ui.glade", NULL);
@@ -38,6 +49,26 @@ main (int argc, char *argv[])
   g_object_unref (G_OBJECT (builder));
 
   gtk_widget_show (window);
+  return window;
+}
+
+int
+main (int argc, char *argv[])
+{
+  GtkWidget *main_window;
+
+  /* intialize GStreamer and GES */
+  if (!g_thread_supported ())
+    g_thread_init (NULL);
+
+  gst_init (&argc, &argv);
+  ges_init ();
+
+  /* initialize UI */
+  gtk_init (&argc, &argv);
+
+  main_window = create_ui ();
+
   gtk_main ();
   return 0;
 }
