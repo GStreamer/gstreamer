79c343badee269afc423a2c08e99de97eeeb1c0b
Filippo Argiolas <filippo.argiolas@gmail.com>
2010-08-01 23:03:24 +0200

    coloreffects: new plugin for lookup table color mapping
    Implements a color lookup table filter with 4 presets:
     - heat: fake heat camera effect
     - sepia: sepia toning
     - xray: invert + shade to blue
     - xpro: cross process
    
    https://bugzilla.gnome.org/show_bug.cgi?id=625817

---
+++ gst/coloreffects/gstplugin.c
@@ -0,0 +1,62 @@
+/* GStreamer
+ * Copyright (C) <2010> Filippo Argiolas <filippo.argiolas@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#include <gst/gst.h>
+
+#include "gstcoloreffects.h"
+
+#ifndef PACKAGE
+#define PACKAGE "coloreffects"
+#endif
+
+struct _elements_entry
+{
+  const gchar *name;
+    GType (*type) (void);
+};
+
+static const struct _elements_entry _elements[] = {
+  {"coloreffects", gst_color_effects_get_type},
+  {NULL, 0},
+};
+
+static gboolean
+plugin_init (GstPlugin * plugin)
+{
+  gint i = 0;
+
+  while (_elements[i].name) {
+    if (!gst_element_register (plugin, _elements[i].name,
+            GST_RANK_NONE, (_elements[i].type) ()))
+      return FALSE;
+    i++;
+  }
+
+  return TRUE;
+}
+
+GST_PLUGIN_DEFINE (GST_VERSION_MAJOR,
+    GST_VERSION_MINOR,
+    "coloreffects",
+    "Color Look-up Table filters",
+    plugin_init, VERSION, "LGPL", "GStreamer", "http://gstreamer.net/")
