3b91ea7c8130d681a8bb46af63afb11f31cdc55b
Stefan Kost <ensonic@users.sf.net>
2010-08-30 16:45:48 +0300

    qtdemux: use GST_BOILERPLATE macro

--- gst/qtdemux/qtdemux.c
+++ gst/qtdemux/qtdemux.c
@@ -364,11 +364,8 @@ GST_STATIC_PAD_TEMPLATE ("subtitle_%02d",
     GST_PAD_SOMETIMES,
     GST_STATIC_CAPS_ANY);
 
-static GstElementClass *parent_class = NULL;
+GST_BOILERPLATE (GstQTDemux, gst_qtdemux, GstQTDemux, GST_TYPE_ELEMENT);
 
-static void gst_qtdemux_class_init (GstQTDemuxClass * klass);
-static void gst_qtdemux_base_init (GstQTDemuxClass * klass);
-static void gst_qtdemux_init (GstQTDemux * quicktime_demux);
 static void gst_qtdemux_dispose (GObject * object);
 
 static void gst_qtdemux_set_index (GstElement * element, GstIndex * index);
@@ -403,29 +400,9 @@ static GstCaps *qtdemux_sub_caps (GstQTDemux * qtdemux,
 static gboolean qtdemux_parse_samples (GstQTDemux * qtdemux,
     QtDemuxStream * stream, guint32 n);
 
-GType
-gst_qtdemux_get_type (void)
-{
-  static GType qtdemux_type = 0;
-
-  if (G_UNLIKELY (!qtdemux_type)) {
-    static const GTypeInfo qtdemux_info = {
-      sizeof (GstQTDemuxClass),
-      (GBaseInitFunc) gst_qtdemux_base_init, NULL,
-      (GClassInitFunc) gst_qtdemux_class_init,
-      NULL, NULL, sizeof (GstQTDemux), 0,
-      (GInstanceInitFunc) gst_qtdemux_init,
-    };
-
-    qtdemux_type =
-        g_type_register_static (GST_TYPE_ELEMENT, "GstQTDemux", &qtdemux_info,
-        0);
-  }
-  return qtdemux_type;
-}
 
 static void
-gst_qtdemux_base_init (GstQTDemuxClass * klass)
+gst_qtdemux_base_init (gpointer klass)
 {
   GstElementClass *element_class = GST_ELEMENT_CLASS (klass);
 
@@ -465,7 +442,7 @@ gst_qtdemux_class_init (GstQTDemuxClass * klass)
 }
 
 static void
-gst_qtdemux_init (GstQTDemux * qtdemux)
+gst_qtdemux_init (GstQTDemux * qtdemux, GstQTDemuxClass * klass)
 {
   qtdemux->sinkpad =
       gst_pad_new_from_static_template (&gst_qtdemux_sink_template, "sink");
