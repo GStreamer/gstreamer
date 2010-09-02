08fd7198984eecedd3b24e8b915e6aff0f8552e0
Thibault Saunier <tsaunier@gnome.org>
2010-09-02 14:34:50 +0200

    alphacolor: Fix classification
    This is no effect but a converter. Fixes bug #628608.

--- gst/alpha/gstalphacolor.c
+++ gst/alpha/gstalphacolor.c
@@ -78,7 +78,7 @@ gst_alpha_color_base_init (gpointer g_class)
   GstElementClass *element_class = GST_ELEMENT_CLASS (g_class);
 
   gst_element_class_set_details_simple (element_class, "Alpha color filter",
-      "Filter/Effect/Video",
+      "Filter/Converter/Video",
       "ARGB from/to AYUV colorspace conversion preserving the alpha channel",
       "Wim Taymans <wim@fluendo.com>");
 
