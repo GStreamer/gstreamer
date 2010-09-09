9b85b13d80b0510da641f56257d6f4960a94f183
Sebastian Dröge <sebastian.droege@collabora.co.uk>
2010-09-09 11:12:56 +0200

    adder: Do debug category initialization in plugin_init again

--- gst/adder/gstadder.c
+++ gst/adder/gstadder.c
@@ -1291,6 +1291,9 @@ gst_adder_change_state (GstElement * element, GstStateChange transition)
 static gboolean
 plugin_init (GstPlugin * plugin)
 {
+  GST_DEBUG_CATEGORY_INIT (GST_CAT_DEFAULT, "adder", 0,
+      "audio channel mixing element");
+
   gst_adder_orc_init ();
 
   if (!gst_element_register (plugin, "adder", GST_RANK_NONE, GST_TYPE_ADDER)) {
