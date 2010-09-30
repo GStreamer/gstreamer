9c429de37a5d7c4a5a278a55f3fac9d5a7b1a9e1
Thijs Vermeir <thijsvermeir@gmail.com>
2010-09-30 11:34:56 +0200

    examples: fix indentation on rtp client example

--- tests/examples/rtp/client-H264.sh
+++ tests/examples/rtp/client-H264.sh
@@ -29,8 +29,8 @@ VIDEO_CAPS="application/x-rtp,media=(string)video,clock-rate=(int)90000,encoding
 # from another machine, change this address.
 DEST=127.0.0.1
 
-gst-launch -v gstrtpbin name=rtpbin                                                \
-	   udpsrc caps=$VIDEO_CAPS port=5000 ! rtpbin.recv_rtp_sink_0              \
-	         rtpbin. ! rtph264depay ! ffdec_h264 ! xvimagesink                 \
-           udpsrc port=5001 ! rtpbin.recv_rtcp_sink_0                              \
-         rtpbin.send_rtcp_src_0 ! udpsink port=5005 host=$DEST sync=false async=false
+gst-launch -v gstrtpbin name=rtpbin                                                 \
+    udpsrc caps=$VIDEO_CAPS port=5000 ! rtpbin.recv_rtp_sink_0                      \
+      rtpbin. ! rtph264depay ! ffdec_h264 ! xvimagesink                             \
+    udpsrc port=5001 ! rtpbin.recv_rtcp_sink_0                                      \
+      rtpbin.send_rtcp_src_0 ! udpsink port=5005 host=$DEST sync=false async=false
