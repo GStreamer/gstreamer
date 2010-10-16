9d662a51d25d60980980ba8af2d65b51646381f6
Stefan Sauer <ensonic@users.sf.net>
2010-10-16 23:56:55 +0300

    build: tweak autogen.sh
    Generate autoregen.sh, run configure.

--- mediainfo/autogen.sh
+++ mediainfo/autogen.sh
@@ -1,1 +1,16 @@
-AUTOPOINT='intltoolize --automake --copy' autoreconf -fiv
+#!/bin/sh
+# Run this to generate all the initial makefiles, etc.
+
+# a silly hack that generates autoregen.sh but it's handy
+echo "#!/bin/sh" > autoregen.sh
+echo "./autogen.sh $@ \$@" >> autoregen.sh
+chmod +x autoregen.sh
+
+test -n "$srcdir" || srcdir=$(dirname "$0")
+test -n "$srcdir" || srcdit=.
+(
+  cd "$srcdir" &&
+  AUTOPOINT='intltoolize --automake --copy' autoreconf -fiv
+) || exit
+test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
+
