9c06f55e19cf9e4b3181ee14e01cff0096fe759e
Edward Hervey <bilboed@bilboed.com>
2010-10-24 16:17:09 +0200

    pbutils: Description for RealAudio container format

--- gst-libs/gst/pbutils/descriptions.c
+++ gst-libs/gst/pbutils/descriptions.c
@@ -70,6 +70,7 @@ static const FormatInfo formats[] = {
   {"application/x-apetag", N_("APE tag"), FLAG_CONTAINER},
   {"application/x-icy", N_("ICY internet radio"), FLAG_CONTAINER},
   {"application/x-3gp", "3GP", FLAG_CONTAINER},
+  {"application/x-pn-realaudio", "RealAudio", FLAG_CONTAINER},
   {"application/x-yuv4mpeg", "Y4M", FLAG_CONTAINER},
   {"multipart/x-mixed-replace", "Multipart", FLAG_CONTAINER},
   {"video/x-fli", "FLI/FLC/FLX Animation", FLAG_CONTAINER},
