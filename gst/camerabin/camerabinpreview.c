d96964e391b26eb72164a90c340374c8600bef15
Thiago Santos <thiago.sousa.santos@collabora.co.uk>
2010-11-02 10:24:49 -0300

    camerabin: Add missing change to previous commits
    Forgot to add this change to the latest commits. This fixes the build.

--- gst/camerabin/camerabinpreview.c
+++ gst/camerabin/camerabinpreview.c
@@ -229,7 +229,7 @@ gst_camerabin_preview_convert (GstCameraBin * camera,
   bus = gst_element_get_bus (pipeline);
   msg = gst_bus_timed_pop_filtered (bus, (25 * GST_SECOND),
       GST_MESSAGE_ERROR | GST_MESSAGE_EOS);
-  gst_bus_unref (bus);
+  gst_object_unref (bus);
 
   if (msg) {
     switch (GST_MESSAGE_TYPE (msg)) {
