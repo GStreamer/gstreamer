84406fde30d1e4a1833ada49b39c3c150ded652b
David Schleef <ds@schleef.org>
2010-12-15 15:58:48 -0800

    y4mdec: Fix copyright

--- gst/y4m/gsty4mdec.h
+++ gst/y4m/gsty4mdec.h
@@ -1,5 +1,5 @@
 /* GStreamer
- * Copyright (C) 2010 REAL_NAME <EMAIL_ADDRESS>
+ * Copyright (C) 2010 David Schleef <ds@schleef.org>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
