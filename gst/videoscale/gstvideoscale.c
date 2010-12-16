32f28f476e2e950fc528bb37ca9fb541b83039b5
Sebastian Dröge <sebastian.droege@collabora.co.uk>
2010-12-16 10:11:43 +0100

    videoscale: Change classification to Filter/Converter/Video/Scaler

--- gst/videoscale/gstvideoscale.c
+++ gst/videoscale/gstvideoscale.c
@@ -219,7 +219,7 @@ gst_video_scale_base_init (gpointer g_class)
   GstElementClass *element_class = GST_ELEMENT_CLASS (g_class);
 
   gst_element_class_set_details_simple (element_class,
-      "Video scaler", "Filter/Effect/Video",
+      "Video scaler", "Filter/Converter/Video/Scaler",
       "Resizes video", "Wim Taymans <wim.taymans@chello.be>");
 
   gst_element_class_add_pad_template (element_class,
