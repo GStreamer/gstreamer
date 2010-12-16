b8ca37d70d334ca69b4da6331b8e851dc7a93a1a
Stefan Sauer <ensonic@users.sf.net>
2010-12-16 11:42:34 +0200

    autogen.sh: fix silly typo

--- mediainfo/autogen.sh
+++ mediainfo/autogen.sh
@@ -7,7 +7,7 @@ echo "./autogen.sh $@ \$@" >> autoregen.sh
 chmod +x autoregen.sh
 
 test -n "$srcdir" || srcdir=$(dirname "$0")
-test -n "$srcdir" || srcdit=.
+test -n "$srcdir" || srcdir=.
 (
   cd "$srcdir" &&
   AUTOPOINT='intltoolize --automake --copy' autoreconf -fiv
