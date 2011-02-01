1ee6a359492ee5665a2488ebb9f4b5f4d4d5e954
Fabrizio (Misto) Milo <mistobaan@gmail.com>
2011-02-01 10:23:04 -0800

    dvbsrc: spell-check

--- sys/dvb/gstdvbsrc.c
+++ sys/dvb/gstdvbsrc.c
@@ -878,7 +878,7 @@ gst_dvbsrc_finalize (GObject * _object)
 /*
  ******************************
  *                            *
- *      Plugin Realisation    *
+ *      Plugin Realization    *
  *                            *
  ******************************
  */
