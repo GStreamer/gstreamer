74ff936752fdb8715bcafb7560726fbe4d0438ae
Tim-Philipp Müller <tim.muller@collabora.co.uk>
2011-02-03 10:28:01 +0000

    Revert "info: use the publicly visible address to fix the tests"
    This reverts commit eb56687a6dfd207507a4ca000eae53f93b5e33ea.
    
    While this commit may have fixed a problem on one of the build bots,
    it didn't actually fix the original bug reported for win32.
    
    Also, it causes other problems, such as the lookup failing when
    called from C++ code (gst-phonon, amarok).
    
    This needs to be fixed differently.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=640771
    https://bugzilla.gnome.org/show_bug.cgi?id=625396

--- gst/gstinfo.c
+++ gst/gstinfo.c
@@ -351,16 +351,7 @@ _gst_debug_init (void)
   _GST_CAT_DEBUG = _gst_debug_category_new ("GST_DEBUG",
       GST_DEBUG_BOLD | GST_DEBUG_FG_YELLOW, "debugging subsystem");
 
-  /* we need to use the publicly visible address, so that one could remove it by
-   * the same address */
-  {
-    GModule *module = g_module_open (NULL, 0);
-    gpointer ptr;
-
-    g_module_symbol (module, "gst_debug_log_default", &ptr);
-    gst_debug_add_log_function (ptr, NULL);
-    g_module_close (module);
-  }
+  gst_debug_add_log_function (gst_debug_log_default, NULL);
 
   /* FIXME: add descriptions here */
   GST_CAT_GST_INIT = _gst_debug_category_new ("GST_INIT",
