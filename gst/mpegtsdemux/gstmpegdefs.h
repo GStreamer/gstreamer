535bf57d1f76fc1c73cde5af636a78b78b950733
Janne Grunau <janne.grunau@collabora.co.uk>
2011-02-10 12:35:47 +0100

    mpegtsdemux: relicense gstmpegdefs.h, gstmpegdesc.h and gstmpegdesc.c to LGPL only
    with permission from the license header:
    """
    This library is licensed under 2 different licenses and you
    can choose to use it under the terms of either one of them. The
    two licenses are the MPL 1.1 and the LGPL.
    """

--- gst/mpegtsdemux/gstmpegdefs.h
+++ gst/mpegtsdemux/gstmpegdefs.h
@@ -1,21 +1,4 @@
 /*
- * This library is licensed under 2 different licenses and you
- * can choose to use it under the terms of either one of them. The
- * two licenses are the MPL 1.1 and the LGPL.
- *
- * MPL:
- *
- * The contents of this file are subject to the Mozilla Public License
- * Version 1.1 (the "License"); you may not use this file except in
- * compliance with the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/.
- *
- * Software distributed under the License is distributed on an "AS IS"
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
- * License for the specific language governing rights and limitations
- * under the License.
- *
- * LGPL:
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
--- gst/mpegtsdemux/gstmpegdesc.h
+++ gst/mpegtsdemux/gstmpegdesc.h
@@ -1,13 +1,19 @@
 /* 
- * The contents of this file are subject to the Mozilla Public License
- * Version 1.1 (the "License"); you may not use this file except in
- * compliance with the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/.
  *
- * Software distributed under the License is distributed on an "AS IS"
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
- * License for the specific language governing rights and limitations
- * under the License.
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
  *
  * The Original Code is Fluendo MPEG Demuxer plugin.
  *
@@ -17,16 +23,6 @@
  *
  * Contributor(s): Wim Taymans <wim@fluendo.com>
  *
- * Alternatively, the contents of this file may be used under the terms of
- * the GNU Lesser General Public License Version 2 or later (the "LGPL"),
- * in which case the provisions of the LGPL are applicable instead
- * of those above. If you wish to allow use of your version of this file only
- * under the terms of the LGPL, and not to allow others to
- * use your version of this file under the terms of the MPL, indicate your
- * decision by deleting the provisions above and replace them with the notice
- * and other provisions required by the LGPL. If you do not delete
- * the provisions above, a recipient may use your version of this file under
- * the terms of the MPL or the LGPL.
  */
 
 #ifndef __GST_MPEG_DESC_H__
