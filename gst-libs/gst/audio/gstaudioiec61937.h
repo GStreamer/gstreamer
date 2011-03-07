8ff93a6a3d24957dc23a4216c88d5c997fa6711d
Arun Raghavan <arun.raghavan@collabora.co.uk>
2011-03-07 20:49:16 +0530

    audio: Add an IEC 61937 payloading library
    This can be used by sinks to take compressed formats, correctly payload
    these in IEC 61937 frames and feed these to sinks that support
    passthrough output over IEC 60958 (S/PDIF) or, in the case of MP3, over
    Bluetooth.
    
    Initial implementation includes AC3, E-AC3, MPEG-1, MPEG-2 (non-AAC),
    and DTS (type-I/II/II) payloading. More formats can be added as needed.
    
    API: gst_audio_iec61937_frame_size()
    API: gst_audio_iec61937_payload()
    
    https://bugzilla.gnome.org/show_bug.cgi?id=642730

---
+++ gst-libs/gst/audio/gstaudioiec61937.h
@@ -0,0 +1,38 @@
+/* GStreamer audio helper functions for IEC 61937 payloading
+ * (c) 2011 Intel Corporation
+ *     2011 Collabora Multimedia
+ *     2011 Arun Raghavan <arun.raghavan@collabora.co.uk>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+/**
+ * SECTION:gstaudioiec61937
+ * @short_description: Utility functions for IEC 61937 payloading
+ *
+ * This module contains some helper functions for encapsulating various
+ * audio formats in IEC 61937 headers and padding.
+ */
+
+#ifndef __GST_AUDIO_IEC61937_H__
+#define __GST_AUDIO_IEC61937_H__
+
+#include <gst/audio/gstringbuffer.h>
+
+guint gst_audio_iec61937_frame_size (const GstRingBufferSpec * spec);
+gboolean gst_audio_iec61937_payload (const guint8 * src, guint src_n,
+    guint8 * dst, guint dst_n, const GstRingBufferSpec * spec);
+
+#endif /* __GST_AUDIO_IEC61937_H__ */
