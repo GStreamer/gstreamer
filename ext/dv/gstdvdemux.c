9e44fff3dbcc9c75e048f9554216f0f0501e7b91
Sebastian Dröge <sebastian.droege@collabora.co.uk>
2011-03-11 13:47:26 +0100

    dvdemux: Chain up to the parent class' ::send_event for non-seek events

--- ext/dv/gstdvdemux.c
+++ ext/dv/gstdvdemux.c
@@ -1161,6 +1161,7 @@ gst_dvdemux_send_event (GstElement * element, GstEvent * event)
       break;
     }
     default:
+      res = GST_ELEMENT_CLASS (parent_class)->send_event (element, event);
       break;
   }
 
