e44456b481e86f71661b31a11e4f1829caaff14f
Tim-Philipp Müller <tim.muller@collabora.co.uk>
2011-04-07 14:43:42 +0100

    audioparsers: hook up to build

--- gst/audioparsers/plugin.c
+++ gst/audioparsers/plugin.c
@@ -52,6 +52,6 @@ plugin_init (GstPlugin * plugin)
 
 GST_PLUGIN_DEFINE (GST_VERSION_MAJOR,
     GST_VERSION_MINOR,
-    "audioparsersbad",
     "audioparsers",
+    "Parsers for various audio formats",
     plugin_init, VERSION, "LGPL", GST_PACKAGE_NAME, GST_PACKAGE_ORIGIN);
