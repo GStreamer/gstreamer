1e6a607e0189e56ae9295691932fbaa475ac7cb6
Thibault Saunier <thibault.saunier@collabora.co.uk>
2011-04-10 18:24:22 -0400

    android: make it ready for androgenizer
    To build gstreamer for android we are now using androgenizer which
    generates the needed Android.mk files.
    
    Androgenizer can be found here: http://git.collabora.co.uk/?p=user/derek/androgenizer.git

--- Android.mk
+++ Android.mk
@@ -1,12 +1,52 @@
 LOCAL_PATH := $(call my-dir)
-
-GSTREAMER_TOP := $(LOCAL_PATH)
-
 include $(CLEAR_VARS)
-#dependancy library 
-#include $(GSTREAMER_TOP)/android/amrnb_library.mk
-#plugins
-include $(GSTREAMER_TOP)/android/mpegaudioparse.mk
-include $(GSTREAMER_TOP)/android/asf.mk
-#include $(GSTREAMER_TOP)/android/amrnb.mk
-#include $(GSTREAMER_TOP)/android/amrwbdec.mk
+
+GST_PLUGINS_UGLY_TOP := $(LOCAL_PATH)
+
+GST_PLUGINS_UGLY_BUILT_SOURCES = \
+	ext/mad/Android.mk \
+	gst/synaesthesia/Android.mk \
+	gst/iec958/Android.mk \
+	gst/mpegaudioparse/Android.mk \
+	gst/mpegstream/Android.mk \
+	gst/realmedia/Android.mk \
+	gst/dvdsub/Android.mk \
+	gst/dvdlpcmdec/Android.mk \
+	gst/asfdemux/Android.mk \
+	ext/x264/Android.mk
+
+GST_PLUGINS_UGLY_BUILT_SOURCES := $(patsubst %, $(abspath $(GST_PLUGINS_UGLY_TOP))/%, $(GST_PLUGINS_UGLY_BUILT_SOURCES))
+
+.PHONY: gst-plugins-ugly-configure gst-plugins-ugly-configure-real
+
+gst-plugins-ugly-configure-real:
+	cd $(GST_PLUGINS_UGLY_TOP) ; \
+	CC="$(CONFIGURE_CC)" \
+	CFLAGS="$(CONFIGURE_CFLAGS)" \
+	LD=$(TARGET_LD) \
+	LDFLAGS="$(CONFIGURE_LDFLAGS)" \
+	CPP=$(CONFIGURE_CPP) \
+	CPPFLAGS="$(CONFIGURE_CPPFLAGS)" \
+	PKG_CONFIG_LIBDIR="$(CONFIGURE_PKG_CONFIG_LIBDIR)" \
+	PKG_CONFIG_TOP_BUILD_DIR=/ \
+	$(abspath $(GST_PLUGINS_UGLY_TOP))/$(CONFIGURE) --host=arm-linux-androideabi \
+	--prefix=/system --disable-orc --disable-valgrind --disable-gtk-doc && \
+	for file in $(GST_PLUGINS_UGLY_BUILT_SOURCES); do \
+		rm -f $$file && \
+		make -C $$(dirname $$file) $$(basename $$file) ; \
+	done
+
+gst-plugins-ugly-configure: gst-plugins-ugly-configure-real
+
+CONFIGURE_TARGETS += gst-plugins-ugly-configure
+
+-include $(GST_PLUGINS_UGLY_TOP)/ext/mad/Android.mk
+-include $(GST_PLUGINS_UGLY_TOP)/ext/x264/Android.mk
+-include $(GST_PLUGINS_UGLY_TOP)/gst/synaesthesia/Android.mk
+-include $(GST_PLUGINS_UGLY_TOP)/gst/iec958/Android.mk
+-include $(GST_PLUGINS_UGLY_TOP)/gst/mpegaudioparse/Android.mk
+-include $(GST_PLUGINS_UGLY_TOP)/gst/mpegstream/Android.mk
+-include $(GST_PLUGINS_UGLY_TOP)/gst/realmedia/Android.mk
+-include $(GST_PLUGINS_UGLY_TOP)/gst/dvdsub/Android.mk
+-include $(GST_PLUGINS_UGLY_TOP)/gst/dvdlpcmdec/Android.mk
+-include $(GST_PLUGINS_UGLY_TOP)/gst/asfdemux/Android.mk
