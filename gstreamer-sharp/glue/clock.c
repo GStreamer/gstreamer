e67cf80368a41f8807fd054f671b393cae3bac69
Sebastian Dröge <sebastian.droege@collabora.co.uk>
2011-04-13 17:33:05 +0200

    The GMutex and GCond API uses macros to function tables instead of real function
    We need to add real functions for these macros. Fixes bug #647529.

--- gstreamer-sharp/glue/clock.c
+++ gstreamer-sharp/glue/clock.c
@@ -23,3 +23,40 @@ gstsharp_gst_clock_get_times_offset (void)
 {
   return (guint) G_STRUCT_OFFSET (GstClock, times);
 }
+
+void
+gstsharp_g_cond_wait (GCond * cond, GMutex * mutex)
+{
+  g_cond_wait (cond, mutex);
+}
+
+gboolean
+gstsharp_g_cond_timed_wait (GCond *cond, GMutex *mutex, GTimeVal *abs_time)
+{
+  return g_cond_timed_wait (cond, mutex, abs_time);
+}
+
+void
+gstsharp_g_cond_broadcast (GCond *cond)
+{
+  g_cond_broadcast (cond);
+}
+
+void
+gstsharp_g_mutex_lock (GMutex *mutex)
+{
+  g_mutex_lock (mutex);
+}
+
+void
+gstsharp_g_mutex_unlock (GMutex *mutex)
+{
+  g_mutex_unlock (mutex);
+}
+
+gboolean
+gstsharp_g_mutex_trylock (GMutex *mutex)
+{
+  return g_mutex_trylock (mutex);
+}
+
