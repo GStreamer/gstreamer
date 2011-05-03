cd5fc6ed8df339682744baf9b94aea0599140e4d
Sjoerd Simons <sjoerd.simons@collabora.co.uk>
2011-05-03 14:57:16 +0100

    x264enc: get_caps function should return a copy of the caps

--- ext/x264/gstx264enc.c
+++ ext/x264/gstx264enc.c
@@ -1466,7 +1466,7 @@ gst_x264_enc_sink_get_caps (GstPad * pad)
 
   /* If we already have caps return them */
   if (GST_PAD_CAPS (pad))
-    return GST_PAD_CAPS (pad);
+    return gst_caps_copy (GST_PAD_CAPS (pad));
 
   encoder = GST_X264_ENC (gst_pad_get_parent (pad));
   if (!encoder)
