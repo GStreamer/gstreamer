e5fc7a9f13a032c714fc3ad105ddf352accb82c3
Marc-André Lureau <marcandre.lureau@gmail.com>
2011-05-14 14:48:56 +0200

    gst/dccp: fix build on newer mingw
    Fix this build error:
      CC     libgstdccp_la-gstdccpplugin.lo
    In file included from ../../../gst/dccp/gstdccpclientsrc.h:29:0,
                     from ../../../gst/dccp/gstdccpplugin.c:24:
    ../../../gst/dccp/gstdccp_common.h:32:0: warning: WINVER redefined [enabled by default]
    /usr/i686-w64-mingw32/sys-root/mingw/include/_mingw.h:231:0: note: this is the location of the previous definition
    In file included from ../../../gst/dccp/gstdccpplugin.c:24:0:
    ../../../gst/dccp/gstdccpclientsrc.h:58:3: error: unknown type name 'uint8_t'
    In file included from ../../../gst/dccp/gstdccpplugin.c:25:0:
    ../../../gst/dccp/gstdccpserversink.h:74:3: error: unknown type name 'uint8_t'
    In file included from ../../../gst/dccp/gstdccpplugin.c:26:0:
    ../../../gst/dccp/gstdccpclientsink.h:67:3: error: unknown type name 'uint8_t'
    In file included from ../../../gst/dccp/gstdccpplugin.c:27:0:
    ../../../gst/dccp/gstdccpserversrc.h:58:3: error: unknown type name 'uint8_t'
    make: *** [libgstdccp_la-gstdccpplugin.lo] Error 1
    
    https://bugzilla.gnome.org/show_bug.cgi?id=650171

--- gst/dccp/gstdccp_common.h
+++ gst/dccp/gstdccp_common.h
@@ -29,7 +29,9 @@
 #else
 /* ws2_32.dll has getaddrinfo and freeaddrinfo on Windows XP and later.
  * minwg32 headers check WINVER before allowing the use of these */
+#ifndef WINVER
 #  define WINVER 0x0501
+#endif
 #  include <winsock2.h>
 #  include <ws2tcpip.h>
 #ifndef socklen_t
@@ -37,6 +39,7 @@
 #endif
 #endif
 #include <sys/types.h>
+#include <_stdint.h>
 #include <unistd.h>
 #include <string.h>
 
