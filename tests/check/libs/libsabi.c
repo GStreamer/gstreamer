fa298d13ebdc0876413915888b44069c758ca505
Edward Hervey <edward.hervey@collabora.co.uk>
2011-06-01 17:24:30 +0200

    libsabi: Add structure sizes for arm

--- tests/check/libs/libsabi.c
+++ tests/check/libs/libsabi.c
@@ -93,9 +93,14 @@
 #include "struct_x86_64.h"
 #define HAVE_ABI_SIZES TRUE
 #else
+#ifdef HAVE_CPU_ARM
+#include "struct_arm.h"
+#define HAVE_ABI_SIZES TRUE
+#else
 /* in case someone wants to generate a new arch */
 #include "struct_i386.h"
 #define HAVE_ABI_SIZES FALSE
+#endif
 #endif
 #endif
 
