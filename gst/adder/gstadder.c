5f3ab36dcd91e5f409db9faed613a91f27bef3b2
David Schleef <ds@schleef.org>
2011-06-04 13:36:55 -0700

    adder: Work around changes in g_atomic API
    See #651514 for details.

--- gst/adder/gstadder.c
+++ gst/adder/gstadder.c
@@ -943,7 +943,11 @@ gst_adder_request_new_pad (GstElement * element, GstPadTemplate * templ,
   adder = GST_ADDER (element);
 
   /* increment pad counter */
+#if GLIB_CHECK_VERSION(2,29,5)
+  padcount = g_atomic_int_add (&adder->padcount, 1);
+#else
   padcount = g_atomic_int_exchange_and_add (&adder->padcount, 1);
+#endif
 
   name = g_strdup_printf ("sink%d", padcount);
   newpad = gst_pad_new_from_template (templ, name);
