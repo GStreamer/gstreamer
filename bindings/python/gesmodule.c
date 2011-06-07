66efde4cc5ca4a0036205585f00377b9845345c4
Thibault Saunier <thibault.saunier@collabora.com>
2011-06-07 19:17:10 -0400

    pyges: Add the necessary file to compile the bindings
    defs files have been generated with the h2defs.py script

---
+++ bindings/python/gesmodule.c
@@ -0,0 +1,34 @@
+/* -*- Mode: C; c-basic-offset: 4 -*- */
+#ifdef HAVE_CONFIG_H
+#  include "config.h"
+#endif
+#include <Python.h>
+#include <pygobject.h>
+#include <pyglib.h>
+
+/* include any extra headers needed here */
+
+void pyges_register_classes (PyObject * d);
+extern PyMethodDef pyges_functions[];
+DL_EXPORT (void)
+initges (void);
+
+DL_EXPORT (void)
+initges (void)
+{
+  PyObject *m, *d;
+
+  /* perform any initialisation required by the library here */
+
+  m = Py_InitModule ("ges", pyges_functions);
+  d = PyModule_GetDict (m);
+
+  init_pygobject ();
+
+  pyges_register_classes (d);
+
+  /* add anything else to the module dictionary (such as constants) */
+
+  if (PyErr_Occurred ())
+    Py_FatalError ("could not initialise module ges");
+}
