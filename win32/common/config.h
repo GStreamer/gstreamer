673d519898d18c513c3b5eeecd91f5d3091ddf79
Tim-Philipp Müller <tim.muller@collabora.co.uk>
2011-06-15 23:57:34 +0100

    Release 0.10.30
    This is an ad-hoc release that is almost identical to 0.10.29:
    
     * work around GLib atomic ops API change
     * better handling of malformed buffers in RTP depayloders
     * some minor compilation fixes

--- win32/common/config.h
+++ win32/common/config.h
@@ -49,7 +49,7 @@
 #define GST_PACKAGE_ORIGIN "Unknown package origin"
 
 /* GStreamer package release date/time for plugins as YYYY-MM-DD */
-#define GST_PACKAGE_RELEASE_DATETIME "2011-05-10"
+#define GST_PACKAGE_RELEASE_DATETIME "2011-06-15"
 
 /* struct v4l2_buffer missing */
 #undef GST_V4L2_MISSING_BUFDECL
@@ -396,7 +396,7 @@
 #define PACKAGE_NAME "GStreamer Good Plug-ins"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "GStreamer Good Plug-ins 0.10.29"
+#define PACKAGE_STRING "GStreamer Good Plug-ins 0.10.30"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "gst-plugins-good"
@@ -405,7 +405,7 @@
 #undef PACKAGE_URL
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.10.29"
+#define PACKAGE_VERSION "0.10.30"
 
 /* directory where plugins are located */
 #ifdef _DEBUG
@@ -436,7 +436,7 @@
 #undef STDC_HEADERS
 
 /* Version number of package */
-#define VERSION "0.10.29"
+#define VERSION "0.10.30"
 
 /* old wavpack API */
 #undef WAVPACK_OLD_API
