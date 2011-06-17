95fe6ca01e7a1bb7b889b794dd4f64fedcf26139
Raimo Järvi <raimo.jarvi@gmail.com>
2011-06-17 14:05:58 +0300

    codecmap: Don't create dummy extradata for AAC LATM
    If extradata is created for LATM, libav will use it for initialising AAC
    decoder and decoding will fail.
    
    Fixes bug #652812.

--- ext/ffmpeg/gstffmpegcodecmap.c
+++ ext/ffmpeg/gstffmpegcodecmap.c
@@ -2387,7 +2387,7 @@ gst_ffmpeg_caps_with_codecid (enum CodecID codec_id,
     }
 
     GST_DEBUG ("have codec data of size %d", size);
-  } else if (context->extradata == NULL) {
+  } else if (context->extradata == NULL && codec_id != CODEC_ID_AAC_LATM) {
     /* no extradata, alloc dummy with 0 sized, some codecs insist on reading
      * extradata anyway which makes then segfault. */
     context->extradata =
