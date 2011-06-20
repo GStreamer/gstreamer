458d163ccb71b82d5deb4734539b623d616b95ec
Wim Taymans <wim.taymans@collabora.co.uk>
2011-06-20 12:13:11 +0200

    avimux: use string for video format now

--- gst/avi/gstavimux.c
+++ gst/avi/gstavimux.c
@@ -92,7 +92,7 @@ static GstStaticPadTemplate video_sink_factory =
     GST_PAD_SINK,
     GST_PAD_REQUEST,
     GST_STATIC_CAPS ("video/x-raw, "
-        "format = (fourcc) { YUY2, I420 }, "
+        "format = (string) { YUY2, I420 }, "
         "width = (int) [ 16, 4096 ], "
         "height = (int) [ 16, 4096 ], "
         "framerate = (fraction) [ 0, MAX ]; "
