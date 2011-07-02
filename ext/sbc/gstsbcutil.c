b31ee7bffc36010f1cb6fb3a5bd9d95f9837e967
Lucas De Marchi <lucas.demarchi@profusion.mobi>
2011-07-02 12:32:40 -0300

    sbc: Fix common misspelled words
    Accounting of misspelled words, as detected by codespell:
    
    acording      2
    ancilliary    1
    appropiate    1
    atribute      1
    cant          1
    comming       2
    gracefull     1
    lenght        1
    mispelled     1
    occured       1
    occurences    1
    ocurred       3
    prefered      1
    presense      1
    reponse       1
    seperate      1
    succesful     1
    successully   1
    sucessfull    1
    sucessfully   1

--- ext/sbc/gstsbcutil.c
+++ ext/sbc/gstsbcutil.c
@@ -329,7 +329,7 @@ gst_sbc_parse_caps_from_sbc (sbc_t * sbc)
 }
 
 /*
- * Given a GstCaps, this will return a fixed GstCaps on sucessfull conversion.
+ * Given a GstCaps, this will return a fixed GstCaps on successful conversion.
  * If an error occurs, it will return NULL and error_message will contain the
  * error message.
  *
