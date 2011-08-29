e62d326dc2608615d3fef18e0bcdccad495fc5a1
Wim Taymans <wim.taymans@collabora.co.uk>
2011-08-29 19:10:35 +0200

    goom: fix comment

--- gst/goom/gstgoom.c
+++ gst/goom/gstgoom.c
@@ -29,7 +29,7 @@
  * <refsect2>
  * <title>Example launch line</title>
  * |[
- * gst-launch -v audiotestsrc ! goom ! ffmpegcolorspace ! xvimagesink
+ * gst-launch -v audiotestsrc ! goom ! videoconvert ! xvimagesink
  * ]|
  * </refsect2>
  */
