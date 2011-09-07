c29069fd11fa62e95827aea431df2cf0d9976ec3
Sebastian Dröge <sebastian.droege@collabora.co.uk>
2011-09-07 12:11:39 +0200

    ac3parse: Add Converter to the classification because it can convert between different alignments
    This allows decodebin2 to let it negotiate properly.

--- gst/audioparsers/gstac3parse.c
+++ gst/audioparsers/gstac3parse.c
@@ -179,7 +179,7 @@ gst_ac3_parse_base_init (gpointer klass)
       gst_static_pad_template_get (&src_template));
 
   gst_element_class_set_details_simple (element_class,
-      "AC3 audio stream parser", "Codec/Parser/Audio",
+      "AC3 audio stream parser", "Codec/Parser/Converter/Audio",
       "AC3 parser", "Tim-Philipp Müller <tim centricular net>");
 }
 
