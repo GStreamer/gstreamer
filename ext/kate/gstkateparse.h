e21c4abc5046f00360a420d94d4283d2c207e2bc
Stefan Sauer <ensonic@users.sf.net>
2011-09-07 15:37:53 +0200

    kateparse: fix crap in object macros

--- ext/kate/gstkateparse.h
+++ ext/kate/gstkateparse.h
@@ -36,7 +36,7 @@ G_BEGIN_DECLS
   (G_TYPE_CHECK_CLASS_CAST((klass),GST_TYPE_KATE_PARSE,GstKateParseClass))
 #define GST_IS_KATE_PARSE(obj) \
   (G_TYPE_CHECK_INSTANCE_TYPE((obj),GST_TYPE_KATE_PARSE))
-#define GST_IS_KATKATEE_CLASS(klass) \
+#define GST_IS_KATE_PARSE_CLASS(klass) \
   (G_TYPE_CHECK_CLASS_TYPE((klass),GST_TYPE_KATE_PARSE))
 typedef struct _GstKateParse GstKateParse;
 typedef struct _GstKateParseClass GstKateParseClass;
