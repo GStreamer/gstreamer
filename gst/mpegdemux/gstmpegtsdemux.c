38cdb54112734ff0fd0f894324d9469e0b04ab3e
Vincent Penquerc'h <vincent.penquerch@collabora.co.uk>
2011-09-26 11:44:02 +0100

    mpegtsdemux: keep CRC table in .rodata
    https://bugzilla.gnome.org/show_bug.cgi?id=660118

--- gst/mpegdemux/gstmpegtsdemux.c
+++ gst/mpegdemux/gstmpegtsdemux.c
@@ -417,7 +417,7 @@ gst_mpegts_demux_remove_pads (GstMpegTSDemux * demux)
 #endif
 
 
-static guint32 crc_tab[256] = {
+static const guint32 crc_tab[256] = {
   0x00000000, 0x04c11db7, 0x09823b6e, 0x0d4326d9, 0x130476dc, 0x17c56b6b,
   0x1a864db2, 0x1e475005, 0x2608edb8, 0x22c9f00f, 0x2f8ad6d6, 0x2b4bcb61,
   0x350c9b64, 0x31cd86d3, 0x3c8ea00a, 0x384fbdbd, 0x4c11db70, 0x48d0c6c7,
