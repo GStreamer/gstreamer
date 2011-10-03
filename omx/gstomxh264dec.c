37967b4316170436c47f735433aa2a9caecb85ba
Sebastian Dröge <sebastian.droege@collabora.co.uk>
2011-10-03 09:12:44 +0200

    omxh264dec: Require stream-format=byte-stream
    Other stream-formats are unlikely to be supported by OMX components.

--- omx/gstomxh264dec.c
+++ omx/gstomxh264dec.c
@@ -82,7 +82,7 @@ gst_omx_h264_dec_class_init (GstOMXH264DecClass * klass)
 
   videodec_class->default_sink_template_caps = "video/x-h264, "
       "parsed=(boolean) true, "
-      "alignment=(string)au, " "stream-format=(string) avc";
+      "alignment=(string)au, " "stream-format=(string) byte-stream";
 }
 
 static void
