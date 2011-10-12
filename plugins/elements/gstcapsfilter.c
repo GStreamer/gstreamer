fdd115d8c313e51b8e75df82fda6b42f34611c86
René Stadler <rene.stadler@collabora.co.uk>
2011-10-12 14:34:24 +0200

    capsfilter: fix caps leak
    gst_event_new_caps does not steal a reference to the caps.

--- plugins/elements/gstcapsfilter.c
+++ plugins/elements/gstcapsfilter.c
@@ -355,8 +355,7 @@ gst_capsfilter_prepare_buf (GstBaseTransform * trans, GstBuffer * input,
 
       if (!gst_pad_has_current_caps (trans->srcpad))
         gst_pad_push_event (trans->srcpad, gst_event_new_caps (out_caps));
-      else
-        gst_caps_unref (out_caps);
+      gst_caps_unref (out_caps);
     } else {
       gchar *caps_str = gst_caps_to_string (out_caps);
 
