9c103f5dc936f09f6cb348a2e048ceb4219b09f5
Edward Hervey <edward@collabora.com>
2011-11-10 11:42:10 +0100

    tests: Remove old structures from struct_arm.h

--- tests/check/libs/struct_arm.h
+++ tests/check/libs/struct_arm.h
@@ -1,7 +1,5 @@
 
 GstCheckABIStruct list[] = {
-  {"GstAppBufferClass", sizeof (GstAppBufferClass), 16},
-  {"GstAppBuffer", sizeof (GstAppBuffer), 96},
   {"GstAppSinkCallbacks", sizeof (GstAppSinkCallbacks), 28},
   {"GstAppSinkClass", sizeof (GstAppSinkClass), 404},
   {"GstAppSink", sizeof (GstAppSink), 432},
