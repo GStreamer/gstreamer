3b28af5171ecdeb4f830944c8e534cd7a88c1868
Tim-Philipp Müller <tim.muller@collabora.co.uk>
2011-11-28 01:10:20 +0000

    queue2: fix refactoring of draining-on-eos, munge flow return to FLOW_OK

--- plugins/elements/gstqueue2.c
+++ plugins/elements/gstqueue2.c
@@ -2448,6 +2448,9 @@ next:
       data = gst_queue2_dequeue_on_unexpected (queue, &item_type);
       if (data != NULL)
         goto next;
+      /* Since we will still accept EOS and NEWSEGMENT we return _FLOW_OK
+       * to the caller so that the task function does not shut down */
+      result = GST_FLOW_OK;
     }
   } else if (item_type == GST_QUEUE2_ITEM_TYPE_EVENT) {
     GstEvent *event = GST_EVENT_CAST (data);
@@ -2486,6 +2489,9 @@ next:
       data = gst_queue2_dequeue_on_unexpected (queue, &item_type);
       if (data != NULL)
         goto next;
+      /* Since we will still accept EOS and NEWSEGMENT we return _FLOW_OK
+       * to the caller so that the task function does not shut down */
+      result = GST_FLOW_OK;
     }
   }
   return result;
