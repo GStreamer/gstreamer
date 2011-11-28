b8431a03a7e1388028b03f01678657bee8b36bc1
Vincent Penquerc'h <vincent.penquerch@collabora.co.uk>
2011-11-28 13:08:27 +0000

    various: fix pad template ref leaks
    https://bugzilla.gnome.org/show_bug.cgi?id=662664

--- ext/opus/gstopusdec.c
+++ ext/opus/gstopusdec.c
@@ -103,10 +103,10 @@ gst_opus_dec_base_init (gpointer g_class)
 {
   GstElementClass *element_class = GST_ELEMENT_CLASS (g_class);
 
-  gst_element_class_add_pad_template (element_class,
-      gst_static_pad_template_get (&opus_dec_src_factory));
-  gst_element_class_add_pad_template (element_class,
-      gst_static_pad_template_get (&opus_dec_sink_factory));
+  gst_element_class_add_static_pad_template (element_class,
+      &opus_dec_src_factory);
+  gst_element_class_add_static_pad_template (element_class,
+      &opus_dec_sink_factory);
   gst_element_class_set_details_simple (element_class, "Opus audio decoder",
       "Codec/Decoder/Audio",
       "decode opus streams to audio",
