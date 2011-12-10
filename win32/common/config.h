43083f5a9447c8b1ca31fb1fd0940e422fdbda83
Tim-Philipp Müller <tim.muller@collabora.co.uk>
2011-12-10 15:23:26 +0000

    0.10.18.2 pre-release

--- win32/common/config.h
+++ win32/common/config.h
@@ -46,13 +46,13 @@
 #define GST_LICENSE "LGPL"
 
 /* package name in plugins */
-#define GST_PACKAGE_NAME "GStreamer Ugly Plug-ins git"
+#define GST_PACKAGE_NAME "GStreamer Ugly Plug-ins prerelease"
 
 /* package origin */
 #define GST_PACKAGE_ORIGIN "Unknown package origin"
 
 /* GStreamer package release date/time for plugins as YYYY-MM-DD */
-#define GST_PACKAGE_RELEASE_DATETIME "2011-05-14T09:08Z"
+#define GST_PACKAGE_RELEASE_DATETIME "2011-12-10T15:16Z"
 
 /* Define to enable a52dec (used by a52dec). */
 #undef HAVE_A52DEC
@@ -147,9 +147,6 @@
 /* Define if you have the iconv() function and it works. */
 #undef HAVE_ICONV
 
-/* defined if libid3tag is available */
-#undef HAVE_ID3TAG
-
 /* Define to 1 if you have the <inttypes.h> header file. */
 #undef HAVE_INTTYPES_H
 
@@ -236,7 +233,7 @@
 #define PACKAGE_NAME "GStreamer Ugly Plug-ins"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "GStreamer Ugly Plug-ins 0.10.18.1"
+#define PACKAGE_STRING "GStreamer Ugly Plug-ins 0.10.18.2"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "gst-plugins-ugly"
@@ -245,7 +242,7 @@
 #undef PACKAGE_URL
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.10.18.1"
+#define PACKAGE_VERSION "0.10.18.2"
 
 /* directory where plugins are located */
 #ifdef _DEBUG
@@ -273,7 +270,7 @@
 #undef STDC_HEADERS
 
 /* Version number of package */
-#define VERSION "0.10.18.1"
+#define VERSION "0.10.18.2"
 
 /* Define WORDS_BIGENDIAN to 1 if your processor stores words with the most
    significant byte first (like Motorola and SPARC, unlike Intel). */
