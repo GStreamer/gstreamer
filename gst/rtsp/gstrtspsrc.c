b8b8454bcb2aba9a1e5477763b7841705df55052
Tim-Philipp Müller <tim.muller@collabora.co.uk>
2011-12-12 02:52:13 +0000

    Suppress deprecation warnings in selected files, for g_static_rec_mutex_* mostly
    GStaticRecMutex is part of our API/ABI, not much we can do here
    in 0.10 for most of these.

--- gst/rtsp/gstrtspsrc.c
+++ gst/rtsp/gstrtspsrc.c
@@ -81,6 +81,10 @@
 #include "config.h"
 #endif
 
+/* FIXME 0.11: suppress warnings for deprecated API such as GStaticRecMutex
+ * with newer GLib versions (>= 2.31.0) */
+#define GLIB_DISABLE_DEPRECATION_WARNINGS
+
 #ifdef HAVE_UNISTD_H
 #include <unistd.h>
 #endif /* HAVE_UNISTD_H */
