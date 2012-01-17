59e08fa503a0914b04a470ddad120da88ca2d514
Sebastian Dröge <sebastian.droege@collabora.co.uk>
2012-01-17 16:53:31 +0100

    configure: Remove socket/winsock specific checks
    Not necessary anymore.

--- gst/rtp/gstasteriskh263.c
+++ gst/rtp/gstasteriskh263.c
@@ -26,16 +26,6 @@
 #include <gst/rtp/gstrtpbuffer.h>
 #include "gstasteriskh263.h"
 
-/* Cygwin has both netinet/in.h and winsock2.h, but
- * only one can be included, so prefer the unix one */
-#ifdef HAVE_NETINET_IN_H
-#  include <netinet/in.h>
-#else
-#ifdef HAVE_WINSOCK2_H
-#  include <winsock2.h>
-#endif
-#endif
-
 #define GST_ASTERISKH263_HEADER_LEN 6
 
 typedef struct _GstAsteriskH263Header
