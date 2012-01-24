29623239b9a0eda74a102e603c4375d8961a9fc2
Gwenole Beauchesne <gwenole.beauchesne@intel.com>
2012-01-24 10:06:37 +0100

    Add template for workarounds.

---
+++ gst-libs/gst/vaapi/gstvaapiworkarounds.h
@@ -0,0 +1,29 @@
+/*
+ *  gstvaapiworkaround.h - GStreamer/VA workarounds
+ *
+ *  Copyright (C) 2011-2012 Intel Corporation
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2.1
+ *  of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free
+ *  Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ *  Boston, MA 02110-1301 USA
+ */
+
+#ifndef GST_VAAPI_WORKAROUNDS_H
+#define GST_VAAPI_WORKAROUNDS_H
+
+G_BEGIN_DECLS
+
+G_END_DECLS
+
+#endif /* GST_VAAPI_WORKAROUNDS_H */
