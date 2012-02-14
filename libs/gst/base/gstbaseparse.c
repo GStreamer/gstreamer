36cc9efa80d2803659b831cbb0e297c695854d37
Mark Nauwelaerts <mark.nauwelaerts@collabora.co.uk>
2012-02-14 20:17:37 +0100

    baseparse: Revert "baseparse: really provide upstream ts to subclass"
    This reverts commit 2363490ef5a9fe8d414369d24fcaa65a9dfa83ac.

--- libs/gst/base/gstbaseparse.c
+++ libs/gst/base/gstbaseparse.c
@@ -2632,7 +2632,6 @@ gst_base_parse_chain (GstPad * pad, GstObject * parent, GstBuffer * buffer)
     gst_buffer_take_memory (tmpbuf, -1,
         gst_memory_new_wrapped (GST_MEMORY_FLAG_READONLY |
             GST_MEMORY_FLAG_NO_SHARE, (gpointer) data, NULL, av, 0, av));
-    GST_BUFFER_TIMESTAMP (tmpbuf) = timestamp;
 
     /* keep the adapter mapped, so keep track of what has to be flushed */
     ret = gst_base_parse_handle_buffer (parse, tmpbuf, &skip, &flush);
