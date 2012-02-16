adc947e127d4335a842ad7b8c640e1df4fd802c5
Thibault Saunier <thibault.saunier@collabora.com>
2012-02-16 09:54:24 -0300

    tsdemux: fix glib deprecation

--- gst/mpegtsdemux/mpegtspacketizer.c
+++ gst/mpegtsdemux/mpegtspacketizer.c
@@ -24,6 +24,11 @@
 
 #include <string.h>
 
+/* FIXME 0.11: suppress warnings for deprecated API such as GValueArray
+ * with newer GLib versions (>= 2.31.0) */
+#define GLIB_DISABLE_DEPRECATION_WARNINGS
+
+
 #include "mpegtspacketizer.h"
 #include "gstmpegdesc.h"
 
