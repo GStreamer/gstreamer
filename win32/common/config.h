b99e43406e948150ab5d0d8c40d953286e96787d
Wim Taymans <wim.taymans@collabora.co.uk>
2012-02-18 00:02:20 +0100

    win32: back to development

--- win32/common/config.h
+++ win32/common/config.h
@@ -52,13 +52,13 @@
 #define GST_LICENSE "LGPL"
 
 /* package name in plugins */
-#define GST_PACKAGE_NAME "GStreamer Ugly Plug-ins source release"
+#define GST_PACKAGE_NAME "GStreamer Ugly Plug-ins git"
 
 /* package origin */
 #define GST_PACKAGE_ORIGIN "Unknown package origin"
 
 /* GStreamer package release date/time for plugins as YYYY-MM-DD */
-#define GST_PACKAGE_RELEASE_DATETIME "2012-02-16"
+#define GST_PACKAGE_RELEASE_DATETIME "2012-02-17T22:58Z"
 
 /* Define to enable a52dec (used by a52dec). */
 #undef HAVE_A52DEC
@@ -239,7 +239,7 @@
 #define PACKAGE_NAME "GStreamer Ugly Plug-ins"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "GStreamer Ugly Plug-ins 0.11.1"
+#define PACKAGE_STRING "GStreamer Ugly Plug-ins 0.11.1.1"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "gst-plugins-ugly"
@@ -248,7 +248,7 @@
 #undef PACKAGE_URL
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.11.1"
+#define PACKAGE_VERSION "0.11.1.1"
 
 /* directory where plugins are located */
 #ifdef _DEBUG
@@ -276,7 +276,7 @@
 #undef STDC_HEADERS
 
 /* Version number of package */
-#define VERSION "0.11.1"
+#define VERSION "0.11.1.1"
 
 /* Define WORDS_BIGENDIAN to 1 if your processor stores words with the most
    significant byte first (like Motorola and SPARC, unlike Intel). */
