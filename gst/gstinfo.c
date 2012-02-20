3eeb6e6c05a2af5181a3190cf915f0aae726ec9c
Wim Taymans <wim.taymans@collabora.co.uk>
2012-02-20 14:22:00 +0100

    info: debug segment duration as well

--- gst/gstinfo.c
+++ gst/gstinfo.c
@@ -726,11 +726,11 @@ gst_debug_print_segment (gpointer ptr)
       return g_strdup_printf ("time segment start=%" GST_TIME_FORMAT
           ", stop=%" GST_TIME_FORMAT ", rate=%f, applied_rate=%f"
           ", flags=0x%02x, time=%" GST_TIME_FORMAT ", base=%" GST_TIME_FORMAT
-          ", position %" GST_TIME_FORMAT,
+          ", position %" GST_TIME_FORMAT ", duration %" GST_TIME_FORMAT,
           GST_TIME_ARGS (segment->start), GST_TIME_ARGS (segment->stop),
           segment->rate, segment->applied_rate, (guint) segment->flags,
           GST_TIME_ARGS (segment->time), GST_TIME_ARGS (segment->base),
-          GST_TIME_ARGS (segment->position));
+          GST_TIME_ARGS (segment->position), GST_TIME_ARGS (segment->duration));
     }
     default:{
       const gchar *format_name;
@@ -741,10 +741,10 @@ gst_debug_print_segment (gpointer ptr)
       return g_strdup_printf ("%s segment start=%" G_GINT64_FORMAT
           ", stop=%" G_GINT64_FORMAT ", rate=%f, applied_rate=%f"
           ", flags=0x%02x, time=%" G_GINT64_FORMAT ", base=%" G_GINT64_FORMAT
-          ", position %" G_GINT64_FORMAT,
+          ", position %" G_GINT64_FORMAT ", duration %" G_GINT64_FORMAT,
           format_name, segment->start, segment->stop, segment->rate,
           segment->applied_rate, (guint) segment->flags,
-          segment->time, segment->base, segment->position);
+          segment->time, segment->base, segment->position, segment->duration);
     }
   }
 }
