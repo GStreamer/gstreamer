1ac6c436e73cde5289c90f21eb79ba9845e60d75
David Schleef <ds@schleef.org>
2012-02-21 16:51:35 -0800

    autogen: workaround for gettext/automake with git
    Fixes: #669207.

--- autogen.sh
+++ autogen.sh
@@ -35,6 +35,9 @@ then
     ln -s ../../common/hooks/pre-commit.hook .git/hooks/pre-commit
 fi
 
+# GNU gettext automake support doesn't get along with git.
+# https://bugzilla.gnome.org/show_bug.cgi?id=661128
+touch -t 200001010000 po/gst-plugins-base-0.10.pot
 
 CONFIGURE_DEF_OPT='--enable-maintainer-mode --enable-gtk-doc'
 
