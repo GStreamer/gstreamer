e012ea80ffae23ec0781625fe57662c66bd9ac86
Sreerenj Balachandran <sreerenj.balachandran@intel.com>
2012-02-28 21:00:24 +0200

    waylandsink: Fix in shell_surface_set_fullscreen

--- ext/wayland/gstwaylandsink.c
+++ ext/wayland/gstwaylandsink.c
@@ -594,7 +594,8 @@ create_window (GstWaylandSink * sink, struct display *display, int width,
   window->shell_surface = wl_shell_get_shell_surface (display->shell,
       window->surface);
 //  wl_shell_surface_set_toplevel (window->shell_surface);
-  wl_shell_surface_set_fullscreen (window->shell_surface);
+  wl_shell_surface_set_fullscreen (window->shell_surface,
+      WL_SHELL_SURFACE_FULLSCREEN_METHOD_DEFAULT, 0, NULL);
 
   sink->window = window;
 
