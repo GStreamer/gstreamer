0bd9ae0deaad4f88407b9af75eebe17f9ad3ee36
Sebastian Dröge <sebastian.droege@collabora.co.uk>
2012-03-06 14:44:25 +0100

    mpegtsparse: Fix 'variable 'pid' is uninitialized when used here' compiler warning

--- gst/mpegdemux/mpegtsparse.c
+++ gst/mpegdemux/mpegtsparse.c
@@ -222,11 +222,9 @@ mpegts_parse_base_init (gpointer klass)
 {
   GstElementClass *element_class = GST_ELEMENT_CLASS (klass);
 
-  gst_element_class_add_static_pad_template (element_class,
-      &sink_template);
+  gst_element_class_add_static_pad_template (element_class, &sink_template);
   gst_element_class_add_static_pad_template (element_class, &src_template);
-  gst_element_class_add_static_pad_template (element_class,
-      &program_template);
+  gst_element_class_add_static_pad_template (element_class, &program_template);
 
   gst_element_class_set_details_simple (element_class,
       "MPEG transport stream parser", "Codec/Parser",
@@ -1041,7 +1039,7 @@ mpegts_parse_apply_pmt (MpegTSParse * parse,
   MpegTSParseProgram *program;
   guint program_number;
   guint pcr_pid;
-  guint pid;
+  guint pid = 0;
   guint stream_type;
   GstStructure *stream;
   gint i;
