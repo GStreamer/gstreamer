a0ee86b23c914a4c6a3925b58a973984d3101838
Sebastian Dröge <sebastian.droege@collabora.co.uk>
2012-04-05 18:02:56 +0200

    gst: Update for GST_PLUGIN_DEFINE() API changes

--- plugins/elements/gstdataurisrc.c
+++ plugins/elements/gstdataurisrc.c
@@ -456,6 +456,6 @@ plugin_init (GstPlugin * plugin)
 
 GST_PLUGIN_DEFINE (GST_VERSION_MAJOR,
     GST_VERSION_MINOR,
-    "dataurisrc",
+    dataurisrc,
     "data: URI source",
     plugin_init, VERSION, GST_LICENSE, GST_PACKAGE_NAME, GST_PACKAGE_ORIGIN);
