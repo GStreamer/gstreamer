b27c88c93d2a256583bf66e0e005542cfc406f23
Sebastian Dröge <sebastian.droege@collabora.co.uk>
2012-04-05 17:40:12 +0200

    gst: Update for GST_PLUGIN_DEFINE() API changes

--- ext/twolame/gsttwolamemp2enc.c
+++ ext/twolame/gsttwolamemp2enc.c
@@ -886,6 +886,6 @@ plugin_init (GstPlugin * plugin)
 
 GST_PLUGIN_DEFINE (GST_VERSION_MAJOR,
     GST_VERSION_MINOR,
-    "twolame",
+    twolame,
     "Encode MP2s with TwoLAME",
     plugin_init, VERSION, "LGPL", GST_PACKAGE_NAME, GST_PACKAGE_ORIGIN);
