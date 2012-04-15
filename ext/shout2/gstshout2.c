e0eddb4f254031e20b1fe0b030d6e928625f3cf3
Tim-Philipp Müller <tim.muller@collabora.co.uk>
2012-04-15 22:49:47 +0100

    shout2: update for ogg media type changes

--- ext/shout2/gstshout2.c
+++ ext/shout2/gstshout2.c
@@ -77,7 +77,7 @@ enum
 static GstStaticPadTemplate sink_template = GST_STATIC_PAD_TEMPLATE ("sink",
     GST_PAD_SINK,
     GST_PAD_ALWAYS,
-    GST_STATIC_CAPS ("application/ogg; "
+    GST_STATIC_CAPS ("application/ogg; audio/ogg; video/ogg; "
         "audio/mpeg, mpegversion = (int) 1, layer = (int) [ 1, 3 ]" WEBM_CAPS));
 
 static void gst_shout2send_finalize (GstShout2send * shout2send);
