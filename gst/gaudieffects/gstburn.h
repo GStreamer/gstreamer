c123ac0735d5513dd37fda2f64d2e5f05a3ddf23
Luis de Bethencourt <luis@debethencourt.com>
2012-05-08 16:38:48 +0100

    gaudieffects: port burn to 0.11

--- gst/gaudieffects/gstburn.h
+++ gst/gaudieffects/gstburn.h
@@ -1,7 +1,7 @@
 /*
  * GStreamer
  * Copyright (C) 2010 Luis de Bethencourt <luis@debethencourt.com>
- * 
+ *
  * Burn - curve adjustment film video effect.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
@@ -48,6 +48,7 @@
 
 #include <gst/gst.h>
 
+#include <gst/video/video.h>
 #include <gst/video/gstvideofilter.h>
 
 G_BEGIN_DECLS
@@ -75,6 +76,7 @@ struct _GstBurn
 
   gint width, height;
 
+  GstVideoInfo info;
   gint adjustment;
   gboolean silent;
 };
