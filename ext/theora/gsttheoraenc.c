772e3d514dd98fab7a58f10d5bdc998fee5a0040
Thiago Santos <thiago.sousa.santos@collabora.com>
2012-05-31 18:28:14 -0300

    theoraenc: fix printf format variable

--- ext/theora/gsttheoraenc.c
+++ ext/theora/gsttheoraenc.c
@@ -851,7 +851,7 @@ theora_enc_buffer_from_header_packet (GstTheoraEnc * enc, ogg_packet * packet)
   GST_BUFFER_TIMESTAMP (outbuf) = GST_CLOCK_TIME_NONE;
   GST_BUFFER_DURATION (outbuf) = GST_CLOCK_TIME_NONE;
 
-  GST_DEBUG ("created header packet buffer, %d bytes",
+  GST_DEBUG ("created header packet buffer, %zu bytes",
       gst_buffer_get_size (outbuf));
   return outbuf;
 }
