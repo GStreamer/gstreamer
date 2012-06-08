8b10be67e414c5cc115a294836daf016f1db8752
Vincent Penquerc'h <vincent.penquerch@collabora.co.uk>
2012-06-08 13:58:51 +0100

    videodecoder: use GST_CLOCK_TIME_IS_VALID where appropriate

--- gst-libs/gst/video/gstvideodecoder.c
+++ gst-libs/gst/video/gstvideodecoder.c
@@ -1997,7 +1997,9 @@ gst_video_decoder_finish_frame (GstVideoDecoder * decoder,
 
   /* Check for clipping */
   start = frame->pts;
-  stop = start >= 0 ? frame->pts + frame->duration : GST_CLOCK_TIME_NONE;
+  stop =
+      GST_CLOCK_TIME_IS_VALID (start) ? frame->pts +
+      frame->duration : GST_CLOCK_TIME_NONE;
 
   segment = &decoder->output_segment;
   if (gst_segment_clip (segment, GST_FORMAT_TIME, start, stop, &start, &stop)) {
