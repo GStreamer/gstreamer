a396ba2bde151760b6c5a0ec84266674f501bf74
Thiago Santos <thiago.sousa.santos@collabora.com>
2012-06-21 01:28:43 -0300

    inputselector: avoid notify-tags holding lock
    unlock before issuing this notification to prevent
    deadlocks when other elements reacts to new tags.
    
    Fixes #678220

--- plugins/elements/gstinputselector.c
+++ plugins/elements/gstinputselector.c
@@ -484,6 +484,7 @@ gst_selector_pad_event (GstPad * pad, GstEvent * event)
 {
   gboolean res = TRUE;
   gboolean forward;
+  gboolean new_tags = FALSE;
   GstInputSelector *sel;
   GstSelectorPad *selpad;
   GstPad *prev_active_sinkpad;
@@ -570,7 +571,7 @@ gst_selector_pad_event (GstPad * pad, GstEvent * event)
         gst_tag_list_free (oldtags);
       GST_DEBUG_OBJECT (pad, "received tags %" GST_PTR_FORMAT, newtags);
 
-      g_object_notify (G_OBJECT (selpad), "tags");
+      new_tags = TRUE;
       break;
     }
     case GST_EVENT_EOS:
@@ -597,6 +598,8 @@ gst_selector_pad_event (GstPad * pad, GstEvent * event)
       break;
   }
   GST_INPUT_SELECTOR_UNLOCK (sel);
+  if (new_tags)
+    g_object_notify (G_OBJECT (selpad), "tags");
   if (forward) {
     GST_DEBUG_OBJECT (pad, "forwarding event");
     res = gst_pad_push_event (sel->srcpad, event);
