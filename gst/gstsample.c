4b5c8cada2c88f3cca9f3dd5bed6816f17fb159f
Tim-Philipp Müller <tim@centricular.net>
2012-06-23 17:04:53 +0100

    sample: some more g-i annotations

--- gst/gstsample.c
+++ gst/gstsample.c
@@ -79,10 +79,10 @@ _gst_sample_free (GstSample * sample)
 
 /**
  * gst_sample_new:
- * @buffer: a #GstBuffer
- * @caps: a #GstCaps
- * @segment: a #GstSegment
- * @info: a #GstStructure
+ * @buffer: (transfer none) (allow-none): a #GstBuffer, or NULL
+ * @caps: (transfer none) (allow-none): a #GstCaps, or NULL
+ * @segment: transfer none) (allow-none): a #GstSegment, or NULL
+ * @info: (transfer full) (allow-none): a #GstStructure, or NULL
  *
  * Create a new #GstSample with the provided details.
  *
