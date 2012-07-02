e5c55ad554ebc80cea7ab0ba5c49d833c7189599
David Schleef <ds@schleef.org>
2012-07-02 12:04:19 -0700

    interaudiosink: reenable channel property

--- gst/inter/gstinteraudiosink.c
+++ gst/inter/gstinteraudiosink.c
@@ -160,12 +160,10 @@ gst_inter_audio_sink_class_init (GstInterAudioSinkClass * klass)
   base_sink_class->unlock_stop =
       GST_DEBUG_FUNCPTR (gst_inter_audio_sink_unlock_stop);
 
-#if 0
   g_object_class_install_property (gobject_class, PROP_CHANNEL,
       g_param_spec_string ("channel", "Channel",
           "Channel name to match inter src and sink elements",
           "default", G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS));
-#endif
 }
 
 static void
