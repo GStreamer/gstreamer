0fa3992e372c3535ea58fe31c8b7b8529a83bc00
Tim-Philipp Müller <tim.muller@collabora.co.uk>
2012-07-03 17:52:11 +0100

    audiopanorama: fix negotiation and unit test
    Must remove a possibly-fixed channel-mask field if
    we're going to set unfixed channels on the structure,
    or a different channel count.

--- gst/audiofx/audiopanorama.c
+++ gst/audiofx/audiopanorama.c
@@ -334,9 +334,11 @@ gst_audio_panorama_transform_caps (GstBaseTransform * base,
   if (direction == GST_PAD_SRC) {
     GST_INFO ("allow 1-2 channels");
     gst_structure_set (structure, "channels", GST_TYPE_INT_RANGE, 1, 2, NULL);
+    gst_structure_remove_field (structure, "channel-mask");
   } else {
     GST_INFO ("allow 2 channels");
     gst_structure_set (structure, "channels", G_TYPE_INT, 2, NULL);
+    gst_structure_remove_field (structure, "channel-mask");
   }
 
   return res;
