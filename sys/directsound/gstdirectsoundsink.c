GStreamer/gst-plugins-good

directsoundsink: Fix caps leaks

sys/directsound/gstdirectsoundsink.c
@@ -90,7 +90,8 @@ static void gst_directsound_sink_reset (GstAudioSink * asink);
 static GstCaps *gst_directsound_probe_supported_formats (GstDirectSoundSink *
     dsoundsink, const GstCaps * template_caps);
 static gboolean gst_directsound_sink_acceptcaps (GstPad * pad, GstCaps * caps);
-static boolean gst_directsound_sink_is_spdif_format (GstDirectSoundSink * dsoundsink);
+static boolean gst_directsound_sink_is_spdif_format (GstDirectSoundSink *
+    dsoundsink);
 
 /* interfaces */
 static void gst_directsound_sink_interfaces_init (GType type);
@@ -805,6 +806,7 @@ gst_directsound_probe_supported_formats (GstDirectSoundSink * dsoundsink,
   DSBUFFERDESC descSecondary;
   WAVEFORMATEX wfx;
   GstCaps *caps;
+  GstCaps *tmp, *tmp2;
 
   caps = gst_caps_copy (template_caps);
 
@@ -836,8 +838,16 @@ gst_directsound_probe_supported_formats (GstDirectSoundSink * dsoundsink,
     GST_INFO_OBJECT (dsoundsink, "AC3 passthrough not supported "
         "(IDirectSound_CreateSoundBuffer returned: %s)\n",
         DXGetErrorString9 (hRes));
-    caps = gst_caps_subtract (caps, gst_caps_new_simple ("audio/x-ac3", NULL));
-    caps = gst_caps_subtract (caps, gst_caps_new_simple ("audio/x-dts", NULL));
+    tmp = gst_caps_new_simple ("audio/x-ac3", NULL);
+    tmp2 = gst_caps_subtract (caps, tmp);
+    gst_caps_unref (tmp);
+    gst_caps_unref (caps);
+    caps = tmp2;
+    tmp = gst_caps_new_simple ("audio/x-dts", NULL);
+    tmp2 = gst_caps_subtract (caps, tmp);
+    gst_caps_unref (tmp);
+    gst_caps_unref (caps);
+    caps = tmp2;
   } else {
     GST_INFO_OBJECT (dsoundsink, "AC3 passthrough supported");
     hRes = IDirectSoundBuffer_Release (dsoundsink->pDSBSecondary);
@@ -848,8 +858,16 @@ gst_directsound_probe_supported_formats (GstDirectSoundSink * dsoundsink,
     }
   }
 #else
-  caps = gst_caps_subtract (caps, gst_caps_new_simple ("audio/x-ac3", NULL));
-  caps = gst_caps_subtract (caps, gst_caps_new_simple ("audio/x-dts", NULL));
+  tmp = gst_caps_new_simple ("audio/x-ac3", NULL);
+  tmp2 = gst_caps_subtract (caps, tmp);
+  gst_caps_unref (tmp);
+  gst_caps_unref (caps);
+  caps = tmp2;
+  tmp = gst_caps_new_simple ("audio/x-dts", NULL);
+  tmp2 = gst_caps_subtract (caps, tmp);
+  gst_caps_unref (tmp);
+  gst_caps_unref (caps);
+  caps = tmp2;
 #endif
 
   return caps;
@@ -858,7 +876,7 @@ gst_directsound_probe_supported_formats (GstDirectSoundSink * dsoundsink,
 static GstBuffer *
 gst_directsound_sink_payload (GstBaseAudioSink * sink, GstBuffer * buf)
 {
-  if (gst_directsound_sink_is_spdif_format((GstDirectSoundSink*) sink)) {
+  if (gst_directsound_sink_is_spdif_format ((GstDirectSoundSink *) sink)) {
     gint framesize = gst_audio_iec61937_frame_size (&sink->ringbuffer->spec);
     GstBuffer *out;
 

GStreamer/gstreamer

sys/directsound/gstdirectsoundsink.c
@@ -102,7 +102,8 @@ static void gst_directsound_sink_set_mute (GstDirectSoundSink * sink,
     gboolean mute);
 static gboolean gst_directsound_sink_get_mute (GstDirectSoundSink * sink);
 
-static gboolean gst_directsound_sink_is_spdif_format (GstDirectSoundSink * dsoundsink);
+static gboolean gst_directsound_sink_is_spdif_format (GstDirectSoundSink *
+    dsoundsink);
 
 static GstStaticPadTemplate directsoundsink_sink_factory =
     GST_STATIC_PAD_TEMPLATE ("sink",
@@ -705,6 +706,7 @@ gst_directsound_probe_supported_formats (GstDirectSoundSink * dsoundsink,
   DSBUFFERDESC descSecondary;
   WAVEFORMATEX wfx;
   GstCaps *caps;
+  GstCaps *tmp, *tmp2;
 
   caps = gst_caps_copy (template_caps);
 
@@ -736,8 +738,16 @@ gst_directsound_probe_supported_formats (GstDirectSoundSink * dsoundsink,
     GST_INFO_OBJECT (dsoundsink, "AC3 passthrough not supported "
         "(IDirectSound_CreateSoundBuffer returned: %s)\n",
         DXGetErrorString9 (hRes));
-    caps = gst_caps_subtract (caps, gst_caps_new_empty_simple ("audio/x-ac3"));
-    caps = gst_caps_subtract (caps, gst_caps_new_empty_simple ("audio/x-dts"));
+    tmp = gst_caps_new_empty_simple ("audio/x-ac3");
+    tmp2 = gst_caps_subtract (caps, tmp);
+    gst_caps_unref (tmp);
+    gst_caps_unref (caps);
+    caps = tmp2;
+    tmp = gst_caps_new_empty_simple ("audio/x-dts");
+    tmp2 = gst_caps_subtract (caps, tmp);
+    gst_caps_unref (tmp);
+    gst_caps_unref (caps);
+    caps = tmp2;
   } else {
     GST_INFO_OBJECT (dsoundsink, "AC3 passthrough supported");
     hRes = IDirectSoundBuffer_Release (dsoundsink->pDSBSecondary);
@@ -748,8 +758,16 @@ gst_directsound_probe_supported_formats (GstDirectSoundSink * dsoundsink,
     }
   }
 #else
-  caps = gst_caps_subtract (caps, gst_caps_new_simple ("audio/x-ac3", NULL));
-  caps = gst_caps_subtract (caps, gst_caps_new_simple ("audio/x-dts", NULL));
+  tmp = gst_caps_new_empty_simple ("audio/x-ac3");
+  tmp2 = gst_caps_subtract (caps, tmp);
+  gst_caps_unref (tmp);
+  gst_caps_unref (caps);
+  caps = tmp2;
+  tmp = gst_caps_new_empty_simple ("audio/x-dts");
+  tmp2 = gst_caps_subtract (caps, tmp);
+  gst_caps_unref (tmp);
+  gst_caps_unref (caps);
+  caps = tmp2;
 #endif
 
   return caps;