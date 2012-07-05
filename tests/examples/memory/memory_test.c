b2b934a928feb52e34211dabd527989607148253
Wim Taymans <wim.taymans@collabora.co.uk>
2012-07-05 14:25:13 +0200

    tests: add memory example

---
+++ tests/examples/memory/memory_test.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/times.h>
+
+#include <gst/gst.h>
+
+int
+main (int argc, char **argv)
+{
+  gst_init (&argc, &argv);
+
+  return 0;
+}
