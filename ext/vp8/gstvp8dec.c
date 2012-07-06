9baf930a89cd4062d0a1389dcccc6d0cc4f7b827
Wim Taymans <wim.taymans@collabora.co.uk>
2012-07-06 11:26:55 +0200

    update for query api changes

--- ext/vp8/gstvp8dec.c
+++ ext/vp8/gstvp8dec.c
@@ -526,7 +526,7 @@ gst_vp8_dec_decide_allocation (GstVideoDecoder * bdec, GstQuery * query)
   g_assert (pool != NULL);
 
   config = gst_buffer_pool_get_config (pool);
-  if (gst_query_has_allocation_meta (query, GST_VIDEO_META_API_TYPE)) {
+  if (gst_query_find_allocation_meta (query, GST_VIDEO_META_API_TYPE, NULL)) {
     gst_buffer_pool_config_add_option (config,
         GST_BUFFER_POOL_OPTION_VIDEO_META);
   }
