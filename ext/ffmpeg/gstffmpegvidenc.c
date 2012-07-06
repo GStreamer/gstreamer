3ae9eb158c84c47675fd90401f64d3b37315b985
Wim Taymans <wim.taymans@collabora.co.uk>
2012-07-06 11:03:30 +0200

    update for allocation query changes

--- ext/ffmpeg/gstffmpegvidenc.c
+++ ext/ffmpeg/gstffmpegvidenc.c
@@ -630,7 +630,7 @@ static gboolean
 gst_ffmpegvidenc_propose_allocation (GstVideoEncoder * encoder,
     GstQuery * query)
 {
-  gst_query_add_allocation_meta (query, GST_VIDEO_META_API_TYPE);
+  gst_query_add_allocation_meta (query, GST_VIDEO_META_API_TYPE, 0);
 
   return GST_VIDEO_ENCODER_CLASS (parent_class)->propose_allocation (encoder,
       query);
