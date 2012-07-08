e234651bd30558d7eb4fe525b790962119867da2
Stefan Sauer <ensonic@users.sf.net>
2012-07-08 20:15:33 +0200

    inspect: suppress glib deprecations warnings for G_VALUE_ARRAY

--- tools/gst-inspect.c
+++ tools/gst-inspect.c
@@ -25,6 +25,10 @@
 #  include "config.h"
 #endif
 
+/* FIXME 0.11: suppress warnings for deprecated API such as GValueArray
+ * with newer GLib versions (>= 2.31.0) */
+#define GLIB_DISABLE_DEPRECATION_WARNINGS
+
 #include "tools.h"
 
 #include <string.h>
