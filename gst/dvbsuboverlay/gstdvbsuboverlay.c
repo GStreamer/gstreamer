584485b9f409ef461e63e13bf4da76d856fa30d5
Tim-Philipp Müller <tim.muller@collabora.co.uk>
2012-07-11 13:59:18 +0100

    dvbsuboverlay: fix compiler warnings
    gstdvbsuboverlay.c:772:15: error: pointer targets in passing argument 5 of 'gst_segment_clip' differ in signedness
    /home/tpm/gst/0.11/gstreamer/gst/gstsegment.h:192:14: note: expected 'guint64 *' but argument is of type 'gint64 *'
    gstdvbsuboverlay.c:772:15: error: pointer targets in passing argument 6 of 'gst_segment_clip' differ in signedness

--- gst/dvbsuboverlay/gstdvbsuboverlay.c
+++ gst/dvbsuboverlay/gstdvbsuboverlay.c
@@ -752,15 +752,15 @@ new_dvb_subtitles_cb (DvbSub * dvb_sub, DVBSubtitles * subs, gpointer user_data)
 {
   GstDVBSubOverlay *overlay = GST_DVBSUB_OVERLAY (user_data);
   int max_page_timeout;
-  gint64 start, stop;
+  guint64 start, stop;
 
   max_page_timeout = g_atomic_int_get (&overlay->max_page_timeout);
   if (max_page_timeout > 0)
     subs->page_time_out = MIN (subs->page_time_out, max_page_timeout);
 
   GST_INFO_OBJECT (overlay,
-      "New DVB subtitles arrived with a page_time_out of %d and %d regions for PTS=%"
-      G_GUINT64_FORMAT ", which should be at time %" GST_TIME_FORMAT,
+      "New DVB subtitles arrived with a page_time_out of %d and %d regions for "
+      "PTS=%" G_GUINT64_FORMAT ", which should be at time %" GST_TIME_FORMAT,
       subs->page_time_out, subs->num_rects, subs->pts,
       GST_TIME_ARGS (subs->pts));
 
