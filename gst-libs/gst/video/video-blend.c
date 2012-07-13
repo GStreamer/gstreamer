GStreamer/gst-plugins-base

video-blend: Fix argument signedness
The x/y values are meant to be signed.

This bug was introduced by 76c0881549e73efb4995ac8b38d596d51d1cc0fe

gst-libs/gst/video/video-blend.c
@@ -1344,7 +1344,7 @@ video_blend_scale_linear_RGBA (GstBlendVideoFormatInfo * src,
  */
 gboolean
 video_blend (GstBlendVideoFormatInfo * dest,
-    GstBlendVideoFormatInfo * src, guint x, guint y, gfloat global_alpha)
+    GstBlendVideoFormatInfo * src, gint x, gint y, gfloat global_alpha)
 {
   guint i, j, global_alpha_val, src_width, src_height;
   GetPutLine getputdest, getputsrc;

GStreamer/gstreamer

Conflicts:
	gst-libs/gst/video/video-blend.c
	gst-libs/gst/video/video-blend.h

gst-libs/gst/video/video-blend.c
@@ -241,7 +241,7 @@ gst_video_blend_scale_linear_RGBA (GstVideoInfo * src, GstBuffer * src_buffer,
  */
 gboolean
 gst_video_blend (GstVideoFrame * dest,
-    GstVideoFrame * src, guint x, guint y, gfloat global_alpha)
+    GstVideoFrame * src, gint x, gint y, gfloat global_alpha)
 {
   guint i, j, global_alpha_val, src_width, src_height, dest_width, dest_height;
   gint xoff;