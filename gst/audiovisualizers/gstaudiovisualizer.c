658e54ff8cd1861b16edb3130b8a2e394db74f02
Wim Taymans <wim.taymans@collabora.co.uk>
2012-07-24 12:28:18 +0200

    audiovisualizer: fixate caps

--- gst/audiovisualizers/gstaudiovisualizer.c
+++ gst/audiovisualizers/gstaudiovisualizer.c
@@ -702,6 +702,7 @@ gst_audio_visualizer_src_negotiate (GstAudioVisualizer * scope)
   gst_structure_fixate_field_nearest_int (structure, "height", scope->height);
   gst_structure_fixate_field_nearest_fraction (structure, "framerate",
       scope->fps_n, scope->fps_d);
+  target = gst_caps_fixate (target);
 
   GST_DEBUG_OBJECT (scope, "final caps are %" GST_PTR_FORMAT, target);
 
