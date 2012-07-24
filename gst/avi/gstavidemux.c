6cb106d690a7622a7ab3ee0ef98d15d84cfc0901
Mark Nauwelaerts <mark.nauwelaerts@collabora.co.uk>
2012-07-24 15:38:24 +0200

    avidemux: use same fourcc to determine caps in determining uncompressed-ness
    Fixes https://bugzilla.gnome.org/show_bug.cgi?id=673898
    
    Conflicts:
    
    	gst/avi/gstavidemux.c

--- gst/avi/gstavidemux.c
+++ gst/avi/gstavidemux.c
@@ -4463,11 +4463,14 @@ gst_avi_demux_invert (GstAviStream * stream, GstBuffer * buf)
   guint8 *tmp = NULL;
   GstMapInfo map;
   GstCaps *caps;
+  guint32 fourcc;
 
   if (stream->strh->type != GST_RIFF_FCC_vids)
     return buf;
 
-  if (!gst_avi_demux_is_uncompressed (stream->strh->fcc_handler)) {
+  fourcc = (stream->strf.vids->compression) ?
+      stream->strf.vids->compression : stream->strh->fcc_handler;
+  if (!gst_avi_demux_is_uncompressed (fourcc)) {
     return buf;                 /* Ignore non DIB buffers */
   }
 
