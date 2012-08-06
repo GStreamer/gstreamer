f7a4318b5427e57887f9aea756575b9910a1cd16
Sreerenj Balachandran <sreerenj.balachandran@intel.com>
2012-08-06 15:31:50 +0300

    asfdemux: avoid some more invalid timestamp arithmetic
    Fixes https://bugzilla.gnome.org/show_bug.cgi?id=681291

--- gst/asfdemux/asfpacket.c
+++ gst/asfdemux/asfpacket.c
@@ -148,6 +148,7 @@ gst_asf_payload_queue_for_stream (GstASFDemux * demux, AsfPayload * payload,
 
   /* make timestamps start from 0 */
   if (G_LIKELY (GST_CLOCK_TIME_IS_VALID (demux->first_ts) &&
+          GST_CLOCK_TIME_IS_VALID (payload->ts) &&
           demux->first_ts < payload->ts))
     payload->ts -= demux->first_ts;
   else
