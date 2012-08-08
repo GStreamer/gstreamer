b61c7fb9c39d5627cf1fb6d100453ab2d5a41cf9
Tim-Philipp Müller <tim.muller@collabora.co.uk>
2012-08-08 15:08:33 +0100

    Release 0.11.93

--- win32/common/_stdint.h
+++ win32/common/_stdint.h
@@ -1,8 +1,8 @@
 #ifndef _GST_PLUGINS_BASE__STDINT_H
 #define _GST_PLUGINS_BASE__STDINT_H 1
 #ifndef _GENERATED_STDINT_H
-#define _GENERATED_STDINT_H "gst-plugins-base 0.11.92.1"
-/* generated using gnu compiler gcc (Ubuntu/Linaro 4.6.3-1ubuntu5) 4.6.3 */
+#define _GENERATED_STDINT_H "gst-plugins-base 0.11.93"
+/* generated using gnu compiler gcc (Debian 4.7.1-5) 4.7.1 */
 #define _STDINT_HAVE_STDINT_H 1
 #include <stdint.h>
 #endif
