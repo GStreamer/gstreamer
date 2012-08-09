d1299d13573da01923aab51bbecb0af16b0f5ef2
Tim-Philipp Müller <tim.muller@collabora.co.uk>
2012-08-09 19:57:49 +0100

    videodecoder: fix seeking again
    Add missing break in FLUSH_STOP case.

--- gst-libs/gst/video/gstvideodecoder.c
+++ gst-libs/gst/video/gstvideodecoder.c
@@ -1011,6 +1011,7 @@ gst_video_decoder_sink_event_default (GstVideoDecoder * decoder,
       /* well, this is kind of worse than a DISCONT */
       gst_video_decoder_flush (decoder, TRUE);
       GST_VIDEO_DECODER_STREAM_UNLOCK (decoder);
+      break;
     }
     case GST_EVENT_TAG:
     {
