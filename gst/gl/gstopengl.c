b227398c0cd8516c9771245aba12914ff030bf61
Matthew Waters <ystreet00@gmail.com>
2012-08-10 16:41:12 +1000

    [557/906] Fix plugin define and remove warning

--- gst/gl/gstopengl.c
+++ gst/gl/gstopengl.c
@@ -202,6 +202,6 @@ plugin_init (GstPlugin * plugin)
 
 GST_PLUGIN_DEFINE (GST_VERSION_MAJOR,
     GST_VERSION_MINOR,
-    "opengl",
+    opengl,
     "OpenGL plugin",
     plugin_init, VERSION, GST_LICENSE, GST_PACKAGE_NAME, GST_PACKAGE_ORIGIN)
