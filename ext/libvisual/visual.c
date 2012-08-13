c4d97601eaf3e7454c919f5097751f1639d52cd4
Wim Taymans <wim.taymans@collabora.co.uk>
2012-08-13 11:51:45 +0200

    visual: remove channel mask
    We don't really care about what's inside those channels. This also makes the
    caps valid because now it's no longer possible to have channels=1 and a mask
    of 0x3.

--- ext/libvisual/visual.c
+++ ext/libvisual/visual.c
@@ -48,7 +48,6 @@ static GstStaticPadTemplate sink_template = GST_STATIC_PAD_TEMPLATE ("sink",
     GST_STATIC_CAPS ("audio/x-raw, "
         "format = (string) " GST_AUDIO_NE (S16) ", "
         "layout = (string) interleaved, " "channels = (int) { 1, 2 }, "
-        "channel-mask = (bitmask) 0x3, "
 #if defined(VISUAL_API_VERSION) && VISUAL_API_VERSION >= 4000 && VISUAL_API_VERSION < 5000
         "rate = (int) { 8000, 11250, 22500, 32000, 44100, 48000, 96000 }"
 #else
