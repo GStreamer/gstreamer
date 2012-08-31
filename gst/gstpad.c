5aefdbaf104d5c72b8433ac01ff6820f969d5727
Jan Schmidt <thaytan@noraisin.net>
2012-08-31 06:25:22 -0700

    gstpad: make some debug statements more verbose

--- gst/gstpad.c
+++ gst/gstpad.c
@@ -2719,7 +2719,8 @@ gst_pad_event_default (GstPad * pad, GstObject * parent, GstEvent * event)
   g_return_val_if_fail (GST_IS_PAD (pad), FALSE);
   g_return_val_if_fail (event != NULL, FALSE);
 
-  GST_LOG_OBJECT (pad, "default event handler");
+  GST_LOG_OBJECT (pad, "default event handler for event %" GST_PTR_FORMAT,
+      event);
 
   switch (GST_EVENT_TYPE (event)) {
     case GST_EVENT_CAPS:
@@ -4466,8 +4467,8 @@ gst_pad_push_event_unchecked (GstPad * pad, GstEvent * event,
   /* Note: we gave away ownership of the event at this point but we can still
    * print the old pointer */
   GST_LOG_OBJECT (pad,
-      "sent event %p to peerpad %" GST_PTR_FORMAT ", ret %s", event, peerpad,
-      gst_flow_get_name (ret));
+      "sent event %" GST_PTR_FORMAT " to peerpad %"
+      GST_PTR_FORMAT ", ret %s", event, peerpad, gst_flow_get_name (ret));
 
   gst_object_unref (peerpad);
 
