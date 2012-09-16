5d1395f43556daac95ebff43e6a65e8d39591ab2
Matthew Waters <ystreet00@gmail.com>
2012-09-16 21:36:09 +1000

    [574/906] memory: implement transperent upload/download for data
    (not async yet)

--- gst-libs/gst/gl/gstglmemory.h
+++ gst-libs/gst/gl/gstglmemory.h
@@ -26,8 +26,13 @@
 #include <gst/gstallocator.h>
 
 #include "gstgldisplay.h"
+#include "gstgldownload.h"
+#include "gstglupload.h"
 
 G_BEGIN_DECLS
+
+typedef struct _GstGLUpload GstGLUpload;
+typedef struct _GstGLDownload GstGLDownload;
 
 #define GST_TYPE_GL_ALLOCATOR (gst_gl_allocator_get_type())
 GType gst_gl_allocator_get_type(void);
@@ -42,6 +47,25 @@ GType gst_gl_allocator_get_type(void);
 typedef struct _GstGLMemory GstGLMemory;
 typedef struct _GstGLAllocator GstGLAllocator;
 typedef struct _GstGLAllocatorClass GstGLAllocatorClass;
+
+typedef enum
+{
+  GST_GL_MEMORY_FLAG_DOWNLOAD_INITTED = (GST_MEMORY_FLAG_LAST << 0),
+  GST_GL_MEMORY_FLAG_UPLOAD_INITTED   = (GST_MEMORY_FLAG_LAST << 1),
+  GST_GL_MEMORY_FLAG_NEED_DOWNLOAD   = (GST_MEMORY_FLAG_LAST << 2),
+  GST_GL_MEMORY_FLAG_NEED_UPLOAD     = (GST_MEMORY_FLAG_LAST << 3)
+} GstGLMemoryFlags;
+
+/**
+ * GST_MAP_GL:
+ *
+ * Flag indicating that we should map the GL object instead of to system memory.
+ *
+ * Combining #GST_MAP_GL with #GST_MAP_WRITE has the same semantics as though
+ * you are writing to OpenGL. Conversely, combining #GST_MAP_GL with
+ * #GST_MAP_READ has the same semantics as though you are reading from OpenGL.
+ */
+#define GST_MAP_GL GST_MAP_FLAG_LAST << 1
 
 /**
  * GstGLMemory:
@@ -64,14 +88,34 @@ struct _GstGLMemory
   GLenum             gl_format;
   GLuint             width;
   GLuint             height;
+
+  GstGLDownload     *download;
+  GLenum             render_target;
+  GstGLUpload       *upload;
+
+  GstMapFlags        map_flags;
+  gpointer           data;
+
+  gboolean           wrapped;
+  GDestroyNotify     notify;
+  gpointer           user_data;
 };
 
 #define GST_GL_MEMORY_ALLOCATOR   "GLMemory"
+
+#define GST_GL_MEMORY_FLAGS(mem) GST_MEMORY_FLAGS(mem)
+#define GST_GL_MEMORY_FLAG_IS_SET(mem,flag) GST_MEMORY_FLAG_IS_SET(mem,flag)
+#define GST_GL_MEMORY_FLAG_SET(mem,flag) GST_MINI_OBJECT_FLAG_SET(mem,flag)
+#define GST_GL_MEMORY_FLAG_UNSET(mem,flag) GST_MEMORY_FLAG_UNSET(mem,flag)
 
 void gst_gl_memory_init (void);
 
 GstMemory * gst_gl_memory_alloc (GstGLDisplay * display, GstVideoFormat format,
                                  gsize width, gsize height);
+
+GstGLMemory * gst_gl_memory_wrapped (GstGLDisplay * display, GstVideoFormat format,
+                                     guint width, guint height, gpointer data,
+                                     gpointer user_data, GDestroyNotify notify);
 
 gboolean gst_is_gl_memory (GstMemory * mem);
 
