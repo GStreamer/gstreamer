2a4e3d21101c9fe19af4d242878cefad3886eebe
Sebastian Dröge <sebastian.droege@collabora.co.uk>
2012-09-16 15:33:57 +0200

    vpx: Rename vp8 plugin to vpx
    This is using libvpx, which can support more codecs than just VP8
    and will likely support future codecs.

--- ext/vpx/plugin.c
+++ ext/vpx/plugin.c
@@ -46,6 +46,6 @@ plugin_init (GstPlugin * plugin)
 
 GST_PLUGIN_DEFINE (GST_VERSION_MAJOR,
     GST_VERSION_MINOR,
-    vp8,
+    vpx,
     "VP8 plugin",
     plugin_init, VERSION, "LGPL", GST_PACKAGE_NAME, GST_PACKAGE_ORIGIN)
