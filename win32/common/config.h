46e8c203d642756bb46245748c7eace327305fb5
Tim-Philipp Müller <tim.muller@collabora.co.uk>
2012-09-24 14:42:25 +0100

    Release 1.0.0

--- win32/common/config.h
+++ win32/common/config.h
@@ -199,7 +199,7 @@
 #undef USE_POISONING
 
 /* Version number of package */
-#define VERSION "0.11.99"
+#define VERSION "1.0.0"
 
 /* Define to 1 if your processor stores words with the most significant byte
    first (like Motorola and SPARC, unlike Intel and VAX). */
