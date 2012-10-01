74543783c08f39279fd9ef592c6fe743f3f4889e
Sebastian Dröge <sebastian.droege@collabora.co.uk>
2012-10-01 10:08:52 +0200

    Only set GSTREAMER_SDK_ROOT if it wasn't set yet

--- gst-sdk/tutorials/android-tutorial-1/jni/Android.mk
+++ gst-sdk/tutorials/android-tutorial-1/jni/Android.mk
@@ -22,10 +22,12 @@ LOCAL_SHARED_LIBRARIES := gstreamer_android
 LOCAL_LDLIBS := -landroid
 include $(BUILD_SHARED_LIBRARY)
 
+ifndef GSTREAMER_SDK_ROOT
 ifndef GSTREAMER_SDK_ROOT_ANDROID
 $(error GSTREAMER_SDK_ROOT_ANDROID is not defined!)
 endif
 GSTREAMER_SDK_ROOT        := $(GSTREAMER_SDK_ROOT_ANDROID)
+endif
 GSTREAMER_NDK_BUILD_PATH  := $(GSTREAMER_SDK_ROOT)/share/gst-android/ndk-build/
 GSTREAMER_PLUGINS         := coreelements ogg theora vorbis ffmpegcolorspace playback audioconvert audiorate audioresample coreindexers gio uridecodebin videorate videoscale typefindfunctions eglglessink soup amc matroska autodetect videofilter videotestsrc opensles volume
 G_IO_MODULES              := gnutls
