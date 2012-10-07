dc35816c9885b862877cfb46a5a29069d7c11ec7
Tim-Philipp Müller <tim@centricular.net>
2012-10-07 16:52:27 +0100

    Back to development (bug fixing)

--- win32/common/config.h
+++ win32/common/config.h
@@ -81,13 +81,13 @@
 #define GST_LICENSE "LGPL"
 
 /* package name in plugins */
-#define GST_PACKAGE_NAME "GStreamer Base Plug-ins source release"
+#define GST_PACKAGE_NAME "GStreamer Base Plug-ins git"
 
 /* package origin */
 #define GST_PACKAGE_ORIGIN "Unknown package origin"
 
 /* GStreamer package release date/time for plugins as YYYY-MM-DD */
-#define GST_PACKAGE_RELEASE_DATETIME "2012-10-07"
+#define GST_PACKAGE_RELEASE_DATETIME "2012-10-07T15:49Z"
 
 /* Define to enable ALSA (used by alsa). */
 #undef HAVE_ALSA
@@ -313,7 +313,7 @@
 #define PACKAGE_NAME "GStreamer Base Plug-ins"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "GStreamer Base Plug-ins 1.0.1"
+#define PACKAGE_STRING "GStreamer Base Plug-ins 1.0.1.1"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "gst-plugins-base"
@@ -322,7 +322,7 @@
 #undef PACKAGE_URL
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.0.1"
+#define PACKAGE_VERSION "1.0.1.1"
 
 /* directory where plugins are located */
 #ifdef _DEBUG
@@ -356,7 +356,7 @@
 #undef USE_TREMOLO
 
 /* Version number of package */
-#define VERSION "1.0.1"
+#define VERSION "1.0.1.1"
 
 /* Define WORDS_BIGENDIAN to 1 if your processor stores words with the most
    significant byte first (like Motorola and SPARC, unlike Intel). */
