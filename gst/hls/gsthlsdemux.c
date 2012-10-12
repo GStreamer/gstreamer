80a3a014f566b828df9ad68b4ee9925d3dc103ec
Sebastian Dröge <sebastian.droege@collabora.co.uk>
2012-10-12 15:09:07 +0200

    hlsdemux: Fix invalid read

--- gst/hls/gsthlsdemux.c
+++ gst/hls/gsthlsdemux.c
@@ -947,7 +947,7 @@ gst_hls_src_buf_to_utf8_playlist (GstBuffer * buf)
 
   /* alloc size + 1 to end with a null character */
   playlist = g_malloc0 (info.size + 1);
-  memcpy (playlist, info.data, info.size + 1);
+  memcpy (playlist, info.data, info.size);
 
   gst_buffer_unmap (buf, &info);
   gst_buffer_unref (buf);
