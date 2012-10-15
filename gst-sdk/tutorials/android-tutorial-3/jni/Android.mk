10a4d1a28c99c1d1ed811dc1ff09c5f3ec7bc579
Xavi Artigas <xartigas@fluendo.com>
2012-10-15 18:48:58 +0200

    Tutorial 3

---
+++ gst-sdk/tutorials/android-tutorial-3/jni/Android.mk
@@ -0,0 +1,20 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE    := tutorial-3
+LOCAL_SRC_FILES := tutorial-3.c
+LOCAL_SHARED_LIBRARIES := gstreamer_android
+LOCAL_LDLIBS := -llog -landroid
+include $(BUILD_SHARED_LIBRARY)
+
+ifndef GSTREAMER_SDK_ROOT
+ifndef GSTREAMER_SDK_ROOT_ANDROID
+$(error GSTREAMER_SDK_ROOT_ANDROID is not defined!)
+endif
+GSTREAMER_SDK_ROOT        := $(GSTREAMER_SDK_ROOT_ANDROID)
+endif
+GSTREAMER_NDK_BUILD_PATH  := $(GSTREAMER_SDK_ROOT)/share/gst-android/ndk-build/
+include $(GSTREAMER_NDK_BUILD_PATH)/plugins.mk
+GSTREAMER_PLUGINS         := $(GSTREAMER_PLUGINS_CORE) $(GSTREAMER_PLUGINS_SYS)
+include $(GSTREAMER_NDK_BUILD_PATH)/gstreamer.mk
