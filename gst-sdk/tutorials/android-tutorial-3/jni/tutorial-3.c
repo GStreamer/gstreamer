2f602601f03ba6e0a98af268837bedc4241b9a4e
Xavi Artigas <xartigas@fluendo.com>
2012-10-17 15:58:17 +0200

    Remove video sink debug

--- gst-sdk/tutorials/android-tutorial-3/jni/tutorial-3.c
+++ gst-sdk/tutorials/android-tutorial-3/jni/tutorial-3.c
@@ -244,7 +244,6 @@ static void gst_native_init (JNIEnv* env, jobject thiz) {
   SET_CUSTOM_DATA (env, thiz, custom_data_field_id, data);
   GST_DEBUG_CATEGORY_INIT (debug_category, "tutorial-3", 0, "Android tutorial 3");
   gst_debug_set_threshold_for_name("tutorial-3", GST_LEVEL_DEBUG);
-  gst_debug_set_threshold_for_name("eglgles*", GST_LEVEL_DEBUG);
   GST_DEBUG ("Created CustomData at %p", data);
   data->app = (*env)->NewGlobalRef (env, thiz);
   GST_DEBUG ("Created GlobalRef for app object at %p", data->app);
