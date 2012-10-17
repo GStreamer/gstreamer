e9040e90a59c4f540a03e10566e8c227a8f6d93f
Wim Taymans <wim.taymans@collabora.co.uk>
2012-10-17 14:23:01 +0200

    jpegdepay: store quant tables in zigzag order

--- gst/rtp/gstrtpjpegdepay.c
+++ gst/rtp/gstrtpjpegdepay.c
@@ -154,6 +154,17 @@ gst_rtp_jpeg_depay_finalize (GObject * object)
   G_OBJECT_CLASS (parent_class)->finalize (object);
 }
 
+static const int zigzag[] = {
+  0, 1, 8, 16, 9, 2, 3, 10,
+  17, 24, 32, 25, 18, 11, 4, 5,
+  12, 19, 26, 33, 40, 48, 41, 34,
+  27, 20, 13, 6, 7, 14, 21, 28,
+  35, 42, 49, 56, 57, 50, 43, 36,
+  29, 22, 15, 23, 30, 37, 44, 51,
+  58, 59, 52, 45, 38, 31, 39, 46,
+  53, 60, 61, 54, 47, 55, 62, 63
+};
+
 /*
  * Table K.1 from JPEG spec.
  */
@@ -198,8 +209,8 @@ MakeTables (GstRtpJPEGDepay * rtpjpegdepay, gint Q, guint8 qtable[128])
     Q = 200 - factor * 2;
 
   for (i = 0; i < 64; i++) {
-    gint lq = (jpeg_luma_quantizer[i] * Q + 50) / 100;
-    gint cq = (jpeg_chroma_quantizer[i] * Q + 50) / 100;
+    gint lq = (jpeg_luma_quantizer[zigzag[i]] * Q + 50) / 100;
+    gint cq = (jpeg_chroma_quantizer[zigzag[i]] * Q + 50) / 100;
 
     /* Limit the quantizers to 1 <= q <= 255 */
     qtable[i] = CLAMP (lq, 1, 255);
