a00f4d59d3026055d6b53fcbf8d05ea6bb2cc6d1
Xavi Artigas <xartigas@fluendo.com>
2012-10-17 15:49:14 +0200

    Add warptv to the pipeline, for added awesomeness.

--- gst-sdk/tutorials/android-tutorial-3/jni/tutorial-3.c
+++ gst-sdk/tutorials/android-tutorial-3/jni/tutorial-3.c
@@ -179,7 +179,7 @@ static void *app_function (void *userdata) {
   g_main_context_push_thread_default(data->context);
 
   /* Build pipeline */
-  data->pipeline = gst_parse_launch("videotestsrc ! ffmpegcolorspace ! autovideosink", &error);
+  data->pipeline = gst_parse_launch("videotestsrc ! warptv ! ffmpegcolorspace ! autovideosink", &error);
   if (error) {
     gchar *message = g_strdup_printf("Unable to build pipeline: %s", error->message);
     g_clear_error (&error);
