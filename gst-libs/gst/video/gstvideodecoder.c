6c0c3eb967ac1dc6d8573e1681a05133fab25767
Tim-Philipp Müller <tim@centricular.net>
2012-10-20 11:37:33 +0100

    videodecoder: don't leak message strings when error is not fatal

--- gst-libs/gst/video/gstvideodecoder.c
+++ gst-libs/gst/video/gstvideodecoder.c
@@ -3174,6 +3174,8 @@ _gst_video_decoder_error (GstVideoDecoder * dec, gint weight,
         domain, code, txt, dbg, file, function, line);
     return GST_FLOW_ERROR;
   } else {
+    g_free (txt);
+    g_free (dbg);
     return GST_FLOW_OK;
   }
 }
