d86ecaf53e08c7b734f13e251599c4a8d0a6e53e
Tim-Philipp Müller <tim@centricular.net>
2012-10-20 19:44:43 +0100

    collectpads: fix g-i annotation for GstCollectPadsBufferFunction
    We pass ownership of the buffer to the function.

--- libs/gst/base/gstcollectpads.h
+++ libs/gst/base/gstcollectpads.h
@@ -147,7 +147,7 @@ typedef GstFlowReturn (*GstCollectPadsFunction) (GstCollectPads *pads, gpointer 
  * GstCollectPadsBufferFunction:
  * @pads: the #GstCollectPads that trigered the callback
  * @data: the #GstCollectData of pad that has received the buffer
- * @buffer: the #GstBuffer
+ * @buffer: (transfer full): the #GstBuffer
  * @user_data: user data passed to gst_collect_pads_set_buffer_function()
  *
  * A function that will be called when a (considered oldest) buffer can be muxed.
