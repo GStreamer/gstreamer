ff1b8da189229b0b291135c5d6951f0a8cbba61b
Xavi Artigas <xartigas@fluendo.com>
2012-10-23 16:59:18 +0200

    Initial Android Tutorial 4

---
+++ gst-sdk/tutorials/android-tutorial-4/jni/Android.mk
@@ -0,0 +1,20 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE    := tutorial-4
+LOCAL_SRC_FILES := tutorial-4.c
+LOCAL_SHARED_LIBRARIES := gstreamer_android
+LOCAL_LDLIBS := -llog -landroid
+include $(BUILD_SHARED_LIBRARY)
+
+ifndef GSTREAMER_SDK_ROOT
+ifndef GSTREAMER_SDK_ROOT_ANDROID
+$(error GSTREAMER_SDK_ROOT_ANDROID is not defined!)
+endif
+GSTREAMER_SDK_ROOT        := $(GSTREAMER_SDK_ROOT_ANDROID)
+endif
+GSTREAMER_NDK_BUILD_PATH  := $(GSTREAMER_SDK_ROOT)/share/gst-android/ndk-build/
+include $(GSTREAMER_NDK_BUILD_PATH)/plugins.mk
+GSTREAMER_PLUGINS         := $(GSTREAMER_PLUGINS_CORE) $(GSTREAMER_PLUGINS_CODECS) $(GSTREAMER_PLUGINS_SYS) $(GSTREAMER_PLUGINS_EFFECTS)
+include $(GSTREAMER_NDK_BUILD_PATH)/gstreamer.mk
