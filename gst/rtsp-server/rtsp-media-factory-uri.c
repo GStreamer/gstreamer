8211cdfdc2c17ca32ac6f5811ebaa6a2fbf29055
Wim Taymans <wim.taymans@collabora.co.uk>
2012-11-20 12:10:16 +0100

    factory-uri: take ref to factory
    Take a ref to the factory that we place in our list.

--- gst/rtsp-server/rtsp-media-factory-uri.c
+++ gst/rtsp-server/rtsp-media-factory-uri.c
@@ -147,7 +147,7 @@ payloader_filter (GstPluginFeature * feature, FilterData * data)
 
   if (list) {
     GST_DEBUG ("adding %s", GST_OBJECT_NAME (fact));
-    *list = g_list_prepend (*list, fact);
+    *list = g_list_prepend (*list, gst_object_ref (fact));
   }
 
   return FALSE;
