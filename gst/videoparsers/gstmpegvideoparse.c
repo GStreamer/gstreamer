9f1257330db0928bd86de541689cf67da51694fa
Sebastian Dröge <sebastian.droege@collabora.co.uk>
2012-12-17 15:04:44 +0100

    videoparsers: Use the peer caps for restrictions instead of the srcpad allowed caps
    Otherwise we will intersect with the srcpad template caps and add all the caps fields
    that the parser will ever set, no matter if downstream restricts this field or not.
    This requires upstream to set this field on the caps to successfully negotiate.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=690184

--- gst/videoparsers/gstmpegvideoparse.c
+++ gst/videoparsers/gstmpegvideoparse.c
@@ -859,7 +859,7 @@ gst_mpegv_parse_get_caps (GstBaseParse * parse, GstCaps * filter)
   GstCaps *res;
 
   templ = gst_pad_get_pad_template_caps (GST_BASE_PARSE_SINK_PAD (parse));
-  peercaps = gst_pad_get_allowed_caps (GST_BASE_PARSE_SRC_PAD (parse));
+  peercaps = gst_pad_peer_query_caps (GST_BASE_PARSE_SRC_PAD (parse), filter);
   if (peercaps) {
     guint i, n;
 
