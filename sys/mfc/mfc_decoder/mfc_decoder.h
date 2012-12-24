dd56c6b5b6f48a3659e618e6dcb7f8a69787d5fb
Sebastian Dröge <sebastian.droege@collabora.co.uk>
2012-12-24 11:49:42 +0100

    mfc: The mfc decoder library is dual licensed too
    Apache License 2.0 and LGPL2+

--- sys/mfc/mfc_decoder/mfc_decoder.h
+++ sys/mfc/mfc_decoder/mfc_decoder.h
@@ -1,5 +1,11 @@
 /*
- * Copyright 2012 FXI Technologies
+ * Copyright (c) 2012 FXI Technologies
+ *
+ * This library is licensed under 2 different licenses and you
+ * can choose to use it under the terms of any one of them. The
+ * two licenses are the Apache License 2.0 and the LGPL.
+ *
+ * Apache License 2.0:
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,6 +18,23 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * LGPL:
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
  */
 /*
  * Author: Haavard Kvaalen <havardk@fxitech.com>
