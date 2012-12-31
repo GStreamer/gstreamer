2cae6df5e574e56cd716f72eb79ebc668d6e12d7
Sebastian Dröge <sebastian.droege@collabora.co.uk>
2012-12-31 11:38:23 +0100

    eglglessink: Downgrade a GST_ERROR to a GST_DEBUG

--- ext/eglgles/gsteglglessink.c
+++ ext/eglgles/gsteglglessink.c
@@ -2612,9 +2612,9 @@ gst_eglglessink_configure_caps (GstEglGlesSink * eglglessink, GstCaps * caps)
   GST_VIDEO_SINK_HEIGHT (eglglessink) = info.height;
 
   if (eglglessink->configured_caps) {
-    GST_ERROR_OBJECT (eglglessink, "Caps were already set");
+    GST_DEBUG_OBJECT (eglglessink, "Caps were already set");
     if (gst_caps_can_intersect (caps, eglglessink->configured_caps)) {
-      GST_INFO_OBJECT (eglglessink, "Caps are compatible anyway");
+      GST_DEBUG_OBJECT (eglglessink, "Caps are compatible anyway");
       goto SUCCEED;
     }
 
