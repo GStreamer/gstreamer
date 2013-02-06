99504837748c7116248b30b4cf120237d4b2a0dc
David Schleef <ds@schleef.org>
2013-02-06 15:48:06 -0800

    interlace: fix negotiation for true interlaced modes

--- gst/interlace/gstinterlace.c
+++ gst/interlace/gstinterlace.c
@@ -471,6 +471,7 @@ gst_interlace_getcaps (GstPad * pad, GstInterlace * interlace, GstCaps * filter)
   GstPad *otherpad;
   GstCaps *othercaps, *tcaps;
   GstCaps *icaps;
+  const char *mode;
 
   otherpad =
       (pad == interlace->srcpad) ? interlace->sinkpad : interlace->srcpad;
@@ -492,8 +493,13 @@ gst_interlace_getcaps (GstPad * pad, GstInterlace * interlace, GstCaps * filter)
   }
 
   icaps = gst_caps_make_writable (icaps);
+  if (interlace->pattern > GST_INTERLACE_PATTERN_2_2) {
+    mode = "mixed";
+  } else {
+    mode = "interleaved";
+  }
   gst_caps_set_simple (icaps, "interlace-mode", G_TYPE_STRING,
-      pad == interlace->srcpad ? "mixed" : "progressive", NULL);
+      pad == interlace->srcpad ? mode : "progressive", NULL);
 
   gst_caps_unref (tcaps);
 
