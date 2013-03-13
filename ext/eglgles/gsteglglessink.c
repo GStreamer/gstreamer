d5f15cf3a277813b6ff920f74b83701e3bef43cc
Sebastian Dröge <sebastian.droege@collabora.co.uk>
2013-03-13 11:09:36 +0100

    eglglessink: Add RGBA to the caps again

--- ext/eglgles/gsteglglessink.c
+++ ext/eglgles/gsteglglessink.c
@@ -425,6 +425,8 @@ gst_eglglessink_fill_supported_fbuffer_configs (GstEglGlesSink * eglglessink)
   if (eglChooseConfig (gst_egl_display_get (eglglessink->eglglesctx.display),
           eglglessink_RGBA8888_attribs, NULL, 1, &cfg_number) != EGL_FALSE) {
     gst_caps_append (caps,
+        _gst_video_format_new_template_caps (GST_VIDEO_FORMAT_RGBA));
+    gst_caps_append (caps,
         _gst_video_format_new_template_caps (GST_VIDEO_FORMAT_BGRA));
     gst_caps_append (caps,
         _gst_video_format_new_template_caps (GST_VIDEO_FORMAT_ARGB));
