a3c8c0aed206c5713974d0b08c9f5b1fb81df3fa
Arun Raghavan <arun.raghavan@collabora.co.uk>
2013-03-14 12:58:16 +0530

    bluez: Fix up some problems that came in with cherry-pick

--- sys/bluez/gstavdtpsink.c
+++ sys/bluez/gstavdtpsink.c
@@ -141,8 +141,6 @@ gst_avdtp_sink_set_property (GObject * object, guint prop_id,
 
   switch (prop_id) {
     case PROP_DEVICE:
-      if (sink->device)
-        g_free (sink->device);
       gst_avdtp_connection_set_device (&sink->conn, g_value_get_string (value));
       break;
 
@@ -151,8 +149,6 @@ gst_avdtp_sink_set_property (GObject * object, guint prop_id,
       break;
 
     case PROP_TRANSPORT:
-      if (sink->transport)
-        g_free (sink->transport);
       gst_avdtp_connection_set_transport (&sink->conn,
           g_value_get_string (value));
       break;
@@ -324,7 +320,7 @@ gst_avdtp_sink_render (GstBaseSink * basesink, GstBuffer * buffer)
     return GST_FLOW_ERROR;
 
   /* FIXME: temporary sanity check */
-  g_assert (!(g_io_channel_get_flags (self->stream) & G_IO_FLAG_NONBLOCK));
+  g_assert (!(g_io_channel_get_flags (self->conn.stream) & G_IO_FLAG_NONBLOCK));
 
   /* FIXME: why not use g_io_channel_write_chars() instead? */
   fd = g_io_channel_unix_get_fd (self->conn.stream);
