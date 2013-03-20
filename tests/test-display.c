110c9ab43c67d975f5031d09f3f46665511aa76e
Gwenole Beauchesne <gwenole.beauchesne@intel.com>
2013-03-20 11:57:57 +0100

    tests: fix license templates.

--- tests/test-display.c
+++ tests/test-display.c
@@ -18,7 +18,7 @@
  *  License along with this library; if not, write to the Free
  *  Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
  *  Boston, MA 02110-1301 USA
-*/
+ */
 
 #include "gst/vaapi/sysdeps.h"
 #include <gst/video/video.h>
