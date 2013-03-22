7f8ea87c4b60a406124e021dfd3640a79b043ac2
Tim-Philipp Müller <tim@centricular.net>
2013-03-22 13:41:58 +0000

    Release 1.0.6

--- win32/common/config.h
+++ win32/common/config.h
@@ -58,7 +58,7 @@
 #define GST_PACKAGE_ORIGIN "Unknown package origin"
 
 /* GStreamer package release date/time for plugins as YYYY-MM-DD */
-#define GST_PACKAGE_RELEASE_DATETIME "2013-01-05"
+#define GST_PACKAGE_RELEASE_DATETIME "2013-03-22"
 
 /* struct v4l2_buffer missing */
 #undef GST_V4L2_MISSING_BUFDECL
@@ -372,7 +372,7 @@
 #define PACKAGE_NAME "GStreamer Good Plug-ins"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "GStreamer Good Plug-ins 1.0.5"
+#define PACKAGE_STRING "GStreamer Good Plug-ins 1.0.6"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "gst-plugins-good"
@@ -381,7 +381,7 @@
 #undef PACKAGE_URL
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.0.5"
+#define PACKAGE_VERSION "1.0.6"
 
 /* directory where plugins are located */
 #ifdef _DEBUG
@@ -415,7 +415,7 @@
 #undef TARGET_CPU
 
 /* Version number of package */
-#define VERSION "1.0.5"
+#define VERSION "1.0.6"
 
 /* old wavpack API */
 #undef WAVPACK_OLD_API
