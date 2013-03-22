9ce8e5b7ff0b757d034c0dbfe0c9802fcd8cd430
Nicolas Dufresne <nicolas.dufresne@collabora.co.uk>
2013-03-21 21:00:54 -0400

    basesrc: Don't send error if negotiate fails because we are flushing
    Negotiation may be aborted by a flush from another thread that need to
    stop the task (i.e. seek). Check that case and silently pause the task.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=696357

--- libs/gst/base/gstbasesrc.c
+++ libs/gst/base/gstbasesrc.c
@@ -2579,7 +2579,10 @@ gst_base_src_loop (GstPad * pad)
   if (gst_pad_check_reconfigure (pad)) {
     if (!gst_base_src_negotiate (src)) {
       gst_pad_mark_reconfigure (pad);
-      goto negotiate_failed;
+      if (GST_PAD_IS_FLUSHING (pad))
+        goto flushing;
+      else
+        goto negotiate_failed;
     }
   }
 
