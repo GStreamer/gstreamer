a45d8c9d00e341161183ba50ce28c53ec9636fc2
Tim-Philipp Müller <tim@centricular.net>
2013-03-22 12:33:48 +0000

    Release 1.0.6

--- win32/common/config.h
+++ win32/common/config.h
@@ -62,7 +62,7 @@
 #define GST_PACKAGE_ORIGIN "Unknown package origin"
 
 /* GStreamer package release date/time for plugins as YYYY-MM-DD */
-#define GST_PACKAGE_RELEASE_DATETIME "2013-01-05"
+#define GST_PACKAGE_RELEASE_DATETIME "2013-03-22"
 
 /* location of the installed gst-plugin-scanner */
 #define GST_PLUGIN_SCANNER_INSTALLED LIBDIR "\\gst-plugin-scanner"
@@ -340,7 +340,7 @@
 #define PACKAGE_NAME "GStreamer"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "GStreamer 1.0.5"
+#define PACKAGE_STRING "GStreamer 1.0.6"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "gstreamer"
@@ -349,7 +349,7 @@
 #undef PACKAGE_URL
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.0.5"
+#define PACKAGE_VERSION "1.0.6"
 
 /* directory where plugins are located */
 #ifdef _DEBUG
@@ -387,7 +387,7 @@
 #undef USE_POISONING
 
 /* Version number of package */
-#define VERSION "1.0.5"
+#define VERSION "1.0.6"
 
 /* Define WORDS_BIGENDIAN to 1 if your processor stores words with the most
    significant byte first (like Motorola and SPARC, unlike Intel). */
--- win32/common/gstversion.h
+++ win32/common/gstversion.h
@@ -57,7 +57,7 @@ G_BEGIN_DECLS
  *
  * The micro version of GStreamer at compile time:
  */
-#define GST_VERSION_MICRO (5)
+#define GST_VERSION_MICRO (6)
 /**
  * GST_VERSION_NANO:
  *
