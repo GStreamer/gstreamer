ford-prefect/gst-plugins-bad

bluez: Add an avdtpsrc element
Source element that connects to a given transport and reads audio over
AVDTP. Does not provide a clock but uses the system clock to timestamp
incoming packets. Only SBC is currently supported.

Conflicts:
	sys/bluez/gsta2dpsink.h
	sys/bluez/gstavdtpsink.c
	sys/bluez/gstavdtpsink.h

sys/bluez/gsta2dpsink.h
@@ -29,7 +29,6 @@
 #include "gstavdtpsink.h"
 
 G_BEGIN_DECLS
-
 #define GST_TYPE_A2DP_SINK \
 	(gst_a2dp_sink_get_type())
 #define GST_A2DP_SINK(obj) \
@@ -40,45 +39,45 @@ G_BEGIN_DECLS
 	(G_TYPE_CHECK_INSTANCE_TYPE((obj),GST_TYPE_A2DP_SINK))
 #define GST_IS_A2DP_SINK_CLASS(obj) \
 	(G_TYPE_CHECK_CLASS_TYPE((klass),GST_TYPE_A2DP_SINK))
-
 typedef struct _GstA2dpSink GstA2dpSink;
 typedef struct _GstA2dpSinkClass GstA2dpSinkClass;
 
-struct _GstA2dpSink {
-	GstBin bin;
+struct _GstA2dpSink
+{
+  GstBin bin;
 
-	GstBaseRTPPayload *rtp;
-	GstAvdtpSink *sink;
-	GstElement *capsfilter;
-	GstElement *fakesink;
+  GstBaseRTPPayload *rtp;
+  GstAvdtpSink *sink;
+  GstElement *capsfilter;
+  GstElement *fakesink;
 
-	gchar *device;
-	gchar *transport;
-	gboolean autoconnect;
-	gboolean sink_is_in_bin;
+  gchar *device;
+  gchar *transport;
+  gboolean autoconnect;
+  gboolean sink_is_in_bin;
 
-	GstGhostPad *ghostpad;
-	GstPadSetCapsFunction ghostpad_setcapsfunc;
-	GstPadEventFunction ghostpad_eventfunc;
+  GstPad *ghostpad;
+  GstPadQueryFunction ghostpad_queryfunc;
+  GstPadEventFunction ghostpad_eventfunc;
 
-	GstEvent *newseg_event;
-	/* Store the tags received before the a2dpsender sink is created
-	 * when it is created we forward this to it */
-	GstTagList *taglist;
+  GstEvent *segment_event;
+  /* Store the tags received before the a2dpsender sink is created
+   * when it is created we forward this to it */
+  GstTagList *taglist;
 
-	GMutex *cb_mutex;
+  GMutex cb_mutex;
 };
 
-struct _GstA2dpSinkClass {
-	GstBinClass parent_class;
+struct _GstA2dpSinkClass
+{
+  GstBinClass parent_class;
 };
 
-GType gst_a2dp_sink_get_type(void);
+GType gst_a2dp_sink_get_type (void);
 
 gboolean gst_a2dp_sink_plugin_init (GstPlugin * plugin);
 
-GstCaps *gst_a2dp_sink_get_device_caps(GstA2dpSink *self);
+GstCaps *gst_a2dp_sink_get_device_caps (GstA2dpSink * self);
 
 G_END_DECLS
-
 #endif

GStreamer/gstreamer

sys/bluez/gsta2dpsink.h
@@ -28,7 +28,6 @@
 #include "gstavdtpsink.h"
 
 G_BEGIN_DECLS
-
 #define GST_TYPE_A2DP_SINK \
 	(gst_a2dp_sink_get_type())
 #define GST_A2DP_SINK(obj) \
@@ -39,45 +38,45 @@ G_BEGIN_DECLS
 	(G_TYPE_CHECK_INSTANCE_TYPE((obj),GST_TYPE_A2DP_SINK))
 #define GST_IS_A2DP_SINK_CLASS(obj) \
 	(G_TYPE_CHECK_CLASS_TYPE((klass),GST_TYPE_A2DP_SINK))
-
 typedef struct _GstA2dpSink GstA2dpSink;
 typedef struct _GstA2dpSinkClass GstA2dpSinkClass;
 
-struct _GstA2dpSink {
-	GstBin bin;
+struct _GstA2dpSink
+{
+  GstBin bin;
 
-	GstElement *rtp;
-	GstAvdtpSink *sink;
-	GstElement *capsfilter;
-	GstElement *fakesink;
+  GstElement *rtp;
+  GstAvdtpSink *sink;
+  GstElement *capsfilter;
+  GstElement *fakesink;
 
-	gchar *device;
-	gchar *transport;
-	gboolean autoconnect;
-	gboolean sink_is_in_bin;
+  gchar *device;
+  gchar *transport;
+  gboolean autoconnect;
+  gboolean sink_is_in_bin;
 
-	GstPad *ghostpad;
-	GstPadQueryFunction ghostpad_queryfunc;
-	GstPadEventFunction ghostpad_eventfunc;
+  GstPad *ghostpad;
+  GstPadQueryFunction ghostpad_queryfunc;
+  GstPadEventFunction ghostpad_eventfunc;
 
-	GstEvent *segment_event;
-	/* Store the tags received before the a2dpsender sink is created
-	 * when it is created we forward this to it */
-	GstTagList *taglist;
+  GstEvent *segment_event;
+  /* Store the tags received before the a2dpsender sink is created
+   * when it is created we forward this to it */
+  GstTagList *taglist;
 
-	GMutex cb_mutex;
+  GMutex cb_mutex;
 };
 
-struct _GstA2dpSinkClass {
-	GstBinClass parent_class;
+struct _GstA2dpSinkClass
+{
+  GstBinClass parent_class;
 };
 
-GType gst_a2dp_sink_get_type(void);
+GType gst_a2dp_sink_get_type (void);
 
 gboolean gst_a2dp_sink_plugin_init (GstPlugin * plugin);
 
-GstCaps *gst_a2dp_sink_get_device_caps(GstA2dpSink *self);
+GstCaps *gst_a2dp_sink_get_device_caps (GstA2dpSink * self);
 
 G_END_DECLS
-
 #endif