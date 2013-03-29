700a56f88c6e08bfefb9162517e029454bf9987a
Thibault Saunier <thibault.saunier@collabora.com>
2013-03-29 18:56:31 +0100

    layer: Fix some mix up in variable names

--- ges/ges-layer.c
+++ ges/ges-layer.c
@@ -215,7 +215,7 @@ static gboolean
 ges_layer_resync_priorities (GESLayer * layer)
 {
   GList *tmp;
-  GESTimelineElement *track_element;
+  GESTimelineElement *element;
 
   GST_DEBUG ("Resync priorities of %p", layer);
 
@@ -224,8 +224,8 @@ ges_layer_resync_priorities (GESLayer * layer)
    * do in the meantime. */
 
   for (tmp = layer->priv->clips_start; tmp; tmp = tmp->next) {
-    track_element = GES_TIMELINE_ELEMENT (tmp->data);
-    _set_priority0 (track_element, _PRIORITY (track_element));
+    element = GES_TIMELINE_ELEMENT (tmp->data);
+    _set_priority0 (element, _PRIORITY (element));
   }
 
   return TRUE;
