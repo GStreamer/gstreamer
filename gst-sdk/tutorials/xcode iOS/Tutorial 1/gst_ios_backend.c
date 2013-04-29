eae0b9b7fe6ce25836209c84126ea3f4b9756825
Xavi Artigas <xartigas@fluendo.com>
2013-04-29 17:45:38 +0200

    Initial import for iOS Tutorial 1

---
+++ gst-sdk/tutorials/xcode iOS/Tutorial 1/gst_ios_backend.c
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2013 Fluendo S.L <support@fluendo.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "gst_ios_backend.h"
+#include "gst_ios_plugins.h"
+
+void
+gst_backend_init (void)
+{
+  gst_init (NULL, NULL);
+  gst_backend_register_plugins ();
+}
---
+++ gst-sdk/tutorials/xcode iOS/Tutorial 1/main.m
@@ -0,0 +1,13 @@
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+#include "gst_ios_backend.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        gst_backend_init();
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
