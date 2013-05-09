3b56db192c63b134cac767b4a09171d27ca1241c
Xavi Artigas <xartigas@fluendo.com>
2013-05-09 18:07:30 +0200

    First iOS Tutorial 4 (a copy of Tutorial 3)

---
+++ gst-sdk/tutorials/xcode iOS/Tutorial 4/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Tutorial 4
+//
+//  Created by Xavi Artigas on 5/9/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
