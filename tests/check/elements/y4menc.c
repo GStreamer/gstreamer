93bd95f51835f12a36fd0ecd60bd6b893fbf8940
Nicolas Dufresne <nicolas.dufresne@collabora.com>
2013-05-09 18:21:54 -0400

    y4menc-test: Send inital events
    https://bugzilla.gnome.org/show_bug.cgi?id=700033

--- tests/check/elements/y4menc.c
+++ tests/check/elements/y4menc.c
@@ -99,7 +99,7 @@ GST_START_TEST (test_y4m)
   /* makes valgrind's memcheck happier */
   gst_buffer_memset (inbuffer, 0, 0, size);
   caps = gst_caps_from_string (VIDEO_CAPS_STRING);
-  fail_unless (gst_pad_set_caps (mysrcpad, caps));
+  gst_check_setup_events (mysrcpad, y4menc, caps, GST_FORMAT_TIME);
   gst_caps_unref (caps);
   GST_BUFFER_TIMESTAMP (inbuffer) = 0;
   ASSERT_BUFFER_REFCOUNT (inbuffer, "inbuffer", 1);
