d53bb226f69ada82cd24b6385123188a09f79eb1
Xavi Artigas <xartigas@fluendo.com>
2013-05-21 10:46:14 +0200

    Remove the LibraryView, as it belongs to Tutorial 5, not Tutorial 4.

--- gst-sdk/tutorials/xcode iOS/Tutorial 4/VideoViewController.m
+++ gst-sdk/tutorials/xcode iOS/Tutorial 4/VideoViewController.m
@@ -66,6 +66,8 @@
     media_width = 320;
     media_height = 240;
 
+    uri = @"http://docs.gstreamer.com/media/sintel_trailer-368p.ogv";
+
     gst_backend = [[GStreamerBackend alloc] init:self videoView:video_view];
 }
 
