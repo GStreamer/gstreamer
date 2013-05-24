789b68e5065c0e5cd280e627c0ebb8ace162fbf9
Tim-Philipp Müller <tim@centricular.net>
2013-05-24 23:28:04 +0100

    tee: fix property description for now-unused "alloc-pad" property
    Should probably proxy ALLOCATION queries on that though, if set.
    But what else? CAPS and ACCEPT_CAPS too?

--- plugins/elements/gsttee.c
+++ plugins/elements/gsttee.c
@@ -262,7 +262,7 @@ gst_tee_class_init (GstTeeClass * klass)
           DEFAULT_PULL_MODE,
           G_PARAM_CONSTRUCT | G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS));
   pspec_alloc_pad = g_param_spec_object ("alloc-pad", "Allocation Src Pad",
-      "The pad used for gst_pad_alloc_buffer", GST_TYPE_PAD,
+      "The pad ALLOCATION queries will be proxied to (unused)", GST_TYPE_PAD,
       G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS);
   g_object_class_install_property (gobject_class, PROP_ALLOC_PAD,
       pspec_alloc_pad);
