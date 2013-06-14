e0c3ff76960438de0ec62d876621e75653636dda
Bruno Gonzalez <stenyak@gmail.com>
2013-06-14 14:07:42 +0200

    matroskademux: Don't unlock stream lock without locking it first
    https://bugzilla.gnome.org/show_bug.cgi?id=702167

--- gst/matroska/matroska-demux.c
+++ gst/matroska/matroska-demux.c
@@ -2050,8 +2050,10 @@ next:
     gst_pad_pause_task (demux->common.sinkpad);
   }
   /* ouch */
-  if (!update)
+  if (!update) {
+    GST_PAD_STREAM_LOCK (demux->common.sinkpad);
     goto exit;
+  }
 
   /* now grab the stream lock so that streaming cannot continue, for
    * non flushing seeks when the element is in PAUSED this could block
