bcc5ac52986fa2f8af9741cf56b6e450e62e6243
Wim Taymans <wim.taymans@collabora.co.uk>
2013-06-21 11:50:33 +0200

    rtsp: dispatch when initial buffer has data
    When we have data in the inital buffer, dispath the read function to read it
    even if the socket has no data to read.
    
    Fixes https://bugzilla.gnome.org/show_bug.cgi?id=702652

--- gst-libs/gst/rtsp/gstrtspconnection.c
+++ gst-libs/gst/rtsp/gstrtspconnection.c
@@ -2941,7 +2941,12 @@ gst_rtsp_source_dispatch (GSource * source, GSourceFunc callback G_GNUC_UNUSED,
     gpointer user_data G_GNUC_UNUSED)
 {
   GstRTSPWatch *watch = (GstRTSPWatch *) source;
+  GstRTSPConnection *conn = watch->conn;
 
+  if (conn->initial_buffer != NULL) {
+    gst_rtsp_source_dispatch_read (G_POLLABLE_INPUT_STREAM (conn->input_stream),
+        watch);
+  }
   return watch->keep_running;
 }
 
