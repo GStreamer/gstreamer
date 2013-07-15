60aefacd32eb1736cb66ca513445e9488cf01e6b
Sebastian Dröge <slomo@circular-chaos.org>
2013-07-15 14:11:20 +0200

    [750/906] glupload: Add support for RGB reordering when using GLES2

--- gst-libs/gst/gl/gstglupload.h
+++ gst-libs/gst/gl/gstglupload.h
@@ -92,12 +92,8 @@ struct _GstGLUploadClass
  *
  * The currently supported formats that can be uploaded
  */
-#if !GST_GL_HAVE_GLES2
 #define GST_GL_UPLOAD_FORMATS "{ RGB, RGBx, RGBA, BGR, BGRx, BGRA, xRGB, " \
                                "xBGR, ARGB, ABGR, I420, YV12, YUY2, UYVY, AYUV }"
-#else /* GST_GL_HAVE_GLES2 */
-# define GST_GL_UPLOAD_FORMATS "{ RGB, RGBx, RGBA, I420, YV12, YUY2, UYVY, AYUV }"
-#endif /* !GST_GL_HAVE_GLES2 */
 
 /**
  * GST_GL_UPLOAD_VIDEO_CAPS:
