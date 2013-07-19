228474806c3b7f616ea3192c537bcd66a4ec8428
Sebastian Dröge <slomo@circular-chaos.org>
2013-07-19 09:20:09 +0200

    [775/906] configure: Check for GLeglImageOES and others again
    https://bugzilla.gnome.org/show_bug.cgi?id=704498

---
+++ gst-libs/gst/gl/glprototypes/gstgl_compat.h
@@ -0,0 +1,40 @@
+ /*
+ * GStreamer
+ * Copyright (C) 2012 Matthew Waters <ystreet00@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifndef __GST_GL_COMPAT_H__
+#define __GST_GL_COMPAT_H__
+
+#include <gst/gl/gstglconfig.h>
+
+/* undefined typedefs */
+#ifndef GST_GL_HAVE_GLEGLIMAGEOES
+typedef gpointer GLeglImageOES;
+#endif
+#ifndef GST_GL_HAVE_GLCHAR
+typedef gchar GLchar;
+#endif
+#ifndef GST_GL_HAVE_GLSIZEIPTR
+typedef ptrdiff_t GLsizeiptr;
+#endif
+#ifndef GST_GL_HAVE_GLINTPTR
+typedef ptrdiff_t GLintptr;
+#endif
+
+#endif
