cd83c0c8e5da2dadda972a719f41dc88f15bb833
Thibault Saunier <thibault.saunier@collabora.com>
2013-07-22 20:06:25 -0400

    tests: Make sure we can have the results into an XML file

--- tests/check/ges/mixers.c
+++ tests/check/ges/mixers.c
@@ -200,6 +200,7 @@ ges_suite (void)
   Suite *s = suite_create ("Smart mixers");
   TCase *tc_chain = tcase_create ("smart-mixers");
 
+  ges_init ();
   suite_add_tcase (s, tc_chain);
 
   tcase_add_test (tc_chain, simple_smart_adder_test);
@@ -209,20 +210,4 @@ ges_suite (void)
   return s;
 }
 
-int
-main (int argc, char **argv)
-{
-  int nf;
-
-  Suite *s = ges_suite ();
-  SRunner *sr = srunner_create (s);
-
-  gst_check_init (&argc, &argv);
-  ges_init ();
-
-  srunner_run_all (sr, CK_NORMAL);
-  nf = srunner_ntests_failed (sr);
-  srunner_free (sr);
-
-  return nf;
-}
+GST_CHECK_MAIN (ges);
