cdbb6bcc1562a561c98d377fdc4072ad2f8583db
Youness Alaoui <youness.alaoui@collabora.co.uk>
2013-08-05 10:46:33 -0400

    rtsp-media: send state in "new-state" signal
    https://bugzilla.gnome.org/show_bug.cgi?id=705110

--- gst/rtsp-server/rtsp-media.c
+++ gst/rtsp-server/rtsp-media.c
@@ -250,7 +250,7 @@ gst_rtsp_media_class_init (GstRTSPMediaClass * klass)
   gst_rtsp_media_signals[SIGNAL_NEW_STATE] =
       g_signal_new ("new-state", G_TYPE_FROM_CLASS (klass), G_SIGNAL_RUN_LAST,
       G_STRUCT_OFFSET (GstRTSPMediaClass, new_state), NULL, NULL,
-      g_cclosure_marshal_VOID__INT, G_TYPE_NONE, 0, G_TYPE_INT);
+      g_cclosure_marshal_VOID__INT, G_TYPE_NONE, 1, G_TYPE_INT);
 
   GST_DEBUG_CATEGORY_INIT (rtsp_media_debug, "rtspmedia", 0, "GstRTSPMedia");
 
