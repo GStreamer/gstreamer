a13f44fb1e83f0494a61f5678d8c69ecef3ef8b1
Sebastian Dröge <slomo@circular-chaos.org>
2013-08-28 12:41:42 +0200

    Release 1.1.4

--- win32/common/_stdint.h
+++ win32/common/_stdint.h
@@ -1,8 +1,8 @@
 #ifndef _GST_PLUGINS_BASE__STDINT_H
 #define _GST_PLUGINS_BASE__STDINT_H 1
 #ifndef _GENERATED_STDINT_H
-#define _GENERATED_STDINT_H "gst-plugins-base 1.1.3"
-/* generated using gnu compiler gcc-4.8 (Debian 4.8.1-8) 4.8.1 */
+#define _GENERATED_STDINT_H "gst-plugins-base 1.1.4"
+/* generated using gnu compiler gcc-4.8 (Debian 4.8.1-9) 4.8.1 */
 #define _STDINT_HAVE_STDINT_H 1
 #include <stdint.h>
 #endif
