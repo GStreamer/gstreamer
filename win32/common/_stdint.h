a2f6ec99999afdcacdff5279a6f7e033c99619cc
Tim-Philipp Müller <tim@centricular.net>
2013-08-28 14:47:45 +0100

    Release 1.0.10

--- win32/common/_stdint.h
+++ win32/common/_stdint.h
@@ -1,7 +1,7 @@
 #ifndef _GST_PLUGINS_BASE__STDINT_H
 #define _GST_PLUGINS_BASE__STDINT_H 1
 #ifndef _GENERATED_STDINT_H
-#define _GENERATED_STDINT_H "gst-plugins-base 1.0.9"
+#define _GENERATED_STDINT_H "gst-plugins-base 1.0.10"
 /* generated using gnu compiler gcc (Debian 4.7.2-5) 4.7.2 */
 #define _STDINT_HAVE_STDINT_H 1
 #include <stdint.h>
--- win32/common/config.h
+++ win32/common/config.h
@@ -84,7 +84,7 @@
 #define GST_PACKAGE_ORIGIN "Unknown package origin"
 
 /* GStreamer package release date/time for plugins as YYYY-MM-DD */
-#define GST_PACKAGE_RELEASE_DATETIME "2013-07-30"
+#define GST_PACKAGE_RELEASE_DATETIME "2013-08-28"
 
 /* Define if static plugins should be built */
 #undef GST_PLUGIN_BUILD_STATIC
@@ -319,7 +319,7 @@
 #define PACKAGE_NAME "GStreamer Base Plug-ins"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "GStreamer Base Plug-ins 1.0.9"
+#define PACKAGE_STRING "GStreamer Base Plug-ins 1.0.10"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "gst-plugins-base"
@@ -328,7 +328,7 @@
 #undef PACKAGE_URL
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.0.9"
+#define PACKAGE_VERSION "1.0.10"
 
 /* directory where plugins are located */
 #ifdef _DEBUG
@@ -362,7 +362,7 @@
 #undef USE_TREMOLO
 
 /* Version number of package */
-#define VERSION "1.0.9"
+#define VERSION "1.0.10"
 
 /* Define WORDS_BIGENDIAN to 1 if your processor stores words with the most
    significant byte first (like Motorola and SPARC, unlike Intel). */
