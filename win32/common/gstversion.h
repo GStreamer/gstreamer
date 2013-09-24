cd202c647b01e49a4951d383d30e8779067c0a0a
Sebastian Dröge <slomo@circular-chaos.org>
2013-09-24 14:07:02 +0200

    Release 1.2.0

--- win32/common/gstversion.h
+++ win32/common/gstversion.h
@@ -51,13 +51,13 @@ G_BEGIN_DECLS
  *
  * The minor version of GStreamer at compile time:
  */
-#define GST_VERSION_MINOR (1)
+#define GST_VERSION_MINOR (2)
 /**
  * GST_VERSION_MICRO:
  *
  * The micro version of GStreamer at compile time:
  */
-#define GST_VERSION_MICRO (90)
+#define GST_VERSION_MICRO (0)
 /**
  * GST_VERSION_NANO:
  *
