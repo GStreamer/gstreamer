866f132821086359c699f0058b73b4e12e1dc2f6
Thiago Santos <ts.santos@partner.samsung.com>
2013-10-08 00:08:34 -0300

    videotestsrc: implement reverse playback
    Decrement the n_frames counter when doing reverse playback to
    have timestamps and offsets reducing instead of increasing
    
    https://bugzilla.gnome.org/show_bug.cgi?id=701813

--- gst/videotestsrc/gstvideotestsrc.h
+++ gst/videotestsrc/gstvideotestsrc.h
@@ -139,6 +139,7 @@ struct _GstVideoTestSrc {
   /* running time and frames for current caps */
   GstClockTime running_time;            /* total running time */
   gint64 n_frames;                      /* total frames sent */
+  gboolean reverse;
 
   /* previous caps running time and frames */
   GstClockTime accum_rtime;              /* accumulated running_time */
