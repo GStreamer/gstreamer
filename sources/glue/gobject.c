ba21234ddaf3b20968bb04d2687547aa2ceaf115
Stephan Sundermann <stephansundermann@gmail.com>
2013-10-11 12:12:18 +0200

    Removed some experimental stuff

--- sources/glue/gobject.c
+++ sources/glue/gobject.c
@@ -26,9 +26,3 @@ gstsharp_g_type_from_instance (GTypeInstance * instance)
 {
   return G_TYPE_FROM_INSTANCE (instance);
 }
-
-GObjectClass*
-gstsharp_gobjectgetclass (gpointer ptr)
-{
-   return G_OBJECT_GET_CLASS (G_OBJECT(ptr));
-}
