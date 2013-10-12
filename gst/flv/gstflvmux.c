29effb522a91986ebf96c60e0c6ed0de6ffc385d
Tim-Philipp Müller <tim@centricular.net>
2013-10-12 20:37:41 +0100

    flvmux: require stream-format=raw for mpeg-2 too, but don't require framed field
    raw implies that it's framed already. Fixes .. ! faac ! flvmux

--- gst/flv/gstflvmux.c
+++ gst/flv/gstflvmux.c
@@ -76,8 +76,7 @@ static GstStaticPadTemplate audiosink_templ = GST_STATIC_PAD_TEMPLATE ("audio",
     GST_STATIC_CAPS
     ("audio/x-adpcm, layout = (string) swf, channels = (int) { 1, 2 }, rate = (int) { 5512, 11025, 22050, 44100 }; "
         "audio/mpeg, mpegversion = (int) 1, layer = (int) 3, channels = (int) { 1, 2 }, rate = (int) { 5512, 8000, 11025, 22050, 44100 }, parsed = (boolean) TRUE; "
-        "audio/mpeg, mpegversion = (int) 2, framed = (boolean) TRUE; "
-        "audio/mpeg, mpegversion = (int) 4, stream-format = (string) raw, framed = (boolean) TRUE; "
+        "audio/mpeg, mpegversion = (int) { 4, 2 }, stream-format = (string) raw; "
         "audio/x-nellymoser, channels = (int) { 1, 2 }, rate = (int) { 5512, 8000, 11025, 16000, 22050, 44100 }; "
         "audio/x-raw, format = (string) { U8, S16LE}, layout = (string) interleaved, channels = (int) { 1, 2 }, rate = (int) { 5512, 11025, 22050, 44100 }; "
         "audio/x-alaw, channels = (int) { 1, 2 }, rate = (int) { 5512, 11025, 22050, 44100 }; "
