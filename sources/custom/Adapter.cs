f0521e5e17a139de747bb7719bca1ad198acc056
Stephan Sundermann <stephansundermann@gmail.com>
2013-10-13 22:35:24 +0200

    Adapter: make it public

--- sources/custom/Adapter.cs
+++ sources/custom/Adapter.cs
@@ -17,7 +17,7 @@ namespace Gst.Base	 {
 	using System;
 	using System.Runtime.InteropServices;
 
-	partial class Adapter 
+	public partial class Adapter 
 	{
 		[DllImport("libgstbase-1.0-0.dll", CallingConvention = CallingConvention.Cdecl)]
 		static extern void gst_adapter_copy(IntPtr raw, out IntPtr dest, int offset, int size);
