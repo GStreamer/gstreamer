e66812498099a1e5d84e9cdfd780ccdd05a84122
Sebastian Dröge <sebastian@centricular.com>
2013-11-01 16:43:56 +0100

    rtspconnection: Fix indention in header

--- gst-libs/gst/rtsp/gstrtspconnection.h
+++ gst-libs/gst/rtsp/gstrtspconnection.h
@@ -73,8 +73,8 @@ GstRTSPResult      gst_rtsp_connection_close          (GstRTSPConnection *conn);
 GstRTSPResult      gst_rtsp_connection_free           (GstRTSPConnection *conn);
 
 /* TLS connections */
-GTlsConnection *   gst_rtsp_connection_get_tls        (GstRTSPConnection * conn, GError ** error);
-gboolean           gst_rtsp_connection_set_tls_validation_flags (GstRTSPConnection * conn, GTlsCertificateFlags flags);
+GTlsConnection *     gst_rtsp_connection_get_tls                  (GstRTSPConnection * conn, GError ** error);
+gboolean             gst_rtsp_connection_set_tls_validation_flags (GstRTSPConnection * conn, GTlsCertificateFlags flags);
 GTlsCertificateFlags gst_rtsp_connection_get_tls_validation_flags (GstRTSPConnection * conn);
 
 
