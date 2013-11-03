d4eb0aced7526fc135ae640941eb601c7bf8fc85
Stephan Sundermann <stephansundermann@gmail.com>
2013-11-03 14:17:27 +0100

    FFT: Add FFTF32 constructor that is marked as not introspectable

---
+++ sources/custom/FFTF32.cs
@@ -0,0 +1,32 @@
+// Copyright (C) 2013  Stephan Sundermann <stephansundermann@gmail.com>
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace Gst.FFT {
+
+	using System;
+	using System.Collections;
+	using System.Collections.Generic;
+	using System.Runtime.InteropServices;
+
+	public partial class FFTF32 : GLib.Opaque {
+
+		[DllImport("libgstfft-1.0-0.dll", CallingConvention = CallingConvention.Cdecl)]
+		static extern IntPtr gst_fft_f32_new (int len, bool inverse);
+
+		public FFTF32 (int len, bool inverse) {
+			Raw = gst_fft_f32_new (len, inverse);
+		}
+	}
+}
