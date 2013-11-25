2ad767a74c4dc950344865513604b87d6202dc46
Simon Farnsworth <simon.farnsworth@onelan.co.uk>
2013-11-25 17:01:48 +0000

    Update FSF address
    The FSF has moved since these files were created. Update the address, in
    order to keep packaging tools such as rpmlint quiet.
    
    Signed-off-by: Simon Farnsworth <simon.farnsworth@onelan.co.uk>
    
    https://bugzilla.gnome.org/show_bug.cgi?id=715182

--- gi/overrides/Gst.py
+++ gi/overrides/Gst.py
@@ -17,7 +17,7 @@
 #
 # You should have received a copy of the GNU Lesser General Public
 # License along with this program; if not, write to the
-# Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+# Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 # Boston, MA 02111-1307, USA.
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
