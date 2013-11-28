b8f689a9d97b7a90a3c40d5249c964e35a000b4a
Tim-Philipp Müller <tim@centricular.com>
2013-11-28 16:09:04 +0000

    videoflip: don't crash on tag events without orientation tag
    Would crash in g_free() trying to free an uninitialised pointer.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=719497

--- gst/videofilter/gstvideoflip.c
+++ gst/videofilter/gstvideoflip.c
@@ -1217,9 +1217,9 @@ gst_video_flip_sink_event (GstBaseTransform * trans, GstEvent * event)
           gst_video_flip_set_method (vf, GST_VIDEO_FLIP_METHOD_VERT, TRUE);
         else if (!g_strcmp0 ("flip-rotate-270", orientation))
           gst_video_flip_set_method (vf, GST_VIDEO_FLIP_METHOD_OTHER, TRUE);
-      }
 
-      g_free (orientation);
+        g_free (orientation);
+      }
       break;
     default:
       break;
