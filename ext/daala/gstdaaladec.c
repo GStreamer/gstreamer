23735c703d2d8c7cf5259d04539d320f41b9ceb7
Sebastian Dröge <sebastian@centricular.com>
2013-12-05 11:45:36 +0100

    daaladec: Use new gst_video_decoder_set_needs_format() API

--- ext/daala/gstdaaladec.c
+++ ext/daala/gstdaaladec.c
@@ -132,6 +132,7 @@ gst_daala_dec_init (GstDaalaDec * dec)
   /* input is packetized,
    * but is not marked that way so data gets parsed and keyframes marked */
   gst_video_decoder_set_packetized (GST_VIDEO_DECODER (dec), FALSE);
+  gst_video_decoder_set_needs_format (GST_VIDEO_DECODER (dec), TRUE);
 }
 
 static void
