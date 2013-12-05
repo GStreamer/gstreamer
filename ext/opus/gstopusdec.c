0c301bfee7d79801a8fd4d3d2485ea31dc7e6d87
Sebastian Dröge <sebastian@centricular.com>
2013-12-05 12:04:59 +0100

    opusdec: Require caps to be set before any data processing

--- ext/opus/gstopusdec.c
+++ ext/opus/gstopusdec.c
@@ -165,6 +165,8 @@ gst_opus_dec_init (GstOpusDec * dec)
   dec->use_inband_fec = FALSE;
   dec->apply_gain = DEFAULT_APPLY_GAIN;
 
+  gst_audio_decoder_set_needs_format (GST_AUDIO_DECODER (dec), TRUE);
+
   gst_opus_dec_reset (dec);
 }
 
