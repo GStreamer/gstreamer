GStreamer/gstreamer

commit ccba91308dd323f1c9d9caaf8e92951c5c840c03

info: Make sure the same category is not added twice

gst/gstinfo.c
@@ -248,6 +248,9 @@ LevelNameEntry;
 static GMutex __cat_mutex;
 static GSList *__categories = NULL;
 
+static GstDebugCategory *_gst_debug_get_category_locked (const gchar * name);
+
+
 /* all registered debug handlers */
 typedef struct
 {
@@ -1523,7 +1526,13 @@ _gst_debug_category_new (const gchar * name, guint color,
 
   /* add to category list */
   g_mutex_lock (&__cat_mutex);
-  __categories = g_slist_prepend (__categories, cat);
+  if (_gst_debug_get_category_locked (name)) {
+    g_free ((gpointer) cat->name);
+    g_free ((gpointer) cat->description);
+    g_slice_free (GstDebugCategory, cat);
+  } else {
+    __categories = g_slist_prepend (__categories, cat);
+  }
   g_mutex_unlock (&__cat_mutex);
 
   return cat;

commit 24a766a13b05a2d06bf9db24abf77eafe3f1f000

info: return existing category if a debug category is registered twice
If a category with the same name is found when creating a new
one, the found category is returned instead of an invalid pointer.
Fixes issue with gst-vaapi (which uses an internal copy of the
codec parsers) caused by commit ccba9130.

https://bugzilla.gnome.org/show_bug.cgi?id=720036

gst/gstinfo.c
@@ -1509,7 +1509,7 @@ GstDebugCategory *
 _gst_debug_category_new (const gchar * name, guint color,
     const gchar * description)
 {
-  GstDebugCategory *cat;
+  GstDebugCategory *cat, *catfound;
 
   g_return_val_if_fail (name != NULL, NULL);
 
@@ -1526,10 +1526,12 @@ _gst_debug_category_new (const gchar * name, guint color,
 
   /* add to category list */
   g_mutex_lock (&__cat_mutex);
-  if (_gst_debug_get_category_locked (name)) {
+  catfound = _gst_debug_get_category_locked (name);
+  if (catfound) {
     g_free ((gpointer) cat->name);
     g_free ((gpointer) cat->description);
     g_slice_free (GstDebugCategory, cat);
+    cat = catfound;
   } else {
     __categories = g_slist_prepend (__categories, cat);
   }