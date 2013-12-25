fb0fecbf48b1a43821484991ec26f6d0d7b9f7ca
Wim Taymans <wtaymans@redhat.com>
2013-12-25 15:22:24 +0100

    video-tile: improve docs
    https://bugzilla.gnome.org/show_bug.cgi?id=707361

--- gst-libs/gst/video/video-tile.h
+++ gst-libs/gst/video/video-tile.h
@@ -71,9 +71,10 @@ typedef enum
 /**
  * GstVideoTileMode:
  * @GST_VIDEO_TILE_MODE_UNKNOWN: Unknown or unset tile mode
- * @GST_VIDEO_TILE_MODE_ZFLIPZ_2X2: Every four adjacent buffers - two
+ * @GST_VIDEO_TILE_MODE_ZFLIPZ_2X2: Every four adjacent blocks - two
  *    horizontally and two vertically are grouped together and are located
- *    in memory in Z or flipped Z order.
+ *    in memory in Z or flipped Z order. In case of odd rows, the last row
+ *    of blocks is arranged in linear order.
  *
  * Enum value describing the available tiling modes.
  */
