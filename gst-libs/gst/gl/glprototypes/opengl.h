48d92cbe88056bda42070af781a95add70fc9397
Wang Xin-yu (王昕宇) <comicfans44@gmail.com>
2014-01-10 02:50:14 +0800

    [861/906] glprototypes: Add missing glScalef declare
    https://bugzilla.gnome.org/show_bug.cgi?id=721903

--- gst-libs/gst/gl/glprototypes/opengl.h
+++ gst-libs/gst/gl/glprototypes/opengl.h
@@ -154,6 +154,8 @@ GST_GL_EXT_FUNCTION (void, Rotatef,
                      (GLfloat angle, GLfloat x, GLfloat y, GLfloat z))
 GST_GL_EXT_FUNCTION (void, Translatef,
                      (GLfloat x, GLfloat y, GLfloat z))
+GST_GL_EXT_FUNCTION (void, Scalef,
+                     (GLfloat x, GLfloat y, GLfloat z))
 GST_GL_EXT_FUNCTION (void, Lightfv,
                      (GLenum light, GLenum pname, const GLfloat *params))
 GST_GL_EXT_FUNCTION (void, ColorMaterial,
