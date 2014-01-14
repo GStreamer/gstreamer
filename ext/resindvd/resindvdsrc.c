078b82d6d1c609257b6528a9c4efb1ed4acd2bb8
Tim-Philipp Müller <tim@centricular.com>
2014-01-14 23:55:53 +0000

    resindvdsrc: fix minor memory leak when creating title info message

--- ext/resindvd/resindvdsrc.c
+++ ext/resindvd/resindvdsrc.c
@@ -23,6 +23,7 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #include <gmodule.h>
 #include <gst/gst.h>
@@ -2491,6 +2492,7 @@ rsn_dvdsrc_post_title_info (GstElement * element)
     } else {
       g_value_set_uint64 (&item, gst_util_uint64_scale (duration, GST_SECOND,
               90000));
+      free (times);
     }
     gst_value_array_append_value (&array, &item);
     g_value_unset (&item);
