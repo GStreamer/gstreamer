84044f28f714379497282aa95d98ea560045cec3
Gwenole Beauchesne <gwenole.beauchesne@intel.com>
2014-01-23 17:06:08 +0100

    libs: add missing file (libgstvaapi_priv_check.h).

---
+++ gst-libs/gst/vaapi/libgstvaapi_priv_check.h
@@ -0,0 +1,25 @@
+/*
+ *  libgstvaapi_priv_check.h - Check file is included from within libgstvaapi
+ *
+ *  Copyright (C) 2014 Intel Corporation
+ *    Author: Gwenole Beauchesne <gwenole.beauchesne@intel.com>
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2.1
+ *  of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free
+ *  Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ *  Boston, MA 02110-1301 USA
+ */
+
+#ifndef IN_LIBGSTVAAPI
+# error "This file is only meant to be included from within libgstvaapi"
+#endif
