larseje/gst-plugins-bad

mpegts: Add support for creating a NIT section
Functions that will enable user to create Network Information Tables.

gst-libs/gst/mpegts/gst-dvb-section.h
@@ -165,6 +165,11 @@ GType gst_mpegts_nit_get_type (void);
 GType gst_mpegts_nit_stream_get_type (void);
 
 const GstMpegTsNIT *gst_mpegts_section_get_nit (GstMpegTsSection *section);
+GstMpegTsSection *gst_mpegts_section_from_nit (GstMpegTsNIT *nit);
+
+GstMpegTsNIT *gst_mpegts_section_new_nit (void);
+GstMpegTsNITStream *gst_mpegts_section_new_nit_stream (void);
+
 
 /* BAT */
 

GStreamer/gstreamer

gst-libs/gst/mpegts/gst-dvb-section.h
@@ -166,6 +166,11 @@ GType gst_mpegts_nit_get_type (void);
 GType gst_mpegts_nit_stream_get_type (void);
 
 const GstMpegTsNIT *gst_mpegts_section_get_nit (GstMpegTsSection *section);
+GstMpegTsSection *gst_mpegts_section_from_nit (GstMpegTsNIT *nit);
+
+GstMpegTsNIT *gst_mpegts_nit_new (void);
+GstMpegTsNITStream *gst_mpegts_nit_stream_new (void);
+
 
 /* BAT */
 