ba0a3f65ebd27833d18ed5a504d3b6df1a22998f
Kipp Cannon <kcannon@cita.utoronto.ca>
2014-02-24 14:56:03 -0500

    debugutilsbad.c fails gst-indent check
    this patch is "gst-indent debugutilsbad.c"
    
    https://bugzilla.gnome.org/show_bug.cgi?id=725090

--- gst/debugutils/debugutilsbad.c
+++ gst/debugutils/debugutilsbad.c
@@ -23,33 +23,32 @@
 
 #include <gst/gst.h>
 
-GType gst_checksum_sink_get_type (void);
-GType fps_display_sink_get_type (void);
-GType gst_chop_my_data_get_type (void);
-GType gst_compare_get_type (void);
-GType gst_debug_spy_get_type (void);
-GType gst_watchdog_get_type (void);
+GType gst_checksum_sink_get_type(void);
+GType fps_display_sink_get_type(void);
+GType gst_chop_my_data_get_type(void);
+GType gst_compare_get_type(void);
+GType gst_debug_spy_get_type(void);
+GType gst_watchdog_get_type(void);
 
-static gboolean
-plugin_init (GstPlugin * plugin)
+static gboolean plugin_init(GstPlugin * plugin)
 {
-  gst_element_register (plugin, "checksumsink", GST_RANK_NONE,
-      gst_checksum_sink_get_type ());
-  gst_element_register (plugin, "fpsdisplaysink", GST_RANK_NONE,
-      fps_display_sink_get_type ());
-  gst_element_register (plugin, "chopmydata", GST_RANK_NONE,
-      gst_chop_my_data_get_type ());
-  gst_element_register (plugin, "compare", GST_RANK_NONE,
-      gst_compare_get_type ());
-  gst_element_register (plugin, "debugspy", GST_RANK_NONE,
-      gst_debug_spy_get_type ());
-  gst_element_register (plugin, "watchdog", GST_RANK_NONE,
-      gst_watchdog_get_type ());
+  gst_element_register(plugin, "checksumsink", GST_RANK_NONE,
+      gst_checksum_sink_get_type());
+  gst_element_register(plugin, "fpsdisplaysink", GST_RANK_NONE,
+      fps_display_sink_get_type());
+  gst_element_register(plugin, "chopmydata", GST_RANK_NONE,
+      gst_chop_my_data_get_type());
+  gst_element_register(plugin, "compare", GST_RANK_NONE,
+      gst_compare_get_type());
+  gst_element_register(plugin, "debugspy", GST_RANK_NONE,
+      gst_debug_spy_get_type());
+  gst_element_register(plugin, "watchdog", GST_RANK_NONE,
+      gst_watchdog_get_type());
 
   return TRUE;
 }
 
-GST_PLUGIN_DEFINE (GST_VERSION_MAJOR,
+GST_PLUGIN_DEFINE(GST_VERSION_MAJOR,
     GST_VERSION_MINOR,
     debugutilsbad,
     "Collection of elements that may or may not be useful for debugging",
