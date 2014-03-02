900c204eb9e00d5fac6e8bb9fa86ce8ba4d428db
Sebastian Rasmussen <sebras@hotmail.com>
2014-03-02 12:58:21 +0100

    videoformat: Remove duplicate/incorrect section
    Fixes https://bugzilla.gnome.org/show_bug.cgi?id=725521

--- gst-libs/gst/video/video-format.c
+++ gst-libs/gst/video/video-format.c
@@ -27,19 +27,6 @@
 #include <stdio.h>
 
 #include "video-format.h"
-
-/**
- * SECTION:gstvideo
- * @short_description: Support library for video operations
- *
- * <refsect2>
- * <para>
- * This library contains some helper functions and includes the
- * videosink and videofilter base classes.
- * </para>
- * </refsect2>
- */
-
 #include "video-orc.h"
 
 /* Line conversion to AYUV */
