GStreamer/gstreamer

commit 71dee6c3843d02d9d41bbb353cb3fa653190018d

launcher: --sync: Only update/clone git repo if specified
Allows:
  * handling non-git-based asset directory
  * working offline
  * working without forcing updates

validate/tools/launcher/main.py
@@ -123,6 +123,8 @@ def main():
     assets_group.add_option("", "--remote-assets-url", dest="remote_assets_url",
                       default=DEFAULT_GST_QA_ASSETS_REPO,
                       help="Url to the remote assets")
+    assets_group.add_option("-S", "--sync", dest="sync", action="store_true",
+                            default=False, help="Synchronize asset repository")
     parser.add_option_group(assets_group)
 
     loggable.init("GST_VALIDATE_LAUNCHER_DEBUG", True, False)
@@ -161,7 +163,7 @@ def main():
 
     tests_launcher.set_settings(options, args)
 
-    if options.remote_assets_url:
+    if options.remote_assets_url and options.sync:
         if os.path.exists(options.clone_dir):
             launch_command("cd %s && %s" % (options.clone_dir,
                                             options.update_assets_command))

commit 36f9ba8f52fa94e052215b445cf3cbc652bf4bf3

launcher: Allow user to set media-files directory
That was broken by 71dee6c3843d02d9d41bbb353cb3fa653190018d

validate/tools/launcher/main.py
@@ -157,8 +157,10 @@ def main():
     if options.http_server_dir is None:
         options.http_server_dir = options.paths
 
-    if not options.sync and not os.path.exists(options.clone_dir):
-        printc("Media path (%s) does not exists. Forgot to run --sync ?" % options.clone_dir, Colors.FAIL, True)
+    if not options.sync and not os.path.exists(options.clone_dir) and \
+            options.clone_dir == os.path.join(options.clone_dir, MEDIAS_FOLDER):
+        printc("Media path (%s) does not exists. Forgot to run --sync ?"
+               % options.clone_dir, Colors.FAIL, True)
         return -1
 
     tests_launcher.set_settings(options, args)