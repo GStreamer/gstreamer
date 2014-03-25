183e441d887ad85c73f5a0f70751df1e3801db50
Wim Taymans <wtaymans@redhat.com>
2014-03-25 11:07:34 +0100

    rtsptransport: UDP is also default for SAVP and AVPF

--- gst-libs/gst/rtsp/gstrtsptransport.c
+++ gst-libs/gst/rtsp/gstrtsptransport.c
@@ -655,7 +655,10 @@ gst_rtsp_transport_as_text (GstRTSPTransport * transport)
   g_ptr_array_add (strs, g_ascii_strup (tmp, -1));
 
   if (transport->trans != GST_RTSP_TRANS_RTP ||
-      transport->profile != GST_RTSP_PROFILE_AVP ||
+      (transport->profile != GST_RTSP_PROFILE_AVP &&
+          transport->profile != GST_RTSP_PROFILE_SAVP &&
+          transport->profile != GST_RTSP_PROFILE_AVPF &&
+          transport->profile != GST_RTSP_PROFILE_SAVPF) ||
       transport->lower_transport == GST_RTSP_LOWER_TRANS_TCP) {
     g_ptr_array_add (strs, g_strdup ("/"));
 
