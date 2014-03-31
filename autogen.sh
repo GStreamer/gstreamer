f9f7cbe93e6a45c4ca3f3e34e2d9369da6b5d988
Andres G. Aragoneses <knocte@gmail.com>
2014-03-31 18:02:09 +0200

    autogen.sh: don't run ./configure if autoreconf failed
    The following line could be seen in the middle of the log:
    autoreconf: automake failed with exit status: 1
    
    Clearly the configuration phase should not continue from there.

--- autogen.sh
+++ autogen.sh
@@ -4,5 +4,5 @@ if [ $? -ne 0 ] ; then
        echo "Please install autoconf" && exit 1;
 fi
 mkdir -p m4
-autoreconf  -i --force --warnings=none -I . -I m4
+autoreconf  -i --force --warnings=none -I . -I m4 && \
 ./configure --enable-maintainer-mode $*
