f4181f0223f454ab24024c632a9e996d61950abd
Andres G. Aragoneses <knocte@gmail.com>
2014-03-31 17:46:02 +0200

    autogen.sh: advice about installing autoconf if autoreconf not found

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-
+which autoreconf > /dev/null
+if [ $? -ne 0 ] ; then
+       echo "Please install autoconf" && exit 1;
+fi
 mkdir -p m4
 autoreconf  -i --force --warnings=none -I . -I m4
 ./configure --enable-maintainer-mode $*
