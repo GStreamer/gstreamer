2b8de2d1bcf77d738b2b75138ba0e7fec12cec64
Julien Isorce <julien.isorce@collabora.co.uk>
2014-04-30 15:28:35 +0100

    gleffects: use new helper functions to avoid duplicating the same vertex shader text
    And also use the default fragment shader text for the identity effect

--- ext/gl/effects/gstgleffectssources.c
+++ ext/gl/effects/gstgleffectssources.c
@@ -64,27 +64,6 @@ fill_gaussian_kernel (float *kernel, int size, float sigma)
 
 /* *INDENT-OFF* */
 
-/* Vertex shader */
-const gchar *vertex_shader_source =
-  "attribute vec4 a_position;"
-  "attribute vec2 a_texCoord;"
-  "varying vec2 v_texCoord;"
-  "void main()"
-  "{"
-  "   gl_Position = a_position;"
-  "   v_texCoord = a_texCoord;"
-  "}";
-
-/* Identity effect */
-const gchar *identity_fragment_source =
-  "precision mediump float;"
-  "varying vec2 v_texCoord;"
-  "uniform sampler2D tex;"
-  "void main()"
-  "{"
-  "  gl_FragColor = texture2D(tex, v_texCoord);"
-  "}";
-
 /* Mirror effect */
 #if GST_GL_HAVE_OPENGL
 const gchar *mirror_fragment_source_opengl =
