58a574e523d8ca2eaab9946c67114bd24c8939e8
Luis de Bethencourt <luis@debethencourt.com>
2014-05-01 14:52:24 -0400

    scripts/git-version.sh: add more modules
    Add more git repositories to check (so git-version.sh is consistent with
    gst-uninstalled) and display the date of the last commit, which is more valuable
    information than the last commit's hash.

--- scripts/git-version.sh
+++ scripts/git-version.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
-# display the latest commit in each branch of all gstreamer modules.
+# display the latest commit in the current branch of all gstreamer modules.
 # run this from a directory that contains the checkouts for each of the
 # modules
 
 for m in \
   gstreamer gst-plugins-base \
   gst-plugins-good gst-plugins-ugly gst-plugins-bad \
-  gst-ffmpeg \
-  gst-python \
+  gst-ffmpeg gst-libav \
+  gst-editing-services \
+  gst-python gstreamer-sharp \
   gnonlin \
+  gst-rtsp-server \
+  gst-omx \
+  gst-devtools \
   ; do
   if test -d $m; then
     cd $m
       echo $m
       git branch | grep '*'
-      git log | head -n 1
+      git log | head -n 3 | sed -n '1p;3p'
+      echo ""
     cd ..
   fi
 done
