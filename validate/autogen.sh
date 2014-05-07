08898ff21962724f9396ee3be14b116fe134a505
Thibault Saunier <tsaunier@gnome.org>
2014-05-07 09:50:28 +0200

    validate: Properly set the pre commit hook

--- validate/autogen.sh
+++ validate/autogen.sh
@@ -25,10 +25,10 @@ fi
 . common/gst-autogen.sh
 
 # install pre-commit hook for doing clean commits
-if test ! \( -x .git/hooks/pre-commit -a -L .git/hooks/pre-commit \);
+if test ! \( -x ../.git/hooks/pre-commit -a -L ../.git/hooks/pre-commit \);
 then
-    rm -f .git/hooks/pre-commit
-    ln -s ../../common/hooks/pre-commit.hook .git/hooks/pre-commit
+    rm -f ../.git/hooks/pre-commit
+    ln -s ../../validate/common/hooks/pre-commit.hook ../.git/hooks/pre-commit
 fi
 
 # GNU gettext automake support doesn't get along with git.
