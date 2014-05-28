2b44f2ffec18b6dd1796f921c3352a92337cdbf7
Andrés G. Aragoneses <knocte@gmail.com>
2014-05-28 13:03:33 +0200

    custom: move copyright and author info to top of the file
    The headers for this file were not consistent with the rest of
    the files.

--- sources/custom/Version.cs
+++ sources/custom/Version.cs
@@ -1,3 +1,12 @@
+//
+// Version.cs: Lightweight Version Object for GStreamer
+//
+// Authors:
+//   Aaron Bockover <abockover@novell.com>
+//   Stephan Sundermann <stephansundermann@gmail.com>
+//
+// Copyright (C) 2006 Novell, Inc.
+//
 // This library is free software; you can redistribute it and/or
 // modify it under the terms of the GNU Lesser General Public
 // License as published by the Free Software Foundation; either
@@ -11,16 +20,6 @@
 // You should have received a copy of the GNU Lesser General Public
 // License along with this library; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-
-//
-// Version.cs: Lightweight Version Object for GStreamer
-//
-// Authors:
-//   Aaron Bockover (abockover@novell.com)
-//	 Stephan Sundermann (stephansundermann@gmail.com)
-//
-// Copyright (C) 2006 Novell, Inc.
-//
 
 using System;
 using System.Runtime.InteropServices;
