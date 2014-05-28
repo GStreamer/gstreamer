c878ce3a35dbe90687277be81646ed02c8a1dfcd
Andrés G. Aragoneses <knocte@gmail.com>
2014-05-28 02:44:39 +0200

    custom: rename NavigationQuery.cs to NavigationAdapter.cs
    The filename wasn't reflecting the class name.

--- sources/custom/NavigationAdapter.cs
+++ sources/custom/NavigationAdapter.cs
@@ -1,5 +1,5 @@
 // Copyright (C) 2013  Stephan Sundermann <stephansundermann@gmail.com>
-// 
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as
 // published by the Free Software Foundation, either version 3 of the
