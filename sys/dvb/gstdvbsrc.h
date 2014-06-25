3d22f686b69d759ffec0250d4d45aa963f97b476
Reynaldo H. Verdejo Pinochet <r.verdejo@sisa.samsung.com>
2014-06-24 22:15:53 -0400

    dvbsrc: add copyright/license stub to header

--- sys/dvb/gstdvbsrc.h
+++ sys/dvb/gstdvbsrc.h
@@ -1,3 +1,24 @@
+/* GStreamer DVB source
+ * Copyright (C) 2006 Zaheer Abbas Merali <zaheerabbas at merali
+ *                                         dot org>
+ * Copyright (C) 2014 Samsung Electronics. All rights reserved.
+ *     @Author: Reynaldo H. Verdejo Pinochet <r.verdejo@sisa.samsung.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
 
 #ifndef __GST_DVBSRC_H__
 #define __GST_DVBSRC_H__
