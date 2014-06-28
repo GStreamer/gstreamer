97cf5acb209a29f7154261e80bc0028b29d7c31d
Sebastian Dröge <sebastian@centricular.com>
2014-06-28 17:01:52 +0200

    badvideo: Rename videoconvert functions to prevent conflicts with static linking
    https://bugzilla.gnome.org/show_bug.cgi?id=728443

--- gst-libs/gst/video/gstvideoaggregator.c
+++ gst-libs/gst/video/gstvideoaggregator.c
@@ -139,7 +139,7 @@ gst_videoaggregator_pad_finalize (GObject * o)
   GstVideoAggregatorPad *vaggpad = GST_VIDEO_AGGREGATOR_PAD (o);
 
   if (vaggpad->priv->convert)
-    videoconvert_convert_free (vaggpad->priv->convert);
+    badvideoconvert_convert_free (vaggpad->priv->convert);
   vaggpad->priv->convert = NULL;
 
   G_OBJECT_CLASS (gst_videoaggregator_pad_parent_class)->finalize (o);
@@ -433,7 +433,7 @@ gst_videoaggregator_update_converters (GstVideoAggregator * vagg)
       continue;
 
     if (pad->priv->convert)
-      videoconvert_convert_free (pad->priv->convert);
+      badvideoconvert_convert_free (pad->priv->convert);
 
     pad->priv->convert = NULL;
 
@@ -446,7 +446,7 @@ gst_videoaggregator_update_converters (GstVideoAggregator * vagg)
       GST_DEBUG_OBJECT (pad, "This pad will be converted from %d to %d",
           GST_VIDEO_INFO_FORMAT (&pad->info),
           GST_VIDEO_INFO_FORMAT (&best_info));
-      pad->priv->convert = videoconvert_convert_new (&pad->info, &best_info);
+      pad->priv->convert = badvideoconvert_convert_new (&pad->info, &best_info);
       pad->need_conversion_update = TRUE;
       if (!pad->priv->convert) {
         g_free (colorimetry);
@@ -1059,7 +1059,8 @@ prepare_frames (GstVideoAggregator * vagg, GstVideoAggregatorPad * pad)
         return FALSE;
       }
 
-      videoconvert_convert_convert (pad->priv->convert, converted_frame, frame);
+      badvideoconvert_convert_convert (pad->priv->convert, converted_frame,
+          frame);
       pad->converted_buffer = converted_buf;
       gst_video_frame_unmap (frame);
     } else {
