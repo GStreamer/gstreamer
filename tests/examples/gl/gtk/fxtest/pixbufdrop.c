1533fc7e2d0bc904c229083ef415cd8f04b57ee0
Tim-Philipp Müller <tim@centricular.com>
2014-07-17 10:05:47 +0100

    examples: fix compilation of gl fxtest example on win32 with Gtk+ 3.0
    https://bugzilla.gnome.org/show_bug.cgi?id=733063

--- tests/examples/gl/gtk/fxtest/pixbufdrop.c
+++ tests/examples/gl/gtk/fxtest/pixbufdrop.c
@@ -141,10 +141,6 @@ on_drag_data_received (GtkWidget * widget,
     GtkSelectionData * seldata, guint inf, guint time, gpointer data)
 {
   SourceData *userdata = g_new0 (SourceData, 1);
-#ifdef G_OS_WIN32
-  gchar *filename =
-      g_filename_from_uri ((const gchar *) seldata->data, NULL, NULL);
-#else
   GdkPixbufFormat *format;
   gchar **uris = gtk_selection_data_get_uris (seldata);
   gchar *filename = NULL;
@@ -156,7 +152,6 @@ on_drag_data_received (GtkWidget * widget,
   g_return_if_fail (format);
   g_print ("received %s image: %s\n", filename,
       gdk_pixbuf_format_get_name (format));
-#endif
 
   userdata->nick = (gchar *) "location";
   userdata->value = g_strdup (filename);
