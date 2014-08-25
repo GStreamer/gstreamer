255e28dde3a575f60afdf59f99626e23a42b910b
Sebastian Dröge <sebastian@centricular.com>
2014-08-25 13:14:36 +0300

    audiodecoder: Don't ignore ::start/stop return values

--- gst-libs/gst/audio/gstaudiodecoder.c
+++ gst-libs/gst/audio/gstaudiodecoder.c
@@ -2655,7 +2655,7 @@ gst_audio_decoder_stop (GstAudioDecoder * dec)
   if (ret)
     dec->priv->active = FALSE;
 
-  return TRUE;
+  return ret;
 }
 
 static gboolean
@@ -2678,7 +2678,7 @@ gst_audio_decoder_start (GstAudioDecoder * dec)
   if (ret)
     dec->priv->active = TRUE;
 
-  return TRUE;
+  return ret;
 }
 
 static void
