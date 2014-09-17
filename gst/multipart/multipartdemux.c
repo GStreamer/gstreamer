3bf81ad12ccf8868e90ab32054392fc172dd90f3
Ognyan Tonchev <ognyan@axis.com>
2014-09-17 16:23:21 +0200

    multipartdemux: do not leak new stream event
    https://bugzilla.gnome.org/show_bug.cgi?id=736805

--- gst/multipart/multipartdemux.c
+++ gst/multipart/multipartdemux.c
@@ -347,6 +347,7 @@ gst_multipart_find_pad_by_mime (GstMultipartDemux * demux, gchar * mime,
 
     gst_pad_store_sticky_event (pad, event);
     g_free (stream_id);
+    gst_event_unref (event);
 
     /* take the mime type, convert it to the caps name */
     capsname = gst_multipart_demux_get_gstname (demux, mime);
