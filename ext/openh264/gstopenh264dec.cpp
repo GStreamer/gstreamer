1dfcdb9ce313dce1d29cff529a2d939bf8b241e3
Sebastian Dröge <sebastian@centricular.com>
2014-10-02 16:21:37 +0300

    openh264dec: GstVideoDecoder works on frames, not sub-frames (e.g. NALs)
    And we can just pass complete AUs to the decoder without problems.

--- ext/openh264/gstopenh264dec.cpp
+++ ext/openh264/gstopenh264dec.cpp
@@ -81,7 +81,7 @@ struct _GstOpenh264DecPrivate
 static GstStaticPadTemplate gst_openh264dec_sink_template = GST_STATIC_PAD_TEMPLATE("sink",
     GST_PAD_SINK,
     GST_PAD_ALWAYS,
-    GST_STATIC_CAPS("video/x-h264, stream-format=(string)byte-stream, alignment=(string)nal"));
+    GST_STATIC_CAPS("video/x-h264, stream-format=(string)byte-stream, alignment=(string)au"));
 
 static GstStaticPadTemplate gst_openh264dec_src_template = GST_STATIC_PAD_TEMPLATE("src",
     GST_PAD_SRC,
