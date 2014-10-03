bc8e1465dc1029019e378eb901cf562bdf4d2f4e
Sebastian Dröge <sebastian@centricular.com>
2014-10-03 17:20:28 +0300

    openh264dec: On ::finish() stop trying if the decoder returned that no output is available

--- ext/openh264/gstopenh264dec.cpp
+++ ext/openh264/gstopenh264dec.cpp
@@ -347,7 +347,7 @@ gst_openh264dec_handle_frame (GstVideoDecoder * decoder,
 
   /* No output available yet */
   if (dst_buf_info.iBufferStatus != 1) {
-    return GST_FLOW_OK;
+    return (frame ? GST_FLOW_OK : GST_FLOW_EOS);
   }
 
   actual_width = dst_buf_info.UsrData.sSystemBuffer.iWidth;
