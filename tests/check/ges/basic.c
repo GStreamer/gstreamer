d106bc24f763b1ff724012fe34e0f59116ca597f
Tim-Philipp Müller <tim@centricular.com>
2014-10-26 20:24:09 +0000

    tests: don't leak clips list in basic unit test

--- tests/check/ges/basic.c
+++ tests/check/ges/basic.c
@@ -498,6 +498,8 @@ GST_START_TEST (test_ges_timeline_remove_track)
   ASSERT_OBJECT_REFCOUNT (timeline, "1 for the us", 1);
   tmp = ges_layer_get_clips (layer);
   assert_equals_int (g_list_length (tmp), 3);
+  g_list_foreach (tmp, (GFunc) gst_object_unref, NULL);
+  g_list_free (tmp);
 
   check_destroyed (G_OBJECT (timeline), G_OBJECT (layer), t1, t2, t3, NULL);
 }
