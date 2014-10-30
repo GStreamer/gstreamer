ef9dc6c9e4fdf1b349a76ad9f39da168a1906355
Aleix Conchillo Flaqué <aleix@oblong.com>
2014-10-29 21:01:39 -0700

    rtsp-client: mikey memory leaks
    https://bugzilla.gnome.org/show_bug.cgi?id=739383

--- gst/rtsp-server/rtsp-client.c
+++ gst/rtsp-server/rtsp-client.c
@@ -1607,6 +1607,7 @@ handle_mikey_data (GstRTSPClient * client, GstRTSPContext * ctx,
     gst_caps_unref (caps);
   }
   gst_mikey_message_unref (msg);
+  gst_buffer_unref (key);
 
   return TRUE;
 
@@ -1694,7 +1695,9 @@ handle_keymgmt (GstRTSPClient * client, GstRTSPContext * ctx, gchar * keymgmt)
         handle_mikey_data (client, ctx, data, size);
       }
     }
+    g_strfreev (split);
   }
+  g_strfreev (specs);
   return TRUE;
 }
 
