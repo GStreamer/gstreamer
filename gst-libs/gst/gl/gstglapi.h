85f67667b2e5d6de6b1b3d59cd08c44c12a30955
Julien Isorce <julien.isorce@gmail.com>
2014-11-03 23:08:09 +0000

    gl/cocoa: include gl3.h to have GL_CONTEXT_PROFILE_MASK

--- gst-libs/gst/gl/gstglapi.h
+++ gst-libs/gst/gl/gstglapi.h
@@ -71,6 +71,10 @@
 # ifdef __APPLE__
 #  include <OpenGL/OpenGL.h>
 #  include <OpenGL/gl.h>
+#  if MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_7
+#   define GL_DO_NOT_WARN_IF_MULTI_GL_VERSION_HEADERS_INCLUDED
+#   include <OpenGL/gl3.h>
+#  endif
 #  include <OpenGL/glu.h>
 # else
 #  include <GL/gl.h>
