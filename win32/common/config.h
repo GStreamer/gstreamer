1a2ce9a291a8be5cddc6ca85c3bc557ac4b6ad50
Sebastian Dröge <sebastian@centricular.com>
2014-11-06 13:16:19 +0100

    Release 1.4.4

--- win32/common/config.h
+++ win32/common/config.h
@@ -61,7 +61,7 @@
 #define GST_PACKAGE_ORIGIN "Unknown package origin"
 
 /* GStreamer package release date/time for plugins as YYYY-MM-DD */
-#define GST_PACKAGE_RELEASE_DATETIME "2014-09-24"
+#define GST_PACKAGE_RELEASE_DATETIME "2014-11-06"
 
 /* Define if static plugins should be built */
 #undef GST_PLUGIN_BUILD_STATIC
@@ -248,7 +248,7 @@
 #define PACKAGE_NAME "GStreamer Ugly Plug-ins"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "GStreamer Ugly Plug-ins 1.4.3"
+#define PACKAGE_STRING "GStreamer Ugly Plug-ins 1.4.4"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "gst-plugins-ugly"
@@ -257,7 +257,7 @@
 #undef PACKAGE_URL
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.4.3"
+#define PACKAGE_VERSION "1.4.4"
 
 /* directory where plugins are located */
 #ifdef _DEBUG
@@ -288,7 +288,7 @@
 #undef TARGET_CPU
 
 /* Version number of package */
-#define VERSION "1.4.3"
+#define VERSION "1.4.4"
 
 /* Define WORDS_BIGENDIAN to 1 if your processor stores words with the most
    significant byte first (like Motorola and SPARC, unlike Intel). */
