480e661618d694ee9f09830f59a579f0dd9897bd
Sebastian Dröge <sebastian@centricular.com>
2014-11-09 10:47:14 +0100

    capsfilter-renegotiation: Use assertions from libcheck for more information on failures

--- tests/check/pipelines/capsfilter-renegotiation.c
+++ tests/check/pipelines/capsfilter-renegotiation.c
@@ -104,13 +104,13 @@ run_capsfilter_renegotiation (const gchar * launch_line)
   current_caps = NULL;
 
   pipeline = gst_parse_launch (launch_line, NULL);
-  g_assert (pipeline);
+  fail_unless (pipeline != NULL);
 
   capsfilter = gst_bin_get_by_name (GST_BIN (pipeline), "cf");
-  g_assert (capsfilter);
+  fail_unless (capsfilter != NULL);
 
   sink = gst_bin_get_by_name (GST_BIN (pipeline), "sink");
-  g_assert (sink);
+  fail_unless (sink != NULL);
 
   pad = gst_element_get_static_pad (sink, "sink");
   gst_pad_add_probe (pad, GST_PAD_PROBE_TYPE_BUFFER, buffer_probe, capsfilter,
@@ -125,8 +125,8 @@ run_capsfilter_renegotiation (const gchar * launch_line)
   msg = gst_bus_timed_pop_filtered (bus, GST_CLOCK_TIME_NONE,
       GST_MESSAGE_EOS | GST_MESSAGE_ERROR);
 
-  g_assert (GST_MESSAGE_TYPE (msg) == GST_MESSAGE_EOS);
-  g_assert (caps_change == 4);
+  fail_unless_equals_int (GST_MESSAGE_TYPE (msg), GST_MESSAGE_EOS);
+  fail_unless_equals_int (caps_change, 4);
 
   gst_element_set_state (pipeline, GST_STATE_NULL);
 
