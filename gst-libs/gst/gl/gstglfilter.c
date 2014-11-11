36ade75f4a618d66163ba71035048447fe2871de
Matthew Waters <matthew@centricular.com>
2014-11-11 14:23:55 +1100

    gl: remove the width/height fields from the caps to support frame resizing
    It was previously only occuring with sysmem caps features
    
    https://bugzilla.gnome.org/show_bug.cgi?id=739334

--- gst-libs/gst/gl/gstglfilter.c
+++ gst-libs/gst/gl/gstglfilter.c
@@ -718,7 +718,8 @@ gst_gl_filter_caps_remove_format_info (GstCaps * caps)
         && gst_caps_features_is_equal (f,
             GST_CAPS_FEATURES_MEMORY_SYSTEM_MEMORY))
       gst_structure_remove_fields (st, "format", "colorimetry", "chroma-site",
-          "width", "height", NULL);
+          NULL);
+    gst_structure_remove_fields (st, "width", "height", NULL);
 
     gst_caps_append_structure_full (res, st, gst_caps_features_copy (f));
   }
