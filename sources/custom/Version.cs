a53cb1c9119b6663c829d1300a38288fc91c9775
Stephan Sundermann <ssundermann@gnome.org>
2014-11-25 15:56:17 +0100

    Version: Reformat

--- sources/custom/Version.cs
+++ sources/custom/Version.cs
@@ -25,57 +25,60 @@
 using System;
 using System.Runtime.InteropServices;
 
-namespace Gst {
-  public static class Version {
-    private static uint major;
-    private static uint minor;
-    private static uint micro;
-    private static uint nano;
-    private static string version_string;
+namespace Gst
+{
+	public static class Version
+	{
+		private static uint major;
+		private static uint minor;
+		private static uint micro;
+		private static uint nano;
+		private static string version_string;
 
-    static Version() {
-      gst_version (out major, out minor, out micro, out nano);
-    }
+		static Version ()
+		{
+			gst_version (out major, out minor, out micro, out nano);
+		}
 
-    public static string Description {
-      get {
-        if (version_string == null) {
-          IntPtr version_string_ptr = gst_version_string();
-          version_string = GLib.Marshaller.Utf8PtrToString (version_string_ptr);
-        }
+		public static string Description {
+			get {
+				if (version_string == null) {
+					IntPtr version_string_ptr = gst_version_string ();
+					version_string = GLib.Marshaller.Utf8PtrToString (version_string_ptr);
+				}
 
-        return version_string;
-      }
-    }
+				return version_string;
+			}
+		}
 
-    public static uint Major {
-      get {
-        return major;
-      }
-    }
+		public static uint Major {
+			get {
+				return major;
+			}
+		}
 
-    public static uint Minor {
-      get {
-        return minor;
-      }
-    }
+		public static uint Minor {
+			get {
+				return minor;
+			}
+		}
 
-    public static uint Micro {
-      get {
-        return micro;
-      }
-    }
+		public static uint Micro {
+			get {
+				return micro;
+			}
+		}
 
-    public static uint Nano {
-      get {
-        return nano;
-      }
-    }
+		public static uint Nano {
+			get {
+				return nano;
+			}
+		}
 
-    [DllImport ("libgstreamer-1.0-0.dll") ]
-    private static extern void gst_version (out uint major, out uint minor, out uint micro, out uint nano);
+		[DllImport ("libgstreamer-1.0-0.dll")]
+		private static extern void gst_version (out uint major, out uint minor, out uint micro, out uint nano);
 
-    [DllImport ("libgstreamer-1.0-0.dll") ]
-    private static extern IntPtr gst_version_string();
-  }
+		[DllImport ("libgstreamer-1.0-0.dll")]
+		private static extern IntPtr gst_version_string ();
+	}
 }
