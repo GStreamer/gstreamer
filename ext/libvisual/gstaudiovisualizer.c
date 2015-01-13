d3a73827e017b3b5967664b2cb83fe0de185f700
Luis de Bethencourt <luis.bg@samsung.com>
2015-01-13 16:09:49 +0000

    Revert "visual: remove unnecessary variable"
    This reverts commit a91d521a3602f33083405467db9454d422b9da1b.
    
    Being a base class it is better to check the value instead of ignoring it since
    a child class could be created that returns valuable information.

--- ext/libvisual/gstaudiovisualizer.c
+++ ext/libvisual/gstaudiovisualizer.c
@@ -726,6 +726,7 @@ gst_audio_visualizer_src_setcaps (GstAudioVisualizer * scope, GstCaps * caps)
 {
   GstVideoInfo info;
   GstAudioVisualizerClass *klass;
+  gboolean res;
 
   if (!gst_video_info_from_caps (&info, caps))
     goto wrong_caps;
@@ -750,7 +751,7 @@ gst_audio_visualizer_src_setcaps (GstAudioVisualizer * scope, GstCaps * caps)
       GST_MAP_READWRITE);
 
   if (klass->setup)
-    klass->setup (scope);
+    res = klass->setup (scope);
 
   GST_DEBUG_OBJECT (scope, "video: dimension %dx%d, framerate %d/%d",
       GST_VIDEO_INFO_WIDTH (&info), GST_VIDEO_INFO_HEIGHT (&info),
@@ -761,7 +762,9 @@ gst_audio_visualizer_src_setcaps (GstAudioVisualizer * scope, GstCaps * caps)
   gst_pad_set_caps (scope->srcpad, caps);
 
   /* find a pool for the negotiated caps now */
-  return gst_audio_visualizer_do_bufferpool (scope, caps);
+  res = gst_audio_visualizer_do_bufferpool (scope, caps);
+
+  return res;
 
   /* ERRORS */
 wrong_caps:
