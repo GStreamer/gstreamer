64b451c1704b4f5607f6fa26e38d7db57600ecbf
Edward Hervey <bilboed@bilboed.com>
2015-01-21 14:10:06 +0100

    gstpad: Fix debug message

--- gst/gstpad.c
+++ gst/gstpad.c
@@ -4839,7 +4839,7 @@ gst_pad_push_event_unchecked (GstPad * pad, GstEvent * event,
   /* Note: we gave away ownership of the event at this point but we can still
    * print the old pointer */
   GST_LOG_OBJECT (pad,
-      "sent event %p to (%s) peerpad %" GST_PTR_FORMAT ", ret %s", event,
+      "sent event %p (%s) to peerpad %" GST_PTR_FORMAT ", ret %s", event,
       gst_event_type_get_name (event_type), peerpad, gst_flow_get_name (ret));
 
   gst_object_unref (peerpad);
