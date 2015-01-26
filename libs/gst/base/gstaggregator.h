5f176b724b9363e2bc33bd31821dc6da1b1b1453
Thibault Saunier <tsaunier@gnome.org>
2015-01-26 11:25:54 +0100

    aggregator: Hide GstAggregatorPad buffer and EOS fileds
    And add a getter for the EOS.
    
    The user should always use the various getters to access
    those fields
    
    https://bugzilla.gnome.org/show_bug.cgi?id=742684

--- libs/gst/base/gstaggregator.h
+++ libs/gst/base/gstaggregator.h
@@ -70,9 +70,7 @@ struct _GstAggregatorPad
   GstPad                       parent;
 
   /* Protected by the pad's object lock */
-  GstBuffer                 *  buffer;
   GstSegment                   segment;
-  gboolean                     eos;
 
   /* < Private > */
   GstAggregatorPadPrivate   *  priv;
@@ -107,6 +105,7 @@ GType gst_aggregator_pad_get_type           (void);
 GstBuffer * gst_aggregator_pad_steal_buffer (GstAggregatorPad *  pad);
 GstBuffer * gst_aggregator_pad_steal_buffer_unlocked (GstAggregatorPad *  pad);
 GstBuffer * gst_aggregator_pad_get_buffer   (GstAggregatorPad *  pad);
+gboolean    gst_aggregator_pad_is_eos       (GstAggregatorPad *  pad);
 
 /*********************
  * GstAggregator API *
