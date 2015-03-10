1f89d6d7395376e3f891c1c0404a0585977a186c
Luis de Bethencourt <luis.bg@samsung.com>
2015-03-10 16:42:44 +0000

    check: duplicate code branches
    CID #1226446

--- libs/gst/check/libcheck/check_run.c
+++ libs/gst/check/libcheck/check_run.c
@@ -582,11 +582,7 @@ set_fork_info (TestResult * tr, int status, int signal_expected,
         free (tr->msg);
       }
       tr->msg = exit_msg (exit_status);
-      if (exit_status == allowed_exit_value) {
-        tr->rtype = CK_FAILURE; /* normal exit status */
-      } else {
-        tr->rtype = CK_FAILURE; /* early exit */
-      }
+      tr->rtype = CK_FAILURE;   /* normal exit status */
     }
   }
 }
