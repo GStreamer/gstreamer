f532a72406fedf1052e2a0a2a81db3a6cb81b2e9
Ramiro Polla <ramiro.polla@collabora.co.uk>
2015-03-13 18:24:43 +0000

    sdpdemux: properly escape percent sign in documentation

--- gst/sdp/gstsdpdemux.c
+++ gst/sdp/gstsdpdemux.c
@@ -20,7 +20,7 @@
  * SECTION:element-sdpdemux
  *
  * sdpdemux currently understands SDP as the input format of the session description.
- * For each stream listed in the SDP a new stream_%u pad will be created
+ * For each stream listed in the SDP a new stream_\%u pad will be created
  * with caps derived from the SDP media description. This is a caps of mime type
  * "application/x-rtp" that can be connected to any available RTP depayloader
  * element. 
