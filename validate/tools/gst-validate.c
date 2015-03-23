5456ddbae0865f9ecd715eaa92ae93e4e50830ec
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2015-03-23 13:36:45 +0100

    call gst_deinit() when we are done
    More valgrind friendly.

--- validate/tools/gst-validate.c
+++ validate/tools/gst-validate.c
@@ -624,6 +624,7 @@ exit:
 #ifdef G_OS_UNIX
   g_source_remove (signal_watch_id);
 #endif
+  gst_deinit ();
 
   g_print ("\n=======> Test %s (Return value: %i)\n\n",
       ret == 0 ? "PASSED" : "FAILED", ret);
