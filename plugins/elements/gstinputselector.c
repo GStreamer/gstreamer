GStreamer/gstreamer

commit bc1ec4ec5f4879747c0535a163e704afa7f33654

inputselector: Use the same waiting function for EOS and non-EOS waiting

plugins/elements/gstinputselector.c
@@ -434,10 +434,12 @@ gst_selector_pad_iterate_linked_pads (GstPad * pad, GstObject * parent)
   return it;
 }
 
+/* must be called with the SELECTOR_LOCK, will block while the pad is blocked 
+ * or return TRUE when flushing */
 static gboolean
-gst_input_selector_eos_wait (GstInputSelector * self, GstSelectorPad * pad)
+gst_input_selector_wait (GstInputSelector * self, GstSelectorPad * pad)
 {
-  while (!self->eos && !self->flushing && !pad->flushing) {
+  while (!self->eos && self->blocked && !self->flushing && !pad->flushing) {
     /* we can be unlocked here when we are shutting down (flushing) or when we
      * get unblocked */
     GST_INPUT_SELECTOR_WAIT (self);
@@ -534,7 +536,7 @@ gst_selector_pad_event (GstPad * pad, GstObject * parent, GstEvent * event)
 
       if (!forward) {
         /* blocked until active the sind pad or flush */
-        gst_input_selector_eos_wait (sel, selpad);
+        gst_input_selector_wait (sel, selpad);
         forward = TRUE;
       } else {
         /* Notify all waiting pads about going EOS now */
@@ -627,19 +629,6 @@ gst_selector_pad_query (GstPad * pad, GstObject * parent, GstQuery * query)
 
 done:
   return res;
-}
-
-/* must be called with the SELECTOR_LOCK, will block while the pad is blocked 
- * or return TRUE when flushing */
-static gboolean
-gst_input_selector_wait (GstInputSelector * self, GstSelectorPad * pad)
-{
-  while (self->blocked && !self->flushing && !pad->flushing) {
-    /* we can be unlocked here when we are shutting down (flushing) or when we
-     * get unblocked */
-    GST_INPUT_SELECTOR_WAIT (self);
-  }
-  return self->flushing;
 }
 
 static GstClockTime

commit ce663311e1f00df5117b0249931e4ac960123cb8

input-selector: Fix waiting on EOS
This apparently got broken by bc1ec4e. Since self->blocked is always
FALSE, gst_input_selector_wait never actually waits.

Using (!self->eos || self->blocked) && ... as the loop condition would
be incorrect as well, because then the other call to the function in
_chain would block until EOS, so the functions cannot be merged trivially.

Since blocking is obsolete, gst_input_selector_wait will get removed anyway.
As such, just inline the loop.

https://bugzilla.gnome.org/show_bug.cgi?id=746518

plugins/elements/gstinputselector.c
@@ -535,9 +535,10 @@ gst_selector_pad_event (GstPad * pad, GstObject * parent, GstEvent * event)
       selpad->eos = TRUE;
 
       if (!forward) {
-        /* blocked until active the sind pad or flush */
-        gst_input_selector_wait (sel, selpad);
         forward = TRUE;
+        /* Wait until we're the active sink pad or we're flushing */
+        while (!sel->eos && !sel->flushing && !selpad->flushing)
+          GST_INPUT_SELECTOR_WAIT (sel);
       } else {
         /* Notify all waiting pads about going EOS now */
         sel->eos = TRUE;