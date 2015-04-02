43a31d4835994bbea76c5ccda49dd3c2ba29ed8e
Thibault Saunier <tsaunier@gnome.org>
2015-04-02 09:34:00 +0200

    element: Fix request_new_pad introspection
    Marking gst_element_request_pad as the caller of the ->request_new_pad
    virtual method.

--- gst/gstelement.c
+++ gst/gstelement.c
@@ -1069,7 +1069,7 @@ gst_element_get_request_pad (GstElement * element, const gchar * name)
 }
 
 /**
- * gst_element_request_pad:
+ * gst_element_request_pad: (virtual request_new_pad)
  * @element: a #GstElement to find a request pad of.
  * @templ: a #GstPadTemplate of which we want a pad of.
  * @name: (transfer none) (allow-none): the name of the request #GstPad
