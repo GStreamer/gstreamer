4d78375d4985e1155d7dd4713605bca369e98e45
Luis de Bethencourt <luis.bg@samsung.com>
2015-04-07 15:16:41 +0100

    tests: add missing license headers for example apps

--- tests/examples/app/appsink-src2.c
+++ tests/examples/app/appsink-src2.c
@@ -1,3 +1,25 @@
+/* GStreamer
+ *
+ * appsrc-src2.c: example for using gst_app_src_push_sample().
+ *
+ * Copyright (C) 2014 Nicola Murino <nicola.murino@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
 #include <gst/gst.h>
 
 #include <string.h>
--- tests/examples/app/appsrc_ex.c
+++ tests/examples/app/appsrc_ex.c
@@ -1,3 +1,24 @@
+/* GStreamer
+ *
+ * appsrc_ex.c: example for using appsrc and appsink linked.
+ *
+ * Copyright (C) 2007 David Schleef <ds@schleef.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
 
 
 #ifdef HAVE_CONFIG_H
