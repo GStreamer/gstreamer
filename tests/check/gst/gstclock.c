be6d3e9255099c09d00e1676f6326b79972deaa6
Suhwang Kim <suhwang.kim@lge.com>
2015-04-14 13:42:55 +0900

    tests: clock: fix test clock name
    Don't call the slave test clock "Master".
    
    https://bugzilla.gnome.org/show_bug.cgi?id=746430

--- tests/check/gst/gstclock.c
+++ tests/check/gst/gstclock.c
@@ -78,7 +78,7 @@ GST_START_TEST (test_set_master_refcount)
 
   /* create master and slave */
   master = g_object_new (TYPE_TEST_CLOCK, "name", "TestClockMaster", NULL);
-  slave = g_object_new (TYPE_TEST_CLOCK, "name", "TestClockMaster", NULL);
+  slave = g_object_new (TYPE_TEST_CLOCK, "name", "TestClockSlave", NULL);
   GST_OBJECT_FLAG_SET (slave, GST_CLOCK_FLAG_CAN_SET_MASTER);
 
   fail_unless_equals_int (GST_OBJECT_REFCOUNT (master), 1);
