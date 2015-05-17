460a7bf68292d057c77e84d1ea86b8e73fc081f3
Nicolas Dufresne <nicolas.dufresne@collabora.co.uk>
2015-05-16 23:29:03 -0400

    doc: Workaround gtkdoc issue
    With gtkdoc 1.22, the XML generator fails when a itemizedlist is
    followed by a refsect2. Workaround the issue by wrapping the refsect2
    into para.

--- plugins/elements/gstfdsrc.c
+++ plugins/elements/gstfdsrc.c
@@ -44,6 +44,7 @@
  * </listitem>
  * </itemizedlist>
  *
+ * <para>
  * <refsect2>
  * <title>Example launch line</title>
  * |[
@@ -51,6 +52,7 @@
  * ]| A simple pipeline to read from the standard input and dump the data
  * with a fakesink as hex ascii block.
  * </refsect2>
+ * </para>
  */
 
 #ifdef HAVE_CONFIG_H
