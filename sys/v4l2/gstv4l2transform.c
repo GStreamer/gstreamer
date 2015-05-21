927ba9332f19fb4fb7fb59bf0012f55341a0dbea
Pablo Anton <pablo.anton@vodalys-labs.com>
2015-05-21 17:41:12 +0200

    v4l2-*: Configuring output pool correctly for using drivers min_buffer if present.
    Signed-off-by: Pablo Anton <pablo.anton@vodalys-labs.com>
    
    https://bugzilla.gnome.org/show_bug.cgi?id=755736

--- sys/v4l2/gstv4l2transform.c
+++ sys/v4l2/gstv4l2transform.c
@@ -866,8 +866,10 @@ gst_v4l2_transform_prepare_output_buffer (GstBaseTransform * trans,
   /* Ensure input internal pool is active */
   if (!gst_buffer_pool_is_active (pool)) {
     GstStructure *config = gst_buffer_pool_get_config (pool);
+    gint min = self->v4l2output->min_buffers == 0 ? GST_V4L2_MIN_BUFFERS :
+        self->v4l2output->min_buffers;
     gst_buffer_pool_config_set_params (config, self->incaps,
-        self->v4l2output->info.size, 2, 2);
+        self->v4l2output->info.size, min, min);
 
     /* There is no reason to refuse this config */
     if (!gst_buffer_pool_set_config (pool, config))
