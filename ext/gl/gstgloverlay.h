02b2c2edf633ab4723cc1b5a56b3b9fa536f1ae4
Xavier Claessens <xavier.claessens@collabora.com>
2015-05-26 15:04:34 -0400

    gloverlay: remove unused type_file field
    https://bugzilla.gnome.org/show_bug.cgi?id=749846

--- ext/gl/gstgloverlay.h
+++ ext/gl/gstgloverlay.h
@@ -57,7 +57,6 @@ struct _GstGLOverlay
   GstGLMemory  *image_memory;
 
   gboolean      location_has_changed;
-  gint          type_file;               // 0 = No; 1 = PNG and 2 = JPEG
   gint          window_width, window_height;
   gint          image_width, image_height;
 
