8756b6a9d42de500319822665449615342adc314
Luis de Bethencourt <luis.bg@samsung.com>
2015-06-02 17:52:46 +0100

    goom2k1: rebase to use the audiovisualizer class
    Rebase to have goom2k1 using the common GstAudioVisualizer class
    
    https://bugzilla.gnome.org/show_bug.cgi?id=742875

--- gst/goom2k1/gstgoom.h
+++ gst/goom2k1/gstgoom.h
@@ -1,5 +1,7 @@
 /* gstgoom.c: implementation of goom drawing element
  * Copyright (C) <2001> Richard Boulton <richard@tartarus.org>
+ *           (C) <2006> Wim Taymans <wim at fluendo dot com>
+ * Copyright (C) <2015> Luis de Bethencourt <luis@debethencourt.com>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -20,11 +22,10 @@
 #ifndef __GST_GOOM_H__
 #define __GST_GOOM_H__
 
-G_BEGIN_DECLS
-
-#include <gst/gst.h>
-#include <gst/base/gstadapter.h>
+#include "gstaudiovisualizer.h"
 #include "goom_core.h"
+
+G_BEGIN_DECLS
 
 #define GOOM_SAMPLES 512
 
@@ -39,11 +40,7 @@ typedef struct _GstGoomClass GstGoomClass;
 
 struct _GstGoom
 {
-  GstElement element;
-
-  /* pads */
-  GstPad *sinkpad, *srcpad;
-  GstAdapter *adapter;
+  GstAudioVisualizer parent;
 
   /* input tracking */
   gint rate;
@@ -62,17 +59,9 @@ struct _GstGoom
   guint dropped;        /* frames dropped / not dropped */
   guint processed;
 
-  /* samples per frame */
-  guint spf;
-  /* bytes per frame */
-  guint bpf;
-
   /* goom stuff */
   gint16 datain[2][GOOM_SAMPLES];
   GoomData goomdata;
-
-  /* segment state */
-  GstSegment segment;
 
   /* QoS stuff *//* with LOCK */
   gdouble proportion;
@@ -81,10 +70,11 @@ struct _GstGoom
 
 struct _GstGoomClass
 {
-  GstElementClass parent_class;
+  GstAudioVisualizerClass parent_class;
 };
 
 GType gst_goom_get_type (void);
+gboolean gst_goom_plugin_init (GstPlugin * plugin);
 
 G_END_DECLS
 
