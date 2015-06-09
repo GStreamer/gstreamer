5db08a8b89d0b9f2db16b0dff315242ae0b59c1f
Ilya Konstantinov <ilya.konstantinov@gmail.com>
2015-06-10 00:16:27 +0300

    vtenc: add alignment=au to sink caps
    VT compression callback gets a CMSampleBuffer per frame, so vtenc_h264
    should set 'alignment=au' in sink caps to indicate this.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=750635

--- sys/applemedia/vtenc.c
+++ sys/applemedia/vtenc.c
@@ -174,7 +174,8 @@ gst_vtenc_base_init (GstVTEncClass * klass)
       min_fps_n, min_fps_d, max_fps_n, max_fps_d, NULL);
   if (codec_details->format_id == kCMVideoCodecType_H264) {
     gst_structure_set (gst_caps_get_structure (src_caps, 0),
-        "stream-format", G_TYPE_STRING, "avc", NULL);
+        "stream-format", G_TYPE_STRING, "avc",
+        "alignment", G_TYPE_STRING, "au", NULL);
   }
   src_template = gst_pad_template_new ("src", GST_PAD_SRC, GST_PAD_ALWAYS,
       src_caps);
