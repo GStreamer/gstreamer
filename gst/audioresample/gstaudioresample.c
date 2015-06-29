c5dbee33b0a9da4b359357d72d462399eb9e4986
Sebastian Dröge <sebastian@centricular.com>
2015-06-29 13:06:59 +0200

    audioresample: Also copy metas if their API has no tags attached to it
    This is the default basetransform behaviour, being more strict than that
    is not really useful.

--- gst/audioresample/gstaudioresample.c
+++ gst/audioresample/gstaudioresample.c
@@ -1259,9 +1259,9 @@ gst_audio_resample_transform_meta (GstBaseTransform * trans, GstBuffer * outbuf,
 
   tags = gst_meta_api_type_get_tags (info->api);
 
-  if (tags && g_strv_length ((gchar **) tags) == 1
-      && gst_meta_api_type_has_tag (info->api,
-          g_quark_from_string (GST_META_TAG_AUDIO_STR)))
+  if (!tags || (g_strv_length ((gchar **) tags) == 1
+          && gst_meta_api_type_has_tag (info->api,
+              g_quark_from_string (GST_META_TAG_AUDIO_STR))))
     return TRUE;
 
   return FALSE;
