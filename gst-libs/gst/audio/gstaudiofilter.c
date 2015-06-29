0acdfeed690f44ad3baed89ccec464252f26778f
Sebastian Dröge <sebastian@centricular.com>
2015-06-29 13:06:33 +0200

    audiofilter: Also copy metas if their API has no tags attached to it
    This is the default basetransform behaviour, being more strict than that
    is not really useful.

--- gst-libs/gst/audio/gstaudiofilter.c
+++ gst-libs/gst/audio/gstaudiofilter.c
@@ -72,9 +72,9 @@ gst_audio_filter_transform_meta (GstBaseTransform * trans, GstBuffer * inbuf,
 
   tags = gst_meta_api_type_get_tags (info->api);
 
-  if (tags && g_strv_length ((gchar **) tags) == 1
-      && gst_meta_api_type_has_tag (info->api,
-          g_quark_from_string (GST_META_TAG_AUDIO_STR)))
+  if (!tags || (g_strv_length ((gchar **) tags) == 1
+          && gst_meta_api_type_has_tag (info->api,
+              g_quark_from_string (GST_META_TAG_AUDIO_STR))))
     return TRUE;
 
   return
