GStreamer/gstreamer

commit eb465fb392434d839d9bdfaac810aa8fa7a561bf

gstvaapifilter: Add guard for VAProcFilterSkinToneEnhancement
VAProcFilterSkinToneEnhancement is avaialbe from VA >= 0.36.

Signed-off-by: Sreerenj Balachandran <sreerenj.balachandran@intel.com>

gst-libs/gst/vaapi/gstvaapifilter.c
@@ -458,10 +458,12 @@ op_data_new (GstVaapiFilterOp op, GParamSpec * pspec)
       op_data->va_cap_size = sizeof (VAProcFilterCap);
       op_data->va_buffer_size = sizeof (VAProcFilterParameterBuffer);
       break;
+#if VA_CHECK_VERSION(0,36,0)
     case GST_VAAPI_FILTER_OP_SKINTONE:
       op_data->va_type = VAProcFilterSkinToneEnhancement;
       op_data->va_buffer_size = sizeof (VAProcFilterParameterBuffer);
       break;
+#endif
     case GST_VAAPI_FILTER_OP_HUE:
       op_data->va_subtype = VAProcColorBalanceHue;
       goto op_colorbalance;

commit 9f0e054d9d3e1e7380a9c09c978bc325b0ddd86c

gstvaapifilter: Only register STE property if it supported by corresponding VA library
Fix the regression introduced in commit eb465fb.
VAProcFilterSkinToneEnhancement is avaialbe from VA >= 0.36.

Signed-off-by: Sreerenj Balachandran <sreerenj.balachandran@intel.com>

gst-libs/gst/vaapi/gstvaapifilter.c
@@ -401,6 +401,7 @@ init_properties (void)
       GST_VAAPI_TYPE_SCALE_METHOD,
       DEFAULT_SCALING, G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS);
 
+#if VA_CHECK_VERSION(0,36,0)
   /**
    * GstVaapiFilter:skin-tone-enhancement:
    *
@@ -410,6 +411,7 @@ init_properties (void)
       "Skin tone enhancement",
       "Apply the skin tone enhancement algorithm",
       FALSE, G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS);
+#endif
 }
 
 static void