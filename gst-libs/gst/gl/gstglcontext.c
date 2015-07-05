90a1ff1383335b4e57019a710be9165b98a50a7c
Julien Isorce <julien.isorce@gmail.com>
2015-07-06 00:45:45 +0100

    gl: move GL_NUM_EXTENSIONS definition after gl.h
    https://bugzilla.gnome.org/show_bug.cgi?id=752743

--- gst-libs/gst/gl/gstglcontext.c
+++ gst-libs/gst/gl/gstglcontext.c
@@ -40,14 +40,14 @@
 #define _GNU_SOURCE
 #endif
 
-#ifndef GL_NUM_EXTENSIONS
-#define GL_NUM_EXTENSIONS 0x0000821d
-#endif
-
 #include <gmodule.h>
 
 #include "gl.h"
 #include "gstglcontext.h"
+
+#ifndef GL_NUM_EXTENSIONS
+#define GL_NUM_EXTENSIONS 0x0000821d
+#endif
 
 #if GST_GL_HAVE_PLATFORM_GLX
 #include "x11/gstglcontext_glx.h"
