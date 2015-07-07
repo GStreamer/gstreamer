cf454ca71358ca6f7d412488d98e27dd31d1bb18
Vineeth TM <vineeth.tm@samsung.com>
2015-07-07 09:31:01 +0900

    xvimagesink: fix pad memory leak
    pad is not being freed when xwindow is not created
    
    https://bugzilla.gnome.org/show_bug.cgi?id=752042

--- sys/xvimage/xvimagesink.c
+++ sys/xvimage/xvimagesink.c
@@ -1128,6 +1128,7 @@ gst_xv_image_sink_navigation_send_event (GstNavigation * navigation,
 
     if (!(xwindow = xvimagesink->xwindow)) {
       g_mutex_unlock (&xvimagesink->flow_lock);
+      gst_object_unref (peer);
       return;
     }
 
