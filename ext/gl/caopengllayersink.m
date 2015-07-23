GStreamer/gstreamer

commit 5457e55f255518d679b59a170951e299ecd8c5f6

caopengllayersink: use gst_gl_display_create_context
https://bugzilla.gnome.org/show_bug.cgi?id=750310

ext/gl/caopengllayersink.m
@@ -357,12 +357,8 @@ _ensure_gl_setup (GstCAOpenGLLayerSink * ca_sink)
   gst_gl_display_filter_gl_api (ca_sink->display, SUPPORTED_GL_APIS);
 
   if (!ca_sink->context) {
-    ca_sink->context = gst_gl_context_new (ca_sink->display);
-    if (!ca_sink->context)
-      goto context_creation_error;
-
-    if (!gst_gl_context_create (ca_sink->context, ca_sink->other_context,
-            &error)) {
+    if (!gst_gl_display_create_context (ca_sink->display,
+            ca_sink->other_context, &ca_sink->context, &error)) {
       goto context_error;
     }
   }

commit fb6457e90aed1cbccdca53714d35c6ca291e0490

caopengllayersink: remove unused label context_creation_error
Build error introduced by commit
5457e55f255518d679b59a170951e299ecd8c5f6

https://bugzilla.gnome.org/show_bug.cgi?id=750310

ext/gl/caopengllayersink.m
@@ -368,13 +368,6 @@ _ensure_gl_setup (GstCAOpenGLLayerSink * ca_sink)
 
   return TRUE;
 
-context_creation_error:
-  {
-    GST_ELEMENT_ERROR (ca_sink, RESOURCE, NOT_FOUND,
-        ("Failed to create GL context"), (NULL));
-    return FALSE;
-  }
-
 context_error:
   {
     GST_ELEMENT_ERROR (ca_sink, RESOURCE, NOT_FOUND, ("%s", error->message),