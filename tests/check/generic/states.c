862c85ce4a14757a6b715111971e47b005e42f6b
Edward Hervey <bilboed@bilboed.com>
2015-08-14 11:11:10 +0200

    check: Rename states unit test
    Makes it easier to differentiate from other modules states unit test

--- tests/check/generic/states.c
+++ tests/check/generic/states.c
@@ -207,7 +207,7 @@ GST_END_TEST;
 static Suite *
 states_suite (void)
 {
-  Suite *s = suite_create ("states");
+  Suite *s = suite_create ("states_base");
   TCase *tc_chain = tcase_create ("general");
 
   suite_add_tcase (s, tc_chain);
