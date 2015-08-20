e2581982a6629066c53985ad7f42b45a044de640
Sebastian Dröge <sebastian@centricular.com>
2015-08-20 10:50:30 +0300

    playback/player: gst-play: Fix leak of GError and GOptionContext on parsing errors

--- playback/player/gst-play/gst-play.c
+++ playback/player/gst-play/gst-play.c
@@ -674,6 +674,8 @@ main (int argc, char **argv)
   g_option_context_add_group (ctx, gst_init_get_option_group ());
   if (!g_option_context_parse (ctx, &argc, &argv, &err)) {
     g_print ("Error initializing: %s\n", GST_STR_NULL (err->message));
+    g_clear_error (&err);
+    g_option_context_free (ctx);
     return 1;
   }
   g_option_context_free (ctx);
