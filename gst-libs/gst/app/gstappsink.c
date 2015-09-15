a7bcdc3608b7e21ff4c2a18eb211238cdabfa355
Tim-Philipp Müller <tim@centricular.com>
2015-09-15 15:26:44 +0100

    appsink: minor docs fix

--- gst-libs/gst/app/gstappsink.c
+++ gst-libs/gst/app/gstappsink.c
@@ -844,8 +844,8 @@ gst_app_sink_query (GstBaseSink * bsink, GstQuery * query)
  *
  * Set the capabilities on the appsink element.  This function takes
  * a copy of the caps structure. After calling this method, the sink will only
- * accept caps that match @caps. If @caps is non-fixed, you must check the caps
- * on the buffers to get the actual used caps.
+ * accept caps that match @caps. If @caps is non-fixed, or incomplete,
+ * you must check the caps on the samples to get the actual used caps.
  */
 void
 gst_app_sink_set_caps (GstAppSink * appsink, const GstCaps * caps)
