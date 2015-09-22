GStreamer/gstreamer

commit cfe2871a5e3c5b1db20470927642b776e055a87a

mpdparser: Consider the repeat count when checking if a segment is the last one
Otherwise we play only the first repetition of the last segment and then EOS.

https://bugzilla.gnome.org/show_bug.cgi?id=752480

ext/dash/gstmpdparser.c
@@ -3975,8 +3975,17 @@ gst_mpd_client_has_next_segment (GstMpdClient * client,
   if (forward) {
     guint segments_count = gst_mpd_client_get_segments_counts (client, stream);
 
-    if (segments_count > 0 && stream->segment_index + 1 >= segments_count)
+    if (segments_count > 0 && stream->segment_index + 1 == segments_count) {
+      GstMediaSegment *segment;
+
+      segment = g_ptr_array_index (stream->segments, stream->segment_index);
+      if (segment->repeat >= 0
+          && stream->segment_repeat_index >= segment->repeat)
+        return FALSE;
+    } else if (segments_count > 0
+        && stream->segment_index + 1 >= segments_count) {
       return FALSE;
+    }
   } else {
     if (stream->segment_index < 0)
       return FALSE;

commit a25253130b573c842805439e424eb048c1ef9bbc

mpdparser: Only check stream->segments for a repeated last segment if we have a static list of segments
Otherwise we'll crash, trying to derefence NULL. And if we have no static list
of segments, we can't have repeated segments anyway.

Regression introduced by cfe2871a5e3c5b1db20470927642b776e055a87a

https://bugzilla.gnome.org/show_bug.cgi?id=755411

ext/dash/gstmpdparser.c
@@ -4057,7 +4057,8 @@ gst_mpd_client_has_next_segment (GstMpdClient * client,
   if (forward) {
     guint segments_count = gst_mpd_client_get_segments_counts (client, stream);
 
-    if (segments_count > 0 && stream->segment_index + 1 == segments_count) {
+    if (segments_count > 0 && stream->segments
+        && stream->segment_index + 1 == segments_count) {
       GstMediaSegment *segment;
 
       segment = g_ptr_array_index (stream->segments, stream->segment_index);