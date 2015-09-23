52b94556ed08b07354f15bd2906b59b197cbd225
Víctor Manuel Jáquez Leal <victorx.jaquez@intel.com>
2015-09-23 16:02:46 +0200

    gstcompat: add gst_buffer_copy_deep() if gst < 1.5
    gst_buffer_copy_deep() was added in GStreamer 1.5. If want to use it we should
    add an implementation if gstreamer-vaapi is linked to previous versions.
    
    Signed-off-by: Víctor Manuel Jáquez Leal <victorx.jaquez@intel.com>

--- gst/vaapi/gstcompat.h
+++ gst/vaapi/gstcompat.h
@@ -25,4 +25,27 @@
 
 #include "gst/vaapi/sysdeps.h"
 
+#if !GST_CHECK_VERSION (1,5,0)
+static inline GstBuffer *
+gst_buffer_copy_deep (const GstBuffer * buffer)
+{
+  GstBuffer *copy;
+
+  g_return_val_if_fail (buffer != NULL, NULL);
+
+  copy = gst_buffer_new ();
+
+  if (!gst_buffer_copy_into (copy, (GstBuffer *) buffer,
+      GST_BUFFER_COPY_ALL | GST_BUFFER_COPY_DEEP, 0, -1))
+    gst_buffer_replace (&copy, NULL);
+
+#if GST_CHECK_VERSION (1,4,0)
+  if (copy)
+    GST_BUFFER_FLAG_UNSET (copy, GST_BUFFER_FLAG_TAG_MEMORY);
+#endif
+
+  return copy;
+}
+#endif
+
 #endif /* GST_COMPAT_H */
