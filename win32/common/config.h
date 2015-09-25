b1238ce0e40707e6a08c83145ff348a578fed1e2
Sebastian Dröge <sebastian@centricular.com>
2015-09-25 23:15:55 +0200

    Release 1.6.0

--- win32/common/config.h
+++ win32/common/config.h
@@ -61,7 +61,7 @@
 #define GST_PACKAGE_ORIGIN "Unknown package origin"
 
 /* GStreamer package release date/time for plugins as YYYY-MM-DD */
-#define GST_PACKAGE_RELEASE_DATETIME "2015-09-18"
+#define GST_PACKAGE_RELEASE_DATETIME "2015-09-25"
 
 /* Define if static plugins should be built */
 #undef GST_PLUGIN_BUILD_STATIC
@@ -387,7 +387,7 @@
 #define PACKAGE_NAME "GStreamer Good Plug-ins"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "GStreamer Good Plug-ins 1.5.91"
+#define PACKAGE_STRING "GStreamer Good Plug-ins 1.6.0"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "gst-plugins-good"
@@ -396,7 +396,7 @@
 #undef PACKAGE_URL
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.5.91"
+#define PACKAGE_VERSION "1.6.0"
 
 /* directory where plugins are located */
 #ifdef _DEBUG
@@ -427,7 +427,7 @@
 #undef TARGET_CPU
 
 /* Version number of package */
-#define VERSION "1.5.91"
+#define VERSION "1.6.0"
 
 /* Define WORDS_BIGENDIAN to 1 if your processor stores words with the most
    significant byte first (like Motorola and SPARC, unlike Intel). */
