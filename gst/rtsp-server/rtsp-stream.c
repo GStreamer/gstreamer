81ae320383894892b4b4b53482a04c998441430d
David Svensson Fors <davidsf@axis.com>
2015-10-22 09:15:21 +0200

    rtsp-stream: Always unref return value of gst_object_get_parent()
    Fixes a leak of a GstBin in the udp-mcast case.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=756968

--- gst/rtsp-server/rtsp-stream.c
+++ gst/rtsp-server/rtsp-stream.c
@@ -2881,7 +2881,6 @@ update_transport (GstRTSPStream * stream, GstRTSPStreamTransport * trans,
           gst_object_unref (pad);
           gst_object_unref (selpad);
         }
-        gst_object_unref (bin);
 
         priv->transport_sources =
             g_list_prepend (priv->transport_sources, source);
@@ -2916,6 +2915,8 @@ update_transport (GstRTSPStream * stream, GstRTSPStreamTransport * trans,
           g_slice_free (GstRTSPMulticastTransportSource, source);
         }
       }
+
+      gst_object_unref (bin);
 
       /* fall through for the generic case */
     }
