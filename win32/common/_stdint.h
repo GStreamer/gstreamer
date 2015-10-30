a92ff4f2c57e342dc2b97a79bc3ea234541e3c27
Sebastian Dröge <sebastian@centricular.com>
2015-10-30 16:38:48 +0200

    Release 1.6.1

--- win32/common/_stdint.h
+++ win32/common/_stdint.h
@@ -1,8 +1,8 @@
 #ifndef _GST_PLUGINS_BASE__STDINT_H
 #define _GST_PLUGINS_BASE__STDINT_H 1
 #ifndef _GENERATED_STDINT_H
-#define _GENERATED_STDINT_H "gst-plugins-base 1.6.0"
-/* generated using gnu compiler gcc-5 (Debian 5.2.1-17) 5.2.1 20150911 */
+#define _GENERATED_STDINT_H "gst-plugins-base 1.6.1"
+/* generated using gnu compiler gcc-5 (Debian 5.2.1-23) 5.2.1 20151028 */
 #define _STDINT_HAVE_STDINT_H 1
 #include <stdint.h>
 #endif
