84fcec006540d54b52843151f484c4c2aef8dfe9
Graham Leggett <minfrin@sharp.fm>
2015-11-13 16:00:02 +0000

    gst: Ensure gst_value is initialised before gst_tag
    Otherwise GST_TYPE_FRACTION will work correctly in tags.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=753922

--- gst/gst.c
+++ gst/gst.c
@@ -687,9 +687,9 @@ init_post (GOptionContext * context, GOptionGroup * group, gpointer data,
   _priv_gst_sample_initialize ();
   _priv_gst_context_initialize ();
   _priv_gst_date_time_initialize ();
+  _priv_gst_value_initialize ();
   _priv_gst_tag_initialize ();
   _priv_gst_toc_initialize ();
-  _priv_gst_value_initialize ();
 
   g_type_class_ref (gst_param_spec_fraction_get_type ());
   gst_parse_context_get_type ();
