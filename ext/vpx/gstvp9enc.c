2b7a1b4bb9bfda2f8ff25f0cf4ec06220e5da67a
Reynaldo H. Verdejo Pinochet <reynaldo@osg.samsung.com>
2015-11-14 16:57:13 -0800

    vp9enc: remove unnecessary NULL check before g_free()

--- ext/vpx/gstvp9enc.c
+++ ext/vpx/gstvp9enc.c
@@ -882,8 +882,7 @@ gst_vp9_enc_set_property (GObject * object, guint prop_id,
       global = TRUE;
       break;
     case PROP_MULTIPASS_CACHE_FILE:
-      if (gst_vp9_enc->multipass_cache_prefix)
-        g_free (gst_vp9_enc->multipass_cache_prefix);
+      g_free (gst_vp9_enc->multipass_cache_prefix);
       gst_vp9_enc->multipass_cache_prefix = g_value_dup_string (value);
       break;
     case PROP_TS_NUMBER_LAYERS:
