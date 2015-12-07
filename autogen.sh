258214aa334a82d6ac976414a7d3b181ed071371
Sreerenj Balachandran <sreerenj.balachandran@intel.com>
2015-12-08 00:36:36 +0200

    Remove libvpx submodule
    We will be using upstream codecparsers always.
    No more internal libvpx !

--- autogen.sh
+++ autogen.sh
@@ -26,7 +26,6 @@ else
             submodule_init="yes"
         fi
     done
-    [ -f ext/libvpx/upstream/configure ] || submodule_init="yes"
     if test "$submodule_init" = "yes"; then
         $GIT submodule init
     fi
