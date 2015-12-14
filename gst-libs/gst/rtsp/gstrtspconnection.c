b0c834df1b1e1ad7f1bfa5f5b326aacadb974c81
Sebastian Dröge <sebastian@centricular.com>
2015-12-14 19:03:33 +0100

    rtspconnection: Properly initialize stack-allocated RTSP message to all-zeroes

--- gst-libs/gst/rtsp/gstrtspconnection.c
+++ gst-libs/gst/rtsp/gstrtspconnection.c
@@ -1124,11 +1124,14 @@ gst_rtsp_connection_connect (GstRTSPConnection * conn, GTimeVal * timeout)
 {
   GstRTSPResult result;
   GstRTSPMessage response;
+
+  memset (&response, 0, sizeof (response));
   gst_rtsp_message_init (&response);
 
   result = gst_rtsp_connection_connect_with_response (conn, timeout, &response);
 
   gst_rtsp_message_unset (&response);
+
   return result;
 }
 
