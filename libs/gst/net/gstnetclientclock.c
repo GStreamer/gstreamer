f875bec51e0822889043b8609342320f4e735380
Florin Apostol <florin.apostol@oregan.net>
2016-01-13 21:32:20 +0000

    netclientclock: Fix GError memory leak in handling NTP response
    Error was not released if gst_ntp_packet_receive failed.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=760598

--- libs/gst/net/gstnetclientclock.c
+++ libs/gst/net/gstnetclientclock.c
@@ -740,6 +740,7 @@ gst_net_client_internal_clock_thread (gpointer data)
           if (g_error_matches (err, GST_NTP_ERROR, GST_NTP_ERROR_WRONG_VERSION)
               || g_error_matches (err, GST_NTP_ERROR, GST_NTP_ERROR_KOD_DENY)) {
             GST_ERROR_OBJECT (self, "fatal receive error: %s", err->message);
+            g_clear_error (&err);
             break;
           } else if (g_error_matches (err, GST_NTP_ERROR,
                   GST_NTP_ERROR_KOD_RATE)) {
