022e060563e25c739248689a44aa5798dc04b734
Alessandro Decina <alessandro.d@gmail.com>
2016-02-09 13:39:18 +1100

    applemedia: avfvideosrc: add myself to the copyright holders

--- sys/applemedia/avfvideosrc.m
+++ sys/applemedia/avfvideosrc.m
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2010 Ole André Vadla Ravnås <oleavr@soundrop.com>
+ * Copyright (C) 2016 Alessandro Decina <twi@centricular.com>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
