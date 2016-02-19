8bdb68edbc605e21314b608e7a39bdbaab7302b8
Sebastian Dröge <sebastian@centricular.com>
2016-02-19 11:58:13 +0200

    Release 1.7.2

--- win32/common/config.h
+++ win32/common/config.h
@@ -67,7 +67,7 @@
 #define GST_PACKAGE_ORIGIN "Unknown package origin"
 
 /* GStreamer package release date/time for plugins as YYYY-MM-DD */
-#define GST_PACKAGE_RELEASE_DATETIME "2015-12-24"
+#define GST_PACKAGE_RELEASE_DATETIME "2016-02-19"
 
 /* Define if static plugins should be built */
 #undef GST_PLUGIN_BUILD_STATIC
@@ -189,11 +189,8 @@
 /* Define to enable mpeg2dec (used by mpeg2dec). */
 #undef HAVE_MPEG2DEC
 
-/* Defined for newer opencore-amrnb */
-#undef HAVE_OPENCORE_AMRNB_0_1_3_OR_LATER
-
-/* Defined for newer opencore-amrwb */
-#undef HAVE_OPENCORE_AMRWB_0_1_3_OR_LATER
+/* Define to enable mpg123 audio decoder (used by mpg123). */
+#undef HAVE_MPG123
 
 /* Use Orc */
 #undef HAVE_ORC
@@ -246,8 +243,7 @@
 /* gettext locale dir */
 #define LOCALEDIR PREFIX "\\share\\locale"
 
-/* Define to the sub-directory in which libtool stores uninstalled libraries.
-   */
+/* Define to the sub-directory where libtool stores uninstalled libraries. */
 #undef LT_OBJDIR
 
 /* Name of package */
@@ -260,7 +256,7 @@
 #define PACKAGE_NAME "GStreamer Ugly Plug-ins"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "GStreamer Ugly Plug-ins 1.7.1"
+#define PACKAGE_STRING "GStreamer Ugly Plug-ins 1.7.2"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "gst-plugins-ugly"
@@ -269,7 +265,7 @@
 #undef PACKAGE_URL
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.7.1"
+#define PACKAGE_VERSION "1.7.2"
 
 /* directory where plugins are located */
 #ifdef _DEBUG
@@ -300,7 +296,7 @@
 #undef TARGET_CPU
 
 /* Version number of package */
-#define VERSION "1.7.1"
+#define VERSION "1.7.2"
 
 /* Define WORDS_BIGENDIAN to 1 if your processor stores words with the most
    significant byte first (like Motorola and SPARC, unlike Intel). */
