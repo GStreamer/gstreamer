111bb7c4c2df4032f9c4f307328f9ec1918e7537
Tim-Philipp Müller <tim@centricular.com>
2016-02-26 00:35:30 +0000

    opus: rename plugin to opusparse for the time being
    Until we fix it up and get rid of the opus dependency and
    move it elsewhere too.

--- ext/opus/gstopus.c
+++ ext/opus/gstopus.c
@@ -39,6 +39,6 @@ plugin_init (GstPlugin * plugin)
 
 GST_PLUGIN_DEFINE (GST_VERSION_MAJOR,
     GST_VERSION_MINOR,
-    opus,
-    "OPUS plugin library",
+    opusparse,
+    "OPUS parse plugin",
     plugin_init, VERSION, "LGPL", GST_PACKAGE_NAME, GST_PACKAGE_ORIGIN)
