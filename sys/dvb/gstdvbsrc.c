46473ac287e3f4c3bb12e3ce2e852e2095d900c6
Thiago Santos <thiagoss@osg.samsung.com>
2016-03-01 18:21:12 -0300

    dvbsrc: print time in same format
    Makes it easier to compare

--- sys/dvb/gstdvbsrc.c
+++ sys/dvb/gstdvbsrc.c
@@ -2291,8 +2291,8 @@ gst_dvbsrc_tune_fe (GstDvbSrc * object)
     if (object->tuning_timeout)
       elapsed_time = GST_CLOCK_DIFF (start, gst_util_get_timestamp ());
     GST_LOG_OBJECT (object,
-        "Tuning. Time elapsed %" GST_STIME_FORMAT " Limit %" G_GUINT64_FORMAT,
-        GST_STIME_ARGS (elapsed_time), object->tuning_timeout);
+        "Tuning. Time elapsed %" GST_STIME_FORMAT " Limit %" GST_TIME_FORMAT,
+        GST_STIME_ARGS (elapsed_time), GST_TIME_ARGS (object->tuning_timeout));
   }
 
   if (!(status & FE_HAS_LOCK)) {
