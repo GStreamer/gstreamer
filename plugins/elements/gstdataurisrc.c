a541d632107730e7c82dae0951fb569b0cd495af
Vineeth TM <vineeth.tm@samsung.com>
2016-03-04 15:50:26 +0900

    bad: use new gst_element_class_add_static_pad_template()
    https://bugzilla.gnome.org/show_bug.cgi?id=763081

--- plugins/elements/gstdataurisrc.c
+++ plugins/elements/gstdataurisrc.c
@@ -99,8 +99,7 @@ gst_data_uri_src_class_init (GstDataURISrcClass * klass)
           "URI that should be used",
           NULL, G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS));
 
-  gst_element_class_add_pad_template (element_class,
-      gst_static_pad_template_get (&src_template));
+  gst_element_class_add_static_pad_template (element_class, &src_template);
   gst_element_class_set_static_metadata (element_class,
       "data: URI source element", "Source", "Handles data: uris",
       "Philippe Normand <pnormand@igalia.com>, "
