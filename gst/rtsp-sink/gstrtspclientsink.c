1796ce2f03f54f417ac72de8e8fdc6c0f02c11e5
Vineeth TM <vineeth.tm@samsung.com>
2016-03-07 08:50:01 +0900

    rtspclientsink: use new gst_element_class_add_static_pad_template()
    https://bugzilla.gnome.org/show_bug.cgi?id=763196

--- gst/rtsp-sink/gstrtspclientsink.c
+++ gst/rtsp-sink/gstrtspclientsink.c
@@ -612,8 +612,7 @@ gst_rtsp_client_sink_class_init (GstRTSPClientSinkClass * klass)
   gstelement_class->release_pad =
       GST_DEBUG_FUNCPTR (gst_rtsp_client_sink_release_pad);
 
-  gst_element_class_add_pad_template (gstelement_class,
-      gst_static_pad_template_get (&rtptemplate));
+  gst_element_class_add_static_pad_template (gstelement_class, &rtptemplate);
 
   gst_element_class_set_static_metadata (gstelement_class,
       "RTSP RECORD client", "Sink/Network",
