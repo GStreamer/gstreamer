36f490a325ba938aedae529d7684a0652ea31508
Vineeth T M <vineeth.tm@samsung.com>
2016-03-07 08:53:23 +0900

    codecanalyzer: use new gst_element_class_add_static_pad_template()
    https://bugzilla.gnome.org/show_bug.cgi?id=763197

--- codecanalyzer/src/plugins/gst/analyzersink/gstanalyzersink.c
+++ codecanalyzer/src/plugins/gst/analyzersink/gstanalyzersink.c
@@ -123,8 +123,7 @@ gst_analyzer_sink_class_init (GstAnalyzerSinkClass * klass)
       "Sink",
       "Sink to dump the parsed information",
       "Sreerenj Balachandran<sreerenj.balachandran@intel.com>");
-  gst_element_class_add_pad_template (gstelement_class,
-      gst_static_pad_template_get (&sinktemplate));
+  gst_element_class_add_static_pad_template (gstelement_class, &sinktemplate);
 
   gstelement_class->change_state =
       GST_DEBUG_FUNCPTR (gst_analyzer_sink_change_state);
