dc9585c1f72bb4ed9aeeeb25482628afc6333dbb
Tim-Philipp Müller <tim@centricular.com>
2016-04-14 13:27:12 +0100

    Release 1.6.4

--- win32/common/_stdint.h
+++ win32/common/_stdint.h
@@ -1,8 +1,8 @@
 #ifndef _GST_PLUGINS_BASE__STDINT_H
 #define _GST_PLUGINS_BASE__STDINT_H 1
 #ifndef _GENERATED_STDINT_H
-#define _GENERATED_STDINT_H "gst-plugins-base 1.6.3"
-/* generated using gnu compiler gcc-5 (Debian 5.3.1-6) 5.3.1 20160114 */
+#define _GENERATED_STDINT_H "gst-plugins-base 1.6.4"
+/* generated using gnu compiler gcc (Debian 5.3.1-14) 5.3.1 20160409 */
 #define _STDINT_HAVE_STDINT_H 1
 #include <stdint.h>
 #endif
--- win32/common/config.h
+++ win32/common/config.h
@@ -87,7 +87,7 @@
 #define GST_PACKAGE_ORIGIN "Unknown package origin"
 
 /* GStreamer package release date/time for plugins as YYYY-MM-DD */
-#define GST_PACKAGE_RELEASE_DATETIME "2016-01-20"
+#define GST_PACKAGE_RELEASE_DATETIME "2016-04-14"
 
 /* Define if static plugins should be built */
 #undef GST_PLUGIN_BUILD_STATIC
@@ -337,7 +337,7 @@
 #define PACKAGE_NAME "GStreamer Base Plug-ins"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "GStreamer Base Plug-ins 1.6.3"
+#define PACKAGE_STRING "GStreamer Base Plug-ins 1.6.4"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "gst-plugins-base"
@@ -346,7 +346,7 @@
 #undef PACKAGE_URL
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.6.3"
+#define PACKAGE_VERSION "1.6.4"
 
 /* directory where plugins are located */
 #ifdef _DEBUG
@@ -380,7 +380,7 @@
 #undef USE_TREMOLO
 
 /* Version number of package */
-#define VERSION "1.6.3"
+#define VERSION "1.6.4"
 
 /* Define WORDS_BIGENDIAN to 1 if your processor stores words with the most
    significant byte first (like Motorola and SPARC, unlike Intel). */
