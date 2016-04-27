ebf0d94534cce5203be4cddf53dbfafd66e0d758
Víctor Manuel Jáquez Leal <victorx.jaquez@intel.com>
2016-04-27 21:20:32 +0200

    vaapipostproc: no GLTextureUpload in sinkpad
    As the vaapipostproc does not process frames with the VideoGLTextureUpload
    meta, the feature is removed from the sink pad template.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=765931

--- gst/vaapi/gstvaapipostproc.c
+++ gst/vaapi/gstvaapipostproc.c
@@ -55,9 +55,6 @@ GST_DEBUG_CATEGORY_STATIC (gst_debug_vaapipostproc);
 static const char gst_vaapipostproc_sink_caps_str[] =
   GST_VAAPI_MAKE_SURFACE_CAPS ", "
   GST_CAPS_INTERLACED_MODES "; "
-#if (USE_GLX || USE_EGL)
-  GST_VAAPI_MAKE_GLTEXUPLOAD_CAPS "; "
-#endif
   GST_VIDEO_CAPS_MAKE (GST_VIDEO_FORMATS_ALL) ", "
    GST_CAPS_INTERLACED_MODES;
 /* *INDENT-ON* */
