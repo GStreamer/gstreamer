7c1a09140459bdb5f525f9739b2bf2cb0411b5d8
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2016-05-02 08:33:42 +0200

    filesrc: fix buffer leaks in tests
    gst_check_setup_sink_pad() internally uses gst_check_chain_func() so we
    should call gst_check_drop_buffers() when tearing down tests to free the
    buffers which have been exchanged through the pipeline.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=765903

--- tests/check/elements/filesrc.c
+++ tests/check/elements/filesrc.c
@@ -83,6 +83,7 @@ setup_filesrc (void)
 static void
 cleanup_filesrc (GstElement * filesrc)
 {
+  gst_check_drop_buffers ();
   gst_pad_set_active (mysinkpad, FALSE);
   gst_check_teardown_sink_pad (filesrc);
   gst_check_teardown_element (filesrc);
