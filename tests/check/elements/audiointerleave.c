2ec2e15a2a559718c26e8cc17c5d6498f6f17d80
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2016-05-19 11:19:01 +0300

    audiointerleave: fix message leaks by flushing the bus
    https://bugzilla.gnome.org/show_bug.cgi?id=766663

--- tests/check/elements/audiointerleave.c
+++ tests/check/elements/audiointerleave.c
@@ -269,6 +269,7 @@ GST_START_TEST (test_audiointerleave_2ch)
     g_cond_wait (&data_cond, &data_mutex);
   g_mutex_unlock (&data_mutex);
 
+  gst_bus_set_flushing (bus, TRUE);
   gst_element_set_state (interleave, GST_STATE_NULL);
   gst_element_set_state (queue, GST_STATE_NULL);
 
@@ -410,6 +411,7 @@ GST_START_TEST (test_audiointerleave_2ch_1eos)
     g_cond_wait (&data_cond, &data_mutex);
   g_mutex_unlock (&data_mutex);
 
+  gst_bus_set_flushing (bus, TRUE);
   gst_element_set_state (interleave, GST_STATE_NULL);
   gst_element_set_state (queue, GST_STATE_NULL);
 
