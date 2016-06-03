389e0abeb0c9c80fbd9468f384543d74ee477655
Miguel París Díaz <mparisdiaz@gmail.com>
2016-06-03 13:09:35 +0200

    rtpsource: complete warn log with SSRC
    https://bugzilla.gnome.org/show_bug.cgi?id=767195

--- gst/rtpmanager/rtpsource.c
+++ gst/rtpmanager/rtpsource.c
@@ -1496,7 +1496,8 @@ rtp_source_get_new_sr (RTPSource * src, guint64 ntpnstime,
       t_rtp -= gst_util_uint64_scale_int (diff, src->clock_rate, GST_SECOND);
     }
   } else {
-    GST_WARNING ("no clock-rate, cannot interpolate rtp time");
+    GST_WARNING ("no clock-rate, cannot interpolate rtp time for SSRC %u",
+        src->ssrc);
   }
 
   /* convert the NTP time in nanoseconds to 32.32 fixed point */
