5f2b32e64282285f487f4f3c64b0c222caf645ae
Sebastian Dröge <sebastian@centricular.com>
2016-06-17 15:29:16 +0300

    rtph264pay: Deprecated sprop-parameter-set property
    This is supposed to be either in the codec_data (avc stream format) or inside
    the stream, and we extract it from there. It should not be set from a
    property as it's stream specific.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=767789

--- gst/rtp/gstrtph264pay.c
+++ gst/rtp/gstrtph264pay.c
@@ -120,7 +120,7 @@ gst_rtp_h264_pay_class_init (GstRtpH264PayClass * klass)
           "The base64 sprop-parameter-sets to set in out caps (set to NULL to "
           "extract from stream)",
           DEFAULT_SPROP_PARAMETER_SETS,
-          G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS));
+          G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS | G_PARAM_DEPRECATED));
 
   g_object_class_install_property (G_OBJECT_CLASS (klass),
       PROP_CONFIG_INTERVAL,
