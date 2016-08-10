GStreamer/gstreamer

commit 4259d1aec7b046a992ad908804949d2167706280

vaapiencode: h264,h265: validate fps numerator
Validate that fps numerator is non-zero so it can be used to calculate
the duration of the B frame.

Also it gst_util_uint64_scale() is used instead of normal arithmetic in
order to aviod overflows, underflows and loss of precision.

https://bugzilla.gnome.org/show_bug.cgi?id=768458

gst-libs/gst/vaapi/gstvaapiencoder_h264.c
@@ -2467,11 +2467,11 @@ reset_properties (GstVaapiEncoderH264 * encoder)
     encoder->num_bframes = 0;
   }
 
-  if (encoder->num_bframes)
-    encoder->cts_offset = GST_SECOND * GST_VAAPI_ENCODER_FPS_D (encoder) /
-        GST_VAAPI_ENCODER_FPS_N (encoder);
+  if (encoder->num_bframes > 0 && GST_VAAPI_ENCODER_FPS_N (encoder) > 0)
+    encoder->cts_offset = gst_util_uint64_scale (GST_SECOND,
+        GST_VAAPI_ENCODER_FPS_D (encoder), GST_VAAPI_ENCODER_FPS_N (encoder));
   else
-    encoder->cts_offset = 0;
+    encoder->cts_offset = GST_CLOCK_TIME_NONE;
 
   /* init max_frame_num, max_poc */
   encoder->log2_max_frame_num =

gst-libs/gst/vaapi/gstvaapiencoder_h265.c
@@ -2030,11 +2030,11 @@ reset_properties (GstVaapiEncoderH265 * encoder)
   if (encoder->num_bframes > (base_encoder->keyframe_period + 1) / 2)
     encoder->num_bframes = (base_encoder->keyframe_period + 1) / 2;
 
-  if (encoder->num_bframes)
-    encoder->cts_offset = GST_SECOND * GST_VAAPI_ENCODER_FPS_D (encoder) /
-        GST_VAAPI_ENCODER_FPS_N (encoder);
+  if (encoder->num_bframes > 0 && GST_VAAPI_ENCODER_FPS_N (encoder) > 0)
+    encoder->cts_offset = gst_util_uint64_scale (GST_SECOND,
+        GST_VAAPI_ENCODER_FPS_D (encoder), GST_VAAPI_ENCODER_FPS_N (encoder));
   else
-    encoder->cts_offset = 0;
+    encoder->cts_offset = GST_CLOCK_TIME_NONE;
 
   /* init max_poc */
   encoder->log2_max_pic_order_cnt =

commit ad3b45ebc4fc771fd9d315ac53c8ab376276ed19

encoder: h264: fix C90 mixed declarations and code
Commit 4259d1a introduced this compilation error. This patch fixes it.

gst-libs/gst/vaapi/gstvaapiencoder_h264.c
@@ -2377,12 +2377,14 @@ ensure_bitrate (GstVaapiEncoderH264 * encoder)
            thus estimating +15% here ; and using adaptive 8x8 transforms
            in I-frames could bring up to +10% improvement. */
         guint bits_per_mb = 48;
+        guint64 factor;
+
         if (!encoder->use_cabac)
           bits_per_mb += (bits_per_mb * 15) / 100;
         if (!encoder->use_dct8x8)
           bits_per_mb += (bits_per_mb * 10) / 100;
 
-        guint64 factor = encoder->mb_width * encoder->mb_height * bits_per_mb;
+        factor = encoder->mb_width * encoder->mb_height * bits_per_mb;
         base_encoder->bitrate =
             gst_util_uint64_scale (factor, GST_VAAPI_ENCODER_FPS_N (encoder),
             GST_VAAPI_ENCODER_FPS_D (encoder)) / 1000;