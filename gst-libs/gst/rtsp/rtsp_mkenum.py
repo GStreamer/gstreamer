5c4f4ac1bd51c4db2935ea87113a989d619e4a81
Nirbheek Chauhan <nirbheek@centricular.com>
2016-08-12 20:56:31 +0530

    Add support for Meson as alternative/parallel build system
    https://github.com/mesonbuild/meson
    
    With contributions from:
    
    Tim-Philipp Müller <tim@centricular.com>
    Jussi Pakkanen <jpakkane@gmail.com> (original port)
    
    Highlights of the features provided are:
    * Faster builds on Linux (~40-50% faster)
    * The ability to build with MSVC on Windows
    * Generate Visual Studio project files
    * Generate XCode project files
    * Much faster builds on Windows (on-par with Linux)
    * Seriously fast configure and building on embedded
    
    ... and many more. For more details see:
    
    http://blog.nirbheek.in/2016/05/gstreamer-and-meson-new-hope.html
    http://blog.nirbheek.in/2016/07/building-and-developing-gstreamer-using.html
    
    Building with Meson should work on both Linux and Windows, but may
    need a few more tweaks on other operating systems.

---
+++ gst-libs/gst/rtsp/rtsp_mkenum.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+# This is in its own file rather than inside meson.build
+# because a) mixing the two is ugly and b) trying to
+# make special characters such as \n go through all
+# backends is a fool's errand.
+
+import sys, os, shutil, subprocess
+
+h_array = [
+    '--fhead',
+    "#ifndef __gst_rtsp_ENUM_TYPES_H__\n#define __gst_rtsp_ENUM_TYPES_H__\n\n#include <glib-object.h>\n\nG_BEGIN_DECLS\n",
+    '--fprod',
+    "\n/* enumerations from \"@filename@\" */\n",
+    '--vhead',
+    "GType @enum_name@_get_type (void);\n#define GST_TYPE_@ENUMSHORT@ (@enum_name@_get_type())\n",
+    '--ftail',
+    "G_END_DECLS\n\n#endif /* __gst_rtsp_ENUM_TYPES_H__ */"
+]
+
+c_array = ['--fhead',
+           "#include \"gstrtsp-enumtypes.h\"\n\n#include \"rtsp.h\"",
+           '--fprod',
+           "\n/* enumerations from \"@filename@\" */",
+           '--vhead',
+           "GType\n@enum_name@_get_type (void)\n{\n  static volatile gsize g_define_type_id__volatile = 0;\n  if (g_once_init_enter (&g_define_type_id__volatile)) {\n    static const G@Type@Value values[] = {",
+           '--vprod',
+           "      { @VALUENAME@, \"@VALUENAME@\", \"@valuenick@\" },",
+           '--vtail',
+           "      { 0, NULL, NULL }\n    };\n    GType g_define_type_id = g_@type@_register_static (\"@EnumName@\", values);\n    g_once_init_leave (&g_define_type_id__volatile, g_define_type_id);\n  }\n  return g_define_type_id__volatile;\n}\n"
+]
+
+
+cmd = []
+argn = 1
+# Find the full command needed to run glib-mkenums
+# On UNIX-like, this is just the full path to glib-mkenums
+# On Windows, this is the full path to interpreter + full path to glib-mkenums
+for arg in sys.argv[1:]:
+    cmd.append(arg)
+    argn += 1
+    if arg.endswith('glib-mkenums'):
+        break
+ofilename = sys.argv[argn]
+headers = sys.argv[argn + 1:]
+
+if ofilename.endswith('.h'):
+    arg_array = h_array
+else:
+    arg_array = c_array
+
+cmd_array = cmd + arg_array + headers
+pc = subprocess.Popen(cmd_array, stdout=subprocess.PIPE)
+(stdo, _) = pc.communicate()
+if pc.returncode != 0:
+    sys.exit(pc.returncode)
+open(ofilename, 'wb').write(stdo)
