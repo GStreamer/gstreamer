GStreamer/gstreamer

commit 91fea30ff47f2402385808f270a0ced7fdfc4625

gstglupload: make the GLMemoryUpload method output the correct texture-target
Now when used with video/x-raw as input, the GLMemoryUpload method checks for
->tex_target in input GLMemory(es) and sets the output texture-target
accordingly.

Fixes video corruption with a pipeline like avfvideosrc ! video/x-raw !
glimagesink where on macos avfvideosrc pushes RECTANGLE textures but glupload
was configuring texture-target=2D as output.

gst-libs/gst/gl/gstglupload.c
@@ -189,7 +189,7 @@ struct _UploadMethod
   GstStaticCaps *input_template_caps;
 
     gpointer (*new) (GstGLUpload * upload);
-  GstCaps *(*transform_caps) (GstGLContext * context,
+  GstCaps *(*transform_caps) (gpointer impl, GstGLContext * context,
       GstPadDirection direction, GstCaps * caps);
     gboolean (*accept) (gpointer impl, GstBuffer * buffer, GstCaps * in_caps,
       GstCaps * out_caps);
@@ -203,6 +203,7 @@ struct _UploadMethod
 struct GLMemoryUpload
 {
   GstGLUpload *upload;
+  GstGLTextureTarget input_target;
 };
 
 static gpointer
@@ -211,14 +212,16 @@ _gl_memory_upload_new (GstGLUpload * upload)
   struct GLMemoryUpload *mem = g_new0 (struct GLMemoryUpload, 1);
 
   mem->upload = upload;
+  mem->input_target = GST_GL_TEXTURE_TARGET_NONE;
 
   return mem;
 }
 
 static GstCaps *
-_gl_memory_upload_transform_caps (GstGLContext * context,
+_gl_memory_upload_transform_caps (gpointer impl, GstGLContext * context,
     GstPadDirection direction, GstCaps * caps)
 {
+  struct GLMemoryUpload *upload = impl;
   GstCapsFeatures *passthrough =
       gst_caps_features_from_string
       (GST_CAPS_FEATURE_META_GST_VIDEO_OVERLAY_COMPOSITION);
@@ -231,12 +234,17 @@ _gl_memory_upload_transform_caps (GstGLContext * context,
   gst_caps_features_free (passthrough);
 
   if (direction == GST_PAD_SINK) {
-    GstGLTextureTarget target_mask = 0;
     GstCaps *tmp;
+    GstGLTextureTarget target_mask;
 
-    target_mask |= 1 << GST_GL_TEXTURE_TARGET_2D;
-    target_mask |= 1 << GST_GL_TEXTURE_TARGET_RECTANGLE;
-    target_mask |= 1 << GST_GL_TEXTURE_TARGET_EXTERNAL_OES;
+    if (upload->input_target != GST_GL_TEXTURE_TARGET_NONE) {
+      target_mask = 1 << upload->input_target;
+    } else {
+      target_mask = 1 << GST_GL_TEXTURE_TARGET_2D |
+          1 << GST_GL_TEXTURE_TARGET_RECTANGLE |
+          1 << GST_GL_TEXTURE_TARGET_EXTERNAL_OES;
+    }
+
     tmp = _caps_intersect_texture_target (ret, target_mask);
     gst_caps_unref (ret);
     ret = tmp;
@@ -412,6 +420,12 @@ _gl_memory_upload_perform (gpointer impl, GstBuffer * buffer,
             gl_mem->mem.context))
       return GST_GL_UPLOAD_UNSHARED_GL_CONTEXT;
 
+    if (upload->input_target != gl_mem->tex_target) {
+      upload->input_target = gl_mem->tex_target;
+      *outbuf = NULL;
+      return GST_GL_UPLOAD_RECONFIGURE;
+    }
+
     if (gst_is_gl_memory_pbo (mem))
       gst_gl_memory_pbo_upload_transfer ((GstGLMemoryPBO *) mem);
   }
@@ -466,7 +480,7 @@ _dma_buf_upload_new (GstGLUpload * upload)
 }
 
 static GstCaps *
-_dma_buf_upload_transform_caps (GstGLContext * context,
+_dma_buf_upload_transform_caps (gpointer impl, GstGLContext * context,
     GstPadDirection direction, GstCaps * caps)
 {
   GstCapsFeatures *passthrough =
@@ -714,13 +728,13 @@ _upload_meta_upload_new (GstGLUpload * upload)
   struct GLUploadMeta *meta = g_new0 (struct GLUploadMeta, 1);
 
   meta->upload = upload;
-  meta->pool = gst_gl_buffer_pool_new (upload->context);
+  meta->pool = NULL;
 
   return meta;
 }
 
 static GstCaps *
-_upload_meta_upload_transform_caps (GstGLContext * context,
+_upload_meta_upload_transform_caps (gpointer impl, GstGLContext * context,
     GstPadDirection direction, GstCaps * caps)
 {
   GstCapsFeatures *passthrough =
@@ -782,6 +796,9 @@ _upload_meta_upload_accept (gpointer impl, GstBuffer * buffer,
 
   if (!ret)
     return ret;
+
+  if (upload->pool == NULL)
+    upload->pool = gst_gl_buffer_pool_new (upload->upload->context);
 
   if (!gst_buffer_pool_is_active (upload->pool)) {
     config = gst_buffer_pool_get_config (upload->pool);
@@ -1012,7 +1029,7 @@ _raw_data_upload_new (GstGLUpload * upload)
 }
 
 static GstCaps *
-_raw_data_upload_transform_caps (GstGLContext * context,
+_raw_data_upload_transform_caps (gpointer impl, GstGLContext * context,
     GstPadDirection direction, GstCaps * caps)
 {
   GstCapsFeatures *passthrough =
@@ -1216,7 +1233,10 @@ gst_gl_upload_new (GstGLContext * context)
   GstGLUpload *upload = g_object_new (GST_TYPE_GL_UPLOAD, NULL);
   gint i, n;
 
-  upload->context = gst_object_ref (context);
+  if (context)
+    gst_gl_upload_set_context (upload, context);
+  else
+    upload->context = NULL;
 
   n = G_N_ELEMENTS (upload_methods);
   upload->priv->upload_impl = g_malloc (sizeof (gpointer) * n);
@@ -1230,6 +1250,14 @@ gst_gl_upload_new (GstGLContext * context)
   return upload;
 }
 
+void
+gst_gl_upload_set_context (GstGLUpload * upload, GstGLContext * context)
+{
+  g_return_if_fail (upload != NULL);
+
+  gst_object_replace ((GstObject **) & upload->context, (GstObject *) context);
+}
+
 static void
 gst_gl_upload_finalize (GObject * object)
 {
@@ -1238,8 +1266,6 @@ gst_gl_upload_finalize (GObject * object)
 
   upload = GST_GL_UPLOAD (object);
 
-  if (upload->priv->method_impl)
-    upload->priv->method->free (upload->priv->method_impl);
   upload->priv->method_i = 0;
 
   if (upload->context) {
@@ -1268,8 +1294,8 @@ gst_gl_upload_finalize (GObject * object)
 }
 
 GstCaps *
-gst_gl_upload_transform_caps (GstGLContext * context, GstPadDirection direction,
-    GstCaps * caps, GstCaps * filter)
+gst_gl_upload_transform_caps (GstGLUpload * upload, GstGLContext * context,
+    GstPadDirection direction, GstCaps * caps, GstCaps * filter)
 {
   GstCaps *result, *tmp;
   gint i;
@@ -1279,7 +1305,9 @@ gst_gl_upload_transform_caps (GstGLContext * context, GstPadDirection direction,
   for (i = 0; i < G_N_ELEMENTS (upload_methods); i++) {
     GstCaps *tmp2;
 
-    tmp2 = upload_methods[i]->transform_caps (context, direction, caps);
+    tmp2 =
+        upload_methods[i]->transform_caps (upload->priv->upload_impl[i],
+        context, direction, caps);
 
     if (tmp2)
       tmp = gst_caps_merge (tmp, tmp2);
@@ -1332,8 +1360,6 @@ _gst_gl_upload_set_caps_unlocked (GstGLUpload * upload, GstCaps * in_caps,
   gst_video_info_from_caps (&upload->priv->in_info, in_caps);
   gst_video_info_from_caps (&upload->priv->out_info, out_caps);
 
-  if (upload->priv->method_impl)
-    upload->priv->method->free (upload->priv->method_impl);
   upload->priv->method_impl = NULL;
   upload->priv->method_i = 0;
 
@@ -1388,16 +1414,14 @@ gst_gl_upload_get_caps (GstGLUpload * upload, GstCaps ** in_caps,
 static gboolean
 _upload_find_method (GstGLUpload * upload)
 {
+  gint method_i;
+
   if (upload->priv->method_i >= G_N_ELEMENTS (upload_methods))
     return FALSE;
 
-  if (upload->priv->method_impl) {
-    upload->priv->method->free (upload->priv->method_impl);
-    upload->priv->method_impl = NULL;
-  }
-
-  upload->priv->method = upload_methods[upload->priv->method_i];
-  upload->priv->method_impl = upload->priv->method->new (upload);
+  method_i = upload->priv->method_i;
+  upload->priv->method = upload_methods[method_i];
+  upload->priv->method_impl = upload->priv->upload_impl[method_i];
 
   GST_DEBUG_OBJECT (upload, "attempting upload with uploader %s",
       upload->priv->method->name);
@@ -1452,19 +1476,24 @@ restart:
       upload->priv->method->perform (upload->priv->method_impl, buffer,
       &outbuf);
   if (ret == GST_GL_UPLOAD_UNSHARED_GL_CONTEXT) {
-    upload->priv->method->free (upload->priv->method_impl);
-    upload->priv->method = &_raw_data_upload;
-    upload->priv->method_impl = upload->priv->method->new (upload);
+    for (int i = 0; i < G_N_ELEMENTS (upload_methods); i++) {
+      if (upload_methods[i] == &_raw_data_upload) {
+        upload->priv->method = &_raw_data_upload;
+        upload->priv->method_impl = upload->priv->upload_impl[i];
+        upload->priv->method_i = i;
+
+        break;
+      }
+    }
     goto restart;
-  } else if (ret == GST_GL_UPLOAD_DONE) {
+  } else if (ret == GST_GL_UPLOAD_DONE || ret == GST_GL_UPLOAD_RECONFIGURE) {
     /* we are done */
   } else {
-    upload->priv->method->free (upload->priv->method_impl);
     upload->priv->method_impl = NULL;
     NEXT_METHOD;
   }
 
-  if (buffer != outbuf)
+  if (outbuf && buffer != outbuf)
     gst_buffer_copy_into (outbuf, buffer,
         GST_BUFFER_COPY_FLAGS | GST_BUFFER_COPY_TIMESTAMPS, 0, -1);
   *outbuf_ptr = outbuf;

ext/gl/gstgluploadelement.c
@@ -97,6 +97,8 @@ static void
 gst_gl_upload_element_init (GstGLUploadElement * upload)
 {
   gst_base_transform_set_prefer_passthrough (GST_BASE_TRANSFORM (upload), TRUE);
+
+  upload->upload = gst_gl_upload_new (NULL);
 }
 
 static gboolean
@@ -133,9 +135,11 @@ static GstCaps *
 _gst_gl_upload_element_transform_caps (GstBaseTransform * bt,
     GstPadDirection direction, GstCaps * caps, GstCaps * filter)
 {
+  GstGLUploadElement *upload = GST_GL_UPLOAD_ELEMENT (bt);
   GstGLContext *context = GST_GL_BASE_FILTER (bt)->context;
 
-  return gst_gl_upload_transform_caps (context, direction, caps, filter);
+  return gst_gl_upload_transform_caps (upload->upload, context, direction, caps,
+      filter);
 }
 
 static gboolean
@@ -177,10 +181,10 @@ _gst_gl_upload_element_decide_allocation (GstBaseTransform * trans,
   if (!ret)
     return FALSE;
 
+  /* GstGLBaseFilter populates ->context in ::decide_allocation so now it's the
+   * time to set the ->upload context */
   context = GST_GL_BASE_FILTER (trans)->context;
-
-  if (!upload->upload)
-    upload->upload = gst_gl_upload_new (context);
+  gst_gl_upload_set_context (upload->upload, context);
 
   return gst_gl_upload_set_caps (upload->upload, upload->in_caps,
       upload->out_caps);
@@ -220,6 +224,10 @@ gst_gl_upload_element_prepare_output_buffer (GstBaseTransform * bt,
     return GST_FLOW_NOT_NEGOTIATED;
 
   ret = gst_gl_upload_perform_with_buffer (upload->upload, buffer, outbuf);
+  if (ret == GST_GL_UPLOAD_RECONFIGURE) {
+    gst_base_transform_reconfigure_src (bt);
+    return GST_FLOW_OK;
+  }
 
   if (ret != GST_GL_UPLOAD_DONE || *outbuf == NULL) {
     GST_ELEMENT_ERROR (bt, RESOURCE, NOT_FOUND, ("%s",

gst-libs/gst/gl/gstglupload.h
@@ -47,8 +47,9 @@ typedef enum
 
   GST_GL_UPLOAD_ERROR = -1,
   GST_GL_UPLOAD_UNSUPPORTED = -2,
+  GST_GL_UPLOAD_RECONFIGURE = -3,
   /* <private> */
-  GST_GL_UPLOAD_UNSHARED_GL_CONTEXT = -3,
+  GST_GL_UPLOAD_UNSHARED_GL_CONTEXT = -100,
 } GstGLUploadReturn;
 
 /**
@@ -82,7 +83,11 @@ GstCaps *     gst_gl_upload_get_input_template_caps (void);
 
 GstGLUpload * gst_gl_upload_new                    (GstGLContext * context);
 
-GstCaps *     gst_gl_upload_transform_caps         (GstGLContext * context,
+void          gst_gl_upload_set_context            (GstGLUpload * upload,
+                                                    GstGLContext * context);
+
+GstCaps *     gst_gl_upload_transform_caps         (GstGLUpload * upload,
+                                                    GstGLContext * context,
                                                     GstPadDirection direction,
                                                     GstCaps * caps,
                                                     GstCaps * filter);

commit a7dccec0fff373b0fb86374dd0026126ff7bbaa7

glupload: fix tests and check for the new RECONFIGURE behaviour
The tests were broken since 91fea30, which changed glupload to return
GST_GL_UPLOAD_RECONFIGURE if the texture target in the input buffers doesn't
match the texture-target configured in the output caps.

This commit fixes that and adds more checks for the new behaviour.

gst-libs/gst/gl/gstglupload.c
@@ -204,6 +204,7 @@ struct GLMemoryUpload
 {
   GstGLUpload *upload;
   GstGLTextureTarget input_target;
+  GstGLTextureTarget output_target;
 };
 
 static gpointer
@@ -213,6 +214,7 @@ _gl_memory_upload_new (GstGLUpload * upload)
 
   mem->upload = upload;
   mem->input_target = GST_GL_TEXTURE_TARGET_NONE;
+  mem->output_target = GST_GL_TEXTURE_TARGET_NONE;
 
   return mem;
 }
@@ -420,8 +422,17 @@ _gl_memory_upload_perform (gpointer impl, GstBuffer * buffer,
             gl_mem->mem.context))
       return GST_GL_UPLOAD_UNSHARED_GL_CONTEXT;
 
-    if (upload->input_target != gl_mem->tex_target) {
-      upload->input_target = gl_mem->tex_target;
+    if (upload->output_target == GST_GL_TEXTURE_TARGET_NONE &&
+        upload->upload->priv->out_caps) {
+      upload->output_target =
+          _caps_get_texture_target (upload->upload->priv->out_caps,
+          GST_GL_TEXTURE_TARGET_NONE);
+    }
+
+    /* always track the last input texture target so ::transform_caps() can
+     * use it to build the output caps */
+    upload->input_target = gl_mem->tex_target;
+    if (upload->output_target != gl_mem->tex_target) {
       *outbuf = NULL;
       return GST_GL_UPLOAD_RECONFIGURE;
     }

tests/check/libs/gstglupload.c
@@ -194,13 +194,14 @@ GST_START_TEST (test_upload_data)
 
 GST_END_TEST;
 
-GST_START_TEST (test_upload_buffer)
+GST_START_TEST (test_upload_gl_memory)
 {
   GstGLBaseMemoryAllocator *base_mem_alloc;
   GstGLVideoAllocationParams *params;
   GstBuffer *buffer, *outbuf;
   GstGLMemory *gl_mem;
   GstCaps *in_caps, *out_caps;
+  GstStructure *out_s;
   GstVideoInfo in_info;
   GstMapInfo map_info;
   gint i = 0;
@@ -231,13 +232,53 @@ GST_START_TEST (test_upload_buffer)
 
   gst_buffer_append_memory (buffer, (GstMemory *) gl_mem);
 
+  /* at this point glupload hasn't received any buffers so can output anything */
+  out_caps = gst_gl_upload_transform_caps (upload, context,
+      GST_PAD_SINK, in_caps, NULL);
+  out_s = gst_caps_get_structure (out_caps, 0);
+  fail_unless (gst_structure_has_field_typed (out_s, "texture-target",
+          GST_TYPE_LIST));
+  gst_caps_unref (out_caps);
+
+  /* set some output caps without setting texture-target: this should trigger RECONFIGURE */
   out_caps = gst_caps_from_string ("video/x-raw(memory:GLMemory),"
       "format=RGBA,width=10,height=10");
 
+  /* set caps with texture-target not fixed. This should trigger RECONFIGURE. */
   gst_gl_upload_set_caps (upload, in_caps, out_caps);
+  gst_caps_unref (out_caps);
+
+  /* push a texture-target=2D buffer */
+  res = gst_gl_upload_perform_with_buffer (upload, buffer, &outbuf);
+  fail_unless (res == GST_GL_UPLOAD_RECONFIGURE);
+  fail_if (outbuf);
+
+  /* now glupload has seen a 2D buffer and so wants to transform to that */
+  out_caps = gst_gl_upload_transform_caps (upload, context,
+      GST_PAD_SINK, in_caps, NULL);
+  out_s = gst_caps_get_structure (out_caps, 0);
+  fail_unless_equals_string (gst_structure_get_string (out_s, "texture-target"),
+      "2D");
+  gst_caps_unref (out_caps);
+
+  /* try setting the wrong type first tho */
+  out_caps = gst_caps_from_string ("video/x-raw(memory:GLMemory),"
+      "format=RGBA,width=10,height=10,texture-target=RECTANGLE");
+  gst_gl_upload_set_caps (upload, in_caps, out_caps);
+  gst_caps_unref (out_caps);
 
   res = gst_gl_upload_perform_with_buffer (upload, buffer, &outbuf);
-  fail_if (res == FALSE, "Failed to upload buffer");
+  fail_unless (res == GST_GL_UPLOAD_RECONFIGURE);
+  fail_if (outbuf);
+
+  /* finally do set the correct texture-target */
+  out_caps = gst_caps_from_string ("video/x-raw(memory:GLMemory),"
+      "format=RGBA,width=10,height=10,texture-target=2D");
+  gst_gl_upload_set_caps (upload, in_caps, out_caps);
+  gst_caps_unref (out_caps);
+
+  res = gst_gl_upload_perform_with_buffer (upload, buffer, &outbuf);
+  fail_unless (res == GST_GL_UPLOAD_DONE, "Failed to upload buffer");
   fail_unless (GST_IS_BUFFER (outbuf));
 
   gst_gl_window_set_preferred_size (window, WIDTH, HEIGHT);
@@ -251,7 +292,6 @@ GST_START_TEST (test_upload_buffer)
   }
 
   gst_caps_unref (in_caps);
-  gst_caps_unref (out_caps);
   gst_buffer_unref (buffer);
   gst_buffer_unref (outbuf);
   gst_object_unref (base_mem_alloc);
@@ -269,7 +309,7 @@ gst_gl_upload_suite (void)
   suite_add_tcase (s, tc_chain);
   tcase_add_checked_fixture (tc_chain, setup, teardown);
   tcase_add_test (tc_chain, test_upload_data);
-  tcase_add_test (tc_chain, test_upload_buffer);
+  tcase_add_test (tc_chain, test_upload_gl_memory);
 
   return s;
 }