740749ac55f680ad61a328f86b595fa2837dd14c
Vincent Penquerc'h <vincent.penquerch@collabora.co.uk>
2016-09-05 09:35:53 +0100

    aacparse: fix varlength number reading as per spec
    https://bugzilla.gnome.org/show_bug.cgi?id=769278

--- gst/audioparsers/gstaacparse.c
+++ gst/audioparsers/gstaacparse.c
@@ -481,7 +481,7 @@ gst_aac_parse_latm_get_value (GstAacParse * aacparse, GstBitReader * br,
   *value = 0;
   if (!gst_bit_reader_get_bits_uint8 (br, &bytes, 2))
     return FALSE;
-  for (i = 0; i < bytes; ++i) {
+  for (i = 0; i <= bytes; ++i) {
     *value <<= 8;
     if (!gst_bit_reader_get_bits_uint8 (br, &byte, 8))
       return FALSE;
