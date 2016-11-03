be5a7733a1665fe49331f7e5a5adc344e44c172c
Tim-Philipp Müller <tim@centricular.com>
2016-11-03 13:27:51 +0000

    tests: wavparse: add test for processing an actual .wav file
    https://bugzilla.gnome.org/show_bug.cgi?id=773861

--- tests/check/elements/wavparse.c
+++ tests/check/elements/wavparse.c
@@ -21,6 +21,67 @@
 
 #include <gst/check/gstcheck.h>
 
+#define SIMPLE_WAV_PATH GST_TEST_FILES_PATH G_DIR_SEPARATOR_S "audiotestsrc.wav"
+
+static void
+do_test_simple_file (GstPadMode mode)
+{
+  GstStateChangeReturn ret;
+  GstElement *pipeline;
+  GstElement *src, *q = NULL;
+  GstElement *wavparse;
+  GstElement *fakesink;
+  GstMessage *msg;
+
+  pipeline = gst_pipeline_new ("testpipe");
+  src = gst_element_factory_make ("filesrc", NULL);
+  fail_if (src == NULL);
+  if (mode == GST_PAD_MODE_PUSH)
+    q = gst_element_factory_make ("queue", NULL);
+  wavparse = gst_element_factory_make ("wavparse", NULL);
+  fail_if (wavparse == NULL);
+  fakesink = gst_element_factory_make ("fakesink", NULL);
+  fail_if (fakesink == NULL);
+
+  gst_bin_add_many (GST_BIN (pipeline), src, wavparse, fakesink, q, NULL);
+
+  g_object_set (src, "location", SIMPLE_WAV_PATH, NULL);
+
+  if (mode == GST_PAD_MODE_PUSH)
+    fail_unless (gst_element_link_many (src, q, wavparse, fakesink, NULL));
+  else
+    fail_unless (gst_element_link_many (src, wavparse, fakesink, NULL));
+
+  ret = gst_element_set_state (pipeline, GST_STATE_PLAYING);
+  fail_unless_equals_int (ret, GST_STATE_CHANGE_ASYNC);
+
+  ret = gst_element_get_state (pipeline, NULL, NULL, GST_CLOCK_TIME_NONE);
+  fail_unless_equals_int (ret, GST_STATE_CHANGE_SUCCESS);
+
+  msg = gst_bus_timed_pop_filtered (GST_ELEMENT_BUS (pipeline),
+      GST_CLOCK_TIME_NONE, GST_MESSAGE_EOS | GST_MESSAGE_ERROR);
+
+  fail_unless_equals_string (GST_MESSAGE_TYPE_NAME (msg), "eos");
+
+  gst_message_unref (msg);
+  gst_element_set_state (pipeline, GST_STATE_NULL);
+  gst_object_unref (pipeline);
+}
+
+GST_START_TEST (test_simple_file_pull)
+{
+  do_test_simple_file (TRUE);
+}
+
+GST_END_TEST;
+
+GST_START_TEST (test_simple_file_push)
+{
+  do_test_simple_file (FALSE);
+}
+
+GST_END_TEST;
+
 static void
 do_test_empty_file (gboolean can_activate_pull)
 {
@@ -81,6 +142,8 @@ wavparse_suite (void)
   suite_add_tcase (s, tc_chain);
   tcase_add_test (tc_chain, test_empty_file_pull);
   tcase_add_test (tc_chain, test_empty_file_push);
+  tcase_add_test (tc_chain, test_simple_file_pull);
+  tcase_add_test (tc_chain, test_simple_file_push);
   return s;
 }
 
