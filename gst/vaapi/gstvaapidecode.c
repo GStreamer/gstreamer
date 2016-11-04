d7231f66ccd7b460c3465c4fdff6694813397114
Víctor Manuel Jáquez Leal <victorx.jaquez@intel.com>
2016-11-04 16:26:18 +0100

    vaapidecode: guard GST_VAAPI_MAKE_GLTEXUPLOAD_CAPS
    In commit 6d11a00 were introduced a regression when gstreamer-vaapi is
    compiled with out EGL/GLX support: it shall not support
    GST_VAAPI_MAKE_GLTEXUPLOAD_CAPS.
    
    This patch guards the inclusion of GST_VAAPI_MAKE_GLTEXUPLOAD_CAPS in the
    allowed src caps for vaapedecode if EGL/GLX.

--- gst/vaapi/gstvaapidecode.c
+++ gst/vaapi/gstvaapidecode.c
@@ -212,8 +212,11 @@ gst_vaapidecode_ensure_allowed_srcpad_caps (GstVaapiDecode * decode)
     return FALSE;
 
   /* Create VA caps */
-  out_caps = gst_caps_from_string (GST_VAAPI_MAKE_SURFACE_CAPS ";"
-      GST_VAAPI_MAKE_GLTEXUPLOAD_CAPS);
+  out_caps = gst_caps_from_string (GST_VAAPI_MAKE_SURFACE_CAPS
+#if (USE_GLX || USE_EGL)
+      ";" GST_VAAPI_MAKE_GLTEXUPLOAD_CAPS
+#endif
+      );
   if (!out_caps) {
     GST_WARNING_OBJECT (decode, "failed to create VA/GL source caps");
     return FALSE;
