584da6e86c1814262de5622520be5817ec1c0dd4
Scott D Phillips <scott.d.phillips@intel.com>
2016-11-11 10:23:17 -0800

    taglist: remove `return void` in gst_tag_register
    MSVC warns on this and the documentation about the warning says:
    
    > The compiler assumes the function returns a value of type int
    
    which is a little scary, so lets just remove the unnecessary 'return'
    
    https://bugzilla.gnome.org/show_bug.cgi?id=774293

--- gst/gsttaglist.c
+++ gst/gsttaglist.c
@@ -509,7 +509,7 @@ gst_tag_register (const gchar * name, GstTagFlag flag, GType type,
   g_return_if_fail (blurb != NULL);
   g_return_if_fail (type != 0 && type != GST_TYPE_LIST);
 
-  return gst_tag_register_static (g_intern_string (name), flag, type,
+  gst_tag_register_static (g_intern_string (name), flag, type,
       g_intern_string (nick), g_intern_string (blurb), func);
 }
 
