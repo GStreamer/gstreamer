6eda156b2041175dc3e88ea6cfb842b9ce5089b1
Olivier Crête <olivier.crete@collabora.com>
2016-11-18 14:41:54 -0500

    aggregator: Don't restrict sink pad names
    Sink pads could have other names than sink_%u
    
    https://bugzilla.gnome.org/show_bug.cgi?id=782920

--- libs/gst/base/gstaggregator.c
+++ libs/gst/base/gstaggregator.c
@@ -1603,8 +1603,7 @@ gst_aggregator_default_create_new_pad (GstAggregator * self,
   gint serial = 0;
   gchar *name = NULL;
 
-  if (templ->direction != GST_PAD_SINK ||
-      g_strcmp0 (templ->name_template, "sink_%u") != 0)
+  if (templ->direction != GST_PAD_SINK)
     goto not_sink;
 
   GST_OBJECT_LOCK (self);
