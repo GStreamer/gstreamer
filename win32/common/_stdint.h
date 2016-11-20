5b4010c7b524a3073d6d5e293638ab4d961ba9d2
Tim-Philipp Müller <tim@centricular.com>
2016-11-20 15:43:42 +0000

    win32: remove copies of generated headers

--- win32/common/_stdint.h
+++
@@ -1,9 +0,0 @@
-#ifndef _GST_PLUGINS_BASE__STDINT_H
-#define _GST_PLUGINS_BASE__STDINT_H 1
-#ifndef _GENERATED_STDINT_H
-#define _GENERATED_STDINT_H "gst-plugins-base 1.10.0"
-/* generated using gnu compiler gcc-6 (Debian 6.2.0-10) 6.2.0 20161027 */
-#define _STDINT_HAVE_STDINT_H 1
-#include <stdint.h>
-#endif
-#endif
