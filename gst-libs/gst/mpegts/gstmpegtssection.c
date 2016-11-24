d1fa342b71be439105bef9836153b47ff3431934
Edward Hervey <edward@centricular.com>
2016-11-24 11:12:23 +0100

    mpegtssection: Don't free empty streams
    Also avoids a useless assertion

--- gst-libs/gst/mpegts/gstmpegtssection.c
+++ gst-libs/gst/mpegts/gstmpegtssection.c
@@ -632,7 +632,8 @@ _gst_mpegts_pmt_free (GstMpegtsPMT * pmt)
 {
   if (pmt->descriptors)
     g_ptr_array_unref (pmt->descriptors);
-  g_ptr_array_unref (pmt->streams);
+  if (pmt->streams)
+    g_ptr_array_unref (pmt->streams);
   g_slice_free (GstMpegtsPMT, pmt);
 }
 
