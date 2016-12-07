09952ddc4b29e384ca06fbe5c4e58a1012cc6395
Haihua Hu <jared.hu@nxp.com>
2016-12-07 16:51:27 +0800

    gl/effects: use non-PBO GLMemory for internal textures
    middle textures in gleffects do not need to use GstGLMemoryPBO as they
    aren't transfering data to/from the GPU. This will cost too much DMA
    memory and cause performance issue. Change the allocator to use non-PBO
    GstGLMemory.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=776072

--- ext/gl/gstgleffects.c
+++ ext/gl/gstgleffects.c
@@ -283,7 +283,7 @@ gst_gl_effects_gl_start (GstGLBaseFilter * base_filter)
     return FALSE;
 
   base_alloc = (GstGLBaseMemoryAllocator *)
-      gst_gl_memory_allocator_get_default (context);
+      gst_allocator_find (GST_GL_MEMORY_ALLOCATOR_NAME);
   params =
       (GstGLAllocationParams *) gst_gl_video_allocation_params_new (context,
       NULL, &filter->out_info, 0, NULL, GST_GL_TEXTURE_TARGET_2D,
