GStreamer/gstreamer

commit 8716c23e2b0553db2982eb84c437f493c974385e

Prevent early EOS by resetting eos flag once GST_OMX_ACQUIRE_BUFFER_EOS is triggered
https://bugzilla.gnome.org/show_bug.cgi?id=774600

omx/gstomx.c
@@ -1343,11 +1343,12 @@ retry:
       GST_DEBUG_OBJECT (comp->parent, "Component %s port %d signalled EOS",
           comp->name, port->index);
       ret = GST_OMX_ACQUIRE_BUFFER_EOS;
+      port->eos = FALSE
       goto done;
     }
   }
 
-  /* 
+  /*
    * At this point we have no error or flushing/eos port
    * and a properly configured port.
    *

commit 7925355d72c8d3127fdf05a62fe5f90bb2f9a866

Fix broken build due to syntax error
Problem was introduced by 8716c23e2b0553db2982eb84c437f493c974385e

omx/gstomx.c
@@ -1343,7 +1343,7 @@ retry:
       GST_DEBUG_OBJECT (comp->parent, "Component %s port %d signalled EOS",
           comp->name, port->index);
       ret = GST_OMX_ACQUIRE_BUFFER_EOS;
-      port->eos = FALSE
+      port->eos = FALSE;
       goto done;
     }
   }