cd00052728a640d81f5d528c85b81e297351b403
Thibault Saunier <thibault.saunier@osg.samsung.com>
2017-01-03 14:38:24 -0300

    validate: Make validate launcher apps work in a meson uninstalled env

--- validate/launcher/__init__.py
+++ validate/launcher/__init__.py
@@ -16,3 +16,6 @@
 # License along with this program; if not, write to the
 # Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
 # Boston, MA 02110-1301, USA.
+
+from pkgutil import extend_path
+__path__ = extend_path(__path__, __name__)
