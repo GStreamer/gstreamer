ac1124efb4e5e1cef2853628ba5cb46cdb30308d
Sebastian Dröge <sebastian@centricular.com>
2017-01-19 14:24:07 +0200

    rtsp-client: Fix handling of keep-alive GET_PARAMETER/SET_PARAMETER
    While they logically have 0 bytes length, GstRTSPConnection is appending
    a '\0' to everything making the size be 1 instead.

--- gst/rtsp-server/rtsp-client.c
+++ gst/rtsp-server/rtsp-client.c
@@ -1250,8 +1250,8 @@ handle_get_param_request (GstRTSPClient * client, GstRTSPContext * ctx)
   if (res != GST_RTSP_OK)
     goto bad_request;
 
-  if (size == 0) {
-    /* no body, keep-alive request */
+  if (size == 1) {
+    /* no body (only '\0'), keep-alive request */
     send_generic_response (client, GST_RTSP_STS_OK, ctx);
   } else {
     /* there is a body, handle the params */
@@ -1302,8 +1302,8 @@ handle_set_param_request (GstRTSPClient * client, GstRTSPContext * ctx)
   if (res != GST_RTSP_OK)
     goto bad_request;
 
-  if (size == 0) {
-    /* no body, keep-alive request */
+  if (size == 1) {
+    /* no body (only '\0'), keep-alive request */
     send_generic_response (client, GST_RTSP_STS_OK, ctx);
   } else {
     /* there is a body, handle the params */
