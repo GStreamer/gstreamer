99d5d7570d0b53dad3bc8eb653b1320ee422aace
Sebastian Dröge <sebastian@centricular.com>
2017-01-19 07:52:33 +0200

    qtdemux: Increment current stts index in all code paths after reading one chunk
    Otherwise we could read more chunks than there are available, doing an
    out of bounds read and potentially crash.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=777469

--- gst/isomp4/qtdemux.c
+++ gst/isomp4/qtdemux.c
@@ -8545,6 +8545,7 @@ done2:
           /* save values */
           stream->stts_time = stts_time;
           stream->stts_sample_index = j + 1;
+          stream->stts_index++;
           goto done3;
         }
       }
