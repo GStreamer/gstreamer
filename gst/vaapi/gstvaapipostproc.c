653bab8f1d69c4ef646ee0d1d27e0ffe70c5521e
Víctor Manuel Jáquez Leal <victorx.jaquez@intel.com>
2017-01-20 19:00:24 +0100

    vaapipostproc: set interlace mode
    if the vaapipostproc is configured to not do deinterlacing, the
    interlace-mode in the src caps should be the same as the input caps.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=777395

--- gst/vaapi/gstvaapipostproc.c
+++ gst/vaapi/gstvaapipostproc.c
@@ -68,7 +68,7 @@ static const char gst_vaapipostproc_src_caps_str[] =
   GST_VAAPI_MAKE_GLTEXUPLOAD_CAPS "; "
 #endif
   GST_VIDEO_CAPS_MAKE (GST_VIDEO_FORMATS_ALL) ", "
-  GST_CAPS_INTERLACED_FALSE;
+  GST_CAPS_INTERLACED_MODES;
 /* *INDENT-ON* */
 
 /* *INDENT-OFF* */
