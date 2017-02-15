ed7d1b668b3a95c135f232828f820c0480c76332
Stefan Sauer <ensonic@users.sf.net>
2017-02-15 20:37:40 +0100

    pad: fix docs for GstPadProbeReturn
    There is no 'block' value, but we have 'drop'. Also fix the markup; it
    is '%' to link to constants (and enum values).

--- gst/gstpad.h
+++ gst/gstpad.h
@@ -532,19 +532,19 @@ typedef enum
  * @GST_PAD_PROBE_OK: normal probe return value. This leaves the probe in
  *        place, and defers decisions about dropping or passing data to other
  *        probes, if any. If there are no other probes, the default behaviour
- *        for the probe type applies (block for blocking probes, and pass for
- *        non-blocking probes).
+ *        for the probe type applies (%GST_PAD_PROBE_DROP for blocking probes,
+ *        and %GST_PAD_PROBE_PASS for non-blocking probes).
  * @GST_PAD_PROBE_DROP: drop data in data probes. For push mode this means that
  *        the data item is not sent downstream. For pull mode, it means that
  *        the data item is not passed upstream. In both cases, no other probes
- *        are called for this item and #GST_FLOW_OK or %TRUE is returned to the
+ *        are called for this item and %GST_FLOW_OK or %TRUE is returned to the
  *        caller.
  * @GST_PAD_PROBE_REMOVE: remove this probe.
  * @GST_PAD_PROBE_PASS: pass the data item in the block probe and block on the
  *        next item.
  * @GST_PAD_PROBE_HANDLED: Data has been handled in the probe and will not be
  *        forwarded further. For events and buffers this is the same behaviour as
- *        @GST_PAD_PROBE_DROP (except that in this case you need to unref the buffer
+ *        %GST_PAD_PROBE_DROP (except that in this case you need to unref the buffer
  *        or event yourself). For queries it will also return %TRUE to the caller.
  *        The probe can also modify the #GstFlowReturn value by using the
  *        #GST_PAD_PROBE_INFO_FLOW_RETURN() accessor.
