dc28155e213279b09c0e03ef0c3960e1569f0f89
Sebastian Dröge <sebastian@centricular.com>
2017-02-28 10:53:04 +0200

    gl: Rename gst_gl_get_affine_transformation_meta_as_ndc_ext() to prevent symbol conflict
    The same symbol also exists in libgstgl, although marked as private and
    internal. This has no effect when doing static linking and there's a
    symbol conflict.

--- ext/gl/gstglvideomixer.c
+++ ext/gl/gstglvideomixer.c
@@ -1535,7 +1535,7 @@ gst_gl_video_mixer_callback (gpointer stuff)
 
       af_meta =
           gst_buffer_get_video_affine_transformation_meta (vagg_pad->buffer);
-      gst_gl_get_affine_transformation_meta_as_ndc (af_meta, matrix);
+      gst_gl_get_affine_transformation_meta_as_ndc_ext (af_meta, matrix);
       gst_gl_shader_set_uniform_matrix_4fv (video_mixer->shader,
           "u_transformation", 1, FALSE, matrix);
     }
