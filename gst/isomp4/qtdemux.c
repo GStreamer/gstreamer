61429a78a8debd2798ddd51ea81a4dc9adcfa90f
Michael Olbrich <m.olbrich@pengutronix.de>
2017-03-22 15:25:17 +0100

    qtdemux: allow larger files
    For really long files such as contiguous recordings of a whole day, the
    50MB limit is not sufficient.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=781458

--- gst/isomp4/qtdemux.c
+++ gst/isomp4/qtdemux.c
@@ -86,7 +86,7 @@
 #define QTDEMUX_MAX_ATOM_SIZE (25*1024*1024)
 
 /* if the sample index is larger than this, something is likely wrong */
-#define QTDEMUX_MAX_SAMPLE_INDEX_SIZE (50*1024*1024)
+#define QTDEMUX_MAX_SAMPLE_INDEX_SIZE (200*1024*1024)
 
 /* For converting qt creation times to unix epoch times */
 #define QTDEMUX_SECONDS_PER_DAY (60 * 60 * 24)
