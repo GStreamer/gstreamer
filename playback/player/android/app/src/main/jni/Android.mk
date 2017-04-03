71d85d3393b06f0b888cfbb6db3a5b4f3ba1ae8d
Arun Raghavan <arun@arunraghavan.net>
2017-04-03 13:23:50 +0530

    playback/player/android: Make plugin list in Android.mk easier to read
    Minor Makefile reformatting to make how users can select what plugins
    they want easier to figure out.

--- playback/player/android/app/src/main/jni/Android.mk
+++ playback/player/android/app/src/main/jni/Android.mk
@@ -30,7 +30,17 @@ endif
 GSTREAMER_NDK_BUILD_PATH  := $(GSTREAMER_ROOT)/share/gst-android/ndk-build/
 
 include $(GSTREAMER_NDK_BUILD_PATH)/plugins.mk
-GSTREAMER_PLUGINS         := $(GSTREAMER_PLUGINS_CORE) $(GSTREAMER_PLUGINS_PLAYBACK) $(GSTREAMER_PLUGINS_CODECS) $(GSTREAMER_PLUGINS_NET) $(GSTREAMER_PLUGINS_SYS) $(GSTREAMER_PLUGINS_CODECS_RESTRICTED) $(GSTREAMER_CODECS_GPL) $(GSTREAMER_PLUGINS_ENCODING) $(GSTREAMER_PLUGINS_VIS) $(GSTREAMER_PLUGINS_EFFECTS) $(GSTREAMER_PLUGINS_NET_RESTRICTED)
+GSTREAMER_PLUGINS         := $(GSTREAMER_PLUGINS_CORE)      \
+                             $(GSTREAMER_PLUGINS_PLAYBACK)  \
+                             $(GSTREAMER_PLUGINS_CODECS)    \
+                             $(GSTREAMER_PLUGINS_NET)       \
+                             $(GSTREAMER_PLUGINS_SYS)       \
+                             $(GSTREAMER_PLUGINS_CODECS_RESTRICTED) \
+                             $(GSTREAMER_CODECS_GPL)        \
+                             $(GSTREAMER_PLUGINS_ENCODING)  \
+                             $(GSTREAMER_PLUGINS_VIS)       \
+                             $(GSTREAMER_PLUGINS_EFFECTS)   \
+                             $(GSTREAMER_PLUGINS_NET_RESTRICTED)
 GSTREAMER_EXTRA_DEPS      := gstreamer-player-1.0 gstreamer-video-1.0 glib-2.0
 
 include $(GSTREAMER_NDK_BUILD_PATH)/gstreamer-1.0.mk
