f2f0a2fa7941c994930ee11d7f04a99f07a00976
Sebastian Dröge <sebastian@centricular.com>
2017-04-20 13:16:24 +0100

    splitmuxsink: Don't use an explicit name for requesting audio pads
    ... unless the muxer uses the same audio pad template name as
    splitmuxsink. We can't request a pad called "audio_0" on a muxer that
    wants pads to be "sink_%d".

--- gst/multifile/gstsplitmuxsink.c
+++ gst/multifile/gstsplitmuxsink.c
@@ -1397,6 +1397,7 @@ gst_splitmux_sink_request_new_pad (GstElement * element,
         mux_template =
             gst_element_class_get_pad_template (GST_ELEMENT_GET_CLASS
             (splitmux->muxer), "audio");
+        name = NULL;
       }
     }
     if (mux_template == NULL) {
@@ -1404,6 +1405,7 @@ gst_splitmux_sink_request_new_pad (GstElement * element,
       mux_template =
           gst_element_class_get_pad_template (GST_ELEMENT_GET_CLASS
           (splitmux->muxer), "sink_%d");
+      name = NULL;
     }
   }
 
