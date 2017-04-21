1d0f9a7f8de2d0f338513fd026fc86c81e7c7040
Scott D Phillips <scott.d.phillips@intel.com>
2017-04-20 20:18:30 -0700

    msdk: use a dummy.cpp file to force c++ linking
    Add a dummy.cpp file to force c++ linking as required by libmfx.a.
    I'm certain that the meson build worked for me in the past, but I
    have no idea how it could have, looking at it now.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=781561

---
+++ sys/msdk/dummy.cpp
@@ -0,0 +1,3 @@
+/* This file exists to force C++ linking of the msdk plugin in the meson build,
+ * as required by libmfx.a. It can be removed when we depend on a version of
+ * meson which has a fix for https://github.com/mesonbuild/meson/issues/1653 */
