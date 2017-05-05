dcfb222e059d3f9c490186260eb342d64eb495ae
Tim-Philipp Müller <tim@centricular.com>
2017-05-05 11:49:08 +0100

    meson: add gst-ptp-helper
    https://bugzilla.gnome.org/show_bug.cgi?id=774418

---
+++ libs/gst/helpers/ptp_helper_post_install.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# Meson install script for gst-ptp-helper
+# Fails silently at the moment if setting permissions/capabilities doesn't work
+helpers_install_dir="$1"
+with_ptp_helper_permissions="$2"
+setcap="$3"
+
+ptp_helper="$MESON_INSTALL_DESTDIR_PREFIX/$helpers_install_dir/gst-ptp-helper"
+
+case "$with_ptp_helper_permissions" in
+  setuid-root)
+    echo "$0: permissions before: "
+    ls -l "$ptp_helper"
+    chown root "$ptp_helper"
+    chmod u+s "$ptp_helper"
+    echo "$0: permissions after: "
+    ls -l "$ptp_helper"
+    ;;
+  capabilities)
+    echo "Calling $setcap cap_net_bind_service,cap_net_admin+ep $ptp_helper"
+    $setcap cap_net_bind_service,cap_net_admin+ep "$ptp_helper"
+    ;;
+  *)
+    echo "$0 ERROR: unexpected permissions value '$with_ptp_helper_permissions'";
+    exit 2;
+esac
