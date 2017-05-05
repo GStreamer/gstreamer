c8f8395f41b30cef6ee18709b70662a85867ce49
Tim-Philipp Müller <tim@centricular.com>
2017-05-05 12:23:43 +0100

    meson: fail silently in ptp helper post install script
    .. in case permissions/capabilities could not be set.

--- libs/gst/helpers/ptp_helper_post_install.sh
+++ libs/gst/helpers/ptp_helper_post_install.sh
@@ -11,14 +11,14 @@ case "$with_ptp_helper_permissions" in
   setuid-root)
     echo "$0: permissions before: "
     ls -l "$ptp_helper"
-    chown root "$ptp_helper"
-    chmod u+s "$ptp_helper"
+    chown root "$ptp_helper" || true
+    chmod u+s "$ptp_helper" || true
     echo "$0: permissions after: "
     ls -l "$ptp_helper"
     ;;
   capabilities)
     echo "Calling $setcap cap_net_bind_service,cap_net_admin+ep $ptp_helper"
-    $setcap cap_net_bind_service,cap_net_admin+ep "$ptp_helper"
+    $setcap cap_net_bind_service,cap_net_admin+ep "$ptp_helper" || true
     ;;
   *)
     echo "$0 ERROR: unexpected permissions value '$with_ptp_helper_permissions'";
