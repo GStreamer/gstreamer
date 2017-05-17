479b95205f08ba6cac2a3c0b640861074fbfd6e0
Haihua Hu <jared.hu@nxp.com>
2017-05-17 16:26:38 +0800

    glformat: Add missing GST_GL_RGB565 in some switch statement
    https://bugzilla.gnome.org/show_bug.cgi?id=782736

--- gst-libs/gst/gl/gstglformat.c
+++ gst-libs/gst/gl/gstglformat.c
@@ -58,6 +58,7 @@ _gl_format_n_components (guint format)
     case GST_VIDEO_GL_TEXTURE_TYPE_RGB:
     case GST_VIDEO_GL_TEXTURE_TYPE_RGB16:
     case GST_GL_RGB:
+    case GST_GL_RGB565:
       return 3;
     case GST_VIDEO_GL_TEXTURE_TYPE_LUMINANCE_ALPHA:
     case GST_VIDEO_GL_TEXTURE_TYPE_RG:
@@ -257,6 +258,7 @@ gst_gl_sized_gl_format_from_gl_format_type (GstGLContext * context,
       break;
     case GST_GL_RGBA8:
     case GST_GL_RGB8:
+    case GST_GL_RGB565:
     case GST_GL_RG8:
     case GST_GL_R8:
     case GST_GL_LUMINANCE:
