GStreamer/gstreamer

commit 7a20692364b67809e2a77769fe591557f445c170

plugins: distinguish allocation and negotiation caps
The vaapi video decoders might have different allocation caps from
the negotiation caps, thus the GstVideoMeta shall use the negotiation
caps, not the allocation caps.

This was done before reusing gst_allocator_get_vaapi_video_info(),
storing there the negotiation caps if they differ from the allocation
ones, but this strategy felt short when the allocator had to be reset
in the vaapi buffer pool, since we need both.

This patch adds gst_allocator_set_vaapi_negotiated_video_info() and
gst_allocator_get_vaapi_negotiated_video_info() to store the
negotiated video info in the allocator, and distinguish it from
the allocation video info.

https://bugzilla.gnome.org/show_bug.cgi?id=783599

gst/vaapi/gstvaapipluginbase.c
@@ -567,24 +567,10 @@ ensure_srcpad_allocator (GstVaapiPluginBase * plugin, GstVideoInfo * vinfo,
     GstCaps * caps)
 {
   gboolean different_caps;
-  GstVideoInfo vi;
   GstVaapiImageUsageFlags usage_flag =
       GST_VAAPI_IMAGE_USAGE_FLAG_NATIVE_FORMATS;
 
-  /* the received caps are the "allocation caps" which may be
-   * different from the "negotiation caps". In this case, we should
-   * indicate the allocator to store the negotiation caps since they
-   * are the one should be used for frame mapping with GstVideoMeta */
-  different_caps = GST_IS_VIDEO_DECODER (plugin) && plugin->srcpad_caps &&
-      !gst_caps_is_strictly_equal (plugin->srcpad_caps, caps);
-
-  if (different_caps) {
-    vi = plugin->srcpad_info;
-  } else {
-    vi = *vinfo;
-  }
-
-  if (!reset_allocator (plugin->srcpad_allocator, &vi))
+  if (!reset_allocator (plugin->srcpad_allocator, vinfo))
     return TRUE;
 
   plugin->srcpad_allocator = NULL;
@@ -609,22 +595,33 @@ ensure_srcpad_allocator (GstVaapiPluginBase * plugin, GstVideoInfo * vinfo,
   if (!plugin->srcpad_allocator)
     goto error_create_allocator;
 
+  /* the received caps are the "allocation caps" which may be
+   * different from the "negotiation caps". In this case, we should
+   * indicate the allocator to store the negotiation caps since they
+   * are the one should be used for frame mapping with GstVideoMeta */
+  different_caps = GST_IS_VIDEO_DECODER (plugin) && plugin->srcpad_caps &&
+      !gst_caps_is_strictly_equal (plugin->srcpad_caps, caps);
+
   if (different_caps) {
-    guint i, flags = 0;
-    const GstVideoInfo *alloc_vi =
-        gst_allocator_get_vaapi_video_info (plugin->srcpad_allocator, &flags);
-    /* update the planes and the size with the allocator image info,
-     * but not the resolution */
-    if (alloc_vi) {
-      for (i = 0; i < GST_VIDEO_INFO_N_PLANES (alloc_vi); i++) {
-        GST_VIDEO_INFO_PLANE_OFFSET (&vi, i) =
-            GST_VIDEO_INFO_PLANE_OFFSET (alloc_vi, i);
-        GST_VIDEO_INFO_PLANE_STRIDE (&vi, i) =
-            GST_VIDEO_INFO_PLANE_STRIDE (alloc_vi, i);
-      }
-      GST_VIDEO_INFO_SIZE (&vi) = GST_VIDEO_INFO_SIZE (alloc_vi);
-      gst_allocator_set_vaapi_video_info (plugin->srcpad_allocator, &vi, flags);
+    guint i;
+    GstVideoInfo vi = plugin->srcpad_info;
+    const GstVideoInfo *image_info =
+        gst_allocator_get_vaapi_video_info (plugin->srcpad_allocator, NULL);
+
+    g_assert (image_info);      /* both allocators should set its video
+                                 * info */
+
+    /* update the planes and the size with the allocator image/surface
+     * info, but not the resolution */
+    for (i = 0; i < GST_VIDEO_INFO_N_PLANES (image_info); i++) {
+      GST_VIDEO_INFO_PLANE_OFFSET (&vi, i) =
+          GST_VIDEO_INFO_PLANE_OFFSET (image_info, i);
+      GST_VIDEO_INFO_PLANE_STRIDE (&vi, i) =
+          GST_VIDEO_INFO_PLANE_STRIDE (image_info, i);
     }
+    GST_VIDEO_INFO_SIZE (&vi) = GST_VIDEO_INFO_SIZE (image_info);
+    gst_allocator_set_vaapi_negotiated_video_info (plugin->srcpad_allocator,
+        &vi);
   }
 
   return TRUE;

gst/vaapi/gstvaapivideobufferpool.c
@@ -137,6 +137,7 @@ gst_vaapi_video_buffer_pool_set_config (GstBufferPool * pool,
   GstCaps *caps;
   GstVideoInfo new_allocation_vinfo;
   const GstVideoInfo *allocator_vinfo;
+  const GstVideoInfo *negotiated_vinfo;
   GstVideoAlignment align;
   GstAllocator *allocator;
   gboolean ret, updated = FALSE;
@@ -176,9 +177,16 @@ gst_vaapi_video_buffer_pool_set_config (GstBufferPool * pool,
       gst_allocator_set_vaapi_video_info (allocator, &new_allocation_vinfo,
           surface_alloc_flags);
     } else if (!priv->use_dmabuf_memory && (vinfo_changed || !allocator)) {
+       negotiated_vinfo =
+          gst_allocator_get_vaapi_negotiated_video_info (allocator);
+
       /* let's destroy the other allocator and create a new one */
       allocator = gst_vaapi_video_allocator_new (priv->display,
           &new_allocation_vinfo, surface_alloc_flags, 0);
+      if (negotiated_vinfo) {
+        gst_allocator_set_vaapi_negotiated_video_info (allocator,
+            negotiated_vinfo);
+      }
       gst_buffer_pool_config_set_allocator (config, allocator, NULL);
       gst_object_unref (allocator);
     }
@@ -197,11 +205,10 @@ gst_vaapi_video_buffer_pool_set_config (GstBufferPool * pool,
         gst_object_unref (priv->allocator);
       if ((priv->allocator = allocator))
         gst_object_ref (allocator);
-      allocator_vinfo =
-          gst_allocator_get_vaapi_video_info (priv->allocator, NULL);
-      if (!allocator_vinfo)
-        goto error_create_allocator_info;
-      priv->vmeta_vinfo = *allocator_vinfo;
+      negotiated_vinfo =
+          gst_allocator_get_vaapi_negotiated_video_info (priv->allocator);
+      priv->vmeta_vinfo = (negotiated_vinfo) ?
+          *negotiated_vinfo : priv->allocation_vinfo;
     }
     if (GST_VIDEO_INFO_SIZE (&priv->vmeta_vinfo) != size) {
       gst_buffer_pool_config_set_params (config, caps,
@@ -271,12 +278,6 @@ error_invalid_allocator:
 error_no_vaapi_video_meta_option:
   {
     GST_ERROR_OBJECT (pool, "no GstVaapiVideoMeta option in config");
-    return FALSE;
-  }
-error_create_allocator_info:
-  {
-    GST_ERROR_OBJECT (pool,
-        "failed to create GstVaapiVideoAllocator `video-info'");
     return FALSE;
   }
 error_no_allocator:

gst/vaapi/gstvaapivideomemory.c
@@ -1185,6 +1185,19 @@ surface_alloc_flags_quark_get (void)
   return g_quark;
 }
 
+#define NEGOTIATED_VINFO_QUARK negotiated_vinfo_quark_get ()
+static GQuark
+negotiated_vinfo_quark_get (void)
+{
+  static gsize g_quark;
+
+  if (g_once_init_enter (&g_quark)) {
+    gsize quark = (gsize) g_quark_from_static_string ("negotiated-vinfo");
+    g_once_init_leave (&g_quark, quark);
+  }
+  return g_quark;
+}
+
 /**
  * gst_allocator_get_vaapi_video_info:
  * @allocator: a #GstAllocator
@@ -1252,6 +1265,44 @@ gst_allocator_set_vaapi_video_info (GstAllocator * allocator,
       (GDestroyNotify) gst_structure_free);
 
   return TRUE;
+}
+
+/**
+ * gst_allocator_set_vaapi_negotiated_video_info:
+ * @allocator: a #GstAllocator
+ * @negotiated_vinfo: the negotiated #GstVideoInfo to store
+ *
+ * Stores as GObject's qdata the @negotiated_vinfo in the allocator
+ * instance.
+ *
+ * The @negotiated_vinfo is different of the @alloc_info from
+ * gst_allocator_set_vaapi_video_info(), and might not be set.
+ **/
+void
+gst_allocator_set_vaapi_negotiated_video_info (GstAllocator * allocator,
+    const GstVideoInfo * negotiated_vinfo)
+{
+  g_return_if_fail (allocator && GST_IS_ALLOCATOR (allocator));
+  g_return_if_fail (negotiated_vinfo);
+
+  g_object_set_qdata_full (G_OBJECT (allocator), NEGOTIATED_VINFO_QUARK,
+      gst_video_info_copy (negotiated_vinfo),
+      (GDestroyNotify) gst_video_info_free);
+}
+
+/**
+ * gst_allocator_get_vaapi_negotiated_video_info:
+ * @allocator: a #GstAllocator
+ *
+ * Returns: the stored negotiation #GstVideoInfo, if it was stored
+ * previously. Otherwise, %NULL
+ **/
+GstVideoInfo *
+gst_allocator_get_vaapi_negotiated_video_info (GstAllocator * allocator)
+{
+  g_return_val_if_fail (GST_IS_ALLOCATOR (allocator), NULL);
+
+  return g_object_get_qdata (G_OBJECT (allocator), NEGOTIATED_VINFO_QUARK);
 }
 
 /**

gst/vaapi/gstvaapivideomemory.h
@@ -279,6 +279,15 @@ gst_allocator_set_vaapi_video_info (GstAllocator * allocator,
     const GstVideoInfo * alloc_info, guint surface_alloc_flags);
 
 G_GNUC_INTERNAL
+void
+gst_allocator_set_vaapi_negotiated_video_info (GstAllocator * allocator,
+    const GstVideoInfo * negotiated_vinfo);
+
+G_GNUC_INTERNAL
+GstVideoInfo *
+gst_allocator_get_vaapi_negotiated_video_info (GstAllocator * allocator);
+
+G_GNUC_INTERNAL
 gboolean
 gst_vaapi_is_dmabuf_allocator (GstAllocator * allocator);
 

commit 05a41009f21678eb4c416fa450668bd50894df02

plugins: update buffer size with the one reported by allocator
There is a regression in 7a206923, since the buffer pool ditches all
the buffers generated by them because the pool config size is
different of the buffer's size.

Test pipeline:

  gst-launch-1.0 filesrc location=big_buck_bunny_1080p_h264.mov \
      ! qtdemux ! vaapih264dec ! vaapipostproc ! xvimagesink    \
      --gst-debug=GST_PERFORMANCE:5

The allocator may update the buffer size according to the VA surface
properties. In order to do this, the video info is modified when the
allocator is created, which reports through the allocation info the
updated size, and set it to the pool config.

gst/vaapi/gstvaapipluginbase.c
@@ -498,16 +498,15 @@ ensure_sinkpad_allocator (GstVaapiPluginBase * plugin, GstCaps * caps,
     guint * size)
 {
   GstVideoInfo vinfo;
+  const GstVideoInfo *image_info;
   GstVaapiImageUsageFlags usage_flag =
       GST_VAAPI_IMAGE_USAGE_FLAG_NATIVE_FORMATS;
 
   if (!gst_video_info_from_caps (&vinfo, caps))
     goto error_invalid_caps;
-  gst_video_info_force_nv12_if_encoded (&vinfo);
-  *size = GST_VIDEO_INFO_SIZE (&vinfo);
 
   if (!reset_allocator (plugin->sinkpad_allocator, &vinfo))
-    return TRUE;
+    goto bail;
 
   if (has_dmabuf_capable_peer (plugin, plugin->sinkpad)) {
     plugin->sinkpad_allocator =
@@ -527,6 +526,14 @@ ensure_sinkpad_allocator (GstVaapiPluginBase * plugin, GstCaps * caps,
 bail:
   if (!plugin->sinkpad_allocator)
     goto error_create_allocator;
+
+  image_info =
+      gst_allocator_get_vaapi_video_info (plugin->sinkpad_allocator, NULL);
+  g_assert (image_info);        /* allocator ought set its image info */
+
+  /* update the size with the one generated by the allocator */
+  *size = GST_VIDEO_INFO_SIZE (image_info);
+
   return TRUE;
 
   /* ERRORS */
@@ -567,11 +574,12 @@ ensure_srcpad_allocator (GstVaapiPluginBase * plugin, GstVideoInfo * vinfo,
     GstCaps * caps)
 {
   gboolean different_caps;
+  const GstVideoInfo *image_info;
   GstVaapiImageUsageFlags usage_flag =
       GST_VAAPI_IMAGE_USAGE_FLAG_NATIVE_FORMATS;
 
   if (!reset_allocator (plugin->srcpad_allocator, vinfo))
-    return TRUE;
+    goto valid_allocator;
 
   plugin->srcpad_allocator = NULL;
   if (caps && gst_caps_is_video_raw (caps)) {
@@ -595,6 +603,15 @@ ensure_srcpad_allocator (GstVaapiPluginBase * plugin, GstVideoInfo * vinfo,
   if (!plugin->srcpad_allocator)
     goto error_create_allocator;
 
+valid_allocator:
+  image_info =
+      gst_allocator_get_vaapi_video_info (plugin->srcpad_allocator, NULL);
+  g_assert (image_info);        /* both allocators ought set its image
+                                 * info */
+
+  /* update the size with the one generated by the allocator */
+  GST_VIDEO_INFO_SIZE (vinfo) = GST_VIDEO_INFO_SIZE (image_info);
+
   /* the received caps are the "allocation caps" which may be
    * different from the "negotiation caps". In this case, we should
    * indicate the allocator to store the negotiation caps since they
@@ -605,11 +622,6 @@ ensure_srcpad_allocator (GstVaapiPluginBase * plugin, GstVideoInfo * vinfo,
   if (different_caps) {
     guint i;
     GstVideoInfo vi = plugin->srcpad_info;
-    const GstVideoInfo *image_info =
-        gst_allocator_get_vaapi_video_info (plugin->srcpad_allocator, NULL);
-
-    g_assert (image_info);      /* both allocators should set its video
-                                 * info */
 
     /* update the planes and the size with the allocator image/surface
      * info, but not the resolution */
@@ -934,6 +946,8 @@ gst_vaapi_plugin_base_decide_allocation (GstVaapiPluginBase * plugin,
   if (!pool) {
     if (!ensure_srcpad_allocator (plugin, &vi, caps))
       goto error;
+    size = GST_VIDEO_INFO_SIZE (&vi);   /* size might be updated by
+                                         * allocator */
     pool = gst_vaapi_plugin_base_create_pool (plugin, caps, size, min, max,
         pool_options, plugin->srcpad_allocator);
     if (!pool)

gst/vaapi/gstvaapivideobufferpool.c
@@ -231,9 +231,11 @@ gst_vaapi_video_buffer_pool_set_config (GstBufferPool * pool,
     priv->vmeta_vinfo = (negotiated_vinfo) ?
         *negotiated_vinfo : new_allocation_vinfo;
 
-    if (GST_VIDEO_INFO_SIZE (&priv->vmeta_vinfo) != size) {
+    /* last resource to set the correct buffer size */
+    allocator_vinfo = gst_allocator_get_vaapi_video_info (allocator, NULL);
+    if (GST_VIDEO_INFO_SIZE (allocator_vinfo) != size) {
       gst_buffer_pool_config_set_params (config, caps,
-          GST_VIDEO_INFO_SIZE (&priv->vmeta_vinfo), min_buffers, max_buffers);
+          GST_VIDEO_INFO_SIZE (allocator_vinfo), min_buffers, max_buffers);
     }
   }
   if (!priv->allocator)