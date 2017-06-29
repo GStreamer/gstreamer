752544841d32eb87e09e8d82b175f4475798a558
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2017-06-29 16:48:07 +0200

    omxh264dec: remove 'parsed=true' from sink pad
    The decoder only requires to receive one frame per buffer which is
    already enforced with 'alignment=au'. There is no need to require to
    have a parser upstream.
    
    Allow to run "encode ! decode" pipeline without having a parser.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=784344

--- omx/gstomxh264dec.c
+++ omx/gstomxh264dec.c
@@ -60,7 +60,6 @@ gst_omx_h264_dec_class_init (GstOMXH264DecClass * klass)
   videodec_class->set_format = GST_DEBUG_FUNCPTR (gst_omx_h264_dec_set_format);
 
   videodec_class->cdata.default_sink_template_caps = "video/x-h264, "
-      "parsed=(boolean) true, "
       "alignment=(string) au, "
       "stream-format=(string) byte-stream, "
       "width=(int) [1,MAX], " "height=(int) [1,MAX]";
