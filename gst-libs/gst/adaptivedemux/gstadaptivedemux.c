GStreamer/gstreamer

commit f4190a49c04f1d5d174cebba0bc9a03a7ec721c2

adaptivedemux: Check live seeking range more often
The live seeking range was only checked when doing actual seeks. This was
assuming that the rate would always be 1.0 (i.e. the playback would
advance in realtime, and therefore fragments would always be available
since the seeking window moves at the same rate).

With non-1.0 rates, this no longer becomes valid, and therefore we need
to check whether we are still within the live seeking range when advancing.

https://bugzilla.gnome.org/show_bug.cgi?id=783075

gst-libs/gst/adaptivedemux/gstadaptivedemux.c
@@ -1448,6 +1448,25 @@ gst_adaptive_demux_get_live_seek_range (GstAdaptiveDemux * demux,
 
 /* must be called with manifest_lock taken */
 static gboolean
+gst_adaptive_demux_stream_in_live_seek_range (GstAdaptiveDemux * demux,
+    GstAdaptiveDemuxStream * stream)
+{
+  gint64 range_start, range_stop;
+  if (gst_adaptive_demux_get_live_seek_range (demux, &range_start, &range_stop)) {
+    GST_LOG_OBJECT (stream->pad,
+        "stream position %" GST_STIME_FORMAT "  live seek range %"
+        GST_STIME_FORMAT " - %" GST_STIME_FORMAT,
+        GST_STIME_ARGS (stream->segment.position), GST_STIME_ARGS (range_start),
+        GST_STIME_ARGS (range_stop));
+    return (stream->segment.position >= range_start
+        && stream->segment.position <= range_stop);
+  }
+
+  return FALSE;
+}
+
+/* must be called with manifest_lock taken */
+static gboolean
 gst_adaptive_demux_can_seek (GstAdaptiveDemux * demux)
 {
   GstAdaptiveDemuxClass *klass;
@@ -3703,7 +3722,8 @@ gst_adaptive_demux_stream_download_loop (GstAdaptiveDemuxStream * stream)
     case GST_FLOW_EOS:
       GST_DEBUG_OBJECT (stream->pad, "EOS, checking to stop download loop");
       /* we push the EOS after releasing the object lock */
-      if (gst_adaptive_demux_is_live (demux)) {
+      if (gst_adaptive_demux_is_live (demux)
+          && gst_adaptive_demux_stream_in_live_seek_range (demux, stream)) {
         GstAdaptiveDemuxClass *demux_class =
             GST_ADAPTIVE_DEMUX_GET_CLASS (demux);
 
@@ -4120,7 +4140,15 @@ gst_adaptive_demux_stream_advance_fragment_unlocked (GstAdaptiveDemux * demux,
   }
   GST_ADAPTIVE_DEMUX_SEGMENT_UNLOCK (demux);
 
-  if (gst_adaptive_demux_is_live (demux)
+  /* When advancing with a non 1.0 rate on live streams, we need to check
+   * the live seeking range again to make sure we can still advance to
+   * that position */
+  if (demux->segment.rate != 1.0 && gst_adaptive_demux_is_live (demux)) {
+    if (!gst_adaptive_demux_stream_in_live_seek_range (demux, stream))
+      ret = GST_FLOW_EOS;
+    else
+      ret = klass->stream_advance_fragment (stream);
+  } else if (gst_adaptive_demux_is_live (demux)
       || gst_adaptive_demux_stream_has_next_fragment (demux, stream)) {
     ret = klass->stream_advance_fragment (stream);
   } else {

commit 2f01c7e56e57547379cd3e5e733921dd6128a50f

adaptivedemux: Workaround for live seek ranges when advancing
This is a workaround for a regression introduced by
f4190a49c04f1d5d174cebba0bc9a03a7ec721c2
 ( adaptivedemux: Check live seeking range more often )

The goal of the previous commit was to be able to cope with non-1.0
rates on live streams which have a "seeking window" (i.e. the server
keeps around quite a bit of the live stream so you can seek back into
it).

Without that commit, two different kind of issues would happen:
* When doing reverse playback, you would never check whether you
  are outside of the seekable region. And would then continuously
  try to download fragments that are no longer present.
* When doing fast forward, you would end up requesting fragments
  which are not present yet.

In order to determine whether one was *really* outside of the seekable
window, we check whether the current stream position is still
within the seekable region.

The *problem* though with that commit is that it assumes that subclasses
will return continuously updated seeking ranges (i.e. dependent on the
current time), which is *NOT* the case.

For example:
* dashdemux does use the current UTC to determine the seekable region
* hlsdemux uses the values from the last updated manifest

Therefore if one downloads fragments faster than realtime, for HLS
we would end up at the end of the last manifest seekable range, and
the previous commit would consider the stream as being ended... which
is not the case.

In the long run, we need to figure out a way to cope with non-1.0
rates on live streams for all types of stream (including HLS).

https://bugzilla.gnome.org/show_bug.cgi?id=783075

gst-libs/gst/adaptivedemux/gstadaptivedemux.c
@@ -3606,9 +3606,12 @@ gst_adaptive_demux_stream_download_loop (GstAdaptiveDemuxStream * stream)
       break;                    /* all is good, let's go */
     case GST_FLOW_EOS:
       GST_DEBUG_OBJECT (stream->pad, "EOS, checking to stop download loop");
+
       /* we push the EOS after releasing the object lock */
       if (gst_adaptive_demux_is_live (demux)
-          && gst_adaptive_demux_stream_in_live_seek_range (demux, stream)) {
+          && (demux->segment.rate == 1.0
+              || gst_adaptive_demux_stream_in_live_seek_range (demux,
+                  stream))) {
         GstAdaptiveDemuxClass *demux_class =
             GST_ADAPTIVE_DEMUX_GET_CLASS (demux);
 