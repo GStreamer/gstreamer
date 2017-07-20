3e0db49f5b83e2b4ad4386c11c64a5d391c79256
Nicolas Dufresne <nicolas.dufresne@collabora.com>
2017-07-20 10:29:18 -0400

    d3dsinkwrapper: Make the DLL name match the plugin name
    Otherwise, since 1.12, the plugin gets blacklisted.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=785168

--- sys/d3dvideosink/d3dvideosink.c
+++ sys/d3dvideosink/d3dvideosink.c
@@ -619,6 +619,6 @@ plugin_init (GstPlugin * plugin)
 
 GST_PLUGIN_DEFINE (GST_VERSION_MAJOR,
     GST_VERSION_MINOR,
-    d3dsinkwrapper,
-    "Direct3D sink wrapper plugin",
+    d3d,
+    "Direct3D plugin",
     plugin_init, VERSION, "LGPL", GST_PACKAGE_NAME, GST_PACKAGE_ORIGIN)
