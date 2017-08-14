301e8d558eea553eb1c95c14252b0e45d7b470d2
Arun Raghavan <arun@arunraghavan.net>
2017-08-14 18:18:07 +0530

    rtpsbcpay: Fix some tabs that crept in somehow

--- gst/rtp/gstrtpsbcpay.h
+++ gst/rtp/gstrtpsbcpay.h
@@ -27,35 +27,35 @@
 G_BEGIN_DECLS
 
 #define GST_TYPE_RTP_SBC_PAY \
-	(gst_rtp_sbc_pay_get_type())
+  (gst_rtp_sbc_pay_get_type())
 #define GST_RTP_SBC_PAY(obj) \
-	(G_TYPE_CHECK_INSTANCE_CAST((obj),GST_TYPE_RTP_SBC_PAY,\
-		GstRtpSBCPay))
+  (G_TYPE_CHECK_INSTANCE_CAST((obj),GST_TYPE_RTP_SBC_PAY,\
+                              GstRtpSBCPay))
 #define GST_RTP_SBC_PAY_CLASS(klass) \
-	(G_TYPE_CHECK_CLASS_CAST((klass),GST_TYPE_RTP_SBC_PAY,\
-		GstRtpSBCPayClass))
+  (G_TYPE_CHECK_CLASS_CAST((klass),GST_TYPE_RTP_SBC_PAY,\
+                           GstRtpSBCPayClass))
 #define GST_IS_RTP_SBC_PAY(obj) \
-	(G_TYPE_CHECK_INSTANCE_TYPE((obj),GST_TYPE_RTP_SBC_PAY))
+  (G_TYPE_CHECK_INSTANCE_TYPE((obj),GST_TYPE_RTP_SBC_PAY))
 #define GST_IS_RTP_SBC_PAY_CLASS(obj) \
-	(G_TYPE_CHECK_CLASS_TYPE((klass),GST_TYPE_RTP_SBC_PAY))
+  (G_TYPE_CHECK_CLASS_TYPE((klass),GST_TYPE_RTP_SBC_PAY))
 
 typedef struct _GstRtpSBCPay GstRtpSBCPay;
 typedef struct _GstRtpSBCPayClass GstRtpSBCPayClass;
 
 struct _GstRtpSBCPay {
-	GstRTPBasePayload base;
+  GstRTPBasePayload base;
 
-	GstAdapter *adapter;
-	GstClockTime last_timestamp;
+  GstAdapter *adapter;
+  GstClockTime last_timestamp;
 
-	guint frame_length;
-        GstClockTime frame_duration;
+  guint frame_length;
+  GstClockTime frame_duration;
 
-	guint min_frames;
+  guint min_frames;
 };
 
 struct _GstRtpSBCPayClass {
-	GstRTPBasePayloadClass parent_class;
+  GstRTPBasePayloadClass parent_class;
 };
 
 GType gst_rtp_sbc_pay_get_type(void);
