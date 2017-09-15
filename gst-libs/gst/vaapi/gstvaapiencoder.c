GStreamer/gstreamer

commit 148f867c120fbde2ed5a92a8f89ef30a805eb61c

vaapiencode/libs: encoder: fix leaks of properties
https://bugzilla.gnome.org/show_bug.cgi?id=786321

gst-libs/gst/vaapi/gstvaapiencoder.c
@@ -1263,6 +1263,8 @@ gst_vaapi_encoder_init_properties (GstVaapiEncoder * encoder)
             NULL) != GST_VAAPI_ENCODER_STATUS_SUCCESS)
       return FALSE;
   }
+
+  g_ptr_array_unref (props);
   return TRUE;
 }
 

gst/vaapi/gstvaapiencode.c
@@ -939,6 +939,7 @@ gst_vaapiencode_init_properties (GstVaapiEncode * encode)
       return FALSE;
     g_ptr_array_add (encode->prop_values, prop_value);
   }
+  g_ptr_array_unref (props);
   return TRUE;
 }
 

commit 29cf49d56a39ab5021b1dd4e2651b334febbf1a5

libs: encoder: don't unref properties
This patch fixes a regression introduced in commit 148f867c, since the
props variable is set to object's member variable
encoder->properties. And it is set in the instance initialization,
thus it will not be leaked.

https://bugzilla.gnome.org/show_bug.cgi?id=787733

gst-libs/gst/vaapi/gstvaapiencoder.c
@@ -1264,7 +1264,6 @@ gst_vaapi_encoder_init_properties (GstVaapiEncoder * encoder)
       return FALSE;
   }
 
-  g_ptr_array_unref (props);
   return TRUE;
 }
 