0bd077718ce700b98979481c4bd642207def94a8
Thibault Saunier <thibault.saunier@osg.samsung.com>
2017-10-11 23:47:35 +0200

    Remove some remaining reference to the glue

--- generate_code.py
+++ generate_code.py
@@ -14,24 +14,19 @@ if __name__ == "__main__":
     parser.add_argument("--gapi-fixup")
     parser.add_argument("--metadata")
     parser.add_argument("--gapi-codegen")
-    parser.add_argument("--glue-file", default="")
-    parser.add_argument("--glue-includes", default="")
+    parser.add_argument("--abi-includes", default="")
     parser.add_argument("--abi-cs-usings", default="")
-    parser.add_argument("--glue-libname", default="")
     parser.add_argument("--assembly-name")
     parser.add_argument("--extra-includes", action='append', default=[])
     parser.add_argument("--out")
     parser.add_argument("--files")
     parser.add_argument("--symbols")
     parser.add_argument("--schema")
-    parser.add_argument("--fakeglue", action='store_true')
+    parser.add_argument("--fake", action='store_true')
 
     opts = parser.parse_args()
-    if opts.fakeglue:
+    if opts.fake:
         exit(0)
-
-    if not opts.glue_libname:
-        opts.glue_libname = opts.assembly_name + 'sharpglue-3'
 
     api_xml = os.path.join(opts.out, os.path.basename(
         opts.api_raw).replace('.raw', '.xml'))
@@ -53,10 +48,8 @@ if __name__ == "__main__":
     cmd = [
         opts.gapi_codegen, '--generate', api_xml,
         '--outdir=' + opts.out,
-        '--glue-filename=' + opts.glue_file,
-        '--gluelib-name=' + opts.glue_libname,
-        '--glue-includes=' + opts.glue_includes,
         '--assembly-name=' + opts.assembly_name,
+        '--glue-includes=' + opts.abi_includes,
         '--abi-c-filename=' + os.path.join(opts.out, opts.assembly_name + "-abi.c"),
         '--abi-cs-filename=' + os.path.join(opts.out, opts.assembly_name + "-abi.cs"),
     ]
