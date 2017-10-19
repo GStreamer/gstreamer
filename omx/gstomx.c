GStreamer/gstreamer

commit b3173144b7c1b12c9e1b7571f78659be45d813f6

omx: also reset nTimeStamp when re-using buffers
Some OMX implementations may check if the timestamp of the output buffers
they receive is actually not set.

https://bugzilla.gnome.org/show_bug.cgi?id=788711

omx/gstomx.c
@@ -231,6 +231,7 @@ gst_omx_buffer_reset (GstOMXBuffer * buf)
   buf->omx_buf->nFlags = 0;
   buf->omx_buf->nOffset = 0;
   buf->omx_buf->nFilledLen = 0;
+  buf->omx_buf->nTimeStamp = 0;
 }
 
 /* NOTE: Call with comp->lock, comp->messages_lock will be used */

commit 6d44c0add52dea232642b33578eb021ce9070075

omx: fix build on rpi
The nTimeStamp field is a OMX_TICKS struct on the rpi so use the proper
macro to set it.
Fix build on the build which has been broken by
b3173144b7c1b12c9e1b7571f78659be45d813f6

https://bugzilla.gnome.org/show_bug.cgi?id=789052

omx/gstomx.c
@@ -231,7 +231,7 @@ gst_omx_buffer_reset (GstOMXBuffer * buf)
   buf->omx_buf->nFlags = 0;
   buf->omx_buf->nOffset = 0;
   buf->omx_buf->nFilledLen = 0;
-  buf->omx_buf->nTimeStamp = 0;
+  GST_OMX_SET_TICKS (buf->omx_buf->nTimeStamp, G_GUINT64_CONSTANT (0));
 }
 
 /* NOTE: Call with comp->lock, comp->messages_lock will be used */