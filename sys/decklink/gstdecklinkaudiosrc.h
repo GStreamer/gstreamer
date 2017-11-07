8dceeb5ccd6e46a61e3bbe79efa45275504fa500
Vivia Nikolaidou <vivia@ahiru.eu>
2017-11-07 21:13:57 +0200

    WIP: Revert "Revert "decklink: Detect gaps on incoming stream times, issue warnings""
    This reverts commit 07819afda461f5e4d77c9be9f484a7aae3c5ec8c.

--- sys/decklink/gstdecklinkaudiosrc.h
+++ sys/decklink/gstdecklinkaudiosrc.h
@@ -71,6 +71,11 @@ struct _GstDecklinkAudioSrc
   /* counter to keep track of timestamps */
   guint64 next_offset;
 
+  /* detect gaps in stream time */
+  GstClockTime expected_stream_time;
+  guint64 processed;
+  guint64 dropped;
+
   /* Last time we noticed a discont */
   GstClockTime discont_time;
 
