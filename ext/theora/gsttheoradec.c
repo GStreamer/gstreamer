c7917df7e22f1fcf543cea1acb4066d13f4e1d41
Edward Hervey <bilboed@bilboed.com>
2017-11-11 07:45:42 +0100

    theoradec: Handle negotiation failure
    Avoids a trove of random issues afterwards (due to decoder not
    *actually* being initialized/negotiated).

--- ext/theora/gsttheoradec.c
+++ ext/theora/gsttheoradec.c
@@ -516,7 +516,8 @@ theora_handle_type_packet (GstTheoraDec * dec)
 
   dec->uncropped_info = state->info;
 
-  gst_video_decoder_negotiate (GST_VIDEO_DECODER (dec));
+  if (!gst_video_decoder_negotiate (GST_VIDEO_DECODER (dec)))
+    goto not_negotiated;
 
   dec->have_header = TRUE;
 
@@ -527,6 +528,12 @@ unsupported_format:
   {
     GST_ERROR_OBJECT (dec, "Invalid pixel format %d", dec->info.pixel_fmt);
     return GST_FLOW_ERROR;
+  }
+
+not_negotiated:
+  {
+    GST_ERROR_OBJECT (dec, "Failed to negotiate");
+    return GST_FLOW_NOT_NEGOTIATED;
   }
 }
 
