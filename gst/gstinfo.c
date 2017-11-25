2d0a1669b3ec8b03f97b19fbf38b4102386ff904
Edward Hervey <edward@centricular.com>
2017-11-25 13:07:12 +0100

    gstinfo: Use free instead of g_free
    Because

--- gst/gstinfo.c
+++ gst/gstinfo.c
@@ -2751,7 +2751,7 @@ generate_backtrace_trace (void)
   for (j = 0; j < nptrs; j++)
     g_string_append_printf (trace, "%s\n", strings[j]);
 
-  g_free (strings);
+  free (strings);
 
   return g_string_free (trace, FALSE);
 }
