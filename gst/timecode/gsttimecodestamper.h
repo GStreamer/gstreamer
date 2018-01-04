f042e57d4e68b3b9b19489c74d041dfd6ec7fb2a
Vivia Nikolaidou <vivia@ahiru.eu>
2018-01-04 15:41:49 +0200

    Revert "WIP: Revert "Revert "timecodestamper: LTC from audio"""
    This reverts commit e0be05dc7059cc97dceb70a48ca9cad4ee2edce6.

--- gst/timecode/gsttimecodestamper.h
+++ gst/timecode/gsttimecodestamper.h
@@ -25,11 +25,6 @@
 
 #include <gst/gst.h>
 #include <gst/video/video.h>
-#include <gst/audio/audio.h>
-
-#if HAVE_LTC
-#include <ltc.h>
-#endif
 
 #define GST_TYPE_TIME_CODE_STAMPER            (gst_timecodestamper_get_type())
 #define GST_TIME_CODE_STAMPER(obj)            (G_TYPE_CHECK_INSTANCE_CAST((obj),GST_TYPE_TIME_CODE_STAMPER,GstTimeCodeStamper))
@@ -38,19 +33,8 @@
 #define GST_IS_TIME_CODE_STAMPER(obj)         (G_TYPE_CHECK_INSTANCE_TYPE((obj),GST_TYPE_TIME_CODE_STAMPER))
 #define GST_IS_TIME_CODE_STAMPER_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE((klass), GST_TYPE_TIME_CODE_STAMPER))
 
-#define GST_TYPE_TIME_CODE_STAMPER_SOURCE (gst_timecodestamper_source_get_type())
-
 typedef struct _GstTimeCodeStamper GstTimeCodeStamper;
 typedef struct _GstTimeCodeStamperClass GstTimeCodeStamperClass;
-
-typedef enum GstTimeCodeStamperSource
-{
-  GST_TIME_CODE_STAMPER_NOREPLACE,
-  GST_TIME_CODE_STAMPER_INTERN,
-  GST_TIME_CODE_STAMPER_EXISTING,
-  GST_TIME_CODE_STAMPER_LTC,
-  GST_TIME_CODE_STAMPER_NRZERO
-} GstTimeCodeStamperSource;
 
 /**
  * GstTimeCodeStamper:
@@ -61,34 +45,14 @@ struct _GstTimeCodeStamper
 {
   GstBaseTransform videofilter;
 
-  GstPad *ltcpad;
-
   /* < private > */
-  GstTimeCodeStamperSource tc_source;
+  gboolean override_existing;
   gboolean drop_frame;
   GstVideoTimeCode *current_tc;
   GstVideoTimeCode *first_tc;
-  GstVideoTimeCode *ltc_current_tc;
-  GstVideoTimeCode *ltc_intern_tc;
-  GstClockTime ltc_max_offset;
-  gint ltc_add;
-  GstSegment ltc_segment;
   GstVideoInfo vinfo;
   gboolean post_messages;
   gboolean first_tc_now;
-  gboolean is_flushing;
-  gboolean no_wait;
-  GMutex mutex;
-
-#if HAVE_LTC
-  LTCDecoder *ltc_dec;
-  ltc_off_t ltc_total;
-  GstAudioInfo audio_info;
-  GstClockTime ltc_first_runtime;
-  GstClockTime ltc_audio_endtime;
-  GCond ltc_cond_video;
-  GCond ltc_cond_audio;
-#endif
 };
 
 struct _GstTimeCodeStamperClass
@@ -97,8 +61,6 @@ struct _GstTimeCodeStamperClass
 };
 
 GType gst_timecodestamper_get_type (void);
-
-GType gst_timecodestamper_source_get_type (void);
 
 G_END_DECLS
 #endif /* __GST_TIME_CODE_STAMPER_H__ */
