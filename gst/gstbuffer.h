ebcdfd2e9c3aafdb0f547494b124e92c2796100a
Mathieu Duponchelle <mathieu@centricular.com>
2018-01-30 16:41:39 +0100

    gstbuffer: add GST_BUFFER_FLAG_NON_DROPPABLE
    This can be used to identify buffers for which a higher percentage
    of redundancy should be allocated when performing forward error
    correction, or to prevent still video frames from being dropped by
    elements due to QoS.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=793008

--- gst/gstbuffer.h
+++ gst/gstbuffer.h
@@ -181,55 +181,61 @@ typedef struct _GstBufferPool GstBufferPool;
 
 /**
  * GstBufferFlags:
- * @GST_BUFFER_FLAG_LIVE:        the buffer is live data and should be discarded in
- *                               the PAUSED state.
- * @GST_BUFFER_FLAG_DECODE_ONLY: the buffer contains data that should be dropped
- *                               because it will be clipped against the segment
- *                               boundaries or because it does not contain data
- *                               that should be shown to the user.
- * @GST_BUFFER_FLAG_DISCONT:     the buffer marks a data discontinuity in the stream.
- *                               This typically occurs after a seek or a dropped buffer
- *                               from a live or network source.
- * @GST_BUFFER_FLAG_RESYNC:      the buffer timestamps might have a discontinuity
- *                               and this buffer is a good point to resynchronize.
- * @GST_BUFFER_FLAG_CORRUPTED:   the buffer data is corrupted.
- * @GST_BUFFER_FLAG_MARKER:      the buffer contains a media specific marker. for
- *                               video this is typically the end of a frame boundary, for audio
- *                               this is usually the start of a talkspurt.
- * @GST_BUFFER_FLAG_HEADER:      the buffer contains header information that is
- *                               needed to decode the following data.
- * @GST_BUFFER_FLAG_GAP:         the buffer has been created to fill a gap in the
- *                               stream and contains media neutral data (elements can
- *                               switch to optimized code path that ignores the buffer
- *                               content).
- * @GST_BUFFER_FLAG_DROPPABLE:   the buffer can be dropped without breaking the
- *                               stream, for example to reduce bandwidth.
- * @GST_BUFFER_FLAG_DELTA_UNIT:  this unit cannot be decoded independently.
- * @GST_BUFFER_FLAG_TAG_MEMORY:  this flag is set when memory of the buffer
- *                               is added/removed
- * @GST_BUFFER_FLAG_SYNC_AFTER:  Elements which write to disk or permanent
+ * @GST_BUFFER_FLAG_LIVE:          the buffer is live data and should be discarded in
+ *                                 the PAUSED state.
+ * @GST_BUFFER_FLAG_DECODE_ONLY:   the buffer contains data that should be dropped
+ *                                 because it will be clipped against the segment
+ *                                 boundaries or because it does not contain data
+ *                                 that should be shown to the user.
+ * @GST_BUFFER_FLAG_DISCONT:       the buffer marks a data discontinuity in the stream.
+ *                                 This typically occurs after a seek or a dropped buffer
+ *                                 from a live or network source.
+ * @GST_BUFFER_FLAG_RESYNC:        the buffer timestamps might have a discontinuity
+ *                                 and this buffer is a good point to resynchronize.
+ * @GST_BUFFER_FLAG_CORRUPTED:     the buffer data is corrupted.
+ * @GST_BUFFER_FLAG_MARKER:        the buffer contains a media specific marker. for
+ *                                 video this is typically the end of a frame boundary, for audio
+ *                                 this is usually the start of a talkspurt.
+ * @GST_BUFFER_FLAG_HEADER:        the buffer contains header information that is
+ *                                 needed to decode the following data.
+ * @GST_BUFFER_FLAG_GAP:           the buffer has been created to fill a gap in the
+ *                                 stream and contains media neutral data (elements can
+ *                                 switch to optimized code path that ignores the buffer
+ *                                 content).
+ * @GST_BUFFER_FLAG_DROPPABLE:     the buffer can be dropped without breaking the
+ *                                 stream, for example to reduce bandwidth.
+ * @GST_BUFFER_FLAG_DELTA_UNIT:    this unit cannot be decoded independently.
+ * @GST_BUFFER_FLAG_TAG_MEMORY:    this flag is set when memory of the buffer
+ *                                 is added/removed
+ * @GST_BUFFER_FLAG_SYNC_AFTER:    Elements which write to disk or permanent
  * 				 storage should ensure the data is synced after
  * 				 writing the contents of this buffer. (Since 1.6)
- * @GST_BUFFER_FLAG_LAST:        additional media specific flags can be added starting from
- *                               this flag.
+ * @GST_BUFFER_FLAG_LAST:          additional media specific flags can be added starting from
+ *                                 this flag.
+ * @GST_BUFFER_FLAG_NON_DROPPABLE: This buffer is important and should not be dropped.
+ *                                 This can be used to mark important buffers, e.g. to flag
+ *                                 RTP packets carrying keyframes or codec setup data for RTP
+ *                                 Forward Error Correction purposes, or to prevent still video
+ *                                 frames from being dropped by elements due to QoS. (Since 1.14)
  *
  * A set of buffer flags used to describe properties of a #GstBuffer.
  */
 typedef enum {
-  GST_BUFFER_FLAG_LIVE        = (GST_MINI_OBJECT_FLAG_LAST << 0),
-  GST_BUFFER_FLAG_DECODE_ONLY = (GST_MINI_OBJECT_FLAG_LAST << 1),
-  GST_BUFFER_FLAG_DISCONT     = (GST_MINI_OBJECT_FLAG_LAST << 2),
-  GST_BUFFER_FLAG_RESYNC      = (GST_MINI_OBJECT_FLAG_LAST << 3),
-  GST_BUFFER_FLAG_CORRUPTED   = (GST_MINI_OBJECT_FLAG_LAST << 4),
-  GST_BUFFER_FLAG_MARKER      = (GST_MINI_OBJECT_FLAG_LAST << 5),
-  GST_BUFFER_FLAG_HEADER      = (GST_MINI_OBJECT_FLAG_LAST << 6),
-  GST_BUFFER_FLAG_GAP         = (GST_MINI_OBJECT_FLAG_LAST << 7),
-  GST_BUFFER_FLAG_DROPPABLE   = (GST_MINI_OBJECT_FLAG_LAST << 8),
-  GST_BUFFER_FLAG_DELTA_UNIT  = (GST_MINI_OBJECT_FLAG_LAST << 9),
-  GST_BUFFER_FLAG_TAG_MEMORY  = (GST_MINI_OBJECT_FLAG_LAST << 10),
-  GST_BUFFER_FLAG_SYNC_AFTER  = (GST_MINI_OBJECT_FLAG_LAST << 11),
+  GST_BUFFER_FLAG_LIVE          = (GST_MINI_OBJECT_FLAG_LAST << 0),
+  GST_BUFFER_FLAG_DECODE_ONLY   = (GST_MINI_OBJECT_FLAG_LAST << 1),
+  GST_BUFFER_FLAG_DISCONT       = (GST_MINI_OBJECT_FLAG_LAST << 2),
+  GST_BUFFER_FLAG_RESYNC        = (GST_MINI_OBJECT_FLAG_LAST << 3),
+  GST_BUFFER_FLAG_CORRUPTED     = (GST_MINI_OBJECT_FLAG_LAST << 4),
+  GST_BUFFER_FLAG_MARKER        = (GST_MINI_OBJECT_FLAG_LAST << 5),
+  GST_BUFFER_FLAG_HEADER        = (GST_MINI_OBJECT_FLAG_LAST << 6),
+  GST_BUFFER_FLAG_GAP           = (GST_MINI_OBJECT_FLAG_LAST << 7),
+  GST_BUFFER_FLAG_DROPPABLE     = (GST_MINI_OBJECT_FLAG_LAST << 8),
+  GST_BUFFER_FLAG_DELTA_UNIT    = (GST_MINI_OBJECT_FLAG_LAST << 9),
+  GST_BUFFER_FLAG_TAG_MEMORY    = (GST_MINI_OBJECT_FLAG_LAST << 10),
+  GST_BUFFER_FLAG_SYNC_AFTER    = (GST_MINI_OBJECT_FLAG_LAST << 11),
+  GST_BUFFER_FLAG_NON_DROPPABLE = (GST_MINI_OBJECT_FLAG_LAST << 12),
 
-  GST_BUFFER_FLAG_LAST        = (GST_MINI_OBJECT_FLAG_LAST << 16)
+  GST_BUFFER_FLAG_LAST          = (GST_MINI_OBJECT_FLAG_LAST << 16)
 } GstBufferFlags;
 
 /**
