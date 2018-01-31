48fa908e15ba474b97a3d2d08d092dee6a4bcd00
George Kiagiadakis <george.kiagiadakis@collabora.com>
2018-01-31 17:41:32 +0200

    tests: audiotestsrc: add unit test for non-interleaved audio output
    https://bugzilla.gnome.org/show_bug.cgi?id=796739

--- tests/check/elements/audiotestsrc.c
+++ tests/check/elements/audiotestsrc.c
@@ -25,6 +25,7 @@
 #endif
 
 #include <gst/check/gstcheck.h>
+#include <gst/check/gstharness.h>
 #include <gst/audio/audio.h>
 
 /* For ease of programming we use globals to keep refs for our floating
@@ -114,6 +115,188 @@ GST_START_TEST (test_all_waves)
 
 GST_END_TEST;
 
+
+#define TEST_LAYOUT_CHANNELS 6
+
+static GstStaticPadTemplate sinktemplate_interleaved =
+GST_STATIC_PAD_TEMPLATE ("sink",
+    GST_PAD_SINK,
+    GST_PAD_ALWAYS,
+    GST_STATIC_CAPS ("audio/x-raw, "
+        "format = (string) " GST_AUDIO_NE (S16) ", "
+        "channels = (int) " G_STRINGIFY (TEST_LAYOUT_CHANNELS) ", "
+        "rate = (int) [ 1,  MAX ], layout = (string) interleaved")
+    );
+
+static GstStaticPadTemplate sinktemplate_planar =
+GST_STATIC_PAD_TEMPLATE ("sink",
+    GST_PAD_SINK,
+    GST_PAD_ALWAYS,
+    GST_STATIC_CAPS ("audio/x-raw, "
+        "format = (string) " GST_AUDIO_NE (S16) ", "
+        "channels = (int) " G_STRINGIFY (TEST_LAYOUT_CHANNELS) ", "
+        "rate = (int) [ 1,  MAX ], layout = (string) non-interleaved")
+    );
+
+typedef enum
+{
+  GST_AUDIO_TEST_SRC_WAVE_SINE,
+  GST_AUDIO_TEST_SRC_WAVE_SQUARE,
+  GST_AUDIO_TEST_SRC_WAVE_SAW,
+  GST_AUDIO_TEST_SRC_WAVE_TRIANGLE,
+  GST_AUDIO_TEST_SRC_WAVE_SILENCE,
+  GST_AUDIO_TEST_SRC_WAVE_WHITE_NOISE,
+  GST_AUDIO_TEST_SRC_WAVE_PINK_NOISE,
+  GST_AUDIO_TEST_SRC_WAVE_SINE_TAB,
+  GST_AUDIO_TEST_SRC_WAVE_TICKS,
+  GST_AUDIO_TEST_SRC_WAVE_GAUSSIAN_WHITE_NOISE,
+  GST_AUDIO_TEST_SRC_WAVE_RED_NOISE,
+  GST_AUDIO_TEST_SRC_WAVE_BLUE_NOISE,
+  GST_AUDIO_TEST_SRC_WAVE_VIOLET_NOISE,
+
+  _GST_AUDIO_TEST_SRC_WAVE_LAST
+} GstAudioTestSrcWave;
+
+GST_START_TEST (test_layout)
+{
+  GstHarness *interleavedsrc, *plannarsrc;
+  GObjectClass *oclass;
+  GParamSpec *property;
+  GEnumValue *values;
+  guint i, j;
+
+  interleavedsrc = gst_harness_new_with_templates ("audiotestsrc", NULL,
+      &sinktemplate_interleaved);
+  plannarsrc = gst_harness_new_with_templates ("audiotestsrc", NULL,
+      &sinktemplate_planar);
+
+  gst_harness_use_testclock (interleavedsrc);
+  gst_harness_use_testclock (plannarsrc);
+  g_object_set (interleavedsrc->element, "is-live", TRUE, NULL);
+  g_object_set (plannarsrc->element, "is-live", TRUE, NULL);
+
+  oclass = G_OBJECT_GET_CLASS (interleavedsrc->element);
+  property = g_object_class_find_property (oclass, "wave");
+  fail_unless (G_IS_PARAM_SPEC_ENUM (property));
+  values = G_ENUM_CLASS (g_type_class_ref (property->value_type))->values;
+
+  for (j = 0; values[j].value_name; j++) {
+    /* these produce random values by definition,
+     * so we can't compare channels */
+    switch (j) {
+      case GST_AUDIO_TEST_SRC_WAVE_WHITE_NOISE:
+      case GST_AUDIO_TEST_SRC_WAVE_PINK_NOISE:
+      case GST_AUDIO_TEST_SRC_WAVE_GAUSSIAN_WHITE_NOISE:
+      case GST_AUDIO_TEST_SRC_WAVE_RED_NOISE:
+      case GST_AUDIO_TEST_SRC_WAVE_BLUE_NOISE:
+      case GST_AUDIO_TEST_SRC_WAVE_VIOLET_NOISE:
+        continue;
+      default:
+        break;
+    }
+
+    GST_DEBUG ("layout test with wave %s", values[j].value_name);
+    g_object_set (interleavedsrc->element, "wave", values[j].value, NULL);
+    g_object_set (plannarsrc->element, "wave", values[j].value, NULL);
+
+    if (j == 0) {
+      GST_DEBUG ("gst_harness_play");
+      gst_harness_play (interleavedsrc);
+      gst_harness_play (plannarsrc);
+    } else {
+      GST_DEBUG ("discarding buffers with old wave");
+      fail_unless (gst_harness_crank_single_clock_wait (interleavedsrc));
+      fail_unless (gst_harness_crank_single_clock_wait (plannarsrc));
+      gst_buffer_unref (gst_harness_pull (interleavedsrc));
+      gst_buffer_unref (gst_harness_pull (plannarsrc));
+    }
+
+    for (i = 0; i < 10; i++) {
+      GstBuffer *ibuf, *pbuf;
+      GstMapInfo imap, pmap;
+      GstAudioMeta *meta;
+      GstAudioBuffer pabuf;
+      gint16 *iptr, *pptr;
+      guint isamples, psamples, s, c;
+
+      GST_DEBUG ("waiting on clock");
+      fail_unless (gst_harness_crank_single_clock_wait (interleavedsrc));
+      fail_unless (gst_harness_crank_single_clock_wait (plannarsrc));
+
+      ibuf = gst_harness_pull (interleavedsrc);
+      pbuf = gst_harness_pull (plannarsrc);
+
+      gst_buffer_map (ibuf, &imap, GST_MAP_READ);
+      gst_buffer_map (pbuf, &pmap, GST_MAP_READ);
+
+      /* buffers should have the same size in bytes and in samples */
+      fail_unless_equals_int (imap.size, pmap.size);
+      isamples = imap.size / TEST_LAYOUT_CHANNELS;
+      isamples /= 2;            /* S16 -> 2 bytes per sample */
+      fail_unless_equals_int (imap.size % TEST_LAYOUT_CHANNELS, 0);
+      psamples = pmap.size / TEST_LAYOUT_CHANNELS;
+      psamples /= 2;            /* S16 -> 2 bytes per sample */
+      fail_unless_equals_int (pmap.size % TEST_LAYOUT_CHANNELS, 0);
+      fail_unless_equals_int (isamples, psamples);
+
+      iptr = (gint16 *) imap.data;
+      pptr = (gint16 *) pmap.data;
+
+      GST_DEBUG ("verifying contents of buffers; samples=%d, channels=%d",
+          isamples, TEST_LAYOUT_CHANNELS);
+
+      for (s = 0; s < isamples; s++) {
+        for (c = 0; c < TEST_LAYOUT_CHANNELS; c++) {
+          guint iidx = s * TEST_LAYOUT_CHANNELS + c;
+          guint pidx = c * isamples + s;
+
+          GST_TRACE ("s = %u | c = %u | iidx (s * channels + c) = %u | "
+              "pidx (c * samples + s) = %u", s, c, iidx, pidx);
+
+          fail_unless (iidx < imap.size / 2);
+          fail_unless (pidx < pmap.size / 2);
+          fail_unless_equals_int (iptr[iidx], pptr[pidx]);
+        }
+      }
+
+      gst_buffer_unmap (pbuf, &pmap);
+
+      GST_DEBUG ("verify that mapping through GstAudioBuffer works the same");
+
+      meta = gst_buffer_get_audio_meta (pbuf);
+      fail_unless (meta);
+
+      gst_audio_buffer_map (&pabuf, &meta->info, pbuf, GST_MAP_READ);
+
+      for (s = 0; s < isamples; s++) {
+        for (c = 0; c < TEST_LAYOUT_CHANNELS; c++) {
+          guint iidx = s * TEST_LAYOUT_CHANNELS + c;
+
+          fail_unless_equals_int (iptr[iidx], ((gint16 *) pabuf.planes[c])[s]);
+        }
+      }
+
+      gst_audio_buffer_unmap (&pabuf);
+      gst_buffer_unmap (ibuf, &imap);
+
+      gst_buffer_unref (ibuf);
+      gst_buffer_unref (pbuf);
+    }
+
+    /* ensure the audiotestsrcs are not in fill() while we change the wave */
+    fail_unless (gst_harness_wait_for_clock_id_waits (interleavedsrc, 1, 1));
+    fail_unless (gst_harness_wait_for_clock_id_waits (plannarsrc, 1, 1));
+  }
+
+  /* make sure we ran the test */
+  fail_unless_equals_int (j, _GST_AUDIO_TEST_SRC_WAVE_LAST);
+
+  gst_harness_teardown (interleavedsrc);
+  gst_harness_teardown (plannarsrc);
+}
+
+GST_END_TEST;
+
 static Suite *
 audiotestsrc_suite (void)
 {
@@ -122,6 +305,7 @@ audiotestsrc_suite (void)
 
   suite_add_tcase (s, tc_chain);
   tcase_add_test (tc_chain, test_all_waves);
+  tcase_add_test (tc_chain, test_layout);
 
   return s;
 }
