c946e323f6ccd4ed0dd2b2da7920e600f8de28b5
George Kiagiadakis <george.kiagiadakis@collabora.com>
2018-02-07 14:36:01 +0200

    libs: audio: Implement GstAudioBuffer & GstAudioMeta
    Library bits to support non-interleaved audio
    
    https://bugzilla.gnome.org/show_bug.cgi?id=751605

--- gst-libs/gst/audio/audio.h
+++ gst-libs/gst/audio/audio.h
@@ -29,6 +29,7 @@
 #include <gst/audio/audio-channels.h>
 #include <gst/audio/audio-channel-mixer.h>
 #include <gst/audio/audio-info.h>
+#include <gst/audio/audio-buffer.h>
 #include <gst/audio/audio-quantize.h>
 #include <gst/audio/audio-converter.h>
 #include <gst/audio/audio-resampler.h>
