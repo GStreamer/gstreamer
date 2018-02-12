2bb00ebfaad3c84726437dae2689e2b45a0507d2
Mathieu Duponchelle <mathieu@centricular.com>
2018-02-12 19:12:35 +0100

    test-cleanup: bind any port
    The meson test suite runs tests in parallel, trying to bind
    a single port made the test fail.

--- tests/test-cleanup.c
+++ tests/test-cleanup.c
@@ -43,6 +43,9 @@ main (int argc, char *argv[])
   /* create a server instance */
   server = gst_rtsp_server_new ();
 
+  /* We just want to bind any port here, so that tests can run in parallel */
+  gst_rtsp_server_set_service (server, "0");
+
   /* attach the server to the default maincontext */
   if ((id = gst_rtsp_server_attach (server, NULL)) == 0)
     goto failed;
