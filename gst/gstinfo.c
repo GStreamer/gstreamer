6436437d836badb1f01f71cf039fa65e4a3a2023
Jan Alexander Steffens (heftig) <jan.steffens@gmail.com>
2018-03-27 10:25:46 +0200

    gstinfo: fix debug levels being applied in the wrong order
    Remove unneeded reapplication of patterns. Besides being
    superfluous (gst_debug_reset_threshold already applies
    patterns) it was also wrong and didn't stop checking patterns
    after the first match (broken in 67e9d139).
    
    Also fix up unit test which checked for the wrong order.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=794717

--- gst/gstinfo.c
+++ gst/gstinfo.c
@@ -1691,18 +1691,6 @@ gst_debug_unset_threshold_for_name (const gchar * name)
   gst_debug_reset_all_thresholds ();
 }
 
-static void
-gst_debug_apply_patterns_to_category (GstDebugCategory * cat)
-{
-  GSList *l;
-
-  g_mutex_lock (&__level_name_mutex);
-  for (l = __level_name; l != NULL; l = l->next) {
-    for_each_threshold_by_entry (cat, (LevelNameEntry *) l->data);
-  }
-  g_mutex_unlock (&__level_name_mutex);
-}
-
 GstDebugCategory *
 _gst_debug_category_new (const gchar * name, guint color,
     const gchar * description)
@@ -1734,9 +1722,6 @@ _gst_debug_category_new (const gchar * name, guint color,
     __categories = g_slist_prepend (__categories, cat);
   }
   g_mutex_unlock (&__cat_mutex);
-
-  /* ensure the filter is applied to categories registered after _debug_init */
-  gst_debug_apply_patterns_to_category (cat);
 
   return cat;
 }

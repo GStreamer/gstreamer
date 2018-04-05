GStreamer/gstreamer

commit 67e9d139504b942552c769791a0e7657e840d872

info: fix performance issue with registering categories after gst_init()
When registering a new debug category after gst_init(), simply check
the existing patterns against that new category.

No need to iterate over all categories and recheck them all against
the existing patterns.

Also, no need to re-parse the existing pattern string set via GST_DEBUG
and add the same set of match patterns all over again to the existing
list of match patterns every time we register a new debug category.

Combined with iterating all debug categories on a change this would
make adding debug categories after gst_init() very very very slow.

gst/gstinfo.c
@@ -1692,6 +1692,18 @@ gst_debug_unset_threshold_for_name (const gchar * name)
   gst_debug_reset_all_thresholds ();
 }
 
+static void
+gst_debug_apply_patterns_to_category (GstDebugCategory * cat)
+{
+  GSList *l;
+
+  g_mutex_lock (&__level_name_mutex);
+  for (l = __level_name; l != NULL; l = l->next) {
+    for_each_threshold_by_entry (cat, (LevelNameEntry *) l->data);
+  }
+  g_mutex_unlock (&__level_name_mutex);
+}
+
 GstDebugCategory *
 _gst_debug_category_new (const gchar * name, guint color,
     const gchar * description)
@@ -1726,7 +1738,7 @@ _gst_debug_category_new (const gchar * name, guint color,
 
   /* ensure the filter is applied to categories registered after _debug_init */
   if (_gst_debug_filter) {
-    gst_debug_set_threshold_from_string (_gst_debug_filter, FALSE);
+    gst_debug_apply_patterns_to_category (cat);
   }
 
   return cat;

commit 6436437d836badb1f01f71cf039fa65e4a3a2023

gstinfo: fix debug levels being applied in the wrong order
Remove unneeded reapplication of patterns. Besides being
superfluous (gst_debug_reset_threshold already applies
patterns) it was also wrong and didn't stop checking patterns
after the first match (broken in 67e9d139).

Also fix up unit test which checked for the wrong order.

https://bugzilla.gnome.org/show_bug.cgi?id=794717

gst/gstinfo.c
@@ -1691,18 +1691,6 @@ gst_debug_unset_threshold_for_name (const gchar * name)
   gst_debug_reset_all_thresholds ();
 }
 
-static void
-gst_debug_apply_patterns_to_category (GstDebugCategory * cat)
-{
-  GSList *l;
-
-  g_mutex_lock (&__level_name_mutex);
-  for (l = __level_name; l != NULL; l = l->next) {
-    for_each_threshold_by_entry (cat, (LevelNameEntry *) l->data);
-  }
-  g_mutex_unlock (&__level_name_mutex);
-}
-
 GstDebugCategory *
 _gst_debug_category_new (const gchar * name, guint color,
     const gchar * description)
@@ -1734,9 +1722,6 @@ _gst_debug_category_new (const gchar * name, guint color,
     __categories = g_slist_prepend (__categories, cat);
   }
   g_mutex_unlock (&__cat_mutex);
-
-  /* ensure the filter is applied to categories registered after _debug_init */
-  gst_debug_apply_patterns_to_category (cat);
 
   return cat;
 }

tests/check/gst/gstinfo.c
@@ -440,7 +440,8 @@ GST_START_TEST (info_post_gst_init_category_registration)
   /* Note: before the fixes this wouldn't work to trigger the problem because
    * only a pattern set via GST_DEBUG before gst_init would be picked up
    * (another bug) */
-  gst_debug_set_threshold_from_string ("*a*b:6,*c:3,d*:2,xyz*:9,ax:1", TRUE);
+  gst_debug_set_threshold_from_string ("*a*b:6,*b*0:6,*c:3,d*:2,xyz*:9,ax:1",
+      TRUE);
 
   fail_unless_equals_int (GST_LEVEL_DEFAULT,
       gst_debug_get_default_threshold ());
@@ -471,8 +472,12 @@ GST_START_TEST (info_post_gst_init_category_registration)
     /* *c:3 */
     fail_unless_equals_int (gst_debug_category_get_threshold (cats[0x4c]),
         GST_LEVEL_FIXME);
-    /* *a*b:6 */
+    /* *a*b:6 and d*:2, but d*:2 takes priority here as cat name is "dog-a1b"
+     * and order matters: items listed later override earlier ones. */
     fail_unless_equals_int (gst_debug_category_get_threshold (cats[0xa1b]),
+        GST_LEVEL_WARNING);
+    /* *a*0:6 */
+    fail_unless_equals_int (gst_debug_category_get_threshold (cats[0xb10]),
         GST_LEVEL_LOG);
   }
 