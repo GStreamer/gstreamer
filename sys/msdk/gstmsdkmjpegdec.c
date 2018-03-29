ee88f699c4a0a8d075b802f110308206ce8f0f82
Sreerenj Balachandran <sreerenj.balachandran@intel.com>
2018-03-29 13:06:41 -0800

    msdk: dec: remove framerate field from sink caps template
    Removes unessential field framerate for decoder so that negotiation
    works even if framerate is not provided from upstream.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=789752

--- sys/msdk/gstmsdkmjpegdec.c
+++ sys/msdk/gstmsdkmjpegdec.c
@@ -52,7 +52,6 @@ static GstStaticPadTemplate sink_factory = GST_STATIC_PAD_TEMPLATE ("sink",
     GST_PAD_SINK,
     GST_PAD_ALWAYS,
     GST_STATIC_CAPS ("image/jpeg, "
-        "framerate = (fraction) [0/1, MAX], "
         "width = (int) [ 1, MAX ], height = (int) [ 1, MAX ], parsed = true ")
     );
 
