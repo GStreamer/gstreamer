96c6a04d7a1914cad2e798937ec7b7c164202aa1
Sreerenj Balachandran <sreerenj.balachandran@intel.com>
2018-04-24 16:46:20 -0800

    msdk: Add more video format mapping
    BGRx format can be supported with Msdk's RGB4

--- sys/msdk/msdk.c
+++ sys/msdk/msdk.c
@@ -56,6 +56,7 @@ static const struct map gst_msdk_video_format_to_mfx_map[] = {
   GST_VIDEO_INFO_TO_MFX_MAP (YUY2, YUV422, YUY2),
   GST_VIDEO_INFO_TO_MFX_MAP (UYVY, YUV422, UYVY),
   GST_VIDEO_INFO_TO_MFX_MAP (BGRA, YUV444, RGB4),
+  GST_VIDEO_INFO_TO_MFX_MAP (BGRx, YUV444, RGB4),
   {0, 0, 0}
 };
 
