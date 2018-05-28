c72ec85879b37207a597cdbf4de8a7b6a6b3facf
Nicolas Dufresne <nicolas.dufresne@collabora.com>
2018-05-28 15:19:52 -0400

    v4l2bufferpool: Drop truncated frames
    Drop truncated frames regardless if they have the ERROR flag or not.
    Truncated frame causes video frame map failure in many elements
    including cluttersink, glupload etc.

--- sys/v4l2/gstv4l2bufferpool.c
+++ sys/v4l2/gstv4l2bufferpool.c
@@ -1719,6 +1719,10 @@ gst_v4l2_buffer_pool_process (GstV4l2BufferPool * pool, GstBuffer ** buf)
                 goto eos;
             }
 
+            if (GST_VIDEO_INFO_FORMAT (&pool->caps_info) !=
+                GST_VIDEO_FORMAT_ENCODED && size < pool->size)
+              goto buffer_truncated;
+
             num_queued = g_atomic_int_get (&pool->num_queued);
             GST_TRACE_OBJECT (pool, "Only %i buffer left in the capture queue.",
                 num_queued);
@@ -1944,6 +1948,14 @@ copy_failed:
 buffer_corrupted:
   {
     GST_WARNING_OBJECT (pool, "Dropping corrupted buffer without payload");
+    gst_buffer_unref (*buf);
+    *buf = NULL;
+    return GST_V4L2_FLOW_CORRUPTED_BUFFER;
+  }
+buffer_truncated:
+  {
+    GST_WARNING_OBJECT (pool,
+        "Dropping truncated buffer, this is likely a driver bug.");
     gst_buffer_unref (*buf);
     *buf = NULL;
     return GST_V4L2_FLOW_CORRUPTED_BUFFER;
