a677d24b9aebcba466d5cc163d644f656fcb660a
Jan Schmidt <jan@centricular.com>
2018-06-01 17:07:19 +1000

    dvb: Fix typo in comment termination

--- sys/dvb/camconditionalaccess.h
+++ sys/dvb/camconditionalaccess.h
@@ -19,7 +19,7 @@
  * License along with this library; if not, write to the
  * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
  * Boston, MA 02110-1301, USA.
- *V/
+ */
 
 #ifndef CAM_CONDITIONAL_ACCESS_H
 #define CAM_CONDITIONAL_ACCESS_H
