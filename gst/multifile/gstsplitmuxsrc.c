3ac5430311b20f30814cdabf5724fb687748bb5b
Jan Schmidt <jan@centricular.com>
2018-07-16 23:43:29 +1000

    splitmuxsrc: Make sure events are writable
    Before setting the seqnum on events sent downstream,
    make sure they are writable.

--- gst/multifile/gstsplitmuxsrc.c
+++ gst/multifile/gstsplitmuxsrc.c
@@ -480,8 +480,10 @@ gst_splitmux_handle_event (GstSplitMuxSrc * splitmux,
 
       gst_event_unref (event);
       event = gst_event_new_segment (&seg);
-      if (splitmux->segment_seqnum)
+      if (splitmux->segment_seqnum) {
+        event = gst_event_make_writable (event);
         gst_event_set_seqnum (event, splitmux->segment_seqnum);
+      }
       splitpad->sent_segment = TRUE;
       break;
     }
