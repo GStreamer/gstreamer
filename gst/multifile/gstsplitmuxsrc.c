f667ab05d2813ac8e36d4ee02969bdb866c58a7b
Jan Schmidt <jan@centricular.com>
2018-07-17 01:33:55 +1000

    Revert "splitmuxsrc: Make sure events are writable"
    This reverts commit 3ac5430311b20f30814cdabf5724fb687748bb5b.
    
    There's no need to make a freshly created event writable,
    and the other half of this patch was already fixed
    and pushed in f2f15a1

--- gst/multifile/gstsplitmuxsrc.c
+++ gst/multifile/gstsplitmuxsrc.c
@@ -480,10 +480,8 @@ gst_splitmux_handle_event (GstSplitMuxSrc * splitmux,
 
       gst_event_unref (event);
       event = gst_event_new_segment (&seg);
-      if (splitmux->segment_seqnum) {
-        event = gst_event_make_writable (event);
+      if (splitmux->segment_seqnum)
         gst_event_set_seqnum (event, splitmux->segment_seqnum);
-      }
       splitpad->sent_segment = TRUE;
       break;
     }
