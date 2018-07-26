7d8a314dffb6fe0c42412f1c56e5b4e76300946e
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2018-07-26 16:30:08 +0200

    omxvideodec: fix gst_video_info_from_caps() caps assertion
    The "use buffers" code path uses gst_video_info_from_caps() which is
    asserting if caps is NULL (because pool was rejected).
    
    https://bugzilla.gnome.org/show_bug.cgi?id=796918

--- omx/gstomxvideodec.c
+++ omx/gstomxvideodec.c
@@ -948,6 +948,9 @@ gst_omx_video_dec_allocate_output_buffers (GstOMXVideoDec * self)
       was_enabled = FALSE;
     }
 
+    if (!caps)
+      self->use_buffers = FALSE;
+
     if (self->use_buffers) {
       GList *images = NULL;
       GList *frames = NULL;
