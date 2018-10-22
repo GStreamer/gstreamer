GStreamer/gstreamer

commit e655d47dfce1652630fe8ff5fb6be56370087004

isomp4: fix wrong DAR calculation for PAR <= 1
CID #1226452

https://bugzilla.gnome.org/show_bug.cgi?id=736396

gst/isomp4/atoms.c
@@ -48,6 +48,7 @@
 #include <gst/gst.h>
 #include <gst/base/gstbytewriter.h>
 #include <gst/tag/tag.h>
+#include <gst/video/video.h>
 
 /**
  * Creates a new AtomsContext for the given flavor.
@@ -3483,7 +3484,7 @@ atom_trak_set_video_type (AtomTRAK * trak, AtomsContext * context,
     VisualSampleEntry * entry, guint32 scale, GList * ext_atoms_list)
 {
   SampleTableEntryMP4V *ste;
-  gint dwidth, dheight;
+  guint dwidth, dheight;
   gint par_n = 0, par_d = 0;
 
   if ((entry->par_n != 1 || entry->par_d != 1) &&
@@ -3497,12 +3498,10 @@ atom_trak_set_video_type (AtomTRAK * trak, AtomsContext * context,
   /* ISO file spec says track header w/h indicates track's visual presentation
    * (so this together with pixels w/h implicitly defines PAR) */
   if (par_n && (context->flavor != ATOMS_TREE_FLAVOR_MOV)) {
-    if (par_n > par_d) {
-      dwidth = entry->width * par_n / par_d;
-      dheight = entry->height;
-    } else {
-      dwidth = entry->width * par_n / par_d;
-      dheight = entry->height;
+    /* Assumes square pixels display */
+    if (!gst_video_calculate_display_ratio (&dwidth, &dheight, entry->width,
+            entry->height, par_n, par_d, 1, 1)) {
+      GST_WARNING ("Could not calculate display ratio");
     }
   }
 

commit 324f8c7f3cf595b1a6be87c44739c41b2d5f5d8f

qtmux: Correctly set tkhd width/height to the display size
It was previously set to the display aspect ratio, e.g. 4x3, 16x9, etc.
but should be set to the display size.

This is a regression from e655d47dfce1652630fe8ff5fb6be56370087004
(1.5.1) and was correct before that.

https://bugzilla.gnome.org/show_bug.cgi?id=797318

gst/isomp4/atoms.c
@@ -4349,11 +4349,8 @@ atom_trak_set_video_type (AtomTRAK * trak, AtomsContext * context,
   /* ISO file spec says track header w/h indicates track's visual presentation
    * (so this together with pixels w/h implicitly defines PAR) */
   if (par_n && (context->flavor != ATOMS_TREE_FLAVOR_MOV)) {
-    /* Assumes square pixels display */
-    if (!gst_video_calculate_display_ratio (&dwidth, &dheight, entry->width,
-            entry->height, par_n, par_d, 1, 1)) {
-      GST_WARNING ("Could not calculate display ratio");
-    }
+    dwidth = entry->width * par_n / par_d;
+    dheight = entry->height;
   }
 
   atom_trak_set_video_commons (trak, context, scale, dwidth, dheight);