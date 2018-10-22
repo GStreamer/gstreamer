dce17521eb48cb2a3ceeb667c378c9640479db10
Thibault Saunier <tsaunier@igalia.com>
2018-10-22 11:30:09 +0200

    directfb: Fixup plugin name to match plugin filename
    Has required by the new PLUGIN_DEFINE macro

--- ext/directfb/dfbvideosink.c
+++ ext/directfb/dfbvideosink.c
@@ -2467,6 +2467,6 @@ plugin_init (GstPlugin * plugin)
 
 GST_PLUGIN_DEFINE (GST_VERSION_MAJOR,
     GST_VERSION_MINOR,
-    dfbvideosink,
+    directfb,
     "DirectFB video output plugin",
     plugin_init, VERSION, GST_LICENSE, GST_PACKAGE_NAME, GST_PACKAGE_ORIGIN)
