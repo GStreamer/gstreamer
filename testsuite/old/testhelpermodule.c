7b1dcdab581d501e6667259e992e5d8fde6d5947
Tim-Philipp Müller <tim@centricular.com>
2018-12-15 13:55:07 +0000

    Fix indentation of .c files
    Required to make gst-indent linter on CI happy.

--- testsuite/old/testhelpermodule.c
+++ testsuite/old/testhelpermodule.c
@@ -12,45 +12,45 @@ _wrap_get_object (PyObject * self)
   if (!obj) {
     return NULL;
   }
-  
-  return pygobject_new(obj);
+
+  return pygobject_new (obj);
 }
 
 static PyObject *
-_wrap_emit_event (PyObject * self, PyObject *args)
+_wrap_emit_event (PyObject * self, PyObject * args)
 {
   PyGObject *obj;
   GstEventType event_type = GST_EVENT_UNKNOWN;
   GstEvent *event;
-  
-  if (!PyArg_ParseTuple(args, "O|i", &obj, &event_type))
+
+  if (!PyArg_ParseTuple (args, "O|i", &obj, &event_type))
     return NULL;
 
-  event = gst_event_new_custom(event_type, NULL);
-  
-  g_signal_emit_by_name(G_OBJECT(obj->obj), "event", event);
-  
-  gst_mini_object_unref(GST_MINI_OBJECT(event));
+  event = gst_event_new_custom (event_type, NULL);
 
-  Py_INCREF(Py_None);
+  g_signal_emit_by_name (G_OBJECT (obj->obj), "event", event);
+
+  gst_mini_object_unref (GST_MINI_OBJECT (event));
+
+  Py_INCREF (Py_None);
   return Py_None;
 }
 
 static PyMethodDef testhelper_methods[] = {
-    { "get_object", (PyCFunction)_wrap_get_object, METH_NOARGS },
-    { "emit_event", (PyCFunction)_wrap_emit_event, METH_VARARGS },
-    { NULL, NULL }
+  {"get_object", (PyCFunction) _wrap_get_object, METH_NOARGS},
+  {"emit_event", (PyCFunction) _wrap_emit_event, METH_VARARGS},
+  {NULL, NULL}
 };
 
-void 
+void
 inittesthelper ()
 {
   PyObject *m, *d;
-  
-  init_pygobject();
-  gst_init(NULL, NULL);
+
+  init_pygobject ();
+  gst_init (NULL, NULL);
 
   m = Py_InitModule ("testhelper", testhelper_methods);
 
-  d = PyModule_GetDict(m);
+  d = PyModule_GetDict (m);
 }
