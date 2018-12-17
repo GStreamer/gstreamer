f4c9267265dfc13bab065a25869946ea3133e472
Edward Hervey <edward@centricular.com>
2018-12-17 09:03:36 +0100

    sdpmessage: Remove dead assignment
    The presence of `key-mgmt` attribute will set the mikey appropriately.
    We therefore don't need to check the return value (which will
    be overwritten afterwards).

--- gst-libs/gst/sdp/gstsdpmessage.c
+++ gst-libs/gst/sdp/gstsdpmessage.c
@@ -4095,7 +4095,7 @@ gst_sdp_message_attributes_to_caps (const GstSDPMessage * msg, GstCaps * caps)
   g_return_val_if_fail (msg != NULL, GST_SDP_EINVAL);
   g_return_val_if_fail (caps != NULL && GST_IS_CAPS (caps), GST_SDP_EINVAL);
 
-  res = gst_sdp_message_parse_keymgmt (msg, &mikey);
+  gst_sdp_message_parse_keymgmt (msg, &mikey);
   if (mikey) {
     if (gst_mikey_message_to_caps (mikey, caps)) {
       res = GST_SDP_EINVAL;
@@ -4131,7 +4131,7 @@ gst_sdp_media_attributes_to_caps (const GstSDPMedia * media, GstCaps * caps)
   g_return_val_if_fail (media != NULL, GST_SDP_EINVAL);
   g_return_val_if_fail (caps != NULL && GST_IS_CAPS (caps), GST_SDP_EINVAL);
 
-  res = gst_sdp_media_parse_keymgmt (media, &mikey);
+  gst_sdp_media_parse_keymgmt (media, &mikey);
   if (mikey) {
     if (!gst_mikey_message_to_caps (mikey, caps)) {
       res = GST_SDP_EINVAL;
