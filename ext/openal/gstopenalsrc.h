2faa08ff2877de8f299e6434048920fca0ef831e
Justin Kim <jeongseok.kim@sk.com>
2018-12-24 23:05:55 +0900

    openal: Fix AL header path for Mac OS X
    OpenAL.framework doesn't provide 'alext.h'.
    Thus, openal is enabled only if the dependency
    is set by pkg-config.
    
    fixes: #852

--- ext/openal/gstopenalsrc.h
+++ ext/openal/gstopenalsrc.h
@@ -56,10 +56,6 @@
 #include <al.h>
 #include <alc.h>
 #include <alext.h>
-#elif defined(__APPLE__)
-#include <OpenAL/al.h>
-#include <OpenAL/alc.h>
-#include <OpenAL/alext.h>
 #else
 #include <AL/al.h>
 #include <AL/alc.h>
