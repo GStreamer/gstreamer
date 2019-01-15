5e3f94ce287dc8ef7f96f6a8c647b0bb9f7612b4
Haihao Xiang <haihao.xiang@intel.com>
2019-01-15 14:30:34 +0800

    msdkh265dec: support P010_10LE in DMABuf mode

--- sys/msdk/gstmsdkh265dec.c
+++ sys/msdk/gstmsdkh265dec.c
@@ -60,7 +60,7 @@ static GstStaticPadTemplate src_factory = GST_STATIC_PAD_TEMPLATE ("src",
         "width = (int) [ 16, MAX ], height = (int) [ 16, MAX ],"
         "interlace-mode = (string) progressive;"
         GST_VIDEO_CAPS_MAKE_WITH_FEATURES (GST_CAPS_FEATURE_MEMORY_DMABUF,
-            "{ NV12 }") ";")
+            "{ NV12, P010_10LE }") ";")
     );
 
 #define gst_msdkh265dec_parent_class parent_class
