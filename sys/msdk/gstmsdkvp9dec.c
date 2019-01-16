cd55f8635f95fb051b749432402a6f8388b0deeb
Haihao Xiang <haihao.xiang@intel.com>
2019-01-16 15:15:13 +0800

    msdkvp9dec: output P010_10LE buffer if possible

--- sys/msdk/gstmsdkvp9dec.c
+++ sys/msdk/gstmsdkvp9dec.c
@@ -54,12 +54,12 @@ static GstStaticPadTemplate src_factory = GST_STATIC_PAD_TEMPLATE ("src",
     GST_PAD_SRC,
     GST_PAD_ALWAYS,
     GST_STATIC_CAPS ("video/x-raw, "
-        "format = (string) { NV12 }, "
+        "format = (string) { NV12, P010_10LE }, "
         "framerate = (fraction) [0, MAX], "
         "width = (int) [ 16, MAX ], height = (int) [ 16, MAX ],"
         "interlace-mode = (string) progressive;"
         GST_VIDEO_CAPS_MAKE_WITH_FEATURES (GST_CAPS_FEATURE_MEMORY_DMABUF,
-            "{ NV12 }") ";")
+            "{ NV12, P010_10LE }") ";")
     );
 
 #define gst_msdkvp9dec_parent_class parent_class
