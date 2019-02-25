52b5a7e206b345053844155b9d69c2034130925d
Haihao Xiang <haihao.xiang@intel.com>
2019-02-25 13:24:46 +0800

    msdk: map MFX_FOURCC_RGB565 to VA_FOURCC_RGB565

--- sys/msdk/msdk_libva.c
+++ sys/msdk/msdk_libva.c
@@ -62,6 +62,7 @@ static const struct fourcc_map gst_msdk_fourcc_mfx_to_va[] = {
   FOURCC_MFX_TO_VA (RGB4, ARGB),
   FOURCC_MFX_TO_VA (P8, P208),
   FOURCC_MFX_TO_VA (P010, P010),
+  FOURCC_MFX_TO_VA (RGB565, RGB565),
   {0, 0}
 };
 
