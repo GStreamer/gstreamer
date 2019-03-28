2b79c0db5ea65f57d6663f5e090056d8d96ad6ba
Thibault Saunier <tsaunier@igalia.com>
2019-03-28 13:08:55 -0300

    tests: Avoid random timeout and let the launcher set it up for us

--- tests/check/ges/mixers.c
+++ tests/check/ges/mixers.c
@@ -199,7 +199,7 @@ GST_START_TEST (audio_video_mixed_with_pipeline)
   fail_if (gst_element_set_state (GST_ELEMENT (pipeline), GST_STATE_PLAYING)
       == GST_STATE_CHANGE_FAILURE);
 
-  message = gst_bus_timed_pop_filtered (bus, 5 * GST_SECOND,
+  message = gst_bus_timed_pop_filtered (bus, GST_CLOCK_TIME_NONE,
       GST_MESSAGE_ASYNC_DONE | GST_MESSAGE_ERROR);
 
   if (message == NULL) {
