6ade19cc15c4cef80c0fb245b50d80c331e36be4
Aaron Boxer <aaron.boxer@collabora.com>
2019-04-01 11:10:21 -0400

    discoverer: async_timeout_cb: don't clean up timeout_source
    discoverer_collect() already does this. So, the second
    unref triggers an assert.

--- gst-libs/gst/pbutils/gstdiscoverer.c
+++ gst-libs/gst/pbutils/gstdiscoverer.c
@@ -1992,8 +1992,6 @@ async_timeout_cb (GstDiscoverer * dc)
     dc->priv->processing = FALSE;
     discoverer_collect (dc);
     discoverer_cleanup (dc);
-    g_source_unref (dc->priv->timeout_source);
-    dc->priv->timeout_source = NULL;
   }
   return FALSE;
 }
