31e1c8745c7407d85fe5fd7dbf0b4b902576f8a1
Sebastian Dröge <sebastian@centricular.com>
2019-04-22 18:33:20 +0300

    video-anc: Add DID16 value for SMPTE 2016-3 AFD/Bar data

--- gst-libs/gst/video/video-anc.h
+++ gst-libs/gst/video/video-anc.h
@@ -86,6 +86,7 @@ typedef enum {
  * GstVideoAncillaryDID16:
  * @GST_VIDEO_ANCILLARY_DID16_S334_EIA_708: CEA 708 Ancillary data according to SMPTE 334
  * @GST_VIDEO_ANCILLARY_DID16_S334_EIA_608: CEA 608 Ancillary data according to SMPTE 334
+ * @GST_VIDEO_ANCILLARY_DID16_S2016_3_AFD_BAR: AFD/Bar Ancillary data according to SMPTE 2016-3 (Since: 1.18)
  *
  * Some know types of Ancillary Data identifiers.
  *
@@ -94,6 +95,7 @@ typedef enum {
 typedef enum {
   GST_VIDEO_ANCILLARY_DID16_S334_EIA_708	= 0x6101,
   GST_VIDEO_ANCILLARY_DID16_S334_EIA_608	= 0x6102,
+  GST_VIDEO_ANCILLARY_DID16_S2016_3_AFD_BAR	= 0x4105,
 } GstVideoAncillaryDID16;
 
 /* Closed Caption support */
