f53beb876c92916b4a1a89354f220d16d0653461
Fabio D'Urso <fabio.durso@m2dtech.com>
2019-04-26 16:25:17 +0200

    sctp: increase DEFAULT_NUMBER_OF_SCTP_STREAMS to 1024
    This change makes it possible to create more than just 5 webrtc
    data channels. The maximum number of data channels is exactly
    DEFAULT_NUMBER_OF_SCTP_STREAMS / 2, therefore the limit is now
    512.

--- ext/sctp/sctpassociation.c
+++ ext/sctp/sctpassociation.c
@@ -88,7 +88,7 @@ static guint signals[LAST_SIGNAL] = { 0 };
 
 static GParamSpec *properties[NUM_PROPERTIES];
 
-#define DEFAULT_NUMBER_OF_SCTP_STREAMS 10
+#define DEFAULT_NUMBER_OF_SCTP_STREAMS 1024
 #define DEFAULT_LOCAL_SCTP_PORT 0
 #define DEFAULT_REMOTE_SCTP_PORT 0
 
