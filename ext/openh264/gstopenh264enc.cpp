4e01eabb613626234819f74b1f4c3f5f6f17ff35
Víctor Manuel Jáquez Leal <vjaquez@igalia.com>
2019-05-08 13:35:25 +0200

    openh264enc: Fix compilation with openh264 v2.0
    As OpenH264 increased its version to 2.0 the guard for structure
    member is not valid.
    
    This patch will fix the compilation with gst-build and openh264.

--- ext/openh264/gstopenh264enc.cpp
+++ ext/openh264/gstopenh264enc.cpp
@@ -739,7 +739,7 @@ gst_openh264enc_set_format (GstVideoEncoder * encoder,
   enc_params.bEnableSceneChangeDetect = openh264enc->scene_change_detection;
   enc_params.bEnableFrameSkip = openh264enc->enable_frame_skip;
   enc_params.bEnableLongTermReference = 0;
-#if OPENH264_MINOR >= 4
+#if (OPENH264_MAJOR > 1 || (OPENH264_MAJOR == 1 && OPENH264_MINOR >= 4))
   enc_params.eSpsPpsIdStrategy = CONSTANT_ID;
 #else
   enc_params.bEnableSpsPpsIdAddition = 0;
