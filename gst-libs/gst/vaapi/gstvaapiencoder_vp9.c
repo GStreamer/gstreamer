GStreamer/gstreamer

commit 0afc81312359cd2f1756bd713efa48a161d6d8c2

libs: encoder: Consider vp9 profiles based on input format.
Only support GST_VAAPI_PROFILE_VP9_0 and GST_VAAPI_PROFILE_VP9_2 now.

Fix: #184

gst-libs/gst/vaapi/gstvaapiencoder_vp9.c
@@ -149,8 +149,20 @@ ensure_bitrate (GstVaapiEncoderVP9 * encoder)
 static GstVaapiEncoderStatus
 ensure_profile (GstVaapiEncoderVP9 * encoder)
 {
-  /* Always start from "simple" profile for maximum compatibility */
-  encoder->profile = GST_VAAPI_PROFILE_VP9_0;
+  /*
+     Profile Color | Depth Chroma | Subsampling
+     0             | 8 bit/sample | 4:2:0
+     1             | 8 bit        | 4:2:2, 4:4:4
+     2             | 10 or 12 bit | 4:2:0
+     3             | 10 or 12 bit | 4:2:2, 4:4:4     */
+  const GstVideoFormat format =
+      GST_VIDEO_INFO_FORMAT (GST_VAAPI_ENCODER_VIDEO_INFO (encoder));
+  if (format == GST_VIDEO_FORMAT_P010_10LE)
+    encoder->profile = GST_VAAPI_PROFILE_VP9_2;
+  else if (format == GST_VIDEO_FORMAT_NV12)
+    encoder->profile = GST_VAAPI_PROFILE_VP9_0;
+  else
+    return GST_VAAPI_ENCODER_STATUS_ERROR_UNSUPPORTED_PROFILE;
 
   /* Ensure bitrate if not set already */
   ensure_bitrate (encoder);

commit e4bb8f5895ddb93296ae76a6978cd69cda565e28

libs: encoder: vp9: set VP9_0 profile as default
Commit 0afc8131 introduced a regression and only NV12 format were
admitted, failing in any other valid color format.

This patch sets the profile to GST_VAAPI_PROFILE_VP9_0 by default.

gst-libs/gst/vaapi/gstvaapiencoder_vp9.c
@@ -159,10 +159,8 @@ ensure_profile (GstVaapiEncoderVP9 * encoder)
       GST_VIDEO_INFO_FORMAT (GST_VAAPI_ENCODER_VIDEO_INFO (encoder));
   if (format == GST_VIDEO_FORMAT_P010_10LE)
     encoder->profile = GST_VAAPI_PROFILE_VP9_2;
-  else if (format == GST_VIDEO_FORMAT_NV12)
-    encoder->profile = GST_VAAPI_PROFILE_VP9_0;
   else
-    return GST_VAAPI_ENCODER_STATUS_ERROR_UNSUPPORTED_PROFILE;
+    encoder->profile = GST_VAAPI_PROFILE_VP9_0;
 
   /* Ensure bitrate if not set already */
   ensure_bitrate (encoder);