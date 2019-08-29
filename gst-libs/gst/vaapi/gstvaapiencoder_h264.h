fe369fb523a73c0ba15acf7773b8d836ca2a5618
He Junyan <junyan.he@hotmail.com>
2019-08-29 14:59:12 +0800

    libs: encoder: delete get_default_properties of H264

--- gst-libs/gst/vaapi/gstvaapiencoder_h264.h
+++ gst-libs/gst/vaapi/gstvaapiencoder_h264.h
@@ -94,9 +94,6 @@ gst_vaapi_encoder_h264_get_type (void) G_GNUC_CONST;
 GstVaapiEncoder *
 gst_vaapi_encoder_h264_new (GstVaapiDisplay * display);
 
-GPtrArray *
-gst_vaapi_encoder_h264_get_default_properties (void);
-
 gboolean
 gst_vaapi_encoder_h264_set_max_profile (GstVaapiEncoderH264 * encoder,
     GstVaapiProfile profile);
