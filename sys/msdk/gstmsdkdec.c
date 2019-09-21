GStreamer/gstreamer

commit c05acf40a8916b5b848e344cae7cfb0534082ed0

msdkdec: set pointer to NULL to avoid freeing an used surface
The surface should be cached in the surface list when GST_FLOW_OK is
going to be returned

This fixes https://gitlab.freedesktop.org/gstreamer/gst-plugins-bad/issues/1051

sys/msdk/gstmsdkdec.c
@@ -1067,6 +1067,7 @@ gst_msdkdec_handle_frame (GstVideoDecoder * decoder, GstVideoCodecFrame * frame)
 
       if (bitstream.DataLength == 0) {
         flow = GST_FLOW_OK;
+        surface = NULL;
         break;
       }
     } else if (status == MFX_ERR_MORE_DATA) {

commit c689c94458f0889871740715bff80aaf4852509a

msdkdec: release the surface if this surface is not in use
If the surface is not in use, we may release it even if GST_FLOW_OK is going
to be returned, which may avoid the issue of failing to get surface
available

This fixes the regression caused by commit c05acf4

sys/msdk/gstmsdkdec.c
@@ -1067,7 +1067,11 @@ gst_msdkdec_handle_frame (GstVideoDecoder * decoder, GstVideoCodecFrame * frame)
 
       if (bitstream.DataLength == 0) {
         flow = GST_FLOW_OK;
-        surface = NULL;
+
+        /* Don't release it if the current surface is in use */
+        if (surface && task->surface == surface->surface)
+          surface = NULL;
+
         break;
       }
     } else if (status == MFX_ERR_MORE_DATA) {