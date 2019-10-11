851682629ee543a0058dd2fde6297271d05fc876
Vivia Nikolaidou <vivia@ahiru.eu>
2019-10-11 16:19:26 +0300

    videoparsers: Disable gst_base_parse_set_infer_ts
    From the documentation of gst_base_parse_set_infer_ts, it should be
    disabled for non-audio data. Currently just disabling for all video
    parsers that have reordered data: h264, h265, mpeg, mpeg4, vc1. Was
    already disabled in h263.

--- gst/videoparsers/gstvc1parse.c
+++ gst/videoparsers/gstvc1parse.c
@@ -251,6 +251,10 @@ gst_vc1_parse_init (GstVC1Parse * vc1parse)
   gst_vc1_parse_reset (vc1parse);
   GST_PAD_SET_ACCEPT_INTERSECT (GST_BASE_PARSE_SINK_PAD (vc1parse));
   GST_PAD_SET_ACCEPT_TEMPLATE (GST_BASE_PARSE_SINK_PAD (vc1parse));
+
+  /* We have reordered frames - don't interpolate PTS */
+  gst_base_parse_set_pts_interpolation (GST_BASE_PARSE (vc1parse), FALSE);
+  gst_base_parse_set_infer_ts (GST_BASE_PARSE (vc1parse), FALSE);
 }
 
 static void
