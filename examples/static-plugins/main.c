7fa292406e7d15c7716ef4883b02873d922d96f4
Xavier Claessens <xavier.claessens@collabora.com>
2019-12-16 16:05:30 -0500

    Add example using gstreamer-full
    It will be used by the CI to verify we can build it.

---
+++ examples/static-plugins/main.c
@@ -0,0 +1,17 @@
+#include <gst/gst.h>
+#include <gst/gstinitstaticplugins.h>
+
+int
+main (int argc, char *argv[])
+{
+  GstElement *e;
+
+  gst_init (&argc, &argv);
+  gst_init_static_plugins ();
+
+  e = gst_element_factory_make ("identity", NULL);
+  g_assert_nonnull (e);
+  g_object_unref (e);
+
+  return 0;
+}
