8a85aa6eba87773c154087ee0ff6e80ea5974659
Guillaume Desmottes <guillaume.desmottes@collabora.com>
2019-12-23 11:52:20 +0530

    docker: export RUSTUP/CARGO_HOME in container
    The rust binaries (rustc) needs those variables to be defined to work
    properly.

--- docker/fedora/prepare.sh
+++ docker/fedora/prepare.sh
@@ -202,9 +202,6 @@ RUSTUP_VERSION=1.21.0
 RUST_VERSION=1.40.0
 RUST_ARCH="x86_64-unknown-linux-gnu"
 
-# rustup-init uses those variables as install paths
-export RUSTUP_HOME=/usr/local/rustup
-export CARGO_HOME=/usr/local/cargo
 dnf install -y wget
 RUSTUP_URL=https://static.rust-lang.org/rustup/archive/$RUSTUP_VERSION/$RUST_ARCH/rustup-init
 wget $RUSTUP_URL
