0320050e063004de972ba88de9d3b2bd5c9138f9
Jordan Petridis <jordan@centricular.com>
2020-01-21 20:22:56 +0200

    docker/windows: install cmake with choco as well

--- docker/windows/install_toolchain.ps1
+++ docker/windows/install_toolchain.ps1
@@ -1,7 +1,6 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;
 
 $msvc_2017_url = 'https://aka.ms/vs/15/release/vs_buildtools.exe'
-$cmake_url = 'https://github.com/Kitware/CMake/releases/download/v3.16.1/cmake-3.16.1-win64-x64.msi'
 $msys2_url = 'https://netcologne.dl.sourceforge.net/project/msys2/Base/x86_64/msys2-base-x86_64-20190524.tar.xz'
 $msys_mingw_get_url = 'https://dotsrc.dl.osdn.net/osdn/mingw/68260/mingw-get-0.6.3-mingw32-pre-20170905-1-bin.tar.xz'
 
@@ -9,11 +8,6 @@ Write-Host "Installing VisualStudio"
 Invoke-WebRequest -Uri $msvc_2017_url -OutFile C:\vs_buildtools.exe
 Start-Process C:\vs_buildtools.exe -ArgumentList '--quiet --wait --norestart --nocache --installPath C:\BuildTools --add Microsoft.VisualStudio.Workload.VCTools --includeRecommended' -Wait
 Remove-Item C:\vs_buildtools.exe -Force
-
-Write-Host "Installing CMake for Cerbero"
-Invoke-WebRequest -Uri $cmake_url -OutFile C:\cmake-x64.msi
-Start-Process msiexec -ArgumentList '-i C:\cmake-x64.msi -quiet -norestart ADD_CMAKE_TO_PATH=System' -Wait
-Remove-Item C:\cmake-x64.msi -Force
 
 Write-Host "Downloading and extracting mingw-get for MSYS"
 Invoke-WebRequest -Uri $msys_mingw_get_url -OutFile C:\mingw-get.tar.xz
