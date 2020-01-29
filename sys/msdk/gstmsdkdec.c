5d160a8968bc0192b9dd65ad6bcad2284ba7fa78
Seungha Yang <seungha.yang@navercorp.com>
2020-01-29 14:10:06 +0900

    msdkdec: Fix GstMsdkContext leak

--- sys/msdk/gstmsdkdec.c
+++ sys/msdk/gstmsdkdec.c
@@ -1635,6 +1635,8 @@ gst_msdkdec_dispose (GObject * object)
   GstMsdkDec *thiz = GST_MSDKDEC (object);
 
   g_clear_object (&thiz->adapter);
+  gst_clear_object (&thiz->context);
+  gst_clear_object (&thiz->old_context);
 
   G_OBJECT_CLASS (parent_class)->dispose (object);
 }
