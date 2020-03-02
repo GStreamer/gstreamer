af9e42d13048abedd22c034e13abc848179df72b
Nirbheek Chauhan <nirbheek@centricular.com>
2020-03-02 16:38:17 +0530

    Cerbero has moved from gnutls+openssl to only openssl

--- examples/tutorials/android/android-tutorial-5/jni/Android.mk
+++ examples/tutorials/android/android-tutorial-5/jni/Android.mk
@@ -29,6 +29,6 @@ endif
 GSTREAMER_NDK_BUILD_PATH  := $(GSTREAMER_ROOT)/share/gst-android/ndk-build/
 include $(GSTREAMER_NDK_BUILD_PATH)/plugins.mk
 GSTREAMER_PLUGINS         := $(GSTREAMER_PLUGINS_CORE) $(GSTREAMER_PLUGINS_PLAYBACK) $(GSTREAMER_PLUGINS_CODECS) $(GSTREAMER_PLUGINS_NET) $(GSTREAMER_PLUGINS_SYS)
-G_IO_MODULES              := gnutls
+G_IO_MODULES              := openssl
 GSTREAMER_EXTRA_DEPS      := gstreamer-video-1.0
 include $(GSTREAMER_NDK_BUILD_PATH)/gstreamer-1.0.mk
