38719d0aefd152c3289deacea5ec2521ecdf5b79
Matthew Waters <matthew@centricular.com>
2020-04-24 14:16:23 +1000

    docker: remove unused Dockerfiles
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gst-ci/-/merge_requests/286>

--- docker/build-base-images/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-echo Building gstreamer/build-base-fedora:latest
-
-docker build -t gstreamer/build-base-fedora:latest . -f Dockerfile-fedora
-
-echo Building gstreamer/build-base-ubuntu:latest
-
-docker build -t gstreamer/build-base-ubuntu:latest . -f Dockerfile-ubuntu
--- docker/runtime-images/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-echo Building gstreamer/base-fedora:latest
-
-docker build -t gstreamer/base-fedora:latest . -f Dockerfile-fedora
-
-echo Building gstreamer/base-ubuntu:latest
-
-docker build -t gstreamer/base-ubuntu:latest . -f Dockerfile-ubuntu
-
