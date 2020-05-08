be3745cb925f9d9004c7dcba6b0e160a879a2807
Stéphane Cerveau <scerveau@collabora.com>
2020-05-08 11:03:52 +0200

    video-hdr: fix typo
    fix typo in video_hdr_ascii_string_to_unsigned
    method name.
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gst-plugins-base/-/merge_requests/675>

--- gst-libs/gst/video/video-hdr.c
+++ gst-libs/gst/video/video-hdr.c
@@ -42,9 +42,9 @@
 /* g_ascii_string_to_unsigned is available since 2.54. Get rid of this wrapper
  * when we bump the version in 1.18 */
 #if !GLIB_CHECK_VERSION(2,54,0)
-#define g_ascii_string_to_unsigned vidoe_hdr_ascii_string_to_unsigned
+#define g_ascii_string_to_unsigned video_hdr_ascii_string_to_unsigned
 static gboolean
-vidoe_hdr_ascii_string_to_unsigned (const gchar * str, guint base, guint64 min,
+video_hdr_ascii_string_to_unsigned (const gchar * str, guint base, guint64 min,
     guint64 max, guint64 * out_num, GError ** error)
 {
   gchar *endptr = NULL;
