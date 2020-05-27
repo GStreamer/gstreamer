9aa7705c95be8eea8e3d03efe0535f754851a5a9
Thibault Saunier <tsaunier@igalia.com>
2020-05-27 19:25:42 -0400

    scripts: Rework the release tarball layout
    Adding everything to a `GStreamer-doc-%(project_version)` folder.
    Printing release output directory
    
    Fixes https://gitlab.freedesktop.org/gstreamer/gst-docs/-/issues/35
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gst-docs/-/merge_requests/92>

--- scripts/release.py
+++ scripts/release.py
@@ -6,12 +6,13 @@ import tarfile
 
 if __name__ == "__main__":
     files = sys.argv[1]
-    outname = sys.argv[2]
+    release_name = sys.argv[2]
     readme = sys.argv[3]
+    outname = release_name + '.tar.xz'
 
+    print("Generating %s" % os.path.realpath(os.path.join(os.path.curdir, outname)), file=sys.stderr)
     tar = tarfile.open(outname, 'w:xz')
-    os.chdir(files)
-    tar.add(os.path.curdir)
+    tar.add(files, release_name)
     os.chdir(os.path.dirname(readme))
-    tar.add(os.path.basename(readme))
+    tar.add(os.path.basename(readme), os.path.join(release_name, os.path.basename(readme)))
     tar.close()
