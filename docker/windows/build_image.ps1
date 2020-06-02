869fa72595ffd8a92dc8795b6202acb04ea85eed
Jordan Petridis <jordan@centricular.com>
2020-06-02 11:33:47 +0300

    docker/windows: update msys snapshot
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gst-ci/-/merge_requests/297>

--- docker/windows/build_image.ps1
+++ docker/windows/build_image.ps1
@@ -1,5 +1,6 @@
 $env:DEFAULT_BRANCH='master'
-$env:tag ="registry.freedesktop.org/gstreamer/gst-ci/amd64/windows:v10-$env:DEFAULT_BRANCH"
+$env:VERSION='v11'
+$env:tag ="registry.freedesktop.org/gstreamer/gst-ci/amd64/windows:$env:VERSION-$env:DEFAULT_BRANCH"
 echo "Building $env:tag"
 docker build --build-arg DEFAULT_BRANCH=$env:DEFAULT_BRANCH -f Dockerfile -t $env:tag .
 # docker push $tag
