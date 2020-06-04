2408fbe92d3c5afeb51563af6f0868a54e046fed
cketti <ck@cketti.de>
2020-06-04 13:33:59 +0200

    curlsmtpsink: Use correct email date format
    See https://www.rfc-editor.org/rfc/rfc5322.html#section-3.3
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gst-plugins-bad/-/merge_requests/1317>

--- ext/curl/gstcurlsmtpsink.c
+++ ext/curl/gstcurlsmtpsink.c
@@ -635,7 +635,7 @@ gst_curl_smtp_sink_set_transfer_options_unlocked (GstCurlBaseSink * bcsink)
 
   /* time */
   date = g_date_time_new_now_local ();
-  date_str = g_date_time_format (date, "%a %b %e %H:%M:%S %Y %z");
+  date_str = g_date_time_format (date, "%a, %e %b %Y %H:%M:%S %z");
   g_date_time_unref (date);
 
   /* recipient, sender and subject are all UTF-8 strings, which are additionally
