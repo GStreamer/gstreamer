7e598e9184bf68d1b94b8c0b99c90e2657a78441
Mathieu Duponchelle <mathieu@centricular.com>
2020-06-06 00:41:51 +0200

    plugins: uddate gst_type_mark_as_plugin_api() calls

--- gst/rtsp-sink/gstrtspclientsink.c
+++ gst/rtsp-sink/gstrtspclientsink.c
@@ -821,8 +821,8 @@ gst_rtsp_client_sink_class_init (GstRTSPClientSinkClass * klass)
 
   gstbin_class->handle_message = gst_rtsp_client_sink_handle_message;
 
-  gst_type_mark_as_plugin_api (GST_TYPE_RTSP_CLIENT_SINK_PAD);
-  gst_type_mark_as_plugin_api (GST_TYPE_RTSP_CLIENT_SINK_NTP_TIME_SOURCE);
+  gst_type_mark_as_plugin_api (GST_TYPE_RTSP_CLIENT_SINK_PAD, 0);
+  gst_type_mark_as_plugin_api (GST_TYPE_RTSP_CLIENT_SINK_NTP_TIME_SOURCE, 0);
 }
 
 static void
