d77de13ca6028d0011f5a2a285a2a6f751b5d8da
Haihua Hu <jared.hu@nxp.com>
2020-06-09 10:53:17 +0800

    waylandsink: add wl_registry.global_remove listener
    when hotplug display, wayland client will call this listener
    to notify client do clean up. Temporarily set a dummy function
    here to avoid app abort
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gst-plugins-bad/-/merge_requests/1327>

--- ext/wayland/wldisplay.c
+++ ext/wayland/wldisplay.c
@@ -234,8 +234,16 @@ registry_handle_global (void *data, struct wl_registry *registry,
   }
 }
 
+static void
+registry_handle_global_remove (void *data, struct wl_registry *registry,
+    uint32_t name)
+{
+  /* temporarily do nothing */
+}
+
 static const struct wl_registry_listener registry_listener = {
-  registry_handle_global
+  registry_handle_global,
+  registry_handle_global_remove
 };
 
 static gpointer
