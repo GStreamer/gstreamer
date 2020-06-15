GStreamer/gstreamer

commit a7732a68e8bc6b4ba15629c652056c240c624ff0

Dynamically reconfigure pipeline in PLAY based on transports
The initial pipeline does not contain specific transport
elements. The receiver and the sender parts are added
after PLAY.
If the media is shared, the streams are dynamically
reconfigured after each PLAY.

https://bugzilla.gnome.org/show_bug.cgi?id=788340

gst/rtsp-server/rtsp-media.c
@@ -2115,6 +2115,21 @@ media_streams_set_blocked (GstRTSPMedia * media, gboolean blocked)
 }
 
 static void
+stream_unblock (GstRTSPStream * stream, GstRTSPMedia * media)
+{
+  gst_rtsp_stream_unblock_linked (stream);
+}
+
+static void
+media_unblock_linked (GstRTSPMedia * media)
+{
+  GstRTSPMediaPrivate *priv = media->priv;
+
+  GST_DEBUG ("media %p unblocking linked streams", media);
+  g_ptr_array_foreach (priv->streams, (GFunc) stream_unblock, media);
+}
+
+static void
 gst_rtsp_media_set_status (GstRTSPMedia * media, GstRTSPMediaStatus status)
 {
   GstRTSPMediaPrivate *priv = media->priv;
@@ -2526,8 +2541,6 @@ default_handle_message (GstRTSPMedia * media, GstMessage * message)
         GST_INFO ("%p: ignoring ASYNC_DONE", media);
       } else {
         GST_INFO ("%p: got ASYNC_DONE", media);
-        collect_media_stats (media);
-
         if (priv->status == GST_RTSP_MEDIA_STATUS_PREPARING)
           gst_rtsp_media_set_status (media, GST_RTSP_MEDIA_STATUS_PREPARED);
       }
@@ -2642,6 +2655,9 @@ pad_added_cb (GstElement * element, GstPad * pad, GstRTSPMedia * media)
     GST_WARNING ("failed to join bin element");
   }
 
+  if (priv->blocked)
+    gst_rtsp_stream_set_blocked (stream, TRUE);
+
   priv->adding = FALSE;
   g_rec_mutex_unlock (&priv->state_lock);
 
@@ -2720,7 +2736,9 @@ start_preroll (GstRTSPMedia * media)
   GstStateChangeReturn ret;
 
   GST_INFO ("setting pipeline to PAUSED for media %p", media);
-  /* first go to PAUSED */
+
+  /* start blocked since it is possible that there are no sink elements yet */
+  media_streams_set_blocked (media, TRUE);
   ret = set_target_state (media, GST_STATE_PAUSED, TRUE);
 
   switch (ret) {
@@ -2737,10 +2755,7 @@ start_preroll (GstRTSPMedia * media)
        * seeking query in preroll instead */
       priv->seekable = -1;
       priv->is_live = TRUE;
-      if (!(priv->transport_mode & GST_RTSP_TRANSPORT_MODE_RECORD)) {
-        /* start blocked  to make sure nothing goes to the sink */
-        media_streams_set_blocked (media, TRUE);
-      }
+
       ret = set_state (media, GST_STATE_PLAYING);
       if (ret == GST_STATE_CHANGE_FAILURE)
         goto state_failed;
@@ -3100,6 +3115,8 @@ finish_unprepare (GstRTSPMedia * media)
   set_state (media, GST_STATE_NULL);
   g_rec_mutex_lock (&priv->state_lock);
 
+  media_streams_set_blocked (media, FALSE);
+
   if (priv->status != GST_RTSP_MEDIA_STATUS_UNPREPARING)
     return;
 
@@ -3209,8 +3226,6 @@ gst_rtsp_media_unprepare (GstRTSPMedia * media)
     goto is_busy;
 
   GST_INFO ("unprepare media %p", media);
-  if (priv->blocked)
-    media_streams_set_blocked (media, FALSE);
   set_target_state (media, GST_STATE_NULL, FALSE);
   success = TRUE;
 
@@ -3563,7 +3578,6 @@ default_suspend (GstRTSPMedia * media)
 {
   GstRTSPMediaPrivate *priv = media->priv;
   GstStateChangeReturn ret;
-  gboolean unblock = FALSE;
 
   switch (priv->suspend_mode) {
     case GST_RTSP_SUSPEND_MODE_NONE:
@@ -3574,7 +3588,6 @@ default_suspend (GstRTSPMedia * media)
       ret = set_target_state (media, GST_STATE_PAUSED, TRUE);
       if (ret == GST_STATE_CHANGE_FAILURE)
         goto state_failed;
-      unblock = TRUE;
       break;
     case GST_RTSP_SUSPEND_MODE_RESET:
       GST_DEBUG ("media %p suspend to NULL", media);
@@ -3587,15 +3600,10 @@ default_suspend (GstRTSPMedia * media)
        * is actually from NULL to PLAY will create a new sequence
        * number. */
       g_ptr_array_foreach (priv->streams, (GFunc) do_set_seqnum, NULL);
-      unblock = TRUE;
       break;
     default:
       break;
   }
-
-  /* let the streams do the state changes freely, if any */
-  if (unblock)
-    media_streams_set_blocked (media, FALSE);
 
   return TRUE;
 
@@ -3674,7 +3682,19 @@ default_unsuspend (GstRTSPMedia * media)
 
   switch (priv->suspend_mode) {
     case GST_RTSP_SUSPEND_MODE_NONE:
-      gst_rtsp_media_set_status (media, GST_RTSP_MEDIA_STATUS_PREPARED);
+      if ((priv->transport_mode & GST_RTSP_TRANSPORT_MODE_RECORD))
+        break;
+      gst_rtsp_media_set_status (media, GST_RTSP_MEDIA_STATUS_PREPARING);
+      /* at this point the media pipeline has been updated and contain all
+       * specific transport parts: all active streams contain at least one sink
+       * element and it's safe to unblock any blocked streams that are active */
+      media_unblock_linked (media);
+      g_rec_mutex_unlock (&priv->state_lock);
+      if (gst_rtsp_media_get_status (media) == GST_RTSP_MEDIA_STATUS_ERROR) {
+        g_rec_mutex_lock (&priv->state_lock);
+        goto preroll_failed;
+      }
+      g_rec_mutex_lock (&priv->state_lock);
       break;
     case GST_RTSP_SUSPEND_MODE_PAUSE:
       gst_rtsp_media_set_status (media, GST_RTSP_MEDIA_STATUS_PREPARED);
@@ -3682,6 +3702,10 @@ default_unsuspend (GstRTSPMedia * media)
     case GST_RTSP_SUSPEND_MODE_RESET:
     {
       gst_rtsp_media_set_status (media, GST_RTSP_MEDIA_STATUS_PREPARING);
+      /* at this point the media pipeline has been updated and contain all
+       * specific transport parts: all active streams contain at least one sink
+       * element and it's safe to unblock any blocked streams that are active */
+      media_unblock_linked (media);
       if (!start_preroll (media))
         goto start_failed;
 
@@ -3771,7 +3795,7 @@ media_set_pipeline_state_locked (GstRTSPMedia * media, GstState state)
     } else {
       if (state == GST_STATE_PLAYING)
         /* make sure pads are not blocking anymore when going to PLAYING */
-        media_streams_set_blocked (media, FALSE);
+        media_unblock_linked (media);
 
       set_state (media, state);
 
@@ -4006,4 +4030,53 @@ gst_rtsp_media_seekable (GstRTSPMedia * media)
   /* Currently we are not able to seek on live streams,
    * and no stream is seekable only to the beginning */
   return media->priv->seekable;
+}
+
+/**
+ * gst_rtsp_media_complete_pipeline:
+ * @media: a #GstRTSPMedia
+ * @transports: a list of #GstRTSPTransport
+ *
+ * Add a receiver and sender parts to the pipeline based on the transport from
+ * SETUP.
+ *
+ * Returns: %TRUE if the media pipeline has been sucessfully updated.
+ */
+gboolean
+gst_rtsp_media_complete_pipeline (GstRTSPMedia * media, GPtrArray * transports)
+{
+  GstRTSPMediaPrivate *priv;
+  guint i;
+
+  g_return_val_if_fail (GST_IS_RTSP_MEDIA (media), FALSE);
+  g_return_val_if_fail (transports, FALSE);
+
+  GST_DEBUG_OBJECT (media, "complete pipeline");
+
+  priv = media->priv;
+
+  g_mutex_lock (&priv->lock);
+  for (i = 0; i < priv->streams->len; i++) {
+    GstRTSPStreamTransport *transport;
+    GstRTSPStream *stream;
+    const GstRTSPTransport *rtsp_transport;
+
+    transport = g_ptr_array_index (transports, i);
+    if (!transport)
+      continue;
+
+    stream = gst_rtsp_stream_transport_get_stream (transport);
+    if (!stream)
+      continue;
+
+    rtsp_transport = gst_rtsp_stream_transport_get_transport (transport);
+
+    if (!gst_rtsp_stream_complete_stream (stream, rtsp_transport)) {
+      g_mutex_unlock (&priv->lock);
+      return FALSE;
+    }
+  }
+  g_mutex_unlock (&priv->lock);
+
+  return TRUE;
 }

gst/rtsp-server/rtsp-client.c
@@ -1518,6 +1518,7 @@ handle_play_request (GstRTSPClient * client, GstRTSPContext * ctx)
   gint matched;
   gchar *seek_style = NULL;
   GstRTSPStatusCode sig_result;
+  GPtrArray *transports;
 
   if (!(session = ctx->session))
     goto no_session;
@@ -1555,6 +1556,14 @@ handle_play_request (GstRTSPClient * client, GstRTSPContext * ctx)
   rtspstate = gst_rtsp_session_media_get_rtsp_state (sessmedia);
   if (rtspstate != GST_RTSP_STATE_PLAYING && rtspstate != GST_RTSP_STATE_READY)
     goto invalid_state;
+
+  /* update the pipeline */
+  transports = gst_rtsp_session_media_get_transports (sessmedia);
+  if (!gst_rtsp_media_complete_pipeline (media, transports)) {
+    g_ptr_array_unref (transports);
+    goto pipeline_error;
+  }
+  g_ptr_array_unref (transports);
 
   /* in play we first unsuspend, media could be suspended from SDP or PAUSED */
   if (!gst_rtsp_media_unsuspend (media))
@@ -1662,6 +1671,13 @@ sig_failed:
 invalid_state:
   {
     GST_ERROR ("client %p: not PLAYING or READY", client);
+    send_generic_response (client, GST_RTSP_STS_METHOD_NOT_VALID_IN_THIS_STATE,
+        ctx);
+    return FALSE;
+  }
+pipeline_error:
+  {
+    GST_ERROR ("client %p: failed to configure the pipeline", client);
     send_generic_response (client, GST_RTSP_STS_METHOD_NOT_VALID_IN_THIS_STATE,
         ctx);
     return FALSE;
@@ -1784,39 +1800,52 @@ default_configure_client_transport (GstRTSPClient * client,
   GstRTSPClientPrivate *priv = client->priv;
 
   /* we have a valid transport now, set the destination of the client. */
-  if (ct->lower_transport == GST_RTSP_LOWER_TRANS_UDP_MCAST) {
-    gboolean use_client_settings;
+  if (ct->lower_transport == GST_RTSP_LOWER_TRANS_UDP_MCAST ||
+      ct->lower_transport == GST_RTSP_LOWER_TRANS_UDP) {
 
-    use_client_settings =
-        gst_rtsp_auth_check (GST_RTSP_AUTH_CHECK_TRANSPORT_CLIENT_SETTINGS);
+    /* allocate UDP ports */
+    GSocketFamily family;
+    gboolean use_client_settings = FALSE;
 
-    if (ct->destination && use_client_settings) {
-      GstRTSPAddress *addr;
+    family = priv->is_ipv6 ? G_SOCKET_FAMILY_IPV6 : G_SOCKET_FAMILY_IPV4;
+    if ((ct->lower_transport == GST_RTSP_LOWER_TRANS_UDP_MCAST) &&
+        gst_rtsp_auth_check (GST_RTSP_AUTH_CHECK_TRANSPORT_CLIENT_SETTINGS) &&
+        (ct->destination != NULL))
+      use_client_settings = TRUE;
 
-      addr = gst_rtsp_stream_reserve_address (ctx->stream, ct->destination,
-          ct->port.min, ct->port.max - ct->port.min + 1, ct->ttl);
+    if (!gst_rtsp_stream_allocate_udp_sockets (ctx->stream, family, ct,
+          use_client_settings))
+      goto error_allocating_ports;
 
-      if (addr == NULL)
-        goto no_address;
+    if (ct->lower_transport == GST_RTSP_LOWER_TRANS_UDP_MCAST) {
+      GstRTSPAddress *addr = NULL;
 
-      gst_rtsp_address_free (addr);
+      if (use_client_settings) {
+        /* the address has been successfully allocated, let's check if it's
+         * the one requested by the client */
+        addr = gst_rtsp_stream_reserve_address (ctx->stream, ct->destination,
+            ct->port.min, ct->port.max - ct->port.min + 1, ct->ttl);
+
+        if (addr == NULL)
+          goto no_address;
+      } else {
+        g_free (ct->destination);
+        addr = gst_rtsp_stream_get_multicast_address (ctx->stream, family);
+        if (addr == NULL)
+          goto no_address;
+        ct->destination = g_strdup (addr->address);
+        ct->port.min = addr->port;
+        ct->port.max = addr->port + addr->n_ports - 1;
+        ct->ttl = addr->ttl;
+
+        gst_rtsp_address_free (addr);
+      }
     } else {
-      GstRTSPAddress *addr;
-      GSocketFamily family;
+      GstRTSPUrl *url;
 
-      family = priv->is_ipv6 ? G_SOCKET_FAMILY_IPV6 : G_SOCKET_FAMILY_IPV4;
-
-      addr = gst_rtsp_stream_get_multicast_address (ctx->stream, family);
-      if (addr == NULL)
-        goto no_address;
-
+      url = gst_rtsp_connection_get_url (priv->connection);
       g_free (ct->destination);
-      ct->destination = g_strdup (addr->address);
-      ct->port.min = addr->port;
-      ct->port.max = addr->port + addr->n_ports - 1;
-      ct->ttl = addr->ttl;
-
-      gst_rtsp_address_free (addr);
+      ct->destination = g_strdup (url->host);
     }
   } else {
     GstRTSPUrl *url;
@@ -1863,9 +1892,14 @@ default_configure_client_transport (GstRTSPClient * client,
   return TRUE;
 
   /* ERRORS */
+error_allocating_ports:
+  {
+    GST_ERROR_OBJECT (client, "Failed to allocate UDP ports");
+    return FALSE;
+  }
 no_address:
   {
-    GST_ERROR_OBJECT (client, "failed to acquire address for stream");
+    GST_ERROR_OBJECT (client, "Failed to acquire address for stream");
     return FALSE;
   }
 }
@@ -3036,6 +3070,7 @@ handle_record_request (GstRTSPClient * client, GstRTSPContext * ctx)
   gchar *path;
   gint matched;
   GstRTSPStatusCode sig_result;
+  GPtrArray *transports;
 
   if (!(session = ctx->session))
     goto no_session;
@@ -3074,7 +3109,15 @@ handle_record_request (GstRTSPClient * client, GstRTSPContext * ctx)
   if (rtspstate != GST_RTSP_STATE_PLAYING && rtspstate != GST_RTSP_STATE_READY)
     goto invalid_state;
 
-  /* in play we first unsuspend, media could be suspended from SDP or PAUSED */
+  /* update the pipeline */
+  transports = gst_rtsp_session_media_get_transports (sessmedia);
+  if (!gst_rtsp_media_complete_pipeline (media, transports)) {
+    g_ptr_array_unref (transports);
+    goto pipeline_error;
+  }
+  g_ptr_array_unref (transports);
+
+  /* in record we first unsuspend, media could be suspended from SDP or PAUSED */
   if (!gst_rtsp_media_unsuspend (media))
     goto unsuspend_failed;
 
@@ -3136,6 +3179,13 @@ unsupported_mode:
 invalid_state:
   {
     GST_ERROR ("client %p: not PLAYING or READY", client);
+    send_generic_response (client, GST_RTSP_STS_METHOD_NOT_VALID_IN_THIS_STATE,
+        ctx);
+    return FALSE;
+  }
+pipeline_error:
+  {
+    GST_ERROR ("client %p: failed to configure the pipeline", client);
     send_generic_response (client, GST_RTSP_STS_METHOD_NOT_VALID_IN_THIS_STATE,
         ctx);
     return FALSE;

gst/rtsp-server/rtsp-media.h
@@ -382,6 +382,9 @@ GST_EXPORT
 void                  gst_rtsp_media_set_pipeline_state (GstRTSPMedia * media,
                                                          GstState state);
 
+GST_EXPORT
+gboolean              gst_rtsp_media_complete_pipeline (GstRTSPMedia * media, GPtrArray * transports);
+
 #ifdef G_DEFINE_AUTOPTR_CLEANUP_FUNC
 G_DEFINE_AUTOPTR_CLEANUP_FUNC(GstRTSPMedia, gst_object_unref)
 #endif

gst/rtsp-server/rtsp-stream.h
@@ -160,6 +160,10 @@ gboolean          gst_rtsp_stream_set_blocked      (GstRTSPStream * stream,
 GST_EXPORT
 gboolean          gst_rtsp_stream_is_blocking      (GstRTSPStream * stream);
 
+
+GST_EXPORT
+gboolean          gst_rtsp_stream_unblock_linked   (GstRTSPStream * stream);
+
 GST_EXPORT
 void              gst_rtsp_stream_set_client_side (GstRTSPStream *stream, gboolean client_side);
 
@@ -272,13 +276,16 @@ GstElement *      gst_rtsp_stream_request_aux_sender         (GstRTSPStream * st
 
 GST_EXPORT
 gboolean          gst_rtsp_stream_allocate_udp_sockets       (GstRTSPStream * stream, GSocketFamily family,
-                                                              GstRTSPTransport *transport, gboolean use_client_setttings);
+                                                              GstRTSPTransport *transport, gboolean use_client_settings);
 
 GST_EXPORT
 void                    gst_rtsp_stream_set_publish_clock_mode (GstRTSPStream * stream, GstRTSPPublishClockMode mode);
 
 GST_EXPORT
 GstRTSPPublishClockMode gst_rtsp_stream_get_publish_clock_mode (GstRTSPStream * stream);
+
+GST_EXPORT
+gboolean          gst_rtsp_stream_complete_stream (GstRTSPStream * stream, const GstRTSPTransport * transport);
 
 /**
  * GstRTSPStreamTransportFilterFunc:

tests/check/gst/client.c
@@ -793,7 +793,6 @@ GST_START_TEST (test_client_multicast_invalid_transport_specific)
   g_object_unref (session_pool);
 
 
-
   /* simple SETUP with a valid URI and multicast, but an invalid prt */
   fail_unless (gst_rtsp_message_init_request (&request, GST_RTSP_SETUP,
           "rtsp://localhost/test/stream=0") == GST_RTSP_OK);
@@ -813,7 +812,6 @@ GST_START_TEST (test_client_multicast_invalid_transport_specific)
   g_object_unref (session_pool);
 
 
-
   /* simple SETUP with a valid URI and multicast, but an invalid ttl */
   fail_unless (gst_rtsp_message_init_request (&request, GST_RTSP_SETUP,
           "rtsp://localhost/test/stream=0") == GST_RTSP_OK);
@@ -831,7 +829,6 @@ GST_START_TEST (test_client_multicast_invalid_transport_specific)
   fail_unless (session_pool != NULL);
   fail_unless (gst_rtsp_session_pool_get_n_sessions (session_pool) == 0);
   g_object_unref (session_pool);
-
 
   teardown_client (client);
   g_object_unref (ctx.auth);

tests/check/gst/media.c
@@ -21,7 +21,73 @@
 
 #include <rtsp-media-factory.h>
 
-GST_START_TEST (test_launch)
+GST_START_TEST (test_media_seek)
+{
+  GstRTSPMediaFactory *factory;
+  GstRTSPMedia *media;
+  GstRTSPUrl *url;
+  GstRTSPStream *stream;
+  GstRTSPTimeRange *range;
+  gchar *str;
+  GstRTSPThreadPool *pool;
+  GstRTSPThread *thread;
+  GstRTSPTransport *transport;
+
+  factory = gst_rtsp_media_factory_new ();
+  fail_if (gst_rtsp_media_factory_is_shared (factory));
+  fail_unless (gst_rtsp_url_parse ("rtsp://localhost:8554/test",
+          &url) == GST_RTSP_OK);
+
+  gst_rtsp_media_factory_set_launch (factory,
+      "( videotestsrc ! rtpvrawpay pt=96 name=pay0 )");
+
+  media = gst_rtsp_media_factory_construct (factory, url);
+  fail_unless (GST_IS_RTSP_MEDIA (media));
+
+  fail_unless (gst_rtsp_media_n_streams (media) == 1);
+
+  stream = gst_rtsp_media_get_stream (media, 0);
+  fail_unless (stream != NULL);
+
+  pool = gst_rtsp_thread_pool_new ();
+  thread = gst_rtsp_thread_pool_get_thread (pool,
+      GST_RTSP_THREAD_TYPE_MEDIA, NULL);
+
+  fail_unless (gst_rtsp_media_prepare (media, thread));
+
+  /* define transport */
+  fail_unless (gst_rtsp_transport_new (&transport) == GST_RTSP_OK);
+  transport->lower_transport = GST_RTSP_LOWER_TRANS_TCP;
+
+  fail_unless (gst_rtsp_stream_complete_stream (stream, transport));
+
+  fail_unless (gst_rtsp_transport_free (transport) == GST_RTSP_OK);
+  fail_unless (gst_rtsp_range_parse ("npt=5.0-", &range) == GST_RTSP_OK);
+
+  /* the media is seekable now */
+  fail_unless (gst_rtsp_media_seek (media, range));
+
+  str = gst_rtsp_media_get_range_string (media, FALSE, GST_RTSP_RANGE_NPT);
+  fail_unless (g_str_equal (str, "npt=5-"));
+
+  gst_rtsp_range_free (range);
+  g_free (str);
+
+  fail_unless (gst_rtsp_media_unprepare (media));
+  g_object_unref (media);
+
+  gst_rtsp_url_free (url);
+  g_object_unref (factory);
+
+  g_object_unref (pool);
+
+  gst_rtsp_thread_pool_cleanup ();
+}
+
+GST_END_TEST;
+
+
+GST_START_TEST (test_media_seek_no_sinks)
 {
   GstRTSPMediaFactory *factory;
   GstRTSPMedia *media;
@@ -70,15 +136,8 @@ GST_START_TEST (test_launch)
   fail_unless (g_str_equal (str, "npt=0-"));
   g_free (str);
 
-  fail_unless (gst_rtsp_media_seek (media, range));
-
-  str = gst_rtsp_media_get_range_string (media, FALSE, GST_RTSP_RANGE_NPT);
-  fail_unless (g_str_equal (str, "npt=5-"));
-  g_free (str);
-
-  str = gst_rtsp_media_get_range_string (media, TRUE, GST_RTSP_RANGE_NPT);
-  fail_unless (g_str_equal (str, "npt=5-"));
-  g_free (str);
+  /* fails, need to be prepared and contain sink elements */
+  fail_if (gst_rtsp_media_seek (media, range));
 
   fail_unless (gst_rtsp_media_unprepare (media));
 
@@ -126,12 +185,13 @@ GST_START_TEST (test_media)
 GST_END_TEST;
 
 static void
-test_prepare_reusable (GstRTSPThreadPool * pool, const gchar * launch_line)
+test_prepare_reusable (const gchar * launch_line)
 {
   GstRTSPMediaFactory *factory;
   GstRTSPMedia *media;
   GstRTSPUrl *url;
   GstRTSPThread *thread;
+  GstRTSPThreadPool *pool;
 
   factory = gst_rtsp_media_factory_new ();
   fail_if (gst_rtsp_media_factory_is_shared (factory));
@@ -146,6 +206,7 @@ test_prepare_reusable (GstRTSPThreadPool * pool, const gchar * launch_line)
 
   g_object_set (G_OBJECT (media), "reusable", TRUE, NULL);
 
+  pool = gst_rtsp_thread_pool_new ();
   thread = gst_rtsp_thread_pool_get_thread (pool,
       GST_RTSP_THREAD_TYPE_MEDIA, NULL);
   fail_unless (gst_rtsp_media_prepare (media, thread));
@@ -161,6 +222,17 @@ test_prepare_reusable (GstRTSPThreadPool * pool, const gchar * launch_line)
   gst_rtsp_url_free (url);
   g_object_unref (factory);
 }
+
+GST_START_TEST (test_media_reusable)
+{
+
+  /* test reusable media */
+  test_prepare_reusable ("( videotestsrc ! rtpvrawpay pt=96 name=pay0 )");
+  test_prepare_reusable (
+      "( videotestsrc is-live=true ! rtpvrawpay pt=96 name=pay0 )");
+}
+
+GST_END_TEST;
 
 GST_START_TEST (test_media_prepare)
 {
@@ -198,11 +270,6 @@ GST_START_TEST (test_media_prepare)
   g_object_unref (media);
   gst_rtsp_url_free (url);
   g_object_unref (factory);
-
-  /* test reusable media */
-  test_prepare_reusable (pool, "( videotestsrc ! rtpvrawpay pt=96 name=pay0 )");
-  test_prepare_reusable (pool,
-      "( videotestsrc is-live=true ! rtpvrawpay pt=96 name=pay0 )");
 
   g_object_unref (pool);
   gst_rtsp_thread_pool_cleanup ();
@@ -454,9 +521,11 @@ rtspmedia_suite (void)
 
   suite_add_tcase (s, tc);
   tcase_set_timeout (tc, 20);
-  tcase_add_test (tc, test_launch);
+  tcase_add_test (tc, test_media_seek);
+  tcase_add_test (tc, test_media_seek_no_sinks);
   tcase_add_test (tc, test_media);
   tcase_add_test (tc, test_media_prepare);
+  tcase_add_test (tc, test_media_reusable);
   tcase_add_test (tc, test_media_dyn_prepare);
   tcase_add_test (tc, test_media_take_pipeline);
   tcase_add_test (tc, test_media_reset);

commit fb8004a6ebfbad1bc16c17d9bf0a845d2f760ebf

rtsp-media: Add/configure transports when completing the pipeline
Otherwise the transports are not set up yet during the PLAY request
handling when unsuspending (and thus unblocking) the media.

In case of live pipelines this then causes the first few packets to go
to the sinks before they know what to do with them, and they simply
discard them which is rather suboptimal in case of keyframes.

For non-live pipelines this is not a problem because the sink will still
be PAUSED and as such not send out the data yet but wait until it goes
to PLAYING, which is late enough.

Adding the transports multiple times is not a problem: if the transport
is already added it won't be added another time and TRUE will be
returned.

This fixes a regression introduced by a7732a68e8bc6b4ba15629c652056c240c624ff0
before 1.14.0.

Fixes https://gitlab.freedesktop.org/gstreamer/gst-rtsp-server/-/issues/107

Part-of: <https://gitlab.freedesktop.org/gstreamer/gst-rtsp-server/-/merge_requests/135>

gst/rtsp-server/rtsp-media.c
@@ -5012,6 +5012,11 @@ gst_rtsp_media_complete_pipeline (GstRTSPMedia * media, GPtrArray * transports)
       g_mutex_unlock (&priv->lock);
       return FALSE;
     }
+
+    if (!gst_rtsp_stream_add_transport (stream, transport)) {
+      g_mutex_unlock (&priv->lock);
+      return FALSE;
+    }
   }
 
   priv->complete = TRUE;