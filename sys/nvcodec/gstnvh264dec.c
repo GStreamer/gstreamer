133b9020441ca61d4828ed0e1a7c76d9caa17349
Seungha Yang <seungha@centricular.com>
2020-06-22 20:15:13 +0900

    nvh264sldec: Remove useless double space
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gst-plugins-bad/-/merge_requests/1357>

--- sys/nvcodec/gstnvh264dec.c
+++ sys/nvcodec/gstnvh264dec.c
@@ -26,7 +26,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
-  * This library is free software; you can redistribute it and/or
+ * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
  * License as published by the Free Software Foundation; either
  * version 2 of the License, or (at your option) any later version.
