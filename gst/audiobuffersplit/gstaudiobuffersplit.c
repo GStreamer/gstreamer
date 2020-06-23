ea5f38440dafa1f2f1ee99b50feddabb322aebe2
Sebastian Dröge <sebastian@centricular.com>
2020-06-23 09:50:46 +0300

    audiobuffersplit: Specify in the template caps that only interleaved audio is supported
    Needs special support for non-interleaved audio and e.g. use the
    GstPlanarAudioAdapter.
    
    See https://gitlab.freedesktop.org/gstreamer/gst-plugins-base/-/issues/779
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gst-plugins-bad/-/merge_requests/1363>

--- gst/audiobuffersplit/gstaudiobuffersplit.c
+++ gst/audiobuffersplit/gstaudiobuffersplit.c
@@ -30,13 +30,15 @@ GST_DEBUG_CATEGORY_STATIC (GST_CAT_DEFAULT);
 static GstStaticPadTemplate sink_template = GST_STATIC_PAD_TEMPLATE ("sink",
     GST_PAD_SINK,
     GST_PAD_ALWAYS,
-    GST_STATIC_CAPS ("audio/x-raw")
+    GST_STATIC_CAPS (GST_AUDIO_CAPS_MAKE (GST_AUDIO_FORMATS_ALL)
+        ", layout = (string) interleaved")
     );
 
 static GstStaticPadTemplate src_template = GST_STATIC_PAD_TEMPLATE ("src",
     GST_PAD_SRC,
     GST_PAD_ALWAYS,
-    GST_STATIC_CAPS ("audio/x-raw")
+    GST_STATIC_CAPS (GST_AUDIO_CAPS_MAKE (GST_AUDIO_FORMATS_ALL)
+        ", layout = (string) interleaved")
     );
 
 enum
