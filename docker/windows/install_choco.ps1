800c46e67c3c024165caa9a623f20a34ff2032a9
Jordan Petridis <jordan@centricular.com>
2020-08-28 19:30:41 +0300

    docker/windows: tweak installation of git
    Make sure we set autocrlf to false, which is needed by cerbero.
    Additionally remove useless for the container integrations with
    the shell.
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gst-ci/-/merge_requests/333>

--- docker/windows/install_choco.ps1
+++ docker/windows/install_choco.ps1
@@ -2,4 +2,5 @@ Write-Host "Installing Choco"
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 Write-Host "Installing Choco packages"
 choco install -y cmake --installargs 'ADD_CMAKE_TO_PATH=System'
-choco install -y python3 git git-lfs 7zip
+choco install -y git --params "/NoAutoCrlf /NoCredentialManager /NoShellHereIntegration /NoGuiHereIntegration /NoShellIntegration"
+choco install -y python3 git-lfs 7zip
