GStreamer/gstreamer

commit 827afa206d8c9675f2a7af402396552c2ed1df09

webrtc, rtmp2: Fix parsing of userinfo in URI strings
While parsing the string, `gst_uri_from_string()` also unescapes the
userinfo. This is bad if your username contains a `:` character, since
we will then split the userinfo at the wrong location when parsing it.

To fix this, we can use the new `gst_uri_from_string_escaped()` API
that was added in
https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/583

Fixes https://gitlab.freedesktop.org/gstreamer/gst-plugins-bad/-/issues/831

Part-of: <https://gitlab.freedesktop.org/gstreamer/gst-plugins-bad/-/merge_requests/1481>

gst/rtmp2/gstrtmp2locationhandler.c
@@ -165,7 +165,7 @@ uri_handler_set_uri (GstURIHandler * handler, const gchar * string,
 
   {
     gchar *string_without_path = g_strndup (string, path_sep - string);
-    uri = gst_uri_from_string (string_without_path);
+    uri = gst_uri_from_string_escaped (string_without_path);
     g_free (string_without_path);
   }
 

ext/webrtc/gstwebrtcice.c
@@ -780,7 +780,7 @@ _clear_ice_stream (struct NiceStreamItem *item)
 static GstUri *
 _validate_turn_server (GstWebRTCICE * ice, const gchar * s)
 {
-  GstUri *uri = gst_uri_from_string (s);
+  GstUri *uri = gst_uri_from_string_escaped (s);
   const gchar *userinfo, *scheme;
   GList *keys = NULL, *l;
   gchar *user = NULL, *pass = NULL;
@@ -855,7 +855,7 @@ out:
 void
 gst_webrtc_ice_set_stun_server (GstWebRTCICE * ice, const gchar * uri_s)
 {
-  GstUri *uri = gst_uri_from_string (uri_s);
+  GstUri *uri = gst_uri_from_string_escaped (uri_s);
   const gchar *msg = "must be of the form stun://<host>:<port>";
 
   GST_DEBUG_OBJECT (ice, "setting stun server, %s", uri_s);

commit ce18a344f456707e51c30f1ecba3adca341f0ba9

rtmp2: Need to unescape the userinfo before setting
This regressed in 827afa206d8c9675f2a7af402396552c2ed1df09. The same
fix was also committed to the webrtc element, but rtmp2 was missed.

Part-of: <https://gitlab.freedesktop.org/gstreamer/gst-plugins-bad/-/merge_requests/1547>

gst/rtmp2/gstrtmp2locationhandler.c
@@ -212,6 +212,7 @@ uri_handler_set_uri (GstURIHandler * handler, const gchar * string,
 
   userinfo = gst_uri_get_userinfo (uri);
   if (userinfo) {
+    gchar *user, *pass;
     gchar **split = g_strsplit (userinfo, ":", 2);
 
     if (!split || !split[0] || !split[1]) {
@@ -226,8 +227,13 @@ uri_handler_set_uri (GstURIHandler * handler, const gchar * string,
           "assume that the first ':' delineates user:pass. You should escape "
           "the user and pass before adding to the URI.", userinfo);
 
-    g_object_set (self, "username", split[0], "password", split[1], NULL);
+    user = g_uri_unescape_string (split[0], NULL);
+    pass = g_uri_unescape_string (split[1], NULL);
     g_strfreev (split);
+
+    g_object_set (self, "username", user, "password", pass, NULL);
+    g_free (user);
+    g_free (pass);
   }
 
   ret = TRUE;