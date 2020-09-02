671e459d6952f3dcd7bf0b813a8d7b59ba6794a3
Sebastian Dröge <sebastian@centricular.com>
2020-09-02 17:01:35 +0300

    filesrc: Remove unused #define on Windows
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/610>

--- plugins/elements/gstfilesrc.c
+++ plugins/elements/gstfilesrc.c
@@ -47,8 +47,6 @@
 #include <io.h>                 /* lseek, open, close, read */
 /* On win32, stat* default to 32 bit; we need the 64-bit
  * variants, so explicitly define it that way. */
-#undef stat
-#define stat __stat64
 #undef fstat
 #define fstat _fstat64
 #undef lseek
