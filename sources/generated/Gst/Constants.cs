9d7f360da7e3e0b3cf16adef83778d358aa35da7
Tim-Philipp Müller <tim@centricular.com>
2020-09-08 16:59:17 +0100

    Back to development

--- sources/generated/Gst/Constants.cs
+++ sources/generated/Gst/Constants.cs
@@ -169,8 +169,8 @@ namespace Gst {
 		public const int VALUE_UNORDERED = 2;
 		public const int VERSION_MAJOR = 1;
 		public const int VERSION_MICRO = 0;
-		public const int VERSION_MINOR = 18;
-		public const int VERSION_NANO = 0;
+		public const int VERSION_MINOR = 19;
+		public const int VERSION_NANO = 1;
 #endregion
 	}
 }
