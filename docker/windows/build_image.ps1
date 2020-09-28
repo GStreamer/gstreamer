a26129c78cd9096ca92c85fad6a235cd3f3384ee
Jordan Petridis <jordan@centricular.com>
2020-09-28 14:00:24 +0300

    windows: rebuild docker image
    also:
    
    Revert "citemplate: set the cerbero msvc build to allow_failure again"
    
    This reverts commit 3dddfacfbf710fa71777488145594eb0abe11ee4.
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gst-ci/-/merge_requests/359>

--- docker/windows/build_image.ps1
+++ docker/windows/build_image.ps1
@@ -1,5 +1,5 @@
 $env:DEFAULT_BRANCH='master'
-$env:VERSION='v12'
+$env:VERSION='v13'
 $env:tag ="registry.freedesktop.org/gstreamer/gst-ci/amd64/windows:$env:VERSION-$env:DEFAULT_BRANCH"
 echo "Building $env:tag"
 docker build --build-arg DEFAULT_BRANCH=$env:DEFAULT_BRANCH -f Dockerfile -t $env:tag .
