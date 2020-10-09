139fbac92e08b572925e37e807e3447c403a070b
Stéphane Cerveau <scerveau@collabora.com>
2020-10-09 12:58:07 +0200

    gstreamer-full: add assert for global symbols
    Add _gst_caps_features_any not null assert
    Add _gst_caps_features_memory_system_memory as well
    
    A bug has been detected when using -Bsymbolic in the link
    of libgstreamer-full.so
    
    This flag has been introduced to fix this issue:
    
    https://gitlab.freedesktop.org/gstreamer/gst-build/-/issues/108
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gst-build/-/merge_requests/204>

--- tests/static-plugins/test-gst-full.c
+++ tests/static-plugins/test-gst-full.c
@@ -7,8 +7,12 @@ main (int argc, char *argv[])
 
   gst_init (&argc, &argv);
 
+  /* -Bsymbolic option is introducing a regression where this variable
+   * were duplicated over the use in a dynamical use of libgstreamer-full.so */
+  g_assert_nonnull (_gst_caps_features_any);
+  g_assert_nonnull (_gst_caps_features_memory_system_memory);
+
   e = gst_element_factory_make ("pipeline", NULL);
   g_assert_nonnull (e);
   g_object_unref (e);
-
 }
