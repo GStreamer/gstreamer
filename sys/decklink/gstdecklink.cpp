GStreamer/gstreamer

commit 309f6187fef890c7ffa49305f38e89beac3b1423

decklink: Re-order modes enum for backwards compatibility with 1.16
The PAL/NTSC widescreen modes were added after 1.16 but inserted before
the HD modes, which changed the integer values of the enums.

Move them to the very end instead to keep backwards compatibility.

Fixes https://gitlab.freedesktop.org/gstreamer/gst-plugins-bad/-/issues/1048

Part-of: <https://gitlab.freedesktop.org/gstreamer/gst-plugins-bad/-/merge_requests/1492>

sys/decklink/gstdecklink.cpp
@@ -48,12 +48,6 @@ gst_decklink_mode_get_type (void)
     {GST_DECKLINK_MODE_NTSC_P, "NTSC SD 60p", "ntsc-p"},
     {GST_DECKLINK_MODE_PAL_P, "PAL SD 50p", "pal-p"},
 
-    {GST_DECKLINK_MODE_NTSC_WIDESCREEN, "NTSC SD 60i Widescreen", "ntsc-widescreen"},
-    {GST_DECKLINK_MODE_NTSC2398_WIDESCREEN, "NTSC SD 60i Widescreen (24 fps)", "ntsc2398-widescreen"},
-    {GST_DECKLINK_MODE_PAL_WIDESCREEN, "PAL SD 50i Widescreen", "pal-widescreen"},
-    {GST_DECKLINK_MODE_NTSC_P_WIDESCREEN, "NTSC SD 60p Widescreen", "ntsc-p-widescreen"},
-    {GST_DECKLINK_MODE_PAL_P_WIDESCREEN, "PAL SD 50p Widescreen", "pal-p-widescreen"},
-
     {GST_DECKLINK_MODE_1080p2398, "HD1080 23.98p", "1080p2398"},
     {GST_DECKLINK_MODE_1080p24, "HD1080 24p", "1080p24"},
     {GST_DECKLINK_MODE_1080p25, "HD1080 25p", "1080p25"},
@@ -93,6 +87,12 @@ gst_decklink_mode_get_type (void)
     {GST_DECKLINK_MODE_2160p50, "4k 50p", "2160p50"},
     {GST_DECKLINK_MODE_2160p5994, "4k 59.94p", "2160p5994"},
     {GST_DECKLINK_MODE_2160p60, "4k 60p", "2160p60"},
+
+    {GST_DECKLINK_MODE_NTSC_WIDESCREEN, "NTSC SD 60i Widescreen", "ntsc-widescreen"},
+    {GST_DECKLINK_MODE_NTSC2398_WIDESCREEN, "NTSC SD 60i Widescreen (24 fps)", "ntsc2398-widescreen"},
+    {GST_DECKLINK_MODE_PAL_WIDESCREEN, "PAL SD 50i Widescreen", "pal-widescreen"},
+    {GST_DECKLINK_MODE_NTSC_P_WIDESCREEN, "NTSC SD 60p Widescreen", "ntsc-p-widescreen"},
+    {GST_DECKLINK_MODE_PAL_P_WIDESCREEN, "PAL SD 50p Widescreen", "pal-p-widescreen"},
 
     {0, NULL, NULL}
   };

sys/decklink/gstdecklink.h
@@ -64,12 +64,6 @@ typedef enum {
   GST_DECKLINK_MODE_NTSC_P,
   GST_DECKLINK_MODE_PAL_P,
 
-  GST_DECKLINK_MODE_NTSC_WIDESCREEN,
-  GST_DECKLINK_MODE_NTSC2398_WIDESCREEN,
-  GST_DECKLINK_MODE_PAL_WIDESCREEN,
-  GST_DECKLINK_MODE_NTSC_P_WIDESCREEN,
-  GST_DECKLINK_MODE_PAL_P_WIDESCREEN,
-
   GST_DECKLINK_MODE_1080p2398,
   GST_DECKLINK_MODE_1080p24,
   GST_DECKLINK_MODE_1080p25,
@@ -108,7 +102,13 @@ typedef enum {
   GST_DECKLINK_MODE_2160p30,
   GST_DECKLINK_MODE_2160p50,
   GST_DECKLINK_MODE_2160p5994,
-  GST_DECKLINK_MODE_2160p60
+  GST_DECKLINK_MODE_2160p60,
+
+  GST_DECKLINK_MODE_NTSC_WIDESCREEN,
+  GST_DECKLINK_MODE_NTSC2398_WIDESCREEN,
+  GST_DECKLINK_MODE_PAL_WIDESCREEN,
+  GST_DECKLINK_MODE_NTSC_P_WIDESCREEN,
+  GST_DECKLINK_MODE_PAL_P_WIDESCREEN
 } GstDecklinkModeEnum;
 #define GST_TYPE_DECKLINK_MODE (gst_decklink_mode_get_type ())
 GType gst_decklink_mode_get_type (void);

sys/decklink/gstdecklinkvideosink.cpp
@@ -1089,7 +1089,7 @@ write_vbi (GstDecklinkVideoSink * self, GstBuffer * buffer,
 
       /* See SMPTE 2016-3 Section 4 */
       /* AFD and AR */
-      if (self->mode < (gint) GST_DECKLINK_MODE_NTSC_WIDESCREEN) {
+      if (self->mode <= (gint) GST_DECKLINK_MODE_PAL_P) {
         afd_bar_data_ptr[0] = (afd << 3) | 0x0;
       } else {
         afd_bar_data_ptr[0] = (afd << 3) | 0x4;

commit c6aca271a1ac4ff10b0be08c779782f6879ba770

decklink: Update enum value bounds check in gst_decklink_get_mode()
The widescreen modes moved after GST_DECKLINK_MODE_2160p60 and using
them now would cause an assertion. This is a regression from
309f6187fef890c7ffa49305f38e89beac3b1423.

Part-of: <https://gitlab.freedesktop.org/gstreamer/gst-plugins-bad/-/merge_requests/1737>

sys/decklink/gstdecklink.cpp
@@ -425,7 +425,7 @@ static const struct
 const GstDecklinkMode *
 gst_decklink_get_mode (GstDecklinkModeEnum e)
 {
-  if (e < GST_DECKLINK_MODE_AUTO || e > GST_DECKLINK_MODE_2160p60)
+  if (e < GST_DECKLINK_MODE_AUTO || e > GST_DECKLINK_MODE_PAL_P_WIDESCREEN)
     return NULL;
   return &modes[e];
 }