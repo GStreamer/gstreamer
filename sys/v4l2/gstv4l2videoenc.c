7da5e6a4a0702c186663035adb55170d53f038c8
Bing Song <bing.song@nxp.com>
2021-02-04 13:43:17 +0800

    v4l2videoenc: support resolution change stream encode.
    Resolution change stream transcoding will drain before send new video
    frame buffer. Need encode video frame after process EOS.
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gst-plugins-good/-/merge_requests/872>

--- sys/v4l2/gstv4l2videoenc.c
+++ sys/v4l2/gstv4l2videoenc.c
@@ -738,10 +738,13 @@ gst_v4l2_video_enc_handle_frame (GstVideoEncoder * encoder,
   if (task_state == GST_TASK_STOPPED || task_state == GST_TASK_PAUSED) {
     GstBufferPool *pool = GST_BUFFER_POOL (self->v4l2output->pool);
 
-    /* It possible that the processing thread stopped due to an error */
+    /* It is possible that the processing thread stopped due to an error or
+     * when the last buffer has been met during the draining process. */
     if (self->output_flow != GST_FLOW_OK &&
-        self->output_flow != GST_FLOW_FLUSHING) {
-      GST_DEBUG_OBJECT (self, "Processing loop stopped with error, leaving");
+        self->output_flow != GST_FLOW_FLUSHING &&
+        self->output_flow != GST_V4L2_FLOW_LAST_BUFFER) {
+      GST_DEBUG_OBJECT (self, "Processing loop stopped with error: %s, leaving",
+          gst_flow_get_name (self->output_flow));
       ret = self->output_flow;
       goto drop;
     }
