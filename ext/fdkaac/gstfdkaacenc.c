a917648be3d980a3845ed227322a74300632643b
Thibault Saunier <tsaunier@igalia.com>
2021-08-09 19:02:56 -0400

    fdkaacdec: Add Converter class to hint gst-validate
    fdkaacdec have minimal conversion capability, adding the Converter class allow
    gst-validate to behave properly and not spit an error when it notice that the
    number of channels or rate miss-match in and out.
    
    Same logic as with opusdec, see: https://gitlab.freedesktop.org/gstreamer/gst-plugins-base/-/merge_requests/1142>
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gst-plugins-bad/-/merge_requests/2462>

--- ext/fdkaac/gstfdkaacenc.c
+++ ext/fdkaac/gstfdkaacenc.c
@@ -580,7 +580,7 @@ gst_fdkaacenc_class_init (GstFdkAacEncClass * klass)
   gst_element_class_add_static_pad_template (element_class, &src_template);
 
   gst_element_class_set_static_metadata (element_class, "FDK AAC audio encoder",
-      "Codec/Encoder/Audio", "FDK AAC audio encoder",
+      "Codec/Encoder/Audio/Converter", "FDK AAC audio encoder",
       "Sebastian Dröge <sebastian@centricular.com>");
 
   GST_DEBUG_CATEGORY_INIT (gst_fdkaacenc_debug, "fdkaacenc", 0,
