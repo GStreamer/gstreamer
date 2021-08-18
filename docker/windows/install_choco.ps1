37c0ee72806b767a01b4eb60e533e3dc9c4bcbef
Jordan Petridis <jordan@centricular.com>
2021-08-18 15:16:24 +0300

    docker/windows: install vcredist140 explicitly
    We depend on it for GStreamer builds and it was gettings pulled
    transiently by python3 install.
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gst-ci/-/merge_requests/405>

--- docker/windows/install_choco.ps1
+++ docker/windows/install_choco.ps1
@@ -4,6 +4,9 @@ Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://cho
 Import-Module "$env:ProgramData\chocolatey\helpers\chocolateyProfile.psm1"
 Update-SessionEnvironment
 
+choco install -y vcredist140
+$vc140_install = $?
+
 Write-Host "Installing Chocolatey packages"
 choco install -y cmake --installargs 'ADD_CMAKE_TO_PATH=System'
 $cmake_install = $?
@@ -14,7 +17,7 @@ $git_install = $?
 choco install -y python3 git-lfs 7zip
 $rest_installs = $?
 
-if (!($cmake_install -and $git_install -and $rest_installs)) {
+if (!($vc140_install -and $cmake_install -and $git_install -and $rest_installs)) {
     Write-Host "Failed to install some dependencies from choco"
     Exit 1
 }
