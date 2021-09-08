b81620c3a75b6fff821e815c65b20f4fe36c677a
Jordan Petridіs <jpetridis@gnome.org>
2021-09-08 07:36:47 +0000

    Revert "docker/windows: create a rust image for gst-rs"
    This reverts commit 9c21b05d1bf438d728a69d94c3acae8ddf85d496

--- docker/windows/build_image.ps1
+++ docker/windows/build_image.ps1
@@ -1,25 +1,12 @@
-$env:ErrorActionPreference='Stop'
-
 $env:DEFAULT_BRANCH='master'
 $env:VERSION='v18'
 $env:tag ="registry.freedesktop.org/gstreamer/gst-ci/amd64/windows:$env:VERSION-$env:DEFAULT_BRANCH"
-$env:rust_tag ="registry.freedesktop.org/gstreamer/gst-ci/amd64/windows-rust:$env:VERSION-$env:DEFAULT_BRANCH"
-
-Set-Location './docker/windows/'
 
 Get-Date
 Write-Output "Building $env:tag"
-docker build --isolation=hyperv -m 12g --build-arg DEFAULT_BRANCH=$env:DEFAULT_BRANCH -f Dockerfile -t $env:tag .
+docker build --build-arg DEFAULT_BRANCH=$env:DEFAULT_BRANCH -f Dockerfile -t $env:tag .
 if (!$?) {
   Write-Host "Failed to build docker image $env:tag"
-  Exit 1
-}
-
-Get-Date
-Write-Output "Building $env:rust_tag"
-docker build --isolation=hyperv -m 12g --build-arg DEFAULT_BRANCH=$env:DEFAULT_BRANCH -f rust.Dockerfile -t $env:rust_tag .
-if (!$?) {
-  Write-Host "Failed to build docker image $env:rust_tag"
   Exit 1
 }
 
@@ -30,15 +17,6 @@ if (!$?) {
 #   Write-Host "Failed to push docker image $env:tag"
 #   Exit 1
 # }
-
-# Get-Date
-# Write-Output "Pushing $env:rust_tag"
-# docker push $env:rust_tag
-# if (!$?) {
-#   Write-Host "Failed to push docker image $env:rust_tag"
-#   Exit 1
-# }
-
 
 Get-Date
 Write-Output "Build Finished"
