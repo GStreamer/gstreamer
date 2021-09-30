f89dff303a1e06b91563d878607b5fb3c9e0cb04
Andoni Morales Alastruey <ylatuya@gmail.com>
2021-09-30 13:01:26 +0200

    gstreamer-sharp: ensure a consitent code format
    Use EditorConfig and dotnet-format to ensure a consistent
    code style.
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/983>

---
+++ ci/docker/indent/prepare.sh
@@ -0,0 +1,15 @@
+set -e
+set -x
+# Install dotnet-format
+apt update -yqq
+apt install -y gnupg apt-transport-https
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.asc.gpg
+mv microsoft.asc.gpg /etc/apt/trusted.gpg.d/
+curl -O https://packages.microsoft.com/config/debian/9/prod.list
+mv prod.list /etc/apt/sources.list.d/microsoft-prod.list
+chown root:root /etc/apt/trusted.gpg.d/microsoft.asc.gpg
+chown root:root /etc/apt/sources.list.d/microsoft-prod.list
+apt update -yqq
+apt install -y dotnet-sdk-5.0
+dotnet tool install --global dotnet-format
+ln -s ~/.dotnet/tools/dotnet-format /usr/local/bin/dotnet-format
