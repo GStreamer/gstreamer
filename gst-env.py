4dece4701ddea7e656c501c382c32101862d362a
Mark Nauwelaerts <mnauw@users.sourceforge.net>
2021-12-26 19:01:24 +0100

    gst-env: extend PATH to include plugin scanner and generator

--- gst-env.py
+++ gst-env.py
@@ -318,6 +318,10 @@ def get_subprocess_env(options, gst_version):
     prepend_env_var(env, "PATH", os.path.join(options.builddir, 'subprojects',
                                               'gstreamer', 'tools'),
                     options.sysroot)
+    # plugin scanner and generator
+    prepend_env_var(env, "PATH", os.path.join(options.builddir, 'subprojects',
+                                              'gstreamer', 'docs'),
+                    options.sysroot)
     prepend_env_var(env, "PATH", os.path.join(options.builddir, 'subprojects',
                                               'gst-plugins-base', 'tools'),
                     options.sysroot)
