b0370cdf00371dc91e03a3b661cdc838e507949c
Jordan Petridis <jordan@centricular.com>
2022-01-03 12:43:15 +0200

    ci: replace license notice
    This file was copied from microsoft documentation [1]
    and its header was refering to the license on that repo.
    
    This change was lost during gstreamer/gstreamer!1000
    
    [1] https://github.com/MicrosoftDocs/visualstudio-docs/blob/9ec230dfd4908a92738d8dac5f44ad3e4ac751e3/LICENSE-CODE
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/1487>

--- ci/docker/windows/Install.cmd
+++ ci/docker/windows/Install.cmd
@@ -1,5 +1,20 @@
-@rem Copyright (C) Microsoft Corporation. All rights reserved.
-@rem Licensed under the MIT license. See LICENSE.txt in the project root for license information.
+@REM The MIT License (MIT)
+@REM Copyright (c) Microsoft Corporation
+
+@REM Permission is hereby granted, free of charge, to any person obtaining a copy of this software and 
+@REM associated documentation files (the "Software"), to deal in the Software without restriction, 
+@REM including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+@REM and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, 
+@REM subject to the following conditions:
+
+@REM The above copyright notice and this permission notice shall be included in all copies or substantial 
+@REM portions of the Software.
+
+@REM THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT 
+@REM NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+@REM IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+@REM WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+@REM SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 @if not defined _echo echo off
 setlocal enabledelayedexpansion
