701ed92d275584151b6edad504e464440abc89e1
Tim-Philipp Müller <tim@centricular.com>
2022-02-04 11:15:47 +0000

    Back to development
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/1635>

--- subprojects/gstreamer-sharp/sources/generated/Gst.PbUtils/Constants.cs
+++ subprojects/gstreamer-sharp/sources/generated/Gst.PbUtils/Constants.cs
@@ -18,8 +18,8 @@ namespace Gst.PbUtils {
 		public const string ENCODING_CATEGORY_STORAGE_EDITING = @"storage-editing";
 		public const int PLUGINS_BASE_VERSION_MAJOR = 1;
 		public const int PLUGINS_BASE_VERSION_MICRO = 0;
-		public const int PLUGINS_BASE_VERSION_MINOR = 20;
-		public const int PLUGINS_BASE_VERSION_NANO = 0;
+		public const int PLUGINS_BASE_VERSION_MINOR = 21;
+		public const int PLUGINS_BASE_VERSION_NANO = 1;
 #endregion
 	}
 }
