50b58aa9744d5ae5c4d000760e379f168cc6272b
Xavier Claessens <xavier.claessens@collabora.com>
2022-03-25 11:21:03 -0400

    gst-play: Do not split translatable string
    Concatenating N_() strings does not work with gi18n.h macro, was working
    only with GStreamer's redefinition.
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/2028>

--- subprojects/gst-plugins-base/tools/gst-play.c
+++ subprojects/gst-plugins-base/tools/gst-play.c
@@ -1636,8 +1636,8 @@ main (int argc, char **argv)
     {"quiet", 'q', 0, G_OPTION_ARG_NONE, &quiet,
         N_("Do not print any output (apart from errors)"), NULL},
     {"use-playbin3", 0, 0, G_OPTION_ARG_NONE, &use_playbin3,
-          N_("Use playbin3 pipeline")
-          N_("(default varies depending on 'USE_PLAYBIN' env variable)"),
+          N_("Use playbin3 pipeline"
+              "(default varies depending on 'USE_PLAYBIN' env variable)"),
         NULL},
     {"wait-on-eos", 0, 0, G_OPTION_ARG_NONE, &wait_on_eos,
           N_
