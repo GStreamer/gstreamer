42193657566ea23f39d4c61d8b8afe9c2bc16077
Jordan Petridis <jordan@centricular.com>
2022-05-13 11:23:45 +0300

    ci: remove windows-rust jobs
    These are moved now into gstreamer-rs repository.
    
    https://gitlab.freedesktop.org/gstreamer/gstreamer-rs/-/merge_requests/701
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/2808>

--- ci/docker/windows/build_image.ps1
+++ ci/docker/windows/build_image.ps1
@@ -3,7 +3,6 @@ $env:ErrorActionPreference='Stop'
 $env:DEFAULT_BRANCH='main'
 $env:VERSION='test'
 $env:tag ="registry.freedesktop.org/gstreamer/gst-ci/amd64/windows:$env:VERSION-$env:DEFAULT_BRANCH"
-$env:rust_tag ="registry.freedesktop.org/gstreamer/gst-ci/amd64/windows-rust:$env:VERSION-$env:DEFAULT_BRANCH"
 
 Set-Location './docker/windows/'
 
@@ -12,14 +11,6 @@ Write-Output "Building $env:tag"
 docker build --isolation=hyperv -m 12g --build-arg DEFAULT_BRANCH=$env:DEFAULT_BRANCH -f Dockerfile -t $env:tag .
 if (!$?) {
   Write-Host "Failed to build docker image $env:tag"
-  Exit 1
-}
-
-Get-Date
-Write-Output "Building $env:rust_tag"
-docker build --isolation=hyperv -m 12g --build-arg DEFAULT_BRANCH=$env:DEFAULT_BRANCH -f rust.Dockerfile -t $env:rust_tag .
-if (!$?) {
-  Write-Host "Failed to build docker image $env:rust_tag"
   Exit 1
 }
 
