0857f5af5f6c276fa8b4e4345e6a7d1fcb0140d2
Edward Hervey <edward@centricular.com>
2022-07-20 10:57:41 +0200

    adaptivedemux2: Modify custom sync loss flow return
    Make it a custom sucess and not an error
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/2839>

--- subprojects/gst-plugins-good/ext/adaptivedemux2/gstadaptivedemux.h
+++ subprojects/gst-plugins-good/ext/adaptivedemux2/gstadaptivedemux.h
@@ -106,7 +106,7 @@ typedef GstObjectClass GstAdaptiveDemux2StreamClass;
 #define GST_ADAPTIVE_DEMUX_FLOW_RESTART_FRAGMENT GST_FLOW_CUSTOM_SUCCESS_2
 
 /* The live stream has lost synchronization and the demuxer needs to be resetted */
-#define GST_ADAPTIVE_DEMUX_FLOW_LOST_SYNC GST_FLOW_CUSTOM_ERROR_1
+#define GST_ADAPTIVE_DEMUX_FLOW_LOST_SYNC GST_FLOW_CUSTOM_SUCCESS_2 + 1
 
 typedef enum _GstAdaptiveDemux2StreamState GstAdaptiveDemux2StreamState;
 
