895290f411da7b4e9c08a23e839dd927a9ad9a91
Nirbheek Chauhan <nirbheek@centricular.com>
2022-09-18 02:31:01 +0530

    ci: Bootstrap with visualstudio variant enabled
    visualstudio variant now pulls in more build-tools deps on top of
    mingw since it enables the rust variant by default. It's also the
    variant that's tested on Cerbero CI.
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/2507>

--- ci/docker/windows/prepare_cerbero_env.sh
+++ ci/docker/windows/prepare_cerbero_env.sh
@@ -12,7 +12,7 @@ echo 'vs_install_path = "C:/BuildTools"' >> localconf.cbc
 echo 'vs_install_version = "vs16"' >> localconf.cbc
 
 # Fetch all bootstrap requirements
-./cerbero-uninstalled -t -c localconf.cbc -c config/win64.cbc fetch-bootstrap --jobs=4
+./cerbero-uninstalled -t  -v visualstudio -c localconf.cbc -c config/win64.cbc fetch-bootstrap --jobs=4
 # Fetch all package requirements for a mingw gstreamer build
 ./cerbero-uninstalled -t -c localconf.cbc -c config/win64.cbc fetch-package --jobs=4 gstreamer-1.0
 # Fetch all package requirements for a visualstudio gstreamer build
