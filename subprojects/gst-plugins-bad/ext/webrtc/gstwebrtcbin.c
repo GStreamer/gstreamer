f8b248cb1ab8d757e0d75b4880648893117ef5dc
Sangchul Lee <sc11.lee@samsung.com>
2022-09-22 22:39:31 +0900

    webrtcbin: Fix pointer dereference before null check
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/3133>

--- subprojects/gst-plugins-bad/ext/webrtc/gstwebrtcbin.c
+++ subprojects/gst-plugins-bad/ext/webrtc/gstwebrtcbin.c
@@ -5762,6 +5762,12 @@ _set_description_task (GstWebRTCBin * webrtc, struct set_description *sd)
         continue;
       }
 
+      if (!pad->trans) {
+        GST_LOG_OBJECT (pad, "doesn't have a transceiver");
+        tmp = tmp->next;
+        continue;
+      }
+
       if (pad->trans->mline >= gst_sdp_message_medias_len (sd->sdp->sdp)) {
         GST_DEBUG_OBJECT (pad, "not mentioned in this description. Skipping");
         tmp = tmp->next;
@@ -5774,12 +5780,6 @@ _set_description_task (GstWebRTCBin * webrtc, struct set_description *sd)
         /* FIXME: arrange for an appropriate flow return */
         GST_FIXME_OBJECT (pad, "Media has been rejected.  Need to arrange for "
             "a more correct flow return.");
-        tmp = tmp->next;
-        continue;
-      }
-
-      if (!pad->trans) {
-        GST_LOG_OBJECT (pad, "doesn't have a transceiver");
         tmp = tmp->next;
         continue;
       }
