f6898303a019ea3dd58a1419f423ade2a1349e24
Sebastian Dröge <sebastian@centricular.com>
2022-10-17 19:36:13 +0300

    play: Add/fix various annotations
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/3194>

--- subprojects/gst-plugins-bad/gst-libs/gst/play/gstplay-signal-adapter.c
+++ subprojects/gst-plugins-bad/gst-libs/gst/play/gstplay-signal-adapter.c
@@ -420,6 +420,14 @@ gst_play_signal_adapter_class_init (GstPlaySignalAdapterClass * klass)
       G_SIGNAL_RUN_LAST | G_SIGNAL_NO_RECURSE | G_SIGNAL_NO_HOOKS, 0, NULL,
       NULL, NULL, G_TYPE_NONE, 0, G_TYPE_INVALID);
 
+  /**
+   * GstPlaySignalAdapter::error:
+   * @adapter: The #GstPlaySignalAdapter
+   * @error: The error
+   * @details: (nullable): Additional error details
+   *
+   * Emitted on errors.
+   */
   signals[SIGNAL_ERROR] =
       g_signal_new ("error", G_TYPE_FROM_CLASS (klass),
       G_SIGNAL_RUN_LAST | G_SIGNAL_NO_RECURSE | G_SIGNAL_NO_HOOKS, 0, NULL,
@@ -445,6 +453,14 @@ gst_play_signal_adapter_class_init (GstPlaySignalAdapterClass * klass)
       G_SIGNAL_RUN_LAST | G_SIGNAL_NO_RECURSE | G_SIGNAL_NO_HOOKS, 0, NULL,
       NULL, NULL, G_TYPE_NONE, 1, G_TYPE_BOOLEAN);
 
+  /**
+   * GstPlaySignalAdapter::warning:
+   * @adapter: The #GstPlaySignalAdapter
+   * @error: The warning
+   * @details: (nullable): Additional warning details
+   *
+   * Emitted on warnings.
+   */
   signals[SIGNAL_WARNING] =
       g_signal_new ("warning", G_TYPE_FROM_CLASS (klass),
       G_SIGNAL_RUN_LAST | G_SIGNAL_NO_RECURSE | G_SIGNAL_NO_HOOKS, 0, NULL,
