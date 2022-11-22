7cfc3130a7906c199861d2ef331a07d749a4b769
Stéphane Cerveau <scerveau@igalia.com>
2022-11-22 12:32:52 +0100

    zxing: update to 1.4.0 tag
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/3450>

--- subprojects/gst-plugins-bad/ext/zxing/gstzxing.cpp
+++ subprojects/gst-plugins-bad/ext/zxing/gstzxing.cpp
@@ -368,7 +368,7 @@ gst_zxing_transform_frame_ip (GstVideoFilter * vfilter, GstVideoFrame * frame)
   auto result = ReadBarcode ({(unsigned char *)data, width, height, zxing->image_format}, hints);
   if (result.isValid ()) {
     GST_DEBUG_OBJECT (zxing, "Symbol found. Text: %s Format: %s",
-        TextUtfEncoding::ToUtf8 (result.text ()).c_str (),
+        result.text ().c_str (),
         ToString (result.format ()));
   } else {
     goto out;
@@ -396,7 +396,7 @@ gst_zxing_transform_frame_ip (GstVideoFilter * vfilter, GstVideoFrame * frame)
         "running-time", G_TYPE_UINT64, running_time,
         "type", G_TYPE_STRING, ToString (result.format ()),
         "symbol", G_TYPE_STRING,
-        TextUtfEncoding::ToUtf8 (result.text ()).c_str (), NULL);
+        result.text ().c_str (), NULL);
 
     if (zxing->attach_frame) {
       /* create a sample from image */
