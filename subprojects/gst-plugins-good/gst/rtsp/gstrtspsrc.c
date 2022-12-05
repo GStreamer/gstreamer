4f9440e9f167c4c5ee551157c59cba31da6ec5d6
Aleksandr Slobodeniuk <aslobodeniuk@fluendo.com>
2022-12-05 13:14:25 +0100

    Revert "rtspsrc: Prevent cleaning when reconnecting/flushing"
    This reverts commit 9abd4e3c1a7489a61e0716cb187ebc005e2877e5.

--- subprojects/gst-plugins-good/gst/rtsp/gstrtspsrc.c
+++ subprojects/gst-plugins-good/gst/rtsp/gstrtspsrc.c
@@ -2480,7 +2480,6 @@ gst_rtspsrc_cleanup (GstRTSPSrc * src)
 
   GST_DEBUG_OBJECT (src, "cleanup");
 
-  GST_RTSP_STATE_LOCK (src);
   for (walk = src->streams; walk; walk = g_list_next (walk)) {
     GstRTSPStream *stream = (GstRTSPStream *) walk->data;
 
@@ -2532,7 +2531,6 @@ gst_rtspsrc_cleanup (GstRTSPSrc * src)
     free_param_data (req);
   }
   GST_OBJECT_UNLOCK (src);
-  GST_RTSP_STATE_UNLOCK (src);
 
 }
 
