aef545614019bab235a393f0f296798549ee3131
Tim-Philipp Müller <tim@centricular.com>
2023-01-07 19:40:42 +0000

    taglist, tagsetter: drop use of GSlice
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/3695>

--- subprojects/gstreamer/gst/gsttaglist.c
+++ subprojects/gstreamer/gst/gsttaglist.c
@@ -549,7 +549,7 @@ gst_tag_register_static (const gchar * name, GstTagFlag flag, GType type,
     return;
   }
 
-  info = g_slice_new (GstTagInfo);
+  info = g_new (GstTagInfo, 1);
   info->flag = flag;
   info->type = type;
   info->name_quark = g_quark_from_static_string (name);
@@ -693,7 +693,7 @@ gst_tag_list_new_internal (GstStructure * s, GstTagScope scope)
 
   g_assert (s != NULL);
 
-  tag_list = (GstTagList *) g_slice_new (GstTagListImpl);
+  tag_list = (GstTagList *) g_new (GstTagListImpl, 1);
 
   gst_mini_object_init (GST_MINI_OBJECT_CAST (tag_list), 0, GST_TYPE_TAG_LIST,
       (GstMiniObjectCopyFunction) __gst_tag_list_copy, NULL,
@@ -724,7 +724,7 @@ __gst_tag_list_free (GstTagList * list)
   memset (list, 0xff, sizeof (GstTagListImpl));
 #endif
 
-  g_slice_free1 (sizeof (GstTagListImpl), list);
+  g_free (list);
 }
 
 static GstTagList *
