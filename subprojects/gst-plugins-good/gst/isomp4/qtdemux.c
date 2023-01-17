c03a6d0ea28d4c90e58bb6f7f2aff3393943b8d6
Sebastian Dröge <sebastian@centricular.com>
2023-01-17 17:50:27 +0200

    isomp4: Add `stream-format = (string) obu-stream` to AV1 caps
    Anything else is not allowed in MP4.
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/3741>

--- subprojects/gst-plugins-good/gst/isomp4/qtdemux.c
+++ subprojects/gst-plugins-good/gst/isomp4/qtdemux.c
@@ -14761,7 +14761,9 @@ qtdemux_video_caps (GstQTDemux * qtdemux, QtDemuxStream * stream,
       break;
     case FOURCC_av01:
       _codec ("AV1");
-      caps = gst_caps_new_empty_simple ("video/x-av1");
+      caps = gst_caps_new_simple ("video/x-av1",
+          "stream-format", G_TYPE_STRING, "obu-stream",
+          "alignment", G_TYPE_STRING, "tu", NULL);
       break;
     case GST_MAKE_FOURCC ('k', 'p', 'c', 'd'):
     default:
