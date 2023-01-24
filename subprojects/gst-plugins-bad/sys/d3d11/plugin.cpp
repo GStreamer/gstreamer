c5313d44da2d693450453e422d32d11988cf0cb3
Aleksandr Slobodeniuk <aslobodeniuk@fluendo.com>
2023-01-24 16:26:47 +0100

    d3d11: introduce new element d3d11d2d1
    It's used for drawing with Direct 2d API on
    top of the DirectX texture.
    NOTE: currently missing optimizations

--- subprojects/gst-plugins-bad/sys/d3d11/plugin.cpp
+++ subprojects/gst-plugins-bad/sys/d3d11/plugin.cpp
@@ -85,6 +85,9 @@
 #ifdef HAVE_D3D11_VIDEO_PROC
 #include "gstd3d11deinterlace.h"
 #endif
+#ifdef HAVE_D2D1
+#include "gstd3d11d2d1.h"
+#endif
 
 GST_DEBUG_CATEGORY (gst_d3d11_debug);
 GST_DEBUG_CATEGORY (gst_d3d11_shader_debug);
@@ -114,6 +117,10 @@ GST_DEBUG_CATEGORY (gst_d3d11_screen_capture_device_debug);
 
 #ifdef HAVE_D3D11_VIDEO_PROC
 GST_DEBUG_CATEGORY (gst_d3d11_deinterlace_debug);
+#endif
+
+#ifdef HAVE_D2D1
+GST_DEBUG_CATEGORY(gst_d3d11_d2d1_debug);
 #endif
 
 #define GST_CAT_DEFAULT gst_d3d11_debug
@@ -170,6 +177,11 @@ plugin_init (GstPlugin * plugin)
 #ifdef HAVE_D3D11_VIDEO_PROC
   GST_DEBUG_CATEGORY_INIT (gst_d3d11_deinterlace_debug,
       "d3d11deinterlace", 0, "Direct3D11 Deinterlacer");
+#endif
+
+#ifdef HAVE_D2D1
+  GST_DEBUG_CATEGORY_INIT(gst_d3d11_d2d1_debug,
+      "d3d11d2d1", 0, "d3d11d2d1 element");
 #endif
 
   /* Enumerate devices to register decoders per device and to get the highest
@@ -269,6 +281,11 @@ plugin_init (GstPlugin * plugin)
         "d3d11screencapturedeviceprovider", GST_RANK_PRIMARY,
         GST_TYPE_D3D11_SCREEN_CAPTURE_DEVICE_PROVIDER);
   }
+#endif
+
+#ifdef HAVE_D2D1
+  gst_element_register(plugin,
+      "d3d11d2d1", GST_RANK_NONE, GST_TYPE_D3D11D2D1);
 #endif
 
   return TRUE;
