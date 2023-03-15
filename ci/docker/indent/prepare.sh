f75d4f5f9f90187730229ab6f23dc9f536edd762
Tim-Philipp Müller <tim@centricular.com>
2023-03-15 19:01:47 +0000

    ci: update indent image to GNU indent 2.2.12
    ... and dotnet-sdk to 7.0.
    
    Fixes #340
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/4182>

--- ci/docker/indent/prepare.sh
+++ ci/docker/indent/prepare.sh
@@ -5,11 +5,12 @@ apt update -yqq
 apt install -y gnupg apt-transport-https
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.asc.gpg
 mv microsoft.asc.gpg /etc/apt/trusted.gpg.d/
-curl -O https://packages.microsoft.com/config/debian/9/prod.list
+# FIXME: this is bullseye, but image is actually bookworm (testing at the time)
+curl -O https://packages.microsoft.com/config/debian/11/prod.list
 mv prod.list /etc/apt/sources.list.d/microsoft-prod.list
 chown root:root /etc/apt/trusted.gpg.d/microsoft.asc.gpg
 chown root:root /etc/apt/sources.list.d/microsoft-prod.list
 apt update -yqq
-apt install -y dotnet-sdk-5.0
+apt install -y dotnet-sdk-7.0
 dotnet tool install --global dotnet-format
 ln -s ~/.dotnet/tools/dotnet-format /usr/local/bin/dotnet-format
