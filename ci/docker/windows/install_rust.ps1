4e8d0b766cd724897c1d2b0fd0dcb3c56ea3f87a
Jordan Petridis <jordan@centricular.com>
2023-04-03 16:23:02 +0300

    ci: Add rust in the windows base image
    This is preperation for gstreamer/gstreamer!3889
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/4327>

---
+++ ci/docker/windows/install_rust.ps1
@@ -0,0 +1,18 @@
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;
+
+$rust_version = '1.68.2'
+$rustup_url = 'https://win.rustup.rs/x86_64'
+
+Invoke-WebRequest -Uri $rustup_url -OutFile C:\rustup-init.exe
+
+if (!$?) {
+  Write-Host "Failed to download rustup"
+  Exit 1
+}
+
+C:\rustup-init.exe -y --profile minimal --default-toolchain $rust_version
+
+if (!$?) {
+  Write-Host "Failed to install rust"
+  Exit 1
+}
