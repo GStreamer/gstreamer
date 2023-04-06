15b7c7ea2537c54cc9abe6125b9806c834272a75
Matthias Fuchs <matthias1.fuchs@zeiss.com>
2023-04-06 08:54:30 +0200

    glvideoflip: fix leaked caps
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/4394>

--- subprojects/gst-plugins-base/ext/gl/gstglvideoflip.c
+++ subprojects/gst-plugins-base/ext/gl/gstglvideoflip.c
@@ -345,10 +345,13 @@ _set_active_method (GstGLVideoFlip * vf, GstVideoOrientationMethod method,
   gst_caps_append (output_caps, gst_caps_ref (templ));
   GST_OBJECT_UNLOCK (vf);
 
-  g_object_set (vf->input_capsfilter, "caps", gst_caps_ref (caps), NULL);
+  g_object_set (vf->input_capsfilter, "caps", caps, NULL);
   g_object_set (vf->output_capsfilter, "caps", output_caps, NULL);
   g_object_set (vf->transformation, "rotation-z", rot_z, "scale-x", scale_x,
       "scale-y", scale_y, NULL);
+
+  gst_caps_unref (output_caps);
+
   GST_OBJECT_LOCK (vf);
 }
 
