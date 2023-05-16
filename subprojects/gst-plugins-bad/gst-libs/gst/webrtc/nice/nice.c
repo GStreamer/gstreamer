556449a9177b86b755840118ea6eedcd649b8732
Tim-Philipp Müller <tim@centricular.com>
2023-05-16 16:17:05 +0100

    Revert "webrtc/nice: support consent-freshness RFC7675"
    This reverts commit 0161687505156f54ff38b976668804ac2763a863.
    
    This causes problems when the connection is congested because
    libnice uses a too agressive timeout contrary to the spec, so
    it's easy for consent to lapse and streaming to stop if there's
    packet loss.
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/4649>

--- subprojects/gst-plugins-bad/gst-libs/gst/webrtc/nice/nice.c
+++ subprojects/gst-plugins-bad/gst-libs/gst/webrtc/nice/nice.c
@@ -1583,7 +1583,6 @@ gst_webrtc_nice_constructed (GObject * object)
 
   options |= NICE_AGENT_OPTION_ICE_TRICKLE;
   options |= NICE_AGENT_OPTION_REGULAR_NOMINATION;
-  options |= NICE_AGENT_OPTION_CONSENT_FRESHNESS;
 
   ice->priv->nice_agent = nice_agent_new_full (ice->priv->main_context,
       NICE_COMPATIBILITY_RFC5245, options);
