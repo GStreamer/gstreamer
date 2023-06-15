GStreamer/gstreamer

commit 6c2f6c3bd4dd525eb02c6d7000e84b76663d3daf

all: Use new navigation interface and API
Use and implement the new navigation interface in all relevant sink elements,
and use API functions everywhere instead of directy accessing the event structure.

Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/1633>

subprojects/gst-plugins-bad/sys/d3dvideosink/d3dvideosink.c
@@ -70,7 +70,7 @@ static void gst_d3dvideosink_expose (GstVideoOverlay * overlay);
 static void gst_d3dvideosink_navigation_interface_init (GstNavigationInterface *
     iface);
 static void gst_d3dvideosink_navigation_send_event (GstNavigation * navigation,
-    GstStructure * structure);
+    GstEvent * event);
 /* GObject */
 static void gst_d3dvideosink_set_property (GObject * object, guint prop_id,
     const GValue * value, GParamSpec * pspec);
@@ -580,7 +580,7 @@ gst_d3dvideosink_video_overlay_interface_init (GstVideoOverlayInterface * iface)
 static void
 gst_d3dvideosink_navigation_interface_init (GstNavigationInterface * iface)
 {
-  iface->send_event = gst_d3dvideosink_navigation_send_event;
+  iface->send_event_simple = gst_d3dvideosink_navigation_send_event;
 }
 
 /* Video Render Code */
@@ -622,23 +622,19 @@ gst_d3dvideosink_show_frame (GstVideoSink * vsink, GstBuffer * buffer)
 
 static void
 gst_d3dvideosink_navigation_send_event (GstNavigation * navigation,
-    GstStructure * structure)
+    GstEvent * event)
 {
   GstD3DVideoSink *sink = GST_D3DVIDEOSINK (navigation);
-  GstEvent *e;
 
-  if ((e = gst_event_new_navigation (structure))) {
-    GstPad *pad;
-    if ((pad = gst_pad_get_peer (GST_VIDEO_SINK_PAD (sink)))) {
-      if (!gst_pad_send_event (pad, gst_event_ref (e))) {
-        /* If upstream didn't handle the event we'll post a message with it
-         * for the application in case it wants to do something with it */
-        gst_element_post_message (GST_ELEMENT_CAST (sink),
-            gst_navigation_message_new_event (GST_OBJECT_CAST (sink), e));
-      }
-      gst_event_unref (e);
-      gst_object_unref (pad);
+  GstPad *pad;
+  if ((pad = gst_pad_get_peer (GST_VIDEO_SINK_PAD (sink)))) {
+    if (!gst_pad_send_event (pad, gst_event_ref (event))) {
+      /* If upstream didn't handle the event we'll post a message with it
+       * for the application in case it wants to do something with it */
+      gst_element_post_message (GST_ELEMENT_CAST (sink),
+          gst_navigation_message_new_event (GST_OBJECT_CAST (sink), event));
     }
+    gst_object_unref (pad);
   }
 }
 

subprojects/gst-plugins-bad/ext/directfb/dfbvideosink.c
@@ -605,8 +605,8 @@ gst_dfbvideosink_event_thread (GstDfbVideoSink * dfbvideosink)
             default:
               GST_DEBUG_OBJECT (dfbvideosink, "key press event %c !",
                   event.input.key_symbol);
-              gst_navigation_send_key_event (GST_NAVIGATION (dfbvideosink),
-                  "key-press", "prout");
+              gst_dfbvideosink_navigation_send_event
+                  (gst_navigation_event_new_key_press ("prout"));
           }
         } else if (event.input.type == DIET_BUTTONPRESS) {
           gint x, y;
@@ -616,8 +616,9 @@ gst_dfbvideosink_event_thread (GstDfbVideoSink * dfbvideosink)
           GST_DEBUG_OBJECT (dfbvideosink, "button %d pressed at %dx%d",
               event.input.button, x, y);
 
-          gst_navigation_send_mouse_event (GST_NAVIGATION (dfbvideosink),
-              "mouse-button-press", event.input.button, x, y);
+          gst_dfbvideosink_navigation_send_event
+              (gst_navigation_event_new_mouse_button_press (event.input.button,
+                  x, y));
         } else if (event.input.type == DIET_BUTTONRELEASE) {
           gint x, y;
 
@@ -626,14 +627,15 @@ gst_dfbvideosink_event_thread (GstDfbVideoSink * dfbvideosink)
           GST_DEBUG_OBJECT (dfbvideosink, "button %d released at %dx%d",
               event.input.button, x, y);
 
-          gst_navigation_send_mouse_event (GST_NAVIGATION (dfbvideosink),
-              "mouse-button-release", event.input.button, x, y);
+          gst_dfbvideosink_navigation_send_event
+              (gst_navigation_event_new_mouse_button_release
+              (event.input.button, x, y));
         } else if (event.input.type == DIET_AXISMOTION) {
           gint x, y;
 
           dfbvideosink->layer->GetCursorPosition (dfbvideosink->layer, &x, &y);
-          gst_navigation_send_mouse_event (GST_NAVIGATION (dfbvideosink),
-              "mouse-move", 0, x, y);
+          gst_dfbvideosink_navigation_send_event
+              (gst_navigation_event_new_mouse_move (x, y));
         } else {
           GST_WARNING_OBJECT (dfbvideosink, "unhandled event type %d",
               event.input.type);
@@ -1983,10 +1985,9 @@ beach:
 
 static void
 gst_dfbvideosink_navigation_send_event (GstNavigation * navigation,
-    GstStructure * structure)
+    GstEvent * event)
 {
   GstDfbVideoSink *dfbvideosink = GST_DFBVIDEOSINK (navigation);
-  GstEvent *event;
   GstVideoRectangle dst = { 0, };
   GstVideoRectangle src = { 0, };
   GstVideoRectangle result;
@@ -1999,13 +2000,15 @@ gst_dfbvideosink_navigation_send_event (GstNavigation * navigation,
   dst.h = dfbvideosink->out_height;
   gst_video_sink_center_rect (src, dst, &result, dfbvideosink->hw_scaling);
 
-  event = gst_event_new_navigation (structure);
+  event = gst_event_make_writable (event);
 
   /* Our coordinates can be wrong here if we centered the video */
 
   /* Converting pointer coordinates to the non scaled geometry */
-  if (gst_structure_get_double (structure, "pointer_x", &old_x)) {
+  if gst_navigation_event_get_coordinates
+    (event, &old_x, &old_y) {
     x = old_x;
+    y = old_y;
 
     if (x >= result.x && x <= (result.x + result.w)) {
       x -= result.x;
@@ -2014,13 +2017,6 @@ gst_dfbvideosink_navigation_send_event (GstNavigation * navigation,
     } else {
       x = 0;
     }
-    GST_DEBUG_OBJECT (dfbvideosink, "translated navigation event x "
-        "coordinate from %f to %f", old_x, x);
-    gst_structure_set (structure, "pointer_x", G_TYPE_DOUBLE, x, NULL);
-  }
-  if (gst_structure_get_double (structure, "pointer_y", &old_y)) {
-    y = old_y;
-
     if (y >= result.y && y <= (result.y + result.h)) {
       y -= result.y;
       y *= dfbvideosink->video_height;
@@ -2028,14 +2024,17 @@ gst_dfbvideosink_navigation_send_event (GstNavigation * navigation,
     } else {
       y = 0;
     }
+
+    GST_DEBUG_OBJECT (dfbvideosink, "translated navigation event x "
+        "coordinate from %f to %f", old_x, x);
     GST_DEBUG_OBJECT (dfbvideosink, "translated navigation event y "
         "coordinate from %fd to %fd", old_y, y);
-    gst_structure_set (structure, "pointer_y", G_TYPE_DOUBLE, y, NULL);
-  }
+    gst_navigation_event_set_coordinates (event, x, y);
+    }
 
   pad = gst_pad_get_peer (GST_VIDEO_SINK_PAD (dfbvideosink));
 
-  if (GST_IS_PAD (pad) && GST_IS_EVENT (event)) {
+  if (GST_IS_PAD (pad)) {
     if (!gst_pad_send_event (pad, gst_event_ref (event))) {
       /* If upstream didn't handle the event we'll post a message with it
        * for the application in case it wants to do something with it */
@@ -2051,7 +2050,7 @@ gst_dfbvideosink_navigation_send_event (GstNavigation * navigation,
 static void
 gst_dfbvideosink_navigation_init (GstNavigationInterface * iface)
 {
-  iface->send_event = gst_dfbvideosink_navigation_send_event;
+  iface->send_event_simple = gst_dfbvideosink_navigation_send_event;
 }
 
 static void

subprojects/gst-plugins-bad/ext/opencv/gsthanddetect.cpp
@@ -122,7 +122,7 @@ static CascadeClassifier *gst_handdetect_load_profile (GstHanddetect * filter,
 static void gst_handdetect_navigation_interface_init (GstNavigationInterface *
     iface);
 static void gst_handdetect_navigation_send_event (GstNavigation * navigation,
-    GstStructure * structure);
+    GstEvent * event);
 
 G_DEFINE_TYPE_WITH_CODE (GstHanddetect, gst_handdetect,
     GST_TYPE_OPENCV_VIDEO_FILTER,
@@ -136,7 +136,7 @@ GST_ELEMENT_REGISTER_DEFINE (handdetect, "handdetect", GST_RANK_NONE,
 static void
 gst_handdetect_navigation_interface_init (GstNavigationInterface * iface)
 {
-  iface->send_event = gst_handdetect_navigation_send_event;
+  iface->send_event_simple = gst_handdetect_navigation_send_event;
 }
 
 /* FIXME: this function used to parse the region of interests coordinates
@@ -146,14 +146,12 @@ gst_handdetect_navigation_interface_init (GstNavigationInterface * iface)
  */
 static void
 gst_handdetect_navigation_send_event (GstNavigation * navigation,
-    GstStructure * structure)
+    GstEvent * event)
 {
   GstHanddetect *filter = GST_HANDDETECT (navigation);
   GstPad *peer;
 
   if ((peer = gst_pad_get_peer (GST_BASE_TRANSFORM_CAST (filter)->sinkpad))) {
-    GstEvent *event;
-    event = gst_event_new_navigation (structure);
     gst_pad_send_event (peer, event);
     gst_object_unref (peer);
   }
@@ -481,11 +479,10 @@ gst_handdetect_transform_ip (GstOpencvVideoFilter * transform,
          * !!! this will CHANGE in the future !!!
          * !!! by adding gst_navigation_send_hand_detect_event() in navigation.c !!!
          */
-        gst_navigation_send_mouse_event (GST_NAVIGATION (filter),
-            "mouse-move",
-            0,
-            (double) (filter->best_r->x + filter->best_r->width * 0.5),
-            (double) (filter->best_r->y + filter->best_r->height * 0.5));
+        gst_handdetect_navigation_send_event (GST_NAVIGATION (filter),
+            gst_navigation_event_new_mouse_move (
+              (double) (filter->best_r->x + filter->best_r->width * 0.5),
+              (double) (filter->best_r->y + filter->best_r->height * 0.5)));
 
 #endif
       }
@@ -571,11 +568,10 @@ gst_handdetect_transform_ip (GstOpencvVideoFilter * transform,
            * !!! this will CHANGE in the future !!!
            * !!! by adding gst_navigation_send_hand_detect_event() in navigation.c !!!
            */
-          gst_navigation_send_mouse_event (GST_NAVIGATION (filter),
-              "mouse-move",
-              0,
-              (double) (filter->best_r->x + filter->best_r->width * 0.5),
-              (double) (filter->best_r->y + filter->best_r->height * 0.5));
+          gst_handdetect_navigation_send_event (GST_NAVIGATION (filter),
+              gst_navigation_event_new_mouse_move (
+                (double) (filter->best_r->x + filter->best_r->width * 0.5),
+                (double) (filter->best_r->y + filter->best_r->height * 0.5)));
 
           /* or use another way to send upstream navigation event for debug
            *

subprojects/gst-plugins-bad/ext/vulkan/vksink.c
@@ -632,69 +632,79 @@ _display_size_to_stream_size (GstVulkanSink * vk_sink,
 
 static void
 gst_vulkan_sink_navigation_send_event (GstNavigation * navigation,
-    GstStructure * structure)
+    GstEvent * event)
 {
   GstVulkanSink *vk_sink = GST_VULKAN_SINK (navigation);
   GstVideoRectangle display_rect;
-  GstEvent *event = NULL;
+  gboolean handled;
   gdouble x, y;
 
   if (!vk_sink->swapper || !vk_sink->swapper->window) {
-    gst_structure_free (structure);
+    gst_event_unref (event);
     return;
   }
+
+  event = gst_event_make_writable (event);
 
   gst_vulkan_swapper_get_surface_rectangles (vk_sink->swapper, NULL, NULL,
       &display_rect);
 
   /* Converting pointer coordinates to the non scaled geometry */
   if (display_rect.w != 0 && display_rect.h != 0
-      && gst_structure_get_double (structure, "pointer_x", &x)
-      && gst_structure_get_double (structure, "pointer_y", &y)) {
+      && gst_navigation_event_get_coordinates (event, &x, &y)) {
     gdouble stream_x, stream_y;
 
     _display_size_to_stream_size (vk_sink, &display_rect, x, y, &stream_x,
         &stream_y);
-
-    gst_structure_set (structure, "pointer_x", G_TYPE_DOUBLE,
-        stream_x, "pointer_y", G_TYPE_DOUBLE, stream_y, NULL);
+    gst_navigation_event_set_coordinates (event, stream_x, stream_y);
   }
 
-  event = gst_event_new_navigation (structure);
-  if (event) {
-    gboolean handled;
+  gst_event_ref (event);
+  handled = gst_pad_push_event (GST_VIDEO_SINK_PAD (vk_sink), event);
 
-    gst_event_ref (event);
-    handled = gst_pad_push_event (GST_VIDEO_SINK_PAD (vk_sink), event);
+  if (!handled)
+    gst_element_post_message ((GstElement *) vk_sink,
+        gst_navigation_message_new_event ((GstObject *) vk_sink, event));
 
-    if (!handled)
-      gst_element_post_message ((GstElement *) vk_sink,
-          gst_navigation_message_new_event ((GstObject *) vk_sink, event));
-
-    gst_event_unref (event);
-  }
+  gst_event_unref (event);
 }
 
 static void
 gst_vulkan_sink_navigation_interface_init (GstNavigationInterface * iface)
 {
-  iface->send_event = gst_vulkan_sink_navigation_send_event;
+  iface->send_event_simple = gst_vulkan_sink_navigation_send_event;
 }
 
 static void
 gst_vulkan_sink_key_event_cb (GstVulkanWindow * window, char *event_name, char
     *key_string, GstVulkanSink * vk_sink)
 {
+  GstEvent *event = NULL;
+
   GST_DEBUG_OBJECT (vk_sink, "event %s key %s pressed", event_name, key_string);
-  gst_navigation_send_key_event (GST_NAVIGATION (vk_sink),
-      event_name, key_string);
+  if (0 == g_strcmp0 ("key-press", event_name))
+    event = gst_navigation_event_new_key_press (key_string);
+  else if (0 == g_strcmp0 ("key-release", event_name))
+    event = gst_navigation_event_new_key_release (key_string);
+
+  if (event)
+    gst_navigation_send_event_simple (GST_NAVIGATION (vk_sink), event);
 }
 
 static void
 gst_vulkan_sink_mouse_event_cb (GstVulkanWindow * window, char *event_name,
     int button, double posx, double posy, GstVulkanSink * vk_sink)
 {
+  GstEvent *event = NULL;
+
   GST_DEBUG_OBJECT (vk_sink, "event %s at %g, %g", event_name, posx, posy);
-  gst_navigation_send_mouse_event (GST_NAVIGATION (vk_sink),
-      event_name, button, posx, posy);
+  if (0 == g_strcmp0 ("mouse-button-press", event_name))
+    event = gst_navigation_event_new_mouse_button_press (button, posx, posy);
+  else if (0 == g_strcmp0 ("mouse-button-release", event_name))
+    event = gst_navigation_event_new_mouse_button_release (button, posx, posy);
+  else if (0 == g_strcmp0 ("mouse-move", event_name))
+    event = gst_navigation_event_new_mouse_move (posx, posy);
+
+  if (event)
+    gst_navigation_send_event_simple (GST_NAVIGATION (vk_sink), event);
 }

subprojects/gst-plugins-bad/gst/librfb/gstrfbsrc.c
@@ -551,11 +551,7 @@ gst_rfb_src_event (GstBaseSrc * bsrc, GstEvent * event)
   GstRfbSrc *src = GST_RFB_SRC (bsrc);
   gdouble x, y;
   gint button;
-  const GstStructure *structure;
-  const gchar *event_type;
-  gboolean key_event, key_press;
-
-  key_event = FALSE;
+  GstNavigationEventType event_type;
 
   switch (GST_EVENT_TYPE (event)) {
     case GST_EVENT_NAVIGATION:
@@ -564,55 +560,62 @@ gst_rfb_src_event (GstBaseSrc * bsrc, GstEvent * event)
       if (src->view_only)
         break;
 
-      structure = gst_event_get_structure (event);
-      event_type = gst_structure_get_string (structure, "event");
-
-      if (strcmp (event_type, "key-press") == 0) {
-        key_event = key_press = TRUE;
-      } else if (strcmp (event_type, "key-release") == 0) {
-        key_event = TRUE;
-        key_press = FALSE;
-      }
-
-      if (key_event) {
+      event_type = gst_navigation_event_get_type (event);
+      switch (event_type) {
 #ifdef HAVE_X11
-        const gchar *key;
-        KeySym key_sym;
+        case GST_NAVIGATION_EVENT_KEY_PRESS:
+        case GST_NAVIGATION_EVENT_KEY_RELEASE:{
+          const gchar *key;
+          KeySym key_sym;
 
-        key = gst_structure_get_string (structure, "key");
-        key_sym = XStringToKeysym (key);
+          gst_navigation_event_parse_key_event (event, &key);
+          key_sym = XStringToKeysym (key);
 
-        if (key_sym != NoSymbol)
-          rfb_decoder_send_key_event (src->decoder, key_sym, key_press);
+          if (key_sym != NoSymbol)
+            rfb_decoder_send_key_event (src->decoder, key_sym,
+                event_type == GST_NAVIGATION_EVENT_KEY_PRESS);
+          break;
+        }
 #endif
-        break;
-      }
-
-      gst_structure_get_double (structure, "pointer_x", &x);
-      gst_structure_get_double (structure, "pointer_y", &y);
-      gst_structure_get_int (structure, "button", &button);
-
-      /* we need to take care of the offset's */
-      x += src->decoder->offset_x;
-      y += src->decoder->offset_y;
-
-      if (strcmp (event_type, "mouse-move") == 0) {
-        GST_LOG_OBJECT (src, "sending mouse-move event "
-            "button_mask=%d, x=%d, y=%d", src->button_mask, (gint) x, (gint) y);
-        rfb_decoder_send_pointer_event (src->decoder, src->button_mask,
-            (gint) x, (gint) y);
-      } else if (strcmp (event_type, "mouse-button-release") == 0) {
-        src->button_mask &= ~(1 << (button - 1));
-        GST_LOG_OBJECT (src, "sending mouse-button-release event "
-            "button_mask=%d, x=%d, y=%d", src->button_mask, (gint) x, (gint) y);
-        rfb_decoder_send_pointer_event (src->decoder, src->button_mask,
-            (gint) x, (gint) y);
-      } else if (strcmp (event_type, "mouse-button-press") == 0) {
-        src->button_mask |= (1 << (button - 1));
-        GST_LOG_OBJECT (src, "sending mouse-button-press event "
-            "button_mask=%d, x=%d, y=%d", src->button_mask, (gint) x, (gint) y);
-        rfb_decoder_send_pointer_event (src->decoder, src->button_mask,
-            (gint) x, (gint) y);
+        case GST_NAVIGATION_EVENT_MOUSE_BUTTON_PRESS:{
+          gst_navigation_event_parse_mouse_button_event (event,
+              &button, &x, &y);
+          x += src->decoder->offset_x;
+          y += src->decoder->offset_y;
+          src->button_mask |= (1 << (button - 1));
+          GST_LOG_OBJECT (src, "sending mouse-button-press event "
+              "button_mask=%d, x=%d, y=%d",
+              src->button_mask, (gint) x, (gint) y);
+          rfb_decoder_send_pointer_event (src->decoder, src->button_mask,
+              (gint) x, (gint) y);
+          break;
+        }
+        case GST_NAVIGATION_EVENT_MOUSE_BUTTON_RELEASE:{
+          gst_navigation_event_parse_mouse_button_event (event,
+              &button, &x, &y);
+          x += src->decoder->offset_x;
+          y += src->decoder->offset_y;
+          src->button_mask &= ~(1 << (button - 1));
+          GST_LOG_OBJECT (src, "sending mouse-button-release event "
+              "button_mask=%d, x=%d, y=%d",
+              src->button_mask, (gint) x, (gint) y);
+          rfb_decoder_send_pointer_event (src->decoder, src->button_mask,
+              (gint) x, (gint) y);
+          break;
+        }
+        case GST_NAVIGATION_EVENT_MOUSE_MOVE:{
+          gst_navigation_event_parse_mouse_move_event (event, &x, &y);
+          x += src->decoder->offset_x;
+          y += src->decoder->offset_y;
+          GST_LOG_OBJECT (src, "sending mouse-move event "
+              "button_mask=%d, x=%d, y=%d",
+              src->button_mask, (gint) x, (gint) y);
+          rfb_decoder_send_pointer_event (src->decoder, src->button_mask,
+              (gint) x, (gint) y);
+          break;
+        }
+        default:
+          break;
       }
       break;
     default:

subprojects/gst-plugins-bad/sys/d3d11/gstd3d11videosink.cpp
@@ -687,9 +687,17 @@ static void
 gst_d3d11_video_sink_key_event (GstD3D11Window * window, const gchar * event,
     const gchar * key, GstD3D11VideoSink * self)
 {
+  GstEvent *key_event = NULL;
+
   if (self->enable_navigation_events) {
     GST_LOG_OBJECT (self, "send key event %s, key %s", event, key);
-    gst_navigation_send_key_event (GST_NAVIGATION (self), event, key);
+    if (0 == g_strcmp0 ("key-press", event))
+      key_event = gst_navigation_event_new_key_press (key);
+    else if (0 == g_strcmp0 ("key-release", event))
+      key_event = gst_navigation_event_new_key_release (key);
+
+    if (event)
+      gst_navigation_send_event_simple (GST_NAVIGATION (self), key_event);
   }
 }
 
@@ -697,11 +705,20 @@ static void
 gst_d3d11_video_mouse_key_event (GstD3D11Window * window, const gchar * event,
     gint button, gdouble x, gdouble y, GstD3D11VideoSink * self)
 {
+  GstEvent *mouse_event = NULL;
+
   if (self->enable_navigation_events) {
     GST_LOG_OBJECT (self,
         "send mouse event %s, button %d (%.1f, %.1f)", event, button, x, y);
-    gst_navigation_send_mouse_event (GST_NAVIGATION (self), event, button, x,
-        y);
+    if (0 == g_strcmp0 ("mouse-button-press", event))
+      mouse_event = gst_navigation_event_new_mouse_button_press (button, x, y);
+    else if (0 == g_strcmp0 ("mouse-button-release", event))
+      mouse_event = gst_navigation_event_new_mouse_button_release (button, x, y);
+    else if (0 == g_strcmp0 ("mouse-move", event))
+      mouse_event = gst_navigation_event_new_mouse_move (x, y);
+
+    if (event)
+      gst_navigation_send_event_simple (GST_NAVIGATION (self), mouse_event);
   }
 }
 
@@ -1330,10 +1347,9 @@ gst_d3d11_video_sink_video_overlay_init (GstVideoOverlayInterface * iface)
 /* Navigation interface */
 static void
 gst_d3d11_video_sink_navigation_send_event (GstNavigation * navigation,
-    GstStructure * structure)
+    GstEvent * event)
 {
   GstD3D11VideoSink *self = GST_D3D11_VIDEO_SINK (navigation);
-  GstEvent *event = gst_event_new_navigation (structure);
 
   /* TODO: add support for translating native coordinate and video coordinate
    * when force-aspect-ratio is set */
@@ -1354,7 +1370,7 @@ gst_d3d11_video_sink_navigation_send_event (GstNavigation * navigation,
 static void
 gst_d3d11_video_sink_navigation_init (GstNavigationInterface * iface)
 {
-  iface->send_event = gst_d3d11_video_sink_navigation_send_event;
+  iface->send_event_simple = gst_d3d11_video_sink_navigation_send_event;
 }
 
 static gboolean

subprojects/gst-plugins-bad/sys/d3dvideosink/d3dhelpers.c
@@ -2140,11 +2140,11 @@ d3d_wnd_proc (HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
           gchar *utfrep = g_utf16_to_utf8 (wcrep, 128, NULL, NULL, NULL);
           if (utfrep) {
             if (message == WM_KEYDOWN)
-              gst_navigation_send_key_event (GST_NAVIGATION (sink), "key-press",
-                  utfrep);
+              gst_navigation_send_event_simple (GST_NAVIGATION (sink),
+                  gst_navigation_event_new_key_press (utfrep));
             else if (message == WM_KEYUP)
-              gst_navigation_send_key_event (GST_NAVIGATION (sink),
-                  "key-release", utfrep);
+              gst_navigation_send_event_simple (GST_NAVIGATION (sink),
+                  gst_navigation_event_new_key_release (utfrep));
             g_free (utfrep);
           }
         }
@@ -2161,44 +2161,37 @@ d3d_wnd_proc (HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
       if (sink->enable_navigation_events
           && d3d_get_render_coordinates (sink, LOWORD (lParam), HIWORD (lParam),
               &x, &y)) {
-        gint button;
-        const gchar *action = NULL;
         switch (message) {
           case WM_MOUSEMOVE:
-            button = 0;
-            action = "mouse-move";
+            gst_navigation_send_event_simple (GST_NAVIGATION (sink),
+                gst_navigation_event_new_mouse_move (x, y));
             break;
           case WM_LBUTTONDOWN:
-            button = 1;
-            action = "mouse-button-press";
+            gst_navigation_send_event_simple (GST_NAVIGATION (sink),
+                gst_navigation_event_new_mouse_button_press (1, x, y));
             break;
           case WM_LBUTTONUP:
-            button = 1;
-            action = "mouse-button-release";
+            gst_navigation_send_event_simple (GST_NAVIGATION (sink),
+                gst_navigation_event_new_mouse_button_release (1, x, y));
             break;
           case WM_RBUTTONDOWN:
-            button = 2;
-            action = "mouse-button-press";
+            gst_navigation_send_event_simple (GST_NAVIGATION (sink),
+                gst_navigation_event_new_mouse_button_press (2, x, y));
             break;
           case WM_RBUTTONUP:
-            button = 2;
-            action = "mouse-button-release";
+            gst_navigation_send_event_simple (GST_NAVIGATION (sink),
+                gst_navigation_event_new_mouse_button_release (2, x, y));
             break;
           case WM_MBUTTONDOWN:
-            button = 3;
-            action = "mouse-button-press";
+            gst_navigation_send_event_simple (GST_NAVIGATION (sink),
+                gst_navigation_event_new_mouse_button_press (3, x, y));
             break;
           case WM_MBUTTONUP:
-            button = 3;
-            action = "mouse-button-release";
+            gst_navigation_send_event_simple (GST_NAVIGATION (sink),
+                gst_navigation_event_new_mouse_button_release (3, x, y));
             break;
           default:
             break;
-        }
-        if (action) {
-          /* GST_DEBUG_OBJECT(sink, "%s: %lfx%lf", action, x, y); */
-          gst_navigation_send_mouse_event (GST_NAVIGATION (sink), action,
-              button, x, y);
         }
       }
       break;

subprojects/gst-plugins-bad/sys/directshow/dshowvideosink.cpp
@@ -155,13 +155,10 @@ gst_dshowvideosink_videooverlay_init (GstVideoOverlayInterface * iface)
 
 static void
 gst_dshowvideosink_navigation_send_event (GstNavigation * navigation,
-    GstStructure * structure)
+    GstEvent * event)
 {
   GstDshowVideoSink *sink = GST_DSHOWVIDEOSINK (navigation);
-  GstEvent *event = NULL;
   GstPad *pad = NULL;
-
-  event = gst_event_new_navigation (structure);
 
   /* FXIME: handle aspect ratio. */
 
@@ -178,10 +175,9 @@ static void
 gst_dshowvideosink_navigation_interface_init (GstNavigationInterface * iface)
 {
   /* FIXME: navigation interface partially implemented.
-   * Need to call gst_navigation_send_mouse_event and
-   * gst_navigation_send_key_event like in directdrawsink.
+   * Need to call gst_navigation_send_event_simple and like in directdrawsink.
    */
-  iface->send_event = gst_dshowvideosink_navigation_send_event;
+  iface->send_event_simple = gst_dshowvideosink_navigation_send_event;
 }
 
 static void

commit 110ec5d4b209813e1f400de009e783d949f5aebb

d3dvideosink: Fix navigation event leak
Fixing regression introduced in 6c2f6c3bd4dd525eb02c6d7000e84b76663d3daf

Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/4863>

subprojects/gst-plugins-bad/sys/d3dvideosink/d3dvideosink.c
@@ -634,6 +634,7 @@ gst_d3dvideosink_navigation_send_event (GstNavigation * navigation,
       gst_element_post_message (GST_ELEMENT_CAST (sink),
           gst_navigation_message_new_event (GST_OBJECT_CAST (sink), event));
     }
+    gst_event_unref (event);
     gst_object_unref (pad);
   }
 }