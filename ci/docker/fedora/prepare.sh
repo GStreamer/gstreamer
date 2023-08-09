63b027047d52f622e70e463f95dbbfcb531757ac
Sebastian Dröge <sebastian@centricular.com>
2023-08-09 17:45:57 +0300

    ci: Update to cargo-c 0.9.23
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/5155>

--- ci/docker/fedora/prepare.sh
+++ ci/docker/fedora/prepare.sh
@@ -237,7 +237,7 @@ rm rustup-init;
 chmod -R a+w $RUSTUP_HOME $CARGO_HOME
 
 # Apparently rustup did not do that, and it fails now
-cargo install cargo-c --version 0.9.22+cargo-0.72
+cargo install cargo-c --version 0.9.23+cargo-0.72.2
 
 rustup --version
 cargo --version
