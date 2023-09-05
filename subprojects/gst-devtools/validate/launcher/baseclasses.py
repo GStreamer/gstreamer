GStreamer/gstreamer

commit ba61160d6c6a69104aa0f68370139d745af11484

validate-launcher: Fix race condition on repeated tests
Fixes https://gitlab.freedesktop.org/gstreamer/gstreamer/-/issues/2900

The `reports` list was being copied as a reference, therefore, copies of
a test ended up inadvertedly sharing the same list of reports. Reports
added by one instance of the test would be reflected in all instances.

This caused a race condition where, if a test was run on repeat with
gst-validate-launcher -f, very often wrong log file was shown to the
user. For instance, gst-validate-launcher would say "test failed, see
log for iteration7", but iteration7 would contain "TEST PASSED".

Worse, the runner would add the report to that incorrect log file,
mixing problems between different executions of the tests.

Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/5177>

subprojects/gst-devtools/validate/launcher/baseclasses.py
@@ -163,6 +163,7 @@ class Test(Loggable):
 
     def copy(self, nth=None):
         copied_test = copy.copy(self)
+        copied_test.reports = copy.deepcopy(self.reports)
         if nth:
             copied_test.classname += '_it' + str(nth)
             copied_test._uuid = None

commit 135f859629aa5383dd5aee3dd4cf2b11c46cf5d4

validate: Fix error on GstTest.copy()
Fixes regression introduced in ba61160d6c6a69104aa0f68370139d745af11484,
where running check tests with gst-validate-launcher -f would trigger
this exception:

    AttributeError: 'GstCheckTest' object has no attribute 'reports'.
    Did you mean: 'reporter'?

The member `reports` is meant to be just part of GstValidateTest, but
not other subclasses, even though a usage is still found in the base
class GstTest in the method test_end().

This patch introduces an override of the methods copy() and test_end()
in GstValidateTest so that `reports` is copied and cleared respectively,
but only for validate tests.

Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/5281>

subprojects/gst-devtools/validate/launcher/baseclasses.py
@@ -163,7 +163,6 @@ class Test(Loggable):
 
     def copy(self, nth=None):
         copied_test = copy.copy(self)
-        copied_test.reports = copy.deepcopy(self.reports)
         if nth:
             copied_test.classname += '_it' + str(nth)
             copied_test._uuid = None
@@ -743,10 +742,6 @@ class Test(Loggable):
             clean_env[n] = self.proc_env.get(n, None)
         self.proc_env = clean_env
 
-        # Don't keep around JSON report objects, they were processed
-        # in check_results already
-        self.reports = []
-
         return self.result
 
 
@@ -1001,6 +996,11 @@ class GstValidateTest(Test):
         self.speed = 1.0
         self.actions_infos = []
 
+    def copy(self, nth=None):
+        new_test = super().copy(nth=nth)
+        new_test.reports = copy.deepcopy(self.reports)
+        return new_test
+
     def build_arguments(self):
         super(GstValidateTest, self).build_arguments()
         if "GST_VALIDATE" in os.environ:
@@ -1232,6 +1232,15 @@ class GstValidateTest(Test):
         result = super(GstValidateTest, self).get_valgrind_suppressions()
         result.extend(utils.get_gst_build_valgrind_suppressions())
         return result
+
+    def test_end(self, retry_on_failures=False):
+        ret = super().test_end(retry_on_failures=retry_on_failures)
+
+        # Don't keep around JSON report objects, they were processed
+        # in check_results already
+        self.reports = []
+
+        return ret
 
 
 class VariableFramerateMode(Enum):