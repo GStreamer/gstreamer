080c837cd6f65a28fe85a9cf868284051387363d
Jordan Petridis <jordan@centricular.com>
2023-09-28 16:53:55 +0200

    ci: fedora: Install a more recent wayland-protocols version
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/1060>

--- ci/docker/fedora/prepare.sh
+++ ci/docker/fedora/prepare.sh
@@ -251,6 +251,14 @@ meson install -C _build
 cd ..
 rm -rf gdk-pixbuf
 
+# Install a more up to date wayland-protocols
+git clone --branch 1.32 https://gitlab.freedesktop.org/wayland/wayland-protocols.git
+cd wayland-protocols
+meson setup _build --prefix=/usr -Dtests=false
+meson install -C _build
+cd ..
+rm -rf wayland-protocols
+
 # Install Rust
 RUSTUP_VERSION=1.26.0
 RUST_VERSION=1.73.0
