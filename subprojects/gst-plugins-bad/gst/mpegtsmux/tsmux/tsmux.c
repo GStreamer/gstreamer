6198c4b8a10149e09376d71b6e1d4ba4a8233d2e
Jan Alexander Steffens (heftig) <jan.steffens@ltnglobal.com>
2023-10-16 23:54:20 +0200

    tsmux: Don't memset in pad_stream when writing a PCR packet
    tsmux_write_ts_header will write a stuffing adaptation field, so we
    don't need to prefill the buffer.
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/5516>

--- subprojects/gst-plugins-bad/gst/mpegtsmux/tsmux/tsmux.c
+++ subprojects/gst-plugins-bad/gst/mpegtsmux/tsmux/tsmux.c
@@ -1549,8 +1549,6 @@ pad_stream (TsMux * mux, TsMuxStream * stream, gint64 cur_ts)
         goto done;
       }
 
-      memset (map.data, 0xFF, map.size);
-
       new_pcr = write_new_pcr (mux, stream, get_current_pcr (mux, cur_ts),
           get_next_pcr (mux, cur_ts));
       if (new_pcr != -1) {
@@ -1565,6 +1563,7 @@ pad_stream (TsMux * mux, TsMuxStream * stream, gint64 cur_ts)
           goto done;
         }
         tsmux_write_null_ts_header (map.data);
+        memset (map.data + TSMUX_HEADER_LENGTH, 0xFF, TSMUX_PAYLOAD_LENGTH);
       }
 
       gst_buffer_unmap (buf, &map);
