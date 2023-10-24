7e6cb3e15f551a5589a824acfb78f67199bf8a65
Jordan Petridis <jordan@centricular.com>
2023-10-24 06:48:59 +0300

    create-subprojects-cache: Remove git gc call
    The repos on github/gitlab are mostly pruned when we
    clone them now, so we can remove the manual gc and make
    the build faster.
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/5629>

--- ci/docker/fedora/create-subprojects-cache.sh
+++ ci/docker/fedora/create-subprojects-cache.sh
@@ -8,11 +8,5 @@ git -C /gstreamer submodule update --init --depth=1
 meson subprojects download --sourcedir /gstreamer
 ./ci/scripts/handle-subprojects-cache.py --build --cache-dir /subprojects /gstreamer/subprojects/
 
-# Run git gc to prune unwanted refs and reduce the size of the image
-for i in $(find /subprojects/ -mindepth 1 -maxdepth 1 -type d);
-do
-    git -C $i gc --aggressive || true;
-done
-
 # Now remove the gstreamer clone
 rm -rf /gstreamer
