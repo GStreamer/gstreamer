62ed87dec45ea0af4c8f7cff43c1cbd7939376b5
Tim-Philipp Müller <tim@centricular.com>
2023-10-26 15:03:22 +0100

    gstreamer: misc: relicense network-clock scheme files to LGPL v2.1+
    Permission to relicense granted by author by e-mail:
    
    Message-Id: <155C6D51-7F74-4C40-814A-2ADDED7707FD@pobox.com>
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/5559>

--- subprojects/gstreamer/tests/misc/network-clock.scm
+++ subprojects/gstreamer/tests/misc/network-clock.scm
@@ -5,23 +5,20 @@ exec guile --debug -l $0 -e main -- "$@"
 
 ;; GStreamer
 ;; Copyright (C) 2005 Andy Wingo <wingo at pobox.com>
-
-;; This program is free software; you can redistribute it and/or    
-;; modify it under the terms of the GNU General Public License as   
-;; published by the Free Software Foundation; either version 2 of   
-;; the License, or (at your option) any later version.              
-;;                                                                  
-;; This program is distributed in the hope that it will be useful,  
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of   
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    
-;; GNU General Public License for more details.                     
-;;                                                                  
-;; You should have received a copy of the GNU General Public License
-;; along with this program; if not, contact:
 ;;
-;; Free Software Foundation           Voice:  +1-617-542-5942
-;; 51 Franklin St, Fifth Floor        Fax:    +1-617-542-2652
-;; Boston, MA  02110-1301,  USA       gnu@gnu.org
+;; This library is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU Lesser General Public
+;; License as published by the Free Software Foundation; either
+;; version 2.1 of the License, or (at your option) any later version.
+;;
+;; This library is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; Lesser General Public License for more details.
+;;
+;; You should have received a copy of the GNU Lesser General Public
+;; License along with this library; if not, write to the Free Software
+;; Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 
 ;;; Commentary:
