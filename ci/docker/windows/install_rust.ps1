6ee94cd8c7292b22117231e12a1a2041c62450a9
Sebastian Dröge <sebastian@centricular.com>
2023-11-16 16:47:09 +0200

    ci: Update to Rust 1.74
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/5680>

--- ci/docker/windows/install_rust.ps1
+++ ci/docker/windows/install_rust.ps1
@@ -1,6 +1,6 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;
 
-$rust_version = '1.73.0'
+$rust_version = '1.74.0'
 $rustup_url = 'https://win.rustup.rs/x86_64'
 
 Invoke-WebRequest -Uri $rustup_url -OutFile C:\rustup-init.exe
