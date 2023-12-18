fc31fd09b3b583ef35cde814e04f25514f326dec
Philippe Normand <philn@igalia.com>
2023-12-18 17:01:36 +0000

    play: Fix error details parsing
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/5831>

--- subprojects/gst-plugins-bad/gst-libs/gst/play/gstplay.c
+++ subprojects/gst-plugins-bad/gst-libs/gst/play/gstplay.c
@@ -4683,8 +4683,8 @@ gst_play_message_parse_error (GstMessage * msg, GError ** error,
     GstStructure ** details)
 {
   PARSE_MESSAGE_FIELD (msg, GST_PLAY_MESSAGE_DATA_ERROR, G_TYPE_ERROR, error);
-  PARSE_MESSAGE_FIELD (msg, GST_PLAY_MESSAGE_DATA_ERROR, GST_TYPE_STRUCTURE,
-      details);
+  PARSE_MESSAGE_FIELD (msg, GST_PLAY_MESSAGE_DATA_ERROR_DETAILS,
+      GST_TYPE_STRUCTURE, details);
 }
 
 /**
