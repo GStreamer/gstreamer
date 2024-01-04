7c68ef354b4fadbf455f93986bae5b11ead3078a
Edward Hervey <edward@centricular.com>
2024-01-04 13:43:20 +0000

    gst-python: Fix override loading in python >= 3.12
    The `imp` module got removed in python 3.12 and the `importlib` module should be
    used instead.
    
    This is also a good excuse to switch to the new finder module from PEP 451 :
    https://www.python.org/dev/peps/pep-0451/
    
    This only requires implement the `find_spec()` method in our custom loaders
    
    Co-authored-by: Stefan <107316-stefan6419846@users.noreply.gitlab.freedesktop.org>
    Co-authored-by: Jordan Petrids <jordan@centricular.com>
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/5633>

--- subprojects/gst-python/testsuite/overrides_hack.py
+++ subprojects/gst-python/testsuite/overrides_hack.py
@@ -1,30 +1,23 @@
 import os
 import sys
-import imp
+import importlib
 
 
 class GstOverrideImport:
-    def find_module(self, fullname, path=None):
-        if fullname in ('gi.overrides.Gst', 'gi.overrides._gi_gst'):
-            return self
-        return None
-
-    def load_module(self, name):
-        if name in sys.modules:
-            return sys.modules[name]
-
-        fp, pathname, description = imp.find_module(name.split('.')[-1], [
-            os.environ.get('GST_OVERRIDE_SRC_PATH'),
-            os.environ.get('GST_OVERRIDE_BUILD_PATH'),
-        ])
-
-        try:
-            module = imp.load_module(name, fp, pathname, description)
-        finally:
-            if fp:
-                fp.close()
-        sys.modules[name] = module
-        return module
+    def find_spec(self, fullname, path, target=None):
+        if not (fullname.startswith("gi.overrides.Gst") or fullname.startswith("gi.overrides._gi_gst")):
+            return None
+        finder = importlib.machinery.PathFinder()
+        # From find_spec the docs:
+        # If name is for a submodule (contains a dot), the parent module is automatically imported.
+        spec = finder.find_spec(
+            fullname,
+            [
+                os.environ.get('GST_OVERRIDE_SRC_PATH'),
+                os.environ.get('GST_OVERRIDE_BUILD_PATH'),
+            ]
+        )
+        return spec
 
 
 sys.meta_path.insert(0, GstOverrideImport())
