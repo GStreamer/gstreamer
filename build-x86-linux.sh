aea56b297770f3858be5dd7eec6739392c36e947
zhou.weiguo <zhouwg2000@gmail.com>
2024-03-28 20:52:15 +0800

    build: add a simple build script for x86-linux(ubuntu)

---
+++ build-x86-linux.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+export PROJECT_ROOT_PATH=`pwd`
+
+function build_init()
+{
+if [ "x${PROJECT_ROOT_PATH}" == "x" ]; then
+    echo "pwd is `pwd`"
+    exit 1
+fi
+}
+
+function sanity_check_for_ubuntu
+{
+    sudo apt-get remove libpixman-1-dev
+}
+
+function build_gstreamer()
+{
+meson setup --buildtype ${PROJECT_BUILD_TYPE} --default-library=static -Dgpl=enabled -Dgst-full-target-type=static_library -Dgstreamer:tools=enabled -Dgst-plugins-good:lame=disabled  --prefix=${PROJECT_ROOT_PATH}/out/x86-linux/prefix ${PROJECT_ROOT_PATH}/out/x86-linux
+
+#meson compile -C ${PROJECT_ROOT_PATH}/out/x86-linux --verbose
+meson compile -C ${PROJECT_ROOT_PATH}/out/x86-linux
+meson install -C ${PROJECT_ROOT_PATH}/out/x86-linux
+}
+
+
+build_init
+
+#sanity_check_for_ubuntu
+
+build_gstreamer
