c63bcb1c2a81439314a1a9cdd816456640e1617b
He Junyan <junyan.he@intel.com>
2024-04-19 00:22:50 +0800

    vaav1enc: Set the correct buffer flag for output
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/6751>

--- subprojects/gst-plugins-bad/sys/va/gstvaav1enc.c
+++ subprojects/gst-plugins-bad/sys/va/gstvaav1enc.c
@@ -4056,6 +4056,16 @@ gst_va_av1_enc_prepare_output (GstVaBaseEnc * base,
   *complete = TRUE;
   base->output_frame_count++;
 
+  GST_BUFFER_FLAG_SET (buf, GST_BUFFER_FLAG_MARKER);
+  if (frame_enc->frame_num == 0) {
+    GST_BUFFER_FLAG_SET (buf, GST_BUFFER_FLAG_HEADER);
+    GST_VIDEO_CODEC_FRAME_SET_SYNC_POINT (frame);
+    GST_BUFFER_FLAG_UNSET (buf, GST_BUFFER_FLAG_DELTA_UNIT);
+  } else {
+    GST_VIDEO_CODEC_FRAME_UNSET_SYNC_POINT (frame);
+    GST_BUFFER_FLAG_SET (buf, GST_BUFFER_FLAG_DELTA_UNIT);
+  }
+
   gst_buffer_replace (&frame->output_buffer, buf);
   gst_clear_buffer (&buf);
 
