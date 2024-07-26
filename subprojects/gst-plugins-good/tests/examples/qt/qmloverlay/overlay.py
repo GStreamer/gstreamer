6218b153fd2cca91c0cfdb4a05f618cecfee8909
Matthew Waters <matthew@centricular.com>
2024-07-26 11:12:25 +1000

    tests/examples/qmlglveray.py: fix formatting for commit lint
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/7244>

--- subprojects/gst-plugins-good/tests/examples/qt/qmloverlay/overlay.py
+++ subprojects/gst-plugins-good/tests/examples/qt/qmloverlay/overlay.py
@@ -26,22 +26,23 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
 # THE POSSIBILITY OF SUCH DAMAGE.
 
+from PySide2.QtQuick import QQuickItem
+from PySide2.QtGui import QGuiApplication
+from gi.repository import Gst, GLib
+import gi
+import sys
 import signal
 signal.signal(signal.SIGINT, signal.SIG_DFL)
 
-import sys
-import gi
 gi.require_version('Gst', '1.0')
 
-from gi.repository import Gst, GLib
-from PySide2.QtGui import QGuiApplication
-from PySide2.QtQuick import QQuickItem
 
 def main(args):
     app = QGuiApplication(args)
     Gst.init(args)
 
-    pipeline = Gst.parse_launch("""videotestsrc ! glupload ! qmlgloverlay name=o ! gldownload ! videoconvert ! autovideosink""")
+    pipeline = Gst.parse_launch(
+        """videotestsrc ! glupload ! qmlgloverlay name=o ! gldownload ! videoconvert ! autovideosink""")
     o = pipeline.get_by_name('o')
     f = open('overlay.qml', 'r')
     o.set_property('qml-scene', f.read())
@@ -49,6 +50,7 @@ def main(args):
     pipeline.set_state(Gst.State.PLAYING)
     app.exec_()
     pipeline.set_state(Gst.State.NULL)
+
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv))
