73624fa5c93dd9c21ef49a5f532c8ef920457417
Matthew Waters <matthew@centricular.com>
2024-07-26 11:06:30 +1000

    tests/examples/qmlglsrc: add copytright and licenses headers
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/7244>

--- subprojects/gst-plugins-good/tests/examples/qt/qmlsrc/main.cpp
+++ subprojects/gst-plugins-good/tests/examples/qt/qmlsrc/main.cpp
@@ -1,3 +1,29 @@
+// SPDX-License-Identifier: BSD-2-Clause
+//
+// Copyright (C) 2016, Haihua Hu <jared.hu@nxp.com>
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// a) Redistributions of source code must retain the above copyright notice,
+//    this list of conditions and the following disclaimer.
+//
+// b) Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+// THE POSSIBILITY OF SUCH DAMAGE.
+
 #include <QApplication>
 #include <QQmlApplicationEngine>
 #include <QQuickWindow>
@@ -28,7 +54,7 @@ SetPlaying::~SetPlaying ()
 {
   if (this->pipeline_)
     gst_object_unref (this->pipeline_);
-  
+
 }
 
 void
@@ -47,7 +73,7 @@ int main(int argc, char *argv[])
 
   GstElement *pipeline = gst_pipeline_new (NULL);
   GstElement *src = gst_element_factory_make ("qmlglsrc", NULL);
-  GstElement *sink = gst_element_factory_make ("glimagesink", NULL); 
+  GstElement *sink = gst_element_factory_make ("glimagesink", NULL);
 
   g_assert (src && sink);
 
