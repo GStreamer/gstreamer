5beb4dc435c6b09c6e938062298e00cac1cc549a
Jan Schmidt <jan@centricular.com>
2024-09-03 19:20:22 +1000

    decodebin2: Use gst_value_take_structure()
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/7647>

--- subprojects/gst-plugins-base/gst/playback/gstdecodebin2.c
+++ subprojects/gst-plugins-base/gst/playback/gstdecodebin2.c
@@ -4602,10 +4602,9 @@ gst_decode_chain_get_topology (GstDecodeChain * chain)
     for (l = chain->active_group->children; l; l = l->next) {
       s = gst_decode_chain_get_topology (l->data);
       if (s) {
-        gst_value_set_structure (&item, s);
+        gst_value_take_structure (&item, s);
         gst_value_list_append_value (&list, &item);
         g_value_reset (&item);
-        gst_structure_free (s);
       }
     }
     gst_structure_set_value (u, "next", &list);
