0f5e894931c801b0973ffa431b6e99ec59f06579
Sebastian Dröge <sebastian@centricular.com>
2024-09-05 22:04:37 +0300

    ci: Update to Rust 1.81
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/7458>

--- ci/docker/windows/install_rust.ps1
+++ ci/docker/windows/install_rust.ps1
@@ -1,6 +1,6 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;
 
-$rust_version = '1.80.1'
+$rust_version = '1.81.0'
 $rustup_url = 'https://win.rustup.rs/x86_64'
 
 Invoke-WebRequest -Uri $rustup_url -OutFile C:\rustup-init.exe
