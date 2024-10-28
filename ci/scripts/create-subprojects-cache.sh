db17f542531e6c25bfb1db965dfa095b0ad7c39b
Jordan Petridis <jordan@centricular.com>
2024-10-28 14:21:56 +0200

    build: Add devcontainer manifest for our CI image
    Devcontainer [1] is a popular container schema that makes
    IDEs and tooling aware about the container image you want
    to use and develop against.
    
    The most popular implementation of devcontainer are Visual
    Studio and VSCode and this allows us to have a out of the
    box pre-defined and working build environment that is very
    close to the environment CI runs against.
    
    This also allows VSCode on Windows and Mac to setup a
    Linux docker container (vm) to develop against if so
    desired.
    
    [1] https://containers.dev/
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/7816>

--- ci/scripts/create-subprojects-cache.sh
+++ ci/scripts/create-subprojects-cache.sh
@@ -8,5 +8,10 @@ git -C /gstreamer submodule update --init --depth=1
 meson subprojects download --sourcedir /gstreamer
 ./ci/scripts/handle-subprojects-cache.py --build --cache-dir /subprojects /gstreamer/subprojects/
 
+# Avoid the cache being owned by root
+# and make sure its readable to anyone
+chown containeruser:containeruser --recursive /subprojects/
+chmod --recursive a+r /subprojects/
+
 # Now remove the gstreamer clone
 rm -rf /gstreamer
