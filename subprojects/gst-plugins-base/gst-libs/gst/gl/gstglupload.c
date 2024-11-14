3c8b1427ca853fb2a94d1610e7157c9dff42cc6c
Nicolas Dufresne <nicolas.dufresne@collabora.com>
2024-11-14 15:13:50 -0500

    glupload: dmabuf: Fix emulated tiled import
    The added check for LINEAR modifier broke dmabuf import for
    NV12 emulation (R8/RG88) of all sort (linear and tiled).
    
    Fixes !5461
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/7891>

--- subprojects/gst-plugins-base/gst-libs/gst/gl/gstglupload.c
+++ subprojects/gst-plugins-base/gst-libs/gst/gl/gstglupload.c
@@ -1507,7 +1507,8 @@ _dma_buf_upload_accept (gpointer impl, GstBuffer * buffer, GstCaps * in_caps,
     return FALSE;
   }
 
-  if (!dmabuf->direct && in_info_drm->drm_modifier != DRM_FORMAT_MOD_LINEAR) {
+  if (!dmabuf->direct && in_info_drm->drm_modifier != DRM_FORMAT_MOD_LINEAR
+      && in_info_drm->drm_modifier != DRM_FORMAT_MOD_INVALID) {
     GST_DEBUG_OBJECT (dmabuf->upload,
         "Indirect uploads are only support for linear formats.");
     return FALSE;
