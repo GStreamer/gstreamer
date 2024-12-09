0c93eaec4fe84091b5608485ae8a107eebf8a10d
Nirbheek Chauhan <nirbheek@centricular.com>
2024-12-09 15:12:57 +0530

    vtenc: Mark rate-control enum as plugin API, and update cache
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/8120>

--- subprojects/gst-plugins-bad/sys/applemedia/vtenc.c
+++ subprojects/gst-plugins-bad/sys/applemedia/vtenc.c
@@ -531,6 +531,8 @@ gst_vtenc_class_init (GstVTEncClass * klass)
             VTENC_DEFAULT_PRESERVE_ALPHA,
             G_PARAM_READWRITE | G_PARAM_CONSTRUCT | G_PARAM_STATIC_STRINGS));
   }
+
+  gst_type_mark_as_plugin_api (GST_TYPE_VTENC_RATE_CONTROL, 0);
 }
 
 static void
