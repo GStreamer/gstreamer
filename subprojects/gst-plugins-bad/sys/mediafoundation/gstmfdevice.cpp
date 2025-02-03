73f8de33054870903f9e08087383516114dd4660
Seungha Yang <seungha@centricular.com>
2025-02-04 02:52:51 +0900

    mfdevice: Fix memory leak
    Release resources on dispose() as intended
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/8405>

--- subprojects/gst-plugins-bad/sys/mediafoundation/gstmfdevice.cpp
+++ subprojects/gst-plugins-bad/sys/mediafoundation/gstmfdevice.cpp
@@ -198,12 +198,13 @@ gst_mf_device_provider_device_enum_completed (GstWinRTDeviceWatcher *
 #endif
 
 static void
-gst_mf_device_provider_on_device_updated (GstMFDeviceProvider * self);
-
-static void
 gst_mf_device_provider_class_init (GstMFDeviceProviderClass * klass)
 {
+  auto object_class = G_OBJECT_CLASS (klass);
   GstDeviceProviderClass *provider_class = GST_DEVICE_PROVIDER_CLASS (klass);
+
+  object_class->dispose = gst_mf_device_provider_dispose;
+  object_class->finalize = gst_mf_device_provider_finalize;
 
   provider_class->probe = GST_DEBUG_FUNCPTR (gst_mf_device_provider_probe);
   provider_class->start = GST_DEBUG_FUNCPTR (gst_mf_device_provider_start);
