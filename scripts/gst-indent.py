915b917ba655e1724f6da9969823d44a39c0c2c8
L. E. Segovia <amy@centricular.com>
2025-02-04 13:53:12 -0300

    gst-indent: Refactor indent call to make it callable from other scripts
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/8409>

--- scripts/gst-indent.py
+++ scripts/gst-indent.py
@@ -1,36 +1,8 @@
 #!/usr/bin/env python
 
-import subprocess
 from sys import argv
-import shutil
+from gst_indent_common import indent
+
 
 if __name__ == '__main__':
-    indent = shutil.which('gst-indent-1.0')
-
-    if not indent:
-        raise RuntimeError('''Did not find gst-indent-1.0, please install it before continuing.''')
-
-    version = subprocess.run([indent, '--version'], capture_output=True, text=True)
-
-    if 'GNU' not in version.stdout:
-        raise RuntimeError(f'''Did not find gst-indent-1.0, please install it before continuing.
-      (Found {indent}, but it doesn't seem to be gst-indent-1.0)''')
-
-    # Run twice. GNU indent isn't idempotent
-    # when run once
-    for i in range(2):
-        subprocess.check_call([indent,
-            '--braces-on-if-line',
-            '--case-brace-indentation0',
-            '--case-indentation2',
-            '--braces-after-struct-decl-line',
-            '--line-length80',
-            '--no-tabs',
-            '--cuddle-else',
-            '--dont-line-up-parentheses',
-            '--continuation-indentation4',
-            '--honour-newlines',
-            '--tab-size8',
-            '--indent-level2',
-            '--leave-preprocessor-space'] + argv[1:]
-        )
+    indent(argv[1:])
