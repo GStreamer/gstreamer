68f964a63519f92a662b78f86db550a93a4ae4e7
L. E. Segovia <amy@centricular.com>
2025-02-04 18:38:36 -0300

    gst-indent: Synchronize defaults with our fork of GNU indent
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/8409>

--- scripts/gst_indent_common.py
+++ scripts/gst_indent_common.py
@@ -10,25 +10,8 @@ def indent(*args):
 
     version = subprocess.run([indent, '--version'], capture_output=True, text=True)
 
-    if 'GNU' not in version.stdout:
+    if 'gst-indent' not in version.stdout:
         raise RuntimeError(f'''Did not find gst-indent-1.0, please install it before continuing.
       (Found {indent}, but it doesn't seem to be gst-indent-1.0)''')
 
-    # Run twice. GNU indent isn't idempotent
-    # when run once
-    for i in range(2):
-        subprocess.check_call([indent,
-            '--braces-on-if-line',
-            '--case-brace-indentation0',
-            '--case-indentation2',
-            '--braces-after-struct-decl-line',
-            '--line-length80',
-            '--no-tabs',
-            '--cuddle-else',
-            '--dont-line-up-parentheses',
-            '--continuation-indentation4',
-            '--honour-newlines',
-            '--tab-size8',
-            '--indent-level2',
-            '--leave-preprocessor-space'] + list(args)
-        )
+    subprocess.check_call([indent] + list(args))
