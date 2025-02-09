3e8f88d756f44b6577ca6653ce4ac77458713830
Tim-Philipp Müller <tim@centricular.com>
2025-02-09 17:35:17 +0000

    Release 1.25.50

--- subprojects/gstreamer-sharp/sources/generated/Gst/Constants.cs
+++ subprojects/gstreamer-sharp/sources/generated/Gst/Constants.cs
@@ -171,9 +171,9 @@ namespace Gst {
 		public const int VALUE_LESS_THAN = -1;
 		public const int VALUE_UNORDERED = 2;
 		public const int VERSION_MAJOR = 1;
-		public const int VERSION_MICRO = 1;
+		public const int VERSION_MICRO = 50;
 		public const int VERSION_MINOR = 25;
-		public const int VERSION_NANO = 1;
+		public const int VERSION_NANO = 0;
 #endregion
 	}
 }
