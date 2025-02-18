4e958fa45a96798d91950d3584b0fa3bc4e9ea20
Jordan Yelloz <jordan.yelloz@collabora.com>
2025-02-18 13:08:37 -0700

    gstmselogging: Added helper function to get nicknames of enum values
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/8512>

--- subprojects/gst-plugins-bad/gst-libs/gst/mse/gstmselogging.c
+++ subprojects/gst-plugins-bad/gst-libs/gst/mse/gstmselogging.c
@@ -33,3 +33,13 @@ gst_mse_init_logging (void)
 {
   GST_DEBUG_CATEGORY_INIT (gst_mse_debug, "gst-mse", 0, "GstMse");
 }
+
+const gchar *
+gst_mse_enum_value_nick (GType enum_type, gint mse_enum_value)
+{
+  GEnumClass *enum_class = (GEnumClass *) g_type_class_ref (enum_type);
+  GEnumValue *enum_value = g_enum_get_value (enum_class, mse_enum_value);
+  const gchar *nick = enum_value->value_nick;
+  g_type_class_unref (enum_class);
+  return nick;
+}
