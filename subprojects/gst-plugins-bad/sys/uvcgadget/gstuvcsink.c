GStreamer/gstreamer

commit 69c17461392daed192f4288bf5326d8f5b1910be

uvcgadget: Properly implement GET_INFO control responses
According to the UVC 1.5 specification, section 4.1.2, the GET_INFO request
must return a bitmap indicating supported operations for the control.
Value 0x00 indicates that neither GET nor SET operations are supported.

This patch fixes control handling in the UVC gadget implementation to properly
respond to GET_INFO requests with the correct bitmap, allowing host systems
to properly detect supported control operations (none in this case).

The pipeline I'm using to test this is:

  gst-launch-1.0 videotestsrc ! uvcsink v4l2sink::device=/dev/video0

This is the equivalent of [0] but the difference is that we are now returning
0x00 instead of 0x03.

Without this change the host in my case is unable to probe the UVC gadget at
all, automatically disconnecting the device after a few seconds.

Following is the log when the gadget is not working (without this fix):

  usb 1-1.2: new high-speed USB device number 73 using xhci_hcd
  usb 1-1.2: New USB device found, idVendor=0525, idProduct=a4a2, bcdDevice= 5.15
  usb 1-1.2: New USB device strings: Mfr=1, Product=2, SerialNumber=3
  usb 1-1.2: Product: UVC Gadget
  usb 1-1.2: Manufacturer: localhost.localdomain
  usb 1-1.2: SerialNumber: 0123456789
  usb 1-1.2: Found UVC 1.10 device UVC Gadget (0525:a4a2)
  usb 1-1.2: Failed to query (GET_INFO) UVC control 2 on unit 1: -110 (exp. 1).
  usb 1-1.2: UVC non compliance - GET_DEF(PROBE) not supported. Enabling workaround.
  uvcvideo 1-1.2:1.1: Failed to query (129) UVC probe control : -71 (exp. 34).
  uvcvideo 1-1.2:1.1: Failed to initialize the device (-71).
  cdc_subset 1-1.2:1.0: probe with driver cdc_subset failed with error -22
  cdc_subset 1-1.2:1.1: probe with driver cdc_subset failed with error -22
  usb 1-1.2: USB disconnect, device number 73

With the fix the USB device is correctly probed:

  usb 1-1.2: new high-speed USB device number 88 using xhci_hcd

subprojects/gst-plugins-bad/sys/uvcgadget/uvc.c
@@ -347,6 +347,12 @@ static void
 uvc_events_parse_control (GstUvcSink * self, uint8_t req,
     uint8_t cs, uint8_t entity_id, uint8_t len, struct uvc_request_data *resp)
 {
+  /*
+   * Responding to controls is not currently implemented.
+   */
+  resp->data[0] = 0x00;
+  resp->length = 1;
+
   switch (entity_id) {
     case 0:
       GST_DEBUG_OBJECT (self, "%s",

commit 3b345568bc05f3b0e307eae4c2e8c3c139f48c33

uvcsink: Respond to control requests with proper error handling
The complete handling on the control interface is currently dead.
We return with EOPNOTSUPP for the caller to know that a response
to such requests is not valid. The host however may ask
control interface why these control requests were not available.

For this the UVC_VC_REQUEST_ERROR_CODE_CONTROL is used. As an overall
exception for the control interface we just always return 0x06 as
an response which is representing "not implemented".

This patch is a necessary feature to properly pass the UVC Functionality
Test of the USB3CV Compliance Software.

Fixes: 69c17461392d ('uvcgadget: Properly implement GET_INFO control responses')
Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/7524>

subprojects/gst-plugins-bad/sys/uvcgadget/gstuvcsink.c
@@ -640,6 +640,8 @@ gst_uvc_sink_init (GstUvcSink * self)
   gst_pad_set_query_function (self->sinkpad, gst_uvc_sink_query);
   gst_pad_set_event_function (self->sinkpad, gst_uvc_sink_event);
 
+  self->request_error_code = REQUEST_ERROR_CODE_NO_ERROR;
+
   self->cur_caps = gst_caps_new_empty ();
 }
 
@@ -985,6 +987,7 @@ gst_uvc_sink_change_state (GstElement * element, GstStateChange transition)
     case GST_STATE_CHANGE_PAUSED_TO_READY:
       gst_element_sync_state_with_parent (GST_ELEMENT (self->fakesink));
       gst_uvc_sink_remove_idle_probe (self);
+      self->request_error_code = REQUEST_ERROR_CODE_NO_ERROR;
       break;
     case GST_STATE_CHANGE_READY_TO_NULL:
       if (!gst_uvc_sink_unwatch (self))

subprojects/gst-plugins-bad/sys/uvcgadget/gstuvcsink.h
@@ -17,6 +17,9 @@
 
 G_BEGIN_DECLS GST_DEBUG_CATEGORY_EXTERN (uvcsink_debug);
 
+#define REQUEST_ERROR_CODE_NO_ERROR 0x0
+#define REQUEST_ERROR_CODE_INVALID_REQUEST 0x6
+
 #define GST_TYPE_UVCSINK (gst_uvc_sink_get_type())
 G_DECLARE_FINAL_TYPE (GstUvcSink, gst_uvc_sink, GST, UVCSINK, GstBin)
 
@@ -53,6 +56,8 @@ struct _GstUvcSink
   struct uvc_streaming_control commit;
 
   int control;
+
+  int request_error_code;
 
   /* probes */
   int buffer_peer_probe_id;

subprojects/gst-plugins-bad/sys/uvcgadget/uvc.c
@@ -343,7 +343,7 @@ uvc_events_process_streaming (GstUvcSink * self, uint8_t req, uint8_t cs,
   return 0;
 }
 
-static void
+static int
 uvc_events_parse_control (GstUvcSink * self, uint8_t req,
     uint8_t cs, uint8_t entity_id, uint8_t len, struct uvc_request_data *resp)
 {
@@ -357,26 +357,50 @@ uvc_events_parse_control (GstUvcSink * self, uint8_t req,
     case 0:
       GST_DEBUG_OBJECT (self, "%s",
           uvc_video_control_interface_control_selector_name (cs));
-      break;
+      /*
+       * For ERROR_CODE_CONTROL requests we have to return not implemented so
+       * bail out gracefully here to properly send control is not
+       * currently implemented. (0x06) (4.2.1.2 Request Error Code Control)
+       */
+      if (cs == UVC_VC_REQUEST_ERROR_CODE_CONTROL) {
+        resp->data[0] = self->request_error_code;
+        resp->length = 1;
+
+        break;
+      }
+      goto error;
 
     case 1:
       GST_DEBUG_OBJECT (self, "%s: %s",
           uvc_camera_terminal_control_selector_name (cs),
           uvc_request_name (req));
-      break;
+      goto error;
 
     case 2:
       GST_DEBUG_OBJECT (self, "%s: %s",
           uvc_processing_unit_control_selector_name (cs),
           uvc_request_name (req));
-      break;
+      goto error;
 
     default:
       GST_DEBUG_OBJECT (self,
           "Unknown entity ID (0x%02x), CS: 0x%02x, Request %s (0x%02x)",
           entity_id, cs, uvc_request_name (req), req);
-      break;
+      goto error;
   }
+
+  self->request_error_code = REQUEST_ERROR_CODE_NO_ERROR;
+
+  return 0;
+
+error:
+
+  self->request_error_code = REQUEST_ERROR_CODE_INVALID_REQUEST;
+  /*
+   * Stall and don't response the control requests that are
+   * actually not implemented
+   */
+  return -EOPNOTSUPP;
 }
 
 static int
@@ -389,9 +413,8 @@ uvc_events_process_class (GstUvcSink * self,
     return -EINVAL;
 
   if (interface == UVC_STRING_CONTROL_IDX) {
-    uvc_events_parse_control (self, ctrl->bRequest, ctrl->wValue >> 8,
-        ctrl->wIndex >> 8, ctrl->wLength, resp);
-    return -EOPNOTSUPP;
+    return uvc_events_parse_control (self, ctrl->bRequest,
+        ctrl->wValue >> 8, ctrl->wIndex >> 8, ctrl->wLength, resp);
   } else if (interface == UVC_STRING_STREAMING_IDX) {
     return uvc_events_process_streaming (self, ctrl->bRequest,
         le16toh (ctrl->wValue) >> 8, resp);