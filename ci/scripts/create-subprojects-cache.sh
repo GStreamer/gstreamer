a74bda813bcc82b0a56a7731da9a7c174a5aa845
Jordan Petridis <jordan@centricular.com>
2025-05-14 15:27:30 +0300

    ci: Always create the subproject cache from the upstream repo
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/8994>

--- ci/scripts/create-subprojects-cache.sh
+++ ci/scripts/create-subprojects-cache.sh
@@ -2,8 +2,11 @@
 
 set -eux
 
+branch="${GST_UPSTREAM_BRANCH:-main}"
+repo_url="https://gitlab.freedesktop.org/gstreamer/gstreamer.git"
+
 # get gstreamer and make all subprojects available
-git clone -b "${GIT_BRANCH}" "${GIT_URL}" /gstreamer
+git clone -b "${branch}" --depth=1 "${repo_url}" /gstreamer
 git -C /gstreamer submodule update --init --depth=1
 meson subprojects download --sourcedir /gstreamer
 ./ci/scripts/handle-subprojects-cache.py --build --cache-dir /subprojects /gstreamer/subprojects/
