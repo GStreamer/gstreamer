378e20c85267b1f93da97e4e13d15d0fd62efa34
Olivier Crête <olivier.crete@collabora.com>
2025-05-18 14:12:36 +0200

    overlaycomposition: Implement the video matrix transformation
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/9497>

--- subprojects/gst-plugins-base/gst-libs/gst/video/video-overlay-composition.c
+++ subprojects/gst-plugins-base/gst-libs/gst/video/video-overlay-composition.c
@@ -207,7 +207,7 @@ static gboolean
 gst_video_overlay_composition_meta_transform (GstBuffer * dest, GstMeta * meta,
     GstBuffer * buffer, GQuark type, gpointer data)
 {
-  GstVideoOverlayCompositionMeta *dmeta, *smeta;
+  GstVideoOverlayCompositionMeta *dmeta = NULL, *smeta;
 
   smeta = (GstVideoOverlayCompositionMeta *) meta;
 
@@ -221,6 +221,51 @@ gst_video_overlay_composition_meta_transform (GstBuffer * dest, GstMeta * meta,
       return FALSE;
 
     dmeta->overlay = gst_video_overlay_composition_ref (smeta->overlay);
+  } else if (GST_VIDEO_META_TRANSFORM_IS_MATRIX (type)) {
+    GstVideoMetaTransformMatrix *trans = data;
+    GstVideoOverlayComposition *new_comp;
+    guint n_rectangles;
+    gboolean have_rect = FALSE;
+
+    new_comp = gst_video_overlay_composition_new (NULL);
+    n_rectangles = gst_video_overlay_composition_n_rectangles (smeta->overlay);
+    for (guint i = 0; i < n_rectangles; i++) {
+      GstVideoOverlayRectangle *rect =
+          gst_video_overlay_composition_get_rectangle (smeta->overlay, i);
+      GstVideoOverlayRectangle *new_rect =
+          gst_video_overlay_rectangle_copy (rect);
+      GstVideoRectangle render_rect;
+      guint width = G_MAXINT;
+      guint height = G_MAXINT;
+
+      gst_video_overlay_rectangle_get_render_rectangle (rect, &render_rect.x,
+          &render_rect.y, &width, &height);
+      render_rect.w = MIN (width, G_MAXINT);
+      render_rect.h = MIN (height, G_MAXINT);
+
+      GstVideoRectangle orig_render_rect = render_rect;
+
+      if (gst_video_meta_transform_matrix_rectangle (trans, &render_rect)) {
+
+        GST_DEBUG ("overlay rectangle %u (seqnum: %d) (%dx%d)x(%dx%d)"
+            " -> (%dx%d)->(%dx%d)", i,
+            gst_video_overlay_rectangle_get_seqnum (rect), orig_render_rect.x,
+            orig_render_rect.y, orig_render_rect.w, orig_render_rect.h,
+            render_rect.x, render_rect.y, render_rect.w, render_rect.h);
+
+        gst_video_overlay_rectangle_set_render_rectangle (new_rect,
+            render_rect.x, render_rect.y, render_rect.w, render_rect.h);
+        gst_video_overlay_composition_add_rectangle (new_comp, new_rect);
+        have_rect = TRUE;
+      }
+      gst_video_overlay_rectangle_unref (new_rect);
+    }
+
+    if (have_rect)
+      dmeta = gst_buffer_add_video_overlay_composition_meta (dest, new_comp);
+    gst_video_overlay_composition_unref (new_comp);
+    if (!have_rect || !dmeta)
+      return FALSE;
   } else if (GST_VIDEO_META_TRANSFORM_IS_SCALE (type)) {
     GstVideoMetaTransform *trans = data;
     GstVideoOverlayComposition *new_comp;
@@ -234,8 +279,6 @@ gst_video_overlay_composition_meta_transform (GstBuffer * dest, GstMeta * meta,
 
     GST_DEBUG ("scaling video overlay composition metadata %dx%d -> %dx%d", ow,
         oh, nw, nh);
-
-    smeta = (GstVideoOverlayCompositionMeta *) meta;
 
     new_comp = gst_video_overlay_composition_new (NULL);
     n_rectangles = gst_video_overlay_composition_n_rectangles (smeta->overlay);
