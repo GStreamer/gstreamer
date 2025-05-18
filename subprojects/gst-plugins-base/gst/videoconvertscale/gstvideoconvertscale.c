1c515e5f76211b4e8d48f985294552495fe951af
Sebastian Dröge <sebastian@centricular.com>
2025-05-18 11:30:24 +0300

    videoconvertscale: Use new gst_meta_api_type_tags_contain_only() API
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/9048>

--- subprojects/gst-plugins-base/gst/videoconvertscale/gstvideoconvertscale.c
+++ subprojects/gst-plugins-base/gst/videoconvertscale/gstvideoconvertscale.c
@@ -139,8 +139,6 @@ GST_DEBUG_CATEGORY_STATIC (CAT_PERFORMANCE);
 #define DEFAULT_PROP_PRIMARIES_MODE GST_VIDEO_PRIMARIES_MODE_NONE
 #define DEFAULT_PROP_N_THREADS 1
 
-static GQuark _colorspace_quark;
-
 enum
 {
   PROP_0,
@@ -184,6 +182,7 @@ static GstStaticCaps gst_video_convert_scale_format_caps =
 
 static GQuark _size_quark;
 static GQuark _scale_quark;
+static GQuark _tags_quark;
 
 #define GST_TYPE_VIDEO_SCALE_METHOD (gst_video_scale_method_get_type())
 static GType
@@ -288,8 +287,6 @@ gst_video_convert_scale_class_init (GstVideoConvertScaleClass * klass)
   GST_DEBUG_CATEGORY_INIT (video_convertscale_debug, "videoconvertscale", 0,
       "videoconvertscale element");
   GST_DEBUG_CATEGORY_GET (CAT_PERFORMANCE, "GST_PERFORMANCE");
-
-  _colorspace_quark = g_quark_from_static_string ("colorspace");
 
   gobject_class->finalize =
       (GObjectFinalizeFunc) gst_video_convert_scale_finalize;
@@ -398,6 +395,7 @@ gst_video_convert_scale_class_init (GstVideoConvertScaleClass * klass)
 
   _size_quark = g_quark_from_static_string (GST_META_TAG_VIDEO_SIZE_STR);
   _scale_quark = gst_video_meta_transform_scale_get_quark ();
+  _tags_quark = g_quark_from_static_string ("tags");
 
   gst_type_mark_as_plugin_api (GST_TYPE_VIDEO_SCALE_METHOD, 0);
   trans_class->transform_caps =
@@ -697,45 +695,45 @@ gst_video_convert_scale_transform_caps (GstBaseTransform * trans,
   return ret;
 }
 
+/* This is public API in 1.28 */
+static gboolean
+gst_meta_api_type_tags_contain_only (GType api, const gchar ** valid_tags)
+{
+  const gchar **tags, **curr;
+  g_return_val_if_fail (api != 0, FALSE);
+
+  tags = g_type_get_qdata (api, _tags_quark);
+
+  if (!tags)
+    return TRUE;
+
+  for (curr = tags; *curr; ++curr) {
+
+    if (!g_strv_contains (valid_tags, *curr)) {
+      return FALSE;
+    }
+  }
+
+  return TRUE;
+}
+
 static gboolean
 gst_video_convert_scale_transform_meta (GstBaseTransform * trans,
     GstBuffer * outbuf, GstMeta * meta, GstBuffer * inbuf)
 {
   GstVideoFilter *videofilter = GST_VIDEO_FILTER (trans);
   const GstMetaInfo *info = meta->info;
-  const gchar *const *tags;
-  const gchar *const *curr = NULL;
   gboolean should_copy = TRUE;
-  const gchar *const valid_tags[] = {
+  const gchar *valid_tags[] = {
     GST_META_TAG_VIDEO_STR,
     GST_META_TAG_VIDEO_ORIENTATION_STR,
     GST_META_TAG_VIDEO_SIZE_STR,
+    /* don't copy colorspace specific metadata, FIXME, we need a MetaTransform
+     * for the colorspace metadata. */
     NULL
   };
 
-  tags = gst_meta_api_type_get_tags (info->api);
-
-  /* No specific tags, we are good to copy */
-  if (!tags) {
-    return TRUE;
-  }
-
-  if (gst_meta_api_type_has_tag (info->api, _colorspace_quark)) {
-    /* don't copy colorspace specific metadata, FIXME, we need a MetaTransform
-     * for the colorspace metadata. */
-    return FALSE;
-  }
-
-  /* We are only changing size, we can preserve other metas tagged as
-     orientation and colorspace */
-  for (curr = tags; *curr; ++curr) {
-
-    /* We dont handle any other tag */
-    if (!g_strv_contains (valid_tags, *curr)) {
-      should_copy = FALSE;
-      break;
-    }
-  }
+  should_copy = gst_meta_api_type_tags_contain_only (info->api, valid_tags);
 
   /* Cant handle the tags in this meta, let the parent class handle it */
   if (!should_copy) {
