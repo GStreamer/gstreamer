c607dbc13b98e72546c85c53c6794e063c9f4e61
Mathieu Duponchelle <mathieu@centricular.com>
2025-07-09 00:47:58 +0200

    rtmp2src: don't send window acknowledgement size commands twice
    This causes servers such as AWS MediaLive to drop the connection, and
    the message flow chart as documented in the "spec" always has the
    server sending it first, and the client replying to it on reception of
    the Set Peer Bandwidth, which we do since 286a3829b637.
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/9372>

--- subprojects/gst-plugins-bad/gst/rtmp2/rtmp/rtmpclient.c
+++ subprojects/gst-plugins-bad/gst/rtmp2/rtmp/rtmpclient.c
@@ -1438,9 +1438,6 @@ send_create_stream (GTask * task)
     gst_rtmp_connection_send_command (connection, NULL, NULL, 0,
         "FCPublish", command_object, stream_name, NULL);
   } else {
-    /* Matches librtmp */
-    gst_rtmp_connection_request_window_size (connection,
-        GST_RTMP_DEFAULT_WINDOW_ACK_SIZE);
     send_set_buffer_length (connection, 0, 300);
   }
 
