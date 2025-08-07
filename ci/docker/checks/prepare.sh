78c2140db7fb5f3a63f9a11976db9d976961e072
Sebastian Dröge <sebastian@centricular.com>
2025-08-07 15:18:27 +0300

    ci: Update to Rust 1.89
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/9503>

--- ci/docker/checks/prepare.sh
+++ ci/docker/checks/prepare.sh
@@ -34,7 +34,7 @@ python3 -m pip install gitlint
 
 # Install Rust
 RUSTUP_VERSION=1.28.1
-RUST_VERSION=1.88.0
+RUST_VERSION=1.89.0
 RUST_ARCH="x86_64-unknown-linux-gnu"
 
 RUSTUP_URL=https://static.rust-lang.org/rustup/archive/$RUSTUP_VERSION/$RUST_ARCH/rustup-init
