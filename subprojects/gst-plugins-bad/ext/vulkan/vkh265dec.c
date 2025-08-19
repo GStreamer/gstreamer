48dd6aee51c7b06e4d4d4305b455a5d2f7a7f6fa
Víctor Manuel Jáquez Leal <vjaquez@igalia.com>
2025-08-19 20:37:06 +0200

    vulkanh265dec: fix validation layer complain
    Silence the validation VUID-VkImageMemoryBarrier2-srcAccessMask-03915
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/9591>

--- subprojects/gst-plugins-bad/ext/vulkan/vkh265dec.c
+++ subprojects/gst-plugins-bad/ext/vulkan/vkh265dec.c
@@ -413,7 +413,7 @@ gst_vulkan_h265_decoder_decide_allocation (GstVideoDecoder * decoder,
 
   gst_vulkan_image_buffer_pool_config_set_allocation_params (config, usage,
       VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, VK_IMAGE_LAYOUT_VIDEO_DECODE_DST_KHR,
-      VK_ACCESS_TRANSFER_WRITE_BIT);
+      VK_ACCESS_NONE_KHR);
   gst_vulkan_image_buffer_pool_config_set_decode_caps (config, profile_caps);
 
   gst_caps_unref (profile_caps);
