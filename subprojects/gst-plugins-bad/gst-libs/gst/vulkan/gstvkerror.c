f8ae7366c560b54ce129f0ffe19c8f9e3e1a777c
Víctor Manuel Jáquez Leal <vjaquez@igalia.com>
2025-09-25 19:21:50 +0200

    vkerror: add video profile errors
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/9748>

--- subprojects/gst-plugins-bad/gst-libs/gst/vulkan/gstvkerror.c
+++ subprojects/gst-plugins-bad/gst-libs/gst/vulkan/gstvkerror.c
@@ -55,7 +55,10 @@ static const struct
   {VK_ERROR_INCOMPATIBLE_DISPLAY_KHR, "Incompatible display"},
   {VK_ERROR_NATIVE_WINDOW_IN_USE_KHR, "Native window in use"},
 #if defined(VK_KHR_video_queue)
+  {VK_ERROR_VIDEO_PICTURE_LAYOUT_NOT_SUPPORTED_KHR, "Video picture layout not supported"},
+  {VK_ERROR_VIDEO_PROFILE_CODEC_NOT_SUPPORTED_KHR, "Video codec not supported"},
   {VK_ERROR_VIDEO_PROFILE_FORMAT_NOT_SUPPORTED_KHR, "Video profile format not supported"},
+  {VK_ERROR_VIDEO_PROFILE_OPERATION_NOT_SUPPORTED_KHR, "Video profile operation not supported"},
 #endif
 #if defined(VK_KHR_video_encode_queue)
   {VK_ERROR_INVALID_VIDEO_STD_PARAMETERS_KHR, "Invalid video std parameters"},
