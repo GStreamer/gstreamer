640d9075d6127b26ce65ca69847c930d1ce9cfae
Branko Subasic <branko.subasic@axis.com>
2025-10-09 08:09:33 +0200

    mpegtsmux: Properly check if pads are EOS in find_best_pad
    In find_best_pad(), when checking if a pad is EOS we use the macro
    GST_PAD_IS_EOS. But the aggregator pads will not propagate EOS received
    on it's sinkpads until it has received EOS on all of them. Instead the
    EOS event is put in the queue, which means that GST_PAD_IS_EOS will
    return FALSE.
    
    This makes find_best_pad() fail to find a best pad when all but one sink
    pad has received EOS, and have no data left in their queues, and the
    last one has data. The problem is solved by modifying find_best_pad() to
    use gst_aggregator_pad_is_eos() instead.
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/9821>

--- subprojects/gst-plugins-bad/gst/mpegtsmux/gstbasetsmux.c
+++ subprojects/gst-plugins-bad/gst/mpegtsmux/gstbasetsmux.c
@@ -2670,7 +2670,7 @@ gst_base_ts_mux_find_best_pad (GstAggregator * aggregator,
 
     buffer = gst_aggregator_pad_peek_buffer (apad);
     if (!buffer) {
-      if (!timeout && !GST_PAD_IS_EOS (apad)) {
+      if (!timeout && !gst_aggregator_pad_is_eos (apad)) {
         best = NULL;
         best_ts = GST_CLOCK_TIME_NONE;
         break;
