4ebcb4b9c081ab0bcdd6cebe9e3be0b57b0d84bd
Thibault Saunier <tsaunier@igalia.com>
2025-10-27 16:47:35 +0000

    python: testsuite: Fix reference leak in test_plugin
    The testLoad test was leaking the pipeline and element references,
    causing the garbage collection verification to fail. Explicitly delete
    the pipeline after setting it to NULL state to ensure proper cleanup.
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/9918>

--- subprojects/gst-python/testsuite/test_plugin.py
+++ subprojects/gst-python/testsuite/test_plugin.py
@@ -18,25 +18,28 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
 
+from gi.repository import Gst
+import gi
+from common import TestCase, unittest
 import overrides_hack
 overrides_hack
 
-from common import TestCase, unittest
 
-import gi
 gi.require_version("Gst", "1.0")
-from gi.repository import Gst
 
 
 class TestPlugin(TestCase):
     def testLoad(self):
         Gst.init(None)
-        p = Gst.parse_launch ("fakesrc ! test_identity_py name=id ! fakesink")
-        assert p.get_by_name("id").transformed == False
+        p = Gst.parse_launch("fakesrc ! test_identity_py name=id ! fakesink")
+        assert p.get_by_name("id").transformed is False
         p.set_state(Gst.State.PLAYING)
         p.get_state(Gst.CLOCK_TIME_NONE)
         p.set_state(Gst.State.NULL)
-        assert p.get_by_name("id").transformed == True
+        assert p.get_by_name("id").transformed is True
+
+        del p
+
 
 if __name__ == "__main__":
     unittest.main()
