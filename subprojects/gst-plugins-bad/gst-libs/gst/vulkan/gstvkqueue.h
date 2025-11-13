afa266d6a73e6ede87b4c5d72fb7a344c0be828a
Matthew Semeniuk <megaman22342@live.ca>
2025-11-12 16:53:32 -0800

    vulkan: add G_DECLS to gstvkqueue
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/10078>

--- subprojects/gst-plugins-bad/gst-libs/gst/vulkan/gstvkqueue.h
+++ subprojects/gst-plugins-bad/gst-libs/gst/vulkan/gstvkqueue.h
@@ -24,6 +24,8 @@
 #include <gst/vulkan/gstvkdevice.h>
 #include <gst/vulkan/gstvkcommandpool.h>
 
+G_BEGIN_DECLS
+
 #define GST_TYPE_VULKAN_QUEUE         (gst_vulkan_queue_get_type())
 #define GST_VULKAN_QUEUE(o)           (G_TYPE_CHECK_INSTANCE_CAST((o), GST_TYPE_VULKAN_QUEUE, GstVulkanQueue))
 #define GST_VULKAN_QUEUE_CLASS(k)     (G_TYPE_CHECK_CLASS_CAST((k), GST_TYPE_VULKAN_QUEUE, GstVulkanQueueClass))
@@ -105,5 +107,7 @@ gboolean            gst_vulkan_queue_handle_context_query       (GstElement * el
 GST_VULKAN_API
 gboolean            gst_vulkan_queue_run_context_query          (GstElement * element,
                                                                  GstVulkanQueue ** queue);
+
+G_END_DECLS
 
 #endif /* __GST_VULKAN_QUEUE_H__ */
