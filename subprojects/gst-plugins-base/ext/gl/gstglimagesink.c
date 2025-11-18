e9c68b92639ded895bfe2fe8c035ddd96441f4cb
Nicolas Dufresne <nicolas.dufresne@collabora.com>
2025-11-18 16:52:20 -0500

    glimagesink: Simplify the affine transform handling
    The multiply function already allow to write into one of its member.
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/10120>

--- subprojects/gst-plugins-base/ext/gl/gstglimagesink.c
+++ subprojects/gst-plugins-base/ext/gl/gstglimagesink.c
@@ -2483,14 +2483,10 @@ gst_glimage_sink_on_draw (GstGLImageSink * gl_sink)
           gst_buffer_get_video_affine_transformation_meta
           (gl_sink->stored_buffer[0]);
 
-      if (gl_sink->transform_matrix) {
-        gfloat tmp[16];
+      gst_gl_get_affine_transformation_meta_as_ndc (af_meta, matrix);
 
-        gst_gl_get_affine_transformation_meta_as_ndc (af_meta, tmp);
-        gst_gl_multiply_matrix4 (tmp, gl_sink->transform_matrix, matrix);
-      } else {
-        gst_gl_get_affine_transformation_meta_as_ndc (af_meta, matrix);
-      }
+      if (gl_sink->transform_matrix)
+        gst_gl_multiply_matrix4 (matrix, gl_sink->transform_matrix, matrix);
 
       gst_gl_shader_set_uniform_matrix_4fv (gl_sink->redisplay_shader,
           "u_transformation", 1, FALSE, matrix);
