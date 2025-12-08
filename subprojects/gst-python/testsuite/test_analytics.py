11c10fc55f1dc02de8b3c11e4948be6b2ae23e37
Daniel Morin <daniel.morin@collabora.com>
2025-12-08 12:58:44 -0500

    gst-python: make python linter happy with test_analytics
    - Change to be confirm fory PEP8
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/10244>

--- subprojects/gst-python/testsuite/test_analytics.py
+++ subprojects/gst-python/testsuite/test_analytics.py
@@ -24,8 +24,6 @@ import overrides_hack
 overrides_hack
 
 from common import TestCase
-import unittest
-import sys
 
 import gi
 gi.require_version("GLib", "2.0")
@@ -126,8 +124,8 @@ class TestAnalyticsClsMtd(TestCase):
         self.assertIsNotNone(meta)
 
         qks = (GLib.quark_from_string("q1"),
-              GLib.quark_from_string("q2"),
-              GLib.quark_from_string("q3"))
+               GLib.quark_from_string("q2"),
+               GLib.quark_from_string("q3"))
 
         (ret, mtd) = meta.add_cls_mtd([0.1, 0.2, 0.3], qks)
         self.assertTrue(ret)
@@ -228,9 +226,9 @@ class TestAnalyticsTensorMeta(TestCase):
 
         data2 = Gst.Buffer.new_allocate(None, 2 * 3 * 4 * 5)
         tensor2 = GstAnalytics.Tensor.new_simple(0, GstAnalytics.TensorDataType.UINT16,
-                                                data2,
-                                                GstAnalytics.TensorDimOrder.ROW_MAJOR,
-                                                [1, 3, 4, 5])
+                                                 data2,
+                                                 GstAnalytics.TensorDimOrder.ROW_MAJOR,
+                                                 [1, 3, 4, 5])
         tmeta.set([tensor, tensor2])
 
         tmeta2 = GstAnalytics.buffer_get_tensor_meta(buf)
@@ -268,8 +266,8 @@ class TestAnalyticsRelationMetaIterator(TestCase):
         (_, trk_mtd) = rmeta.add_tracking_mtd(1, 10)
         (_, trk_mtd1) = rmeta.add_tracking_mtd(1, 11)
         (_, seg_mtd) = rmeta.add_segmentation_mtd(mask_buf,
-                                               GstAnalytics.SegmentationType.SEMANTIC,
-                                               [7, 4, 2], 0, 0, 7, 13)
+                                                  GstAnalytics.SegmentationType.SEMANTIC,
+                                                  [7, 4, 2], 0, 0, 7, 13)
 
         mtds = [
             (od_mtd, GstAnalytics.ODMtd.get_mtd_type()),
@@ -299,7 +297,6 @@ class TestAnalyticsRelationMetaIterator(TestCase):
             assert mtds[j][1] == i.get_mtd_type()
             # call a method to ensure it's a ODMtd
             loc = i.get_location()
-
 
         # Iterating on type GstAnalytics.ClsMtd
         for j, i in zip(cls_index_mtds, rmeta.iter_on_type(GstAnalytics.ClsMtd)):
@@ -354,26 +351,26 @@ class TestAnalyticsRelationMetaIterator(TestCase):
         # Iterate over all type
         for mtd in od_mtd.iter_direct_related(GstAnalytics.RelTypes.RELATE_TO):
             assert mtd.id == expected_mtd_ids[count]
-            assert type(mtd) == expected_mtd_type[count]
-            if (type(mtd) == GstAnalytics.ODMtd):
+            assert type(mtd) is expected_mtd_type[count]
+            if (type(mtd) is GstAnalytics.ODMtd):
                 assert mtd.get_obj_type() == GLib.quark_from_string("od")
-            elif (type(mtd) == GstAnalytics.ClsMtd):
+            elif (type(mtd) is GstAnalytics.ClsMtd):
                 assert mtd.get_quark(0) == GLib.quark_from_string("cls")
             count = count + 1
 
-        assert(count == len(expected_mtd_ids))
+        assert (count == len(expected_mtd_ids))
 
         # Iterate over only with type GstAnalytics.ODMtd
         count = 0
         for mtd in od_mtd.iter_direct_related(GstAnalytics.RelTypes.RELATE_TO, GstAnalytics.ODMtd):
             assert mtd.id == expected_mtd_ids[count]
-            assert  type(mtd) == GstAnalytics.ODMtd
+            assert type(mtd) is GstAnalytics.ODMtd
             count = count + 1
 
-        assert(count == 2)
+        assert (count == 2)
 
         # Create a relation path as od_mtd -> cls_mtd -> trk_mtd -> seg_mtd
-        rmeta.set_relation(GstAnalytics.RelTypes.NONE, od_mtd.id, trk_mtd.id) # clear relation
+        rmeta.set_relation(GstAnalytics.RelTypes.NONE, od_mtd.id, trk_mtd.id)  # clear relation
         rmeta.set_relation(GstAnalytics.RelTypes.RELATE_TO, cls_mtd.id, trk_mtd.id)
         rmeta.set_relation(GstAnalytics.RelTypes.RELATE_TO, trk_mtd.id, seg_mtd.id)
         count = 0
@@ -381,4 +378,365 @@ class TestAnalyticsRelationMetaIterator(TestCase):
         for i in od_mtd.relation_path(seg_mtd, max_span=4):
             assert i == expected_rel_ids[count]
             count += 1
-        assert(count == 4)
+        assert (count == 4)
+
+
+class TestModelInfo(TestCase):
+    def test_modelinfo_load_not_found(self):
+        """Test loading a modelinfo file that doesn't exist"""
+        modelinfo = GstAnalytics.ModelInfo.load("/nonexistent/model.onnx")
+        # Should return None if file not found
+        self.assertIsNone(modelinfo)
+
+    def test_modelinfo_with_temporary_file(self):
+        """Test modelinfo API with a temporary modelinfo file"""
+        import tempfile
+        import os
+
+        # Create a temporary modelinfo file
+        modelinfo_content = """
+[modelinfo]
+version=1.0
+group-id=test-model-v1
+
+[input_tensor]
+dims=1,224,224,3
+dir=input
+type=uint8
+ranges=0.0,255.0
+
+[output_tensor]
+dims=1,1000
+dir=output
+type=float32
+id=output_logits
+"""
+
+        # Create temporary file
+        with tempfile.NamedTemporaryFile(mode='w', suffix='.modelinfo',
+                                         delete=False) as f:
+            f.write(modelinfo_content)
+            temp_modelinfo = f.name
+
+        try:
+            # Remove .modelinfo extension to get model filename
+            model_filename = temp_modelinfo[:-10]  # Remove '.modelinfo'
+
+            # Load the modelinfo using ModelInfo.load()
+            modelinfo = GstAnalytics.ModelInfo.load(model_filename)
+            self.assertIsNotNone(modelinfo)
+
+            # Verify it's a ModelInfo object
+            self.assertIsInstance(modelinfo, GstAnalytics.ModelInfo)
+
+            # Test get_version
+            version = modelinfo.get_version()
+            self.assertEqual(version, "1.0")
+
+            # Test get_group_id
+            group_id = modelinfo.get_group_id()
+            self.assertEqual(group_id, "test-model-v1")
+
+            # Test get_group_id as quark
+            group_id_quark = modelinfo.get_quark_group_id()
+            self.assertEqual(group_id_quark, GLib.quark_from_string("test-model-v1"))
+
+            # Test find_tensor_name by name
+            tensor_name = modelinfo.find_tensor_name(
+                GstAnalytics.ModelInfoTensorDirection.INPUT,
+                0,  # index
+                "input_tensor",  # in_tensor_name hint
+                GstAnalytics.TensorDataType.UINT8,
+                [1, 224, 224, 3]  # dims
+            )
+            self.assertEqual(tensor_name, "input_tensor")
+
+            # Test get_id
+            output_id = modelinfo.get_id("output_tensor")
+            self.assertEqual(output_id, "output_logits")
+
+            # Test get_id as quark
+            output_id_quark = modelinfo.get_quark_id("output_tensor")
+            self.assertEqual(output_id_quark, GLib.quark_from_string("output_logits"))
+
+            # Test get_input_scales_offsets
+            # Case 1: uint8 input [0, 255] to target range [0, 255] (passthrough)
+            # GObject Introspection returns (success, scales, offsets)
+            input_mins = [0.0]  # uint8 minimum
+            input_maxs = [255.0]  # uint8 maximum
+            result = modelinfo.get_input_scales_offsets("input_tensor",
+              input_mins, input_maxs)
+            self.assertTrue(result[0])  # success
+            scales = result[1]
+            offsets = result[2]
+            self.assertEqual(len(scales), 1)  # scales should have 1 element
+            self.assertEqual(len(offsets), 1)  # offsets should have 1 element
+            self.assertAlmostEqual(scales[0], 1.0, 6)  # (255-0)/(255-0) = 1.0
+            self.assertAlmostEqual(offsets[0], 0.0, 6)  # 0 - 0*1.0 = 0.0
+
+            # Test get_dims_order (should default to row-major)
+            dims_order = modelinfo.get_dims_order("input_tensor")
+            self.assertEqual(dims_order, GstAnalytics.TensorDimOrder.ROW_MAJOR)
+
+            # Test get_target_ranges (returns arrays of min/max from ranges)
+            result = modelinfo.get_target_ranges("input_tensor")
+            # ranges field contains "0.0,255.0" so this should succeed
+            self.assertTrue(result[0])  # success
+            mins = result[1]
+            maxs = result[2]
+            self.assertEqual(len(mins), 1)  # should have 1 range
+            self.assertEqual(len(maxs), 1)  # should have 1 range
+            self.assertAlmostEqual(mins[0], 0.0, 6)
+            self.assertAlmostEqual(maxs[0], 255.0, 6)
+
+            # Free the modelinfo
+            modelinfo.free()
+
+        finally:
+            # Clean up temporary file
+            if os.path.exists(temp_modelinfo):
+                os.unlink(temp_modelinfo)
+            if os.path.exists(model_filename):
+                os.unlink(model_filename)
+
+    def test_modelinfo_version_major_minor(self):
+        """Test modelinfo version string parsing for major and minor versions"""
+        import tempfile
+        import os
+
+        # Test case: Version 1.0 (current format version)
+        modelinfo_content_1_0 = """
+[modelinfo]
+version=1.0
+group-id=test-model-v1
+
+[input_tensor]
+dims=1,224,224,3
+dir=input
+type=uint8
+
+[output_tensor]
+dims=1,1000
+dir=output
+type=float32
+id=output_logits
+"""
+
+        with tempfile.NamedTemporaryFile(mode='w', suffix='.modelinfo',
+                                         delete=False) as f:
+            f.write(modelinfo_content_1_0)
+            temp_modelinfo = f.name
+
+        try:
+            model_filename = temp_modelinfo[:-10]  # Remove '.modelinfo'
+            modelinfo = GstAnalytics.ModelInfo.load(model_filename)
+            self.assertIsNotNone(modelinfo)
+
+            # Verify version string
+            version = modelinfo.get_version()
+            self.assertEqual(version, "1.0")
+
+            # Parse version string to verify major and minor components
+            version_parts = version.split('.')
+            self.assertEqual(len(version_parts), 2)
+            major_version = int(version_parts[0])
+            minor_version = int(version_parts[1])
+            self.assertEqual(major_version, 1)
+            self.assertEqual(minor_version, 0)
+
+            modelinfo.free()
+        finally:
+            if os.path.exists(temp_modelinfo):
+                os.unlink(temp_modelinfo)
+            if os.path.exists(model_filename):
+                os.unlink(model_filename)
+
+    def test_modelinfo_version_major_upgrade_rejected(self):
+        """Test that modelinfo with unsupported major version is rejected"""
+        import tempfile
+        import os
+
+        # Test case: Version 2.0 (unsupported major version)
+        # The version check should reject this
+        modelinfo_content_2_0 = """
+[modelinfo]
+version=2.0
+group-id=test-model-v2
+
+[input_tensor]
+dims=1,224,224,3
+dir=input
+type=uint8
+
+[output_tensor]
+dims=1,1000
+dir=output
+type=float32
+id=output_logits
+"""
+
+        with tempfile.NamedTemporaryFile(mode='w', suffix='.modelinfo',
+                                         delete=False) as f:
+            f.write(modelinfo_content_2_0)
+            temp_modelinfo = f.name
+
+        try:
+            model_filename = temp_modelinfo[:-10]  # Remove '.modelinfo'
+            # Load should fail because version 2.0 is not supported
+            modelinfo = GstAnalytics.ModelInfo.load(model_filename)
+            self.assertIsNone(modelinfo)
+        finally:
+            if os.path.exists(temp_modelinfo):
+                os.unlink(temp_modelinfo)
+            if os.path.exists(model_filename):
+                os.unlink(model_filename)
+
+    def test_modelinfo_input_ranges_transformations(self):
+        """Test modelinfo get_input_scales_offsets with different input ranges"""
+        import tempfile
+        import os
+
+        # Create a modelinfo with a tensor that expects normalized [0, 1] range
+        modelinfo_content = """
+[modelinfo]
+version=1.0
+group-id=test-model-normalization
+
+[input_normalized]
+dims=1,224,224,3
+dir=input
+type=uint8
+ranges=0.0,1.0;0.0,1.0;0.0,1.0
+"""
+
+        with tempfile.NamedTemporaryFile(mode='w', suffix='.modelinfo',
+                                         delete=False) as f:
+            f.write(modelinfo_content)
+            temp_modelinfo = f.name
+
+        try:
+            model_filename = temp_modelinfo[:-10]
+            modelinfo = GstAnalytics.ModelInfo.load(model_filename)
+            self.assertIsNotNone(modelinfo)
+
+            # Test 1: uint8 input [0, 255] to target [0, 1] (normalization)
+            # Expected: scale = (1-0)/(255-0) ≈ 0.00392, offset = 0 - 0*scale = 0.0
+            input_mins = [0.0, 0.0, 0.0]
+            input_maxs = [255.0, 255.0, 255.0]
+            result = modelinfo.get_input_scales_offsets("input_normalized",
+                input_mins, input_maxs)
+            self.assertTrue(result[0])
+            scales = result[1]
+            offsets = result[2]
+            self.assertEqual(len(scales), 3)
+            for i in range(3):
+                self.assertAlmostEqual(scales[i], 1.0 / 255.0, 6)
+                self.assertAlmostEqual(offsets[i], 0.0, 6)
+
+            modelinfo.free()
+        finally:
+            if os.path.exists(temp_modelinfo):
+                os.unlink(temp_modelinfo)
+            if os.path.exists(model_filename):
+                os.unlink(model_filename)
+
+        # Create a modelinfo with a tensor that expects [-1, 1] range
+        modelinfo_content_signed = """
+[modelinfo]
+version=1.0
+group-id=test-model-signed
+
+[input_signed]
+dims=1,224,224,3
+dir=input
+type=int8
+ranges=-1.0,1.0;-1.0,1.0;-1.0,1.0
+"""
+
+        with tempfile.NamedTemporaryFile(mode='w', suffix='.modelinfo',
+                                         delete=False) as f:
+            f.write(modelinfo_content_signed)
+            temp_modelinfo = f.name
+
+        try:
+            model_filename = temp_modelinfo[:-10]
+            modelinfo = GstAnalytics.ModelInfo.load(model_filename)
+            self.assertIsNotNone(modelinfo)
+
+            # Test 2: int8 input [-128, 127] to target [-1, 1]
+            # Expected: scale = (1-(-1))/(127-(-128)) = 2/255 ≈ 0.00784
+            #           offset = -1 - (-128)*scale = -1 + 128*0.00784 ≈ 0.00392
+            input_mins = [-128.0, -128.0, -128.0]
+            input_maxs = [127.0, 127.0, 127.0]
+            result = modelinfo.get_input_scales_offsets("input_signed",
+              input_mins, input_maxs)
+            self.assertTrue(result[0])
+            scales = result[1]
+            offsets = result[2]
+            self.assertEqual(len(scales), 3)
+            expected_scale = 2.0 / 255.0
+            expected_offset = -1.0 - (-128.0) * expected_scale
+            for i in range(3):
+                self.assertAlmostEqual(scales[i], expected_scale, 6)
+                self.assertAlmostEqual(offsets[i], expected_offset, 6)
+
+            modelinfo.free()
+        finally:
+            if os.path.exists(temp_modelinfo):
+                os.unlink(temp_modelinfo)
+            if os.path.exists(model_filename):
+                os.unlink(model_filename)
+
+    def test_modelinfo_version_minor_upgrade_accepted(self):
+        """Test that modelinfo with same major version but higher minor version is accepted"""
+        import tempfile
+        import os
+
+        # Test case: Version 1.5 (same major version, higher minor version)
+        # The version check should accept this since it's backward compatible
+        modelinfo_content_1_5 = """[modelinfo]
+version=1.5
+group-id=test-model-v1-5
+
+[input_tensor]
+dims=1,224,224,3
+dir=input
+type=uint8
+
+[output_tensor]
+dims=1,1000
+dir=output
+type=float32
+id=output_logits
+"""
+
+        with tempfile.NamedTemporaryFile(mode='w', suffix='.modelinfo',
+                                         delete=False) as f:
+            f.write(modelinfo_content_1_5)
+            temp_modelinfo = f.name
+
+        try:
+            model_filename = temp_modelinfo[:-10]  # Remove '.modelinfo'
+            # Load should succeed because version 1.5 is compatible with 1.0
+            # (same major version)
+            modelinfo = GstAnalytics.ModelInfo.load(model_filename)
+            self.assertIsNotNone(modelinfo)
+
+            # Verify version string
+            version = modelinfo.get_version()
+            self.assertEqual(version, "1.5")
+
+            # Parse version string to verify major and minor components
+            version_parts = version.split('.')
+            self.assertEqual(len(version_parts), 2)
+            major_version = int(version_parts[0])
+            minor_version = int(version_parts[1])
+            self.assertEqual(major_version, 1)
+            self.assertEqual(minor_version, 5)
+
+            modelinfo.free()
+        finally:
+            if os.path.exists(temp_modelinfo):
+                os.unlink(temp_modelinfo)
+            if os.path.exists(model_filename):
+                os.unlink(model_filename)
