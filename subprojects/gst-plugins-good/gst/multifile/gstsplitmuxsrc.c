b469d392a47efe34d939396f41ce463ecaa5e640
Jan Schmidt <jan@centricular.com>
2025-12-09 22:48:22 +1100

    splitmuxsrc: Clear segment seqnum when restarting
    Minor fix for clean reuse. Also, use the GST_SEQNUM_INVALID
    constant instead of direct comparison to 0
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/10348>

--- subprojects/gst-plugins-good/gst/multifile/gstsplitmuxsrc.c
+++ subprojects/gst-plugins-good/gst/multifile/gstsplitmuxsrc.c
@@ -794,7 +794,7 @@ gst_splitmux_handle_event (GstSplitMuxSrc * splitmux,
       if (gst_splitmux_end_of_part (splitmux, splitpad))
         // Continuing to next part, drop the EOS
         goto drop_event;
-      if (splitmux->segment_seqnum) {
+      if (splitmux->segment_seqnum != GST_SEQNUM_INVALID) {
         event = gst_event_make_writable (event);
         gst_event_set_seqnum (event, splitmux->segment_seqnum);
       }
@@ -850,7 +850,7 @@ gst_splitmux_handle_event (GstSplitMuxSrc * splitmux,
 
       gst_event_unref (event);
       event = gst_event_new_segment (&seg);
-      if (splitmux->segment_seqnum)
+      if (splitmux->segment_seqnum != GST_SEQNUM_INVALID)
         gst_event_set_seqnum (event, splitmux->segment_seqnum);
       splitpad->sent_segment = TRUE;
       break;
@@ -1231,6 +1231,8 @@ gst_splitmux_src_start (GstSplitMuxSrc * splitmux)
       }
     }
   }
+
+  splitmux->segment_seqnum = GST_SEQNUM_INVALID;
 
   splitmux->pads_complete = FALSE;
   splitmux->running = TRUE;
