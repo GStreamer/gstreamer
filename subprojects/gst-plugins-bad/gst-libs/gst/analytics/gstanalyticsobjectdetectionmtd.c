61f40ddbd869a6757af76414ae5ef2d00ae00666
Olivier Crête <olivier.crete@collabora.com>
2025-12-24 11:08:10 -0500

    objectdetectionmtd: Reject transformations that aren't 90deg based or symetries
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/10443>

--- subprojects/gst-plugins-bad/gst-libs/gst/analytics/gstanalyticsobjectdetectionmtd.c
+++ subprojects/gst-plugins-bad/gst-libs/gst/analytics/gstanalyticsobjectdetectionmtd.c
@@ -82,6 +82,28 @@ gst_analytics_od_mtd_meta_transform (GstBuffer * transbuf,
         transmtd->id);
     GstVideoRectangle rect = { oddata->x, oddata->y, oddata->w, oddata->h };
 
+    gboolean is_diagonal = trans->matrix[0][1] == 0 && trans->matrix[1][0] == 0;
+    gboolean is_antidiagonal = trans->matrix[0][0] == 0 &&
+        trans->matrix[1][1] == 0;
+
+    if (!is_diagonal && !is_antidiagonal) {
+      GST_WARNING ("Transformation not possible from buffer %" GST_PTR_FORMAT
+          " to buffer %" GST_PTR_FORMAT, buffer, transbuf);
+      return FALSE;
+    } else if (is_diagonal) {
+      if (trans->matrix[0][0] == 0 || trans->matrix[1][1] == 0) {
+        GST_WARNING ("Transformation not possible from buffer %" GST_PTR_FORMAT
+            " to buffer %" GST_PTR_FORMAT, buffer, transbuf);
+        return FALSE;
+      }
+    } else {
+      if (trans->matrix[0][1] == 0 || trans->matrix[1][0] == 0) {
+        GST_WARNING ("Transformation not possible from buffer %" GST_PTR_FORMAT
+            " to buffer %" GST_PTR_FORMAT, buffer, transbuf);
+        return FALSE;
+      }
+    }
+
     if (!gst_video_meta_transform_matrix_rectangle (trans, &rect))
       return FALSE;
 
