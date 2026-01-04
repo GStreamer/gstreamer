2700371870f924d727176b8b82262e9a88fbd077
Christian Gräfe <cgraefe83@gmail.com>
2026-01-04 14:31:05 +0100

    smoothstreaming: remove trailing space from desc
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/10729>

--- subprojects/gst-plugins-bad/ext/smoothstreaming/gstsmoothstreaming-plugin.c
+++ subprojects/gst-plugins-bad/ext/smoothstreaming/gstsmoothstreaming-plugin.c
@@ -37,5 +37,5 @@ plugin_init (GstPlugin * plugin)
 GST_PLUGIN_DEFINE (GST_VERSION_MAJOR,
     GST_VERSION_MINOR,
     smoothstreaming,
-    "Microsoft's Smooth Streaming format support ",
+    "Microsoft's Smooth Streaming format support",
     plugin_init, VERSION, "LGPL", GST_PACKAGE_NAME, GST_PACKAGE_ORIGIN);
