1c6e163aa33962f5ee4a87d29319ccdd5cb67612
Ruben Gonzalez <rgonzalez@fluendo.com>
2026-01-08 22:16:19 +0100

    identity: cosmetic change to mimic fakesink
    Remove extra space at chain log:
    ```
    $ gst-launch-1.0 -v videotestsrc num-buffers=1
      ! identity silent=false name=i1 ! fakesink name=i2 silent=false
    
    ...pts: 0:00:00.000000000, duration: 0:00:00.033333333, offset: 0, offset_end:  1, flags: 00000040..
    ...pts: 0:00:00.000000000, duration: 0:00:00.033333333, offset: 0, offset_end: 1, flags: 00000040...
    ```
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/10518>

--- subprojects/gstreamer/plugins/elements/gstidentity.c
+++ subprojects/gstreamer/plugins/elements/gstidentity.c
@@ -713,7 +713,7 @@ gst_identity_update_last_message_for_buffer (GstIdentity * identity,
   g_free (identity->last_message);
   identity->last_message = g_strdup_printf ("%s   ******* (%s:%s) "
       "(%" G_GSIZE_FORMAT " bytes, dts: %s, pts: %s, duration: %s, offset: %"
-      G_GINT64_FORMAT ", " "offset_end: % " G_GINT64_FORMAT
+      G_GINT64_FORMAT ", " "offset_end: %" G_GINT64_FORMAT
       ", flags: %08x %s, meta: %s) %p", action,
       GST_DEBUG_PAD_NAME (GST_BASE_TRANSFORM_CAST (identity)->sinkpad), size,
       print_pretty_time (dts_str, sizeof (dts_str), GST_BUFFER_DTS (buf)),
