d29234fa07384599a2f35e129a431a6c379f28a1
Marcus Hanestad <marlhan@proton.me>
2026-02-03 10:59:13 +0100

    androidmedia: support decoding flac
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/10753>

--- subprojects/gst-plugins-bad/sys/androidmedia/gstamcaudiodec.c
+++ subprojects/gst-plugins-bad/sys/androidmedia/gstamcaudiodec.c
@@ -160,6 +160,8 @@ caps_to_mime (GstCaps * caps)
     return "audio/vorbis";
   } else if (strcmp (name, "audio/x-opus") == 0) {
     return "audio/opus";
+  } else if (strcmp (name, "audio/x-flac") == 0) {
+    return "audio/flac";
   } else if (strcmp (name, "audio/x-ac3") == 0) {
     return "audio/ac3";
   } else if (strcmp (name, "audio/x-eac3") == 0) {
