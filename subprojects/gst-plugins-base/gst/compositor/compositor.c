GStreamer/gstreamer

commit e8e12db5f471dc432529185790109486a1fc64f0

debug: Category init should happen in class_init when possible
plugin_init() will not get called if element/feature registration
happens manually, such as when using linking only specific plugin
features with gstreamer-full. That is possible when plugins contain
static features.

Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/9496>

subprojects/gst-plugins-base/gst/compositor/compositor.c
@@ -723,6 +723,11 @@ gst_compositor_pad_class_init (GstCompositorPadClass * klass)
       GST_DEBUG_FUNCPTR (gst_compositor_pad_create_conversion_info);
 
   gst_type_mark_as_plugin_api (GST_TYPE_COMPOSITOR_SIZING_POLICY, 0);
+
+  GST_DEBUG_CATEGORY_INIT (gst_compositor_debug, "compositor", 0, "compositor");
+
+  gst_compositor_init_blend ();
+
 }
 
 static void
@@ -2109,10 +2114,6 @@ gst_compositor_child_proxy_init (gpointer g_iface, gpointer iface_data)
 static gboolean
 plugin_init (GstPlugin * plugin)
 {
-  GST_DEBUG_CATEGORY_INIT (gst_compositor_debug, "compositor", 0, "compositor");
-
-  gst_compositor_init_blend ();
-
   return GST_ELEMENT_REGISTER (compositor, plugin);
 }
 

subprojects/gst-plugins-bad/ext/svthevcenc/gstsvthevcenc.c
@@ -627,6 +627,9 @@ gst_svthevc_enc_class_init (GstSvtHevcEncClass * klass)
   gst_type_mark_as_plugin_api (GST_SVTHEVC_ENC_PRED_STRUCT_TYPE, 0);
   gst_type_mark_as_plugin_api (GST_SVTHEVC_ENC_RC_TYPE, 0);
   gst_type_mark_as_plugin_api (GST_SVTHEVC_ENC_TUNE_TYPE, 0);
+
+  GST_DEBUG_CATEGORY_INIT (svthevc_enc_debug, "svthevcenc", 0,
+      "h265 encoding element");
 }
 
 static void
@@ -2283,9 +2286,6 @@ gst_svthevc_enc_get_property (GObject * object, guint prop_id,
 static gboolean
 plugin_init (GstPlugin * plugin)
 {
-  GST_DEBUG_CATEGORY_INIT (svthevc_enc_debug, "svthevcenc", 0,
-      "h265 encoding element");
-
   return gst_element_register (plugin, "svthevcenc",
       GST_RANK_PRIMARY, GST_TYPE_SVTHEVC_ENC);
 }

subprojects/gst-plugins-bad/ext/wayland/gstwaylandsink.c
@@ -209,6 +209,9 @@ gst_wayland_sink_class_init (GstWaylandSinkClass * klass)
   * Since: 1.22
   */
   gst_video_overlay_install_properties (gobject_class, PROP_LAST);
+
+  GST_DEBUG_CATEGORY_INIT (gstwayland_debug, "waylandsink", 0,
+      " wayland video sink");
 }
 
 static void
@@ -1252,9 +1255,6 @@ gst_wayland_sink_expose (GstVideoOverlay * overlay)
 static gboolean
 plugin_init (GstPlugin * plugin)
 {
-  GST_DEBUG_CATEGORY_INIT (gstwayland_debug, "waylandsink", 0,
-      " wayland video sink");
-
   return GST_ELEMENT_REGISTER (waylandsink, plugin);
 }
 

subprojects/gst-plugins-bad/sys/winks/gstksclock.c
@@ -65,6 +65,8 @@ gst_ks_clock_class_init (GstKsClockClass * klass)
 
   gobject_class->dispose = gst_ks_clock_dispose;
   gobject_class->finalize = gst_ks_clock_finalize;
+
+  gst_ks_debug_init ();
 }
 
 static void

subprojects/gst-plugins-bad/sys/winks/gstksvideodevice.c
@@ -131,6 +131,8 @@ gst_ks_video_device_class_init (GstKsVideoDeviceClass * klass)
       g_param_spec_string ("device-path", "Device Path",
           "The device path", DEFAULT_DEVICE_PATH,
           G_PARAM_READWRITE | G_PARAM_CONSTRUCT_ONLY | G_PARAM_STATIC_STRINGS));
+
+  gst_ks_debug_init ();
 }
 
 static void

subprojects/gst-plugins-bad/sys/winks/gstksvideosrc.c
@@ -221,6 +221,8 @@ gst_ks_video_src_class_init (GstKsVideoSrcClass * klass)
       g_param_spec_boolean ("enable-quirks", "Enable quirks",
           "Enable driver-specific quirks", DEFAULT_ENABLE_QUIRKS,
           G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS));
+
+  gst_ks_debug_init ();
 }
 
 static void
@@ -1028,8 +1030,7 @@ error_alloc_buffer:
 static gboolean
 plugin_init (GstPlugin * plugin)
 {
-  GST_DEBUG_CATEGORY_INIT (gst_ks_debug, "ksvideosrc",
-      0, "Kernel streaming video source");
+  gst_ks_debug_init ();
 
   if (!gst_element_register (plugin, "ksvideosrc",
           GST_RANK_PRIMARY, GST_TYPE_KS_VIDEO_SRC))

subprojects/gst-plugins-bad/sys/winks/ksdeviceprovider.c
@@ -66,6 +66,8 @@ gst_ks_device_provider_class_init (GstKsDeviceProviderClass * klass)
       "KernelStreaming Device Provider", "Sink/Source/Audio/Video",
       "List and provide KernelStreaming source and sink devices",
       "Руслан Ижбулатов <lrn1986@gmail.com>");
+
+  gst_ks_debug_init ();
 }
 
 static void
@@ -658,6 +660,8 @@ gst_ks_device_class_init (GstKsDeviceClass * klass)
       g_param_spec_string ("path", "System device path",
           "The system path to the device", "",
           G_PARAM_STATIC_STRINGS | G_PARAM_READWRITE | G_PARAM_CONSTRUCT_ONLY));
+
+  gst_ks_debug_init ();
 }
 
 static void

subprojects/gst-plugins-bad/sys/winks/ksvideohelpers.c
@@ -748,3 +748,14 @@ ks_video_get_all_caps (void)
 
   return caps;
 }
+
+void
+gst_ks_debug_init (void)
+{
+  static gsize res = 0;
+  if (g_once_init_enter (&res)) {
+    GST_DEBUG_CATEGORY_INIT (gst_ks_debug, "ksvideosrc",
+        0, "Kernel streaming video source");
+    g_once_init_leave (&res, 1);
+  }
+}

commit ae20e7b221aea1ef820a26743656aa0b53d63202

compositor: move gst_compositor_init_blend() to element class_init
Commit e8e12db5f4 moved gst_compositor_init_blend() from plugin_init()
to gst_compositor_pad_class_init() so it would be called even without
plugin_init() (e.g. gstreamer-full static linking).

However, pad_class_init is only called when the first GstCompositorPad
is instantiated. When compositor runs with force-live=true and no sink
pads, no pad is ever created, so the global blend/fill function pointers
remain NULL, causing a segfault in _draw_background().

Move the call to gst_compositor_class_init() instead, which is always
called when a GstCompositor element is created, regardless of whether
any pads exist. This preserves the original intent of not depending on
plugin_init() while ensuring the blend functions are always initialized.

This is the same pattern applied by c0f9029a13 which moved
GST_DEBUG_CATEGORY_INIT from pad_class_init to compositor_class_init
for the same reason.

Fixes: e8e12db5f4 ("debug: Category init should happen in class_init when possible")

Reproducer:
  gst-launch-1.0 compositor force-live=true background=black \
    ! fakesink sync=true

Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/11078>

subprojects/gst-plugins-base/gst/compositor/compositor.c
@@ -723,8 +723,6 @@ gst_compositor_pad_class_init (GstCompositorPadClass * klass)
       GST_DEBUG_FUNCPTR (gst_compositor_pad_create_conversion_info);
 
   gst_type_mark_as_plugin_api (GST_TYPE_COMPOSITOR_SIZING_POLICY, 0);
-
-  gst_compositor_init_blend ();
 }
 
 static void
@@ -2035,6 +2033,8 @@ gst_compositor_class_init (GstCompositorClass * klass)
   agg_class->negotiated_src_caps = _negotiated_caps;
   agg_class->stop = GST_DEBUG_FUNCPTR (gst_composior_stop);
   videoaggregator_class->aggregate_frames = gst_compositor_aggregate_frames;
+
+  gst_compositor_init_blend ();
 
   g_object_class_install_property (gobject_class, PROP_BACKGROUND,
       g_param_spec_enum ("background", "Background", "Background type",