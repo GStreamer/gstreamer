1f823ee39f253fc23ad95e6833281146cad8ecad
Sebastian Dröge <sebastian@centricular.com>
2026-03-26 18:37:50 +0200

    h264parse: Avoid NULL pointer dereferences when freeing partially parsed SPS/MVC data
    Fixes https://gitlab.freedesktop.org/gstreamer/gstreamer/-/work_items/4992
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/11240>

--- subprojects/gst-plugins-bad/gst-libs/gst/codecparsers/gsth264parser.c
+++ subprojects/gst-plugins-bad/gst-libs/gst/codecparsers/gsth264parser.c
@@ -2623,15 +2623,19 @@ gst_h264_sps_mvc_clear (GstH264SPS * sps)
   g_free (mvc->view);
   mvc->view = NULL;
 
-  for (i = 0; i <= mvc->num_level_values_signalled_minus1; i++) {
-    GstH264SPSExtMVCLevelValue *const level_value = &mvc->level_value[i];
+  if (mvc->level_value) {
+    for (i = 0; i <= mvc->num_level_values_signalled_minus1; i++) {
+      GstH264SPSExtMVCLevelValue *const level_value = &mvc->level_value[i];
 
-    for (j = 0; j <= level_value->num_applicable_ops_minus1; j++) {
-      g_free (level_value->applicable_op[j].target_view_id);
-      level_value->applicable_op[j].target_view_id = NULL;
+      if (level_value->applicable_op) {
+        for (j = 0; j <= level_value->num_applicable_ops_minus1; j++) {
+          g_free (level_value->applicable_op[j].target_view_id);
+          level_value->applicable_op[j].target_view_id = NULL;
+        }
+      }
+      g_free (level_value->applicable_op);
+      level_value->applicable_op = NULL;
     }
-    g_free (level_value->applicable_op);
-    level_value->applicable_op = NULL;
   }
   g_free (mvc->level_value);
   mvc->level_value = NULL;
