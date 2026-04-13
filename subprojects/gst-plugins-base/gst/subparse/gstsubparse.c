ae57f973ad6eef185fdbe9272efe967dde6f57d9
Sebastian Dröge <sebastian@centricular.com>
2026-04-13 19:23:04 +0300

    subparse: Use a GString to accumulate text results from GMarkupParser
    Repeated g_strconcat() is unnecessarily inefficient.
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/11362>

--- subprojects/gst-plugins-base/gst/subparse/gstsubparse.c
+++ subprojects/gst-plugins-base/gst/subparse/gstsubparse.c
@@ -1631,16 +1631,9 @@ static void
 xml_text (GMarkupParseContext * context,
     const gchar * text, gsize text_len, gpointer user_data, GError ** error)
 {
-  gchar **accum = (gchar **) user_data;
-  gchar *concat;
+  GString *accum = user_data;
 
-  if (*accum) {
-    concat = g_strconcat (*accum, text, NULL);
-    g_free (*accum);
-    *accum = concat;
-  } else {
-    *accum = g_strdup (text);
-  }
+  g_string_append_len (accum, text, text_len);
 }
 
 static gchar *
@@ -1648,10 +1641,10 @@ strip_pango_markup (gchar * markup, GError ** error)
 {
   GMarkupParser parser = { 0, };
   GMarkupParseContext *context;
-  gchar *accum = NULL;
+  GString *accum = g_string_new (NULL);
 
   parser.text = xml_text;
-  context = g_markup_parse_context_new (&parser, 0, &accum, NULL);
+  context = g_markup_parse_context_new (&parser, 0, accum, NULL);
 
   g_markup_parse_context_parse (context, "<root>", 6, NULL);
   g_markup_parse_context_parse (context, markup, strlen (markup), error);
@@ -1665,10 +1658,10 @@ strip_pango_markup (gchar * markup, GError ** error)
 
 done:
   g_markup_parse_context_free (context);
-  return accum;
+  return accum ? g_string_free (accum, FALSE) : NULL;
 
 error:
-  g_free (accum);
+  g_string_free (accum, TRUE);
   accum = NULL;
   goto done;
 }
