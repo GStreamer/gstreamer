f82735122839f981ae2ba328e82396b4b08482c7
Sebastian Dröge <sebastian@centricular.com>
2026-05-03 11:48:44 +0300

    aggregator: Fix documentation
    Parameter docs for various functions were missing.
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/11488>

--- subprojects/gstreamer/libs/gst/base/gstaggregator.c
+++ subprojects/gstreamer/libs/gst/base/gstaggregator.c
@@ -4162,6 +4162,8 @@ gst_aggregator_simple_get_next_time (GstAggregator * self)
 
 /**
  * gst_aggregator_update_segment:
+ * @self: A #GstAggregator
+ * @segment: The new #GstSegment
  *
  * Subclasses should use this to update the segment on their
  * source pad, instead of directly pushing new segment events
@@ -4192,6 +4194,7 @@ gst_aggregator_update_segment (GstAggregator * self, const GstSegment * segment)
 
 /**
  * gst_aggregator_selected_samples:
+ * @self: A #GstAggregator
  * @pts: The presentation timestamp of the next output buffer
  * @dts: The decoding timestamp of the next output buffer
  * @duration: The duration of the next output buffer
@@ -4228,6 +4231,7 @@ gst_aggregator_selected_samples (GstAggregator * self,
 
 /**
  * gst_aggregator_set_ignore_inactive_pads:
+ * @self: A #GstAggregator
  * @ignore: whether inactive pads should not be waited on
  *
  * Subclasses should call this when they don't want to time out
@@ -4251,6 +4255,7 @@ gst_aggregator_set_ignore_inactive_pads (GstAggregator * self, gboolean ignore)
 
 /**
  * gst_aggregator_get_ignore_inactive_pads:
+ * @self: A #GstAggregator
  *
  * Returns: whether inactive pads will not be waited on
  * Since: 1.20
@@ -4271,6 +4276,7 @@ gst_aggregator_get_ignore_inactive_pads (GstAggregator * self)
 
 /**
  * gst_aggregator_get_force_live:
+ * @self: A #GstAggregator
  *
  * Subclasses may use the return value to inform whether they should return
  * %GST_FLOW_EOS from their aggregate implementation.
@@ -4287,6 +4293,8 @@ gst_aggregator_get_force_live (GstAggregator * self)
 
 /**
  * gst_aggregator_set_force_live:
+ * @self: A #GstAggregator
+ * @force_live: The new value
  *
  * Subclasses should call this at construction time in order for @self to
  * aggregate on a timeout even when no live source is connected.
