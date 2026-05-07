29d83db7a3928ad43844bf800b0d0c0d4e8a0232
Arthur Chan <arthur.chan@adalogics.com>
2026-05-07 16:49:08 +0100

    h266parser: Add missing freeing function for H266 SEI message
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/11571>

--- subprojects/gst-plugins-bad/gst-libs/gst/codecparsers/gsth266parser.c
+++ subprojects/gst-plugins-bad/gst-libs/gst/codecparsers/gsth266parser.c
@@ -6128,6 +6128,18 @@ error:
   return GST_H266_PARSER_ERROR;
 }
 
+// This is public API since 1.30
+static void
+gst_h266_sei_clear (GstH266SEIMessage * sei)
+{
+  if (sei->payloadType == GST_H266_SEI_REGISTERED_USER_DATA) {
+    GstH266RegisteredUserData *rud = &sei->payload.registered_user_data;
+    g_free ((guint8 *) rud->data);
+    rud->data = NULL;
+  }
+  memset (sei, 0, sizeof (GstH266SEIMessage));
+}
+
 /**
  * gst_h266_parser_parse_sei:
  * @parser: a #GstH266Parser
@@ -6154,6 +6166,7 @@ gst_h266_parser_parse_sei (GstH266Parser * nalparser, GstH266NalUnit * nalu,
   nal_reader_init (&nr, nalu->data + nalu->offset + nalu->header_bytes,
       nalu->size - nalu->header_bytes);
   *messages = g_array_new (FALSE, FALSE, sizeof (GstH266SEIMessage));
+  g_array_set_clear_func (*messages, (GDestroyNotify) gst_h266_sei_clear);
 
   do {
     res = gst_h266_parser_parse_sei_message (&sei, &nr, nalparser, nalu->type,
