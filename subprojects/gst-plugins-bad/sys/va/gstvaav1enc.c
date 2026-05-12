caa67c6d1a251acd5e7142dee73b38015f9c2aac
He Junyan <junyan.he@intel.com>
2026-05-12 18:36:27 +0800

    va: av1enc: Fix the typo of last_poped_index
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/11604>

--- subprojects/gst-plugins-bad/sys/va/gstvaav1enc.c
+++ subprojects/gst-plugins-bad/sys/va/gstvaav1enc.c
@@ -142,7 +142,7 @@ struct _GstVaAV1GFGroup
   /* Be different from group_frame_num, include repeat */
   gint output_frame_num;
   gint last_pushed_num;
-  gint last_poped_index;
+  gint last_popped_index;
   guint8 highest_level;
   gboolean use_alt;
   gboolean intra_only;
@@ -601,8 +601,8 @@ _av1_print_gf_group (GstVaAV1Enc * self, GstVaAV1GFGroup * gf_group)
       " GF Group ===========================\n");
   g_string_append_printf (str, " start:%d,  size:%d  ",
       gf_group->start_frame_offset, gf_group->group_frame_num);
-  g_string_append_printf (str, "pushed:%d,  poped:%d  ",
-      pushed_frame_num, gf_group->last_poped_index + 1);
+  g_string_append_printf (str, "pushed:%d,  popped:%d  ",
+      pushed_frame_num, gf_group->last_popped_index + 1);
   g_string_append_printf (str, "\n ALT: %s  max level: %d  output num: %d",
       gf_group->use_alt ? "yes" : "no", gf_group->highest_level,
       gf_group->output_frame_num);
@@ -868,7 +868,7 @@ _av1_init_gf_group (GstVaAV1GFGroup * gf_group, GQueue * reorder_list)
   gf_group->last_pushed_num = -1;
   gf_group->use_alt = FALSE;
   gf_group->intra_only = FALSE;
-  gf_group->last_poped_index = -1;
+  gf_group->last_popped_index = -1;
   gf_group->output_frame_num = 0;
 
   for (i = 0; i < MAX_GF_GROUP_SIZE * 2; i++) {
@@ -914,11 +914,11 @@ _av1_start_gf_group (GstVaAV1Enc * self, GstVideoCodecFrame * gf_frame)
   gf_group->use_alt = use_alt;
   gf_group->intra_only = intra_only;
 
-  gf_group->last_poped_index = -1;
+  gf_group->last_popped_index = -1;
   /* An already encoded frame as the GF,
      for example, the ALT of the previous GF group. */
   if (frame->flags & FRAME_FLAG_ALREADY_ENCODED)
-    gf_group->last_poped_index = 0;
+    gf_group->last_popped_index = 0;
 
   for (i = 0; i < MAX_GF_GROUP_SIZE * 2; i++) {
     gf_group->frame_types[i] = FRAME_TYPE_INVALID;
@@ -1007,7 +1007,7 @@ _av1_gf_group_push_frame (GstVaAV1GFGroup * gf_group,
 
   if (gf_group->use_alt)
     /* If we already begin pop, no more push again. */
-    g_return_val_if_fail (gf_group->last_poped_index <= 0, FALSE);
+    g_return_val_if_fail (gf_group->last_popped_index <= 0, FALSE);
 
   g_queue_push_tail (gf_group->reorder_list,
       gst_video_codec_frame_ref (gst_frame));
@@ -1033,10 +1033,10 @@ _av1_gf_group_pop_frame (GstVaAV1GFGroup * gf_group,
     goto no_frame;
 
   if (!gf_group->use_alt) {
-    g_assert (gf_group->last_poped_index < pushed_frame_num);
+    g_assert (gf_group->last_popped_index < pushed_frame_num);
 
-    if (gf_group->last_poped_index + 1 < pushed_frame_num) {
-      gf_group->last_poped_index++;
+    if (gf_group->last_popped_index + 1 < pushed_frame_num) {
+      gf_group->last_popped_index++;
       goto find_frame;
     }
 
@@ -1044,8 +1044,8 @@ _av1_gf_group_pop_frame (GstVaAV1GFGroup * gf_group,
   }
 
   /* The first frame of a GF group has no backward ref, pop immediately. */
-  if (gf_group->last_poped_index < 0) {
-    gf_group->last_poped_index++;
+  if (gf_group->last_popped_index < 0) {
+    gf_group->last_popped_index++;
     goto find_frame;
   }
 
@@ -1053,8 +1053,8 @@ _av1_gf_group_pop_frame (GstVaAV1GFGroup * gf_group,
   if (pushed_frame_num < gf_group->group_frame_num)
     goto no_frame;
 
-  gf_group->last_poped_index++;
-  g_assert (gf_group->last_poped_index < gf_group->output_frame_num);
+  gf_group->last_popped_index++;
+  g_assert (gf_group->last_popped_index < gf_group->output_frame_num);
 
 find_frame:
   vaframe = NULL;
@@ -1062,7 +1062,7 @@ find_frame:
 
   /* If repeating some frame, it should be in reference list,
      or it should be in reorder list. */
-  if (gf_group->frame_types[gf_group->last_poped_index] == FRAME_TYPE_REPEAT) {
+  if (gf_group->frame_types[gf_group->last_popped_index] == FRAME_TYPE_REPEAT) {
     for (i = 0; i < GST_AV1_NUM_REF_FRAMES; i++) {
       GstVaAV1EncFrame *vaf;
 
@@ -1071,7 +1071,7 @@ find_frame:
 
       vaf = _enc_frame (ref_list[i]);
       if (vaf->frame_num == gf_group->start_frame_offset +
-          gf_group->frame_offsets[gf_group->last_poped_index]) {
+          gf_group->frame_offsets[gf_group->last_popped_index]) {
         vaframe = vaf;
         frame = ref_list[i];
         break;
@@ -1090,7 +1090,7 @@ find_frame:
       f = g_queue_peek_nth (gf_group->reorder_list, i);
       vaf = _enc_frame (f);
       if (vaf->frame_num == gf_group->start_frame_offset +
-          gf_group->frame_offsets[gf_group->last_poped_index]) {
+          gf_group->frame_offsets[gf_group->last_popped_index]) {
         vaframe = vaf;
         frame = f;
         break;
@@ -1101,10 +1101,10 @@ find_frame:
     /* Clear that frame from reorder list. */
     g_queue_pop_nth (gf_group->reorder_list, i);
 
-    vaframe->type = gf_group->frame_types[gf_group->last_poped_index];
+    vaframe->type = gf_group->frame_types[gf_group->last_popped_index];
     vaframe->pyramid_level =
-        gf_group->pyramid_levels[gf_group->last_poped_index];
-    vaframe->flags = gf_group->flags[gf_group->last_poped_index];
+        gf_group->pyramid_levels[gf_group->last_popped_index];
+    vaframe->flags = gf_group->flags[gf_group->last_popped_index];
 
     /* unref frame popped from reorder queue */
     gst_video_codec_frame_unref (frame);
@@ -1130,7 +1130,7 @@ _av1_finish_current_gf_group (GstVaAV1Enc * self, GstVaAV1GFGroup * gf_group)
   g_assert (pushed_frame_num <= gf_group->group_frame_num);
 
   /* Alt comes and already finished. */
-  if (gf_group->use_alt && gf_group->last_poped_index > 0)
+  if (gf_group->use_alt && gf_group->last_popped_index > 0)
     return;
 
   /* Already pushed all frames. */
@@ -1142,7 +1142,7 @@ _av1_finish_current_gf_group (GstVaAV1Enc * self, GstVaAV1GFGroup * gf_group)
     gf_group->use_alt = FALSE;
 
   if (gf_group->use_alt == FALSE) {
-    g_assert (gf_group->last_poped_index < pushed_frame_num);
+    g_assert (gf_group->last_popped_index < pushed_frame_num);
 
     gf_group->group_frame_num = pushed_frame_num;
 
@@ -1215,15 +1215,15 @@ _av1_gf_group_is_empty (GstVaAV1GFGroup * gf_group)
     return TRUE;
 
   if (gf_group->use_alt == FALSE) {
-    g_assert (gf_group->last_poped_index + 1 <= pushed_frame_num);
-    if (gf_group->last_poped_index + 1 == pushed_frame_num)
+    g_assert (gf_group->last_popped_index + 1 <= pushed_frame_num);
+    if (gf_group->last_popped_index + 1 == pushed_frame_num)
       return TRUE;
 
     return FALSE;
   }
 
-  g_assert (gf_group->last_poped_index < gf_group->output_frame_num);
-  if (gf_group->last_poped_index == gf_group->output_frame_num - 1)
+  g_assert (gf_group->last_popped_index < gf_group->output_frame_num);
+  if (gf_group->last_popped_index == gf_group->output_frame_num - 1)
     return TRUE;
 
   return FALSE;
@@ -1232,8 +1232,8 @@ _av1_gf_group_is_empty (GstVaAV1GFGroup * gf_group)
 static inline gboolean
 _av1_gf_group_is_finished (GstVaAV1GFGroup * gf_group)
 {
-  g_assert (gf_group->last_poped_index < gf_group->output_frame_num);
-  if (gf_group->last_poped_index == gf_group->output_frame_num - 1)
+  g_assert (gf_group->last_popped_index < gf_group->output_frame_num);
+  if (gf_group->last_popped_index == gf_group->output_frame_num - 1)
     return TRUE;
 
   return FALSE;
@@ -1355,7 +1355,7 @@ gst_va_av1_enc_reorder_frame (GstVaBaseEnc * base, GstVideoCodecFrame * frame,
     if (_av1_gf_group_is_finished (&self->gop.current_group)) {
       g_assert (_av1_gf_group_is_empty (&self->gop.current_group));
 
-      /* Already poped all of the last group,
+      /* Already popped all of the last group,
          so begin a new group with this keyframe. */
       _av1_start_gf_group (self, frame);
     } else {
