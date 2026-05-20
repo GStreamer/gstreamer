c5627b621c8742ca3d5f45fe85c12a055d64bfa3
Jan Schmidt <jan@centricular.com>
2026-05-20 13:34:33 +1000

    gst-rtsp-server: Mark plugin with static features as such
    Add gst_plugin_set_static_features_flag() to the rtsp-sink plugin.
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/12018>

--- subprojects/gst-rtsp-server/gst/rtsp-sink/plugin.c
+++ subprojects/gst-rtsp-server/gst/rtsp-sink/plugin.c
@@ -7,6 +7,7 @@
 static gboolean
 plugin_init (GstPlugin * plugin)
 {
+  gst_plugin_set_static_features_flag (plugin);
 #ifdef ENABLE_NLS
   bindtextdomain (GETTEXT_PACKAGE, LOCALEDIR);
   bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
