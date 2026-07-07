a9aab10ece050e072c6c2ecb1712bf60e5323d3f
Sebastian Dröge <sebastian@centricular.com>
2026-07-07 15:22:34 +0300

    openjpegdec: Fix some typos in error messages
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/12246>

--- subprojects/gst-plugins-bad/ext/openjpeg/gstopenjpegdec.c
+++ subprojects/gst-plugins-bad/ext/openjpeg/gstopenjpegdec.c
@@ -1535,7 +1535,7 @@ gst_openjpeg_dec_decode_stripe (GstElement * element, gpointer user_data)
 
   ret = gst_openjpeg_dec_negotiate (self, image);
   if (ret != GST_FLOW_OK)
-    DECODE_ERROR (self, message, OPENJPEG_ERROR_NEGOCIATE, TRUE);
+    DECODE_ERROR (self, message, OPENJPEG_ERROR_NEGOTIATE, TRUE);
 
   if (message->frame->output_buffer == NULL) {
     ret = gst_video_decoder_allocate_output_frame (decoder, message->frame);
@@ -1751,9 +1751,9 @@ error:
       GST_ELEMENT_ERROR (self, LIBRARY, INIT,
           ("Failed to decode OpenJPEG data"), (NULL));
       break;
-    case OPENJPEG_ERROR_NEGOCIATE:
+    case OPENJPEG_ERROR_NEGOTIATE:
       GST_ELEMENT_ERROR (self, LIBRARY, INIT,
-          ("Failed to negociate OpenJPEG data"), (NULL));
+          ("Failed to negotiate OpenJPEG data"), (NULL));
       break;
     case OPENJPEG_ERROR_ALLOCATE:
       GST_ELEMENT_ERROR (self, LIBRARY, INIT,
@@ -1761,7 +1761,7 @@ error:
       break;
     default:
       GST_ELEMENT_ERROR (self, LIBRARY, INIT,
-          ("Failed to encode OpenJPEG data"), (NULL));
+          ("Failed to decode OpenJPEG data"), (NULL));
       break;
   }
 
