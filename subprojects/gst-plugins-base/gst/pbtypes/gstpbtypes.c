644854e558fef1046f54e397f645610b8f7e2349
Seungha Yang <seungha@centricular.com>
2026-09-19 14:48:48 +0900

    pbtypes: Register caption meta to meta factory
    ... so that it can be automatically registered on deserialization
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/10335>

--- subprojects/gst-plugins-base/gst/pbtypes/gstpbtypes.c
+++ subprojects/gst-plugins-base/gst/pbtypes/gstpbtypes.c
@@ -37,7 +37,8 @@ plugin_init (GstPlugin * plugin)
 
   return GST_DYNAMIC_TYPE_REGISTER (video_multiview_flagset, plugin) &&
       gst_meta_factory_register (plugin, gst_video_meta_get_info ()) &&
-      gst_meta_factory_register (plugin, gst_audio_meta_get_info ());
+      gst_meta_factory_register (plugin, gst_audio_meta_get_info ()) &&
+      gst_meta_factory_register (plugin, gst_video_caption_meta_get_info ());
 }
 
 GST_PLUGIN_DEFINE (GST_VERSION_MAJOR,
