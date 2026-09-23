e337f646862469369ed85f11c552301e9804e885
Sebastian Dröge <sebastian@centricular.com>
2026-09-23 09:44:55 +0300

    rtpmparobustdepay: Check for enough available data in all cases before parsing payload descriptor
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/12530>

--- subprojects/gst-plugins-good/gst/rtp/gstrtpmparobustdepay.c
+++ subprojects/gst-plugins-good/gst/rtp/gstrtpmparobustdepay.c
@@ -682,18 +682,19 @@ gst_rtp_mpa_robust_depay_process (GstRTPBaseDepayload * depayload,
    */
   while (payload_len) {
     if (G_LIKELY (rtpmpadepay->has_descriptor)) {
+      if (payload_len < 2)
+        goto short_read;
+
       cont = !!(payload[offset] & 0x80);
       dtype = !!(payload[offset] & 0x40);
       if (dtype) {
         size = (payload[offset] & 0x3f) << 8 | payload[offset + 1];
         payload_len--;
         offset++;
-      } else if (payload_len >= 2) {
+      } else {
         size = (payload[offset] & 0x3f);
         payload_len -= 2;
         offset += 2;
-      } else {
-        goto short_read;
       }
     } else {
       cont = FALSE;
